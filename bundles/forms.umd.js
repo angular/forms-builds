fb4fb60955dd9666921d3b75b784f95854d36cc3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-11-30 22:39:18 +0000

    f275f36 fix(version): take all of version string after patch version

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-beta.0-e628b66
+ * @license Angular v2.3.0-beta.0-f275f36
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5653,7 +5653,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-beta.0-e628b66');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-beta.0-f275f36');
 
     exports.VERSION = VERSION;
     exports.AbstractControlDirective = AbstractControlDirective;
