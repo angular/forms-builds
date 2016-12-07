3f1cd3e4d1f4ac92a71c46fbbf884d99086cae8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 01:23:23 +0000

    4a09c81 fix(language-service): do not throw for invalid metadata (#13261)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-rc.0-16efb13
+ * @license Angular v2.3.0-rc.0-4a09c81
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-16efb13');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-rc.0-4a09c81');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption, NgSelectMultipleOption, DefaultValueAccessor, NumberValueAccessor,
