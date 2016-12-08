28b40f99cdb71a201afb3422400b93cf06c03e43
GitHub <noreply@github.com>
2016-12-08 22:01:46 +0000

    1b54788 build(tslint): enable no-inner-declarations (#13316)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-9591a08
+ * @license Angular v2.3.0-1b54788
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-9591a08');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-1b54788');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption, NgSelectMultipleOption, DefaultValueAccessor, NumberValueAccessor,
