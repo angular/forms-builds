11cdaa18e8682360b0b88c276f7c9ee83be64904
Victor Berchet <victor@suumit.com>
2016-12-09 23:50:33 +0000

    90c2235 feat(http): simplify URLSearchParams creation (#13338)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-a006c14
+ * @license Angular v2.3.0-90c2235
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-a006c14');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-90c2235');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption, NgSelectMultipleOption, DefaultValueAccessor, NumberValueAccessor,
