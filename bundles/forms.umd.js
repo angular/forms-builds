3d8fe31bb2091c1663cef909271f29cad864384a
Victor Berchet <victor@suumit.com>
2016-12-09 02:06:18 +0000

    4e3d58a Revert "fix(compiler): fix transpiled ES5 code (#13322)"

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-61d7c1e
+ * @license Angular v2.3.0-4e3d58a
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-61d7c1e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4e3d58a');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption, NgSelectMultipleOption, DefaultValueAccessor, NumberValueAccessor,
