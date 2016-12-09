ddba0e0b6ec4003d8856c11f30370c413bee06a1
Victor Berchet <victor@suumit.com>
2016-12-09 02:55:21 +0000

    5c6ec20 refactor(router): simplify regexp

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-4e3d58a
+ * @license Angular v2.3.0-5c6ec20
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-4e3d58a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-5c6ec20');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption, NgSelectMultipleOption, DefaultValueAccessor, NumberValueAccessor,
