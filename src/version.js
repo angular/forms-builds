5488fb87811e26a232bd0b278dfd83aeef60d2f5
GitHub <noreply@github.com>
2016-12-13 20:48:03 +0000

    a8d2375 test(i18n): fix a typo in the reference xmb (#13441)

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-d036165');
+export var /** @type {?} */ VERSION = new Version('2.3.0-a8d2375');
 //# sourceMappingURL=version.js.map
