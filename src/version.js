0db541963dd23c67ea15d7abcb4df544b373743a
Victor Berchet <victor@suumit.com>
2016-12-13 20:46:28 +0000

    d17e690 test(upgrade): fix failing test in browsers which do not support RAF

--- src/version.js
+++ src/version.js
@@ -9,5 +9,5 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-714f2af');
+export var /** @type {?} */ VERSION = new Version('2.3.0-d17e690');
 //# sourceMappingURL=version.js.map
