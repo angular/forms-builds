1a27d84cdb7f3699f82345e02c690537720f3971
Victor Berchet <victor@suumit.com>
2016-12-14 23:26:21 +0000

    c65b4fa refactor: format & lint

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-169ed82
+ * @license Angular v2.3.0-c65b4fa
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-169ed82');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-c65b4fa');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
