6291005a03a94bbaf1e28783a463c258c2ba4306
Victor Berchet <victor@suumit.com>
2016-12-14 23:50:30 +0000

    fa9e21e fix(compiler): fix merge error in compiler_host

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-c65b4fa
+ * @license Angular v2.3.0-fa9e21e
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-c65b4fa');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-fa9e21e');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
