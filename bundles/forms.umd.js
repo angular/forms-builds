7d0df2d9a91926f4a256c45d989c195d33a0e705
Victor Berchet <victor@suumit.com>
2016-12-14 16:47:25 +0000

    3edca4d fix(core): properly destroy embedded Views attatched to ApplicationRef (#13459)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-a0a0504
+ * @license Angular v2.3.0-3edca4d
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5593,7 +5593,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-a0a0504');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-3edca4d');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
