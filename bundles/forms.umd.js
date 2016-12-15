58384810156b0860c7e6fd5e11ca0b3177a7c54c
Chuck Jazdzewski <chuckj@google.com>
2016-12-15 20:18:54 +0000

    14e785f fix(build): use bash string comparison operator (#13502)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-33910dd
+ * @license Angular v4.0.0-beta.0-14e785f
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-33910dd');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-14e785f');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
