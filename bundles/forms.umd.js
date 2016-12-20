6091dd6967c2c6344a5971d1a1a23aeab9189527
Chuck Jazdzewski <chuckj@google.com>
2016-12-20 02:15:57 +0000

    6efdf84 docs(changelog): add changelog for 2.4.0

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-171a9bd
+ * @license Angular v2.4.0-6efdf84
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,24 +5650,9 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-171a9bd');
-
-    var NgNovalidate = (function () {
-        function NgNovalidate() {
-        }
-        NgNovalidate.decorators = [
-            { type: _angular_core.Directive, args: [{
-                        selector: 'form:not([ngNoForm])',
-                        host: { 'novalidate': '' },
-                    },] },
-        ];
-        /** @nocollapse */
-        NgNovalidate.ctorParameters = function () { return []; };
-        return NgNovalidate;
-    }());
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.0-6efdf84');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
-        NgNovalidate,
         NgSelectOption,
         NgSelectMultipleOption,
         DefaultValueAccessor,
