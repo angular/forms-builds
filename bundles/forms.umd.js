b0ec740063ea8c5bf564d7b357f5c1a32f73a049
Chuck Jazdzewski <chuckj@google.com>
2016-12-21 00:30:15 +0000

    383adc9 fix(core): improve error message when component factory cannot be found (#13541)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-9b8488f
+ * @license Angular v4.0.0-beta.0-383adc9
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-9b8488f');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-383adc9');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
