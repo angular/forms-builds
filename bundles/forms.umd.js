50135b85a7b4a3b9a92b5412d82f9062a7330a9e
GitHub <noreply@github.com>
2016-12-21 02:02:33 +0000

    e69c1fb refactor(platform-browser): resolver merge conflict for tslint (#13601)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.0-fcd116f
+ * @license Angular v4.0.0-beta.0-e69c1fb
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-fcd116f');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.0-e69c1fb');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
