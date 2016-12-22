7657eb66a0484d8583674a66a265fc9d73174958
Hans <hans@hansl.ca>
2016-12-22 20:49:41 +0000

    e7ece6c fixed minor typo (#13626)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-f114e40
+ * @license Angular v4.0.0-beta.1-e7ece6c
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-f114e40');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-e7ece6c');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
