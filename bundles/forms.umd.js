e8c42d09d7bb917fd11f0109d797a5c27fbacfa6
Hans <hans@hansl.ca>
2016-12-27 23:35:54 +0000

    0eca960 fix(router): fix lazy loaded module with wildcard route (#13649)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-eed8344
+ * @license Angular v4.0.0-beta.1-0eca960
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-eed8344');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-0eca960');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
