1f87afa77531c85db821dd68bcc259e4aebe7cc7
Hans <hans@hansl.ca>
2016-12-27 22:16:06 +0000

    445ed43 fix(compiler): throw an error for invalid provider (#13544)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-174334d
+ * @license Angular v4.0.0-beta.1-445ed43
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-174334d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-445ed43');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
