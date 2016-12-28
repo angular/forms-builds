e3b57e0229783cbce3fdc5514fa75ea44704998f
Hans <hans@hansl.ca>
2016-12-28 00:10:06 +0000

    07e0fce fix(router): update route snapshot before emit new values (#13558)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-0ac8e10
+ * @license Angular v4.0.0-beta.1-07e0fce
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-0ac8e10');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-07e0fce');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
