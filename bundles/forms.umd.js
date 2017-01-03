e9fbca5cbebb3c8f26f80157dbc81dd2d3a144ef
Hans <hans@hansl.ca>
2017-01-03 18:17:31 +0000

    c5c53f3 fix(core): Remove reference to "Angular 2" in dev mode warning (#13751)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.1-bb0d23f
+ * @license Angular v4.0.0-beta.1-c5c53f3
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5650,7 +5650,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-bb0d23f');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.1-c5c53f3');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
