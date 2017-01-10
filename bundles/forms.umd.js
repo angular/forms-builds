2cc80c5e0eef009fff43d1dcdcc8e03295f9ffaa
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 00:09:28 +0000

    61ba223 fix(router): throw an error when navigate to null/undefined path

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-6164eb2
+ * @license Angular v2.4.2-61ba223
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5651,7 +5651,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.2-6164eb2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.2-61ba223');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
