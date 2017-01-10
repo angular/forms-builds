2d758cfaaa09a11a6942394b8a76f4a6a976afbb
Matias Niemelä <matias@yearofmoo.com>
2017-01-10 01:43:23 +0000

    9aeb8c5 refactor(test): `<template>`/`<ng-container>`/*-directives

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.2-f14d549
+ * @license Angular v4.0.0-beta.2-9aeb8c5
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5651,7 +5651,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.2-f14d549');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.2-9aeb8c5');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
