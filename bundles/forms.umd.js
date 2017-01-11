c295aed1219bcdb8b0261f81e4bff58777883d12
Matias Niemelä <matias@yearofmoo.com>
2017-01-11 22:20:26 +0000

    a7d2804 docs(changelog): add changelog for 2.4.3

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-4b56f79
+ * @license Angular v2.4.3-a7d2804
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5651,7 +5651,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.2-4b56f79');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.3-a7d2804');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
