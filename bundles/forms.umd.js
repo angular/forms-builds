a75e172fdf3997844291c6b1b413ee84cfc624ff
Matias Niemelä <matias@yearofmoo.com>
2017-01-11 02:00:55 +0000

    d7f2a3c fix(i18n): translate attributes inside elements marked for translation

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.2-d43e5dd
+ * @license Angular v2.4.2-d7f2a3c
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5651,7 +5651,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.2-d43e5dd');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.2-d7f2a3c');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
