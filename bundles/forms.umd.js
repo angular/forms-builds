0ef4833518395908cbf40c68252b937d6b3fed66
Miško Hevery <misko@hevery.com>
2017-01-13 21:28:24 +0000

    5237b1c chore(compiler-cli): Move calculateEmitPath into CompilerHost (#13904)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-dc63cef
+ * @license Angular v4.0.0-beta.3-5237b1c
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5651,7 +5651,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.3-dc63cef');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.3-5237b1c');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
