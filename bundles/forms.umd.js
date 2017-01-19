fb35d66e9430b5a25d93a149751600477a00d0bc
Miško Hevery <misko@hevery.com>
2017-01-19 00:07:44 +0000

    2d7b3a8 refactor(core): remove an unused import in application_ref (#13901)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.3-523fd84
+ * @license Angular v4.0.0-beta.3-2d7b3a8
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5758,7 +5758,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.3-523fd84');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.3-2d7b3a8');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
