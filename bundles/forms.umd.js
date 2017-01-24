27293a83eb39807044795608d95d396b53524067
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-01-24 23:05:16 +0000

    fc55018 ci: disable broken commit message validation

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.4-0c7726d
+ * @license Angular v4.0.0-beta.4-fc55018
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5758,7 +5758,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-0c7726d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.4-fc55018');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
