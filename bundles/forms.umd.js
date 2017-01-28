b4f1b6b6c2c0b2f8dc22c0103e34d2c48aa49fab
Igor Minar <iminar@google.com>
2017-01-28 05:38:30 +0000

    bc1320d docs: update COMITTER.md with info about pullapprove.com

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-01da422
+ * @license Angular v4.0.0-beta.5-bc1320d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5758,7 +5758,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-01da422');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-bc1320d');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
