7686e81ceb0d3e58433ba5f4990aefe392be492f
Miško Hevery <misko@hevery.com>
2017-01-28 04:45:17 +0000

    df7f5fc style: fix typo in CHANGELOG.md (#14009)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.5-24ea3f0
+ * @license Angular v4.0.0-beta.5-df7f5fc
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5758,7 +5758,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-24ea3f0');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.5-df7f5fc');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
