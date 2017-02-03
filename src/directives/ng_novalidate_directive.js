4b995469154f80f8262f69c3e953048b349062e3
Misko Hevery <misko@hevery.com>
2017-02-03 05:42:29 +0000

    559cf9d docs(changelog): add changelog for 4.0.0-beta.6

---
+++ src/directives/ng_novalidate_directive.js
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Directive } from '@angular/core/index';
+export class NgNovalidate {
+}
+NgNovalidate.decorators = [
+    { type: Directive, args: [{
+                selector: 'form:not([ngNoForm])',
+                host: { 'novalidate': '' },
+            },] },
+];
+/** @nocollapse */
+NgNovalidate.ctorParameters = () => [];
+function NgNovalidate_tsickle_Closure_declarations() {
+    /** @type {?} */
+    NgNovalidate.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    NgNovalidate.ctorParameters;
+}
+//# sourceMappingURL=ng_novalidate_directive.js.map
