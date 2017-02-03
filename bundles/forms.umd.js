0c351427a958e5da1ed706b56c713130385b7b96
Igor Minar <iminar@google.com>
2017-02-03 07:46:34 +0000

    a909643 feat(aio): delete unused kruft

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-559cf9d
+ * @license Angular v4.0.0-beta.6-a909643
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5965,7 +5965,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-559cf9d');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-a909643');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
