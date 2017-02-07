f766032f0ffb63f20aa47df5bfa870568f3fbde4
Miško Hevery <misko@hevery.com>
2017-02-07 18:17:25 +0000

    5bb47db fix(upgrade): allow non-element selectors for downgraded components (#14291)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.6-343ee8a
+ * @license Angular v2.4.6-5bb47db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5766,7 +5766,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.6-343ee8a');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.6-5bb47db');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
