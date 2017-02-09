1e69486e72cd91b3f742c3750eed1a503efc28e9
Miško Hevery <misko@hevery.com>
2017-02-09 19:03:03 +0000

    5e7a2fa build: update jasmine to 2.4 (#14362)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-09b4bd0
+ * @license Angular v4.0.0-beta.7-5e7a2fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5984,7 +5984,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-09b4bd0');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-5e7a2fa');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
