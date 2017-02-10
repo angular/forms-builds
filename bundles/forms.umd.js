fe94107a8e21a50794b5c2062e17c84368b767c8
Miško Hevery <misko@hevery.com>
2017-02-10 21:09:36 +0000

    7c87c52 fix(upgrade): pass correct values to `ngOnChanges` for interpolation bindings (#14400)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.7-541de26
+ * @license Angular v2.4.7-7c87c52
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5766,7 +5766,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.7-541de26');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.7-7c87c52');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
