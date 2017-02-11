2f898cf774af5c57cc516770db8361b3da8b9507
Misko Hevery <misko@hevery.com>
2017-02-11 05:16:08 +0000

    d3f174a ci: fix validate-commit-messages

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-8dd16bb
+ * @license Angular v4.0.0-beta.7-d3f174a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-8dd16bb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-d3f174a');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
