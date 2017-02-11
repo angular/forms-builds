72c4e505c2c72f8810aa7050bb5fd3ed6d5756e8
Miško Hevery <misko@hevery.com>
2017-02-11 04:49:27 +0000

    8dd16bb test: add missing change to test-main.js

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-5279d06
+ * @license Angular v4.0.0-beta.7-8dd16bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-5279d06');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-8dd16bb');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
