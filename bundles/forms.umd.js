c63d385a5c0708bd6cc66bc0cf03afa8ecfa6632
Miško Hevery <misko@hevery.com>
2017-02-14 01:11:53 +0000

    e4e9dbe feat(compiler): integrate compiler with view engine - change detection tests work (#14412)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-1dc9be4
+ * @license Angular v4.0.0-beta.7-e4e9dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-1dc9be4');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-e4e9dbe');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
