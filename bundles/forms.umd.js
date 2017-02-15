a27a62ab68ab78f37b81a4b7377e66d30b9a5905
Igor Minar <iminar@google.com>
2017-02-15 16:58:44 +0000

    4e7752a feat(compiler): integrate compiler with view engine (#14487)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-e9ba7aa
+ * @license Angular v4.0.0-beta.7-4e7752a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-e9ba7aa');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-4e7752a');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
