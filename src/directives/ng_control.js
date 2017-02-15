7df642551522bf7a7ba1cda55a815f8c28821552
GitHub <noreply@github.com>
2017-02-15 16:56:06 +0000

    59b2b65 build: update .pullapprove

---
+++ src/directives/ng_control.js
@@ -0,0 +1,89 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+import { AbstractControlDirective } from './abstract_control_directive';
+/**
+ * @return {?}
+ */
+function unimplemented() {
+    throw new Error('unimplemented');
+}
+/**
+ * A base class that all control directive extend.
+ * It binds a {\@link FormControl} object to a DOM element.
+ *
+ * Used internally by Angular forms.
+ *
+ * \@stable
+ * @abstract
+ */
+var NgControl = (function (_super) {
+    __extends(NgControl, _super);
+    function NgControl() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        /** @internal */
+        _this._parent = null;
+        _this.name = null;
+        _this.valueAccessor = null;
+        /** @internal */
+        _this._rawValidators = [];
+        /** @internal */
+        _this._rawAsyncValidators = [];
+        return _this;
+    }
+    Object.defineProperty(NgControl.prototype, "validator", {
+        /**
+         * @return {?}
+         */
+        get: function () { return (unimplemented()); },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(NgControl.prototype, "asyncValidator", {
+        /**
+         * @return {?}
+         */
+        get: function () { return (unimplemented()); },
+        enumerable: true,
+        configurable: true
+    });
+    /**
+     * @abstract
+     * @param {?} newValue
+     * @return {?}
+     */
+    NgControl.prototype.viewToModelUpdate = function (newValue) { };
+    return NgControl;
+}(AbstractControlDirective));
+export { NgControl };
+function NgControl_tsickle_Closure_declarations() {
+    /**
+     * \@internal
+     * @type {?}
+     */
+    NgControl.prototype._parent;
+    /** @type {?} */
+    NgControl.prototype.name;
+    /** @type {?} */
+    NgControl.prototype.valueAccessor;
+    /**
+     * \@internal
+     * @type {?}
+     */
+    NgControl.prototype._rawValidators;
+    /**
+     * \@internal
+     * @type {?}
+     */
+    NgControl.prototype._rawAsyncValidators;
+}
+//# sourceMappingURL=ng_control.js.map
