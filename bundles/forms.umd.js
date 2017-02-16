c11812a283ba6268aceec58b362a36f6e9b172aa
Igor Minar <iminar@google.com>
2017-02-16 23:07:21 +0000

    88eb3b2 fix(aio): fix prod build (#14541)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-0fa3895
+ * @license Angular v4.0.0-beta.7-88eb3b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-0fa3895');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-88eb3b2');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
