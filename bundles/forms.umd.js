f4d6796d21244723c52a191bb671aaf8cb3fb507
GitHub <noreply@github.com>
2017-02-16 23:46:25 +0000

    8c20aaa fix(aio): use cleanUrls option rather than bruteforce rewriting rule (#14547)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-88eb3b2
+ * @license Angular v4.0.0-beta.7-8c20aaa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-88eb3b2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-8c20aaa');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
