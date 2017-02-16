7ac2696ad26858d2be62ccaaa943cacd108524b2
Igor Minar <iminar@google.com>
2017-02-16 18:38:57 +0000

    612f120 fix(platform-server): default unspecified sections of the url to empty string (#14512)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.7-4a56b6e
+ * @license Angular v4.0.0-beta.7-612f120
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-4a56b6e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.7-612f120');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
