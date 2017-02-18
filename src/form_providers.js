3a54a6136dabd4b22a7b33ea42b64e2929e85a58
Igor Minar <igor@angularjs.org>
2017-02-18 23:36:46 +0000

    1bdf706 release: cut v4.0.0-beta.8

---
+++ src/form_providers.js
@@ -0,0 +1,68 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { NgModule } from '@angular/core';
+import { InternalFormsSharedModule, REACTIVE_DRIVEN_DIRECTIVES, TEMPLATE_DRIVEN_DIRECTIVES } from './directives';
+import { RadioControlRegistry } from './directives/radio_control_value_accessor';
+import { FormBuilder } from './form_builder';
+/**
+ * The ng module for forms.
+ * \@stable
+ */
+var FormsModule = (function () {
+    function FormsModule() {
+    }
+    return FormsModule;
+}());
+export { FormsModule };
+FormsModule.decorators = [
+    { type: NgModule, args: [{
+                declarations: TEMPLATE_DRIVEN_DIRECTIVES,
+                providers: [RadioControlRegistry],
+                exports: [InternalFormsSharedModule, TEMPLATE_DRIVEN_DIRECTIVES]
+            },] },
+];
+/** @nocollapse */
+FormsModule.ctorParameters = function () { return []; };
+function FormsModule_tsickle_Closure_declarations() {
+    /** @type {?} */
+    FormsModule.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    FormsModule.ctorParameters;
+}
+/**
+ * The ng module for reactive forms.
+ * \@stable
+ */
+var ReactiveFormsModule = (function () {
+    function ReactiveFormsModule() {
+    }
+    return ReactiveFormsModule;
+}());
+export { ReactiveFormsModule };
+ReactiveFormsModule.decorators = [
+    { type: NgModule, args: [{
+                declarations: [REACTIVE_DRIVEN_DIRECTIVES],
+                providers: [FormBuilder, RadioControlRegistry],
+                exports: [InternalFormsSharedModule, REACTIVE_DRIVEN_DIRECTIVES]
+            },] },
+];
+/** @nocollapse */
+ReactiveFormsModule.ctorParameters = function () { return []; };
+function ReactiveFormsModule_tsickle_Closure_declarations() {
+    /** @type {?} */
+    ReactiveFormsModule.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    ReactiveFormsModule.ctorParameters;
+}
+//# sourceMappingURL=form_providers.js.map
