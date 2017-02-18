91caf4b8fa2375b214d2cdbc332b1c2933facd6f
Igor Minar <igor@angularjs.org>
2017-02-18 22:35:50 +0000

    4f93ac8 release: cut v2.4.8

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.7-612950b
+ * @license Angular v2.4.8-4f93ac8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5770,7 +5770,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.7-612950b');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-4f93ac8');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
