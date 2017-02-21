4c1b9821b5e130c00920fded018b1166647b107c
Victor Berchet <victor@suumit.com>
2017-02-21 00:39:20 +0000

    5f3c844 refactor(router): misc

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-601fd3e
+ * @license Angular v4.0.0-beta.8-5f3c844
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5988,7 +5988,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-601fd3e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-5f3c844');
 
     var NgNovalidate = (function () {
         function NgNovalidate() {
