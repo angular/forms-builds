b0f4a0ee2b63ff00dee4d39b5e7ff4015404a487
Igor Minar <iminar@google.com>
2017-02-21 17:33:16 +0000

    bb0460b refactor(core): don’t use switch fall through but rather multiple `if`s

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-2ddd1c3
+ * @license Angular v4.0.0-beta.8-bb0460b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5995,7 +5995,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-2ddd1c3');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-bb0460b');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
