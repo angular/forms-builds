85b7c255775096cffaf6a7b8291cf8037b364978
GitHub <noreply@github.com>
2017-02-21 01:13:28 +0000

    2a12346 fix(router): improve robustness (#14602)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.8-4f93ac8
+ * @license Angular v2.4.8-2a12346
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5770,7 +5770,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-4f93ac8');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.8-2a12346');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
