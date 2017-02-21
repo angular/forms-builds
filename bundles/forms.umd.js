b5084bc6ee9d8f037e7a5910715befb04a738858
GitHub <noreply@github.com>
2017-02-21 02:14:23 +0000

    fbe4b76 fix(http): Make ResponseOptionsArgs an interface (#14607)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-8742432
+ * @license Angular v4.0.0-beta.8-fbe4b76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5995,7 +5995,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-8742432');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.8-fbe4b76');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
