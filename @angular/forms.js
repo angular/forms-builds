1aa691929ba15185aab32c5bd6c9b95b075dcc58
Igor Minar <iminar@google.com>
2017-02-23 01:12:17 +0000

    a1d4769 feat: add a PLATFORM_ID token that provides a platform id Object. (#14647)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-88bc143
+ * @license Angular v4.0.0-beta.8-a1d4769
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-88bc143');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-a1d4769');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
