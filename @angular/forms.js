21834d309ab0ded85915fcab97ef746aad2ae05a
Igor Minar <iminar@google.com>
2017-02-23 22:22:05 +0000

    4b54c0e refactor(core): enable new animations dsl

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-187f7b6
+ * @license Angular v4.0.0-beta.8-4b54c0e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-187f7b6');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-4b54c0e');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
