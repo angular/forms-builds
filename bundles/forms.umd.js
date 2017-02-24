df9f3c064b8295e5a009187e0747ce3571c7480e
Igor Minar <iminar@google.com>
2017-02-24 08:53:41 +0000

    32c2fd5 refactor(animations): change NoOp -> Noop (#14690)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-1f3198c');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-32c2fd5');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
