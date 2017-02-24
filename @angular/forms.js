86a13194a272c969a997aa7dd1ed8aa0c664a966
Igor Minar <iminar@google.com>
2017-02-24 20:32:19 +0000

    436a179 fix(animations): properly cache renderer and namespace triggers (#14703)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-5094aef
+ * @license Angular v4.0.0-beta.8-436a179
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-5094aef');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-436a179');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-5094aef');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-436a179');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
