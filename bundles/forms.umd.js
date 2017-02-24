bb25accea8a291f008c4256c2be86a050087370d
Igor Minar <iminar@google.com>
2017-02-24 07:15:46 +0000

    2da3844 build(language-service): support deep import bundling of language services (#14689)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-41da599');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-2da3844');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
