cdc980b178e1eccb72a6712ae5ed50bbb2a41d22
Igor Minar <iminar@google.com>
2017-02-24 02:24:53 +0000

    ee747f7 refactor(core): change abstract classes for interfaces (#12324)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-a23634d');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-ee747f7');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
