bdaf43069c06085cc6cef8d29c25e60a04c5403d
Igor Minar <iminar@google.com>
2017-02-24 23:38:15 +0000

    932a02f build(language-service): fix tsconfig needed for language-service rollup (#14717)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-3c9a46c');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-932a02f');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
