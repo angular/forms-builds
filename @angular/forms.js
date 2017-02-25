bd40db94b1225f2079531727d68de6a1c771e88d
Igor Minar <igor@angularjs.org>
2017-02-25 02:29:05 +0000

    c7d1b36 docs: update release schedule due to bad 4.0.0-rc.0 release

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-2602b03
+ * @license Angular v4.0.0-rc.1-c7d1b36
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-2602b03');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-c7d1b36');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
