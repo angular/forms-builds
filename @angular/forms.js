771229e7172f828e4f2949fa160ae0a155ee783b
Igor Minar <igor@angularjs.org>
2017-02-25 02:43:47 +0000

    26ed262 docs: fix typo in the changelog

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-c7d1b36
+ * @license Angular v4.0.0-rc.1-26ed262
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-c7d1b36');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-26ed262');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-c7d1b36');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-26ed262');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
