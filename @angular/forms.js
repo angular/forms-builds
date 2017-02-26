dd8710913a363573b50cf329bc0147b18b657283
GitHub <noreply@github.com>
2017-02-26 04:36:34 +0000

    c868457 docs: improve Flat ES Modules docs

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-f32f4de
+ * @license Angular v4.0.0-rc.1-c868457
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-f32f4de');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-c868457');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f32f4de');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-c868457');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
