9facc55c53dca1fd3248aa3b3bc756d81667be5a
Igor Minar <iminar@google.com>
2017-03-01 21:55:09 +0000

    6bae737 fix(language-service): tolerate errors in decorators (#14634)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-7a66a41');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-6bae737');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
