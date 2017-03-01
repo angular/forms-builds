23faf64c3fab940b08ac92cf11d96fe0c7a38aa7
Igor Minar <iminar@google.com>
2017-03-01 16:33:47 +0000

    49b462e test: closure integration test for change detection (#14691)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-d1182af
+ * @license Angular v4.0.0-rc.1-49b462e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-d1182af');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-49b462e');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-d1182af');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-49b462e');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
