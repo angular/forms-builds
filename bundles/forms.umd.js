a81c34861ba8275eb577f9fcc6008023e2cf52f7
Igor Minar <iminar@google.com>
2017-03-01 16:25:35 +0000

    d1182af refactor: change remaining `<template>` to `<ng-template>` (#14706)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5753de5');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-d1182af');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
