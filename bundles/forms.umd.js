ded5251bdfc0676bc92da9c98b471c1ff5f93b01
Igor Minar <iminar@google.com>
2017-03-01 23:48:18 +0000

    dd49901 ci: add NGBUILDS_IO_KEY to .travis.yaml for ngbuilds.io integration

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -5320,7 +5320,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-a6996a9');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-dd49901');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
