b46d33faa7855274e75247e065662bb463edb0d7
Igor Minar <iminar@google.com>
2017-03-01 16:22:13 +0000

    fc9e6b2 docs: add details about `OpaqueToken` deprecation in the changelog (#14790)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-968995a
+ * @license Angular v4.0.0-rc.1-fc9e6b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-968995a');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-fc9e6b2');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
