73a633fcab7c8150920d36c7109d1377f9ebf386
Igor Minar <iminar@google.com>
2017-03-01 02:45:50 +0000

    5ba55b0 fix(compiler): fix identifier names of `EMPTY_MAP` / `EMPTY_ARRAY` (#14806)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-126fda2
+ * @license Angular v4.0.0-rc.1-5ba55b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5408,7 +5408,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-126fda2');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-5ba55b0');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
