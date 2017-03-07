c59c0df3e751c93031f6b8dcdde444f9cba5bdef
Chuck Jazdzewski <chuckj@google.com>
2017-03-07 19:41:06 +0000

    1cff125 refactor: remove old compiler options (#14891)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -4986,7 +4986,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-07122f0');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-1cff125');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
