8b783a56ae1046d5b119ddd9d2b848385b871c8e
Chuck Jazdzewski <chuckj@google.com>
2017-03-08 00:56:58 +0000

    ad3b44a RendererV2 -> Renderer2 rename (#14998)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -4986,7 +4986,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-5df998d');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-ad3b44a');
 
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
