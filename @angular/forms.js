d94f10920f668c5e13c8782090da1a5639205b40
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 17:48:36 +0000

    0e2dd76 build: remove obsolete `ci-lite/` directory and files (#14994)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff71eff
+ * @license Angular v4.0.0-rc.3-0e2dd76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5181,7 +5181,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ff71eff');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-0e2dd76');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ff71eff
+ * @license Angular v4.0.0-rc.3-0e2dd76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-ff71eff');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-0e2dd76');
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
      *
