c61a876d3bf46ed363e67bac990a9fb088b40f3c
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:54:52 +0000

    75147ff fix(aio): remove invalid brace from CSS (#15122)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-018e5c9
+ * @license Angular v4.0.0-rc.3-75147ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5181,7 +5181,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-018e5c9');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-75147ff');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
