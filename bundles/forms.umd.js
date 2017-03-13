3200be57b86d156e6871201d9065a7d45e200d17
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 18:44:32 +0000

    6559425 fix(core): ErrorHandler should not rethrow an error by default (#15077)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-df914ef
+ * @license Angular v4.0.0-rc.3-6559425
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-df914ef');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-6559425');
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
      *
