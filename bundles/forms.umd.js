97c8fb762ba84f20d551ae53fc6084967964ff7d
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:40:08 +0000

    018e5c9 fix(platform-server): fix get/set title in parse5 adapter (#14965)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e7dab7e
+ * @license Angular v4.0.0-rc.3-018e5c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-e7dab7e');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-018e5c9');
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
      *
