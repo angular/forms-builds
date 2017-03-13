611924b65fb5381315d95e2c71799ece1c28ddf6
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 18:02:31 +0000

    df914ef fix(core): don’t recreate `TemplateRef` when used as a reference. (#15066)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-4e1cf5b
+ * @license Angular v4.0.0-rc.3-df914ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5560,7 +5560,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * @stable
  */
-var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-4e1cf5b');
+var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-df914ef');
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
  *
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-4e1cf5b
+ * @license Angular v4.0.0-rc.3-df914ef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5181,7 +5181,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-4e1cf5b');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-df914ef');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
