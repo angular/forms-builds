aaddc6e22f3d5b4bfcf6b15733b047a13b85eaeb
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 22:57:49 +0000

    80649ea fix(platform-server): correctly implement get href in parse5 adapter (#15022)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-778f7d6
+ * @license Angular v4.0.0-rc.3-80649ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5159,7 +5159,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-778f7d6');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-80649ea');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
