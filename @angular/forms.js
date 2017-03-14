21a825f0f8d811125c70f3d85711c83e5b449cd4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 15:22:38 +0000

    423bfb0 ci(aio): whitelist 'aio-contributors' for deploying PR previews

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2f250c1
+ * @license Angular v4.0.0-rc.3-423bfb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5181,7 +5181,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-2f250c1');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-423bfb0');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-2f250c1
+ * @license Angular v4.0.0-rc.3-423bfb0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2f250c1');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-423bfb0');
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
      *
