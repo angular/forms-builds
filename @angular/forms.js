7ffb6d58fa63f3888bff20409fe416330a968742
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 00:11:15 +0000

    8850098 build(aio): ensure API docs are generated in the correct place (#15103)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-e6c81d2
+ * @license Angular v4.0.0-rc.3-8850098
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5181,7 +5181,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-e6c81d2');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-8850098');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
