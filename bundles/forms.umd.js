31b8060892ae3beacda93eefdb2192f5cdc9a26a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 14:09:27 +0000

    2f250c1 refactor(aio): move window:resize handler into component

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5563,7 +5563,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-d4205bb');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-2f250c1');
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
      *
