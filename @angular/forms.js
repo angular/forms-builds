1a6a2e3e243f5d3b66a30bf4a171165b129448f3
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 04:03:14 +0000

    c10c060 feat(common): support `as` syntax in template/* bindings (#15025)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-5fe2d8f
+ * @license Angular v4.0.0-rc.3-c10c060
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5159,7 +5159,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-5fe2d8f');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-c10c060');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
