236dd8c9dc80ec414033cf8c779b1018ed8fe31f
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:45:10 +0000

    f1b33ab fix(http): Make ResponseOptionsArgs an interface (#14607) (#14623)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5611,7 +5611,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * @stable
  */
-var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-029d0f2');
+var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-f1b33ab');
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
  *
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-029d0f2');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-f1b33ab');
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
      *
