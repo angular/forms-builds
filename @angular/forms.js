a36ee5d1c7531df9f5d7f0a564ffd378afa45b57
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 03:58:51 +0000

    5fe2d8f fix(core): update peer dep on zone.js to ^0.8.2 (#15078)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-5c34066
+ * @license Angular v4.0.0-rc.3-5fe2d8f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5159,7 +5159,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-5c34066');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-5fe2d8f');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
