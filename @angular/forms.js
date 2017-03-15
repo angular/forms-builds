99e3d48a15aff9a3427cc0aee4b2a9efc9d7629a
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 20:57:01 +0000

    3f38c6f fix(animations): always fire callbacks even for noop animations (#15170)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-80112a9
+ * @license Angular v4.0.0-rc.3-3f38c6f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-80112a9');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3f38c6f');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
