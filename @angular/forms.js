d34d93feb3a315800853bbb036a1eb150a97cc3d
GitHub <noreply@github.com>
2017-03-15 20:54:35 +0000

    795638e ci: remove unused import from integration test (#15175)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-322bf7a
+ * @license Angular v4.0.0-rc.3-795638e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-322bf7a');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-795638e');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
