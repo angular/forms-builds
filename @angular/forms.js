787107cebdf6159273ff7e4c92780c50fe274db9
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 23:46:13 +0000

    3131581 Revert "fix(core): ErrorHandler should not rethrow an error by default (#15077)"

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6772c91
+ * @license Angular v4.0.0-rc.3-3131581
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-6772c91');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3131581');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
