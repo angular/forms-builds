75576da33479cbd6d8d931a484893f979843438b
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:28:47 +0000

    959a03a fix(compiler): fix utf8encode, move to sharted utils, add tests (#15076)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-3b1956b
+ * @license Angular v4.0.0-rc.3-959a03a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5533,7 +5533,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * @stable
  */
-var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3b1956b');
+var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-959a03a');
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
  *
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3b1956b
+ * @license Angular v4.0.0-rc.3-959a03a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5159,7 +5159,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3b1956b');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-959a03a');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
