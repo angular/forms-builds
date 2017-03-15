93e1aede88854707fc614240bb174022fb006f84
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 23:24:15 +0000

    bf98d9d fix(aio): scroll to hash fragment element on URL change

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a9b1880
+ * @license Angular v4.0.0-rc.3-bf98d9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5159,7 +5159,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-a9b1880');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-bf98d9d');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
