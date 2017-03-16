d6ad47dfaa00c8ce627eca8ee8aec6667e1aefde
Igor Minar <iminar@google.com>
2017-03-16 06:50:24 +0000

    480a407 build(aio): implement prerendering

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-480a407
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-923d0c5');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-480a407');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
