97f739219afb8c427b3920c46eadad5251819b47
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 05:07:25 +0000

    923d0c5 fix: correct UMD resolutions for platform-browser_animations (#15190)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6772c91
+ * @license Angular v4.0.0-rc.3-923d0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5614,7 +5614,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-6772c91');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-923d0c5');
     /**
      * \@whatItDoes Adds `novalidate` attribute to all forms by default.
      *
