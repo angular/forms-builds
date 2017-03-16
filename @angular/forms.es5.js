de4b838707cf7fc1af53d3926d48b5b6dcc27ea9
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 00:28:48 +0000

    923d0c5 fix: correct UMD resolutions for platform-browser_animations (#15190)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-013d806
+ * @license Angular v4.0.0-rc.3-923d0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5611,7 +5611,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * @stable
  */
-var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-013d806');
+var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-923d0c5');
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
  *
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-013d806
+ * @license Angular v4.0.0-rc.3-923d0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5237,7 +5237,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-013d806');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-923d0c5');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
