dec4d398fbf189031b1a8772f40853d3d7a4386d
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 00:49:41 +0000

    410aa33 build: fix paths to typings files so tsickle resolves imports correctly

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-994089d
+ * @license Angular v4.0.0-rc.3-410aa33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5227,7 +5227,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.3-994089d');
+const VERSION = new Version('4.0.0-rc.3-410aa33');
 
 /**
  * \@whatItDoes Adds `novalidate` attribute to all forms by default.
