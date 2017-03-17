f4e5d93d98b8ff15a46e0f7ad48e04fa64f051ca
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 02:50:48 +0000

    6b57928 docs: add changelog for 2.4.10

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.4.9-b0ae464
+ * @license Angular v2.4.10-6b57928
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5770,7 +5770,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.9-b0ae464');
+    var /** @type {?} */ VERSION = new _angular_core.Version('2.4.10-6b57928');
 
     var /** @type {?} */ SHARED_FORM_DIRECTIVES = [
         NgSelectOption,
