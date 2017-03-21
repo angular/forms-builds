88676fe8ca8e9554ec06d9a19f8daeb674b4198a
Miško Hevery <misko@hevery.com>
2017-03-21 19:40:26 +0000

    8e6995c fix(core): stringify shouldn't throw when toString returns null/undefined (#14975)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-2489e4b
+ * @license Angular v4.0.0-rc.5-8e6995c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5421,7 +5421,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-2489e4b');
+const VERSION = new Version('4.0.0-rc.5-8e6995c');
 
 /**
  * @license
