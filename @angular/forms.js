7c37348fd8389778755a58ba4c4a0d25eaefaafd
Igor Minar <iminar@google.com>
2017-03-22 22:29:26 +0000

    a50d79d fix(compiler-cli): adding missing format xliff for the extractor (#15386)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5455,7 +5455,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-64285a2');
+const VERSION = new Version('4.0.0-rc.5-a50d79d');
 
 /**
  * @license
