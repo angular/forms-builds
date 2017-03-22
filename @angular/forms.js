138405ec6f00b8bc624249ed30af6ca6178938f9
Miško Hevery <misko@hevery.com>
2017-03-22 16:56:28 +0000

    c584997 fix(tsc-wrapped): use windows friendly path normalization in bundler (#15374)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-1bcbcfd
+ * @license Angular v4.0.0-rc.5-c584997
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5455,7 +5455,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-1bcbcfd');
+const VERSION = new Version('4.0.0-rc.5-c584997');
 
 /**
  * @license
