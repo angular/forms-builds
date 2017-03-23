3698c382e1f21ed96eaaf18b2a5d5b6911ed3863
Igor Minar <iminar@google.com>
2017-03-23 00:31:10 +0000

    26f6bd4 ci(travis): update excluded branch to g3 (#15391)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea49a95
+ * @license Angular v4.0.0-rc.5-26f6bd4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5455,7 +5455,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-ea49a95');
+const VERSION = new Version('4.0.0-rc.5-26f6bd4');
 
 /**
  * @license
