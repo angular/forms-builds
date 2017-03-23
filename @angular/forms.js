98ee4fd22dd3d555098cc9f7fc55e6b48d9ce9c5
Victor Berchet <victor@suumit.com>
2017-03-23 20:55:47 +0000

    0dda01e fix(compiler): correctly handle when `toString` is exported (#15430)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-92084f2
+ * @license Angular v4.0.0-rc.6-0dda01e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5455,7 +5455,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-92084f2');
+const VERSION = new Version('4.0.0-rc.6-0dda01e');
 
 /**
  * @license
