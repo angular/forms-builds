186476c266d3276be8fe0c3cd23a445d85c445d3
Victor Berchet <victor@suumit.com>
2017-03-23 17:44:18 +0000

    376088d build(aio): delete content folder before doc-gen (#15414)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-73808dd
+ * @license Angular v4.0.0-rc.6-376088d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5455,7 +5455,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-73808dd');
+const VERSION = new Version('4.0.0-rc.6-376088d');
 
 /**
  * @license
