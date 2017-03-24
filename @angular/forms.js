4b92abf246b84164f033d347a64abc41b7a9426a
GitHub <noreply@github.com>
2017-03-24 01:01:29 +0000

    c65b754 docs: add the 4.0.0 code name to the changelog

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-db0dca3
+ * @license Angular v4.0.0-c65b754
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5455,7 +5455,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-db0dca3');
+const VERSION = new Version('4.0.0-c65b754');
 
 /**
  * @license
