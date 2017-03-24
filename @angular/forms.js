1278ea3f54d2b720f89bc4c3770e78553402bfbc
Igor Minar <iminar@google.com>
2017-03-24 15:37:43 +0000

    d92930e docs: fixed broken links (#15455)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c2892da
+ * @license Angular v4.0.0-d92930e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5455,7 +5455,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-c2892da');
+const VERSION = new Version('4.0.0-d92930e');
 
 /**
  * @license
