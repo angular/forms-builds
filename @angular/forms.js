8f0875a465ad7310d2c689892f9f86e576e0b659
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 14:39:24 +0000

    c9e51d9 feat(aio): add new classes, table and images styling (#15478)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b283038
+ * @license Angular v4.0.0-c9e51d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-b283038');
+const VERSION = new Version('4.0.0-c9e51d9');
 
 /**
  * @license
