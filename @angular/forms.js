d344697f16379f1393c37d574fb895fb1594e338
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 09:30:46 +0000

    52ea193 build(aio): left align code regions

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-228238e');
+const VERSION = new Version('4.0.0-52ea193');
 
 /**
  * @license
