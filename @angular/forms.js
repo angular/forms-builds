67ea4692673aec80a8e54b57835c3496aceeac21
Victor Berchet <victor@suumit.com>
2017-03-28 20:49:41 +0000

    6269d28 fix(language-service): improve performance of `updateModuleAnalysis()` (#15543)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-a5c972a
+ * @license Angular v4.0.0-6269d28
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-a5c972a');
+const VERSION = new Version('4.0.0-6269d28');
 
 /**
  * @license
