8dc9365bc745f98e05321156f3d907bc31412a0c
Victor Berchet <victor@suumit.com>
2017-03-28 16:59:00 +0000

    d05aa70 docs(core): fix typo and example in InjectionToken doc (#15449)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-eac99c1
+ * @license Angular v4.0.0-d05aa70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-eac99c1');
+const VERSION = new Version('4.0.0-d05aa70');
 
 /**
  * @license
