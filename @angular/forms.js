08bf763c50611ab58e6b8a3a252a0593f8ab7c20
Victor Berchet <victor@suumit.com>
2017-03-28 23:24:11 +0000

    a580f8c fix(animations): make sure style calculations are not computed too early (#15540)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-f368381
+ * @license Angular v4.0.0-a580f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-f368381');
+const VERSION = new Version('4.0.0-a580f8c');
 
 /**
  * @license
