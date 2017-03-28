9c8c1142af7f5b7c23945c17e5e747e17b9ba6e8
Victor Berchet <victor@suumit.com>
2017-03-28 20:51:08 +0000

    aa16ccd fix(core): check for undefined on normalizeDebugBindingValue (#15503)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d438b88
+ * @license Angular v4.0.0-aa16ccd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-d438b88');
+const VERSION = new Version('4.0.0-aa16ccd');
 
 /**
  * @license
