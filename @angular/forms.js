6ac32904ebfc3ba33d2bcac31dfcfeb176f13570
Victor Berchet <victor@suumit.com>
2017-03-29 17:36:45 +0000

    331b9f6 fix(language-service): don't require `reflect-metadata` module to be provided (#15569)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d58a242
+ * @license Angular v4.0.0-331b9f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-d58a242');
+const VERSION = new Version('4.0.0-331b9f6');
 
 /**
  * @license
