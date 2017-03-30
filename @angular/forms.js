a6b6f4584e1d711ab388655f90f07b02fbe5cd9b
Victor Berchet <victor@suumit.com>
2017-03-30 00:00:08 +0000

    6b79ab5 docs: add changelog for 4.0.1

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-ca66530
+ * @license Angular v4.0.1-6b79ab5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-ca66530');
+const VERSION = new Version('4.0.1-6b79ab5');
 
 /**
  * @license
