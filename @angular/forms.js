372f7e0045d6a2e52d171f298567bb81a85af01f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:52:49 +0000

    5c38012 fix(aio): contributor.html typos

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-46f8a6d
+ * @license Angular v4.1.0-beta.0-5c38012
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-46f8a6d');
+const VERSION = new Version('4.1.0-beta.0-5c38012');
 
 /**
  * @license
