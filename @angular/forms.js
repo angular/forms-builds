2c73c4b4334fb7528daca3c1801037fe9cd84ecb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 06:32:21 +0000

    e1c2e50 docs(aio): move bio images into content folder

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-08941aa
+ * @license Angular v4.1.0-beta.0-e1c2e50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-08941aa');
+const VERSION = new Version('4.1.0-beta.0-e1c2e50');
 
 /**
  * @license
