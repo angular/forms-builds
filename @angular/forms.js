4ff52a9ac980af46a80b8efa5fd66b1e94d0d32a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-02 21:44:22 +0000

    ae70293 feat(aio): add DtComponent to view/change the raw contents

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-1e623a3
+ * @license Angular v4.1.0-beta.0-ae70293
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-1e623a3');
+const VERSION = new Version('4.1.0-beta.0-ae70293');
 
 /**
  * @license
