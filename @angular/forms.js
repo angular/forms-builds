2984de97c563ab77d47980b5e0d465f9a17d0a21
Hans <hans@hansl.ca>
2017-04-10 22:18:21 +0000

    258d539 fix(language-service): detect when there isn't a tsconfig.json

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-09d9f5f
+ * @license Angular v4.1.0-beta.0-258d539
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-09d9f5f');
+const VERSION = new Version('4.1.0-beta.0-258d539');
 
 /**
  * @license
