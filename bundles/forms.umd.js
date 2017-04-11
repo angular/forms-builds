47675432779eec28977508da106d74f7d8c97c51
Hans <hans@hansl.ca>
2017-04-11 23:18:28 +0000

    04fb29b test(aio): add test for escaped newline in CodeComponent

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-3ced940
+ * @license Angular v4.1.0-beta.0-04fb29b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.0-3ced940
+ * @license Angular v4.1.0-beta.0-04fb29b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5844,7 +5844,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.0-3ced940');
+var VERSION = new _angular_core.Version('4.1.0-beta.0-04fb29b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
