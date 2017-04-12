a4fe97458c35988d5009de40457d4cabf30e7c11
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 14:04:00 +0000

    075f3f8 feat(aio): search results layer fix and responsive table

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-2798c1b
+ * @license Angular v4.1.0-beta.0-075f3f8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-2798c1b');
+const VERSION = new Version('4.1.0-beta.0-075f3f8');
 
 /**
  * @license
