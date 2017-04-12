5ff4d9dee65cbc804ba24562cf7f0aa28d5bb14a
GitHub <noreply@github.com>
2017-04-12 19:40:56 +0000

    4a052cd build(aio): upgrade to @angular/material#2.0.0-beta.3 (#15606)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-28c2a71
+ * @license Angular v4.1.0-beta.0-4a052cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-28c2a71');
+const VERSION = new Version('4.1.0-beta.0-4a052cd');
 
 /**
  * @license
