5ba139e0b526d1c4aa95108d3a6e7347439edba3
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 21:46:50 +0000

    56c46d7 fix(router): Update types for TypeScript nullability support

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c36ec9b
+ * @license Angular v4.1.0-beta.1-56c46d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-c36ec9b');
+const VERSION = new Version('4.1.0-beta.1-56c46d7');
 
 /**
  * @license
