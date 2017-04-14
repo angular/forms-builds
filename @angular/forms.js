2df57d26d9ccf6ff4a0fe025e984d4cb159293bc
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 16:20:38 +0000

    8ad464d feat(upgrade): allow setting the angularjs lib at runtime (#15168)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-ce47d33
+ * @license Angular v4.1.0-beta.1-8ad464d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5463,7 +5463,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-ce47d33');
+const VERSION = new Version('4.1.0-beta.1-8ad464d');
 
 /**
  * @license
