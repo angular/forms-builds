a78873ce9423af258be2e12f793f3d9ef4667064
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:51:40 +0000

    16673fa build(aio): remove unused config code

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-624b5a5
+ * @license Angular v4.1.0-beta.1-16673fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-624b5a5');
+const VERSION = new Version('4.1.0-beta.1-16673fa');
 
 /**
  * @license
