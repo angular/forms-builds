214d18e856be1cac56547422969d9dda4e62426b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:54:35 +0000

    7d986ae feat(aio): smaller typography, code header pad

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a2bdcc9
+ * @license Angular v4.1.0-beta.1-7d986ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-a2bdcc9');
+const VERSION = new Version('4.1.0-beta.1-7d986ae');
 
 /**
  * @license
