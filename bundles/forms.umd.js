42245319f01de4fd30f98884484b095311550118
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:37:47 +0000

    092f0df test(aio): remove dross from NavMenuComponent spec

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-dfc81c3
+ * @license Angular v4.1.0-beta.1-092f0df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-dfc81c3
+ * @license Angular v4.1.0-beta.1-092f0df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-beta.1-dfc81c3');
+var VERSION = new _angular_core.Version('4.1.0-beta.1-092f0df');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
