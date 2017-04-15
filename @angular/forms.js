6b058254439a60f9eeb49ec551bf9588dfa04fc6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:52:27 +0000

    a2bdcc9 feat(aio): callout, code ex header styles

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-16673fa
+ * @license Angular v4.1.0-beta.1-a2bdcc9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-16673fa');
+const VERSION = new Version('4.1.0-beta.1-a2bdcc9');
 
 /**
  * @license
