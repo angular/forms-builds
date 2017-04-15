cb4561e8ef16f37c02df24cbc39276674dd079d5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:46:18 +0000

    b57c960 feat(aio): pwa bg color and name

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-49dfc9f
+ * @license Angular v4.1.0-beta.1-b57c960
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-49dfc9f');
+const VERSION = new Version('4.1.0-beta.1-b57c960');
 
 /**
  * @license
