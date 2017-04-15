73a9868084e07791ffe95d17156ea3950fd3b0cc
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:26 +0000

    c0b1bbe feat(aio): add log rotation in preview server

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8479e9e
+ * @license Angular v4.1.0-beta.1-c0b1bbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-8479e9e');
+const VERSION = new Version('4.1.0-beta.1-c0b1bbe');
 
 /**
  * @license
