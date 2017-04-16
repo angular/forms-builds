d316fe36901f8b1ec5f880d88ae911a2b8cb5ad8
Peter Bacon Darwin <pete@bacondarwin.com>
2017-04-16 20:59:21 +0000

    7c418a7 style(aio): add missing semi-colon

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-58f080a
+ * @license Angular v4.1.0-beta.1-7c418a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-58f080a');
+const VERSION = new Version('4.1.0-beta.1-7c418a7');
 
 /**
  * @license
