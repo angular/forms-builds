7e4319fa08fdc3a566a6fc479c7d91f963803a2d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:48:56 +0000

    11b54f6 feat(aio): edit page link styling and placement

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-4a3c66f
+ * @license Angular v4.1.0-beta.1-11b54f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5830,7 +5830,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-4a3c66f');
+var VERSION = new Version('4.1.0-beta.1-11b54f6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-4a3c66f
+ * @license Angular v4.1.0-beta.1-11b54f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-4a3c66f');
+const VERSION = new Version('4.1.0-beta.1-11b54f6');
 
 /**
  * @license
