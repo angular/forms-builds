ed3e098a40d6991c75b058033e7250d3a4327e42
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-17 13:02:37 +0000

    7165eb1 build(aio): add div with class around content

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-e20a722
+ * @license Angular v4.1.0-beta.1-7165eb1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-e20a722');
+const VERSION = new Version('4.1.0-beta.1-7165eb1');
 
 /**
  * @license
