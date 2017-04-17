fad83a131be826d392edc45308b546fe9e63e1e6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-17 07:09:01 +0000

    e20a722 feat(aio): when select node in sidebar close other expanded nodes

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-abecf3e
+ * @license Angular v4.1.0-beta.1-e20a722
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5830,7 +5830,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-abecf3e');
+var VERSION = new Version('4.1.0-beta.1-e20a722');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
