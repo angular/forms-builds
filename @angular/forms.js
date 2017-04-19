5d69982f0b0431f8c2d5afe0d6d11c2e71f10272
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 10:00:40 +0000

    648de7d build(aio): render titles correctly in content pages

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9d40ab9
+ * @license Angular v4.1.0-beta.1-648de7d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-9d40ab9');
+const VERSION = new Version('4.1.0-beta.1-648de7d');
 
 /**
  * @license
