ad2c47a00963613cfa1997e8120d2b4dcb1c6c7e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 13:23:54 +0000

    73c7503 fix(aio): sidenav padding

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-79db6c3
+ * @license Angular v4.1.0-beta.1-73c7503
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-79db6c3');
+const VERSION = new Version('4.1.0-beta.1-73c7503');
 
 /**
  * @license
