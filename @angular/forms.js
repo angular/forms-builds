483ef2a3de79b3fa283c07b53e07c7763b53a96b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:48:19 +0000

    9d40ab9 docs(aio): rename ToH to match the guide name

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-691e86c
+ * @license Angular v4.1.0-beta.1-9d40ab9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-691e86c');
+const VERSION = new Version('4.1.0-beta.1-9d40ab9');
 
 /**
  * @license
