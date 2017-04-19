a75d684dcda5df20a55f26e409b96f1615ae7162
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 15:12:15 +0000

    e07b7ea docs(aio): fix broken regions

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-73c7503
+ * @license Angular v4.1.0-beta.1-e07b7ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-73c7503');
+const VERSION = new Version('4.1.0-beta.1-e07b7ea');
 
 /**
  * @license
