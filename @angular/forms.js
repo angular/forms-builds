b59e260458e9fb85663337012130f4a528deba9e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:42:21 +0000

    93d8f8c docs(aio): add VS Code advice to readme.md

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8fa4453
+ * @license Angular v4.1.0-beta.1-93d8f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-8fa4453');
+const VERSION = new Version('4.1.0-beta.1-93d8f8c');
 
 /**
  * @license
