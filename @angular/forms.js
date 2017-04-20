1df1492fa77a78eb8a4321db725a7b570f9acdb6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:26:26 +0000

    d313aad docs(aio): remove js examples

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-ccb6c45
+ * @license Angular v4.1.0-beta.1-d313aad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-ccb6c45');
+const VERSION = new Version('4.1.0-beta.1-d313aad');
 
 /**
  * @license
