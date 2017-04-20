37c1893b0dda4e76d48ff1aed89c74249d5894bc
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:24:16 +0000

    a54fe63 docs(aio): move file to the right folder

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-800b1b0
+ * @license Angular v4.1.0-beta.1-a54fe63
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-800b1b0');
+const VERSION = new Version('4.1.0-beta.1-a54fe63');
 
 /**
  * @license
