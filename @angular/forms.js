ab0bc6a7597443b9c3d3e0195f230fe59a424b24
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 15:24:23 +0000

    ccb6c45 docs(aio): rename http guide

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a54fe63
+ * @license Angular v4.1.0-beta.1-ccb6c45
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-a54fe63');
+const VERSION = new Version('4.1.0-beta.1-ccb6c45');
 
 /**
  * @license
