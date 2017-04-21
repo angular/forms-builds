5d6472e83fb0e8b193d8dce0bdfc77e472c1c878
Miško Hevery <misko@hevery.com>
2017-04-21 03:28:47 +0000

    5461182 docs(aio): make glossary links work again (#16201)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-cbe95f3
+ * @license Angular v4.1.0-beta.1-5461182
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-cbe95f3');
+const VERSION = new Version('4.1.0-beta.1-5461182');
 
 /**
  * @license
