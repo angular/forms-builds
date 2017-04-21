70f7626424bf634c41cdf3718161fb6837696baa
Miško Hevery <misko@hevery.com>
2017-04-21 05:00:32 +0000

    6ccb937 docs: add changelog for 4.0.3

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.2-14a2d1a
+ * @license Angular v4.0.3-6ccb937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5838,7 +5838,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.2-14a2d1a');
+var VERSION = new Version('4.0.3-6ccb937');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.2-14a2d1a
+ * @license Angular v4.0.3-6ccb937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.2-14a2d1a
+ * @license Angular v4.0.3-6ccb937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5844,7 +5844,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.2-14a2d1a');
+var VERSION = new _angular_core.Version('4.0.3-6ccb937');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
