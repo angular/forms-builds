6cff7dd2fd6295f28a0d328acd07ee2abde162b9
Miško Hevery <misko@hevery.com>
2017-04-21 17:27:19 +0000

    062fc4a docs(aio): change hardcoded links to the old site (#16196)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-cf034f7
+ * @license Angular v4.1.0-rc.0-062fc4a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-cf034f7');
+const VERSION = new Version('4.1.0-rc.0-062fc4a');
 
 /**
  * @license
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-cf034f7
+ * @license Angular v4.1.0-rc.0-062fc4a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-cf034f7
+ * @license Angular v4.1.0-rc.0-062fc4a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5836,7 +5836,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-rc.0-cf034f7');
+var VERSION = new _angular_core.Version('4.1.0-rc.0-062fc4a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
