eb2446391b624a59b6cb35be2c462c413f4feb30
Miško Hevery <misko@hevery.com>
2017-04-21 03:33:36 +0000

    9730351 build(aio): compute which examples to include when watching guide docs (#16213)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5461182
+ * @license Angular v4.1.0-beta.1-9730351
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-5461182');
+const VERSION = new Version('4.1.0-beta.1-9730351');
 
 /**
  * @license
