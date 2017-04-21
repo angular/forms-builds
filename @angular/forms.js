06075861d11eef244d46c8bfade74c49bc283b62
Miško Hevery <misko@hevery.com>
2017-04-21 05:42:34 +0000

    d855e90 docs: add changelog for 4.1.0-rc.0

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9730351
+ * @license Angular v4.1.0-rc.0-d855e90
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-9730351');
+const VERSION = new Version('4.1.0-rc.0-d855e90');
 
 /**
  * @license
