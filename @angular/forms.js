33bd1a27dd3060bc2ee5cac0bb27aa0ce63f4dbf
Miško Hevery <misko@hevery.com>
2017-04-24 17:10:09 +0000

    2991221 docs(aio): remove duplicate `as` (#16265)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d7719aa
+ * @license Angular v4.1.0-rc.0-2991221
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-d7719aa');
+const VERSION = new Version('4.1.0-rc.0-2991221');
 
 /**
  * @license
