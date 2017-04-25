b464a39c1c3c4dab1395cb5311edd4e3c21169d0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:53:03 +0000

    1762047 build(aio): fix other dgeni templates

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-23e6502
+ * @license Angular v4.1.0-rc.0-1762047
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-23e6502');
+const VERSION = new Version('4.1.0-rc.0-1762047');
 
 /**
  * @license
