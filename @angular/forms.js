b955003e165513887345aa6f9ee516fda14a2f88
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 11:49:22 +0000

    9b7f2ce test(aio): add missing test for AppComponent 'starting' feature

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-11b2f62
+ * @license Angular v4.1.0-rc.0-9b7f2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-11b2f62');
+const VERSION = new Version('4.1.0-rc.0-9b7f2ce');
 
 /**
  * @license
