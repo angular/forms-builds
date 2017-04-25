bb8cd7cfc42b7445045a6adb481818251c040e77
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:21:09 +0000

    64ef69f feat(aio): api page column layout

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-rc.0-249cd8c
+ * @license Angular v4.1.0-rc.0-64ef69f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5830,7 +5830,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-rc.0-249cd8c');
+var VERSION = new Version('4.1.0-rc.0-64ef69f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
