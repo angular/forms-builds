8eb2cb1b53f29f383dc87338de8b246737e4dc6c
Miško Hevery <misko@hevery.com>
2017-04-26 22:10:50 +0000

    6bac4e6 build(aio): update dgeni-packages to 0.17.2 (#16358)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a28f5ee
+ * @license Angular v4.1.0-6bac4e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-a28f5ee');
+const VERSION = new Version('4.1.0-6bac4e6');
 
 /**
  * @license
