9893a4cbf6d6141d5e06a04057b53b0f9e82166c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:37:08 +0000

    ac5e6ba fix(aio): AppComponent should scroll only once when location changes

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-710b4a3
+ * @license Angular v4.1.0-rc.0-ac5e6ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-710b4a3');
+const VERSION = new Version('4.1.0-rc.0-ac5e6ba');
 
 /**
  * @license
