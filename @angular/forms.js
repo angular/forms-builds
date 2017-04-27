252682ded0ca4fa3b148b9bc788c79b6ea000a72
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-27 09:45:01 +0000

    2a7f636 build(aio): update @angular/service-worker to v1.0.0-beta.10

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-6bac4e6
+ * @license Angular v4.1.0-2a7f636
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-6bac4e6');
+const VERSION = new Version('4.1.0-2a7f636');
 
 /**
  * @license
