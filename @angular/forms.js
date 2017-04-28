b665e75ff338c23a01bc356df045edfdc3448017
Miško Hevery <misko@hevery.com>
2017-04-28 23:27:38 +0000

    a4de214 fix(core): don’t set `ng-version` for dynamically created components (#16394)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a2b2afb
+ * @license Angular v4.1.0-a4de214
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5605,7 +5605,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-a2b2afb');
+const VERSION = new Version('4.1.0-a4de214');
 
 /**
  * @license
