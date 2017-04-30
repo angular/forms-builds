9a2a7ddb784be34ca21ffdf576ab7e212d325db0
Miško Hevery <misko@hevery.com>
2017-04-30 16:31:33 +0000

    f32bfcd build(aio): convert triple backtick blocks to `code-example` tags

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-de8de84
+ * @license Angular v4.1.0-f32bfcd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5605,7 +5605,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-de8de84');
+const VERSION = new Version('4.1.0-f32bfcd');
 
 /**
  * @license
