3fb1d011836b3b6e639c2b8fed730e1e3c8e3961
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:31:43 +0000

    1c8772a fix(aio): delete guide/index.md which is no longer used

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-a0b9c23
+ * @license Angular v4.1.0-1c8772a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-a0b9c23
+ * @license Angular v4.1.0-1c8772a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5974,7 +5974,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-a0b9c23');
+var VERSION = new _angular_core.Version('4.1.0-1c8772a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
