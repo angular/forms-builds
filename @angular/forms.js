69e53fe69fed58b88416a66c05c12357e74d7b3c
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 00:30:38 +0000

    e263e19 fix(core): don’t stop change detection because of errors

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-ac220fc
+ * @license Angular v4.1.0-e263e19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5605,7 +5605,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-ac220fc');
+const VERSION = new Version('4.1.0-e263e19');
 
 /**
  * @license
