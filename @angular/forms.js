94dfa3857521ca87dd12b606e9f98a7fd2a4ab9d
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 16:54:10 +0000

    9684d78 build: update `concurrently` to latest version

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-71f5b73
+ * @license Angular v4.1.0-9684d78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-71f5b73');
+const VERSION = new Version('4.1.0-9684d78');
 
 /**
  * @license
