e87b868b8a667c867119487965201728deb03b62
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:07:49 +0000

    55b8de9 build(aio): lint examples

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-eb56ab3');
+const VERSION = new Version('4.1.0-55b8de9');
 
 /**
  * @license
