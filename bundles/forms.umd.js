33e4c16a656c2b3f615ef0501a44ce1d813144cf
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:59:26 +0000

    464701a docs(aio): revert removal of tutorial examples

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-5b96fb9
+ * @license Angular v4.1.0-464701a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5971,7 +5971,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.1.0-5b96fb9');
+var VERSION = new _angular_core.Version('4.1.0-464701a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
