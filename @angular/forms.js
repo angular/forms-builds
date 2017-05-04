0038e1fba662716dd533ea97220e0ce6fc8fbcdb
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:48:35 +0000

    77b8a76 feat(aio): toc styling

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-55b8de9
+ * @license Angular v4.1.0-77b8a76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-55b8de9');
+const VERSION = new Version('4.1.0-77b8a76');
 
 /**
  * @license
