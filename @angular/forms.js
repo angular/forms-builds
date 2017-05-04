99d97de533f1a0625ca08936ad92010b1ae7a147
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:51:18 +0000

    919ff12 fix(aio): cheatsheet table layout fix

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-6748aea
+ * @license Angular v4.1.0-919ff12
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-6748aea');
+const VERSION = new Version('4.1.0-919ff12');
 
 /**
  * @license
