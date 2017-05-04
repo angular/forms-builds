21dc97299f603d3acd11e5cfb8ab4b02d32a5787
Martin Probst <martin@probst.io>
2017-05-04 18:12:21 +0000

    478d2ce fix(compiler): avoid a `...null` spread in extraction

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-83d9533
+ * @license Angular v4.1.0-478d2ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-83d9533');
+const VERSION = new Version('4.1.0-478d2ce');
 
 /**
  * @license
