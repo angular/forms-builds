f0d58df636d2250a41431122b2b2e2f192a33edd
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 20:40:48 +0000

    9da6340 fix(http): flatten metadata for @angular/http/testing

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-547c363
+ * @license Angular v4.2.0-beta.0-9da6340
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-547c363');
+const VERSION = new Version('4.2.0-beta.0-9da6340');
 
 /**
  * @license
