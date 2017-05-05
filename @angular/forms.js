550df08b54e5dfa8619010bd2134381ab10e070a
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 21:00:11 +0000

    42dc2c1 refactor(aio): remove redundant test

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-9da6340
+ * @license Angular v4.2.0-beta.0-42dc2c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-9da6340');
+const VERSION = new Version('4.2.0-beta.0-42dc2c1');
 
 /**
  * @license
