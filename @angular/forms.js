c9611d9baac6a4308772f95e535043f46376420d
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-06 13:53:16 +0000

    5cf6426 docs(aio): fix sizing of marketing doc images

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-03513e9
+ * @license Angular v4.2.0-beta.0-5cf6426
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-03513e9');
+const VERSION = new Version('4.2.0-beta.0-5cf6426');
 
 /**
  * @license
