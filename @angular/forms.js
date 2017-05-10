1b5d5b8fe5719305c1796beb6e8d3cd9d9524394
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 07:09:16 +0000

    412ab3f fix(aio): basic fix to the TOC styling

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-954c08e
+ * @license Angular v4.2.0-beta.0-412ab3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-954c08e');
+const VERSION = new Version('4.2.0-beta.0-412ab3f');
 
 /**
  * @license
