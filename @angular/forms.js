b6270fd22ba80620a9a5bbc4f0f0a9cb419df9e1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:09:11 +0000

    f795f64 refactor(aio): remove redundant styles

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-063db64
+ * @license Angular v4.2.0-beta.0-f795f64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-063db64');
+const VERSION = new Version('4.2.0-beta.0-f795f64');
 
 /**
  * @license
