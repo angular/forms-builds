56c8f199ba562de24ee98052fbbe092d4fd846f6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:25:12 +0000

    e242e20 fix(aio): style TOC scrollbar similar to other scrollbars

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-f148ebe
+ * @license Angular v4.2.0-beta.0-e242e20
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-f148ebe');
+const VERSION = new Version('4.2.0-beta.0-e242e20');
 
 /**
  * @license
