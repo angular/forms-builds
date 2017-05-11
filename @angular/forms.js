cce1395d292bf5dd87e376c222fa5109c213f76c
Igor Minar <iminar@google.com>
2017-05-11 22:20:23 +0000

    decb4cc ci(aio): create previews for all PRs that touch non-spec files in `aio/` or `packages/` (#16733)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-44c7ac0
+ * @license Angular v4.2.0-beta.1-decb4cc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-44c7ac0');
+const VERSION = new Version('4.2.0-beta.1-decb4cc');
 
 /**
  * @license
