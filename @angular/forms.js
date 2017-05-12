63254fffab3454d4bad8c396ec2343bc2c798cf5
Igor Minar <iminar@google.com>
2017-05-12 07:05:14 +0000

    37f1fcb docs: remove angular.io from changelog (#16723)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-4a599ee
+ * @license Angular v4.2.0-beta.1-37f1fcb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-4a599ee');
+const VERSION = new Version('4.2.0-beta.1-37f1fcb');
 
 /**
  * @license
