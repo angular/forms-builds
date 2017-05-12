e2bca4015d8b6743c562d5c08f5d53efa82ba7ad
Igor Minar <iminar@google.com>
2017-05-12 19:36:46 +0000

    0626464 build: use subshells when changing directories

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-21d213d');
+const VERSION = new Version('4.2.0-beta.1-0626464');
 
 /**
  * @license
