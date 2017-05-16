7ec3ed6f27a27f3bf86a3be865e4c6209f2fbf3e
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-16 23:53:12 +0000

    d761059 fix(compiler-cli): allow '==' to compare nullable types (#16731)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-c805082
+ * @license Angular v4.2.0-beta.1-d761059
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-c805082
+ * @license Angular v4.2.0-beta.1-d761059
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5970,7 +5970,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.1-c805082');
+var VERSION = new _angular_core.Version('4.2.0-beta.1-d761059');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
