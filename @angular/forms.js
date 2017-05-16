9f923fc658f02c1673030143018a2ce32cd5056c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 10:02:05 +0000

    221f85a fix(aio): no white band below toolbar when mobile

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a68ad6d
+ * @license Angular v4.2.0-beta.1-221f85a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-a68ad6d');
+const VERSION = new Version('4.2.0-beta.1-221f85a');
 
 /**
  * @license
