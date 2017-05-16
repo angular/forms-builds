aff766e20f9910e988d9de685138dce70f8c3c25
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-16 09:22:05 +0000

    a68ad6d feat(aio): sidenav headers should be focusable buttons

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-7ae0440
+ * @license Angular v4.2.0-beta.1-a68ad6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5602,7 +5602,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-7ae0440');
+const VERSION = new Version('4.2.0-beta.1-a68ad6d');
 
 /**
  * @license
