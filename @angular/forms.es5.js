53cc8f1d1a87132e624be68c6f89a59bfe14b3e3
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-17 07:43:31 +0000

    238c523 fix(aio): remove searchbox shrink animation

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-593fe5e
+ * @license Angular v4.2.0-beta.1-238c523
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5964,7 +5964,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-593fe5e');
+var VERSION = new Version('4.2.0-beta.1-238c523');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
