9162aefa6e8100680e206fa7cd6f49a4c62e4f58
Matias Niemelä <matias@yearofmoo.com>
2017-05-17 23:46:51 +0000

    712630c perf(animations): reduce size of animations bundle

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-6416e79
+ * @license Angular v4.2.0-beta.1-712630c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5964,7 +5964,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-6416e79');
+var VERSION = new Version('4.2.0-beta.1-712630c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-6416e79
+ * @license Angular v4.2.0-beta.1-712630c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-6416e79
+ * @license Angular v4.2.0-beta.1-712630c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5970,7 +5970,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.1-6416e79');
+var VERSION = new _angular_core.Version('4.2.0-beta.1-712630c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
