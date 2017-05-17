31842c908407c07cd20843102548c167bb101ad1
Jason Aden <jasonaden@google.com>
2017-05-17 22:32:34 +0000

    6f039d7 build: do not create the bundles when updating the public API

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.2-62a8618
+ * @license Angular v4.1.2-6f039d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5470,7 +5470,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.2-62a8618');
+const VERSION = new Version('4.1.2-6f039d7');
 
 /**
  * @license
