58a706a8e6ebdfabd3718f3c0ed9f62c7387521a
Jason Aden <jasonaden@google.com>
2017-05-19 23:52:11 +0000

    3d382dc docs: add changelog for 4.2.0-rc.0

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-f1a9e3c
+ * @license Angular v4.2.0-rc.0-3d382dc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5964,7 +5964,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-f1a9e3c');
+var VERSION = new Version('4.2.0-rc.0-3d382dc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
