e05a91e23ead8b347e4c71bcac67f395dce296cb
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 22:54:05 +0000

    51d7a65 docs: add changelog for 4.2.0-rc.1

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-d108479
+ * @license Angular v4.2.0-rc.1-51d7a65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5955,7 +5955,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-d108479');
+var VERSION = new Version('4.2.0-rc.1-51d7a65');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
