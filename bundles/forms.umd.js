4888294ecd4489ad93ed1d5b71ff33c6f717308d
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 01:18:59 +0000

    5aa53d7 refactor(compiler): change `bundle` to `flatModuleIndex` in the code (#17028)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-3ab86bd
+ * @license Angular v4.2.0-rc.0-5aa53d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-3ab86bd
+ * @license Angular v4.2.0-rc.0-5aa53d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-3ab86bd');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-5aa53d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
