6c00bb93ce211248140101df540c933734a6d590
Igor Minar <iminar@google.com>
2017-05-26 08:38:05 +0000

    db3113b build(aio): externalize @angular/cli patches into a js file

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.0-0c691af
+ * @license Angular v4.2.0-rc.0-db3113b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5955,7 +5955,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-0c691af');
+var VERSION = new Version('4.2.0-rc.0-db3113b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-0c691af
+ * @license Angular v4.2.0-rc.0-db3113b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-0c691af');
+const VERSION = new Version('4.2.0-rc.0-db3113b');
 
 /**
  * @license
