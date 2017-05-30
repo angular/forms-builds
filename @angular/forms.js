66a7012ac35cd10dfa00de29a6a50a52b79fcd9a
Victor Berchet <victor@suumit.com>
2017-05-30 22:33:27 +0000

    0d894a1 fix(aio): fix ToC styling (#17110)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-e513808
+ * @license Angular v4.2.0-rc.1-0d894a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-e513808');
+const VERSION = new Version('4.2.0-rc.1-0d894a1');
 
 /**
  * @license
