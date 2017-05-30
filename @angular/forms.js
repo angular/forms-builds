e769b0f55ca7ff18424213fcd38607849538b2fe
Victor Berchet <victor@suumit.com>
2017-05-30 22:34:11 +0000

    2f35392 fix(aio): make the search-pane larger (and improve the search-box) (#17105)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-b056adc
+ * @license Angular v4.2.0-rc.1-2f35392
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-b056adc');
+const VERSION = new Version('4.2.0-rc.1-2f35392');
 
 /**
  * @license
