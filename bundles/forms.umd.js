837d4ceba73f2bdf0976983c6dee08f7a6d5f43c
Victor Berchet <victor@suumit.com>
2017-05-31 21:59:44 +0000

    c20f60b fix(router): make remove trailing slash consistent with URL params

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1408357
+ * @license Angular v4.2.0-rc.1-c20f60b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-1408357
+ * @license Angular v4.2.0-rc.1-c20f60b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-1408357');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-c20f60b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
