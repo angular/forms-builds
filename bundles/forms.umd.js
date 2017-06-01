b14e8dc54a9515d0e0b1847ca86563abaefcc664
Victor Berchet <victor@suumit.com>
2017-06-01 22:19:33 +0000

    82ec02d docs: add changelog for 4.2.0-rc.2

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-7822187
+ * @license Angular v4.2.0-rc.2-82ec02d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-7822187
+ * @license Angular v4.2.0-rc.2-82ec02d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-7822187');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-82ec02d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
