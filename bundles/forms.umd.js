1b95859425593746964e71828ee1a14660969fa3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:36:47 +0000

    7c78282 docs: fix typo in 'Routes' docs

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-02d74ca
+ * @license Angular v4.2.0-rc.2-7c78282
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-02d74ca
+ * @license Angular v4.2.0-rc.2-7c78282
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-02d74ca');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-7c78282');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
