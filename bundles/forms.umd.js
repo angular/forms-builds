22b8c44790b43de08891a443b0d829906de1f97b
GitHub <noreply@github.com>
2017-06-06 06:27:19 +0000

    42176a7 build(aio): upgrade ngo and purify (#17210)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-76b4b80
+ * @license Angular v4.2.0-rc.2-42176a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-76b4b80
+ * @license Angular v4.2.0-rc.2-42176a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-76b4b80');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-42176a7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
