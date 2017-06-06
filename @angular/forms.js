7ecc41d304f9b7d6793e768d044121b9dee53213
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 21:38:37 +0000

    b00b80a feat(compiler-cli): introduce synchronous codegen API

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-269bbe0
+ * @license Angular v4.2.0-rc.2-b00b80a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-269bbe0');
+const VERSION = new Version('4.2.0-rc.2-b00b80a');
 
 /**
  * @license
