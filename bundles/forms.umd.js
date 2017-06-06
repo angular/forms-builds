7924543896c315f3d3a3ced8a64f352e27ddb63f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 14:13:48 +0000

    95f1ea2 fix(aio): topnav toolbar position styles

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-784347f
+ * @license Angular v4.2.0-rc.2-95f1ea2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-784347f
+ * @license Angular v4.2.0-rc.2-95f1ea2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-784347f');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-95f1ea2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
