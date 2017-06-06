478b2ad7adb673a76ed10a151f0119931f3a21e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-06 20:28:56 +0000

    b8979c8 fix(aio): fix and clean up topbar styling

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-d4e1960
+ * @license Angular v4.2.0-rc.2-b8979c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-d4e1960
+ * @license Angular v4.2.0-rc.2-b8979c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-d4e1960');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-b8979c8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
