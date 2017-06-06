bdeadb18d02a1d222999c1bf16d926f629d6fa9e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 15:58:45 +0000

    3a121a6 build(aio): migrate contribute page to html

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-c06f4fc
+ * @license Angular v4.2.0-rc.2-3a121a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-c06f4fc
+ * @license Angular v4.2.0-rc.2-3a121a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-c06f4fc');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-3a121a6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
