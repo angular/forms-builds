3499f7a520723485eb1010bb7597e70f1ed1d3a0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-07 13:11:35 +0000

    4759975 fix(aio): adjust SideNav top to avoid extra whitespace

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-078a4b0
+ * @license Angular v4.2.0-rc.2-4759975
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-078a4b0');
+const VERSION = new Version('4.2.0-rc.2-4759975');
 
 /**
  * @license
