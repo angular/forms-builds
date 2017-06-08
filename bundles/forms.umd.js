38721577dfd9a31a0e91fc577e78fb8d8a6a482f
Igor Minar <iminar@google.com>
2017-06-08 19:29:14 +0000

    0940e6d fix(aio): contributor img offset and hover fix (#17338)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-5364b51
+ * @license Angular v4.2.0-rc.2-0940e6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-5364b51
+ * @license Angular v4.2.0-rc.2-0940e6d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5986,7 +5986,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-5364b51');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-0940e6d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
