4d01d77b91c4a8803318c264463a9ebc289c2f30
Igor Minar <iminar@google.com>
2017-06-08 08:13:58 +0000

    8be2e4c fix(aio): make the ToC left hand-side bar shorter (#17280)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a4a2901
+ * @license Angular v4.2.0-rc.2-8be2e4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5597,7 +5597,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-a4a2901');
+const VERSION = new Version('4.2.0-rc.2-8be2e4c');
 
 /**
  * @license
