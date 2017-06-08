a1567547ebb5fd692e386aeb78cff5a5c03a9ed0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:17:42 +0000

    41a765d fix(aio): re-color code pretty print

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-f370fd3
+ * @license Angular v4.2.0-rc.2-41a765d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-f370fd3');
+const VERSION = new Version('4.2.0-rc.2-41a765d');
 
 /**
  * @license
