8041dc8ecd45038fcd8583a97d60e9049a4e4bde
Igor Minar <iminar@google.com>
2017-06-09 21:54:28 +0000

    db5e506 build(aio): update typescript to 2.3.2 (#17382)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-0020dad
+ * @license Angular v4.2.0-db5e506
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-0020dad');
+const VERSION = new Version('4.2.0-db5e506');
 
 /**
  * @license
