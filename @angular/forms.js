bc4eac03ddf29a3d9410b0e682d2a17a53ce4176
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 13:55:51 +0000

    a0b30e5 docs(aio): remove example specific `.gitignore` files

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-e894f5c
+ * @license Angular v4.2.0-a0b30e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-e894f5c');
+const VERSION = new Version('4.2.0-a0b30e5');
 
 /**
  * @license
