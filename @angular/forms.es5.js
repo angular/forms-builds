2fce26b921d3061f750127c71ba35388acde4b72
Igor Minar <iminar@google.com>
2017-06-09 21:53:46 +0000

    0020dad docs(aio): update examples to 4.2 (#17367)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-65d49d5
+ * @license Angular v4.2.0-0020dad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5959,7 +5959,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-65d49d5');
+var VERSION = new Version('4.2.0-0020dad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
