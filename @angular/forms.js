b53a4682a94e2af9a4e6553304ab8788bbd5c237
Igor Minar <iminar@google.com>
2017-06-09 20:50:38 +0000

    503fd1f docs(aio): fix image width on the homepage

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-ce18c68');
+const VERSION = new Version('4.2.0-503fd1f');
 
 /**
  * @license
