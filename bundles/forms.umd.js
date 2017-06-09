8ebc1f76f7abdf6e7e5ffd16a19365988273cefc
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 16:15:18 +0000

    2d56239 fix(aio): sidebar folder state after select item

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-a0b30e5
+ * @license Angular v4.2.0-2d56239
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-a0b30e5
+ * @license Angular v4.2.0-2d56239
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5990,7 +5990,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-a0b30e5');
+var VERSION = new _angular_core.Version('4.2.0-2d56239');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
