f86ebc859289cf580924eb64ad5684eb4b5af4bf
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 09:54:55 +0000

    8524187 fix(aio): restrain scrolling inside ToC (when cursor over ToC)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-f7422a9
+ * @license Angular v4.2.0-8524187
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-f7422a9');
+const VERSION = new Version('4.2.0-8524187');
 
 /**
  * @license
