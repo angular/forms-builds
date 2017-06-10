d92791bcec25b2e12bd0b7d3d1b53b92b36d4111
Igor Minar <iminar@google.com>
2017-06-10 01:48:19 +0000

    afbc2b0 build(aio): upgrade purify to deal with messed up comment in animations code

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-15090a8
+ * @license Angular v4.2.1-afbc2b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5959,7 +5959,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-15090a8');
+var VERSION = new Version('4.2.1-afbc2b0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-15090a8
+ * @license Angular v4.2.1-afbc2b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-15090a8');
+const VERSION = new Version('4.2.1-afbc2b0');
 
 /**
  * @license
