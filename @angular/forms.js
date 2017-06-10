c814a8a583d91510490d88c7c46fbdfc2dc78fd2
Igor Minar <iminar@google.com>
2017-06-10 01:57:19 +0000

    df7774c docs(aio): restore animation original value (#17385)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-c8c0cee
+ * @license Angular v4.2.1-df7774c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-c8c0cee');
+const VERSION = new Version('4.2.1-df7774c');
 
 /**
  * @license
