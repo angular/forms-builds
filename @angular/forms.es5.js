6814631f268aaf519b51adfa7564f53e041e9068
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-11 18:05:50 +0000

    d961911 docs(aio): fix broken example in upgrade guide

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.1-e543272
+ * @license Angular v4.2.1-d961911
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5959,7 +5959,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.1-e543272');
+var VERSION = new Version('4.2.1-d961911');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-e543272
+ * @license Angular v4.2.1-d961911
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-e543272');
+const VERSION = new Version('4.2.1-d961911');
 
 /**
  * @license
