850891e71d20a69ed50446eaf05beed0f1d22b01
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 19:40:08 +0000

    451257a fix(animations): do not treat a `0` animation state as `void`

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-185075d
+ * @license Angular v4.2.1-451257a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-185075d');
+const VERSION = new Version('4.2.1-451257a');
 
 /**
  * @license
