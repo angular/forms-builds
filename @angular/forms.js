5554e00284ea6be8c582527b6b6969bb57b44911
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 19:38:36 +0000

    185075d fix(animations): compute removal node height correctly

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-3d5f520
+ * @license Angular v4.2.1-185075d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-3d5f520');
+const VERSION = new Version('4.2.1-185075d');
 
 /**
  * @license
