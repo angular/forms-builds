4dd98b8b9291be672920fe653f508317a642d8e5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 13:15:20 +0000

    052331f build(aio): add staging environment

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-541c9a9
+ * @license Angular v4.2.2-052331f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-541c9a9');
+const VERSION = new Version('4.2.2-052331f');
 
 /**
  * @license
