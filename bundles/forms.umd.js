b6b7135c2b091a43e6e2a5a41796b864481bc41f
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:20:23 +0000

    2b1de07 docs(aio): incorporate Ward's comments

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2c3e948
+ * @license Angular v4.2.2-2b1de07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-2c3e948
+ * @license Angular v4.2.2-2b1de07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5990,7 +5990,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-2c3e948');
+var VERSION = new _angular_core.Version('4.2.2-2b1de07');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
