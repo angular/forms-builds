f8bfe703294e4cd5b7554b283a24c39e9a9e0a9d
Hans <hans@hansl.ca>
2017-06-15 22:11:06 +0000

    e096a85 fix(animations): remove duplicate license header

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-53f57d7
+ * @license Angular v4.2.2-e096a85
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-53f57d7');
+const VERSION = new Version('4.2.2-e096a85');
 
 /**
  * @license
