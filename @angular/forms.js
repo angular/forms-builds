26cb499004473a78a95a680a3f093f36c3608f2b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:21:24 +0000

    2cc931e fix(aio): add missing redirect rule for `/styleguide`

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-ce00fa3
+ * @license Angular v4.2.2-2cc931e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-ce00fa3');
+const VERSION = new Version('4.2.2-2cc931e');
 
 /**
  * @license
