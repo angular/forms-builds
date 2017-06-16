b3437e9585d1b94499de7ad3c462f115f8c26fc5
Hans <hans@hansl.ca>
2017-06-16 19:34:57 +0000

    fe126cb fix(aio): remove unused icon reference and file

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-adc1b12
+ * @license Angular v4.2.2-fe126cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-adc1b12');
+const VERSION = new Version('4.2.2-fe126cb');
 
 /**
  * @license
