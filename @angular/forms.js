5a0df2232e93ef9907f376e84452a7981357ee98
Hans <hans@hansl.ca>
2017-06-16 19:34:26 +0000

    f58211d fix(aio): improve no-javascript screen

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-9315ab8
+ * @license Angular v4.2.2-f58211d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-9315ab8');
+const VERSION = new Version('4.2.2-f58211d');
 
 /**
  * @license
