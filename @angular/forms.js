108f764888737725565726f26a366ff23ede7c79
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:23:37 +0000

    dbc6a4c build(aio): do not fail if `check-env` for the main angular project fails

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b192dd5
+ * @license Angular v4.2.2-dbc6a4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-b192dd5');
+const VERSION = new Version('4.2.2-dbc6a4c');
 
 /**
  * @license
