044d718c75810e62e190c58c541a233fbd1dc52c
Hans Larsen <hans@hansl.ca>
2017-06-16 18:48:41 +0000

    623b9c6 test(compiler): fix typo "mamespace"

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-8a547ee
+ * @license Angular v4.2.3-623b9c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-8a547ee');
+const VERSION = new Version('4.2.3-623b9c6');
 
 /**
  * @license
