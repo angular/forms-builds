83ea994837d738531c9929a44a4f986540b0bbc6
Hans <hans@hansl.ca>
2017-06-16 21:06:42 +0000

    3093c55 build(aio): update @angular/service-worker to 1.0.0-beta.16

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-5ac3919
+ * @license Angular v4.2.2-3093c55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-5ac3919');
+const VERSION = new Version('4.2.2-3093c55');
 
 /**
  * @license
