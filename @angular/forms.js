4adfe7864de1b72f8b592ebc7deaf13662ed3542
Hans Larsen <hans@hansl.ca>
2017-06-19 19:00:31 +0000

    3607a48 docs: remove aio from changelog

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-ab81c1c
+ * @license Angular v4.2.3-3607a48
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-ab81c1c');
+const VERSION = new Version('4.2.3-3607a48');
 
 /**
  * @license
