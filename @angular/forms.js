92b9636ed999b63100560552725923f02f045575
Hans Larsen <hans@hansl.ca>
2017-06-19 22:12:48 +0000

    09512c7 docs(RELEASE_SCHEDULE): fix version numbers for August/September releases

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-3607a48
+ * @license Angular v4.2.3-09512c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-3607a48');
+const VERSION = new Version('4.2.3-09512c7');
 
 /**
  * @license
