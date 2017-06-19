176b6936bd38ceb81ae59a38036caf71b149217e
Hans <hans@hansl.ca>
2017-06-19 22:43:00 +0000

    d3c92a3 fix(router): update the version placeholder so that it gets replaced during the build

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-8f5836c
+ * @license Angular v4.2.2-d3c92a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-8f5836c');
+const VERSION = new Version('4.2.2-d3c92a3');
 
 /**
  * @license
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-8f5836c
+ * @license Angular v4.2.2-d3c92a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-8f5836c
+ * @license Angular v4.2.2-d3c92a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5990,7 +5990,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-8f5836c');
+var VERSION = new _angular_core.Version('4.2.2-d3c92a3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
