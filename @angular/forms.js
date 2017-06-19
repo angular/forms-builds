6d00a952af4dea6ff7391b856a8434fed8c0a96e
Hans <hans@hansl.ca>
2017-06-19 18:04:31 +0000

    4f37f86 fix(aio): switch from `innerText` to `textContent` to support older browsers

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3093c55
+ * @license Angular v4.2.2-4f37f86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-3093c55');
+const VERSION = new Version('4.2.2-4f37f86');
 
 /**
  * @license
