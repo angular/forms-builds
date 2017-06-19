ebfb6b4c753c587260146b51f76232d5cc2fc933
Hans Larsen <hans@hansl.ca>
2017-06-19 23:02:43 +0000

    2e8efda fix(aio): leave results panel open when opening search result on new page

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-09512c7
+ * @license Angular v4.2.3-2e8efda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5601,7 +5601,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-09512c7');
+const VERSION = new Version('4.2.3-2e8efda');
 
 /**
  * @license
