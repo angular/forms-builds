b9c5f2755b626a9b431224d8c2489cd479b9cf5d
Hans Larsen <hans@hansl.ca>
2017-06-20 20:33:54 +0000

    f997937 docs(aio): update resource listing

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-77860a0
+ * @license Angular v4.2.3-f997937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.3-77860a0
+ * @license Angular v4.2.3-f997937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.3-77860a0');
+var VERSION = new _angular_core.Version('4.2.3-f997937');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
