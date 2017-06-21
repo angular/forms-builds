7ed9189bdafdc07738d9e8557c42f3839e89614f
Hans <hans@hansl.ca>
2017-06-21 23:38:29 +0000

    4352dd2 fix(compiler): avoid emitting self importing factories

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-34cc3f2
+ * @license Angular v4.2.2-4352dd2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-34cc3f2');
+var VERSION = new Version('4.2.2-4352dd2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
