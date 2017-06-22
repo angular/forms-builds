845f9c43aa5d5ca25b60aca3b9f5e4946aad2992
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:18:18 +0000

    133b5e6 docs(aio): added link to Russian Angular Courses

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-92bcfef
+ * @license Angular v4.3.0-beta.0-133b5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-92bcfef');
+const VERSION = new Version('4.3.0-beta.0-133b5e6');
 
 /**
  * @license
