deb6b15a0227f7124984a6399dc2e1ce51f587eb
Hans Larsen <hans@hansl.ca>
2017-06-22 00:29:06 +0000

    02a38d3 docs: add changelog for 4.2.4

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-37cdc4f');
+var VERSION = new Version('4.2.2-02a38d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-37cdc4f
+ * @license Angular v4.2.2-02a38d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-37cdc4f');
+var VERSION = new _angular_core.Version('4.2.2-02a38d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
