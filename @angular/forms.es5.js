e6e755b8d423deb578a9123af2a358c8de7864a0
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 17:49:03 +0000

    9344c86 docs: fix “under to hood” typo in changelog.md

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.4-353de51
+ * @license Angular v4.2.4-9344c86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.4-353de51');
+var VERSION = new Version('4.2.4-9344c86');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
