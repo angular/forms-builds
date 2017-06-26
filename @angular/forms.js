081b4b3acf66027660c72301729511b4ef4b2194
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 20:42:16 +0000

    448d9f9 test(compiler): add a test for components not part of any `NgModule`

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-97bb374
+ * @license Angular v4.3.0-beta.0-448d9f9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-97bb374');
+const VERSION = new Version('4.3.0-beta.0-448d9f9');
 
 /**
  * @license
