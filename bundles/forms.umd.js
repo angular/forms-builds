8b73a0f1ecda37051cc21dae4a4de9c8ff6b113e
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:40:29 +0000

    8786ba9 build(aio): boilerplate wont be removed by default now

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-263c1a1
+ * @license Angular v4.3.0-beta.0-8786ba9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-263c1a1
+ * @license Angular v4.3.0-beta.0-8786ba9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-263c1a1');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-8786ba9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
