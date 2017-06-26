0e6c9856513682d89db03d323a578ff6a9eb6586
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:40:36 +0000

    233044e docs(aio): fix typo

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-8786ba9
+ * @license Angular v4.3.0-beta.0-233044e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-8786ba9');
+const VERSION = new Version('4.3.0-beta.0-233044e');
 
 /**
  * @license
