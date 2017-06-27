0c8c7607e6f9c83767b0bf04687b4a1bede960b4
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 23:47:10 +0000

    6c1a8da docs: correct grammar in CONTRIBUTING.md

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-d699c35
+ * @license Angular v4.3.0-beta.0-6c1a8da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-d699c35');
+const VERSION = new Version('4.3.0-beta.0-6c1a8da');
 
 /**
  * @license
