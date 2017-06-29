533e5139d13af5260f06c2eb34c9eb0ae3d35cc7
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 22:08:48 +0000

    1ac9085 docs: add changelog for 4.3.0-beta.1

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-96b1703
+ * @license Angular v4.3.0-beta.1-1ac9085
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-96b1703');
+const VERSION = new Version('4.3.0-beta.1-1ac9085');
 
 /**
  * @license
