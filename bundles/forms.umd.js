075dfad879cf925d4a2b24956d042748a925d664
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 01:48:14 +0000

    af14b1e test(animations): add proper tests to check multiple combinations of :enter/:leave animation queries

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-6c1a8da
+ * @license Angular v4.3.0-beta.0-af14b1e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-6c1a8da
+ * @license Angular v4.3.0-beta.0-af14b1e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-6c1a8da');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-af14b1e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
