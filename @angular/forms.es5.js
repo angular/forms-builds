ff12a2eb3c0ce653a6ecff0689919b77c3826471
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 22:58:00 +0000

    7728d7e ci: remove print-logs.sh to try to fix failing builds on Travis

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.4-7205d7a
+ * @license Angular v4.2.4-7728d7e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.4-7205d7a');
+var VERSION = new Version('4.2.4-7728d7e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
