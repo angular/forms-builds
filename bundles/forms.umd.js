d213662c641379e0b9b5122da1b0109a4eae7db2
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:47:27 +0000

    154154d docs(aio): fix quickstart-cli bad HTML escape chars

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b052ef5
+ * @license Angular v4.3.0-beta.1-154154d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-b052ef5
+ * @license Angular v4.3.0-beta.1-154154d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-b052ef5');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-154154d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
