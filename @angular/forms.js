47a9de4ed8c1f71a5b865a83b9c3d9b3f5d06943
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-30 12:32:16 +0000

    1a476b8 ci(aio): use correct URL for preview in PWA score test

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-27b87e8
+ * @license Angular v4.2.5-1a476b8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.5-27b87e8');
+const VERSION = new Version('4.2.5-1a476b8');
 
 /**
  * @license
