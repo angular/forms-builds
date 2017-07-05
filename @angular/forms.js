4f21b2eb9e6d2b07db9bbc04b318a7d202f0361b
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:31:37 +0000

    b51697c ci: add Chuck to the owners of core (#17892)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-92d7ecf
+ * @license Angular v4.3.0-beta.1-b51697c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-92d7ecf');
+const VERSION = new Version('4.3.0-beta.1-b51697c');
 
 /**
  * @license
