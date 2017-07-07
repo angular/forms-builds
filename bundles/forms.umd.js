7a7566543e42e9341f71838f52e828d722d304c8
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:13:54 +0000

    6bae73c refactor(common): replace `Object.assign` with the spread operator (#17982)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-11db3bd
+ * @license Angular v4.3.0-beta.1-6bae73c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-11db3bd
+ * @license Angular v4.3.0-beta.1-6bae73c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-11db3bd');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-6bae73c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
