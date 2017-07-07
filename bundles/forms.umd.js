4515d1f8ce9cd8f7357056573e63d76f14f3221e
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 22:14:47 +0000

    c81ad9d feat(common): two missing features in HttpClient (#17996)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-37797e2
+ * @license Angular v4.3.0-beta.1-c81ad9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-37797e2
+ * @license Angular v4.3.0-beta.1-c81ad9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-37797e2');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-c81ad9d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
