5591f77ac892f6769e8c31346e93701634094b71
Jason Aden <jasonaden@angular.io>
2017-07-07 19:33:56 +0000

    37797e2 feat(common): new HttpClient API

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-72747e5
+ * @license Angular v4.3.0-beta.1-37797e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-72747e5');
+const VERSION = new Version('4.3.0-beta.1-37797e2');
 
 /**
  * @license
