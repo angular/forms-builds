5996bf81c25d7caa144fa2d58dfae724a2aa3cd5
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:47:31 +0000

    c1474f3 feat(compiler-cli): add parameters to ngc main needed by bazel rules (#17885)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-0ede642
+ * @license Angular v4.3.0-beta.1-c1474f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-0ede642');
+const VERSION = new Version('4.3.0-beta.1-c1474f3');
 
 /**
  * @license
