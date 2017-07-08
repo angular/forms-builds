c46ad1bdc999f30fd7d68c48fd7325d8a57583b5
Jason Aden <jasonaden@google.com>
2017-07-08 05:15:36 +0000

    b69f0fa docs: add changelog for 4.2.6

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.5-8b8b4cb');
+var VERSION = new Version('4.2.6-b69f0fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
