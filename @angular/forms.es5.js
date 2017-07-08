4ddb4ef03aa92eda09cfb87182940126187d9a5c
Jason Aden <jasonaden@google.com>
2017-07-08 04:30:56 +0000

    8b8b4cb refactor(upgrade): move shareable functionality to `UpgradeHelper` class (#17971)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.5-3e61bf7
+ * @license Angular v4.2.5-8b8b4cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.5-3e61bf7');
+var VERSION = new Version('4.2.5-8b8b4cb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
