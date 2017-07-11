b96fc8f67bf8a7e46a800e7e4a5e124841dffa74
Igor Minar <iminar@google.com>
2017-07-11 19:42:58 +0000

    1b1a6ba docs: fix 404 link to SFEIR website (#17913)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-df06e8b
+ * @license Angular v4.3.0-rc.0-1b1a6ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-df06e8b');
+const VERSION = new Version('4.3.0-rc.0-1b1a6ba');
 
 /**
  * @license
