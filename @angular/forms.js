2a921d39b66be738b42fac78a8012b2a24cf2cec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 16:53:38 +0000

    8e56c3c docs: fix type error in HttpClient sample

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-7955cac
+ * @license Angular v4.3.0-rc.0-8e56c3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-7955cac');
+const VERSION = new Version('4.3.0-rc.0-8e56c3c');
 
 /**
  * @license
