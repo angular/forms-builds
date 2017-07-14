ff7af0c0ac8b4bd1e164185fd4b0e607d89524e9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:39:44 +0000

    a0b06be docs: add changelog for 4.3.0

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e0fa727
+ * @license Angular v4.3.0-a0b06be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e0fa727');
+const VERSION = new Version('4.3.0-a0b06be');
 
 /**
  * @license
