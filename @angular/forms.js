3e4270d8a4290fccb8340cea55d8ee550eae0a35
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-15 08:09:52 +0000

    0bf0c35 build(aio): render type parameters of API function exports

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-97e6901
+ * @license Angular v4.3.0-0bf0c35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-97e6901');
+const VERSION = new Version('4.3.0-0bf0c35');
 
 /**
  * @license
