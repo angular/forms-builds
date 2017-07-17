fe51364839b231558f132d563cea5e7da2027080
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:34:08 +0000

    8c81c62 docs: fix HttpClient logging's sample

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-47b3ecd
+ * @license Angular v4.3.0-8c81c62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-47b3ecd');
+const VERSION = new Version('4.3.0-8c81c62');
 
 /**
  * @license
