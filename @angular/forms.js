234ca041c4c555083a3fced4e99e3e2bcfe88c32
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:44:55 +0000

    9f99f4f docs: fix HttpClient sample

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c6ad212
+ * @license Angular v4.3.0-9f99f4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-c6ad212');
+const VERSION = new Version('4.3.0-9f99f4f');
 
 /**
  * @license
