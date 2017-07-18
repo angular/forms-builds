5f4ed673d4a9c1b7121e4c6c6f5e052b8e6c7348
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:20:35 +0000

    59c23c7 feat(upgrade): propagate touched state of NgModelController

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-3b588fe
+ * @license Angular v4.3.0-59c23c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-3b588fe');
+const VERSION = new Version('4.3.0-59c23c7');
 
 /**
  * @license
