87ea31cec9bf65cb8752e93aca4a2e6f01635fb6
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 16:51:21 +0000

    cdb069a docs(aio): fix cheatsheet layout for narrow screens

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c453b7b
+ * @license Angular v4.3.0-cdb069a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-c453b7b');
+const VERSION = new Version('4.3.0-cdb069a');
 
 /**
  * @license
