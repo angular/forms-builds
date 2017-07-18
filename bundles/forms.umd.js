888dfaa3fb241da97072c6f3e7754edcaac5802d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:34:44 +0000

    256bc8a docs(http): Make name of injected HttpTestingController consistent

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5880,7 +5880,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-59c23c7');
+var VERSION = new _angular_core.Version('4.3.0-256bc8a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
