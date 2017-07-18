37dc2bca22f07804b451025af97ee2349ed4e2f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:30:31 +0000

    8076482 fix(platform-server): provide XhrFactory for HttpClient

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-a5205c6
+ * @license Angular v4.3.0-8076482
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-a5205c6');
+const VERSION = new Version('4.3.0-8076482');
 
 /**
  * @license
