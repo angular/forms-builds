b60cb8a1163adfc2cebc3cecc286b93cd4cfe36c
Miško Hevery <misko@hevery.com>
2017-07-20 16:55:30 +0000

    8de44cf docs: fix wrong link in CONTRIBUTING.md (#18228)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-54ea5b6
+ * @license Angular v5.0.0-beta.0-8de44cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-54ea5b6');
+const VERSION = new Version('5.0.0-beta.0-8de44cf');
 
 /**
  * @license
