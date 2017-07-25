bb418bca287ce61369cf2cf97a5e7266d7c42def
Miško Hevery <misko@hevery.com>
2017-07-25 23:11:25 +0000

    b582e2b docs(aio): update examples to 4.3

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-91ab39c
+ * @license Angular v5.0.0-beta.0-b582e2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5928,7 +5928,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-91ab39c');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-b582e2b');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
