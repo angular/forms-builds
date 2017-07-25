f871d60627793173d02f0b37a5d78bd26192f2cf
Miško Hevery <misko@hevery.com>
2017-07-25 22:54:47 +0000

    91ab39c docs(aio) - Fixed link to the glossary dash-case term (#18311)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-6279e50
+ * @license Angular v5.0.0-beta.0-91ab39c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-6279e50
+ * @license Angular v5.0.0-beta.0-91ab39c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5928,7 +5928,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-6279e50');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-91ab39c');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
