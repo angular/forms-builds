10a8e51f224a11b91b9a927ab25c44f14483de19
Miško Hevery <misko@hevery.com>
2017-07-26 17:11:52 +0000

    205abe8 build: fix broken bazel build (#18335)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-b582e2b
+ * @license Angular v5.0.0-beta.0-205abe8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-b582e2b
+ * @license Angular v5.0.0-beta.0-205abe8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5928,7 +5928,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-b582e2b');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-205abe8');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
