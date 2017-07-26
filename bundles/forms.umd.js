864560025e99640cf6fad798707cb58a55e9412d
Miško Hevery <misko@hevery.com>
2017-07-26 23:33:16 +0000

    086f4aa fix(router): child CanActivate guard should wait for parent to complete (#18110)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-205abe8
+ * @license Angular v5.0.0-beta.0-086f4aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-205abe8
+ * @license Angular v5.0.0-beta.0-086f4aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5928,7 +5928,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-205abe8');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-086f4aa');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
