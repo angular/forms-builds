cea149eca68fd31a911763e47a4ae321ce6805b8
Miško Hevery <misko@hevery.com>
2017-07-27 21:21:13 +0000

    12b8e1a Revert "fix(router): should throw when lazy loaded module doesn't define any routes (#15001)"

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.1-2245748
+ * @license Angular v4.3.1-12b8e1a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5849,7 +5849,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.1-2245748');
+var VERSION = new Version('4.3.1-12b8e1a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.1-2245748
+ * @license Angular v4.3.1-12b8e1a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5497,7 +5497,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.1-2245748');
+const VERSION = new Version('4.3.1-12b8e1a');
 
 /**
  * @license
