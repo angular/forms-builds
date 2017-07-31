e64081585cda046b36d58b2cbb9a297e614fb616
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:46:54 +0000

    381471d fix(compiler): fix for element needing implicit parent placed in top-level ng-container

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-ebef5e6
+ * @license Angular v5.0.0-beta.1-381471d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5595,7 +5595,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-ebef5e6');
+const VERSION = new Version('5.0.0-beta.1-381471d');
 
 /**
  * @fileoverview added by tsickle
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-ebef5e6
+ * @license Angular v5.0.0-beta.1-381471d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.1-ebef5e6
+ * @license Angular v5.0.0-beta.1-381471d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5976,7 +5976,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-ebef5e6');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-381471d');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
