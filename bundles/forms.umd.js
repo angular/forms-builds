3869c281dbfd0cf94fc531391bf486af065bb26b
Victor Berchet <victor@suumit.com>
2017-08-02 23:23:06 +0000

    b65fe3e ci: remove chromium fold reference (#18445)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-dbc5c58
+ * @license Angular v4.3.3-b65fe3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5883,7 +5883,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-dbc5c58');
+var VERSION = new _angular_core.Version('4.3.3-b65fe3e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
