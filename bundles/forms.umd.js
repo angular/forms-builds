a825f18b97287618263f3f18997c66efd4073da8
Victor Berchet <victor@suumit.com>
2017-08-03 00:44:04 +0000

    1640d2a refactor(platform-browser): compiler platform-browser packages cleanly (#18464)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-5f501c7
+ * @license Angular v5.0.0-beta.2-1640d2a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5979,7 +5979,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-5f501c7');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-1640d2a');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
