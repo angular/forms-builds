6a9835841d906fc27b5a92cafdb0bcb4b44a5f90
Victor Berchet <victor@suumit.com>
2017-08-07 19:25:02 +0000

    e0660b1 fix(animations): support persisting dynamic styles within animation states (#18468)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-3212f8c
+ * @license Angular v4.3.3-e0660b1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-3212f8c
+ * @license Angular v4.3.3-e0660b1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5892,7 +5892,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-3212f8c');
+var VERSION = new _angular_core.Version('4.3.3-e0660b1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
