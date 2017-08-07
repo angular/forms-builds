43f3ffcb5d996dc4ea4097bb74ef8e78ae370c27
Victor Berchet <victor@suumit.com>
2017-08-07 19:39:35 +0000

    04b18a9 docs(common): fix the DatePipe API docs (#18548)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-05472cb
+ * @license Angular v5.0.0-beta.2-04b18a9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5663,7 +5663,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-05472cb');
+const VERSION = new Version('5.0.0-beta.2-04b18a9');
 
 /**
  * @fileoverview added by tsickle
