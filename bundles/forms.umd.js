f0d714050928eaf7d333a9475bffac8cbd3b0b41
Victor Berchet <victor@suumit.com>
2017-08-09 22:01:29 +0000

    f4cb453 test(aio): fix running docs examples against local builds (#18520)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-3efc88f
+ * @license Angular v4.3.3-f4cb453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-3efc88f
+ * @license Angular v4.3.3-f4cb453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5892,7 +5892,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-3efc88f');
+var VERSION = new _angular_core.Version('4.3.3-f4cb453');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
