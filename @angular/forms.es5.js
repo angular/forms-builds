e83e4f64090ea13d506f2060215fa87e6a6a5280
Victor Berchet <victor@suumit.com>
2017-08-10 19:00:40 +0000

    06e479f docs: add changelog for 4.3.4

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-77fa3c3
+ * @license Angular v4.3.4-06e479f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5861,7 +5861,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.3-77fa3c3');
+var VERSION = new Version('4.3.4-06e479f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-77fa3c3
+ * @license Angular v4.3.4-06e479f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-77fa3c3
+ * @license Angular v4.3.4-06e479f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5892,7 +5892,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-77fa3c3');
+var VERSION = new _angular_core.Version('4.3.4-06e479f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
