808ae6b73bcc4afcd219d007fe37b195ed29f024
Hans Larsen <hans@hansl.ca>
2017-08-11 20:58:18 +0000

    d7be4f1 perf(aio): update to new version of build-optimizer

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-06e479f
+ * @license Angular v4.3.4-d7be4f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5861,7 +5861,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-06e479f');
+var VERSION = new Version('4.3.4-d7be4f1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
