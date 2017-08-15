2af272b61a3dbe76f7072b689ec752ba4cba8965
Hans <hans@hansl.ca>
2017-08-15 19:48:16 +0000

    9320f34 docs(aio): add Metadata guide based on Chuck’s docs

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-d2c0d98
+ * @license Angular v5.0.0-beta.3-9320f34
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.3-d2c0d98
+ * @license Angular v5.0.0-beta.3-9320f34
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6140,7 +6140,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.3-d2c0d98');
+var VERSION = new _angular_core.Version('5.0.0-beta.3-9320f34');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
