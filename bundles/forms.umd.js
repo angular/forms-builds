c3fb8b39b6c16abc06f940ea7940f99b469e06bd
Hans Larsen <hans@hansl.ca>
2017-08-16 18:26:34 +0000

    75d484e docs: add changelog for 4.3.5

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-32ff21c
+ * @license Angular v5.0.0-beta.3-75d484e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.3-32ff21c
+ * @license Angular v5.0.0-beta.3-75d484e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6222,7 +6222,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.3-32ff21c');
+var VERSION = new _angular_core.Version('5.0.0-beta.3-75d484e');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
