476b6c1c4e2ee17ec68f3e06bf6c2c07df6e79b7
Hans <hans@hansl.ca>
2017-08-16 18:53:45 +0000

    bc22ff1 fix(language-service): remove tsickle dependency

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.3-75d484e
+ * @license Angular v5.0.0-beta.3-bc22ff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6191,7 +6191,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.3-75d484e');
+var VERSION = new Version('5.0.0-beta.3-bc22ff1');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-75d484e
+ * @license Angular v5.0.0-beta.3-bc22ff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5833,7 +5833,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-75d484e');
+const VERSION = new Version('5.0.0-beta.3-bc22ff1');
 
 /**
  * @fileoverview added by tsickle
