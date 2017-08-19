118e49a531a6da7fde0f1299edce3c6a243f9808
Miško Hevery <misko@hevery.com>
2017-08-19 14:33:50 +0000

    e1f45a3 fix(animations): restore auto-style support for removed DOM nodes (#18787)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.5-77ebd2b
+ * @license Angular v4.3.5-e1f45a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5924,7 +5924,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.5-77ebd2b');
+var VERSION = new Version('4.3.5-e1f45a3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
