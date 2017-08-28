daa367527d9cb8b50180f2881c0165bdcfd7e671
Miško Hevery <misko@hevery.com>
2017-08-28 18:49:19 +0000

    f0c681a build: disable sass under bazel(#18848)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-3d2ee6c
+ * @license Angular v5.0.0-beta.4-f0c681a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5848,7 +5848,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-3d2ee6c');
+const VERSION = new Version('5.0.0-beta.4-f0c681a');
 
 /**
  * @fileoverview added by tsickle
