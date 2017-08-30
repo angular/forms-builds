81d7d18a722fc80f6252331af6af76ab7c8c83c1
Jason Aden <jasonaden@google.com>
2017-08-30 00:47:08 +0000

    a203a95 fix(common): fix improper packaging for @angular/common/http (#18613)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5572,7 +5572,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-8821723');
+const VERSION = new Version('4.3.6-a203a95');
 
 /**
  * @license
