d0e9ab2ccb768b83251cc0dfcdf1c22806b8657f
Jason Aden <jasonaden@google.com>
2017-08-30 00:46:19 +0000

    8821723 fix(common): fix XSSI prefix stripping by using JSON.parse always (#18466)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-dfe2bad
+ * @license Angular v4.3.6-8821723
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5572,7 +5572,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-dfe2bad');
+const VERSION = new Version('4.3.6-8821723');
 
 /**
  * @license
