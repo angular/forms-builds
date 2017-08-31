82c9a6cf7abb0ab87a7116a68ed49556f64804db
Jason Aden <jasonaden@google.com>
2017-08-31 01:40:56 +0000

    ba7d70e build: fix changelog to remove AIO line items (#18956)

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5924,7 +5924,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.6-497e017');
+var VERSION = new Version('4.3.6-ba7d70e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-497e017
+ * @license Angular v4.3.6-ba7d70e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5955,7 +5955,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-497e017');
+var VERSION = new _angular_core.Version('4.3.6-ba7d70e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
