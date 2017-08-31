3b40a669b80c935c2ddb84fcde8f9d8b0a94fdee
Jason Aden <jasonaden@google.com>
2017-08-31 22:58:32 +0000

    ee04217 refactor(compiler-cli): expose ngtools api separately (#18978)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-a1293b2
+ * @license Angular v5.0.0-beta.5-ee04217
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5848,7 +5848,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-a1293b2');
+const VERSION = new Version('5.0.0-beta.5-ee04217');
 
 /**
  * @fileoverview added by tsickle
