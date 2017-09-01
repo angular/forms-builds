ed590126d809a9a73b089b16e447c35304a0a493
Jason Aden <jasonaden@google.com>
2017-09-01 17:53:55 +0000

    1cc3fe2 fix(animations): do not leak DOM nodes/styling for host triggered animations (#18853)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-ba7d70e
+ * @license Angular v4.3.6-1cc3fe2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5572,7 +5572,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-ba7d70e');
+const VERSION = new Version('4.3.6-1cc3fe2');
 
 /**
  * @license
