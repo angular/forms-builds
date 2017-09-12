09da8420e7eef0168310a18c00d781d0aef8254c
Matias Niemelä <matias@yearofmoo.com>
2017-09-12 18:22:57 +0000

    9ab9437 build(aio): auto-link more code items

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-ca5aeba
+ * @license Angular v5.0.0-beta.6-9ab9437
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.6-ca5aeba
+ * @license Angular v5.0.0-beta.6-9ab9437
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8113,7 +8113,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-ca5aeba');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-9ab9437');
 
 /**
  * @fileoverview added by tsickle
--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-ca5aeba
+ * @license Angular v5.0.0-beta.6-9ab9437
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5831,7 +5831,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.6-ca5aeba');
+const VERSION = new Version('5.0.0-beta.6-9ab9437');
 
 /**
  * @fileoverview added by tsickle
