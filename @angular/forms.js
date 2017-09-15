221a5687fa1fec45505fa1f1feb1c1a3a27e9fc9
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:20:33 +0000

    ff87294 docs(changelog): fix the changelog link for comparing 4.4.1 to 4.3.6

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5573,7 +5573,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.1-0c72f7d');
+const VERSION = new Version('4.4.1-ff87294');
 
 /**
  * @license
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.1-0c72f7d
+ * @license Angular v4.4.1-ff87294
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5956,7 +5956,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.1-0c72f7d');
+var VERSION = new _angular_core.Version('4.4.1-ff87294');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
