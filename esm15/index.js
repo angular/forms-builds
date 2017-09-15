588184fcf6724c754654a6dc70c2e3910b5c4cc2
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:20:58 +0000

    2373186 docs: update combined changelog for 4.4 stable

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b6431c6
+ * @license Angular v5.0.0-beta.7-2373186
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5831,7 +5831,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-b6431c6');
+const VERSION = new Version('5.0.0-beta.7-2373186');
 
 /**
  * @fileoverview added by tsickle
