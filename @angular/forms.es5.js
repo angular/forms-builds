464d6ba8f94fdb5cb7762c0b5669f5d23082cb02
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:06:32 +0000

    818f4a7 release: cut the 4.4.1 release

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.0-RC.0-b20c5d2
+ * @license Angular v4.4.1-818f4a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5925,7 +5925,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.0-RC.0-b20c5d2');
+var VERSION = new Version('4.4.1-818f4a7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
