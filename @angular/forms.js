3d823af06ba3024f22c73a22e633f819e43df35f
Igor Minar <igor@angularjs.org>
2017-09-19 22:59:40 +0000

    e2e8ba6 release: cut the 4.4.3 release

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.2-0371538
+ * @license Angular v4.4.3-e2e8ba6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5573,7 +5573,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.2-0371538');
+const VERSION = new Version('4.4.3-e2e8ba6');
 
 /**
  * @license
