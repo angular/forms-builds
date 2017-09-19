242e987087dd002e2b6b78c192ff45c8286c0be0
Igor Minar <igor@angularjs.org>
2017-09-19 23:32:30 +0000

    9d2236a ci: update yarn to 1.0.2 (#19270)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e17128e
+ * @license Angular v5.0.0-beta.7-9d2236a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-e17128e
+ * @license Angular v5.0.0-beta.7-9d2236a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8113,7 +8113,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-e17128e');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-9d2236a');
 
 /**
  * @fileoverview added by tsickle
