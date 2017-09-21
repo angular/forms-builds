11bf3bd8293b88ba80fd834fda40bc7844c38d2d
Igor Minar <igor@angularjs.org>
2017-09-21 00:13:29 +0000

    4586fcc refactor(upgrade): use correct property name (#19180)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-9e84719');
+const VERSION = new Version('5.0.0-beta.7-4586fcc');
 
 /**
  * @fileoverview added by tsickle
