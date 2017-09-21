41707602108b74e7d9dc60aacfe139d35ec5d648
Igor Minar <igor@angularjs.org>
2017-09-21 18:43:50 +0000

    4c73b52 docs(aio): tidy up tooling documentation (#18151)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-4586fcc
+ * @license Angular v5.0.0-beta.7-4c73b52
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-4586fcc');
+const VERSION = new Version('5.0.0-beta.7-4c73b52');
 
 /**
  * @fileoverview added by tsickle
