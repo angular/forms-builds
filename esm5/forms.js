4ca3d7fc036e03250490217d37c62e96122369ad
Igor Minar <igor@angularjs.org>
2017-09-22 21:39:25 +0000

    0f5c70d build: update npm dependencies (#19328)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9bbf009
+ * @license Angular v5.0.0-beta.7-0f5c70d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7936,7 +7936,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-9bbf009');
+var VERSION = new Version('5.0.0-beta.7-0f5c70d');
 
 /**
  * @fileoverview added by tsickle
