8b58579d8339637ad0af5a419d4f853f5bb00128
Victor Berchet <victor@suumit.com>
2017-09-25 20:54:17 +0000

    13613d4 fix(compiler): skip &nbsp; when trimming / removing whitespaces (#19310)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-97e02c2
+ * @license Angular v5.0.0-beta.7-13613d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-beta.7-97e02c2
+ * @license Angular v5.0.0-beta.7-13613d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7974,7 +7974,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-97e02c2');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-13613d4');
 
 /**
  * @fileoverview added by tsickle
