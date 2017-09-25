b83be535ce5e8aa9e63f7f20b6b8f87aa7333bff
Victor Berchet <victor@suumit.com>
2017-09-25 20:03:12 +0000

    4767902 fix(compiler-cli): do not validate metadata from declaration files (#19324)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e224e3d
+ * @license Angular v5.0.0-beta.7-4767902
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7936,7 +7936,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-e224e3d');
+var VERSION = new Version('5.0.0-beta.7-4767902');
 
 /**
  * @fileoverview added by tsickle
