2dbd3672cceb6cbff47fb3c293e0b01e6f83dfc4
Victor Berchet <victor@suumit.com>
2017-09-26 20:44:22 +0000

    bc81fbd fix(animations): properly support the query limit option value (#19419)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-c7aa8a1
+ * @license Angular v4.4.3-bc81fbd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5573,7 +5573,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-c7aa8a1');
+const VERSION = new Version('4.4.3-bc81fbd');
 
 /**
  * @license
