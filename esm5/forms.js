ffc45f9d33989d5732c43236356c19aab24beb47
Victor Berchet <victor@suumit.com>
2017-09-27 22:44:15 +0000

    334caf8 docs(aio): add copy about NgForOf (#18686)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-48c6ece
+ * @license Angular v5.0.0-beta.7-334caf8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7936,7 +7936,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-48c6ece');
+var VERSION = new Version('5.0.0-beta.7-334caf8');
 
 /**
  * @fileoverview added by tsickle
