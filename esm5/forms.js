350d6b6143e89d8315d3eb0070b0f44bdc5346c9
Victor Berchet <victor@suumit.com>
2017-09-27 21:01:50 +0000

    f7c7038 docs(aio): update metadata documentation to include expression lowering (#19203)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-627f048
+ * @license Angular v5.0.0-beta.7-f7c7038
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7936,7 +7936,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-627f048');
+var VERSION = new Version('5.0.0-beta.7-f7c7038');
 
 /**
  * @fileoverview added by tsickle
