c48561ff242185dd36a7021c06d902bde9a5e0cc
Victor Berchet <victor@suumit.com>
2017-09-28 17:01:48 +0000

    2b84b86 refactor(core): Removed readonly getters and changed to readonly (#19225)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-c1b029a
+ * @license Angular v5.0.0-beta.7-2b84b86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-c1b029a');
+const VERSION = new Version('5.0.0-beta.7-2b84b86');
 
 /**
  * @fileoverview added by tsickle
