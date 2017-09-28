960bae9b02c827f8358a61f23d306a9daa306388
Victor Berchet <victor@suumit.com>
2017-09-28 19:43:04 +0000

    bed8ac7 docs: add changelog for 4.4.4

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-a9d32a3
+ * @license Angular v5.0.0-beta.7-bed8ac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5774,7 +5774,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-a9d32a3');
+const VERSION = new Version('5.0.0-beta.7-bed8ac7');
 
 /**
  * @fileoverview added by tsickle
