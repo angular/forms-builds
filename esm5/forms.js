19720ac7852a804b1a66e2e2724734265e83be44
Victor Berchet <victor@suumit.com>
2017-09-28 16:48:45 +0000

    5406a49 docs: add 'bazel' as an Angular component (#19346)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-8dbe2af
+ * @license Angular v5.0.0-beta.7-5406a49
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7936,7 +7936,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.7-8dbe2af');
+var VERSION = new Version('5.0.0-beta.7-5406a49');
 
 /**
  * @fileoverview added by tsickle
