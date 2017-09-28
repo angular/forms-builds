d4a9cc60d2c40efa5bcf5966a9266f235f2d6b4b
Victor Berchet <victor@suumit.com>
2017-09-28 18:18:58 +0000

    c3b39ba fix(compiler): correctly map error message locations (#19424)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-2cd88bf
+ * @license Angular v4.4.3-c3b39ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5573,7 +5573,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-2cd88bf');
+const VERSION = new Version('4.4.3-c3b39ba');
 
 /**
  * @license
