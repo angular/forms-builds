1a50344f54cbcf3cdbe715e82caaccab6003fc87
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 16:04:26 +0000

    544a7ad docs(aio): Fix incorrect wording

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-d489ad5
+ * @license Angular v4.4.4-544a7ad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-d489ad5
+ * @license Angular v4.4.4-544a7ad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5956,7 +5956,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.4-d489ad5');
+var VERSION = new _angular_core.Version('4.4.4-544a7ad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
