68c5087be0ea2dbc399503c60de036de57089051
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:33:11 +0000

    f98eb35 docs(aio): document Custom Async validators

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.4-95f3b1d
+ * @license Angular v4.4.4-f98eb35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5925,7 +5925,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.4-95f3b1d');
+var VERSION = new Version('4.4.4-f98eb35');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
