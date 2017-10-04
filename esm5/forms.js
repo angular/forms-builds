c9ba8d39de457ef02cd21506706d20c6adb3de3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:20:49 +0000

    696af79 fix(compiler): properly work on windows

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-d4d9009
+ * @license Angular v5.0.0-rc.0-696af79
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7936,7 +7936,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.0-d4d9009');
+var VERSION = new Version('5.0.0-rc.0-696af79');
 
 /**
  * @fileoverview added by tsickle
