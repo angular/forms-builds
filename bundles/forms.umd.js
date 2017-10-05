5fbd4b483d8323794605ba97fcd116361e476e7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-05 21:24:14 +0000

    ccc25ee fix: reformat files from previous cherry-picks

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-8f66880
+ * @license Angular v4.4.4-ccc25ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-8f66880
+ * @license Angular v4.4.4-ccc25ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5956,7 +5956,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.4-8f66880');
+var VERSION = new _angular_core.Version('4.4.4-ccc25ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
