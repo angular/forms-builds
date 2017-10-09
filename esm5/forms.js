3d1d44cad066179a9e1f94d001e7afbaefa326cc
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 19:03:21 +0000

    c3a5269 perf(animations): reduce size of bundle by removing AST classes (#19539)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-b1ca5d4
+ * @license Angular v5.0.0-rc.1-c3a5269
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7936,7 +7936,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-b1ca5d4');
+var VERSION = new Version('5.0.0-rc.1-c3a5269');
 
 /**
  * @fileoverview added by tsickle
