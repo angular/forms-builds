e4a70d108accc754cc73e90151ac4e22f90592ba
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 23:19:07 +0000

    f89ac92 test(aio): temporarily disable failing examples e2e tests (#19600)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5956,7 +5956,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.4-ccc25ee');
+var VERSION = new _angular_core.Version('4.4.4-f89ac92');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
