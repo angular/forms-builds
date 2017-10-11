263fbfaeff5d176c9bc19fc721f958cb05539877
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:53:29 +0000

    3232125 build(aio): remove unnecessary `--` from yarn commands (#19565)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-f89ac92
+ * @license Angular v4.4.4-3232125
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-f89ac92
+ * @license Angular v4.4.4-3232125
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5956,7 +5956,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.4-f89ac92');
+var VERSION = new _angular_core.Version('4.4.4-3232125');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
