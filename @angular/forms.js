38007f082b4244263ed1f4245f4dc10339cb9531
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 20:27:48 +0000

    912068e docs: add changelog for 4.4.5

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-f27f6e4
+ * @license Angular v4.4.5-912068e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5580,7 +5580,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-f27f6e4');
+const VERSION = new Version('4.4.5-912068e');
 
 /**
  * @license
