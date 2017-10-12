262ccca629a4203c27a57767e1bfe4780fdeb8fa
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 20:55:42 +0000

    32a814b docs: removing unnecessary commits from change log

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-912068e
+ * @license Angular v4.4.5-32a814b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.5-912068e
+ * @license Angular v4.4.5-32a814b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5963,7 +5963,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.5-912068e');
+var VERSION = new _angular_core.Version('4.4.5-32a814b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
