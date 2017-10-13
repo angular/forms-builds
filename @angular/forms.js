9155e5f4ee66f786e3fe74d692ae0486f7ee3403
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 17:05:23 +0000

    f983a6c fix(animations): properly support boolean-based transitions and state changes (#19672)

--- @angular/forms.js
+++ @angular/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-18f1b01
+ * @license Angular v4.4.5-f983a6c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5580,7 +5580,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-18f1b01');
+const VERSION = new Version('4.4.5-f983a6c');
 
 /**
  * @license
--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-18f1b01
+ * @license Angular v4.4.5-f983a6c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.5-18f1b01
+ * @license Angular v4.4.5-f983a6c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5963,7 +5963,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.5-18f1b01');
+var VERSION = new _angular_core.Version('4.4.5-f983a6c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
