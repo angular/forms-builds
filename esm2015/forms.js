60077869f5856d42e1c258d21f4bcd4a7da0fa83
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:35:35 +0000

    e81d1fc docs(animations): add missing bracket to fadeAnimation

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-02394d2
+ * @license Angular v5.0.0-rc.2-e81d1fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.2-02394d2');
+const VERSION = new Version('5.0.0-rc.2-e81d1fc');
 
 /**
  * @fileoverview added by tsickle
