f3cb4ccc7672b4efb8fc3804d8d80f9e83ed7dd2
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 22:50:31 +0000

    1be22df ci(aio): raise payload limit to accommodate the new search feature (#19704)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-f983a6c
+ * @license Angular v4.4.5-1be22df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.5-f983a6c
+ * @license Angular v4.4.5-1be22df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5963,7 +5963,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.5-f983a6c');
+var VERSION = new _angular_core.Version('4.4.5-1be22df');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
