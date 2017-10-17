0a95832041ff29794f75ee9fa3206a5d1042b391
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 22:25:44 +0000

    af8c2fa build: don’t make BrowserStack required as it fails with “Access denied”

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-0789601
+ * @license Angular v4.4.5-af8c2fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5932,7 +5932,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-0789601');
+var VERSION = new Version('4.4.5-af8c2fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
