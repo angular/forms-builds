5b462a2fb670b1bd09229bb7a1472eb53dbd8cdc
Tobias Bosch <tbosch1009@gmail.com>
2017-10-19 00:46:22 +0000

    7231f5e docs: add changelog for 4.4.6

--- @angular/forms.es5.js
+++ @angular/forms.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-269f5ac
+ * @license Angular v4.4.6-7231f5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5932,7 +5932,7 @@ FormBuilder.ctorParameters = function () { return []; };
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-269f5ac');
+var VERSION = new Version('4.4.6-7231f5e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
