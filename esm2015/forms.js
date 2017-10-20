d1748531a9ddba9ecf7f7a14f7626c621d120afb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:02:37 +0000

    441e01c fix(aio): simplify GaService to avoid e2e test failures

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-fc86352
+ * @license Angular v5.0.0-rc.3-441e01c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-fc86352');
+const VERSION = new Version('5.0.0-rc.3-441e01c');
 
 /**
  * @fileoverview added by tsickle
