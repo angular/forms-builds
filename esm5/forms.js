bdf40605295c8c69ebc3dab95b91a874feec1cac
Matias Niemelä <matias@yearofmoo.com>
2017-10-28 07:12:07 +0000

    308fc8e docs: add changelog for 5.0.0-rc.8

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.7-394d951
+ * @license Angular v5.0.0-rc.8-308fc8e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7943,7 +7943,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.7-394d951');
+var VERSION = new Version('5.0.0-rc.8-308fc8e');
 
 /**
  * @fileoverview added by tsickle
