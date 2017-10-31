17800597f332364c791312f7b17aef9062e4bfd3
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:59:05 +0000

    c295aee build: add `release` as a valid commit message subject (#19955)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-e3a16ed
+ * @license Angular v5.0.0-rc.8-c295aee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-e3a16ed');
+const VERSION = new Version('5.0.0-rc.8-c295aee');
 
 /**
  * @fileoverview added by tsickle
