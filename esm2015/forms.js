a8587de1a3faee91961032f5f633ea5ac1bebd58
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 06:06:31 +0000

    5225fdb build(aio): add i18n boilerplate type (#20004)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-f5b7f2b
+ * @license Angular v5.0.0-rc.9-5225fdb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.9-f5b7f2b');
+const VERSION = new Version('5.0.0-rc.9-5225fdb');
 
 /**
  * @fileoverview added by tsickle
