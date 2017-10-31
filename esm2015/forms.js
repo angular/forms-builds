d5021143ae74092c0f60d0739e9098f27cb4d39b
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:06:57 +0000

    cf5fce8 build: update rollup lint rule from bad merge (#20047)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-f1248b6
+ * @license Angular v5.0.0-rc.8-cf5fce8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-f1248b6');
+const VERSION = new Version('5.0.0-rc.8-cf5fce8');
 
 /**
  * @fileoverview added by tsickle
