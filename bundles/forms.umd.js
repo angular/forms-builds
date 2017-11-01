e77b6b7b13269272294d32863fd086cdcd382c10
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:53:04 +0000

    280dada docs(aio): fix typo in attribute-directives

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-c30eff8
+ * @license Angular v5.0.0-280dada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-c30eff8
+ * @license Angular v5.0.0-280dada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7981,7 +7981,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-c30eff8');
+var VERSION = new _angular_core.Version('5.0.0-280dada');
 
 /**
  * @fileoverview added by tsickle
