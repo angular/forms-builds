de0d84fc1ac90a803f53bf68afdaab4f1933b66a
Victor Berchet <victor@suumit.com>
2017-11-02 23:43:15 +0000

    678d1cf fix(core): __symbol__ should return __zone_symbol__ without zone.js loaded (#19541)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-25843fe
+ * @license Angular v5.0.0-678d1cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-25843fe');
+const VERSION = new Version('5.0.0-678d1cf');
 
 /**
  * @fileoverview added by tsickle
