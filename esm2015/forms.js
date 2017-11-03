b95be878780bc4307be731a12fef786999ac58b0
Victor Berchet <victor@suumit.com>
2017-11-03 19:57:16 +0000

    e4cd3b0 test(aio): make e2e tests less flakey

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-9811bef
+ * @license Angular v5.0.0-e4cd3b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-9811bef');
+const VERSION = new Version('5.0.0-e4cd3b0');
 
 /**
  * @fileoverview added by tsickle
