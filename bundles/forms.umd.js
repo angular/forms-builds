c27849136039992eef4fbd70ff344634bbf22dbc
Victor Berchet <victor@suumit.com>
2017-11-03 19:03:18 +0000

    e6f16a7 docs(aio): Fix typo in Architecture Overview. (#20073)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-678d1cf
+ * @license Angular v5.0.0-e6f16a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-678d1cf
+ * @license Angular v5.0.0-e6f16a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7981,7 +7981,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-678d1cf');
+var VERSION = new _angular_core.Version('5.0.0-e6f16a7');
 
 /**
  * @fileoverview added by tsickle
