f0232b08b1104043016c67f2d716bffcd0915431
Victor Berchet <victor@suumit.com>
2017-11-03 22:41:17 +0000

    200d92d fix(core): should support event.stopImmediatePropagation (#19222)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dbec3ca
+ * @license Angular v5.1.0-beta.0-200d92d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-dbec3ca');
+const VERSION = new Version('5.1.0-beta.0-200d92d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dbec3ca
+ * @license Angular v5.1.0-beta.0-200d92d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7943,7 +7943,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-dbec3ca');
+var VERSION = new Version('5.1.0-beta.0-200d92d');
 
 /**
  * @fileoverview added by tsickle
