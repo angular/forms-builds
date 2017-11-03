f6639cea8e32c36077faaf319f55209971175139
GitHub <noreply@github.com>
2017-11-03 22:13:25 +0000

    f076401 revert: feat(core): add source to `StaticInjectorError` message (#19482) (#20171)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-a99eb16
+ * @license Angular v5.1.0-beta.0-f076401
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-a99eb16');
+const VERSION = new Version('5.1.0-beta.0-f076401');
 
 /**
  * @fileoverview added by tsickle
