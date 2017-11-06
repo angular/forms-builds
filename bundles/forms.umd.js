38c8c509ee2931c864c4fbbf17c0e56ff597b9d8
Victor Berchet <victor@suumit.com>
2017-11-06 22:54:40 +0000

    6e8e3bd refactor(core): misc changes and integrate review feedback on #19996

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-b489259
+ * @license Angular v5.1.0-beta.0-6e8e3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-b489259
+ * @license Angular v5.1.0-beta.0-6e8e3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7981,7 +7981,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-b489259');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-6e8e3bd');
 
 /**
  * @fileoverview added by tsickle
