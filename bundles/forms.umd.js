729be574430b434beccf7651d294b7a17b3d6f3e
Victor Berchet <victor@suumit.com>
2017-11-06 18:37:39 +0000

    3ab0963 docs(aio): update ToH for CLI (#19811)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-7083791
+ * @license Angular v5.0.0-3ab0963
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-7083791
+ * @license Angular v5.0.0-3ab0963
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7981,7 +7981,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-7083791');
+var VERSION = new _angular_core.Version('5.0.0-3ab0963');
 
 /**
  * @fileoverview added by tsickle
