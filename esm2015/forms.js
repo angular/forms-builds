e6b3c0e2dd29c1efb8b7890058fa80218c95fabd
Victor Berchet <victor@suumit.com>
2017-11-06 18:23:24 +0000

    8b50ed0 docs(aio): fixed case-typo for code (#20196)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-3997d97
+ * @license Angular v5.1.0-beta.0-8b50ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-3997d97');
+const VERSION = new Version('5.1.0-beta.0-8b50ed0');
 
 /**
  * @fileoverview added by tsickle
