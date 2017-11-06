bb8ad8cc7af1d0ed718708e43e7ebaeb44d8d51d
Victor Berchet <victor@suumit.com>
2017-11-06 18:42:24 +0000

    6b74883 build(aio): upgrade to @anglar/core@5.0.0 and cli@1.5.0

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-049c896
+ * @license Angular v5.1.0-beta.0-6b74883
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7943,7 +7943,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-049c896');
+var VERSION = new Version('5.1.0-beta.0-6b74883');
 
 /**
  * @fileoverview added by tsickle
