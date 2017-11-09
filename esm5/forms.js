8edcc000fea2607665c19855f57f9a161acfbc00
Victor Berchet <victor@suumit.com>
2017-11-09 00:17:32 +0000

    28985cb test(compiler): do not use `a` as a content selector

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-9de45fa
+ * @license Angular v5.0.1-28985cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7943,7 +7943,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-9de45fa');
+var VERSION = new Version('5.0.1-28985cb');
 
 /**
  * @fileoverview added by tsickle
