a25c2b917bba4c74951ca48c0ef662469882f3ff
Jason Aden <jasonaden@google.com>
2017-11-14 22:07:34 +0000

    91efc7f docs(core): change from deprecated renderer to renderer2 (#19920)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-7acbc19
+ * @license Angular v5.0.1-91efc7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.1-7acbc19');
+const VERSION = new Version('5.0.1-91efc7f');
 
 /**
  * @fileoverview added by tsickle
