775ddf273489d1acefd98f8c5c66dbd428c875ea
Miško Hevery <misko@hevery.com>
2017-11-16 04:50:35 +0000

    1285792 fix(aio): fix window title on Home page (#20440)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7943,7 +7943,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-2d6126e');
+var VERSION = new Version('5.0.1-1285792');
 
 /**
  * @fileoverview added by tsickle
