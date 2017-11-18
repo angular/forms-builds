2248bd03cc39d090e070fb9b0ef4dfc132409e89
Miško Hevery <misko@hevery.com>
2017-11-18 00:42:28 +0000

    82aace6 fix(core): should support event.stopImmediatePropagation (#20469)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-b1f8eb1
+ * @license Angular v5.0.2-82aace6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5781,7 +5781,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.2-b1f8eb1');
+const VERSION = new Version('5.0.2-82aace6');
 
 /**
  * @fileoverview added by tsickle
