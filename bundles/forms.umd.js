62d884547674f0137ec3a61c68b24cca6bfa97bc
Miško Hevery <misko@hevery.com>
2017-11-27 20:03:14 +0000

    a53a040 build(aio): prevent comments in code from leaking into doc-gen code snippets (#20607)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8008,7 +8008,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-330bb2a');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-a53a040');
 
 /**
  * @fileoverview added by tsickle
