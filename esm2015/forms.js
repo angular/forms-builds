b4e8e63e12c5255c1f284535bad21fa0b49516cd
Miško Hevery <misko@hevery.com>
2017-11-29 22:34:15 +0000

    ba850b3 Revert "fix(core): should use native addEventListener in ngZone (#20672)"

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-65a2cb8
+ * @license Angular v5.1.0-beta.2-ba850b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5808,7 +5808,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-65a2cb8');
+const VERSION = new Version('5.1.0-beta.2-ba850b3');
 
 /**
  * @fileoverview added by tsickle
