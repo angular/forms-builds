637623cf279e0d598b81cd6693044252b15bd723
Misko Hevery <misko@hevery.com>
2017-11-30 04:47:11 +0000

    7a2550a styl: broken build due to missing new lines

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-b9e4d62
+ * @license Angular v5.1.0-beta.2-7a2550a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5808,7 +5808,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-b9e4d62');
+const VERSION = new Version('5.1.0-beta.2-7a2550a');
 
 /**
  * @fileoverview added by tsickle
