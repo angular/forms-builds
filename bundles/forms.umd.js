3c93a4080cd7052a5dfdbf0fdd162ac298427697
Misko Hevery <misko@hevery.com>
2017-12-01 05:55:27 +0000

    cbd93fe docs: add changelog for 5.0.4

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-e95b5d7
+ * @license Angular v5.0.4-cbd93fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.3-e95b5d7
+ * @license Angular v5.0.4-cbd93fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7987,7 +7987,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.3-e95b5d7');
+var VERSION = new _angular_core.Version('5.0.4-cbd93fe');
 
 /**
  * @fileoverview added by tsickle
