3dfdc4957162b1854f366eebad995c6026fe74f2
Miško Hevery <misko@hevery.com>
2017-12-01 19:49:28 +0000

    445d833 fix(build): accidental character in commit-message.json

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.4-cbd93fe
+ * @license Angular v5.0.4-445d833
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.4-cbd93fe
+ * @license Angular v5.0.4-445d833
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7987,7 +7987,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.4-cbd93fe');
+var VERSION = new _angular_core.Version('5.0.4-445d833');
 
 /**
  * @fileoverview added by tsickle
