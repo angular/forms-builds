e2681822de32fd3c56b1474b58c459a706f89c34
Igor Minar <igor@angularjs.org>
2017-12-04 23:15:54 +0000

    7e7ff2e ci: fix the payload-size checking scripts (#20683)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7970,7 +7970,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-d507057');
+var VERSION = new Version('5.1.0-rc.1-7e7ff2e');
 
 /**
  * @fileoverview added by tsickle
