0393915e9023e8b52186162ec3381912db3cbfe4
Igor Minar <igor@angular.io>
2017-12-06 20:59:37 +0000

    c2dbc55 release: cut the 5.1.0 release

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-b78ada1
+ * @license Angular v5.1.0-c2dbc55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-rc.1-b78ada1');
+const VERSION = new Version('5.1.0-c2dbc55');
 
 /**
  * @fileoverview added by tsickle
