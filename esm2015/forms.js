45f75ac2fd81e313be8b82598ec99eadd641cab6
Igor Minar <igor@angularjs.org>
2017-12-06 16:44:58 +0000

    b78ada1 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-rc.1-be99449');
+const VERSION = new Version('5.1.0-rc.1-b78ada1');
 
 /**
  * @fileoverview added by tsickle
