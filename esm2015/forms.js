312e124aa516f8f5b4f006c39664bf663e5c84da
Jason Aden <jasonaden@google.com>
2017-12-07 21:53:23 +0000

    ef534c0 build: upgrade bazel rules to latest (#20768)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-073f485
+ * @license Angular v5.1.0-ef534c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-073f485');
+const VERSION = new Version('5.1.0-ef534c0');
 
 /**
  * @fileoverview added by tsickle
