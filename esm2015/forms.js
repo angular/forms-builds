8a9b8678c5035c38df09757d7e0d4f7a51185ac4
Jason Aden <jasonaden@google.com>
2017-12-08 20:31:58 +0000

    d713225 refactor(common): update i18n locale data to CLDR v32 (#20830)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-affa54d
+ * @license Angular v5.1.0-d713225
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-affa54d');
+const VERSION = new Version('5.1.0-d713225');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-affa54d
+ * @license Angular v5.1.0-d713225
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7984,7 +7984,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-affa54d');
+var VERSION = new Version('5.1.0-d713225');
 
 /**
  * @fileoverview added by tsickle
