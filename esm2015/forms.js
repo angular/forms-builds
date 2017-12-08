3ea2cc8f7d4daacc6b88eb07f411eafe1f68fe34
Jason Aden <jasonaden@google.com>
2017-12-08 23:20:28 +0000

    13e663c fix(animations): ensure multi-level route leave animations are queryable (#20787)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-3ce3b4d
+ * @license Angular v5.1.0-13e663c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-3ce3b4d');
+const VERSION = new Version('5.1.0-13e663c');
 
 /**
  * @fileoverview added by tsickle
