2c450ff1059e7023a1c70f22987b6519b099971b
Jason Aden <jasonaden@google.com>
2017-12-08 01:29:10 +0000

    baeec4d fix(router): NavigatonError and NavigationCancel should be emitted after resetting the URL (#20803)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-9dd60a5
+ * @license Angular v5.1.0-baeec4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-9dd60a5
+ * @license Angular v5.1.0-baeec4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8022,7 +8022,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-9dd60a5');
+var VERSION = new _angular_core.Version('5.1.0-baeec4d');
 
 /**
  * @fileoverview added by tsickle
