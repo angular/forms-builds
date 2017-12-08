017d55e70993b4fbe04d579e1d076723eb6a4ed9
Jason Aden <jasonaden@google.com>
2017-12-08 18:25:19 +0000

    53b9de3 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-e6a2805
+ * @license Angular v5.1.0-53b9de3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7984,7 +7984,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-e6a2805');
+var VERSION = new Version('5.1.0-53b9de3');
 
 /**
  * @fileoverview added by tsickle
