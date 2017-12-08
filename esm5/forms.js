680521bca71cf8e2d8528b1362729019548077c3
Jason Aden <jasonaden@google.com>
2017-12-08 18:35:42 +0000

    affa54d build(aio): upgrade to latest `@angular/cli` (#18428)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-53b9de3
+ * @license Angular v5.1.0-affa54d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7984,7 +7984,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-53b9de3');
+var VERSION = new Version('5.1.0-affa54d');
 
 /**
  * @fileoverview added by tsickle
