5b2005ee8a729e37b9f659e5cb7f80dfeaf209d0
Jason Aden <jasonaden@google.com>
2017-12-13 19:55:53 +0000

    e3140ae docs: add changelog for 5.1.1

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d6da798
+ * @license Angular v5.1.1-e3140ae
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7984,7 +7984,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-d6da798');
+var VERSION = new Version('5.1.1-e3140ae');
 
 /**
  * @fileoverview added by tsickle
