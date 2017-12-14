f954a88c8ff8eb7877209d239571cf50e6006240
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-14 16:57:20 +0000

    2686219 ci: sync CircleCI config from master to 5.0.x

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.5-eb57bc3
+ * @license Angular v5.0.5-2686219
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8001,7 +8001,7 @@ var FormBuilder = (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.5-eb57bc3');
+var VERSION = new _angular_core.Version('5.0.5-2686219');
 
 /**
  * @fileoverview added by tsickle
