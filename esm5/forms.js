646819f2470661148b2e6fbeb2ccede2c73df1e2
Igor Minar <igor@angularjs.org>
2017-12-21 21:40:32 +0000

    dc53248 build: update pullapprove:

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-d1f4500
+ * @license Angular v5.1.2-dc53248
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7984,7 +7984,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-d1f4500');
+var VERSION = new Version('5.1.2-dc53248');
 
 /**
  * @fileoverview added by tsickle
