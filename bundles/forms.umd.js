9f2c036d0e031e70cae7d8c1e98fb930c63d90b1
Igor Minar <igor@angularjs.org>
2017-12-22 04:30:26 +0000

    41abcc3 docs(service-worker): fix word wrap (#21114)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-4337768
+ * @license Angular v5.1.2-41abcc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-4337768
+ * @license Angular v5.1.2-41abcc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8022,7 +8022,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-4337768');
+var VERSION = new _angular_core.Version('5.1.2-41abcc3');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-4337768
+ * @license Angular v5.1.2-41abcc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-4337768');
+const VERSION = new Version('5.1.2-41abcc3');
 
 /**
  * @fileoverview added by tsickle
