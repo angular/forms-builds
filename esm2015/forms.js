1b0eb6991aca86a7c29dbb21aa28a1769144e16d
Igor Minar <igor@angularjs.org>
2017-12-22 04:32:28 +0000

    0aa6341 build: make umd.min.js source map paths relative (#21147)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-41abcc3
+ * @license Angular v5.1.2-0aa6341
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-41abcc3');
+const VERSION = new Version('5.1.2-0aa6341');
 
 /**
  * @fileoverview added by tsickle
