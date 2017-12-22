aac368fa9d84ae023c2d4780c35810a89eabc5d4
Igor Minar <igor@angularjs.org>
2017-12-22 17:38:42 +0000

    8c99175 fix(router): fix wildcard route with lazy loaded module (again) (#18139)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-fa0e8ef
+ * @license Angular v5.1.2-8c99175
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7984,7 +7984,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-fa0e8ef');
+var VERSION = new Version('5.1.2-8c99175');
 
 /**
  * @fileoverview added by tsickle
