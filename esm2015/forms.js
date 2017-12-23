9e562f2859c058abb9e0adc14b0af9ad82d08f13
Igor Minar <igor@angular.io>
2017-12-23 05:54:56 +0000

    d787b55 build(common): generate ts declarations for i18n locale files (#21127)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-3e34fa8
+ * @license Angular v5.1.2-d787b55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5822,7 +5822,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-3e34fa8');
+const VERSION = new Version('5.1.2-d787b55');
 
 /**
  * @fileoverview added by tsickle
