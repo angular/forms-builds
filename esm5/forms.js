7c93d61f1209db59bbda7780d78d553cab46a6b8
Miško Hevery <misko@hevery.com>
2018-01-02 22:25:34 +0000

    8006be8 docs(aio): fix trailing space breaking lint

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-48a1f32
+ * @license Angular v5.1.2-8006be8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7996,7 +7996,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-48a1f32');
+var VERSION = new Version('5.1.2-8006be8');
 
 /**
  * @fileoverview added by tsickle
