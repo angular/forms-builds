fa70de93a17de87ef4b497ced2d00b216594ee1a
Kara Erickson <karakara@google.com>
2018-01-04 23:53:21 +0000

    03d16fc docs(aio): add missing imports to interceptor example (#21259)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v5.2.0-rc.0-b5934fc
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v5.2.0-rc.0-03d16fc
+ * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 import { Directive, ElementRef, EventEmitter, Host, Inject, Injectable, InjectionToken, Injector, Input, NgModule, Optional, Output, Renderer2, Self, SkipSelf, Version, forwardRef, ɵisObservable, ɵisPromise, ɵlooseIdentical } from '@angular/core';
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-b5934fc');
+const VERSION = new Version('5.2.0-rc.0-03d16fc');
 
 /**
  * @fileoverview added by tsickle
