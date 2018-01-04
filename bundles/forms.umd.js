06ea8917c5f8295ad686a3e78a7a246d46e0ea82
Kara Erickson <karakara@google.com>
2018-01-04 22:39:17 +0000

    cc0b0a4 fix(aio): do not redirect pages on "next" deployment (#21027)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b9d8ac5
+ * @license Angular v5.1.3-cc0b0a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-b9d8ac5
+ * @license Angular v5.1.3-cc0b0a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-b9d8ac5');
+var VERSION = new _angular_core.Version('5.1.3-cc0b0a4');
 
 /**
  * @fileoverview added by tsickle
