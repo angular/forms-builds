9de748841d2b0e71c1d30878385817112dea0880
Kara Erickson <karakara@google.com>
2018-01-04 23:00:20 +0000

    dcc3eba fix(aio): fix test message's mistake. (#21254)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-6588ca8
+ * @license Angular v5.2.0-rc.0-dcc3eba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-6588ca8');
+const VERSION = new Version('5.2.0-rc.0-dcc3eba');
 
 /**
  * @fileoverview added by tsickle
