f027795dc3abe78c7519eda46f1a6cdc04045b27
Kara Erickson <karakara@google.com>
2018-01-04 19:46:13 +0000

    b9d8ac5 fix(aio): don't use Object.keys on NamedNodeMap to prevent SEO errors (#21305)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-fc217f5
+ * @license Angular v5.1.3-b9d8ac5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7996,7 +7996,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-fc217f5');
+var VERSION = new Version('5.1.3-b9d8ac5');
 
 /**
  * @fileoverview added by tsickle
