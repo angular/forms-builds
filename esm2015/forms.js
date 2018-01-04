cf57111345f818fcf8446f3a7bd143129f6ef7d3
Kara Erickson <karakara@google.com>
2018-01-04 22:35:47 +0000

    81823e8 build(aio): fix example protractor config for Travis (#21309)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ef78538
+ * @license Angular v5.2.0-rc.0-81823e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-ef78538');
+const VERSION = new Version('5.2.0-rc.0-81823e8');
 
 /**
  * @fileoverview added by tsickle
