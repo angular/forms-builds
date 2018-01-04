87cadfc018b704e27f8a18474e67bb28b18b7ecc
Kara Erickson <karakara@google.com>
2018-01-04 23:00:21 +0000

    d79a0ba docs(aio): add missing imports to interceptor example (#21259)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-b7307c9');
+var VERSION = new _angular_core.Version('5.1.3-d79a0ba');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-b7307c9
+ * @license Angular v5.1.3-d79a0ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7996,7 +7996,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-b7307c9');
+var VERSION = new Version('5.1.3-d79a0ba');
 
 /**
  * @fileoverview added by tsickle
