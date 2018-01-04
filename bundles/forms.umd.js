02cb19caa17fcb98bd5f18c7a10c37713d80c708
Misko Hevery <misko@hevery.com>
2018-01-04 00:21:19 +0000

    d138b38 docs: add changelog for 5.1.3

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.2-00308fc
+ * @license Angular v5.1.3-d138b38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-00308fc');
+var VERSION = new _angular_core.Version('5.1.3-d138b38');
 
 /**
  * @fileoverview added by tsickle
