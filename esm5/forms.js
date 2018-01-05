fd2fa736c675790261f16477748ef2868056478b
Kara Erickson <karakara@google.com>
2018-01-05 19:53:52 +0000

    c809545 test(aio): wait for Angular to avoid flake in e2e test (#21047)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-d3c0425
+ * @license Angular v5.1.3-c809545
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7996,7 +7996,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-d3c0425');
+var VERSION = new Version('5.1.3-c809545');
 
 /**
  * @fileoverview added by tsickle
