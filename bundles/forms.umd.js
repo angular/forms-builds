230ed50c51d1c9ce1eaf23956c9a3c26bb756ec4
Kara Erickson <karakara@google.com>
2018-01-05 21:42:38 +0000

    e2cc4db docs(aio): delete duplicated word (#21129)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-52cb79c
+ * @license Angular v5.1.3-e2cc4db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-52cb79c
+ * @license Angular v5.1.3-e2cc4db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-52cb79c');
+var VERSION = new _angular_core.Version('5.1.3-e2cc4db');
 
 /**
  * @fileoverview added by tsickle
