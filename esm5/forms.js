866696a33ad9c762e3dca7be8f6971579d31bc6d
Kara Erickson <karakara@google.com>
2018-01-08 20:25:45 +0000

    74dbf7b build(core): reduce payload limit back to normal (#21394)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-7269fe5
+ * @license Angular v5.2.0-rc.0-74dbf7b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7991,7 +7991,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-rc.0-7269fe5');
+var VERSION = new Version('5.2.0-rc.0-74dbf7b');
 
 /**
  * @fileoverview added by tsickle
