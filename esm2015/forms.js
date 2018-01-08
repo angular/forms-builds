c93dcada4e8aec95e35bbd871cbbb70768b60254
Kara Erickson <karakara@google.com>
2018-01-08 18:57:28 +0000

    e994752 docs(aio): document the template compiler options (#21333)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-b15d50a
+ * @license Angular v5.2.0-rc.0-e994752
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-b15d50a');
+const VERSION = new Version('5.2.0-rc.0-e994752');
 
 /**
  * @fileoverview added by tsickle
