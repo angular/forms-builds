574d8dff9bc1e7d0b9ec706bff492f3c33054418
Kara Erickson <karakara@google.com>
2018-01-08 21:29:30 +0000

    6040ee3 fix(benchpress): forward compat with selenium_webdriver 3.6.0 (#21399)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-74dbf7b
+ * @license Angular v5.2.0-rc.0-6040ee3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-74dbf7b');
+const VERSION = new Version('5.2.0-rc.0-6040ee3');
 
 /**
  * @fileoverview added by tsickle
