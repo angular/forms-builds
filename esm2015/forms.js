b900586a7f1d4586805b42861c5366cb0038ea41
Kara Erickson <karakara@google.com>
2018-01-09 22:36:59 +0000

    3db91ff feat(core): add ngAfterViewInit and ngAfterViewChecked support to render3 (#21266)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-229b76c
+ * @license Angular v5.2.0-rc.0-3db91ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-229b76c');
+const VERSION = new Version('5.2.0-rc.0-3db91ff');
 
 /**
  * @fileoverview added by tsickle
