a7080ee07e1c8b941d57cae53b2dc1cbe0e151c9
Kara Erickson <karakara@google.com>
2018-01-09 18:50:27 +0000

    39f81b2 build: upgrade Bazel to 0.9.0 (#21335)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-0602aaa
+ * @license Angular v5.1.3-39f81b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7996,7 +7996,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-0602aaa');
+var VERSION = new Version('5.1.3-39f81b2');
 
 /**
  * @fileoverview added by tsickle
