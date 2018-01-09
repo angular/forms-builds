dd239e740c87965320abca3a0860daf8ccb37c1b
Kara Erickson <karakara@google.com>
2018-01-09 00:36:58 +0000

    0602aaa fix(benchpress): work around missing events from Chrome 63 (#21396)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-a73b0b6
+ * @license Angular v5.1.3-0602aaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.3-a73b0b6
+ * @license Angular v5.1.3-0602aaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-a73b0b6');
+var VERSION = new _angular_core.Version('5.1.3-0602aaa');
 
 /**
  * @fileoverview added by tsickle
