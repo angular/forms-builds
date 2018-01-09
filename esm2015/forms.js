715aca020e8b41e1c8d311076d58eebd73a6f827
Kara Erickson <karakara@google.com>
2018-01-09 19:13:59 +0000

    9728dce fix(bazel): Give correct module names for ES6 output (#21320)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-463e287
+ * @license Angular v5.2.0-rc.0-9728dce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-463e287');
+const VERSION = new Version('5.2.0-rc.0-9728dce');
 
 /**
  * @fileoverview added by tsickle
