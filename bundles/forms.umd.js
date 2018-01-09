0a644ff240ec93892031cb804f2dcc8434a5e518
Kara Erickson <karakara@google.com>
2018-01-09 00:32:50 +0000

    27196b6 docs(aio): fix punctuation for clarity (#21325) PR Close #21325

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-6040ee3
+ * @license Angular v5.2.0-rc.0-27196b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-6040ee3
+ * @license Angular v5.2.0-rc.0-27196b6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-rc.0-6040ee3');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-27196b6');
 
 /**
  * @fileoverview added by tsickle
