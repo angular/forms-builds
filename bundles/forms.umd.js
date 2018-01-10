d9596d67b62c9b320029ff385c495b9b00ab11b6
Alex Eagle <alexeagle@google.com>
2018-01-10 20:47:32 +0000

    76c781f docs(router): remove extra brackets (#21294)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-51eb3d4
+ * @license Angular v5.2.0-76c781f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-51eb3d4
+ * @license Angular v5.2.0-76c781f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8029,7 +8029,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-51eb3d4');
+var VERSION = new _angular_core.Version('5.2.0-76c781f');
 
 /**
  * @fileoverview added by tsickle
