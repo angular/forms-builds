0db46cfe2ab79a6555787ee949c22b35c4ae7bed
Alex Eagle <alexeagle@google.com>
2018-01-10 23:40:12 +0000

    aad1126 docs: update the release schedule with v6 info (#21435)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-7b463df
+ * @license Angular v5.2.0-aad1126
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-7b463df');
+const VERSION = new Version('5.2.0-aad1126');
 
 /**
  * @fileoverview added by tsickle
