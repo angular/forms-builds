c7dedf15c96bb486f71b378ab6e9e74eb3212788
Alex Eagle <alexeagle@google.com>
2018-01-10 21:28:19 +0000

    5ede67c build: move repeated tsconfig attributes to a macro (#20964)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-31b671a
+ * @license Angular v5.2.0-5ede67c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-31b671a');
+const VERSION = new Version('5.2.0-5ede67c');
 
 /**
  * @fileoverview added by tsickle
