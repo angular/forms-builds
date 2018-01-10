e8bf134607a6b61b263b8f1c661e42508afbb15a
Alex Eagle <alexeagle@google.com>
2018-01-10 20:38:03 +0000

    72246d4 ci: update forms approval list to include guides (#21449)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d2aa8ac
+ * @license Angular v5.2.0-72246d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-d2aa8ac');
+const VERSION = new Version('5.2.0-72246d4');
 
 /**
  * @fileoverview added by tsickle
