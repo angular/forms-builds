921712133b5fd09b8fa0270cd4ea1aa466796a20
Alex Eagle <alexeagle@google.com>
2018-01-10 21:11:58 +0000

    f15ca6c fix(aio): support multibyte character in heading (#21414)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0de219a
+ * @license Angular v5.2.0-f15ca6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-0de219a');
+const VERSION = new Version('5.2.0-f15ca6c');
 
 /**
  * @fileoverview added by tsickle
