5a6e809e5c132dbe678838d908447d6b5887ddfa
Alex Eagle <alexeagle@google.com>
2018-01-10 20:25:30 +0000

    d2aa8ac fix(animations): fix increment/decrement aliases example (#18323)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-ef956a2
+ * @license Angular v5.2.0-d2aa8ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7991,7 +7991,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-ef956a2');
+var VERSION = new Version('5.2.0-d2aa8ac');
 
 /**
  * @fileoverview added by tsickle
