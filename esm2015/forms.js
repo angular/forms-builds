badb01de46a800a7b249b123ce34b3d5dcabfdd7
Alex Eagle <alexeagle@google.com>
2018-01-11 15:21:19 +0000

    5eaaac3 refactor(ivy): remove `type` from `DirectiveDef` (#21374)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c0080d7
+ * @license Angular v5.2.0-5eaaac3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-c0080d7');
+const VERSION = new Version('5.2.0-5eaaac3');
 
 /**
  * @fileoverview added by tsickle
