0233cacac77fb97449f54a8770008921b0c4122d
Alex Eagle <alexeagle@google.com>
2018-01-11 22:16:55 +0000

    6af3672 fix(ivy): Add workaround for AJD in google3 (#21488)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-9b84a32
+ * @license Angular v5.2.0-6af3672
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-9b84a32');
+const VERSION = new Version('5.2.0-6af3672');
 
 /**
  * @fileoverview added by tsickle
