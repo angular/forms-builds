8fa343bac0337643f5c8b7c1f6b6e817b15052b3
Alex Eagle <alexeagle@google.com>
2018-01-11 00:41:37 +0000

    e64b1e9 fix(compiler): make `.ngsummary.json` files idempotent (#21448)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-6be9c04
+ * @license Angular v5.2.0-e64b1e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7991,7 +7991,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-6be9c04');
+var VERSION = new Version('5.2.0-e64b1e9');
 
 /**
  * @fileoverview added by tsickle
