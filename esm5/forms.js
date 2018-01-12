d4de184eedaffa7af8b5488b9b4a44d540aaa7c6
Alex Eagle <alexeagle@google.com>
2018-01-12 19:06:08 +0000

    9b5a485 perf(ivy): add missing dom element in render3_function tree benchmark (#21476)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-50ca01e
+ * @license Angular v5.2.0-9b5a485
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7991,7 +7991,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-50ca01e');
+var VERSION = new Version('5.2.0-9b5a485');
 
 /**
  * @fileoverview added by tsickle
