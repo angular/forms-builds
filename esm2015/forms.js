82cf9d53cdfb14bb148257e7fbd7b8cb303ec12d
Alex Eagle <alexeagle@google.com>
2018-01-12 22:55:42 +0000

    c5c6d84 style(aio): enforce strict TypeScript checks (#21342)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-246de65
+ * @license Angular v5.2.0-c5c6d84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-246de65');
+const VERSION = new Version('5.2.0-c5c6d84');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-246de65
+ * @license Angular v5.2.0-c5c6d84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7991,7 +7991,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-246de65');
+var VERSION = new Version('5.2.0-c5c6d84');
 
 /**
  * @fileoverview added by tsickle
