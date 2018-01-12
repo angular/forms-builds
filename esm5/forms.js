8eb2eb22cb2a7724ed21f43e1705d68d2007592b
Alex Eagle <alexeagle@google.com>
2018-01-12 21:54:36 +0000

    05208b8 fix(common): set correct timezone for ISO8601 dates in Safari (#21506)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-bb62458
+ * @license Angular v5.2.0-05208b8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7991,7 +7991,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-bb62458');
+var VERSION = new Version('5.2.0-05208b8');
 
 /**
  * @fileoverview added by tsickle
