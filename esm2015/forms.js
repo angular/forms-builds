481dbb89424040ef49eabd14620465e97da1393b
Alex Eagle <alexeagle@google.com>
2018-01-12 22:20:11 +0000

    977978e test(common): make date pipe tests work in more timezones (#21379)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-eb70966
+ * @license Angular v5.2.0-977978e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5821,7 +5821,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-eb70966');
+const VERSION = new Version('5.2.0-977978e');
 
 /**
  * @fileoverview added by tsickle
