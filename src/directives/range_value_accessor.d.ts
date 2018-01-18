2e1c6e69ef3f0af39f896ce6879380edf8807f97
GitHub <noreply@github.com>
2018-01-18 20:31:52 +0000

    2717a3e ci: add "PR action: cleanup" to the bot's forbiddenLabels list

--- src/directives/range_value_accessor.d.ts
+++ src/directives/range_value_accessor.d.ts
@@ -5,9 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { ElementRef, Provider, Renderer } from '@angular/core';
+import { ElementRef, Renderer2, StaticProvider } from '@angular/core';
 import { ControlValueAccessor } from './control_value_accessor';
-export declare const RANGE_VALUE_ACCESSOR: Provider;
+export declare const RANGE_VALUE_ACCESSOR: StaticProvider;
 /**
  * The accessor for writing a range value and listening to changes that is used by the
  * {@link NgModel}, {@link FormControlDirective}, and {@link FormControlName} directives.
@@ -22,7 +22,7 @@ export declare class RangeValueAccessor implements ControlValueAccessor {
     private _elementRef;
     onChange: (_: any) => void;
     onTouched: () => void;
-    constructor(_renderer: Renderer, _elementRef: ElementRef);
+    constructor(_renderer: Renderer2, _elementRef: ElementRef);
     writeValue(value: any): void;
     registerOnChange(fn: (_: number | null) => void): void;
     registerOnTouched(fn: () => void): void;
