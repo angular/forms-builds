8e3ef95cdc3e584355ed9aaf9afe09bb8b65c16f
Miško Hevery <misko@hevery.com>
2018-01-23 18:43:58 +0000

    21f7669 refactor(bazel): pass around tsconfig as a file, not a path (#21614)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-4b68fdc
+ * @license Angular v5.2.1-21f7669
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.1-4b68fdc
+ * @license Angular v5.2.1-21f7669
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-4b68fdc');
+var VERSION = new _angular_core.Version('5.2.1-21f7669');
 
 /**
  * @fileoverview added by tsickle
