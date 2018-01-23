7b941381a6ae3cd4fe763b501ebddf6a4c1c3b08
Miško Hevery <misko@hevery.com>
2018-01-23 21:54:45 +0000

    52970c0 fix(compiler-cli): do not fold errors past calls in the collector (#21708)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-21f7669
+ * @license Angular v5.2.1-52970c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.1-21f7669
+ * @license Angular v5.2.1-52970c0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-21f7669');
+var VERSION = new _angular_core.Version('5.2.1-52970c0');
 
 /**
  * @fileoverview added by tsickle
