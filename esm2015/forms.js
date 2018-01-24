d379c4d789a8e90b994aecb8452ceebc176f07ed
Miško Hevery <misko@hevery.com>
2018-01-24 01:03:41 +0000

    acf381b docs: update ICU select messages to use male/female (#21713)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-8baff18
+ * @license Angular v5.9.9-6-beta.0-acf381b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5826,7 +5826,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.9.9-6-beta.0-8baff18');
+const VERSION = new Version('5.9.9-6-beta.0-acf381b');
 
 /**
  * @fileoverview added by tsickle
