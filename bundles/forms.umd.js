9f47337a8270543701324e3b02d7d2a75d412611
Miško Hevery <misko@hevery.com>
2018-01-24 05:58:46 +0000

    925e654 fix(router): don't use ParamsInheritanceStrategy in declarations (#21574)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-1e9cd95
+ * @license Angular v5.9.9-6-beta.0-925e654
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.9.9-6-beta.0-1e9cd95
+ * @license Angular v5.9.9-6-beta.0-925e654
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.9.9-6-beta.0-1e9cd95');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-925e654');
 
 /**
  * @fileoverview added by tsickle
