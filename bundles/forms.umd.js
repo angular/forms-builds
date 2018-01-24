5850afcedacb161a5b67c5186fc1d81b14079101
Miško Hevery <misko@hevery.com>
2018-01-24 21:04:34 +0000

    5423882 build: merge PR to all branches per target: label (#21739)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-de6c644
+ * @license Angular v5.2.1-5423882
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.1-de6c644
+ * @license Angular v5.2.1-5423882
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-de6c644');
+var VERSION = new _angular_core.Version('5.2.1-5423882');
 
 /**
  * @fileoverview added by tsickle
