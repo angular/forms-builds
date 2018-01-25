df2324467affd5ad613ceda38ca11fb9f28ef0be
Misko Hevery <misko@hevery.com>
2018-01-25 06:42:53 +0000

    5269ce2 feat(ivy): support deep queries through view boundaries (#21700)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-3e03dbe
+ * @license Angular v6.0.0-beta.1-5269ce2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5826,7 +5826,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-3e03dbe');
+const VERSION = new Version('6.0.0-beta.1-5269ce2');
 
 /**
  * @fileoverview added by tsickle
