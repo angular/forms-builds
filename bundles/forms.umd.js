b7fb6734796920cba74efb9720c5048b2efbc5f0
Miško Hevery <misko@hevery.com>
2018-01-26 19:02:10 +0000

    35a0721 fix(router): remove @internal tag on ParamInheritanceType (#21773)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-67806a7
+ * @license Angular v5.2.2-35a0721
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-67806a7
+ * @license Angular v5.2.2-35a0721
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.2-67806a7');
+var VERSION = new _angular_core.Version('5.2.2-35a0721');
 
 /**
  * @fileoverview added by tsickle
