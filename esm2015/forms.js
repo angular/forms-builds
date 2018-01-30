265ccc2512d12983888727d742b7f625a64d74f9
Jason Aden <jasonaden@google.com>
2018-01-30 21:42:34 +0000

    407b5cf refactor(ivy): re-introduce QueryPredicate with more focused role (#21857) (#21857)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-285dd6b
+ * @license Angular v6.0.0-beta.1-407b5cf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-285dd6b');
+const VERSION = new Version('6.0.0-beta.1-407b5cf');
 
 /**
  * @fileoverview added by tsickle
