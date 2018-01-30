8dffc338682d42a5a6290af74dfce30626066539
Jason Aden <jasonaden@google.com>
2018-01-30 00:41:26 +0000

    120bdee fix(common): allow HttpInterceptors to inject HttpClient (#19809)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-8dff9d8
+ * @license Angular v6.0.0-beta.1-120bdee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-8dff9d8');
+var VERSION = new Version('6.0.0-beta.1-120bdee');
 
 /**
  * @fileoverview added by tsickle
