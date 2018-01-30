f83122a2acd526267ca4a2b36162ca60dfef6009
Jason Aden <jasonaden@google.com>
2018-01-30 20:21:55 +0000

    285dd6b feat(ivy): observable QueryList (#21859)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-b10540a
+ * @license Angular v6.0.0-beta.1-285dd6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-b10540a');
+var VERSION = new Version('6.0.0-beta.1-285dd6b');
 
 /**
  * @fileoverview added by tsickle
