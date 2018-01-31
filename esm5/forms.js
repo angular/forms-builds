7dcef9b6d1fa7d3b2eeb154c798577ce18d7e6e9
Jason Aden <jasonaden@google.com>
2018-01-31 19:01:17 +0000

    ccd0298 ci(ivy): enable size tracking of a minimal cli render3 application (#21792)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-eeab433
+ * @license Angular v6.0.0-beta.1-ccd0298
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.1-eeab433');
+var VERSION = new Version('6.0.0-beta.1-ccd0298');
 
 /**
  * @fileoverview added by tsickle
