aa5fd817e74af88204905561dd3c1707e60b1dae
Jason Aden <jasonaden@google.com>
2018-01-31 19:19:20 +0000

    2aa460b docs: add http guide sample and adjust text (#21326)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-e0022ae
+ * @license Angular v5.2.2-2aa460b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-e0022ae');
+var VERSION = new Version('5.2.2-2aa460b');
 
 /**
  * @fileoverview added by tsickle
