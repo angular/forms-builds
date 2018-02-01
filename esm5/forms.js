9cf5fe4c3e17473fa1d4c3a25cee5ef48a3b111c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-01 17:06:11 +0000

    65cf1ad fix(ivy): remove unnecessary parameter of NgOnChangesFeature (#21879)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-8b14488
+ * @license Angular v6.0.0-beta.2-65cf1ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-8b14488');
+var VERSION = new Version('6.0.0-beta.2-65cf1ad');
 
 /**
  * @fileoverview added by tsickle
