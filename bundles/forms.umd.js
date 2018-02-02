e6b2708709ea28280a43059ac7f6871a0164e796
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:12:27 +0000

    3cc1d76 fix(ivy): generate correct interpolations (#21946)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-65cf1ad
+ * @license Angular v6.0.0-beta.2-3cc1d76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.2-65cf1ad
+ * @license Angular v6.0.0-beta.2-3cc1d76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8028,7 +8028,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.2-65cf1ad');
+var VERSION = new _angular_core.Version('6.0.0-beta.2-3cc1d76');
 
 /**
  * @fileoverview added by tsickle
