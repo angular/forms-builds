9f57a916f9a7c8976c3303be6432593aca8b2484
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:11:08 +0000

    965eecc build(aio): move zip and live-example generation to `yarn predocs` task (#21970)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8028,7 +8028,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-c4fb696');
+var VERSION = new _angular_core.Version('5.2.3-965eecc');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-c4fb696
+ * @license Angular v5.2.3-965eecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-c4fb696');
+var VERSION = new Version('5.2.3-965eecc');
 
 /**
  * @fileoverview added by tsickle
