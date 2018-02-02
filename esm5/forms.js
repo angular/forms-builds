7a3b16b3fd6a23728c3563fe9d658c0206aa79ca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:24:38 +0000

    743d8bc feat(ivy): add canonical example of a pipe. (#21834)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-f816666
+ * @license Angular v6.0.0-beta.2-743d8bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-f816666');
+var VERSION = new Version('6.0.0-beta.2-743d8bc');
 
 /**
  * @fileoverview added by tsickle
