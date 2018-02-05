8159adff67336a254ff184b9057e452d5ac196dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:04:39 +0000

    adb1d62 docs: clarify npm/yarn commands, add blank lines to mix md/html in table (#21606)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-664f7fa
+ * @license Angular v5.2.3-adb1d62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.3-664f7fa');
+const VERSION = new Version('5.2.3-adb1d62');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-664f7fa
+ * @license Angular v5.2.3-adb1d62
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.3-664f7fa');
+var VERSION = new Version('5.2.3-adb1d62');
 
 /**
  * @fileoverview added by tsickle
