bd886b0e7db6af239347edc3c5580995a4fc24df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:46:21 +0000

    9a0700f build(aio): add API static members to search index (#21988)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-ae7bc22
+ * @license Angular v6.0.0-beta.2-9a0700f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.2-ae7bc22');
+const VERSION = new Version('6.0.0-beta.2-9a0700f');
 
 /**
  * @fileoverview added by tsickle
