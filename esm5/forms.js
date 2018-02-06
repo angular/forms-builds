7d1528565369f75584d2f1a217a97b07cc7b719d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:19:37 +0000

    e81606c fix(core): fix proper propagation of subscriptions in EventEmitter (#22016)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-f791e9f
+ * @license Angular v6.0.0-beta.2-e81606c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-f791e9f');
+var VERSION = new Version('6.0.0-beta.2-e81606c');
 
 /**
  * @fileoverview added by tsickle
