de7eedab8e7f624ae98701857ba5c4b187f2447b
Miško Hevery <misko@hevery.com>
2018-02-07 20:25:18 +0000

    44ea80b docs(forms): Custom Validator example selector name incorrect. (#20464)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-530b824
+ * @license Angular v5.2.4-44ea80b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-530b824');
+const VERSION = new Version('5.2.4-44ea80b');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-530b824
+ * @license Angular v5.2.4-44ea80b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-530b824');
+var VERSION = new Version('5.2.4-44ea80b');
 
 /**
  * @fileoverview added by tsickle
