2d76687d38f7696ac452cf2e5e8c0711ed1f0ca7
Miško Hevery <misko@hevery.com>
2018-02-07 21:25:38 +0000

    07769e5 test(common): disable deprecated date pipe tests on chrome mobile (#21933)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-9fdb804
+ * @license Angular v5.2.4-07769e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-9fdb804');
+var VERSION = new Version('5.2.4-07769e5');
 
 /**
  * @fileoverview added by tsickle
