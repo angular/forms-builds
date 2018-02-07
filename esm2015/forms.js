b2bfc32a449c98fe5dd3ee557cfa1b840e385074
Miško Hevery <misko@hevery.com>
2018-02-07 20:31:19 +0000

    b0ddb5a build(aio): blacklist unwanted URLs from the generated sitemap.xml (#22061)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-44ea80b
+ * @license Angular v5.2.4-b0ddb5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-44ea80b');
+const VERSION = new Version('5.2.4-b0ddb5a');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-44ea80b
+ * @license Angular v5.2.4-b0ddb5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-44ea80b');
+var VERSION = new Version('5.2.4-b0ddb5a');
 
 /**
  * @fileoverview added by tsickle
