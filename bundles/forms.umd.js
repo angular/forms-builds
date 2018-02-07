79a1cea138ef88ac954d7612730b807b62f51cec
Miško Hevery <misko@hevery.com>
2018-02-07 22:05:52 +0000

    b58c352 test(ivy): add canonical spec for object literals (#22045)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-efc67ee
+ * @license Angular v6.0.0-beta.3-b58c352
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.3-efc67ee
+ * @license Angular v6.0.0-beta.3-b58c352
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8034,7 +8034,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-efc67ee');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-b58c352');
 
 /**
  * @fileoverview added by tsickle
