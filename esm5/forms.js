415a34dc46f5f44d0c1e0ea1b84a12d95c954396
Miško Hevery <misko@hevery.com>
2018-02-09 21:56:14 +0000

    854f0ff feat(aio): enable data driven homepage announcements (#22043)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-d4a9db2
+ * @license Angular v5.2.4-854f0ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-d4a9db2');
+var VERSION = new Version('5.2.4-854f0ff');
 
 /**
  * @fileoverview added by tsickle
