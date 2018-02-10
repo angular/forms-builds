628764846fd56cf63b450a782b9bfc8ddccab78a
Miško Hevery <misko@hevery.com>
2018-02-10 01:36:30 +0000

    c878d55 docs: add VSCode interaction issue to bazel docs (#22128)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-263a2ec
+ * @license Angular v6.0.0-beta.3-c878d55
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8002,7 +8002,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-263a2ec');
+var VERSION = new Version('6.0.0-beta.3-c878d55');
 
 /**
  * @fileoverview added by tsickle
