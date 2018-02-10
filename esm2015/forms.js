5e80b7dc0e0e3278e49bf4318c62049b0a2182bb
Miško Hevery <misko@hevery.com>
2018-02-10 01:35:58 +0000

    9b280ee build: merge-pr new checks that all requested changes have been addressed (#21817)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ac815f7
+ * @license Angular v5.2.4-9b280ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-ac815f7');
+const VERSION = new Version('5.2.4-9b280ee');
 
 /**
  * @fileoverview added by tsickle
