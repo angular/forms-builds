bc3011c61ce32c3a1145da3ac52ac25caac83d7c
Miško Hevery <misko@hevery.com>
2018-02-12 18:57:15 +0000

    1e94846 docs(aio): add angular.schule to resources (#22164)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-88bec23
+ * @license Angular v6.0.0-beta.3-1e94846
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-88bec23');
+const VERSION = new Version('6.0.0-beta.3-1e94846');
 
 /**
  * @fileoverview added by tsickle
