2fa8417d8d276ef499f1cebec36d073cc6ef22cf
Miško Hevery <misko@hevery.com>
2018-02-12 18:24:50 +0000

    92a5876 refactor(router): move activation to private method (#22033)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-92d7060
+ * @license Angular v6.0.0-beta.3-92a5876
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-92d7060');
+const VERSION = new Version('6.0.0-beta.3-92a5876');
 
 /**
  * @fileoverview added by tsickle
