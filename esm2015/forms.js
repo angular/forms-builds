1014c8080ec70098653637d47dd4b2fc4ac183de
Miško Hevery <misko@hevery.com>
2018-02-12 19:38:05 +0000

    603e50d refactor(router): move activation to private method (#22144)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ca5b724
+ * @license Angular v5.2.4-603e50d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-ca5b724');
+const VERSION = new Version('5.2.4-603e50d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ca5b724
+ * @license Angular v5.2.4-603e50d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-ca5b724');
+var VERSION = new Version('5.2.4-603e50d');
 
 /**
  * @fileoverview added by tsickle
