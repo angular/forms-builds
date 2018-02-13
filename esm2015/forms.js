873f8733487654cbcc118eaffd4b9a6326b180bf
Miško Hevery <misko@hevery.com>
2018-02-13 21:59:11 +0000

    a589ca0 test(ivy): clean up canonical spec (#22188)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-72f8abd
+ * @license Angular v6.0.0-beta.3-a589ca0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.3-72f8abd');
+const VERSION = new Version('6.0.0-beta.3-a589ca0');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-72f8abd
+ * @license Angular v6.0.0-beta.3-a589ca0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8002,7 +8002,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.3-72f8abd');
+var VERSION = new Version('6.0.0-beta.3-a589ca0');
 
 /**
  * @fileoverview added by tsickle
