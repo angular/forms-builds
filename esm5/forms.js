06fc6141b6a1e2373d33e6dc14d3d1788018f043
Miško Hevery <misko@hevery.com>
2018-02-13 19:11:16 +0000

    e576b69 ci: update ngbot config file (#22173)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-f938774
+ * @license Angular v5.2.4-e576b69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-f938774');
+var VERSION = new Version('5.2.4-e576b69');
 
 /**
  * @fileoverview added by tsickle
