a463e49b5b967ffa69cc2d8da293c14d9a89bdc1
Matias Niemelä <matias@yearofmoo.com>
2018-02-14 22:48:35 +0000

    0b8b06e build: comment-out chromium version checking code temporarily (#22232)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-106b435
+ * @license Angular v5.2.5-0b8b06e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-106b435');
+var VERSION = new Version('5.2.5-0b8b06e');
 
 /**
  * @fileoverview added by tsickle
