b0b0d458d6db0e9223c00e434d491ce4eb7e7112
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 04:19:57 +0000

    831592c Revert "style: fix typos boostrap to bootstrap (#21917)"

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-b54ad05
+ * @license Angular v6.0.0-beta.4-831592c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-b54ad05');
+const VERSION = new Version('6.0.0-beta.4-831592c');
 
 /**
  * @fileoverview added by tsickle
