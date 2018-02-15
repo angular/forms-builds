ba039f2770d7cf7c7a5d8219aec5a883e31393d0
Victor Berchet <victor@suumit.com>
2018-02-15 21:36:44 +0000

    03d93c9 Revert: "build: merge-pr new checks that all requested changes have been addressed (#21817)"

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-0aa9b46
+ * @license Angular v6.0.0-beta.4-03d93c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-0aa9b46');
+const VERSION = new Version('6.0.0-beta.4-03d93c9');
 
 /**
  * @fileoverview added by tsickle
