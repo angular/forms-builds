61b2b7a344a752587cd53270616d7880f832cab0
Victor Berchet <victor@suumit.com>
2018-02-16 23:16:01 +0000

    185a6ab build: add esm5 build (#22258)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-6b45784
+ * @license Angular v5.2.5-185a6ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-6b45784');
+const VERSION = new Version('5.2.5-185a6ab');
 
 /**
  * @fileoverview added by tsickle
