0052496cdf848b6d3660e6a69ab9a871a313ee3f
Victor Berchet <victor@suumit.com>
2018-02-18 21:27:56 +0000

    f4845fa build: use authenticated mode for the merge script (#22269)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.4-f693be3
+ * @license Angular v6.0.0-beta.4-f4845fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8040,7 +8040,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-f693be3');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-f4845fa');
 
 /**
  * @fileoverview added by tsickle
