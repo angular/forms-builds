eae4bc26600e4862c2dd6075a144f9cbd7772cd5
Victor Berchet <victor@suumit.com>
2018-02-18 22:10:15 +0000

    9dae97c docs: correct grammar mistakes in CONTRIBUTING.md (#22285)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-228eb9f
+ * @license Angular v5.2.5-9dae97c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-228eb9f');
+var VERSION = new Version('5.2.5-9dae97c');
 
 /**
  * @fileoverview added by tsickle
