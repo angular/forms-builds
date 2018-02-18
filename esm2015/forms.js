eee9a5e77e99eeae92def953e58ed1fba359ea33
Victor Berchet <victor@suumit.com>
2018-02-18 21:55:08 +0000

    ac2b04a test(ivy): Add small_app spec for sprint #3 (#22018)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-4ec40c6
+ * @license Angular v6.0.0-beta.4-ac2b04a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-4ec40c6');
+const VERSION = new Version('6.0.0-beta.4-ac2b04a');
 
 /**
  * @fileoverview added by tsickle
