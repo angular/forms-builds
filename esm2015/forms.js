a2f78d0640b79e05a01ea0ce67e48d0c5609343f
Victor Berchet <victor@suumit.com>
2018-02-19 04:10:34 +0000

    cdfedc1 fix(common): fix merge error in ng_if.ts

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-c726d1d');
+const VERSION = new Version('5.2.5-cdfedc1');
 
 /**
  * @fileoverview added by tsickle
