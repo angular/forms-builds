9aa2170b8cc4ee4b2edfb6819ef91ef6bb234f49
Victor Berchet <victor@suumit.com>
2018-02-21 19:31:19 +0000

    9e6268b docs(http): fix a typo in code comment (#22327)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-1c1cbba
+ * @license Angular v6.0.0-beta.4-9e6268b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5834,7 +5834,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.4-1c1cbba');
+const VERSION = new Version('6.0.0-beta.4-9e6268b');
 
 /**
  * @fileoverview added by tsickle
