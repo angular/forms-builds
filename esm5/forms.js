7a072811b872fd2f75abd1d9292581493fadced0
Victor Berchet <victor@suumit.com>
2018-02-21 19:37:04 +0000

    dfdade2 docs(aio): Wrong code example. Form status field was added later in the guide. (#21275)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c2f78e1
+ * @license Angular v5.2.5-dfdade2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-c2f78e1');
+var VERSION = new Version('5.2.5-dfdade2');
 
 /**
  * @fileoverview added by tsickle
