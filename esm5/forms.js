d070b3355f8b34f0d68cda7f9073d697ff4c07a2
Victor Berchet <victor@suumit.com>
2018-02-21 00:02:37 +0000

    5bdb3ac build: update tsickle dep from compiler-cli (#22295) PR Close #22295

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-a346d28
+ * @license Angular v5.2.5-5bdb3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-a346d28');
+var VERSION = new Version('5.2.5-5bdb3ac');
 
 /**
  * @fileoverview added by tsickle
