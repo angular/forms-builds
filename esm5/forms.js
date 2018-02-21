b87122deec16c97310747eb3d0fe89e64f62aef7
Victor Berchet <victor@suumit.com>
2018-02-21 23:14:45 +0000

    612cfec docs(aio): updates directive event hooks real capabilities (#16654)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-dfdade2
+ * @license Angular v5.2.5-612cfec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-dfdade2');
+var VERSION = new Version('5.2.5-612cfec');
 
 /**
  * @fileoverview added by tsickle
