bce58c68e3be22d9e401af604f5967110b2104e3
Victor Berchet <victor@suumit.com>
2018-02-21 00:03:33 +0000

    d20a08b docs(aio): update installed mobile tool list (#22331) PR Close #22331

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-5bdb3ac
+ * @license Angular v5.2.5-d20a08b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-5bdb3ac');
+var VERSION = new Version('5.2.5-d20a08b');
 
 /**
  * @fileoverview added by tsickle
