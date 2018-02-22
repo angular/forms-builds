e1cdff8bfdeb1a51f7dfa516b92e465da448633c
Victor Berchet <victor@suumit.com>
2018-02-22 21:45:28 +0000

    b7544cc feat(core): support metadata reflection for native class types (#22356)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-894b098
+ * @license Angular v6.0.0-beta.5-b7544cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.5-894b098
+ * @license Angular v6.0.0-beta.5-b7544cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8055,7 +8055,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-894b098');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-b7544cc');
 
 /**
  * @fileoverview added by tsickle
