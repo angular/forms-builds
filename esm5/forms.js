d6ffa78cb19eac24d2a32039faf008eee487f0e2
Victor Berchet <victor@suumit.com>
2018-02-23 18:13:06 +0000

    aad4316 refactor(ivy): rename componentRefresh to directiveRefresh (#22395)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-a81d599
+ * @license Angular v6.0.0-beta.5-aad4316
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-a81d599');
+var VERSION = new Version('6.0.0-beta.5-aad4316');
 
 /**
  * @fileoverview added by tsickle
