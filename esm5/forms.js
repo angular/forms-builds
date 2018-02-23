8fe14b14e8f405728d16b612ae4fa4afcdde6111
Victor Berchet <victor@suumit.com>
2018-02-23 19:39:35 +0000

    cf91906 docs(aio): Essential JS 2 url updated (#19739) PR Close #19739

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-2b71889
+ * @license Angular v5.2.6-cf91906
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.6-2b71889');
+var VERSION = new Version('5.2.6-cf91906');
 
 /**
  * @fileoverview added by tsickle
