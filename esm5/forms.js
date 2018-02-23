49698af1f74254976e966310ca5a276c4357fa89
Victor Berchet <victor@suumit.com>
2018-02-23 23:53:01 +0000

    4180912 feat(common): export functions to format numbers, percents, currencies & dates (#22423)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-094666d
+ * @license Angular v6.0.0-beta.5-4180912
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-094666d');
+var VERSION = new Version('6.0.0-beta.5-4180912');
 
 /**
  * @fileoverview added by tsickle
