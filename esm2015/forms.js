5dcdecc51a0a7bd630363766b3cafb9109ea9791
Victor Berchet <victor@suumit.com>
2018-02-24 02:34:50 +0000

    a403229 Revert "fix(router): fix URL serialization so special characters are only encoded where needed (#22337)"

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-4180912
+ * @license Angular v6.0.0-beta.5-a403229
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5841,7 +5841,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.5-4180912');
+const VERSION = new Version('6.0.0-beta.5-a403229');
 
 /**
  * @fileoverview added by tsickle
