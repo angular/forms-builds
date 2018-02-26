464ff7c69e072e344dff62dfed7293dc0dc1d255
Alex Eagle <alexeagle@google.com>
2018-02-26 22:00:08 +0000

    c593d69 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-5a32d7e
+ * @license Angular v6.0.0-beta.5-c593d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-5a32d7e');
+var VERSION = new Version('6.0.0-beta.5-c593d69');
 
 /**
  * @fileoverview added by tsickle
