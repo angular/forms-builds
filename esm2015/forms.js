c195c9a99e81e88b3e7d21bd906bd6dbeca36e99
Alex Eagle <alexeagle@google.com>
2018-02-27 02:12:32 +0000

    d033106 docs: update i18n guide for projects that don't use the cli (#21767)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-de02a7a
+ * @license Angular v5.2.6-d033106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.6-de02a7a');
+const VERSION = new Version('5.2.6-d033106');
 
 /**
  * @fileoverview added by tsickle
