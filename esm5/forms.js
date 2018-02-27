02b9f9a05b99fed1687e56d4b3375c229b51c614
Alex Eagle <alexeagle@google.com>
2018-02-27 15:30:51 +0000

    4bd3e5f build(aio): do not render "constructor()" heading in API docs (#22380)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-3f3be42
+ * @license Angular v6.0.0-beta.5-4bd3e5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-3f3be42');
+var VERSION = new Version('6.0.0-beta.5-4bd3e5f');
 
 /**
  * @fileoverview added by tsickle
