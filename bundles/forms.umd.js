29aae27616a0bf74e0456bc39834736f2da2d73b
Alex Eagle <alexeagle@google.com>
2018-02-27 19:47:09 +0000

    f2fa7a2 docs(aio): add Observable and Rx docs (#21423)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8028,7 +8028,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.6-e1fbe20');
+var VERSION = new _angular_core.Version('5.2.6-f2fa7a2');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-e1fbe20
+ * @license Angular v5.2.6-f2fa7a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.6-e1fbe20');
+const VERSION = new Version('5.2.6-f2fa7a2');
 
 /**
  * @fileoverview added by tsickle
