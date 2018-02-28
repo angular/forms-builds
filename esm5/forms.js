87aae349c0e02870df9925ea173978d1ed58129d
Alex Eagle <alexeagle@google.com>
2018-02-28 18:07:53 +0000

    930ecac build: update ts-api-guardian version (#22402)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-e95b61d
+ * @license Angular v6.0.0-beta.5-930ecac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-e95b61d');
+var VERSION = new Version('6.0.0-beta.5-930ecac');
 
 /**
  * @fileoverview added by tsickle
