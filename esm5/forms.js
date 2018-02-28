2b88b05a78dc034675fcc5327492e05485425568
Alex Eagle <alexeagle@google.com>
2018-02-28 23:19:42 +0000

    288851c release: add ng update package group metadata to angular (#22482)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-9eaf1bb
+ * @license Angular v6.0.0-beta.5-288851c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-9eaf1bb');
+var VERSION = new Version('6.0.0-beta.5-288851c');
 
 /**
  * @fileoverview added by tsickle
