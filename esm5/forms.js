2c426b3bfbc246ff31bff22692ccdfdf4878dbe4
Alex Eagle <alexeagle@google.com>
2018-02-28 19:27:50 +0000

    c82cef8 docs: fix dynamic component loader example (#22181)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-7d65356
+ * @license Angular v6.0.0-beta.5-c82cef8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.5-7d65356');
+var VERSION = new Version('6.0.0-beta.5-c82cef8');
 
 /**
  * @fileoverview added by tsickle
