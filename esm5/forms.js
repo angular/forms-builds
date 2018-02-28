2c5a20725b287e0fb417c4cfc098f11f705cd039
Alex Eagle <alexeagle@google.com>
2018-02-28 03:24:39 +0000

    eccce17 docs: fix community tab in GitHub by copying CoC

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-4aef9de
+ * @license Angular v5.2.6-eccce17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.6-4aef9de');
+var VERSION = new Version('5.2.6-eccce17');
 
 /**
  * @fileoverview added by tsickle
