caa6c9d113376463593ce6e0063acff7016367ec
Alex Eagle <alexeagle@google.com>
2018-03-01 16:34:12 +0000

    27962f8 build(aio): improve accuracy of code auto-linking (#22494)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-697d31a
+ * @license Angular v5.2.7-27962f8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.7-697d31a');
+const VERSION = new Version('5.2.7-27962f8');
 
 /**
  * @fileoverview added by tsickle
