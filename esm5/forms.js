e15a8da54205ced071368f9c1465659b122a24d3
Alex Eagle <alexeagle@google.com>
2018-03-01 17:09:50 +0000

    dd53447 fix(compiler): allow tree-shakeable injectables to depend on string tokens (#22376)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-0e311e3
+ * @license Angular v6.0.0-beta.6-dd53447
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-0e311e3');
+var VERSION = new Version('6.0.0-beta.6-dd53447');
 
 /**
  * @fileoverview added by tsickle
