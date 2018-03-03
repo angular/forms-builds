be709e2a172526981edefb1a56809580cb9d463e
Alex Eagle <alexeagle@google.com>
2018-03-03 17:47:24 +0000

    4f60968 test(bazel): Build and test ts-api-guardian locally (#22544)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-d7e5d45
+ * @license Angular v6.0.0-beta.6-4f60968
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-d7e5d45');
+var VERSION = new Version('6.0.0-beta.6-4f60968');
 
 /**
  * @fileoverview added by tsickle
