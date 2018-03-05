057444deb3737bffc378ac8c96da190c3c3a4a96
Alex Eagle <alexeagle@google.com>
2018-03-05 21:49:25 +0000

    2c2b62f fix(ivy): preventDefault when listener returns false (#22529)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-58932c7
+ * @license Angular v6.0.0-beta.6-2c2b62f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-58932c7');
+var VERSION = new Version('6.0.0-beta.6-2c2b62f');
 
 /**
  * @fileoverview added by tsickle
