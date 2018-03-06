fa45eafe28af7ebe7e577f6ab665c356994386be
Alex Eagle <alexeagle@google.com>
2018-03-06 15:17:17 +0000

    2c75acc feat(ivy): add support for the ngProjectAs attribute (#22498)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-f86d8ae
+ * @license Angular v6.0.0-beta.6-2c75acc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-f86d8ae');
+var VERSION = new Version('6.0.0-beta.6-2c75acc');
 
 /**
  * @fileoverview added by tsickle
