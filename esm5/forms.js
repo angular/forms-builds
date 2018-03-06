9fab89a73d0877f1054138a6587bd78afa2f64c2
Alex Eagle <alexeagle@google.com>
2018-03-06 05:43:22 +0000

    2a1e3d1 docs: add info about the WebStorm plugin to BAZEL.md (#22572) PR Close #22572

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2c2b62f
+ * @license Angular v6.0.0-beta.6-2a1e3d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-2c2b62f');
+var VERSION = new Version('6.0.0-beta.6-2a1e3d1');
 
 /**
  * @fileoverview added by tsickle
