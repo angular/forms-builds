82034bf888cf7055bf3484cecc03ba2ae15d8e99
Alex Eagle <alexeagle@google.com>
2018-03-06 05:47:47 +0000

    f86d8ae docs(compiler): fix a line about ivy library (#22579) PR Close #22579

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94bc277
+ * @license Angular v6.0.0-beta.6-f86d8ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-94bc277');
+var VERSION = new Version('6.0.0-beta.6-f86d8ae');
 
 /**
  * @fileoverview added by tsickle
