c3e4ded6d67e47c38061631c1ad2ab1ff5e0bf04
Alex Eagle <alexeagle@google.com>
2018-03-06 15:21:14 +0000

    fa974c7 fix(router): fix URL serialization so special characters are only encoded where needed (#22337)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2c75acc
+ * @license Angular v6.0.0-beta.6-fa974c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5841,7 +5841,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-2c75acc');
+const VERSION = new Version('6.0.0-beta.6-fa974c7');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2c75acc
+ * @license Angular v6.0.0-beta.6-fa974c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-2c75acc');
+var VERSION = new Version('6.0.0-beta.6-fa974c7');
 
 /**
  * @fileoverview added by tsickle
