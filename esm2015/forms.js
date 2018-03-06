c3f0958399f19652f71b8a7ca0fa272528497a08
Alex Eagle <alexeagle@google.com>
2018-03-06 23:25:36 +0000

    505ae75 docs(aio): update deprecated Http reference to HttpClientModule, remove Http reference because another context is used (#21984)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-b3d1761
+ * @license Angular v6.0.0-beta.6-505ae75
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5841,7 +5841,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-b3d1761');
+const VERSION = new Version('6.0.0-beta.6-505ae75');
 
 /**
  * @fileoverview added by tsickle
