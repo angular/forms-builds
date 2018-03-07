a8a0df86712c50e963a0bf6c0c70f0cc77157b98
Alex Eagle <alexeagle@google.com>
2018-03-07 19:12:44 +0000

    4aeb04d docs: update RELEASE_SCHEDULE.md by pushing out v6 rc by one week

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5876fb0
+ * @license Angular v5.2.7-4aeb04d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.7-5876fb0');
+const VERSION = new Version('5.2.7-4aeb04d');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5876fb0
+ * @license Angular v5.2.7-4aeb04d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-5876fb0');
+var VERSION = new Version('5.2.7-4aeb04d');
 
 /**
  * @fileoverview added by tsickle
