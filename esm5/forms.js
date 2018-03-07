19840c5d6aad346df2332096b8e947a50023fd04
Alex Eagle <alexeagle@google.com>
2018-03-07 22:58:55 +0000

    ad8fb84 test: include router in public_api_guard tests (#22628)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-7816791
+ * @license Angular v6.0.0-beta.6-ad8fb84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.6-7816791');
+var VERSION = new Version('6.0.0-beta.6-ad8fb84');
 
 /**
  * @fileoverview added by tsickle
