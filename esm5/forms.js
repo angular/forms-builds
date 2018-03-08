90499b4a21e9073c49a0a6b407d13fea5f80509d
Kara Erickson <karakara@google.com>
2018-03-08 02:45:46 +0000

    6d1367d feat(ivy): provide sanitization methods which can be tree shaken (#22540)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-065bcc5
+ * @license Angular v6.0.0-beta.7-6d1367d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8017,7 +8017,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-065bcc5');
+var VERSION = new Version('6.0.0-beta.7-6d1367d');
 
 /**
  * @fileoverview added by tsickle
