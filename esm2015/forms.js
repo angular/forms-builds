8228bdb25c3cb7a6de149fcfad884e8cee2329bb
Kara Erickson <karakara@google.com>
2018-03-08 02:42:55 +0000

    3112320 docs: add HeroService to code tabs and fix headers (#22373)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-4aeb04d
+ * @license Angular v5.2.8-3112320
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.7-4aeb04d');
+const VERSION = new Version('5.2.8-3112320');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-4aeb04d
+ * @license Angular v5.2.8-3112320
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.7-4aeb04d');
+var VERSION = new Version('5.2.8-3112320');
 
 /**
  * @fileoverview added by tsickle
