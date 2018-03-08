caa051e6e85b10d12f673c0deb4d62b6b2b66a7c
Kara Erickson <karakara@google.com>
2018-03-08 05:20:32 +0000

    f64ee15 feat(ivy): implement pipes (#22254)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-5d4fa7f
+ * @license Angular v6.0.0-beta.7-f64ee15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6056,7 +6056,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-5d4fa7f');
+const VERSION = new Version('6.0.0-beta.7-f64ee15');
 
 /**
  * @fileoverview added by tsickle
