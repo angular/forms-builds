1097ca69c6aef121124c750b288e87b6351f372c
Kara Erickson <karakara@google.com>
2018-03-12 05:39:27 +0000

    86517f2 fix(router): correct over-encoding of URL fragment (#22687)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-6d9a4f8
+ * @license Angular v5.2.8-86517f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.8-6d9a4f8');
+const VERSION = new Version('5.2.8-86517f2');
 
 /**
  * @fileoverview added by tsickle
