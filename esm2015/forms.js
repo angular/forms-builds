750acd368f8928d0ac822104cd5d6753850e2648
Kara Erickson <karakara@google.com>
2018-03-14 06:57:19 +0000

    b1365d1 refactor(ivy): remove directiveRefresh instruction (#22745)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4ac606b
+ * @license Angular v6.0.0-beta.7-b1365d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6056,7 +6056,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.7-4ac606b');
+const VERSION = new Version('6.0.0-beta.7-b1365d1');
 
 /**
  * @fileoverview added by tsickle
