2e925e1358ccc8ce0b470ae5b751e381fa6b68c6
Miško Hevery <misko@hevery.com>
2018-03-16 20:04:24 +0000

    688096b feat(elements): remove attribute/input from config (#22413)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-bafdad9
+ * @license Angular v6.0.0-beta.7-688096b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8226,7 +8226,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-bafdad9');
+var VERSION = new Version('6.0.0-beta.7-688096b');
 
 /**
  * @fileoverview added by tsickle
