08787dc6d605761836f02686a22db8430531199e
Miško Hevery <misko@hevery.com>
2018-03-20 04:06:12 +0000

    5391f96 fix(upgrade): two-way binding and listening for event (#22772)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-aca4735
+ * @license Angular v5.2.9-5391f96
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-aca4735');
+const VERSION = new Version('5.2.9-5391f96');
 
 /**
  * @fileoverview added by tsickle
--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-aca4735
+ * @license Angular v5.2.9-5391f96
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-aca4735');
+var VERSION = new Version('5.2.9-5391f96');
 
 /**
  * @fileoverview added by tsickle
