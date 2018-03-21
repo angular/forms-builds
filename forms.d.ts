6bf733360ab88ffa27cb504ab7d8426be927ecf6
Miško Hevery <misko@hevery.com>
2018-03-21 04:52:43 +0000

    f258ec6 docs(ivy): update status of ivy (#22834)

---
+++ forms.d.ts
@@ -0,0 +1,22 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { InternalFormsSharedModule as ɵbb, REACTIVE_DRIVEN_DIRECTIVES as ɵba, SHARED_FORM_DIRECTIVES as ɵy, TEMPLATE_DRIVEN_DIRECTIVES as ɵz } from './src/directives';
+export { CHECKBOX_VALUE_ACCESSOR as ɵa } from './src/directives/checkbox_value_accessor';
+export { DEFAULT_VALUE_ACCESSOR as ɵb } from './src/directives/default_value_accessor';
+export { AbstractControlStatus as ɵc, ngControlStatusHost as ɵd } from './src/directives/ng_control_status';
+export { formDirectiveProvider as ɵe } from './src/directives/ng_form';
+export { formControlBinding as ɵf } from './src/directives/ng_model';
+export { modelGroupProvider as ɵg } from './src/directives/ng_model_group';
+export { NgNoValidate as ɵbg } from './src/directives/ng_no_validate_directive';
+export { NUMBER_VALUE_ACCESSOR as ɵbc, NumberValueAccessor as ɵbd } from './src/directives/number_value_accessor';
+export { RADIO_VALUE_ACCESSOR as ɵh, RadioControlRegistry as ɵi } from './src/directives/radio_control_value_accessor';
+export { RANGE_VALUE_ACCESSOR as ɵbe, RangeValueAccessor as ɵbf } from './src/directives/range_value_accessor';
+export { NG_MODEL_WITH_FORM_CONTROL_WARNING as ɵj, formControlBinding as ɵk } from './src/directives/reactive_directives/form_control_directive';
+export { controlNameBinding as ɵl } from './src/directives/reactive_directives/form_control_name';
+export { formDirectiveProvider as ɵm } from './src/directives/reactive_directives/form_group_directive';
+export { formArrayNameProvider as ɵo, formGroupNameProvider as ɵn } from './src/directives/reactive_directives/form_group_name';
+export { SELECT_VALUE_ACCESSOR as ɵp } from './src/directives/select_control_value_accessor';
+export { NgSelectMultipleOption as ɵr, SELECT_MULTIPLE_VALUE_ACCESSOR as ɵq } from './src/directives/select_multiple_control_value_accessor';
+export { CHECKBOX_REQUIRED_VALIDATOR as ɵt, EMAIL_VALIDATOR as ɵu, MAX_LENGTH_VALIDATOR as ɵw, MIN_LENGTH_VALIDATOR as ɵv, PATTERN_VALIDATOR as ɵx, REQUIRED_VALIDATOR as ɵs } from './src/directives/validators';
