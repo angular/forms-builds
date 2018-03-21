17d99f0d6d5aad6e9f98ba9f6a9e91dfc94b9f54
Igor Minar <igor@angular.io>
2018-03-21 20:32:51 +0000

    65f8943 fix(service-worker): fix LruList bugs (#22769)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-5391f96
+ * @license Angular v5.2.9-65f8943
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.9-5391f96
+ * @license Angular v5.2.9-65f8943
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8028,7 +8028,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-5391f96');
+var VERSION = new _angular_core.Version('5.2.9-65f8943');
 
 /**
  * @fileoverview added by tsickle
