9daf4e36573cc35d709d2bbbe47555d408f7cbfa
Igor Minar <igor@angular.io>
2018-03-21 21:14:41 +0000

    a846abb docs: fix a typo in aot compiler guide (#22876)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-50761fb
+ * @license Angular v5.2.9-a846abb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7990,7 +7990,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-50761fb');
+var VERSION = new Version('5.2.9-a846abb');
 
 /**
  * @fileoverview added by tsickle
