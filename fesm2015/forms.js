cb748237c32383cac434c684697ed05b2ac8a31c
Igor Minar <igor@angular.io>
2018-03-21 06:34:29 +0000

    7cd8e8d build: update version numbers in ./scripts/release/post-check-next

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-73261a8
+ * @license Angular v6.0.0-rc.0-7cd8e8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6055,7 +6055,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-73261a8');
+const VERSION = new Version('6.0.0-rc.0-7cd8e8d');
 
 /**
  * @fileoverview added by tsickle
