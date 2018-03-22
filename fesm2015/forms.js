46ad38288054adac53384d8723b3824a3a25c031
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:29:04 +0000

    bfe35da fix(ivy): change symbol-extractor to use .accept rather than -—define UPDATE_GOLDEN=1 (#22913)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-f88fba0
+ * @license Angular v6.0.0-rc.0-bfe35da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6055,7 +6055,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-f88fba0');
+const VERSION = new Version('6.0.0-rc.0-bfe35da');
 
 /**
  * @fileoverview added by tsickle
