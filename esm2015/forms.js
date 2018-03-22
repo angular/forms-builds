a3e4febf08c11ff44d4a75d2d5bd0fb6f3588948
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:43:23 +0000

    49f6d1d build: rm --noimplicit_deps from bazel query (#22912)

--- esm2015/forms.js
+++ esm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5824,7 +5824,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.9-641cc49');
+const VERSION = new Version('5.2.9-49f6d1d');
 
 /**
  * @fileoverview added by tsickle
