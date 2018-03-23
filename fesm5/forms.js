79da52ecf0faf94dfeabead59ed4dc991059a853
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:33:26 +0000

    60d9983 docs: update available platforms for `test.sh` (#22958)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-7966744
+ * @license Angular v6.0.0-rc.0-60d9983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-7966744');
+var VERSION = new Version('6.0.0-rc.0-60d9983');
 
 /**
  * @fileoverview added by tsickle
