6813d7bb46367d59484f2f48d6baf4aa7afc8aea
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:31:57 +0000

    16f021c docs(aio): fix TS warning error  - filter expects a boolean function param (#22954)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-3a30f5d
+ * @license Angular v6.0.0-rc.0-16f021c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-3a30f5d');
+var VERSION = new Version('6.0.0-rc.0-16f021c');
 
 /**
  * @fileoverview added by tsickle
