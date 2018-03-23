548f2414084e439ec432d407942279cc8cf4c408
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:37:53 +0000

    bcaa07b refactor(compiler): move handling of translations to the ConstantPool (#22942)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-16f021c
+ * @license Angular v6.0.0-rc.0-bcaa07b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6055,7 +6055,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-16f021c');
+const VERSION = new Version('6.0.0-rc.0-bcaa07b');
 
 /**
  * @fileoverview added by tsickle
