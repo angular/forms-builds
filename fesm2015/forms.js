cc56f512ed5749e3e82b06e22a4c12b460d89e2d
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 21:04:12 +0000

    7b7757d docs(common): add `HttpParamsOptions` to the public API (#20332)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-4bd3a65
+ * @license Angular v6.0.0-rc.0-7b7757d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6055,7 +6055,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-4bd3a65');
+const VERSION = new Version('6.0.0-rc.0-7b7757d');
 
 /**
  * @fileoverview added by tsickle
