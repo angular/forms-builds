921d1467c05d27795ab69eb55378618cd1115e4c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:26:50 +0000

    4e004f3 ci(compiler-cli): run compiler-cli tests in bazel (#22997)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-d9dc46e
+ * @license Angular v6.0.0-rc.0-4e004f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-d9dc46e');
+var VERSION = new Version('6.0.0-rc.0-4e004f3');
 
 /**
  * @fileoverview added by tsickle
