68a08d8d1f8526fcb9f350de865964b780993607
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 16:53:57 +0000

    5a86f71 fix(ivy): store local variables in data instead of calling loadDirective (#23029)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-bd024c0
+ * @license Angular v6.0.0-rc.0-5a86f71
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6055,7 +6055,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.0-bd024c0');
+const VERSION = new Version('6.0.0-rc.0-5a86f71');
 
 /**
  * @fileoverview added by tsickle
