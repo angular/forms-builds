e5f97d0af03f4417d4511deb3abf333a03507ae5
Igor Minar <igor@angular.io>
2018-03-29 21:35:59 +0000

    0d9140c fix(bazel): ng_package should include private exports in fesms (#23054)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-9fb08e2
+ * @license Angular v6.0.0-rc.0-0d9140c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-9fb08e2');
+var VERSION = new Version('6.0.0-rc.0-0d9140c');
 
 /**
  * @fileoverview added by tsickle
