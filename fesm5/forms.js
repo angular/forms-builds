2eef6794a0faa80a2137e2d189c243359416a227
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 16:27:18 +0000

    28058b7 fix(compiler): fix support for html-like text in translatable attributes (#23053)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-0049743
+ * @license Angular v6.0.0-rc.0-28058b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-0049743');
+var VERSION = new Version('6.0.0-rc.0-28058b7');
 
 /**
  * @fileoverview added by tsickle
