3e842a2830f6392caef9474973b7c478e25c5448
Igor Minar <igor@angular.io>
2018-03-30 03:58:51 +0000

    439030f fix(bazel): complete the rollup globals list for all angular entrypoints (#23080)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-481b22e
+ * @license Angular v6.0.0-rc.0-439030f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-481b22e');
+var VERSION = new Version('6.0.0-rc.0-439030f');
 
 /**
  * @fileoverview added by tsickle
