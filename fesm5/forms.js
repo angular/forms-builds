8c34b1d70c7f1c69bc805858cd89222cadb8b250
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:14:32 +0000

    9cd4465 refactor(compiler): generate flat css selectors (#23074)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-6cb1adf
+ * @license Angular v6.0.0-rc.1-9cd4465
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-6cb1adf');
+var VERSION = new Version('6.0.0-rc.1-9cd4465');
 
 /**
  * @fileoverview added by tsickle
