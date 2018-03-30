8e23f90f070e0eac8e03a22d723b642d2c5bf225
Igor Minar <igor@angular.io>
2018-03-30 05:49:39 +0000

    be10bf5 fix(bazel): don't try to do flatmoduleindex under Blaze (#23083)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-439030f
+ * @license Angular v6.0.0-rc.0-be10bf5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-439030f');
+var VERSION = new Version('6.0.0-rc.0-be10bf5');
 
 /**
  * @fileoverview added by tsickle
