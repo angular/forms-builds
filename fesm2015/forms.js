777ccd470284a6fa3c730cc0dadd72709b72fe0c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 21:47:03 +0000

    a2330ff fix(upgrade): propagate return value of resumeBootstrap (#22754)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ff34d5e
+ * @license Angular v6.0.0-rc.1-a2330ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6055,7 +6055,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.1-ff34d5e');
+const VERSION = new Version('6.0.0-rc.1-a2330ff');
 
 /**
  * @fileoverview added by tsickle
