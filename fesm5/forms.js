15e866de031cdfa7b2bfde59a7c9f52e5aeebed0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 17:08:30 +0000

    7a1c437 release: don't include dist when querying for publishable bazel packages (#23102)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-43d6202
+ * @license Angular v6.0.0-rc.1-7a1c437
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8225,7 +8225,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-43d6202');
+var VERSION = new Version('6.0.0-rc.1-7a1c437');
 
 /**
  * @fileoverview added by tsickle
