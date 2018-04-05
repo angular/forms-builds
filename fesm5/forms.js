cf14c62acf38fc1b6444f72e427a192a2fc70c8e
Igor Minar <igor@angular.io>
2018-04-05 17:38:35 +0000

    79cecf9 fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ac316be
+ * @license Angular v6.0.0-rc.1-79cecf9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-ac316be');
+var VERSION = new Version('6.0.0-rc.1-79cecf9');
 
 /**
  * @fileoverview added by tsickle
