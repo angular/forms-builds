72a5eeccd8f35b1da0ef2a966a9ca5f3d248e7e4
Igor Minar <igor@angular.io>
2018-04-05 17:46:25 +0000

    524e5d8 refactor(ivy): adjust types (#23167)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-79cecf9
+ * @license Angular v6.0.0-rc.1-524e5d8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-79cecf9');
+var VERSION = new Version('6.0.0-rc.1-524e5d8');
 
 /**
  * @fileoverview added by tsickle
