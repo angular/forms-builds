1c13c23859f6242687924d465c7dfaffd68571e4
Igor Minar <igor@angular.io>
2018-04-05 16:51:18 +0000

    8ea15b4 docs(core): remove unnecessary `{@link Injector}` jsdoc tag (#23185)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-5db9ab1
+ * @license Angular v6.0.0-rc.1-8ea15b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6057,7 +6057,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-rc.1-5db9ab1');
+const VERSION = new Version('6.0.0-rc.1-8ea15b4');
 
 /**
  * @fileoverview added by tsickle
