3b9528eb1a21998c2a55666ea7d95b39bf55a605
Igor Minar <igor@angular.io>
2018-04-05 17:27:38 +0000

    ac316be build: remove unnecessary stability check (#23176)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-b8053f1
+ * @license Angular v6.0.0-rc.1-ac316be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-b8053f1');
+var VERSION = new Version('6.0.0-rc.1-ac316be');
 
 /**
  * @fileoverview added by tsickle
