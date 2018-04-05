0eb4fc8796be419bfb5b3ddfe9b5b0682ce9fa6a
Igor Minar <igor@angular.io>
2018-04-05 23:51:06 +0000

    a1231be docs: fixed live example for the lifecycle hooks. (#23201)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-73172dd
+ * @license Angular v5.2.9-a1231be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7992,7 +7992,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-73172dd');
+var VERSION = new Version('5.2.9-a1231be');
 
 /**
  * @fileoverview added by tsickle
