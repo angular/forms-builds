b409b7f0f0a3d1cfd5f88f2c24ee87cfb0d3aeed
Igor Minar <igor@angular.io>
2018-04-05 22:26:41 +0000

    628303d fix(ivy): instantiate dirs in correct order (#23178)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-d80e930
+ * @license Angular v6.0.0-rc.2-628303d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-d80e930');
+var VERSION = new Version('6.0.0-rc.2-628303d');
 
 /**
  * @fileoverview added by tsickle
