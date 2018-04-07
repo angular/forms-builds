57297d1e3234b30ab728932c06a0e7bee11ae456
Igor Minar <igor@angular.io>
2018-04-07 05:16:38 +0000

    1fac5f4 docs: recommend best practice for providers (#22934)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-33f8120
+ * @license Angular v6.0.0-rc.3-1fac5f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-33f8120');
+var VERSION = new Version('6.0.0-rc.3-1fac5f4');
 
 /**
  * @fileoverview added by tsickle
