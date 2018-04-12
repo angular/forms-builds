c1b420075680d3c17a99bdcfebaaee4d5e1df55f
Igor Minar <igor@angular.io>
2018-04-12 08:46:19 +0000

    01975ff docs: add changelog for 6.0.0-rc.4

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-1fc72e5
+ * @license Angular v6.0.0-rc.4-01975ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.3-1fc72e5');
+var VERSION = new Version('6.0.0-rc.4-01975ff');
 
 /**
  * @fileoverview added by tsickle
