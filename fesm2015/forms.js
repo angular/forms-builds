8776aedf52505f997949a069faab73a2b7ef0314
Igor Minar <igor@angular.io>
2018-04-12 07:32:45 +0000

    58faa0c build: reorder PACKAGE_GROUP entries to make @angular/core the first one (#23303)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-af46d09
+ * @license Angular v6.0.0-rc.3-58faa0c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6059,7 +6059,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.3-af46d09');
+const VERSION = new Version('6.0.0-rc.3-58faa0c');
 
 /**
  * @fileoverview added by tsickle
