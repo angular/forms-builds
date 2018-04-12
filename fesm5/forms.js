14d478309d7e9689014a7818863e598000096503
Igor Minar <igor@angular.io>
2018-04-12 06:43:48 +0000

    af46d09 fix(elements): avoid exception when window is undefined (#23324)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-7e8cee6
+ * @license Angular v6.0.0-rc.3-af46d09
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.3-7e8cee6');
+var VERSION = new Version('6.0.0-rc.3-af46d09');
 
 /**
  * @fileoverview added by tsickle
