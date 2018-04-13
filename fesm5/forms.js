653094c4d2c63eaa2a99796f60c9d5c9aa0b2d48
Igor Minar <igor@angular.io>
2018-04-13 06:38:57 +0000

    993eeab docs: fix typo in injected variable name (#23315)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-b8f4e43
+ * @license Angular v6.0.0-rc.4-993eeab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-b8f4e43');
+var VERSION = new Version('6.0.0-rc.4-993eeab');
 
 /**
  * @fileoverview added by tsickle
