8c76ff1792b811b21dbaee80f67a57cef98d7569
Igor Minar <igor@angular.io>
2018-04-13 23:43:12 +0000

    2c09b70 refactor(ivy): make return value of define(Component|Directive|Pipe|Injector|Injectable) private (#23371)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-f4017ce
+ * @license Angular v6.0.0-rc.4-2c09b70
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-f4017ce');
+var VERSION = new Version('6.0.0-rc.4-2c09b70');
 
 /**
  * @fileoverview added by tsickle
