c32ee326c636e9dbb93d7067e4de8f89e6e0b252
Igor Minar <igor@angular.io>
2018-04-13 06:43:46 +0000

    75febe7 docs(upgrade): fix detail regarding bootstrapping order (#23225) (#23270)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-993eeab
+ * @license Angular v6.0.0-rc.4-75febe7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-993eeab');
+var VERSION = new Version('6.0.0-rc.4-75febe7');
 
 /**
  * @fileoverview added by tsickle
