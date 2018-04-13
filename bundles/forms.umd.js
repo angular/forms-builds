210eda2c7b0b28ed7a8b6f907b356d2ae1ac7279
Igor Minar <igor@angular.io>
2018-04-13 05:57:19 +0000

    4ea8b17 docs: lock version of in-memory API (#23242)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-c5b6e31
+ * @license Angular v5.2.9-4ea8b17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.9-c5b6e31
+ * @license Angular v5.2.9-4ea8b17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8030,7 +8030,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-c5b6e31');
+var VERSION = new _angular_core.Version('5.2.9-4ea8b17');
 
 /**
  * @fileoverview added by tsickle
