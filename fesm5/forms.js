12c2d9a01a597c18eed93281fae04ef428cedcc4
Igor Minar <igor@angular.io>
2018-04-13 21:04:46 +0000

    e7ef027 fix(ivy): local directives and pipes should be applied to TemplateRef (#23312)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-d5e7f60
+ * @license Angular v6.0.0-rc.4-e7ef027
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-d5e7f60');
+var VERSION = new Version('6.0.0-rc.4-e7ef027');
 
 /**
  * @fileoverview added by tsickle
