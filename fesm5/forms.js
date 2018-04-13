ebb68e345b06f6115d7fe81ba967f46655969d76
Igor Minar <igor@angular.io>
2018-04-13 07:39:47 +0000

    2e270bb build: include tslib in umd bundles (#23354)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-a025f7e
+ * @license Angular v6.0.0-rc.4-2e270bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-a025f7e');
+var VERSION = new Version('6.0.0-rc.4-2e270bb');
 
 /**
  * @fileoverview added by tsickle
