0ad8194de75508337a1076993cde2805dc73a56c
Igor Minar <igor@angular.io>
2018-04-16 06:46:52 +0000

    7b23983 fix(service-worker): add badge to NOTIFICATION_OPTION_NAMES (#23241)

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-21f3301
+ * @license Angular v5.2.9-7b23983
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7992,7 +7992,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-21f3301');
+var VERSION = new Version('5.2.9-7b23983');
 
 /**
  * @fileoverview added by tsickle
