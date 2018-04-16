694d893d691d01ba26e6b603d946fd52f282a07d
Igor Minar <igor@angular.io>
2018-04-16 09:36:29 +0000

    4361560 docs: add changelog for the v4.4.7 release

---
+++ esm2015/src/directives/form_interface.js
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * An interface that `FormGroupDirective` and `NgForm` implement.
+ *
+ * Only used by the forms module.
+ *
+ *
+ * @record
+ */
+export function Form() { }
+function Form_tsickle_Closure_declarations() {
+    /**
+     * Add a control to this form.
+     * @type {?}
+     */
+    Form.prototype.addControl;
+    /**
+     * Remove a control from this form.
+     * @type {?}
+     */
+    Form.prototype.removeControl;
+    /**
+     * Look up the `FormControl` associated with a particular `NgControl`.
+     * @type {?}
+     */
+    Form.prototype.getControl;
+    /**
+     * Add a group of controls to this form.
+     * @type {?}
+     */
+    Form.prototype.addFormGroup;
+    /**
+     * Remove a group of controls from this form.
+     * @type {?}
+     */
+    Form.prototype.removeFormGroup;
+    /**
+     * Look up the `FormGroup` associated with a particular `AbstractFormGroupDirective`.
+     * @type {?}
+     */
+    Form.prototype.getFormGroup;
+    /**
+     * Update the model for a particular control with a new value.
+     * @type {?}
+     */
+    Form.prototype.updateModel;
+}
+//# sourceMappingURL=form_interface.js.map
---
+++ esm2015/src/directives/number_value_accessor.js
@@ -0,0 +1,101 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Directive, ElementRef, Renderer2, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from './control_value_accessor';
+export const /** @type {?} */ NUMBER_VALUE_ACCESSOR = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => NumberValueAccessor),
+    multi: true
+};
+/**
+ * The accessor for writing a number value and listening to changes that is used by the
+ * `NgModel`, `FormControlDirective`, and `FormControlName` directives.
+ *
+ *  ### Example
+ *  ```
+ *  <input type="number" [(ngModel)]="age">
+ *  ```
+ */
+export class NumberValueAccessor {
+    /**
+     * @param {?} _renderer
+     * @param {?} _elementRef
+     */
+    constructor(_renderer, _elementRef) {
+        this._renderer = _renderer;
+        this._elementRef = _elementRef;
+        this.onChange = (_) => { };
+        this.onTouched = () => { };
+    }
+    /**
+     * @param {?} value
+     * @return {?}
+     */
+    writeValue(value) {
+        // The value needs to be normalized for IE9, otherwise it is set to 'null' when null
+        const /** @type {?} */ normalizedValue = value == null ? '' : value;
+        this._renderer.setProperty(this._elementRef.nativeElement, 'value', normalizedValue);
+    }
+    /**
+     * @param {?} fn
+     * @return {?}
+     */
+    registerOnChange(fn) {
+        this.onChange = (value) => { fn(value == '' ? null : parseFloat(value)); };
+    }
+    /**
+     * @param {?} fn
+     * @return {?}
+     */
+    registerOnTouched(fn) { this.onTouched = fn; }
+    /**
+     * @param {?} isDisabled
+     * @return {?}
+     */
+    setDisabledState(isDisabled) {
+        this._renderer.setProperty(this._elementRef.nativeElement, 'disabled', isDisabled);
+    }
+}
+NumberValueAccessor.decorators = [
+    { type: Directive, args: [{
+                selector: 'input[type=number][formControlName],input[type=number][formControl],input[type=number][ngModel]',
+                host: {
+                    '(change)': 'onChange($event.target.value)',
+                    '(input)': 'onChange($event.target.value)',
+                    '(blur)': 'onTouched()'
+                },
+                providers: [NUMBER_VALUE_ACCESSOR]
+            },] },
+];
+/** @nocollapse */
+NumberValueAccessor.ctorParameters = () => [
+    { type: Renderer2, },
+    { type: ElementRef, },
+];
+function NumberValueAccessor_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    NumberValueAccessor.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    NumberValueAccessor.ctorParameters;
+    /** @type {?} */
+    NumberValueAccessor.prototype.onChange;
+    /** @type {?} */
+    NumberValueAccessor.prototype.onTouched;
+    /** @type {?} */
+    NumberValueAccessor.prototype._renderer;
+    /** @type {?} */
+    NumberValueAccessor.prototype._elementRef;
+}
+//# sourceMappingURL=number_value_accessor.js.map
