c79ad8996ba3128fc32131fc5b37a65884858666
Jason Aden <jasonaden@google.com>
2018-04-17 21:09:22 +0000

    99f8e10 ci(aio): fix `aio-monitoring` tests (#23390)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-010a4ef
+ * @license Angular v6.0.0-rc.5-99f8e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-010a4ef');
+var VERSION = new Version('6.0.0-rc.5-99f8e10');
 
 /**
  * @fileoverview added by tsickle
