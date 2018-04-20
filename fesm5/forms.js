8cbf0451963b4cff581dad0e190242f5662ad549
Victor Berchet <victor@suumit.com>
2018-04-20 23:33:52 +0000

    4662878 refactor(compiler): refactor template binding parsing (#23460)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-ca776c5
+ * @license Angular v6.0.0-rc.5-4662878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-ca776c5');
+var VERSION = new Version('6.0.0-rc.5-4662878');
 
 /**
  * @fileoverview added by tsickle
