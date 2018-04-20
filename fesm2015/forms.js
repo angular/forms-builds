fd8c268dbe6d10e68bfc5f204d0d76104315210b
Victor Berchet <victor@suumit.com>
2018-04-20 02:23:16 +0000

    ca776c5 fix(compiler): handle undefined annotation metadata (#23349)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-f2563ca
+ * @license Angular v6.0.0-rc.5-ca776c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6059,7 +6059,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5-f2563ca');
+const VERSION = new Version('6.0.0-rc.5-ca776c5');
 
 /**
  * @fileoverview added by tsickle
