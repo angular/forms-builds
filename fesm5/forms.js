ec4036748900720ef1a6e54590ea88b56aa2f550
Victor Berchet <victor@suumit.com>
2018-04-21 00:45:14 +0000

    6761a64 refactor(compiler): remove a dependency from the IVY AST to the template AST (#23476)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-4662878
+ * @license Angular v6.0.0-rc.5-6761a64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-4662878');
+var VERSION = new Version('6.0.0-rc.5-6761a64');
 
 /**
  * @fileoverview added by tsickle
