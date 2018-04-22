abe11f75b498148e549dbc00b3779225ddd909d1
Victor Berchet <victor@suumit.com>
2018-04-22 19:56:49 +0000

    02424ff Revert "style(compiler): fix lint issues (#23480)"

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-b9431e8
+ * @license Angular v6.0.0-rc.5-02424ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-b9431e8');
+var VERSION = new Version('6.0.0-rc.5-02424ff');
 
 /**
  * @fileoverview added by tsickle
