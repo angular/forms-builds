bd4c740703eec249e3748c4d83da9800bb6f7739
Victor Berchet <victor@suumit.com>
2018-04-24 18:37:14 +0000

    540626a build(common): mark locales files as side-effect-full (#23509)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-391bfce
+ * @license Angular v6.0.0-rc.5-540626a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-391bfce');
+var VERSION = new Version('6.0.0-rc.5-540626a');
 
 /**
  * @fileoverview added by tsickle
