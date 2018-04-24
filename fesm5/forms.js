3f04b8c8d7dbbc69b2d5e36d6602a53d76e8da8c
Victor Berchet <victor@suumit.com>
2018-04-24 21:24:47 +0000

    0bc8443 fix(compiler): avoid a crash in ngc-wrapped. (#23468)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-db17231
+ * @license Angular v6.0.0-rc.5-0bc8443
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-db17231');
+var VERSION = new Version('6.0.0-rc.5-0bc8443');
 
 /**
  * @fileoverview added by tsickle
