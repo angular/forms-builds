505e244a0ac69249f328d7408418cafc291a26c0
Victor Berchet <victor@suumit.com>
2018-04-24 00:33:25 +0000

    1c9200e ci: require green integration tests to publish snapshot (#23517)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+73.sha-ace6440
+ * @license Angular v6.0.0-rc.5+74.sha-1c9200e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6181,7 +6181,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+73.sha-ace6440');
+var VERSION = new core.Version('6.0.0-rc.5+74.sha-1c9200e');
 
 /**
  * @license
