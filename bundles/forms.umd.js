00dd09c12b43acca0b4a81c30d1b92994de8bcd2
Victor Berchet <victor@suumit.com>
2018-04-24 22:01:17 +0000

    811a7f2 docs(benchpress): fix typo in README (#23471) (#23488)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+79.sha-9ed5fb6
+ * @license Angular v6.0.0-rc.5+80.sha-811a7f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6181,7 +6181,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+79.sha-9ed5fb6');
+var VERSION = new core.Version('6.0.0-rc.5+80.sha-811a7f2');
 
 /**
  * @license
