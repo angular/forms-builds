68adacf71bdc9590eade9f8327f0fe000ff2f7f6
Igor Minar <igor@angular.io>
2018-04-25 20:54:39 +0000

    db77d8d feat(ivy): support injection flags at runtime (#23518)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+83.sha-f567e18
+ * @license Angular v6.0.0-rc.5+85.sha-db77d8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6181,7 +6181,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+83.sha-f567e18');
+var VERSION = new core.Version('6.0.0-rc.5+85.sha-db77d8d');
 
 /**
  * @license
