1ca911896fa3906eb3b99572c0e268eda09d4dc3
Igor Minar <igor@angular.io>
2018-04-25 23:18:28 +0000

    eb031c6 refactor(core): tree-shake application_module providers (#23477)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+86.sha-b4c252b
+ * @license Angular v6.0.0-rc.5+87.sha-eb031c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6099,7 +6099,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+86.sha-b4c252b');
+var VERSION = new Version('6.0.0-rc.5+87.sha-eb031c6');
 
 /**
  * @license
