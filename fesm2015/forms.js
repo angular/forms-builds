e0b9dd19182e61d19adc136d07bda7df3a524318
Igor Minar <igor@angular.io>
2018-04-26 02:24:51 +0000

    1a44a0b feat(ivy): support lifecycle hooks of ViewContainerRef (#23396)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+88.sha-b1f040f
+ * @license Angular v6.0.0-rc.5+89.sha-1a44a0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6060,7 +6060,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+88.sha-b1f040f');
+const VERSION = new Version('6.0.0-rc.5+89.sha-1a44a0b');
 
 /**
  * @fileoverview added by tsickle
