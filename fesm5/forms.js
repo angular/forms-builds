4ded11935f5f8a4abb23114f1d266afc67320344
Igor Minar <igor@angular.io>
2018-05-01 18:04:15 +0000

    f11daa2 docs(aio): update Egghead.io URL (#23598)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-31a435e
+ * @license Angular v6.0.0-rc.6-f11daa2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.6-31a435e');
+var VERSION = new Version('6.0.0-rc.6-f11daa2');
 
 /**
  * @fileoverview added by tsickle
