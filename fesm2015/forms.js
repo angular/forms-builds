92dc804336059c8db9f59c3116ba58b13f4f0349
Igor Minar <igor@angular.io>
2018-05-03 21:49:29 +0000

    9027818 docs(aio): Upgrade server-side rendering example to Angular V6 (#23649)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-5bd8c68
+ * @license Angular v6.0.0-9027818
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6059,7 +6059,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  *
  */
-const VERSION = new Version('6.0.0-5bd8c68');
+const VERSION = new Version('6.0.0-9027818');
 
 /**
  * @fileoverview added by tsickle
