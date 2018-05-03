683322f9d592d989533739036098a25201da5046
Igor Minar <igor@angular.io>
2018-05-03 20:48:01 +0000

    2ef4760 docs: improve the GitHub README.md, update links, etc

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+122.sha-61265b4
+ * @license Angular v6.0.0-rc.5+124.sha-2ef4760
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6060,7 +6060,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+122.sha-61265b4');
+const VERSION = new Version('6.0.0-rc.5+124.sha-2ef4760');
 
 /**
  * @fileoverview added by tsickle
