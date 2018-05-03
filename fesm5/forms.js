6f90e618c3a1fcb5e739e3805a73d83d0d1ac089
Igor Minar <igor@angular.io>
2018-05-03 22:29:56 +0000

    a190c45 fix(aio): correct project id for deployment of archive sites

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-9027818
+ * @license Angular v6.0.0-a190c45
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8229,7 +8229,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-9027818');
+var VERSION = new Version('6.0.0-a190c45');
 
 /**
  * @fileoverview added by tsickle
