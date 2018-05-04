1f0713d1af214eaac0474abba7b79e08d2e68659
Igor Minar <igor@angular.io>
2018-05-04 22:32:29 +0000

    65dba9d test: fix firebase deployment script test

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-402f452
+ * @license Angular v5.2.10-65dba9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.10-402f452
+ * @license Angular v5.2.10-65dba9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8030,7 +8030,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.10-402f452');
+var VERSION = new _angular_core.Version('5.2.10-65dba9d');
 
 /**
  * @fileoverview added by tsickle
