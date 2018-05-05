de848931f0028f76da98f4352e061adc75fc8e7d
Igor Minar <igor@angular.io>
2018-05-05 15:38:51 +0000

    e351896 style(animations): fix short param names (#23668)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+136.sha-7e9649b
+ * @license Angular v6.0.0-rc.5+138.sha-e351896
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6181,7 +6181,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+136.sha-7e9649b');
+var VERSION = new core.Version('6.0.0-rc.5+138.sha-e351896');
 
 /**
  * @license
