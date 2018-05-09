d479888292fc301d3dd605cc13e13aeb7a761808
Igor Minar <igor@angular.io>
2018-05-09 19:18:15 +0000

    971e78d ci: Remove Chuck from pullapprove (#23798)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+144.sha-b0eca85
+ * @license Angular v6.0.0-rc.5+145.sha-971e78d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6099,7 +6099,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+144.sha-b0eca85');
+var VERSION = new Version('6.0.0-rc.5+145.sha-971e78d');
 
 /**
  * @license
