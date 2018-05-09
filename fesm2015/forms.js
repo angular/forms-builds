e304d6499bdd30c58d7b16b56c697036b49b1cd0
Igor Minar <igor@angular.io>
2018-05-09 19:32:05 +0000

    2254ac2 fix(service-worker): correctly handle requests with empty `clientId` (#23625)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+38.sha-2b31b6d
+ * @license Angular v6.0.0+37.sha-2254ac2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6060,7 +6060,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  *
  */
-const VERSION = new Version('6.0.0+38.sha-2b31b6d');
+const VERSION = new Version('6.0.0+37.sha-2254ac2');
 
 /**
  * @fileoverview added by tsickle
