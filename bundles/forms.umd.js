ce62251cb59b9056002bbe74f6b10b249830eed0
Igor Minar <igor@angular.io>
2018-05-10 19:27:48 +0000

    484233f docs(aio): add Alain Chautard in GDE list (#23783)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+42.sha-3d8799b
+ * @license Angular v6.0.0+43.sha-484233f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6181,7 +6181,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0+42.sha-3d8799b');
+var VERSION = new core.Version('6.0.0+43.sha-484233f');
 
 /**
  * @license
