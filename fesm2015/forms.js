e1fdc6db4398afe3634cc3f15f597964743b31db
Igor Minar <igor@angular.io>
2018-05-10 23:27:19 +0000

    ca6cb66 docs: change release_schedule.md to link to new angular release page in docs (#23808)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+45.sha-f576851
+ * @license Angular v6.0.0+44.sha-ca6cb66
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6060,7 +6060,7 @@ FormBuilder.ctorParameters = () => [];
 /**
  *
  */
-const VERSION = new Version('6.0.0+45.sha-f576851');
+const VERSION = new Version('6.0.0+44.sha-ca6cb66');
 
 /**
  * @fileoverview added by tsickle
