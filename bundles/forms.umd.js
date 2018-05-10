8671213ea769a337f5e033cad66fb5c09a7a7eb7
Igor Minar <igor@angular.io>
2018-05-10 15:19:46 +0000

    553a680 fix(router): correct the segment parsing so it won't break on ampersand (#23684)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+154.sha-f1e4a15
+ * @license Angular v6.0.0-rc.5+157.sha-553a680
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6181,7 +6181,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+154.sha-f1e4a15');
+var VERSION = new core.Version('6.0.0-rc.5+157.sha-553a680');
 
 /**
  * @license
