fcd504ee87ad795162ae283ef512ba46a3eaeae1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 20:06:23 +0000

    e9d1709 build: only match version tags for BUILD_SCM_VERSION (#23903)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+59.sha-b5d3de5
+ * @license Angular v6.0.0+60.sha-e9d1709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6178,7 +6178,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0+59.sha-b5d3de5');
+var VERSION = new core.Version('6.0.0+60.sha-e9d1709');
 
 /**
  * @license
