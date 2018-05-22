17b5d805d2fd41e1a0500cdd321da4c96f0e361a
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 21:28:05 +0000

    bd149e5 fix(ivy): compile interpolated bindings without superfluous bind instruction (#23923)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+213.sha-fb906a8
+ * @license Angular v6.0.0-rc.5+214.sha-bd149e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6168,7 +6168,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.0-rc.5+213.sha-fb906a8');
+var VERSION = new core.Version('6.0.0-rc.5+214.sha-bd149e5');
 
 /**
  * @license
