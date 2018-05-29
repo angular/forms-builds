f12c180643c713c3643f9871f4667dfbcbec3738
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:14:28 +0000

    84b43da build: pick up rules_typescript karma fix (#24184)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+9.sha-a583d12
+ * @license Angular v6.0.3+7.sha-84b43da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6178,7 +6178,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.3+9.sha-a583d12');
+var VERSION = new core.Version('6.0.3+7.sha-84b43da');
 
 /**
  * @license
