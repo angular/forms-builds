d88e98a51f00f9d0cc202b6c91b64a7fdc18291b
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:20:50 +0000

    5a02ae2 docs: update lts and labs practices (#23922)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+10.sha-7698afe
+ * @license Angular v6.0.3+18.sha-5a02ae2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6178,7 +6178,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new core.Version('6.0.3+10.sha-7698afe');
+var VERSION = new core.Version('6.0.3+18.sha-5a02ae2');
 
 /**
  * @license
