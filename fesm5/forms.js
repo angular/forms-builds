59388c9e77a5591c1ab48818ff4f41d4c255234b
Victor Berchet <victor@suumit.com>
2018-05-30 23:36:19 +0000

    a634a5a Revert "docs: update docs to use HttpClientModule instead of HttpModule (#22727)"

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+34.sha-4122502
+ * @license Angular v6.0.3+35.sha-a634a5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6096,7 +6096,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.3+34.sha-4122502');
+var VERSION = new Version('6.0.3+35.sha-a634a5a');
 
 /**
  * @license
