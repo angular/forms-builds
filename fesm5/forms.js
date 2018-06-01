7e756e4b8625045f8f3c804626f2305d69c9da8d
Victor Berchet <victor@suumit.com>
2018-06-01 15:55:49 +0000

    2991b1b fix(platform-server): avoid dependency cycle when using http interceptor (#24229)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+48.sha-b18cf21
+ * @license Angular v6.0.3+49.sha-2991b1b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6096,7 +6096,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.3+48.sha-b18cf21');
+var VERSION = new Version('6.0.3+49.sha-2991b1b');
 
 /**
  * @license
