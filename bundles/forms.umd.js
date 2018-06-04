5f090495c12b71e371d51f58c4013947d49f5f43
Victor Berchet <victor@suumit.com>
2018-06-04 17:55:31 +0000

    a3d9878 docs(aio): remove an extraneous apostrophe (#24293) PR Close #24293

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+52.sha-0e1919c
+ * @license Angular v6.0.3+54.sha-a3d9878
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5035,7 +5035,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.3+52.sha-0e1919c');
+var VERSION = new core.Version('6.0.3+54.sha-a3d9878');
 
 /**
  * @license
