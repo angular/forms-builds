51d70b034c45a5ce09fb6ab6a35f049cab4ebb43
Victor Berchet <victor@suumit.com>
2018-06-05 00:47:25 +0000

    faf60d9 docs: rename the "aio" component to "docs-infra" (#24295)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+57.sha-0639cb9
+ * @license Angular v6.0.3+58.sha-faf60d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4953,7 +4953,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+57.sha-0639cb9');
+var VERSION = new Version('6.0.3+58.sha-faf60d9');
 
 /**
  * @license
