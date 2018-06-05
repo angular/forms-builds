c67c6781eaaf7faf429d89f7a5d89298f04d7a8a
Victor Berchet <victor@suumit.com>
2018-06-05 22:38:08 +0000

    20c463e feat(router): add navigation execution context info to activation hooks (#24204)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+317.sha-57eacf4
+ * @license Angular v6.0.0-rc.5+318.sha-20c463e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+317.sha-57eacf4');
+const VERSION = new Version('6.0.0-rc.5+318.sha-20c463e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+317.sha-57eacf4
+ * @license Angular v6.0.0-rc.5+318.sha-20c463e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4954,7 +4954,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+317.sha-57eacf4');
+var VERSION = new Version('6.0.0-rc.5+318.sha-20c463e');
 
 /**
  * @license
