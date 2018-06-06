4ec6c87b5e4c89a1596eea4039502149316dedce
Victor Berchet <victor@suumit.com>
2018-06-06 05:09:36 +0000

    22b58a7 feat(ivy): add support of ApplicationRef.bootstrapModuleFactory (#23811)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+322.sha-86b13cc
+ * @license Angular v6.0.0-rc.5+323.sha-22b58a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.0-rc.5+322.sha-86b13cc');
+const VERSION = new Version('6.0.0-rc.5+323.sha-22b58a7');
 
 /**
  * @fileoverview added by tsickle
