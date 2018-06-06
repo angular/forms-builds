e74edd20b0270f155458f5cb7a0784b9190213b4
Victor Berchet <victor@suumit.com>
2018-06-06 19:27:25 +0000

    41698bf release: cut the v6.0.4 release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+67.sha-23c50e2
+ * @license Angular v6.0.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5035,7 +5035,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.3+67.sha-23c50e2');
+var VERSION = new core.Version('6.0.4');
 
 /**
  * @license
