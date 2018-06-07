c635dba117780487a635bf00deb55ac7d79171bb
Miško Hevery <misko@hevery.com>
2018-06-07 23:11:34 +0000

    d4c66d5 docs(ivy): update status of impl progress (#24323)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+17.sha-ce1543f
+ * @license Angular v6.1.0-beta.0+18.sha-d4c66d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.0+17.sha-ce1543f');
+var VERSION = new core.Version('6.1.0-beta.0+18.sha-d4c66d5');
 
 /**
  * @license
