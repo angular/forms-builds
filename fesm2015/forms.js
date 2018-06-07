20d1d52c07ba4f311b757f42809cc85cd177254a
Miško Hevery <misko@hevery.com>
2018-06-07 23:10:37 +0000

    ce1543f docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+16.sha-a6e797b
+ * @license Angular v6.1.0-beta.0+17.sha-ce1543f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+16.sha-a6e797b');
+const VERSION = new Version('6.1.0-beta.0+17.sha-ce1543f');
 
 /**
  * @fileoverview added by tsickle
