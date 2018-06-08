b2b288f9100a452fd618387db54ee7d61c2baa06
Miško Hevery <misko@hevery.com>
2018-06-08 23:10:33 +0000

    49c5234 feat(router): implement scrolling restoration service (#20030)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+26.sha-1b253e1
+ * @license Angular v6.1.0-beta.0+27.sha-49c5234
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4954,7 +4954,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+26.sha-1b253e1');
+var VERSION = new Version('6.1.0-beta.0+27.sha-49c5234');
 
 /**
  * @license
