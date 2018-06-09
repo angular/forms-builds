854e3aab67877e9b19cc74be06635568518b01e6
Miško Hevery <misko@hevery.com>
2018-06-09 00:08:54 +0000

    0139173 fix(animations): always render end-state styles for orphaned DOM nodes (#24236)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+6.sha-f3c9954
+ * @license Angular v6.0.4+7.sha-0139173
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4953,7 +4953,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.4+6.sha-f3c9954');
+var VERSION = new Version('6.0.4+7.sha-0139173');
 
 /**
  * @license
