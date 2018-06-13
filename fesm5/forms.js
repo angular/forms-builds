7683a42388b31d608a4295f787b09bee3c647686
Miško Hevery <misko@hevery.com>
2018-06-13 21:13:04 +0000

    a577c9e docs: edit api doc comments for new template and style (#23682)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+43.sha-70ef061
+ * @license Angular v6.1.0-beta.0+55.sha-a577c9e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4954,7 +4954,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+43.sha-70ef061');
+var VERSION = new Version('6.1.0-beta.0+55.sha-a577c9e');
 
 /**
  * @license
