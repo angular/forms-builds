ffac98b01ea3e389d6c47bc75bb4b26c209da0c0
Miško Hevery <misko@hevery.com>
2018-06-13 21:13:15 +0000

    503a524 docs: change capitalization for css hex color values (#23511)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+55.sha-a577c9e
+ * @license Angular v6.1.0-beta.0+56.sha-503a524
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+55.sha-a577c9e');
+const VERSION = new Version('6.1.0-beta.0+56.sha-503a524');
 
 /**
  * @fileoverview added by tsickle
