73e9d188108a97408d4c5cf344749d139c76d64c
Miško Hevery <misko@hevery.com>
2018-06-14 21:46:36 +0000

    6d246d6 fix(ivy): allow view and content queries to match the same element (#24507)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+25.sha-7c8159b
+ * @license Angular v6.1.0-beta.1+26.sha-6d246d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.1+25.sha-7c8159b');
+const VERSION = new Version('6.1.0-beta.1+26.sha-6d246d6');
 
 /**
  * @fileoverview added by tsickle
