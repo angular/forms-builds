476f9896ec09d0806477a8b2d75e37007c01fb84
Miško Hevery <misko@hevery.com>
2018-06-15 16:37:53 +0000

    e1bc3f5 build(docs-infra): ensure all headings are sentence cased (#24527)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+25.sha-f5b3661
+ * @license Angular v6.0.5+26.sha-e1bc3f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4953,7 +4953,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.5+25.sha-f5b3661');
+var VERSION = new Version('6.0.5+26.sha-e1bc3f5');
 
 /**
  * @license
