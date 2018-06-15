2a6400ca68f498b914cd03a48ff439d01aa1d791
Miško Hevery <misko@hevery.com>
2018-06-15 00:08:17 +0000

    ccbda9d fix(core): Injector correctly honors the @Self flag (#24520)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+30.sha-27bc7dc
+ * @license Angular v6.1.0-beta.1+31.sha-ccbda9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.1+30.sha-27bc7dc');
+const VERSION = new Version('6.1.0-beta.1+31.sha-ccbda9d');
 
 /**
  * @fileoverview added by tsickle
