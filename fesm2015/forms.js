5a432fbde7d0f6d2a88ad978ff59a0895f30f884
Miško Hevery <misko@hevery.com>
2018-06-21 00:28:42 +0000

    1ce5a49 docs(aio): add elana olson to contributor.json file (#24579)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+1.sha-1f532aa
+ * @license Angular v6.0.6+2.sha-1ce5a49
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6238,7 +6238,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.6+1.sha-1f532aa');
+const VERSION = new Version('6.0.6+2.sha-1ce5a49');
 
 /**
  * @fileoverview added by tsickle
