d6f8b30fb8032e42de54889f176520f9a8c73e91
Miško Hevery <misko@hevery.com>
2018-06-21 00:21:36 +0000

    def354d release: cut the v6.1.0-beta.2 release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+43.sha-9782736
+ * @license Angular v6.1.0-beta.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5315,7 +5315,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.1+43.sha-9782736');
+var VERSION = new core.Version('6.1.0-beta.2');
 
 /**
  * @license
