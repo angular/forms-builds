eceaa78da8115d9d744d1b73c2d6fad3b2d45739
Jason Aden <jasonaden@google.com>
2018-06-25 17:13:48 +0000

    65744e4 docs: test api doc for pipes (#24141)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+10.sha-56bc869
+ * @license Angular v6.0.6+11.sha-65744e4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,7 +5232,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.6+10.sha-56bc869');
+var VERSION = new Version('6.0.6+11.sha-65744e4');
 
 /**
  * @license
