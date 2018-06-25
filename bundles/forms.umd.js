c2139275fa3afc7cddaacffb643d835c9469065d
Miško Hevery <misko@hevery.com>
2018-06-25 16:02:56 +0000

    39c7769 docs(core): typo in static injector tests (#24548)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+16.sha-8c51ce6
+ * @license Angular v6.1.0-beta.2+17.sha-39c7769
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5315,7 +5315,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.1.0-beta.2+16.sha-8c51ce6');
+var VERSION = new core.Version('6.1.0-beta.2+17.sha-39c7769');
 
 /**
  * @license
