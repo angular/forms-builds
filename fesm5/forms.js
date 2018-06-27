a0ffffb954dd2bdbb1c08c51f5db628188f8a25b
Jason Aden <jasonaden@google.com>
2018-06-27 22:14:48 +0000

    8dec381 docs: fix unit tests in toh-pt6 (#24491)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+42.sha-32da3e1
+ * @license Angular v6.1.0-beta.2+43.sha-8dec381
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5262,7 +5262,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+42.sha-32da3e1');
+var VERSION = new Version('6.1.0-beta.2+43.sha-8dec381');
 
 /**
  * @license
