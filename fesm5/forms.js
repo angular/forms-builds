2769f0ac249066c15b9af34e41a2490406bdde0a
Miško Hevery <misko@hevery.com>
2018-06-28 22:40:16 +0000

    ac5b69f fix(docs-infra): use clean package.json template when generating zips (#24691)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+1.sha-ad68332
+ * @license Angular v6.0.7+3.sha-ac5b69f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,7 +5232,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+1.sha-ad68332');
+var VERSION = new Version('6.0.7+3.sha-ac5b69f');
 
 /**
  * @license
