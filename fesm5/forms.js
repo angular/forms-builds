766e6785946e3e264c4af277d61fe722e7c5bd10
Jason Aden <jasonaden@google.com>
2018-06-28 01:11:08 +0000

    2880cf9 release: cut the v6.0.7 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+34.sha-97897ab
+ * @license Angular v6.0.7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,7 +5232,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.6+34.sha-97897ab');
+var VERSION = new Version('6.0.7');
 
 /**
  * @license
