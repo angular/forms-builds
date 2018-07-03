27d01219cde8399df5e63f6ca2849a99b01abbe2
GitHub <noreply@github.com>
2018-07-03 20:52:55 +0000

    dd931c7 ci: fix broken build (#24747)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+5.sha-0f1de35
+ * @license Angular v6.0.7+13.sha-dd931c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,7 +5232,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+5.sha-0f1de35');
+var VERSION = new Version('6.0.7+13.sha-dd931c7');
 
 /**
  * @license
