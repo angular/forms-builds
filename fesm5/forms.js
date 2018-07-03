add5ab41fe490b1df52dcd84c82075290159bacd
Miško Hevery <misko@hevery.com>
2018-07-03 16:26:22 +0000

    0922228 fix(core): use addCustomEqualityTester instead of overriding toEqual (#22983)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+27.sha-c94a2c9
+ * @license Angular v6.1.0-beta.3+28.sha-0922228
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5262,7 +5262,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.3+27.sha-c94a2c9');
+var VERSION = new Version('6.1.0-beta.3+28.sha-0922228');
 
 /**
  * @license
