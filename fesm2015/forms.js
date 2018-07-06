7f9814a75e61fdb8f8661d938d077e6e59dfe5a9
Miško Hevery <misko@hevery.com>
2018-07-06 18:27:06 +0000

    fddd2af test: integration test for TS 2.9.x (#24749)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+44.sha-d5a9396
+ * @license Angular v6.1.0-beta.3+45.sha-fddd2af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4885,7 +4885,7 @@ FormBuilder = __decorate([
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+44.sha-d5a9396');
+const VERSION = new Version('6.1.0-beta.3+45.sha-fddd2af');
 
 /**
  * @license
