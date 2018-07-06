5c8da463d1b429aff5c5c1079642099dabe5f1ab
Miško Hevery <misko@hevery.com>
2018-07-06 18:19:30 +0000

    2d1102f docs: add workspace and related cli terms (#24633)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+20.sha-bc2bf18
+ * @license Angular v6.0.7+22.sha-2d1102f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5232,7 +5232,7 @@ var FormBuilder = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+20.sha-bc2bf18');
+var VERSION = new Version('6.0.7+22.sha-2d1102f');
 
 /**
  * @license
