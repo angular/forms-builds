6be851959f08bca3149a3e9c277fbe7a0f9ab16e
Miško Hevery <misko@hevery.com>
2018-07-06 19:09:51 +0000

    f62876b fix(ivy): pipes are pure by default (#24750)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+46.sha-f62876b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4885,7 +4885,7 @@ FormBuilder = __decorate([
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+const VERSION = new Version('6.1.0-beta.3+46.sha-f62876b');
 
 /**
  * @license
