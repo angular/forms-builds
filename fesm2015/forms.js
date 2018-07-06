e2987336f465c077be41bb846c8396dc7138334b
Miško Hevery <misko@hevery.com>
2018-07-06 18:13:34 +0000

    69c8226 docs: add app.module to changed documents (#23876)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+15.sha-ada486a
+ * @license Angular v6.0.7+17.sha-69c8226
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6238,7 +6238,7 @@ FormBuilder.decorators = [
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.7+15.sha-ada486a');
+const VERSION = new Version('6.0.7+17.sha-69c8226');
 
 /**
  * @fileoverview added by tsickle
