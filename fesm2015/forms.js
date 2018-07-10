f823289db36590cafbf81af7372c16d36a4bf2ab
Miško Hevery <misko@hevery.com>
2018-07-10 19:26:44 +0000

    197387d fix(platform-browser): mark Meta and Title services as tree shakable providers (#24815)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+67.sha-1089261
+ * @license Angular v6.1.0-beta.3+68.sha-197387d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+67.sha-1089261');
+const VERSION = new Version('6.1.0-beta.3+68.sha-197387d');
 
 /**
  * @fileoverview added by tsickle
