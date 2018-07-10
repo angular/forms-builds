f117e610d96a0f5f47c9912d01d6ffd62da32a71
Miško Hevery <misko@hevery.com>
2018-07-10 00:10:18 +0000

    3d20c50 fix(ivy): correctly resolve Array  property access (#24664)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+61.sha-3980640
+ * @license Angular v6.1.0-beta.3+64.sha-3d20c50
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __extends, __metadata, __values, __param, __spread } from 'tslib';
+import { __assign, __extends, __values, __decorate, __param, __metadata, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5257,7 +5257,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+61.sha-3980640');
+var VERSION = new Version('6.1.0-beta.3+64.sha-3d20c50');
 
 /**
  * @license
