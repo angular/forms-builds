65be8d1bb6b35fbe748d2c0d895f3b5d3b3d0272
Miško Hevery <misko@hevery.com>
2018-07-10 19:38:04 +0000

    0d5f2d3 fix(compiler-cli): Use typescript to resolve modules for metadata (#22856)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+70.sha-a167bca
+ * @license Angular v6.1.0-beta.3+71.sha-0d5f2d3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5228,7 +5228,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+70.sha-a167bca');
+var VERSION = new Version('6.1.0-beta.3+71.sha-0d5f2d3');
 
 /**
  * @license
