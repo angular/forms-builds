9b22ab3bf1d8bc5f3fbdffef8b8fe27a8f484d98
Miško Hevery <misko@hevery.com>
2018-07-11 02:56:52 +0000

    86d254d fix(router): add ability to recover from malformed url (#23283)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+81.sha-505b54b
+ * @license Angular v6.1.0-beta.3+82.sha-86d254d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+81.sha-505b54b');
+const VERSION = new Version('6.1.0-beta.3+82.sha-86d254d');
 
 /**
  * @fileoverview added by tsickle
