68d360f9a37da9a8a3436494d72bed880509379a
Miško Hevery <misko@hevery.com>
2018-07-11 02:51:20 +0000

    8057664 build: update to latest nodejs bazel rules (#24817)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+78.sha-50fbed8
+ * @license Angular v6.1.0-beta.3+79.sha-8057664
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+78.sha-50fbed8');
+const VERSION = new Version('6.1.0-beta.3+79.sha-8057664');
 
 /**
  * @fileoverview added by tsickle
