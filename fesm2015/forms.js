9e8dc98df98baac89bd24fc02fa6b933805c31e3
GitHub <noreply@github.com>
2018-07-11 00:43:45 +0000

    7d27ecc fix(platform-browser): workaround wrong import path generated by ngc for DOCUMENT (#24830)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+76.sha-03616bc
+ * @license Angular v6.1.0-beta.3+77.sha-7d27ecc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+76.sha-03616bc');
+const VERSION = new Version('6.1.0-beta.3+77.sha-7d27ecc');
 
 /**
  * @fileoverview added by tsickle
