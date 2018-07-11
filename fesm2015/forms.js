4eb75f94b6d20462d215960d3ba824303b164163
Miško Hevery <misko@hevery.com>
2018-07-11 03:02:40 +0000

    b7823e7 docs: unified string chaining (#22735)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+82.sha-86d254d
+ * @license Angular v6.1.0-beta.3+85.sha-b7823e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4880,7 +4880,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+82.sha-86d254d');
+const VERSION = new Version('6.1.0-beta.3+85.sha-b7823e7');
 
 /**
  * @license
