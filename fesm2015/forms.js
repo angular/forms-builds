983689e3a662454238e39a62e0164bbb243f34b4
Miško Hevery <misko@hevery.com>
2018-07-11 02:51:15 +0000

    50fbed8 docs: correct project definition (#24807)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+77.sha-7d27ecc
+ * @license Angular v6.1.0-beta.3+78.sha-50fbed8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6088,7 +6088,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+77.sha-7d27ecc');
+const VERSION = new Version('6.1.0-beta.3+78.sha-50fbed8');
 
 /**
  * @fileoverview added by tsickle
