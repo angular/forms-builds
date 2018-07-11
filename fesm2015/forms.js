3e88e949bd55062444af9f1459ece1b8d25514e0
Miško Hevery <misko@hevery.com>
2018-07-11 22:06:42 +0000

    0b4d85e fix(common): format fractional seconds (#24844)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+88.sha-b9e095a
+ * @license Angular v6.1.0-beta.3+89.sha-0b4d85e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6216,7 +6216,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+88.sha-b9e095a');
+const VERSION = new Version('6.1.0-beta.3+89.sha-0b4d85e');
 
 /**
  * @fileoverview added by tsickle
