d1f8ef62fced67a030feeb9f518fb1fb6006512e
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-11 22:23:33 +0000

    6b3f5dd feat(ivy): implement ngcc ESM5 bits

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+84.sha-da016d0
+ * @license Angular v6.1.0-beta.3+86.sha-6b3f5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4880,7 +4880,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+84.sha-da016d0');
+const VERSION = new Version('6.1.0-beta.3+86.sha-6b3f5dd');
 
 /**
  * @license
