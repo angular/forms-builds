08809c66e0462e62f8e1ec04765b5fc3841318cf
Miško Hevery <misko@hevery.com>
2018-07-11 02:55:28 +0000

    a527c69 fix(common): do not round factional seconds (#24831)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+79.sha-8057664
+ * @license Angular v6.1.0-beta.3+80.sha-a527c69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __extends, __decorate, __metadata, __param, __values, __spread } from 'tslib';
+import { __decorate, __assign, __extends, __values, __metadata, __param, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5257,7 +5257,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+79.sha-8057664');
+var VERSION = new Version('6.1.0-beta.3+80.sha-a527c69');
 
 /**
  * @license
