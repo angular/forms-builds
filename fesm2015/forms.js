cb8bc173ff3f45cc3439d5c7900320c823f0234e
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:43:05 +0000

    ee50ee4 build(bazel): try removing gazelle (#24787)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+95.sha-0399c69
+ * @license Angular v6.1.0-beta.3+98.sha-ee50ee4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6216,7 +6216,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+95.sha-0399c69');
+const VERSION = new Version('6.1.0-beta.3+98.sha-ee50ee4');
 
 /**
  * @fileoverview added by tsickle
