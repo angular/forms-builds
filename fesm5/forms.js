76e948f0eb66a7735a47bdd8ec08375f609fb372
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:44:30 +0000

    ee50ee4 build(bazel): try removing gazelle (#24787)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+95.sha-0399c69
+ * @license Angular v6.1.0-beta.3+98.sha-ee50ee4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __assign, __extends, __values, __metadata, __param, __spread } from 'tslib';
+import { __decorate, __extends, __param, __metadata, __assign, __values, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5396,7 +5396,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+95.sha-0399c69');
+var VERSION = new Version('6.1.0-beta.3+98.sha-ee50ee4');
 
 /**
  * @license
