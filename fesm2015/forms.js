341c22d0bcbf94aec65b46914ec3b3d055e557de
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 21:56:24 +0000

    a663565 build: fix windows scripts (#23121)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+109.sha-85d9c20
+ * @license Angular v6.1.0-beta.3+110.sha-a663565
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+109.sha-85d9c20');
+const VERSION = new Version('6.1.0-beta.3+110.sha-a663565');
 
 /**
  * @fileoverview added by tsickle
