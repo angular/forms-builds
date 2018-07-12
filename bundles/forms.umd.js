6c41aa03b39994298a488fd9d6e70b4d3fc685eb
Matias Niemelä <matias@yearofmoo.com>
2018-07-12 19:16:23 +0000

    328971f feat(router): add urlUpdateStrategy allow updating the browser URL at the beginning of navigation (#24820)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+92.sha-0d6b74d
+ * @license Angular v6.1.0-beta.3+94.sha-328971f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5479,7 +5479,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0-beta.3+92.sha-0d6b74d');
+    var VERSION = new core.Version('6.1.0-beta.3+94.sha-328971f');
 
     /**
      * @license
