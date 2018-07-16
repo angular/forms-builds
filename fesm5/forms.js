327b98e41c91ba2497ef76bf97ff2eff32b0ad9f
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 13:09:10 +0000

    ae04929 feat(ivy): implement initial ngcc package transformer

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-31b4043
+ * @license Angular v6.1.0-beta.3+129.sha-ae04929
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __values, __extends, __assign, __spread } from 'tslib';
+import { __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5384,7 +5384,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+130.sha-31b4043');
+var VERSION = new Version('6.1.0-beta.3+129.sha-ae04929');
 
 /**
  * @license
