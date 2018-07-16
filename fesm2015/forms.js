d8ea8c96073b3593f45dc67fb726a1c1ebcf8915
Matias Niemelä <matias@yearofmoo.com>
2018-07-16 20:33:24 +0000

    d42a662 build: disable IE web worker tests (#24908)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+9.sha-1f163cd
+ * @license Angular v6.0.9+10.sha-d42a662
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6132,7 +6132,7 @@ FormBuilder.decorators = [
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+9.sha-1f163cd');
+const /** @type {?} */ VERSION = new Version('6.0.9+10.sha-d42a662');
 
 /**
  * @fileoverview added by tsickle
