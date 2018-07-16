36a3a29b4eb9f776fcbf1df29b9446da2ca8d600
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 10:07:14 +0000

    31b4043 fixup! refactor(ivy): allow `ImportManager` to have configurable prefix

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+129.sha-acdb672
+ * @license Angular v6.1.0-beta.3+130.sha-31b4043
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+129.sha-acdb672');
+const VERSION = new Version('6.1.0-beta.3+130.sha-31b4043');
 
 /**
  * @fileoverview added by tsickle
