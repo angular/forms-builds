58c586bef01c1835021aa2f646528241a6e34c64
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-16 13:11:47 +0000

    ae04929 feat(ivy): implement initial ngcc package transformer

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-31b4043
+ * @license Angular v6.1.0-beta.3+129.sha-ae04929
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, ɵisObservable, ɵisPromise, EventEmitter, Version, Renderer2, forwardRef, Injector, ɵlooseIdentical, isDevMode, defineInjectable, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵInheritDefinitionFeature } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, EventEmitter, forwardRef, Renderer2, Injector, Version, ɵlooseIdentical, isDevMode, defineInjectable, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵInheritDefinitionFeature, ɵinjectElementRef } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -4562,7 +4562,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.3+130.sha-31b4043');
+const VERSION = new Version('6.1.0-beta.3+129.sha-ae04929');
 
 /**
  * @description
