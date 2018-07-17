79c2f78690ac7ef73f913d59e546b27f0745ecbf
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:04:11 +0000

    ba3eb8b feat(ivy): properly apply class="", [class], [class.foo] and [attr.class] bindings (#24822)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+117.sha-c8ad965
+ * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+117.sha-c8ad965');
+const VERSION = new Version('6.1.0-beta.3+118.sha-ba3eb8b');
 
 /**
  * @fileoverview added by tsickle
