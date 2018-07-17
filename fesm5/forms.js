eccf8e675bf201cdf2371340befef3bc6f164af7
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:21:35 +0000

    0b28732 docs: typos in directives docs (#24665)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+119.sha-06a3398
+ * @license Angular v6.1.0-beta.3+120.sha-0b28732
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __extends, __values, __decorate, __param, __metadata, __spread } from 'tslib';
+import { __decorate, __assign, __param, __metadata, __extends, __values, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5413,7 +5413,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
+var VERSION = new Version('6.1.0-beta.3+120.sha-0b28732');
 
 /**
  * @license
