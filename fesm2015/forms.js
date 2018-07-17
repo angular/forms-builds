3532a16194e4194be229ac5f42ca1d66444334b1
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:20:19 +0000

    0b28732 docs: typos in directives docs (#24665)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+119.sha-06a3398
+ * @license Angular v6.1.0-beta.3+120.sha-0b28732
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+119.sha-06a3398');
+const VERSION = new Version('6.1.0-beta.3+120.sha-0b28732');
 
 /**
  * @fileoverview added by tsickle
