4e7084a02b926acd986c644b8bdddf7997d24e1b
Matias Niemelä <matias@yearofmoo.com>
2018-07-17 21:06:48 +0000

    ba3eb8b feat(ivy): properly apply class="", [class], [class.foo] and [attr.class] bindings (#24822)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.0-beta.3+117.sha-c8ad965
+ * @license Angular v6.1.0-beta.3+118.sha-ba3eb8b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __assign, __extends, __values, __spread } from 'tslib';
-import { InjectionToken, ɵisObservable, ɵisPromise, Version, Renderer2, forwardRef, Injector, defineInjectable, EventEmitter, ɵdefineDirective, ɵdirectiveInject, ɵInheritDefinitionFeature, ɵinjectElementRef, ɵlooseIdentical, ɵdefineNgModule, defineInjector, isDevMode } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, Renderer2, forwardRef, Injector, Version, defineInjectable, EventEmitter, ɵdefineNgModule, defineInjector, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵInheritDefinitionFeature, ɵlooseIdentical, isDevMode } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -4940,7 +4940,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+117.sha-c8ad965');
+var VERSION = new Version('6.1.0-beta.3+118.sha-ba3eb8b');
 
 /**
  * @description
