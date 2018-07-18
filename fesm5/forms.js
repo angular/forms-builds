0058d09fc513b499bbfb7ecb90204e5904885a64
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-18 14:23:31 +0000

    082c994 fixup! feat(ivy): implement esm2015 and esm5 reflection hosts

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-ee37256
+ * @license Angular v6.1.0-beta.3+142.sha-082c994
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __extends, __metadata, __param, __values, __spread } from 'tslib';
+import { __decorate, __assign, __extends, __values, __metadata, __param, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5413,7 +5413,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+130.sha-ee37256');
+var VERSION = new Version('6.1.0-beta.3+142.sha-082c994');
 
 /**
  * @license
