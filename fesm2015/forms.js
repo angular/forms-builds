732e137283ed286e5badb2188741f06332ba544c
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-18 14:22:14 +0000

    082c994 fixup! feat(ivy): implement esm2015 and esm5 reflection hosts

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+130.sha-ee37256
+ * @license Angular v6.1.0-beta.3+142.sha-082c994
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-beta.3+130.sha-ee37256');
+const VERSION = new Version('6.1.0-beta.3+142.sha-082c994');
 
 /**
  * @fileoverview added by tsickle
