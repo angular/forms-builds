3177d97cec5baad6da303c5d8fabaed0ab00b6bb
Victor Berchet <victor@suumit.com>
2018-07-20 20:28:09 +0000

    7960d18 docs: technical review incorporated (#24744)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+41.sha-8620373
+ * @license Angular v6.1.0-rc.3+43.sha-7960d18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __param, __values, __assign, __spread } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5413,7 +5413,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-rc.3+41.sha-8620373');
+var VERSION = new Version('6.1.0-rc.3+43.sha-7960d18');
 
 /**
  * @license
