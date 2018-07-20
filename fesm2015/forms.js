56b457408230d193b99170a68f4e8b5080e36c85
Victor Berchet <victor@suumit.com>
2018-07-20 19:03:05 +0000

    1c533c9 build(docs-infra): add support for examples of type `elements` (#24840)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+3.sha-270176b
+ * @license Angular v6.1.0-rc.3+8.sha-1c533c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+3.sha-270176b');
+const VERSION = new Version('6.1.0-rc.3+8.sha-1c533c9');
 
 /**
  * @fileoverview added by tsickle
