aa9b8e87016deccc3a5d39f33b46ebe3f38814ca
Victor Berchet <victor@suumit.com>
2018-07-20 19:26:51 +0000

    2d38fa1 test(platform-webworker): avoid flakes due to existing `PlatformRef` (#24916)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+11.sha-c438b5e
+ * @license Angular v6.1.0-rc.3+13.sha-2d38fa1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+11.sha-c438b5e');
+const VERSION = new Version('6.1.0-rc.3+13.sha-2d38fa1');
 
 /**
  * @fileoverview added by tsickle
