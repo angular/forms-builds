55100aef8feeb7a504dcb7b3c5d52a9717edf8be
Victor Berchet <victor@suumit.com>
2018-07-23 17:49:55 +0000

    169e9dd feat(ivy): bridge compile instructions to include sanitization helpers (#24938)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+51.sha-edef58f
+ * @license Angular v6.1.0-rc.3+53.sha-169e9dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+51.sha-edef58f');
+const VERSION = new Version('6.1.0-rc.3+53.sha-169e9dd');
 
 /**
  * @fileoverview added by tsickle
