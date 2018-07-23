9ba44115e0a1b904fb11b1e3056e0c3d302c4d74
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-23 07:19:26 +0000

    0650f32 fixup! build: add dependencies to be used by ngcc

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+70.sha-8a7b0e9
+ * @license Angular v6.1.0-rc.3+71.sha-0650f32
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-rc.3+70.sha-8a7b0e9');
+const VERSION = new Version('6.1.0-rc.3+71.sha-0650f32');
 
 /**
  * @license
