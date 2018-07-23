fd832f04f43565499c013fddde464afd4c5293a5
Victor Berchet <victor@suumit.com>
2018-07-23 17:25:57 +0000

    168c2a6 docs: add Truly-UI to resources (#24615) PR Close #24615

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+49.sha-bd576bb
+ * @license Angular v6.1.0-rc.3+48.sha-168c2a6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+49.sha-bd576bb');
+const VERSION = new Version('6.1.0-rc.3+48.sha-168c2a6');
 
 /**
  * @fileoverview added by tsickle
