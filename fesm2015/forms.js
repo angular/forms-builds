b77928bf3636c6e19d45ca90cb7a51f18423926c
Victor Berchet <victor@suumit.com>
2018-07-23 17:23:53 +0000

    ac7658e docs: fix multicasting example in observable guide (#24911)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+36.sha-ac52e1a
+ * @license Angular v6.0.9+34.sha-ac7658e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6132,7 +6132,7 @@ FormBuilder.decorators = [
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+36.sha-ac52e1a');
+const /** @type {?} */ VERSION = new Version('6.0.9+34.sha-ac7658e');
 
 /**
  * @fileoverview added by tsickle
