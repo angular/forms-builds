a2e91f278af91d4ec1d889b0862ca8c0da1e8468
Victor Berchet <victor@suumit.com>
2018-07-24 22:41:05 +0000

    7960d18 docs: technical review incorporated (#24744)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+54.sha-be3cca4
+ * @license Angular v6.1.0-rc.3+43.sha-7960d18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4562,7 +4562,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-rc.3+54.sha-be3cca4');
+const VERSION = new Version('6.1.0-rc.3+43.sha-7960d18');
 
 /**
  * @description
