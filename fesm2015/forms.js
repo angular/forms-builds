d2ec8ce5ffad179e006164fa241f3ef15fa79fba
Victor Berchet <victor@suumit.com>
2018-07-25 18:19:31 +0000

    1ceddb6 fix(ivy): support re-order embedded templates (#24805)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+60.sha-2cb0f68
+ * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+60.sha-2cb0f68');
+const VERSION = new Version('6.1.0-rc.3+64.sha-1ceddb6');
 
 /**
  * @fileoverview added by tsickle
