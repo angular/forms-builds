9b7c383a9343449b8512b83d487800667b86bfb6
Victor Berchet <victor@suumit.com>
2018-07-25 21:10:53 +0000

    e1c6fd5 Revert "feat(core): add support for using async/await with Jasmine" (#25096)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+64.sha-1ceddb6
+ * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+64.sha-1ceddb6');
+const VERSION = new Version('6.1.0-rc.3+75.sha-e1c6fd5');
 
 /**
  * @fileoverview added by tsickle
