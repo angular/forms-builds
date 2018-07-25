7cfd49f3a65f1f029f9bd0a063958787836e068d
Victor Berchet <victor@suumit.com>
2018-07-25 04:56:27 +0000

    c205516 docs: refactor ngmodules example (#25072)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+68.sha-777bd41
+ * @license Angular v6.1.0-rc.3+69.sha-c205516
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+68.sha-777bd41');
+const VERSION = new Version('6.1.0-rc.3+69.sha-c205516');
 
 /**
  * @fileoverview added by tsickle
