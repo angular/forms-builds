1ced7f71e6d09e361950b202d838dd7579a48904
Victor Berchet <victor@suumit.com>
2018-07-25 04:28:04 +0000

    93d325c docs: refactor style guide example 03-06 (#24996)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+44.sha-c7d147e
+ * @license Angular v6.0.9+45.sha-93d325c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6132,7 +6132,7 @@ FormBuilder.decorators = [
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
  */
-const /** @type {?} */ VERSION = new Version('6.0.9+44.sha-c7d147e');
+const /** @type {?} */ VERSION = new Version('6.0.9+45.sha-93d325c');
 
 /**
  * @fileoverview added by tsickle
