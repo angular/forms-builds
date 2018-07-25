73852bc325b88c71bfcef65f88834e42f0e2b171
Victor Berchet <victor@suumit.com>
2018-07-25 18:36:22 +0000

    1e28495 fix(ivy): update compiler with latest runtime for view queries (#25061)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+72.sha-0bcf20c
+ * @license Angular v6.1.0-rc.3+73.sha-1e28495
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+72.sha-0bcf20c');
+const VERSION = new Version('6.1.0-rc.3+73.sha-1e28495');
 
 /**
  * @fileoverview added by tsickle
