9c4c4a59ae7af35b3303b6abcba8c5267b46ac12
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-25 07:14:16 +0000

    70668f7 refactor(ivy): do not deep import from ngtsc into ngcc

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+71.sha-0650f32
+ * @license Angular v6.1.0-rc.3+85.sha-70668f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0-rc.3+71.sha-0650f32');
+const VERSION = new Version('6.1.0-rc.3+85.sha-70668f7');
 
 /**
  * @fileoverview added by tsickle
