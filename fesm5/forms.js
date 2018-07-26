841e32901e3da9f81a74d6843ada97199a38580e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-07-26 17:37:05 +0000

    70174d1 refactor(ivy): do not deep import from ngtsc into ngcc

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+85.sha-70668f7
+ * @license Angular v6.1.0+15.sha-70174d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5413,7 +5413,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-rc.3+85.sha-70668f7');
+var VERSION = new Version('6.1.0+15.sha-70174d1');
 
 /**
  * @license
