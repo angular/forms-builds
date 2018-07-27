0c60a91a43e48d5e433be98ce6a8a42245db5c4c
Igor Minar <igor@angular.io>
2018-07-27 01:06:30 +0000

    3169edd fix(ivy): don't crash in listLazyRoutes() (#25080)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+75.sha-e1c6fd5
+ * @license Angular v6.1.0+8.sha-3169edd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-rc.3+75.sha-e1c6fd5');
+const VERSION = new Version('6.1.0+8.sha-3169edd');
 
 /**
  * @license
