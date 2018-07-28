0671e99b3fbb41cd86337922cd3a684fb8caa078
Igor Minar <igor@angular.io>
2018-07-28 00:57:39 +0000

    a87d44c refactor(ivy): do not deep import from ngtsc into ngcc (#24897)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+25.sha-8e65891
+ * @license Angular v6.1.0+40.sha-a87d44c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4562,7 +4562,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+25.sha-8e65891');
+const VERSION = new Version('6.1.0+40.sha-a87d44c');
 
 /**
  * @description
