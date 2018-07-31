73a546c95270879050384aa86dd9de90eb991218
Igor Minar <igor@angular.io>
2018-07-31 21:27:16 +0000

    76d8eb0 build: make `postinstall` script compatible with Windows (#25232)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+61.sha-4cb1074
+ * @license Angular v6.1.0+60.sha-76d8eb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7,7 +7,7 @@
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 import { ɵgetDOM } from '@angular/platform-browser';
 
 /**
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+61.sha-4cb1074');
+const VERSION = new Version('6.1.0+60.sha-76d8eb0');
 
 /**
  * @license
