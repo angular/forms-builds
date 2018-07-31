cd6f438955e465e14b21ca733bd1c20968adf4f3
Igor Minar <igor@angular.io>
2018-07-31 21:25:32 +0000

    4cb1074 docs(aio): add short description for entryComponents (#21360)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+58.sha-5254d34
+ * @license Angular v6.1.0+61.sha-4cb1074
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2702,31 +2702,31 @@
      * Instantiate a `FormControl`, with an initial value.
      *
      * ```ts
-     * const ctrl = new FormControl('some value');
-     * console.log(ctrl.value);     // 'some value'
+     * const control = new FormControl('some value');
+     * console.log(control.value);     // 'some value'
      *```
      *
      * The following example initializes the control with a form state object. The `value`
      * and `disabled` keys are required in this case.
      *
      * ```ts
-     * const ctrl = new FormControl({ value: 'n/a', disabled: true });
-     * console.log(ctrl.value);     // 'n/a'
-     * console.log(ctrl.status);    // 'DISABLED'
+     * const control = new FormControl({ value: 'n/a', disabled: true });
+     * console.log(control.value);     // 'n/a'
+     * console.log(control.status);    // 'DISABLED'
      * ```
      *
      * The following example initializes the control with a sync validator.
      *
      * ```ts
-     * const ctrl = new FormControl('', Validators.required);
-     * console.log(ctrl.value);      // ''
-     * console.log(ctrl.status);     // 'INVALID'
+     * const control = new FormControl('', Validators.required);
+     * console.log(control.value);      // ''
+     * console.log(control.status);     // 'INVALID'
      * ```
      *
      * The following example initializes the control using an options object.
      *
      * ```ts
-     * const ctrl = new FormControl('', {
+     * const control = new FormControl('', {
      *    validators: Validators.required,
      *    asyncValidators: myAsyncValidator
      * });
@@ -2737,7 +2737,7 @@
      * Set the `updateOn` option to `'blur'` to update on the blur `event`.
      *
      * ```ts
-     * const ctrl = new FormControl('', { updateOn: 'blur' });
+     * const control = new FormControl('', { updateOn: 'blur' });
      * ```
      *
      * ### Configure the control to update on a submit event
@@ -2745,7 +2745,7 @@
      * Set the `updateOn` option to `'submit'` to update on a submit `event`.
      *
      * ```ts
-     * const ctrl = new FormControl('', { updateOn: 'submit' });
+     * const control = new FormControl('', { updateOn: 'submit' });
      * ```
      *
      * ### Reset the control back to an initial value
@@ -2755,7 +2755,7 @@
      * (these are the only two properties that cannot be calculated).
      *
      * ```ts
-     * const ctrl = new FormControl('Nancy');
+     * const control = new FormControl('Nancy');
      *
      * console.log(control.value); // 'Nancy'
      *
@@ -2767,15 +2767,15 @@
      * ### Reset the control back to an initial value and disabled
      *
      * ```
-     * const ctrl = new FormControl('Nancy');
+     * const control = new FormControl('Nancy');
      *
      * console.log(control.value); // 'Nancy'
-     * console.log(this.control.status); // 'DISABLED'
+     * console.log(control.status); // 'VALID'
      *
      * control.reset({ value: 'Drew', disabled: true });
      *
-     * console.log(this.control.value); // 'Drew'
-     * console.log(this.control.status); // 'DISABLED'
+     * console.log(control.value); // 'Drew'
+     * console.log(control.status); // 'DISABLED'
      *
     */
     var FormControl = /** @class */ (function (_super) {
@@ -5502,7 +5502,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.0+58.sha-5254d34');
+    var VERSION = new core.Version('6.1.0+61.sha-4cb1074');
 
     /**
      * @license
