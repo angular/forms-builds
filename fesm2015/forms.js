57203223b4a27ef6684189f3213300a8be6af1f3
Igor Minar <igor@angular.io>
2018-07-31 00:38:33 +0000

    785fb5c docs(aio): add StrongBrew to the trainer list (#24891)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+14.sha-22ebd53
+ * @license Angular v6.1.0+16.sha-785fb5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+14.sha-22ebd53');
+const VERSION = new Version('6.1.0+16.sha-785fb5c');
 
 /**
  * @license
