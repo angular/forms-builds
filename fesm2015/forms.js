2aedb738a85be6d517d8c98cfa02f4d1e06ed1e5
Igor Minar <igor@angular.io>
2018-08-01 20:11:33 +0000

    bafe1a0 build(bazel): fix typo in protractor test target definition (#25235)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+65.sha-c8a4fb1
+ * @license Angular v6.1.0+66.sha-bafe1a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4562,7 +4562,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0+65.sha-c8a4fb1');
+const VERSION = new Version('6.1.0+66.sha-bafe1a0');
 
 /**
  * @description
