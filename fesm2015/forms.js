6319c2b67f4ff081e1c413af9812b7c87e2e0048
Igor Minar <igor@angular.io>
2018-08-01 20:28:05 +0000

    1dd2eaa docs: fix typos and missing word in tutorial (#20764)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+73.sha-166ddaa
+ * @license Angular v6.1.0+80.sha-1dd2eaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+73.sha-166ddaa');
+const VERSION = new Version('6.1.0+80.sha-1dd2eaa');
 
 /**
  * @fileoverview added by tsickle
