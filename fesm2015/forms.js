e44df9368b87dec1938a08894243bbf13f151758
Igor Minar <igor@angular.io>
2018-08-01 22:27:47 +0000

    06af794 test(upgrade): run tests against AngularJS v1.7.x as well (#25231)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+83.sha-bde4402
+ * @license Angular v6.1.0+85.sha-06af794
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+83.sha-bde4402');
+const VERSION = new Version('6.1.0+85.sha-06af794');
 
 /**
  * @fileoverview added by tsickle
