2866af1f534bcd53c9551552eb30b8c4b87ddcc3
Igor Minar <igor@angular.io>
2018-08-01 00:37:11 +0000

    64516da feat(ivy): support inheriting input/output from bare base class (#25094)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+62.sha-aafd502
+ * @license Angular v6.1.0+64.sha-64516da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+62.sha-aafd502');
+const VERSION = new Version('6.1.0+64.sha-64516da');
 
 /**
  * @fileoverview added by tsickle
