b1e976e8c5b01c8211bbbda2141a9deb6866fc95
Igor Minar <igor@angular.io>
2018-08-02 16:15:10 +0000

    9ddf269 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+45.sha-1ef2eae
+ * @license Angular v6.1.0+48.sha-9ddf269
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4940,7 +4940,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+45.sha-1ef2eae');
+var VERSION = new Version('6.1.0+48.sha-9ddf269');
 
 /**
  * @description
