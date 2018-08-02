a007e1e626a4199688bc95300a7306c69e6861da
Igor Minar <igor@angular.io>
2018-08-02 16:36:16 +0000

    7b89711 docs(elements): add section about custom element typings in `elements` guide (#25219)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+90.sha-a224df4
+ * @license Angular v6.1.0+93.sha-7b89711
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+90.sha-a224df4');
+const VERSION = new Version('6.1.0+93.sha-7b89711');
 
 /**
  * @fileoverview added by tsickle
