8b3fd29cdaa090a4f7b6a1de4ec547636e9b23d6
Kara Erickson <karakara@google.com>
2018-08-03 06:54:42 +0000

    728d98d fix(ivy): add bound proerties name to template (#25272)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+4.sha-1fb7111
+ * @license Angular v7.0.0-beta.0+8.sha-728d98d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.0+4.sha-1fb7111');
+const VERSION = new Version('7.0.0-beta.0+8.sha-728d98d');
 
 /**
  * @fileoverview added by tsickle
