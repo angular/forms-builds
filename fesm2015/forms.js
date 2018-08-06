f4ee84ab672a9031897957b9c7734c44d2dd25b5
Kara Erickson <karakara@google.com>
2018-08-06 22:30:11 +0000

    7d006c5 docs(core): fix tree-shakable spelling (#24057)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+28.sha-c158702
+ * @license Angular v7.0.0-beta.0+32.sha-7d006c5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5107,7 +5107,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.0+28.sha-c158702');
+const VERSION = new Version('7.0.0-beta.0+32.sha-7d006c5');
 
 /**
  * @license
