5a997f1a090fc337e2d10f5d36d55863bf42e6aa
Ben Lesh <ben@benlesh.com>
2018-08-10 17:21:55 +0000

    d0f089a docs(aio): add async validation chapter (#25189)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.2+8.sha-fda30cb
+ * @license Angular v6.1.2+10.sha-d0f089a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.2+8.sha-fda30cb');
+const VERSION = new Version('6.1.2+10.sha-d0f089a');
 
 /**
  * @license
