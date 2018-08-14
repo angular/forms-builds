cb12200996418e74a25df45112ef6955a45ceeeb
Ben Lesh <ben@benlesh.com>
2018-08-14 20:18:05 +0000

    0c4209f refactor(ivy): harmonize container and element / elementContainer signatures (#25458)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+34.sha-ecb5dc0
+ * @license Angular v7.0.0-beta.1+37.sha-0c4209f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7,7 +7,7 @@
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { __decorate, __param, __metadata } from 'tslib';
+import { __decorate, __metadata, __param } from 'tslib';
 import { ɵgetDOM } from '@angular/platform-browser';
 
 /**
@@ -5107,7 +5107,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.1+34.sha-ecb5dc0');
+const VERSION = new Version('7.0.0-beta.1+37.sha-0c4209f');
 
 /**
  * @license
