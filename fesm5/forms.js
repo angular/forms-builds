21619042ba91cf0b07680892d999e4cda5e526d5
Ben Lesh <ben@benlesh.com>
2018-08-15 18:10:49 +0000

    1f11039 refactor(ivy): cleanup the public API for core/testing (#25492)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.1+51.sha-01ec5fd
+ * @license Angular v7.0.0-beta.1+52.sha-1f11039
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __values, __assign, __spread } from 'tslib';
-import { InjectionToken, ɵisObservable, ɵisPromise, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵPublicFeature, ɵp, ɵb, ɵInheritDefinitionFeature, defineInjectable, INJECTOR, EventEmitter, ɵNgOnChangesFeature, ɵlooseIdentical, ɵdefineNgModule, defineInjector, isDevMode } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵPublicFeature, EventEmitter, ɵInheritDefinitionFeature, ɵp, ɵb, ɵNgOnChangesFeature, defineInjectable, INJECTOR, ɵlooseIdentical, ɵdefineNgModule, defineInjector, isDevMode } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -5036,7 +5036,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.1+51.sha-01ec5fd');
+var VERSION = new Version('7.0.0-beta.1+52.sha-1f11039');
 
 /**
  * @license
