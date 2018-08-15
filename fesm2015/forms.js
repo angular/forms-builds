c1e205f1ceac9ff4011e9e2dac17b5eee539bd5a
Ben Lesh <ben@benlesh.com>
2018-08-15 00:57:23 +0000

    a0a29fd feat(ivy): Add AOT handling for bare classes with Input and Output decorators (#25367)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+48.sha-b5f354f
+ * @license Angular v7.0.0-beta.1+47.sha-a0a29fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5107,7 +5107,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.1+48.sha-b5f354f');
+const VERSION = new Version('7.0.0-beta.1+47.sha-a0a29fd');
 
 /**
  * @license
