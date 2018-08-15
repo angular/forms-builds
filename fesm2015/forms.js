9684f1068511109d95f118f8e3a9a9ab2de9fb9a
Ben Lesh <ben@benlesh.com>
2018-08-15 00:53:04 +0000

    b5f354f build(bazel): update to rules_typescript 0.16.0 & update to tagged rules_webtesting 0.2.1 (#25433)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+46.sha-26066f2
+ * @license Angular v7.0.0-beta.1+48.sha-b5f354f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+46.sha-26066f2');
+const VERSION = new Version('7.0.0-beta.1+48.sha-b5f354f');
 
 /**
  * @fileoverview added by tsickle
