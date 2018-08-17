4cfa1a985553d6eea6d87190119b9c9c3d5f05fd
Jason Aden <jasonaden@google.com>
2018-08-17 18:02:18 +0000

    f053a3f test(bazel): Remove --no-sandbox for protractor tests (#25362)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+20.sha-07d8d39
+ * @license Angular v7.0.0-beta.2+22.sha-f053a3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6441,7 +6441,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+20.sha-07d8d39');
+const VERSION = new Version('7.0.0-beta.2+22.sha-f053a3f');
 
 /**
  * @fileoverview added by tsickle
