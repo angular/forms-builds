5f97390d172b9fec80e9c2c104b97de303ce8d05
Jason Aden <jasonaden@google.com>
2018-08-17 23:12:43 +0000

    f2aa9c6 refactor(ivy): use generated consts value to set binding index (#25533)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+22.sha-f053a3f
+ * @license Angular v7.0.0-beta.2+26.sha-f2aa9c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+22.sha-f053a3f');
+const VERSION = new Version('7.0.0-beta.2+26.sha-f2aa9c6');
 
 /**
  * @fileoverview added by tsickle
