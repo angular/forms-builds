7ec0db617aa79eb3e8b376cb24e6c167db36846d
Jason Aden <jasonaden@google.com>
2018-08-17 22:41:17 +0000

    d2be3d5 docs: copy edit glossary (#25468)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+24.sha-21d22ce
+ * @license Angular v7.0.0-beta.2+27.sha-d2be3d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.2+24.sha-21d22ce');
+const VERSION = new Version('7.0.0-beta.2+27.sha-d2be3d5');
 
 /**
  * @fileoverview added by tsickle
