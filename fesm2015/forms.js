f7639a8c6f3419395791fa1ff1dfe2a6d531cbeb
Matias Niemelä <matias@yearofmoo.com>
2018-08-22 21:14:44 +0000

    ace4e4f build: remove `NGBUILDS_IO_KEY` now that it is not used any more (#25601)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+22.sha-1fa9790
+ * @license Angular v6.1.3+23.sha-ace4e4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.3+22.sha-1fa9790');
+const VERSION = new Version('6.1.3+23.sha-ace4e4f');
 
 /**
  * @fileoverview added by tsickle
