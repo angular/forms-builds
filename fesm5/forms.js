8ce7afd81dfb887e4ebca26ebad8dde33fbfbc7a
Pete Bacon Darwin <pete@bacondarwin.com>
2018-08-22 20:43:24 +0000

    950639c fix(ivy): ensure factory statements are emitted correctly

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.2+33.sha-73146c1
+ * @license Angular v7.0.0-beta.2+63.sha-950639c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5473,7 +5473,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.2+33.sha-73146c1');
+var VERSION = new Version('7.0.0-beta.2+63.sha-950639c');
 
 /**
  * @license
