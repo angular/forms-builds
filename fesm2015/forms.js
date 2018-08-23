eee65d9e747b4fa26f46d1add94b4f3731def0cd
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:51:37 +0000

    668bfce fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+28.sha-eb1fe19
+ * @license Angular v7.0.0-beta.3+29.sha-668bfce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5107,7 +5107,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.3+28.sha-eb1fe19');
+const VERSION = new Version('7.0.0-beta.3+29.sha-668bfce');
 
 /**
  * @license
