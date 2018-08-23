3a084bbf81110815b738544f7635d232c5a6ea8c
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 02:31:34 +0000

    82f26fe build(bazel): remove workaround no longer needed for module names for ngfactory & ngsummary files (#25604)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4
+ * @license Angular v6.1.4+1.sha-82f26fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.4');
+const VERSION = new Version('6.1.4+1.sha-82f26fe');
 
 /**
  * @fileoverview added by tsickle
