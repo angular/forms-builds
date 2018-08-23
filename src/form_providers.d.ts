6d95803085b1c45daa653ad7b6968069dce48d6d
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 00:33:37 +0000

    61218f5 fix(ivy): ensure factory statements are emitted correctly (#25406)

--- src/form_providers.d.ts
+++ src/form_providers.d.ts
@@ -24,7 +24,7 @@ export declare class FormsModule {
     static withConfig(opts: {
         /** @deprecated as of v6 */ warnOnDeprecatedNgFormSelector?: 'never' | 'once' | 'always';
     }): ModuleWithProviders;
-    static ngModuleDef: i0.ɵNgModuleDef<FormsModule, [typeof i6.NgModel,typeof i7.NgModelGroup,typeof i8.NgForm,typeof i9.NgFormSelectorWarning], never, [typeof i5.InternalFormsSharedModule,typeof i6.NgModel,typeof i7.NgModelGroup,typeof i8.NgForm,typeof i9.NgFormSelectorWarning]>;
+    static ngModuleDef: i0.ɵNgModuleDef<FormsModule, [typeof i6.NgModel, typeof i7.NgModelGroup, typeof i8.NgForm, typeof i9.NgFormSelectorWarning], never, [typeof i5.InternalFormsSharedModule, typeof i6.NgModel, typeof i7.NgModelGroup, typeof i8.NgForm, typeof i9.NgFormSelectorWarning]>;
     static ngInjectorDef: i0.ɵInjectorDef<FormsModule>;
 }
 /**
@@ -35,6 +35,6 @@ export declare class ReactiveFormsModule {
     static withConfig(opts: {
         /** @deprecated as of v6 */ warnOnNgModelWithFormControl: 'never' | 'once' | 'always';
     }): ModuleWithProviders<ReactiveFormsModule>;
-    static ngModuleDef: i0.ɵNgModuleDef<ReactiveFormsModule, [typeof i1.FormControlDirective,typeof i2.FormGroupDirective,typeof i3.FormControlName,typeof i4.FormGroupName,typeof i4.FormArrayName], never, [typeof i5.InternalFormsSharedModule,typeof i1.FormControlDirective,typeof i2.FormGroupDirective,typeof i3.FormControlName,typeof i4.FormGroupName,typeof i4.FormArrayName]>;
+    static ngModuleDef: i0.ɵNgModuleDef<ReactiveFormsModule, [typeof i1.FormControlDirective, typeof i2.FormGroupDirective, typeof i3.FormControlName, typeof i4.FormGroupName, typeof i4.FormArrayName], never, [typeof i5.InternalFormsSharedModule, typeof i1.FormControlDirective, typeof i2.FormGroupDirective, typeof i3.FormControlName, typeof i4.FormGroupName, typeof i4.FormArrayName]>;
     static ngInjectorDef: i0.ɵInjectorDef<ReactiveFormsModule>;
 }
