207cd7864b361dad6f1a8a75742da88e5c6dd547
Jason Aden <jasonaden@google.com>
2018-08-23 00:39:25 +0000

    8de57c9 release: cut the v6.1.4 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.3+23.sha-ace4e4f
+ * @license Angular v6.1.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.3+23.sha-ace4e4f');
+const VERSION = new Version('6.1.4');
 
 /**
  * @fileoverview added by tsickle
