6a46c41c0a735ed96bbcd48a62011e3d9f153d71
Matias Niemelä <matias@yearofmoo.com>
2018-08-27 21:17:46 +0000

    3634575 test(common): fix double instantiation in NgSwitch test (#25632)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+39.sha-9bcd8c2
+ * @license Angular v7.0.0-beta.3+57.sha-3634575
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+39.sha-9bcd8c2');
+const VERSION = new Version('7.0.0-beta.3+57.sha-3634575');
 
 /**
  * @fileoverview added by tsickle
