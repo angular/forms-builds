a3755a1c7aabbead86acb6f123f12977fb873b20
Matias Niemelä <matias@yearofmoo.com>
2018-08-29 22:20:07 +0000

    f394ba0 fix(bazel): Cache fileNameToModuleName lookups (#25731)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.3+75.sha-268cf79
+ * @license Angular v7.0.0-beta.3+76.sha-f394ba0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6442,7 +6442,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+75.sha-268cf79');
+const VERSION = new Version('7.0.0-beta.3+76.sha-f394ba0');
 
 /**
  * @fileoverview added by tsickle
