260fc1b2f1742f3a6f26cfe539d7af2c8e8f727e
Matias Niemelä <matias@yearofmoo.com>
2018-08-29 05:56:14 +0000

    268cf79 docs: release notes for the v6.1.5 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.3+74.sha-9ee29ec
+ * @license Angular v7.0.0-beta.3+75.sha-268cf79
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, EventEmitter, Input, Self, Host, Output, SkipSelf, Injectable, Injector, ɵlooseIdentical, Version, NgModule, isDevMode, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵlistener, ɵloadDirective, ɵPublicFeature, ɵInheritDefinitionFeature, ɵelementProperty, ɵbind, ɵNgOnChangesFeature, defineInjectable, INJECTOR, ɵgetInheritedFactory, ɵdefineNgModule, defineInjector } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Self, EventEmitter, Input, Host, Output, SkipSelf, Injectable, Injector, ɵlooseIdentical, Version, NgModule, isDevMode, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵlistener, ɵloadDirective, ɵPublicFeature, ɵelementProperty, ɵbind, ɵInheritDefinitionFeature, ɵNgOnChangesFeature, defineInjectable, INJECTOR, ɵgetInheritedFactory, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -6442,7 +6442,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.3+74.sha-9ee29ec');
+const VERSION = new Version('7.0.0-beta.3+75.sha-268cf79');
 
 /**
  * @fileoverview added by tsickle
