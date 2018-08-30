1bd5e73d21143f994263a03677a5f0b9d41f8842
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 00:18:03 +0000

    0a3dd87 build: fix bad merge onto patch branch (#25729)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+8.sha-e40519c
+ * @license Angular v6.1.5+2.sha-0a3dd87
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5413,7 +5413,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.4+8.sha-e40519c');
+var VERSION = new Version('6.1.5+2.sha-0a3dd87');
 
 /**
  * @license
