aa9eec8ab29b02bf8fa8764353b002eb2a3332af
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 19:00:32 +0000

    2e09115 docs: edit and organize di guide (#21915)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6
+ * @license Angular v6.1.6+1.sha-2e09115
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6');
+const VERSION = new Version('6.1.6+1.sha-2e09115');
 
 /**
  * @fileoverview added by tsickle
