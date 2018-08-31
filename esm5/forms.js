e8eaa39d3d199984801706ab411bd21200828e68
Miško Hevery <misko@hevery.com>
2018-08-31 17:22:59 +0000

    a065dc2 fix(compiler): remove bogus lines

--- esm5/forms.js
+++ esm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-bc27d4a
+ * @license Angular v5.2.11-a065dc2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7992,7 +7992,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.10-bc27d4a');
+var VERSION = new Version('5.2.11-a065dc2');
 
 /**
  * @fileoverview added by tsickle
