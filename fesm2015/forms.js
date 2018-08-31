80875b1cc22fb2f8b31fdb2557bda822e24eb706
Misko Hevery <misko@hevery.com>
2018-08-31 21:34:20 +0000

    abd29f5 docs(forms): update API reference for reactive and template-driven forms modules (#25687)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.4+31.sha-31349fd
+ * @license Angular v7.0.0-beta.4+35.sha-abd29f5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5107,7 +5107,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.4+31.sha-31349fd');
+const VERSION = new Version('7.0.0-beta.4+35.sha-abd29f5');
 
 /**
  * @license
@@ -5190,10 +5190,20 @@ InternalFormsSharedModule = __decorate([
  */
 var FormsModule_1, ReactiveFormsModule_1;
 /**
- * The ng module for forms.
+ * @description
+ * An `NgModule` that registers the directives and providers for template-driven forms.
  *
+ * @see [Forms Guide](/guide/forms)
  */
 let FormsModule = FormsModule_1 = class FormsModule {
+    /**
+     * @description
+     * Provides options for configuring the template-driven forms module.
+     *
+     * @param opts An object of configuration options
+     * * `warnOnDeprecatedNgFormSelector` Configures when to emit a warning when the deprecated
+     * `ngForm` selector is used.
+     */
     static withConfig(opts) {
         return {
             ngModule: FormsModule_1,
@@ -5209,10 +5219,21 @@ FormsModule = FormsModule_1 = __decorate([
     })
 ], FormsModule);
 /**
- * The ng module for reactive forms.
+ * @description
+ * An `NgModule` that registers the directives and providers for reactive forms.
+ *
+ * @see [Reactive Forms Guide](/guide/reactive-forms)
  *
  */
 let ReactiveFormsModule = ReactiveFormsModule_1 = class ReactiveFormsModule {
+    /**
+     * @description
+     * Provides options for configuring the reactive forms module.
+     *
+     * @param opts An object of configuration options
+     * * `warnOnNgModelWithFormControl` Configures when to emit a warning when an `ngModel`
+     * binding is used with reactive form directives.
+     */
     static withConfig(opts) {
         return {
             ngModule: ReactiveFormsModule_1,
