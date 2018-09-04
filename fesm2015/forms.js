4f7e9fdb6596f71566615cacfbc8bfaac184b501
Misko Hevery <misko@hevery.com>
2018-09-04 20:54:39 +0000

    4fe369e docs: add pwa keyword to service worker page (#25725) PR Close #25725

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+11.sha-d8930bb
+ * @license Angular v6.1.6+12.sha-4fe369e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+11.sha-d8930bb');
+const VERSION = new Version('6.1.6+12.sha-4fe369e');
 
 /**
  * @fileoverview added by tsickle
