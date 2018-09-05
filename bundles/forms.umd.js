7bb197c9acecde90a810319b3f4ef68a9498ddb3
Misko Hevery <misko@hevery.com>
2018-09-05 20:00:54 +0000

    8f8c390 test: remove deprecated Buffer usage in sourcemap test (#25805)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+16.sha-27f0817
+ * @license Angular v6.1.6+20.sha-8f8c390
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5458,7 +5458,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new core.Version('6.1.6+16.sha-27f0817');
+    var VERSION = new core.Version('6.1.6+20.sha-8f8c390');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+16.sha-27f0817
+ * @license Angular v6.1.6+20.sha-8f8c390
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+16.sha-27f0817');
+const VERSION = new Version('6.1.6+20.sha-8f8c390');
 
 /**
  * @fileoverview added by tsickle
