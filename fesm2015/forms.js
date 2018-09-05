6c1a2e436c9e22a5da939ce8873bae400a3b9597
Misko Hevery <misko@hevery.com>
2018-09-05 20:04:30 +0000

    8f8c390 test: remove deprecated Buffer usage in sourcemap test (#25805)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+16.sha-27f0817
+ * @license Angular v6.1.6+20.sha-8f8c390
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -4562,7 +4562,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.6+16.sha-27f0817');
+const VERSION = new Version('6.1.6+20.sha-8f8c390');
 
 /**
  * @description
