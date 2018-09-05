d96757f399b4982ee22434a57e5fc22a951dde23
Misko Hevery <misko@hevery.com>
2018-09-05 20:34:37 +0000

    e72f741 docs: update event page (#25799) docs: change reactiveconf event location PR Close #25799

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+22.sha-82e0676
+ * @license Angular v6.1.6+24.sha-e72f741
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+22.sha-82e0676');
+const VERSION = new Version('6.1.6+24.sha-e72f741');
 
 /**
  * @fileoverview added by tsickle
