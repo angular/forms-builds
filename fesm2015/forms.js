615c1507aaa3e0c764d09eb5884267cf3644cbdd
Misko Hevery <misko@hevery.com>
2018-09-05 20:16:08 +0000

    f0bcfd0 docs: fix showcase address truly-ui (#25757) PR Close #25757

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+20.sha-8f8c390
+ * @license Angular v6.1.6+23.sha-f0bcfd0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.6+20.sha-8f8c390');
+const VERSION = new Version('6.1.6+23.sha-f0bcfd0');
 
 /**
  * @license
