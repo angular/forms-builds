853fe25c1489278ab2378c65dec98d9d36fff3b5
Igor Minar <igor@angular.io>
2018-09-06 23:05:27 +0000

    d6cd041 docs(service-worker): update http-server command (#25845) PR Close #25845

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+1.sha-34ec924
+ * @license Angular v7.0.0-beta.5+4.sha-d6cd041
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6442,7 +6442,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+1.sha-34ec924');
+const VERSION = new Version('7.0.0-beta.5+4.sha-d6cd041');
 
 /**
  * @fileoverview added by tsickle
