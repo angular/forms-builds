efdf5c32dbf2acb1d41164121ac8efbc33058f92
Igor Minar <igor@angular.io>
2018-09-07 22:19:21 +0000

    62be8c2 feat(ivy): allow combined context discovery for components, directives and elements (#25754)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+4.sha-d6cd041
+ * @license Angular v7.0.0-beta.5+6.sha-62be8c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6442,7 +6442,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+4.sha-d6cd041');
+const VERSION = new Version('7.0.0-beta.5+6.sha-62be8c2');
 
 /**
  * @fileoverview added by tsickle
