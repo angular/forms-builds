47c1092a215745dc84eb7886ba0d3fbc4c91fb3c
Igor Minar <igor@angular.io>
2018-09-11 20:45:00 +0000

    55a1ce7 docs: add `disableTypeScriptVersionCheck` documentation (#25537)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+9.sha-8f9aeaa
+ * @license Angular v6.1.7+11.sha-55a1ce7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, ɵisObservable, ɵisPromise, Renderer2, forwardRef, EventEmitter, ɵlooseIdentical, Version, isDevMode, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵInheritDefinitionFeature, ɵdefineNgModule, defineInjector, ɵp, ɵb, defineInjectable, ɵNgOnChangesFeature, INJECTOR } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, Renderer2, forwardRef, Version, EventEmitter, ɵlooseIdentical, isDevMode, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵInheritDefinitionFeature, ɵdefineNgModule, defineInjector, ɵp, ɵb, defineInjectable, INJECTOR, ɵNgOnChangesFeature } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -4562,7 +4562,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.7+9.sha-8f9aeaa');
+const VERSION = new Version('6.1.7+11.sha-55a1ce7');
 
 /**
  * @description
