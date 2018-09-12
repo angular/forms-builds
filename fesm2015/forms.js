fbf00ac9df970255de7f2bed256375b982b86fde
Matias Niemelä <matias@yearofmoo.com>
2018-09-12 18:06:15 +0000

    10a656f refactor(ivy): ensure hello world doesn't pull in context discovery creation code (#25895)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+26.sha-21009b0
+ * @license Angular v7.0.0-beta.5+30.sha-10a656f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6442,7 +6442,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+26.sha-21009b0');
+const VERSION = new Version('7.0.0-beta.5+30.sha-10a656f');
 
 /**
  * @fileoverview added by tsickle
