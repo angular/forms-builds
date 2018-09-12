4c5e13c8d5260f48208aed5916e43ec705ccfb85
Igor Minar <igor@angular.io>
2018-09-12 00:23:11 +0000

    8dc2b11 fix(router): mount correct component if router outlet was not instantiated and if using a route reuse strategy (#25313) (#25314)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+28.sha-f2ba55f
+ * @license Angular v7.0.0-beta.5+29.sha-8dc2b11
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+28.sha-f2ba55f');
+const VERSION = new Version('7.0.0-beta.5+29.sha-8dc2b11');
 
 /**
  * @fileoverview added by tsickle
