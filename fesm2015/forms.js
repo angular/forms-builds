86585b8f80c448a6197f8fa49f0dfcdae6673a9c
Igor Minar <igor@angular.io>
2018-09-12 00:41:43 +0000

    a31cfc5 fix(compiler): Fix look up of entryComponents in AOT Summaries (#24892)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+13.sha-8d7fbb6
+ * @license Angular v6.1.7+12.sha-a31cfc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.7+13.sha-8d7fbb6');
+const VERSION = new Version('6.1.7+12.sha-a31cfc5');
 
 /**
  * @fileoverview added by tsickle
