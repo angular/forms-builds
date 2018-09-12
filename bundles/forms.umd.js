c582a1e788e9f5bda4d53828be477d7f0959826e
Igor Minar <igor@angular.io>
2018-09-12 00:44:30 +0000

    a31cfc5 fix(compiler): Fix look up of entryComponents in AOT Summaries (#24892)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+13.sha-8d7fbb6
+ * @license Angular v6.1.7+12.sha-a31cfc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5014,7 +5014,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new i0.Version('6.1.7+13.sha-8d7fbb6');
+    var VERSION = new i0.Version('6.1.7+12.sha-a31cfc5');
 
     /**
      * @description
