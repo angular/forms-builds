067bc890e6cfd824cf2e88a4ac91897a8a4a117a
Ben Lesh <ben@benlesh.com>
2018-09-13 23:58:15 +0000

    076374b docs: delete old comments from example (#25931)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+12.sha-a31cfc5
+ * @license Angular v6.1.7+15.sha-076374b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6223,7 +6223,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.7+12.sha-a31cfc5');
+const VERSION = new Version('6.1.7+15.sha-076374b');
 
 /**
  * @fileoverview added by tsickle
