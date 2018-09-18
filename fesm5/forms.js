50dd6a7eaff982cd668b3783d688daf29bae0ae4
Alex Eagle <alexeagle@google.com>
2018-09-18 23:38:38 +0000

    48d7f4e fix(bazel): move bazel managed runtime deps for downstream usage (#25690)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.7+21.sha-110c81f
+ * @license Angular v6.1.7+36.sha-48d7f4e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __spread, __assign } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5413,7 +5413,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.7+21.sha-110c81f');
+var VERSION = new Version('6.1.7+36.sha-48d7f4e');
 
 /**
  * @license
