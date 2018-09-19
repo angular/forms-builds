46e09ce6196b452f976b3bafafd33313499706c3
Ben Lesh <ben@benlesh.com>
2018-09-19 22:36:57 +0000

    3988ebf release: cut the v6.1.8 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+39.sha-a41f331
+ * @license Angular v6.1.8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.7+39.sha-a41f331');
+const VERSION = new Version('6.1.8');
 
 /**
  * @license
