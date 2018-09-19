54f0e4acd38e760971fefa97a4c4e873f4828096
Ben Lesh <ben@benlesh.com>
2018-09-19 22:56:12 +0000

    ca1e56d release: cut the v7.0.0-beta.6 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.5+59.sha-d0e710d
+ * @license Angular v7.0.0-beta.6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6298,7 +6298,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.5+59.sha-d0e710d');
+const VERSION = new Version('7.0.0-beta.6');
 
 /**
  * @fileoverview added by tsickle
