dcb561c0325c9d3a9e09e433b52ffc7d26aa4b3a
Ben Lesh <ben@benlesh.com>
2018-09-19 19:22:52 +0000

    a41f331 docs: add ngmodule api doc (#25618)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.7+38.sha-71628f1
+ * @license Angular v6.1.7+39.sha-a41f331
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5036,7 +5036,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.7+38.sha-71628f1');
+const VERSION = new Version('6.1.7+39.sha-a41f331');
 
 /**
  * @license
@@ -5119,8 +5119,9 @@ InternalFormsSharedModule = __decorate([
  */
 var ReactiveFormsModule_1;
 /**
- * @description
- * An `NgModule` that registers the directives and providers for template-driven forms.
+ * Exports the required providers and directives for template-driven forms,
+ * making them available for import by NgModules that import this module.
+ * @see [Forms](guide/forms)
  *
  * @see [Forms Guide](/guide/forms)
  */
@@ -5134,8 +5135,9 @@ FormsModule = __decorate([
     })
 ], FormsModule);
 /**
- * @description
- * An `NgModule` that registers the directives and providers for reactive forms.
+ * Exports the required infrastructure and directives for reactive forms,
+ * making them available for import by NgModules that import this module.
+ * @see [Forms](guide/reactive-forms)
  *
  * @see [Reactive Forms Guide](/guide/reactive-forms)
  *
