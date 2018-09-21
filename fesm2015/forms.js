213a4a8850e935d98c6db60d683083e2e7678a27
Kara Erickson <karakara@google.com>
2018-09-21 18:05:48 +0000

    e42c1b0 ci: correct github robot size check configuration (#26057)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+27.sha-e7ade38
+ * @license Angular v7.0.0-beta.6+28.sha-e42c1b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6462,7 +6462,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+27.sha-e7ade38');
+const VERSION = new Version('7.0.0-beta.6+28.sha-e42c1b0');
 
 /**
  * @fileoverview added by tsickle
