5cc358cf153e93a360c435cb9e3776edd4ca7a30
Kara Erickson <karakara@google.com>
2018-09-24 22:09:22 +0000

    adad170 docs: fix a typo (#26074) PR Close #26074

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+38.sha-a169743
+ * @license Angular v6.1.8+39.sha-adad170
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, ɵisObservable, ɵisPromise, Version, Directive, ElementRef, Inject, Optional, Renderer2, forwardRef, Self, Host, Input, SkipSelf, Injectable, EventEmitter, Injector, Output, ɵlooseIdentical, isDevMode, NgModule, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵp, ɵb, ɵInheritDefinitionFeature, defineInjectable, ɵdefineNgModule, defineInjector, INJECTOR, ɵNgOnChangesFeature } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Self, Host, Input, SkipSelf, Injectable, Version, Injector, EventEmitter, Output, ɵlooseIdentical, isDevMode, NgModule, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵp, ɵb, ɵInheritDefinitionFeature, defineInjectable, INJECTOR, ɵNgOnChangesFeature, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -6236,7 +6236,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.8+38.sha-a169743');
+const VERSION = new Version('6.1.8+39.sha-adad170');
 
 /**
  * \@description
