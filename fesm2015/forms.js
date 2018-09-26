8b72825af7223501b953649eb6e951c810e80a83
Kara Erickson <karakara@google.com>
2018-09-26 23:07:46 +0000

    4cff5b2 release: cut the v7.0.0-beta.7 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.7+18.sha-a01acec
+ * @license Angular v7.0.0-beta.7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6495,7 +6495,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.7+18.sha-a01acec');
+const VERSION = new Version('7.0.0-beta.7');
 
 /**
  * @fileoverview added by tsickle
