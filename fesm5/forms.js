b7bc532ac6111672d4f1fdb3d8d1e0899e8386ab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-09-28 23:18:36 +0000

    2ad1bb4 release: cut the v7.0.0-rc.0 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.7+51.sha-b807106
+ * @license Angular v7.0.0-rc.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __param, __values, __assign, __spread } from 'tslib';
+import { __extends, __decorate, __metadata, __param, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5548,7 +5548,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.7+51.sha-b807106');
+var VERSION = new Version('7.0.0-rc.0');
 
 /**
  * @license
