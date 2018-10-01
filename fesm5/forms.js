42de59fb540ec3f1075fa5ee9c94406e0bc2344c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-01 18:06:59 +0000

    c1bf82a fix(docs-infra): remove unnecessary margin on short descriptions (#25768)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.9+21.sha-a48bf0b
+ * @license Angular v6.1.9+36.sha-c1bf82a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __decorate, __metadata, __param, __values, __assign, __spread } from 'tslib';
+import { __decorate, __metadata, __extends, __param, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5489,7 +5489,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.9+21.sha-a48bf0b');
+var VERSION = new Version('6.1.9+36.sha-c1bf82a');
 
 /**
  * @license
