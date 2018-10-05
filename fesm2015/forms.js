6be89b5bc03addf0223296aeb57bd658db4420d2
Jason Aden <jasonaden@google.com>
2018-10-05 23:10:11 +0000

    ff98003 fix(docs-infra): fix positioning of message for disabled JavaScript (#26198)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+63.sha-b4421bb
+ * @license Angular v6.1.9+65.sha-ff98003
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6277,7 +6277,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+63.sha-b4421bb');
+const VERSION = new Version('6.1.9+65.sha-ff98003');
 
 /**
  * @fileoverview added by tsickle
