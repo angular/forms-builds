4e1a624b92ccbc5376162015800b6031fc4d551e
Jason Aden <jasonaden@google.com>
2018-10-08 21:16:16 +0000

    8a6f372 build(docs-infra): upgrade Angular to 7.0.0-rc.0 and TypeScript to 3.1.1 (#26306)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+73.sha-ed04e99
+ * @license Angular v6.1.9+78.sha-8a6f372
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5112,7 +5112,7 @@ FormBuilder = __decorate([
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.9+73.sha-ed04e99');
+const VERSION = new Version('6.1.9+78.sha-8a6f372');
 
 /**
  * @license
