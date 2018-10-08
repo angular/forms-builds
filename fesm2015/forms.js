f39b3792b5d18f10bdef1da8730a37ecfc854447
Jason Aden <jasonaden@google.com>
2018-10-08 21:12:47 +0000

    9320ec0 build(docs-infra): upgrade Angular to 7.0.0-rc.0 and TypeScript to 3.1.1 (#26202)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.0+65.sha-5a31bde
+ * @license Angular v7.0.0-rc.0+71.sha-9320ec0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.0+65.sha-5a31bde');
+const VERSION = new Version('7.0.0-rc.0+71.sha-9320ec0');
 
 /**
  * @fileoverview added by tsickle
