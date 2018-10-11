bce6405d39ad6efb7932fdc40066240e5403aef8
Miško Hevery <misko@hevery.com>
2018-10-11 21:58:45 +0000

    9973675 docs: add angularmix to events page (#26374)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+20.sha-062fe5c
+ * @license Angular v7.0.0-rc.1+23.sha-9973675
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+20.sha-062fe5c');
+const VERSION = new Version('7.0.0-rc.1+23.sha-9973675');
 
 /**
  * @fileoverview added by tsickle
