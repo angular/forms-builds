7947bd45dc8890a2be4fc953e443c124faa7b2f7
Miško Hevery <misko@hevery.com>
2018-10-11 21:51:47 +0000

    f1e14a3 docs: add angularmix to events page (#26374)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v6.1.10+2.sha-65555fe
+ * @license Angular v6.1.10+4.sha-f1e14a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __values, __assign, __spread } from 'tslib';
-import { InjectionToken, ɵisObservable, ɵisPromise, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵInheritDefinitionFeature, ɵdefineNgModule, defineInjector, ɵp, ɵb, defineInjectable, INJECTOR, EventEmitter, ɵNgOnChangesFeature, ɵlooseIdentical, isDevMode } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵinjectElementRef, ɵL, ɵd, ɵInheritDefinitionFeature, ɵp, ɵb, ɵdefineNgModule, defineInjector, defineInjectable, EventEmitter, INJECTOR, ɵNgOnChangesFeature, ɵlooseIdentical, isDevMode } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -5016,7 +5016,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.10+2.sha-65555fe');
+var VERSION = new Version('6.1.10+4.sha-f1e14a3');
 
 /**
  * @description
