7d4e88a02ece19a6925388460dacc3d6cb1b51fd
Miško Hevery <misko@hevery.com>
2018-10-12 21:40:36 +0000

    bab5b68 fix(docs-infra): prevent unnecessary SideNav scrollbar (#26416)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+35.sha-3daeadd
+ * @license Angular v7.0.0-rc.1+36.sha-bab5b68
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6495,7 +6495,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+35.sha-3daeadd');
+const VERSION = new Version('7.0.0-rc.1+36.sha-bab5b68');
 
 /**
  * @fileoverview added by tsickle
