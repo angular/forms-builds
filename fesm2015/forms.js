5c2e9acb93c07eab257633b4d756ccaebf8f527d
Miško Hevery <misko@hevery.com>
2018-10-12 21:37:28 +0000

    3daeadd docs: visual studio removed from nav and updated to refer to getting started instead of quickstart repo (#26376)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+34.sha-ff15043
+ * @license Angular v7.0.0-rc.1+35.sha-3daeadd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+34.sha-ff15043');
+const VERSION = new Version('7.0.0-rc.1+35.sha-3daeadd');
 
 /**
  * @fileoverview added by tsickle
