597a1fe9d8490ab49921f1e2ba5b8e514c5032ac
Miško Hevery <misko@hevery.com>
2018-10-12 04:32:44 +0000

    071934e fix(upgrade): properly destroy upgraded component elements and descendants (#26209)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+24.sha-9895553
+ * @license Angular v7.0.0-rc.1+27.sha-071934e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+24.sha-9895553');
+const VERSION = new Version('7.0.0-rc.1+27.sha-071934e');
 
 /**
  * @fileoverview added by tsickle
