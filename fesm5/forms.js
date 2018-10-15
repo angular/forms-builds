10a5023c58917c65e16ee4124314c4af03baf92e
Miško Hevery <misko@hevery.com>
2018-10-15 19:01:27 +0000

    2b00c17 docs: minor wording correction. "use" to "user". (#26452) PR Close #26452

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.10+12.sha-81724f5
+ * @license Angular v6.1.10+13.sha-2b00c17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __extends, __param, __values, __assign, __spread } from 'tslib';
+import { __extends, __decorate, __metadata, __param, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5489,7 +5489,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.10+12.sha-81724f5');
+var VERSION = new Version('6.1.10+13.sha-2b00c17');
 
 /**
  * @license
