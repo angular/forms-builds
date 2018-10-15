7b0d84e91b7df30451036dfc0d123ba18686b6d0
Miško Hevery <misko@hevery.com>
2018-10-15 18:56:17 +0000

    9afc9a7 docs: minor wording correction. "use" to "user". (#26452) PR Close #26452

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+43.sha-e76a570
+ * @license Angular v7.0.0-rc.1+42.sha-9afc9a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+43.sha-e76a570');
+const VERSION = new Version('7.0.0-rc.1+42.sha-9afc9a7');
 
 /**
  * @fileoverview added by tsickle
