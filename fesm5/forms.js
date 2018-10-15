4de2f6c6f12cf368837785a382456deba9c96326
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 23:46:27 +0000

    24cf45f build(docs-infra): upgrade cli command docs sources to 301dfa6e5

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+45.sha-6ad85ae
+ * @license Angular v7.0.0-rc.1+45.sha-24cf45f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __values, __spread, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5548,7 +5548,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+45.sha-6ad85ae');
+var VERSION = new Version('7.0.0-rc.1+45.sha-24cf45f');
 
 /**
  * @license
