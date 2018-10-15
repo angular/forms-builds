1cf493eddfe0f542291b9158b159ccd28a678139
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 22:59:29 +0000

    6ad85ae build(docs-infra): upgrade cli command docs sources to 301dfa6e5

---
+++ src/directives/ng_control_status.d.ts
@@ -0,0 +1,62 @@
+import * as i0 from '@angular/core';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { AbstractControlDirective } from './abstract_control_directive';
+import { ControlContainer } from './control_container';
+import { NgControl } from './ng_control';
+export declare class AbstractControlStatus {
+    private _cd;
+    constructor(cd: AbstractControlDirective);
+    readonly ngClassUntouched: boolean;
+    readonly ngClassTouched: boolean;
+    readonly ngClassPristine: boolean;
+    readonly ngClassDirty: boolean;
+    readonly ngClassValid: boolean;
+    readonly ngClassInvalid: boolean;
+    readonly ngClassPending: boolean;
+}
+export declare const ngControlStatusHost: {
+    '[class.ng-untouched]': string;
+    '[class.ng-touched]': string;
+    '[class.ng-pristine]': string;
+    '[class.ng-dirty]': string;
+    '[class.ng-valid]': string;
+    '[class.ng-invalid]': string;
+    '[class.ng-pending]': string;
+};
+/**
+ * Directive automatically applied to Angular form controls that sets CSS classes
+ * based on control status. The following classes are applied as the properties
+ * become true:
+ *
+ * * ng-valid
+ * * ng-invalid
+ * * ng-pending
+ * * ng-pristine
+ * * ng-dirty
+ * * ng-untouched
+ * * ng-touched
+ *
+ * @ngModule FormsModule
+ * @ngModule ReactiveFormsModule
+ */
+export declare class NgControlStatus extends AbstractControlStatus {
+    constructor(cd: NgControl);
+    static ngDirectiveDef: i0.ɵDirectiveDefWithMeta<NgControlStatus, '[formControlName],[ngModel],[formControl]', never, {}, {}, never>;
+}
+/**
+ * Directive automatically applied to Angular form groups that sets CSS classes
+ * based on control status (valid/invalid/dirty/etc).
+ *
+ * @ngModule FormsModule
+ * @ngModule ReactiveFormsModule
+ */
+export declare class NgControlStatusGroup extends AbstractControlStatus {
+    constructor(cd: ControlContainer);
+    static ngDirectiveDef: i0.ɵDirectiveDefWithMeta<NgControlStatusGroup, '[formGroupName],[formArrayName],[ngModelGroup],[formGroup],form:not([ngNoForm]),[ngForm]', never, {}, {}, never>;
+}
