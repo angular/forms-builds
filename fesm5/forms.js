5679eca2266ae602e3c09705762c7f1e77a2fcb8
Miško Hevery <misko@hevery.com>
2018-10-16 22:15:08 +0000

    2326b9c fix(service-worker): clean up caches from old SW versions (#26319)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-41de0e0
+ * @license Angular v7.0.0-rc.1+55.sha-2326b9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __values, __assign, __spread } from 'tslib';
-import { InjectionToken, ɵisObservable, ɵisPromise, ElementRef, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵPublicFeature, ɵInheritDefinitionFeature, ɵlistener, ɵload, ɵelementProperty, ɵbind, EventEmitter, defineInjectable, INJECTOR, ɵNgOnChangesFeature, ɵlooseIdentical, ɵdefineNgModule, defineInjector, isDevMode, ɵgetInheritedFactory } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, ElementRef, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵlistener, ɵload, ɵPublicFeature, ɵInheritDefinitionFeature, ɵelementProperty, ɵbind, defineInjectable, ɵdefineNgModule, defineInjector, EventEmitter, ɵNgOnChangesFeature, INJECTOR, ɵlooseIdentical, ɵgetInheritedFactory, isDevMode } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -5112,7 +5112,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+54.sha-41de0e0');
+var VERSION = new Version('7.0.0-rc.1+55.sha-2326b9c');
 
 /**
  * @license
