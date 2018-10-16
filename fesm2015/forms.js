2431ef3f1ccbaf5b4ca0554d9b76b56292b7e565
Miško Hevery <misko@hevery.com>
2018-10-16 22:15:54 +0000

    ea2cfbb docs: remove setup for local dev and anatomy of setup docs from nav (#26380)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+55.sha-2326b9c
+ * @license Angular v7.0.0-rc.1+56.sha-ea2cfbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6495,7 +6495,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+55.sha-2326b9c');
+const VERSION = new Version('7.0.0-rc.1+56.sha-ea2cfbb');
 
 /**
  * @fileoverview added by tsickle
