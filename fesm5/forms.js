4864c783adb0dbd4a793453aef1d9544dcb53c2e
Miško Hevery <misko@hevery.com>
2018-10-16 00:18:50 +0000

    1c9b065 fix(router): fix regression where navigateByUrl promise didn't resolve on CanLoad failure (#26455)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+49.sha-dd8a851
+ * @license Angular v7.0.0-rc.1+50.sha-1c9b065
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5548,7 +5548,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
+var VERSION = new Version('7.0.0-rc.1+50.sha-1c9b065');
 
 /**
  * @license
