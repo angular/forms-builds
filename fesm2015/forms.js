4c088b75b26226af8fe1c922c60ae02bd631482e
Miško Hevery <misko@hevery.com>
2018-10-16 22:01:23 +0000

    7d82053 docs(ivy): i18n design (#26091)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+50.sha-1c9b065
+ * @license Angular v7.0.0-rc.1+51.sha-7d82053
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6495,7 +6495,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+50.sha-1c9b065');
+const VERSION = new Version('7.0.0-rc.1+51.sha-7d82053');
 
 /**
  * @fileoverview added by tsickle
