096c0f2429849645f9edfad8199a424b528f6d4c
Miško Hevery <misko@hevery.com>
2018-10-16 00:21:09 +0000

    1c9b065 fix(router): fix regression where navigateByUrl promise didn't resolve on CanLoad failure (#26455)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+49.sha-dd8a851
+ * @license Angular v7.0.0-rc.1+50.sha-1c9b065
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6495,7 +6495,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
+const VERSION = new Version('7.0.0-rc.1+50.sha-1c9b065');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-rc.1+49.sha-dd8a851
+ * @license Angular v7.0.0-rc.1+50.sha-1c9b065
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { __extends, __values, __assign, __spread } from 'tslib';
-import { InjectionToken, ɵisObservable, ɵisPromise, ElementRef, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵlistener, ɵload, ɵPublicFeature, ɵInheritDefinitionFeature, ɵelementProperty, ɵbind, defineInjectable, ɵdefineNgModule, defineInjector, EventEmitter, ɵNgOnChangesFeature, ɵlooseIdentical, INJECTOR, ɵgetInheritedFactory, isDevMode } from '@angular/core';
+import { InjectionToken, ɵisObservable, ɵisPromise, ElementRef, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵlistener, ɵload, ɵPublicFeature, ɵInheritDefinitionFeature, ɵelementProperty, ɵbind, defineInjectable, ɵdefineNgModule, defineInjector, INJECTOR, EventEmitter, ɵNgOnChangesFeature, ɵlooseIdentical, ɵgetInheritedFactory, isDevMode } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -5112,7 +5112,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+49.sha-dd8a851');
+var VERSION = new Version('7.0.0-rc.1+50.sha-1c9b065');
 
 /**
  * @license
