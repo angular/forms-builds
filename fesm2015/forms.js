ab6edb85fd8ad161a01c0b34ebcf5b817d4f06d1
Miško Hevery <misko@hevery.com>
2018-10-17 20:40:34 +0000

    f8195e5 fix(ivy): stub TestBed.compileComponents implementation (#26506)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+76.sha-a43b80a
+ * @license Angular v7.0.0-rc.1+85.sha-f8195e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+76.sha-a43b80a');
+const VERSION = new Version('7.0.0-rc.1+85.sha-f8195e5');
 
 /**
  * @fileoverview added by tsickle
