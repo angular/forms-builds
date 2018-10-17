fd6dfbedef475d209399ebdf1ea034740e7dc015
Misko Hevery <misko@hevery.com>
2018-10-17 19:21:39 +0000

    017a087 fix(docs-infra): don't hide contributor links on devices that cannot hover (#26410)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+63.sha-50ccbe7
+ * @license Angular v7.0.0-rc.1+61.sha-017a087
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+63.sha-50ccbe7');
+const VERSION = new Version('7.0.0-rc.1+61.sha-017a087');
 
 /**
  * @fileoverview added by tsickle
