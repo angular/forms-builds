15e178d6c360f0f9506a99f36ee93cb6e431374e
Misko Hevery <misko@hevery.com>
2018-10-17 19:27:24 +0000

    9997ab5 build(docs-infra): update git ref for cli command docs (#26497)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+65.sha-448ab9c
+ * @license Angular v7.0.0-rc.1+72.sha-9997ab5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+65.sha-448ab9c');
+const VERSION = new Version('7.0.0-rc.1+72.sha-9997ab5');
 
 /**
  * @fileoverview added by tsickle
