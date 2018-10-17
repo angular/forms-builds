fab94e691824ea1b365c4e85a7f18380d372eec9
Misko Hevery <misko@hevery.com>
2018-10-17 03:59:27 +0000

    371ffef test(ivy): enable unit tests for @angular/animations (#26470)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+58.sha-0f7d2ca
+ * @license Angular v7.0.0-rc.1+61.sha-371ffef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+58.sha-0f7d2ca');
+const VERSION = new Version('7.0.0-rc.1+61.sha-371ffef');
 
 /**
  * @fileoverview added by tsickle
