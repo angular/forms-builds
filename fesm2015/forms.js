bd589844095cd78ec2b2889ad2a87cd8e2929c5d
Misko Hevery <misko@hevery.com>
2018-10-17 19:23:10 +0000

    d769b44 docs: change links to cli wiki to link to new aio docs (#26489)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+61.sha-017a087
+ * @license Angular v7.0.0-rc.1+64.sha-d769b44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+61.sha-017a087');
+const VERSION = new Version('7.0.0-rc.1+64.sha-d769b44');
 
 /**
  * @fileoverview added by tsickle
