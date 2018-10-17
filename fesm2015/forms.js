f461ecaae6b2f54992c048466b2abae26e218bd6
Miško Hevery <misko@hevery.com>
2018-10-17 20:44:59 +0000

    a4ebf3f docs: fix formatting of PULL_REQUEST_TEMPLATE.md (#26512)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+74.sha-b49b274
+ * @license Angular v7.0.0-rc.1+73.sha-a4ebf3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+74.sha-b49b274');
+const VERSION = new Version('7.0.0-rc.1+73.sha-a4ebf3f');
 
 /**
  * @fileoverview added by tsickle
