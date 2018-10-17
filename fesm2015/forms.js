e6d6828c0d46359acded02c6160a2bdbfb9e3755
Misko Hevery <misko@hevery.com>
2018-10-17 19:24:05 +0000

    017a087 fix(docs-infra): don't hide contributor links on devices that cannot hover (#26410)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+63.sha-50ccbe7
+ * @license Angular v7.0.0-rc.1+61.sha-017a087
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6495,7 +6495,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+63.sha-50ccbe7');
+const VERSION = new Version('7.0.0-rc.1+61.sha-017a087');
 
 /**
  * @fileoverview added by tsickle
