bb07ad86c9e258a0a12a3b894a7006075dbb5f8a
Miško Hevery <misko@hevery.com>
2018-10-18 18:42:58 +0000

    bf656d6 release: cut the v7.0.0 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+78.sha-3b183ce
+ * @license Angular v7.0.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+78.sha-3b183ce');
+const VERSION = new Version('7.0.0');
 
 /**
  * @fileoverview added by tsickle
