ec91f927d913ed37598d8045404c04fad156c8f1
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:15:15 +0000

    aa55d88 docs: forms overview copy edit (#26450)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-rc.1+106.sha-b9bd95b
+ * @license Angular v7.0.0-rc.1+108.sha-aa55d88
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __values, __assign, __spread } from 'tslib';
-import { InjectionToken, ɵisObservable, ɵisPromise, ElementRef, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵlistener, ɵload, ɵPublicFeature, ɵInheritDefinitionFeature, ɵelementProperty, ɵbind, defineInjectable, ɵdefineNgModule, defineInjector, EventEmitter, INJECTOR, ɵNgOnChangesFeature, ɵlooseIdentical, ɵgetInheritedFactory, isDevMode } from '@angular/core';
+import { __extends, __assign, __values, __spread } from 'tslib';
+import { InjectionToken, ɵisObservable, ɵisPromise, ElementRef, Renderer2, forwardRef, Version, ɵdefineDirective, ɵdirectiveInject, ɵlistener, ɵload, ɵPublicFeature, ɵInheritDefinitionFeature, ɵelementProperty, ɵbind, ɵdefineNgModule, defineInjector, defineInjectable, INJECTOR, EventEmitter, ɵNgOnChangesFeature, ɵgetInheritedFactory, ɵlooseIdentical, isDevMode } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ɵgetDOM } from '@angular/platform-browser';
@@ -5112,7 +5112,7 @@ var FormBuilder = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-rc.1+106.sha-b9bd95b');
+var VERSION = new Version('7.0.0-rc.1+108.sha-aa55d88');
 
 /**
  * @license
