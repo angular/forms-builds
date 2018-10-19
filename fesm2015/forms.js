60351d932a6bd891448826036091df1fd125ad45
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:12:14 +0000

    b9bd95b build(docs-infra): break long CLI options onto two lines (#26272)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+103.sha-03bf0d6
+ * @license Angular v7.0.0-rc.1+106.sha-b9bd95b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@ FormBuilder.decorators = [
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+103.sha-03bf0d6');
+const VERSION = new Version('7.0.0-rc.1+106.sha-b9bd95b');
 
 /**
  * @fileoverview added by tsickle
