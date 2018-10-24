9322183f2c07418dafb64f69a20afba6ce216d70
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-24 22:41:07 +0000

    67789f1 release: cut the v7.1.0-beta.0 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+194.sha-6e16a17
+ * @license Angular v7.1.0-beta.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __values, __spread, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.0-rc.1+194.sha-6e16a17');
+var VERSION = new Version('7.1.0-beta.0');
 
 /**
  * @license
