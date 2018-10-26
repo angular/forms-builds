97b2e7b29f69de91a9358e01d3aa89ca0ec31183
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:44:52 +0000

    d2e6d69 docs: add ant design mobile of angular in resources (#26562)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+43.sha-d52d82d
+ * @license Angular v7.1.0-beta.0+42.sha-d2e6d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6389,7 +6389,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+43.sha-d52d82d');
+const VERSION = new Version('7.1.0-beta.0+42.sha-d2e6d69');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+43.sha-d52d82d
+ * @license Angular v7.1.0-beta.0+42.sha-d2e6d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+43.sha-d52d82d');
+var VERSION = new Version('7.1.0-beta.0+42.sha-d2e6d69');
 
 /**
  * @license
