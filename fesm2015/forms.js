7bb070959e24ca840ee26fd870d2a500cce3e7fa
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 23:00:24 +0000

    95993e1 fix(ivy): ensure pipes indices are referenced in styling bindings (#26755)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+42.sha-d2e6d69
+ * @license Angular v7.1.0-beta.0+44.sha-95993e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6389,7 +6389,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.0+42.sha-d2e6d69');
+const VERSION = new Version('7.1.0-beta.0+44.sha-95993e1');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+42.sha-d2e6d69
+ * @license Angular v7.1.0-beta.0+44.sha-95993e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __spread, __assign } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+42.sha-d2e6d69');
+var VERSION = new Version('7.1.0-beta.0+44.sha-95993e1');
 
 /**
  * @license
