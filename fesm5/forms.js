8c1fe587d7e68b2507002c47f3f14923a4a93d51
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:44:09 +0000

    d52d82d fix(ivy): injecting optional TemplateRef on element should not throw (#26664)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+41.sha-da4a28e
+ * @license Angular v7.1.0-beta.0+43.sha-d52d82d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+41.sha-da4a28e');
+var VERSION = new Version('7.1.0-beta.0+43.sha-d52d82d');
 
 /**
  * @license
