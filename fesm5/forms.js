5911a325bfb71bba85847472e33e42a615cfff26
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 23:13:29 +0000

    ede65db fix(ivy): allow root components to inject ViewContainerRef (#26682)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+58.sha-96770e5
+ * @license Angular v7.1.0-beta.0+60.sha-ede65db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __values, __spread, __assign } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+58.sha-96770e5');
+var VERSION = new Version('7.1.0-beta.0+60.sha-ede65db');
 
 /**
  * @license
