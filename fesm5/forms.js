1aa661131ad4e794d812995310727b56038870dd
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:27:35 +0000

    23648b0 docs: configuration file reference (#26484)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+61.sha-91d2368
+ * @license Angular v7.1.0-beta.0+62.sha-23648b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5151,7 +5151,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+61.sha-91d2368');
+var VERSION = new Version('7.1.0-beta.0+62.sha-23648b0');
 
 /**
  * @license
