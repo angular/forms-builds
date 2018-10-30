3016c505a61c0dbf273a4c3b29698c5378cce49b
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:21:09 +0000

    91d2368 docs: update example dependencies to Angular/CLI/Universal V7 releases (#26820)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+59.sha-578e4c7
+ * @license Angular v7.1.0-beta.0+61.sha-91d2368
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+59.sha-578e4c7');
+var VERSION = new Version('7.1.0-beta.0+61.sha-91d2368');
 
 /**
  * @license
