5c6760ff24291ef2828b1e95b626bde800a24a2d
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 19:46:16 +0000

    d042c4a fix(core): Remove static dependency from @angular/core to @angular/compiler (#26734) PR Close #26734

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+74.sha-332394d
+ * @license Angular v7.1.0-beta.0+77.sha-d042c4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5151,7 +5151,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+74.sha-332394d');
+var VERSION = new Version('7.1.0-beta.0+77.sha-d042c4a');
 
 /**
  * @license
