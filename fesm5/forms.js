5914e783b172e84e0d52e826304b1f2b85c73895
Kara Erickson <karakara@google.com>
2018-11-01 22:42:40 +0000

    aed95fd fix(bazel): unknown replay compiler error in windows (#26711)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+39.sha-ea0a996
+ * @license Angular v7.1.0-beta.1+50.sha-aed95fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __param, __extends, __values, __assign, __spread } from 'tslib';
+import { __decorate, __metadata, __param, __extends, __values, __spread, __assign } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+39.sha-ea0a996');
+var VERSION = new Version('7.1.0-beta.1+50.sha-aed95fd');
 
 /**
  * @license
