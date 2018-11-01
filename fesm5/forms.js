a9675f5508e99e00c09659331a9abd4b4e843f53
Kara Erickson <karakara@google.com>
2018-11-01 21:51:56 +0000

    5eb540c docs: Webcomponents activated as of firefox 63 (#26889) PR Close #26889

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+6.sha-7697650
+ * @license Angular v7.0.2+7.sha-5eb540c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+6.sha-7697650');
+var VERSION = new Version('7.0.2+7.sha-5eb540c');
 
 /**
  * @license
