945cfcc4f8693272ea30418216dfff865e39c760
Kara Erickson <karakara@google.com>
2018-11-02 18:04:36 +0000

    5c5b62c ci: remove redundant `start-xvfb` step (#26869)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+11.sha-0c10b57
+ * @license Angular v7.0.2+17.sha-5c5b62c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5587,7 +5587,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2+11.sha-0c10b57');
+var VERSION = new Version('7.0.2+17.sha-5c5b62c');
 
 /**
  * @license
