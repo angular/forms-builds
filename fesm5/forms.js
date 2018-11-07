e72f4cc0a35f9e7b9f27df9f33b7804f4ca61f51
Kara Erickson <karakara@google.com>
2018-11-07 19:32:25 +0000

    9e26216 fix(ivy): compiler should generate bindings to host attrs properly (#26973)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+84.sha-a4398aa
+ * @license Angular v7.1.0-beta.1+85.sha-9e26216
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5763,7 +5763,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+84.sha-a4398aa');
+var VERSION = new Version('7.1.0-beta.1+85.sha-9e26216');
 
 /**
  * @license
