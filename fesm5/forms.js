874f3cf9589a91dfc8bbbb17d116b4a0901b7588
Andrew Kushnir <akushnir@google.com>
2018-11-08 01:15:38 +0000

    15e671e build(bazel): upgrade benchmarks to protractor_web_test_suite for CI without local chrome (#26908)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+1.sha-3567e81
+ * @license Angular v7.1.0-beta.2+2.sha-15e671e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5945,7 +5945,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+1.sha-3567e81');
+var VERSION = new Version('7.1.0-beta.2+2.sha-15e671e');
 
 /**
  * @license
