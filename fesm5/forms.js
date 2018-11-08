a3223706f0fe1214ec756591233533c1cc2751b9
Andrew Kushnir <akushnir@google.com>
2018-11-08 21:35:43 +0000

    1529ecd docs: add Angular subreddit to resources.json (#26976)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+5.sha-0d972d9
+ * @license Angular v7.1.0-beta.2+7.sha-1529ecd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5945,7 +5945,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+5.sha-0d972d9');
+var VERSION = new Version('7.1.0-beta.2+7.sha-1529ecd');
 
 /**
  * @license
