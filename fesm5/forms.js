08e8101ba3ae4cce31261f329741b853e7b7f756
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:23:08 +0000

    78b6f88 fix(ivy): TestBed should use an NgZone (#27015)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+16.sha-83c9bff
+ * @license Angular v7.1.0-beta.2+19.sha-78b6f88
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5945,7 +5945,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+16.sha-83c9bff');
+var VERSION = new Version('7.1.0-beta.2+19.sha-78b6f88');
 
 /**
  * @license
