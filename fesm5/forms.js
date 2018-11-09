42e07aa1326d341cf582a8c9220beb55b584bd12
Andrew Kushnir <akushnir@google.com>
2018-11-09 00:00:38 +0000

    499e303 test(ivy): add global utils to the public_api_guard test (#27008)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+7.sha-1529ecd
+ * @license Angular v7.1.0-beta.2+10.sha-499e303
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -5945,7 +5945,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+7.sha-1529ecd');
+var VERSION = new Version('7.1.0-beta.2+10.sha-499e303');
 
 /**
  * @license
