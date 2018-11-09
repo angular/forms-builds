b33001d9146813d69891abdaedc73e67852f3de8
Andrew Kushnir <akushnir@google.com>
2018-11-09 18:18:41 +0000

    4efb460 docs(router): add RxJS filter pipe operator to example (#25686)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+13.sha-edb50a2
+ * @license Angular v7.1.0-beta.2+15.sha-4efb460
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7222,7 +7222,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-beta.2+13.sha-edb50a2');
+const VERSION = new Version('7.1.0-beta.2+15.sha-4efb460');
 
 /**
  * @fileoverview added by tsickle
