1622923119a900f6f7efd628d5e03496d8790d49
Andrew Kushnir <akushnir@google.com>
2018-11-13 22:17:59 +0000

    4348c47 fix(compiler-cli): add missing tslib dependency (#27063)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+22.sha-2ec0541
+ * @license Angular v7.0.3+23.sha-4348c47
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+22.sha-2ec0541');
+var VERSION = new Version('7.0.3+23.sha-4348c47');
 
 /**
  * @license
