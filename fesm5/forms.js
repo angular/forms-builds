b36ea9f8858efdfff75b77d2ea23b2711f8b3faf
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:26:48 +0000

    7695dbd test(ivy): run common tests with ivy on ci (#27071)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+34.sha-0ada23a
+ * @license Angular v7.1.0-beta.2+37.sha-7695dbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6247,7 +6247,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+34.sha-0ada23a');
+var VERSION = new Version('7.1.0-beta.2+37.sha-7695dbd');
 
 /**
  * @license
