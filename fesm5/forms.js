cf66f0b209660d19d04b572e850036f7ef064c56
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:13:50 +0000

    de7b554 build: avoid writing `\r` in windows (#26888)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+18.sha-bff5c24
+ * @license Angular v7.0.3+20.sha-de7b554
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+18.sha-bff5c24');
+var VERSION = new Version('7.0.3+20.sha-de7b554');
 
 /**
  * @license
