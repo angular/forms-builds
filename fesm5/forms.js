ec2bcbc5b69319ca8c7c98a04016e5c06a8c8d21
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:57:13 +0000

    bf6ac6c feat(router): add pathParamsChange mode for runGuardsAndResolvers (#26861)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.2+51.sha-ce52424
+ * @license Angular v7.1.0-beta.2+48.sha-bf6ac6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __values, __spread, __assign } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6065,7 +6065,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+51.sha-ce52424');
+var VERSION = new Version('7.1.0-beta.2+48.sha-bf6ac6c');
 
 /**
  * @license
