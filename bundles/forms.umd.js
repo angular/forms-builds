58dbe57fc070b621c6e49d8fc7e465f86d4147da
Andrew Kushnir <akushnir@google.com>
2018-11-14 00:09:08 +0000

    a63fd2d feat(ivy): support for i18n & ICU expressions (#26275)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+40.sha-bc652a2
+ * @license Angular v7.1.0-beta.2+43.sha-a63fd2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6154,7 +6154,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.2+40.sha-bc652a2');
+    var VERSION = new core.Version('7.1.0-beta.2+43.sha-a63fd2d');
 
     /**
      * @license
