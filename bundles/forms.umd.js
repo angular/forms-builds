8784ea4a1b7fcbc8db3e98e9b45eaab371835a91
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:57:04 +0000

    ce52424 fix(ivy): implement rootNodes getter on ViewRef (#27095)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+47.sha-3da8233
+ * @license Angular v7.1.0-beta.2+51.sha-ce52424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6154,7 +6154,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0-beta.2+47.sha-3da8233');
+    var VERSION = new core.Version('7.1.0-beta.2+51.sha-ce52424');
 
     /**
      * @license
