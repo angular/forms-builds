699a0c288d21e679ecf0d11d3e8890fddd4dcbed
Andrew Kushnir <akushnir@google.com>
2018-11-14 23:31:22 +0000

    92b0565 release: cut the v7.1.0-rc.0 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+52.sha-4756324
+ * @license Angular v7.1.0-rc.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6247,7 +6247,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+52.sha-4756324');
+var VERSION = new Version('7.1.0-rc.0');
 
 /**
  * @license
