ca90261208e36e3fd7d93f146e0519df1fdfd3db
Misko Hevery <misko@hevery.com>
2018-11-16 20:45:11 +0000

    a02eda5 docs: fix code examples style (#26573)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.4+3.sha-41b5ea1
+ * @license Angular v7.0.4+4.sha-a02eda5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6040,7 +6040,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.4+3.sha-41b5ea1');
+var VERSION = new Version('7.0.4+4.sha-a02eda5');
 
 /**
  * @license
