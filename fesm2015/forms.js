ee28e8d425605f7fb702069061d1a5196964e70c
Misko Hevery <misko@hevery.com>
2018-11-16 05:38:14 +0000

    6ae8a3e docs: add link to workspace config page (#26927)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.4
+ * @license Angular v7.0.4+1.sha-6ae8a3e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6630,7 +6630,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.0.4');
+const VERSION = new Version('7.0.4+1.sha-6ae8a3e');
 
 /**
  * @fileoverview added by tsickle
