41088b42dfd67273a17fbbbd5613c03066509382
Misko Hevery <misko@hevery.com>
2018-11-16 05:45:11 +0000

    6574e61 test(ivy): skip useJit tests with Ivy (#27067)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+3.sha-ee12e72
+ * @license Angular v7.1.0-rc.0+4.sha-6574e61
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6247,7 +6247,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+3.sha-ee12e72');
+var VERSION = new Version('7.1.0-rc.0+4.sha-6574e61');
 
 /**
  * @license
