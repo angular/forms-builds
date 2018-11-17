631022a39d01f96b093835dc35bac71ebad36a50
Misko Hevery <misko@hevery.com>
2018-11-17 00:33:49 +0000

    92e80af feat(ivy): ICU support for Ivy (#26794)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+11.sha-a4934a7
+ * @license Angular v7.1.0-rc.0+12.sha-92e80af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6247,7 +6247,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+11.sha-a4934a7');
+var VERSION = new Version('7.1.0-rc.0+12.sha-92e80af');
 
 /**
  * @license
