e1b21aab61a2c22afba5bea03cc898eb68f5ed32
Misko Hevery <misko@hevery.com>
2018-11-20 19:17:19 +0000

    975c269 docs(forms): language correction (#27177)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+25.sha-2a7210e.with-local-changes
+ * @license Angular v7.1.0-rc.0+27.sha-975c269.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2772,7 +2772,7 @@
         };
         /**
          * Marks the control as `dirty`. A control becomes dirty when
-         * the control's is changed through the UI; compare `markAsTouched`.
+         * the control's value is changed through the UI; compare `markAsTouched`.
          *
          *  @param opts Configuration options that determine how the control propagates changes
          * and emits events after marking is applied.
@@ -3198,6 +3198,7 @@
      *
      * console.log(control.value); // 'Drew'
      * console.log(control.status); // 'DISABLED'
+     * ```
      *
      * @publicApi
      */
@@ -6322,7 +6323,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-rc.0+25.sha-2a7210e.with-local-changes');
+    var VERSION = new i0.Version('7.1.0-rc.0+27.sha-975c269.with-local-changes');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+25.sha-2a7210e.with-local-changes
+ * @license Angular v7.1.0-rc.0+27.sha-975c269.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3225,7 +3225,7 @@ class AbstractControl {
     }
     /**
      * Marks the control as `dirty`. A control becomes dirty when
-     * the control's is changed through the UI; compare `markAsTouched`.
+     * the control's value is changed through the UI; compare `markAsTouched`.
      *
      * @param {?=} opts Configuration options that determine how the control propagates changes
      * and emits events after marking is applied.
@@ -3711,6 +3711,7 @@ class AbstractControl {
  *
  * console.log(control.value); // 'Drew'
  * console.log(control.status); // 'DISABLED'
+ * ```
  *
  * \@publicApi
  */
@@ -7410,7 +7411,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-rc.0+25.sha-2a7210e.with-local-changes');
+const VERSION = new Version('7.1.0-rc.0+27.sha-975c269.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
