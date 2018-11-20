c83c65d94bfb58653fc48394136c48d5ed686446
Misko Hevery <misko@hevery.com>
2018-11-20 20:25:25 +0000

    4390e10 fix(ivy): don't update parent pointers in the ivy switch transform (#27170)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+29.sha-e56c8bf.with-local-changes
+ * @license Angular v7.1.0-rc.0+30.sha-4390e10.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __spread, __assign } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6066,7 +6066,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+29.sha-e56c8bf.with-local-changes');
+var VERSION = new Version('7.1.0-rc.0+30.sha-4390e10.with-local-changes');
 
 /**
  * @license
