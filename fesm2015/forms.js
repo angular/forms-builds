e36e90f42aa769f0dfad2d14771025b68f9cf586
Miško Hevery <misko@hevery.com>
2018-11-20 06:04:08 +0000

    20729b3 fix(ivy): deduplicate imported modules in r3_injector (#27102) PR Close #27102

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+16.sha-81e975a.with-local-changes
+ * @license Angular v7.1.0-rc.0+19.sha-20729b3.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6656,7 +6656,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-rc.0+16.sha-81e975a.with-local-changes');
+const VERSION = new Version('7.1.0-rc.0+19.sha-20729b3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
