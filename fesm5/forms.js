e9329e06a59a7366fc49323f7e0112abee525eb7
Miško Hevery <misko@hevery.com>
2018-11-21 17:44:31 +0000

    8ce59a5 test(ivy): run router tests with ivy on CI (#27195)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+32.sha-a72250b
+ * @license Angular v7.1.0-rc.0+35.sha-8ce59a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __spread, __assign } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6066,7 +6066,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+32.sha-a72250b');
+var VERSION = new Version('7.1.0-rc.0+35.sha-8ce59a5');
 
 /**
  * @license
