59f7b81575771408819de7485224b189087c27bd
Miško Hevery <misko@hevery.com>
2018-11-21 16:10:47 +0000

    a72250b build(bazel): Add Bazel builders (#27141)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+31.sha-026b7e3
+ * @license Angular v7.1.0-rc.0+32.sha-a72250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6249,7 +6249,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+31.sha-026b7e3');
+var VERSION = new Version('7.1.0-rc.0+32.sha-a72250b');
 
 /**
  * @license
