d9cc4b2ed8f3150405a7f0424a6d8a9a192565e1
Jason Aden <jasonaden@google.com>
2018-11-26 20:43:55 +0000

    f45aedc build(docs-infra): upgrade `npm-run-all` to latest version for security (#27274)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+1.sha-dc300c5
+ * @license Angular v7.1.0+11.sha-f45aedc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6657,7 +6657,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+1.sha-dc300c5');
+const VERSION = new Version('7.1.0+11.sha-f45aedc');
 
 /**
  * @fileoverview added by tsickle
