eebaf822900353c0981036a2ef63e08ac63080cb
Jason Aden <jasonaden@google.com>
2018-11-28 22:33:27 +0000

    827e223 docs: replace ChromeNoSandbox by ChromeHeadlessCI (#27304)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+1.sha-14d75d6
+ * @license Angular v7.1.1+2.sha-827e223
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6657,7 +6657,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.1+1.sha-14d75d6');
+const VERSION = new Version('7.1.1+2.sha-827e223');
 
 /**
  * @fileoverview added by tsickle
