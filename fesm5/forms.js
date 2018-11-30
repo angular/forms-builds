17c07709179c18efeaa3d41403274c4ec6104700
Igor Minar <igor@angular.io>
2018-11-30 22:02:56 +0000

    d40af0c feat(router): add a Navigation type available during navigation (#27198)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+75.sha-1b84b11
+ * @license Angular v7.1.0+79.sha-d40af0c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+75.sha-1b84b11');
+var VERSION = new Version('7.1.0+79.sha-d40af0c');
 
 /**
  * @license
