12be5fe362de2f19f41d78740928e58415c42fd4
Igor Minar <igor@angular.io>
2018-11-30 06:18:29 +0000

    4effb89 test(ivy): update //packages/compiler tests for Ivy (#27301)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+63.sha-eb99753
+ * @license Angular v7.1.0+62.sha-4effb89
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+63.sha-eb99753');
+const VERSION = new Version('7.1.0+62.sha-4effb89');
 
 /**
  * @fileoverview added by tsickle
