f79a081f31df51b28332f583ca7917fd276bb6da
Igor Minar <igor@angular.io>
2018-11-30 06:11:36 +0000

    36e7bf1 style: change style css syntax (#27253)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+54.sha-4354fce
+ * @license Angular v7.1.0+56.sha-36e7bf1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6076,7 +6076,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+54.sha-4354fce');
+var VERSION = new Version('7.1.0+56.sha-36e7bf1');
 
 /**
  * @license
