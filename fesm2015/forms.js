08694abdd0283b41e22e8152ce1989c409315ddb
Igor Minar <igor@angular.io>
2018-11-30 22:05:37 +0000

    c8a8dcf build(bazel): update handling of rules_nodejs transitive deps (#27264)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+83.sha-1fa5478
+ * @license Angular v7.1.0+81.sha-c8a8dcf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6795,7 +6795,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+83.sha-1fa5478');
+const VERSION = new Version('7.1.0+81.sha-c8a8dcf');
 
 /**
  * @fileoverview added by tsickle
