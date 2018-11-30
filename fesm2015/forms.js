e9ebb4c7815c01ee4d054f8d3a3c00e8b580b8c3
Igor Minar <igor@angular.io>
2018-11-30 22:47:53 +0000

    64a3461 fix(ivy): support string tokens in dependency injection (#27383)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+84.sha-c96dea2
+ * @license Angular v7.1.0+85.sha-64a3461
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7612,7 +7612,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+84.sha-c96dea2');
+const VERSION = new Version('7.1.0+85.sha-64a3461');
 
 /**
  * @fileoverview added by tsickle
