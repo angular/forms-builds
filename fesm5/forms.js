5eaa984eaa9e31360ea8ddc9d5395cc54c5a0f3f
Igor Minar <igor@angular.io>
2018-11-30 06:09:54 +0000

    2d51579 docs: move Minko to Angular (#27339)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+50.sha-8ceea0f
+ * @license Angular v7.1.0+52.sha-2d51579
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6076,7 +6076,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+50.sha-8ceea0f');
+var VERSION = new Version('7.1.0+52.sha-2d51579');
 
 /**
  * @license
