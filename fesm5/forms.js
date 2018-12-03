a8af2d0059a21c33c85bfdf36d995d3f8aaaae14
Igor Minar <igor@angular.io>
2018-12-03 16:52:35 +0000

    a082f64 fix(ivy): fix style prop instructions to account for zero-based values (#27270)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+105.sha-d847051
+ * @license Angular v7.1.0+106.sha-a082f64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __spread, __assign } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+105.sha-d847051');
+var VERSION = new Version('7.1.0+106.sha-a082f64');
 
 /**
  * @license
