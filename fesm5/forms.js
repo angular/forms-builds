a910c6441941177aceb4244093d083023ad4912b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:06:23 +0000

    7ec05b4 fix(ivy): run initialializers in TestBed on init (#27355)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+110.sha-2a39425
+ * @license Angular v7.1.0+112.sha-7ec05b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+110.sha-2a39425');
+var VERSION = new Version('7.1.0+112.sha-7ec05b4');
 
 /**
  * @license
