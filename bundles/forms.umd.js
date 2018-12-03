8a2cf4c630295a0d9f60f2ebe8018ba1dc1f5f75
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:11:13 +0000

    6552471 fix(ivy): account for `useValue: undefined` providers in module injector (#27035)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+114.sha-75723d5
+ * @license Angular v7.1.0+115.sha-6552471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+114.sha-75723d5');
+    var VERSION = new i0.Version('7.1.0+115.sha-6552471');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+114.sha-75723d5
+ * @license Angular v7.1.0+115.sha-6552471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6518,7 +6518,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+114.sha-75723d5');
+var VERSION = new Version('7.1.0+115.sha-6552471');
 
 /**
  * @license
