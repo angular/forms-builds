0a493480df42e0a700804f1d84163ede69ce8f7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:10:11 +0000

    8eb102a fix(ivy): avoid counting style/class bindings in component/directive `hostBindings` (#27388)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+115.sha-6552471
+ * @license Angular v7.1.0+111.sha-8eb102a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+115.sha-6552471');
+var VERSION = new Version('7.1.0+111.sha-8eb102a');
 
 /**
  * @license
