3d938920159e8757056f3c6a6e873549ce7e6d76
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:11:19 +0000

    8eb102a fix(ivy): avoid counting style/class bindings in component/directive `hostBindings` (#27388)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+115.sha-6552471
+ * @license Angular v7.1.0+111.sha-8eb102a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+115.sha-6552471');
+    var VERSION = new i0.Version('7.1.0+111.sha-8eb102a');
 
     /**
      * @license
