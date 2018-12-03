85b2a31e5b985f38e9c80a9365c2efba1198561b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:09:58 +0000

    6552471 fix(ivy): account for `useValue: undefined` providers in module injector (#27035)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+114.sha-75723d5
+ * @license Angular v7.1.0+115.sha-6552471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6795,7 +6795,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+114.sha-75723d5');
+const VERSION = new Version('7.1.0+115.sha-6552471');
 
 /**
  * @fileoverview added by tsickle
