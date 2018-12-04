f0ffe4463ed543eed9746faed96a92bcdb60eaf4
Igor Minar <igor@angular.io>
2018-12-04 22:38:20 +0000

    d3c08e7 fix(compiler-cli): flatModuleIndex files not generated on windows with multiple input files (#27200)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+117.sha-7ad6b03
+ * @license Angular v7.1.0+121.sha-d3c08e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+117.sha-7ad6b03');
+var VERSION = new Version('7.1.0+121.sha-d3c08e7');
 
 /**
  * @license
