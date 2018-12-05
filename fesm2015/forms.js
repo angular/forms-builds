fd0d2ae41e6960336bb962767d9ed391fc9a7584
Igor Minar <igor@angular.io>
2018-12-05 21:14:26 +0000

    24ebdbd ci: test ts-api-guardian on windows (#27205)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-821fecb
+ * @license Angular v7.1.0+164.sha-24ebdbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6795,7 +6795,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+163.sha-821fecb');
+const VERSION = new Version('7.1.0+164.sha-24ebdbd');
 
 /**
  * @fileoverview added by tsickle
