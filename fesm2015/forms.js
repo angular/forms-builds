56ceaf3ea4973881e4bd3272ba5aee76093df9f8
Igor Minar <igor@angular.io>
2018-12-05 21:51:35 +0000

    4b5cd72 refactor(docs-infra): remove unused custom element (#27250)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+164.sha-24ebdbd
+ * @license Angular v7.1.0+176.sha-4b5cd72
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6795,7 +6795,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+164.sha-24ebdbd');
+const VERSION = new Version('7.1.0+176.sha-4b5cd72');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+164.sha-24ebdbd
+ * @license Angular v7.1.0+176.sha-4b5cd72
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+164.sha-24ebdbd');
+var VERSION = new Version('7.1.0+176.sha-4b5cd72');
 
 /**
  * @license
