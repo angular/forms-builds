ae042aab0647d8e702f2ae71e55e003af4e3869b
Igor Minar <igor@angular.io>
2018-12-05 04:43:50 +0000

    f0b0d64 fix(ivy): adding `projectDef` instructions to all templates where <ng-content> is present (FW-745) (#27384)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+130.sha-b2d6f43
+ * @license Angular v7.1.0+129.sha-f0b0d64
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+130.sha-b2d6f43');
+    var VERSION = new i0.Version('7.1.0+129.sha-f0b0d64');
 
     /**
      * @license
