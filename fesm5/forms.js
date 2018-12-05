9a21d4f0b720a09775ca902831ef4a2b0df1add8
Igor Minar <igor@angular.io>
2018-12-05 07:42:02 +0000

    faca8b1 test(ivy): remove symbol tests which are too big and not useful (#27446)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+148.sha-a3ee089
+ * @license Angular v7.1.0+149.sha-faca8b1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+148.sha-a3ee089');
+var VERSION = new Version('7.1.0+149.sha-faca8b1');
 
 /**
  * @license
