714fdc86eb2cff489d73751a3b252f50c6453e84
Igor Minar <igor@angular.io>
2018-12-06 00:58:42 +0000

    a315ded fix(ivy): fix fixmeIvy invocations in "platform-browser" tests (#27498)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+177.sha-8973dd5
+ * @license Angular v7.1.0+185.sha-a315ded
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6518,7 +6518,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+177.sha-8973dd5');
+var VERSION = new Version('7.1.0+185.sha-a315ded');
 
 /**
  * @license
