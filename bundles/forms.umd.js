765681810816fb124641a9edbdf7f4b6a92a2c1a
Igor Minar <igor@angular.io>
2018-12-06 18:02:13 +0000

    7c52d10 Revert "ci: test ts-api-guardian on windows (#27205)"

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+198.sha-160cace
+ * @license Angular v7.1.0+199.sha-7c52d10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+198.sha-160cace');
+    var VERSION = new i0.Version('7.1.0+199.sha-7c52d10');
 
     /**
      * @license
