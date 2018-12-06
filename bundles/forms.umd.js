543bb475a9269ecce155114da699bf6d2af24ec9
Igor Minar <igor@angular.io>
2018-12-06 05:20:30 +0000

    6740d0d build: remove obsolete gulp build task (#27386)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+185.sha-a315ded
+ * @license Angular v7.1.0+194.sha-6740d0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+185.sha-a315ded');
+    var VERSION = new i0.Version('7.1.0+194.sha-6740d0d');
 
     /**
      * @license
