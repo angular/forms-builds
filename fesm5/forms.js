7f48dfd3d5080aaf6280b4dfc2a6ae9cf70bdf73
Igor Minar <igor@angular.io>
2018-12-06 17:42:08 +0000

    091a504 Revert "fix(router): change processing url tree children condition (#26243)" (#27516)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+194.sha-6740d0d
+ * @license Angular v7.1.0+196.sha-091a504
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6518,7 +6518,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+194.sha-6740d0d');
+var VERSION = new Version('7.1.0+196.sha-091a504');
 
 /**
  * @license
