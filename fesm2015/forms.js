a0b04223f3f2f0edc9b5acbf2e8d17005c8b25d6
Igor Minar <igor@angular.io>
2018-12-06 18:00:54 +0000

    7c52d10 Revert "ci: test ts-api-guardian on windows (#27205)"

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+198.sha-160cace
+ * @license Angular v7.1.0+199.sha-7c52d10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6795,7 +6795,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0+198.sha-160cace');
+const VERSION = new Version('7.1.0+199.sha-7c52d10');
 
 /**
  * @fileoverview added by tsickle
