7729cd1f8b199a09687510b5c4206cd07028dea3
Igor Minar <igor@angular.io>
2018-12-06 22:09:37 +0000

    c71d7b5 fix(ivy): setting up animation properties correctly (FW-643) (#27496)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+205.sha-4da739a
+ * @license Angular v7.1.0+206.sha-c71d7b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6351,7 +6351,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.1.0+205.sha-4da739a');
+    var VERSION = new core.Version('7.1.0+206.sha-c71d7b5');
 
     /**
      * @license
