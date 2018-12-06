795f49b8f23a14a1e34b9176185bc3da47f3e09d
Igor Minar <igor@angular.io>
2018-12-06 22:03:32 +0000

    21d13dd fix(ivy): Added  missing getHostElement to export (#27488)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+202.sha-159ab1c
+ * @license Angular v7.1.0+204.sha-21d13dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+202.sha-159ab1c');
+    var VERSION = new i0.Version('7.1.0+204.sha-21d13dd');
 
     /**
      * @license
