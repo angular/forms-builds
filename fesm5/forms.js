8e1cd56f014bd895fbd0d9e39fe1a5140019bf04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:03:00 +0000

    15ca1bb style: run latest buildifier binary (#27489)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+7.sha-f6a67c9
+ * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6518,7 +6518,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+7.sha-f6a67c9');
+var VERSION = new Version('7.2.0-beta.1+9.sha-15ca1bb');
 
 /**
  * @license
