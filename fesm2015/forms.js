27b95e5e537dd0d2af6172d54aae794cf3b41f35
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:56:12 +0000

    74346e9 style: run latest buildifier binary (#27489)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2
+ * @license Angular v7.1.2+3.sha-74346e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6795,7 +6795,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.2');
+const VERSION = new Version('7.1.2+3.sha-74346e9');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2
+ * @license Angular v7.1.2+3.sha-74346e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.2');
+var VERSION = new Version('7.1.2+3.sha-74346e9');
 
 /**
  * @license
