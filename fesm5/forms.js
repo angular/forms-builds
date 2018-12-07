e7ea3059ecc5766ad00a00290b1d2055b160271a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:20:45 +0000

    84ad0cc build: update source-map-support to get rid of Node's DeprecationWarning (#27538)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+15.sha-255e672
+ * @license Angular v7.2.0-beta.1+14.sha-84ad0cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+15.sha-255e672');
+var VERSION = new Version('7.2.0-beta.1+14.sha-84ad0cc');
 
 /**
  * @license
