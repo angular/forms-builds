f81557f94a6ea53dcb0093c6aa85ce9aaa4c459b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:04:18 +0000

    055dc2d docs: file new issue needs to have opener choose type from template (#27400)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+11.sha-2a35471
+ * @license Angular v7.2.0-beta.1+10.sha-055dc2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7622,7 +7622,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+11.sha-2a35471');
+const VERSION = new Version('7.2.0-beta.1+10.sha-055dc2d');
 
 /**
  * @fileoverview added by tsickle
