2a35e7551bf5f755044025c52ed3031cc069a0e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 18:55:12 +0000

    11996cd docs: remove AngularDart from the version picker (#27550)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+7.sha-8fc763d
+ * @license Angular v7.1.2+8.sha-11996cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.2+7.sha-8fc763d');
+var VERSION = new Version('7.1.2+8.sha-11996cd');
 
 /**
  * @license
