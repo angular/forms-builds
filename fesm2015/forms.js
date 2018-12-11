beb4144b3ae014d614bd95a708c018aed3596620
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:54:15 +0000

    fb6a31c build: exclude ivy commit messages from the release notes (#27532)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+26.sha-3290fc3
+ * @license Angular v7.2.0-beta.1+27.sha-fb6a31c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6805,7 +6805,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+26.sha-3290fc3');
+const VERSION = new Version('7.2.0-beta.1+27.sha-fb6a31c');
 
 /**
  * @fileoverview added by tsickle
