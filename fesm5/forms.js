3f36ced63656264dee3430ff279016c203ecf9d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:42:58 +0000

    01114dc build: fix type-check errors introduced during rules_ts 0.21 (#27586) (#27590)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.2+9.sha-575ef9f
+ * @license Angular v7.1.2+11.sha-01114dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __values, __spread, __assign } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.2+9.sha-575ef9f');
+var VERSION = new Version('7.1.2+11.sha-01114dc');
 
 /**
  * @license
