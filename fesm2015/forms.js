dc27552ffb19fe30847e2cad9eb8710fa02f5477
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:56:18 +0000

    ceb14de fix(ivy): proper slot allocation for pure functions in `hostBindings` function (#27587)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+27.sha-fb6a31c
+ * @license Angular v7.2.0-beta.1+29.sha-ceb14de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6805,7 +6805,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+27.sha-fb6a31c');
+const VERSION = new Version('7.2.0-beta.1+29.sha-ceb14de');
 
 /**
  * @fileoverview added by tsickle
