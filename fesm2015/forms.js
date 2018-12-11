d963fcf924d8bb497407927acb376b07e335baeb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 00:58:10 +0000

    50687e1 build: fix type-check errors introduced during rules_ts 0.21 (#27586)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+22.sha-05cdfb9
+ * @license Angular v7.2.0-beta.1+24.sha-50687e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6805,7 +6805,7 @@ FormBuilder.decorators = [
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+22.sha-05cdfb9');
+const VERSION = new Version('7.2.0-beta.1+24.sha-50687e1');
 
 /**
  * @fileoverview added by tsickle
