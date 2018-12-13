00a41bd353dd37403eacc78afba15be1afa46517
Miško Hevery <misko@hevery.com>
2018-12-13 19:45:19 +0000

    e3a2ca5 fix(docs-infra): fix top menu item clickable area (#27633)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+14.sha-1cc08b4
+ * @license Angular v7.2.0-beta.2+15.sha-e3a2ca5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+14.sha-1cc08b4');
+const VERSION = new Version('7.2.0-beta.2+15.sha-e3a2ca5');
 
 /**
  * @fileoverview added by tsickle
