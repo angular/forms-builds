0f59e2ede8c0e3af010e684eb35f288b5fe410fa
Miško Hevery <misko@hevery.com>
2018-12-13 19:47:36 +0000

    8313ffc fix(bazel): Load http_archive and rules_nodejs dependencies (#27609)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+13.sha-e318081
+ * @license Angular v7.2.0-beta.2+18.sha-8313ffc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __values, __spread, __assign } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+13.sha-e318081');
+var VERSION = new Version('7.2.0-beta.2+18.sha-8313ffc');
 
 /**
  * @license
