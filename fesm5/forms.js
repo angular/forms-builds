a7c30c32c60f1814b2437351603df85449242e38
Miško Hevery <misko@hevery.com>
2018-12-13 19:45:11 +0000

    1cc08b4 fix(bazel): fix major/minor semver check between @angular/bazel npm packager version and angular bazel repo version (#27635)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+12.sha-f00af3e
+ * @license Angular v7.2.0-beta.2+14.sha-1cc08b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+12.sha-f00af3e');
+var VERSION = new Version('7.2.0-beta.2+14.sha-1cc08b4');
 
 /**
  * @license
