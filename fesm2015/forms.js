78809ea481484adbd23475fc6d643fd3532e2da9
Miško Hevery <misko@hevery.com>
2018-12-13 19:47:16 +0000

    e318081 ci: remove packages/bazel from compiler-cli group (#27641)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+17.sha-ffe1b4d
+ * @license Angular v7.2.0-beta.2+13.sha-e318081
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+17.sha-ffe1b4d');
+const VERSION = new Version('7.2.0-beta.2+13.sha-e318081');
 
 /**
  * @fileoverview added by tsickle
