327f7f22a916f56dcfb53a6fbfa54e7b7cf88c90
Miško Hevery <misko@hevery.com>
2018-12-13 19:44:32 +0000

    f00af3e ci: exclude unnedeed files and directories from angular-robot g3sync check (#27653)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+10.sha-7fabe44
+ * @license Angular v7.2.0-beta.2+12.sha-f00af3e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+10.sha-7fabe44');
+const VERSION = new Version('7.2.0-beta.2+12.sha-f00af3e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+10.sha-7fabe44
+ * @license Angular v7.2.0-beta.2+12.sha-f00af3e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+10.sha-7fabe44');
+var VERSION = new Version('7.2.0-beta.2+12.sha-f00af3e');
 
 /**
  * @license
