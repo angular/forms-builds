8c2fe546dd515ae613c34804cbc158224ef046b9
Miško Hevery <misko@hevery.com>
2018-12-13 19:38:05 +0000

    89ace1a fix(bazel): Load http_archive and rules_nodejs dependencies (#27609)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+11.sha-01102cb
+ * @license Angular v7.1.3+16.sha-89ace1a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+11.sha-01102cb');
+var VERSION = new Version('7.1.3+16.sha-89ace1a');
 
 /**
  * @license
