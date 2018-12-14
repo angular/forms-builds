e229d170e9f293a3bbc57fb2c09eee35b3fa1be9
Miško Hevery <misko@hevery.com>
2018-12-14 23:48:04 +0000

    fa3af8b fix(common): KeyValuePipe should return empty array for empty objects (#27258)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+24.sha-0d8528b
+ * @license Angular v7.1.3+26.sha-fa3af8b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values, __spread, __assign } from 'tslib';
+import { __decorate, __param, __metadata, __extends, __values, __assign, __spread } from 'tslib';
 import { InjectionToken, ɵisObservable, ɵisPromise, Directive, ElementRef, Renderer2, forwardRef, Inject, Optional, Injectable, Injector, Input, Host, ɵlooseIdentical, isDevMode, Self, EventEmitter, SkipSelf, Output, Version, NgModule } from '@angular/core';
 import { forkJoin, from } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+24.sha-0d8528b');
+var VERSION = new Version('7.1.3+26.sha-fa3af8b');
 
 /**
  * @license
