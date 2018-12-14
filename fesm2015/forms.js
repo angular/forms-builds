f69d1702b2858d8ad2f1acb2d21ea6098f351595
Miško Hevery <misko@hevery.com>
2018-12-14 18:54:03 +0000

    fe8c6b0 docs(common): Fix typo in KeyValuePipe docs (#27580) PR Close #27580

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+20.sha-f5e7208
+ * @license Angular v7.1.3+22.sha-fe8c6b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6814,7 +6814,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.1.3+20.sha-f5e7208');
+const VERSION = new Version('7.1.3+22.sha-fe8c6b0');
 
 /**
  * @fileoverview added by tsickle
