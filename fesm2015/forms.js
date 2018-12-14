06df9b6d50e81f0a28d43cb0203b05de6abd7a3c
Miško Hevery <misko@hevery.com>
2018-12-14 19:07:08 +0000

    c6ae729 build: re-enable saucelabs non-verbose logging (#27657)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+37.sha-a8ebc83
+ * @license Angular v7.2.0-beta.2+30.sha-c6ae729
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+37.sha-a8ebc83');
+const VERSION = new Version('7.2.0-beta.2+30.sha-c6ae729');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+37.sha-a8ebc83
+ * @license Angular v7.2.0-beta.2+30.sha-c6ae729
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+37.sha-a8ebc83');
+var VERSION = new Version('7.2.0-beta.2+30.sha-c6ae729');
 
 /**
  * @license
