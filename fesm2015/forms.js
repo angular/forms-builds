e97d3629886d5f85de714a33ea7782a9002f92ca
Miško Hevery <misko@hevery.com>
2018-12-14 19:03:00 +0000

    d32939d build: restrict visibility of npm_package targets (#27611)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+28.sha-3680aef
+ * @license Angular v7.2.0-beta.2+34.sha-d32939d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+28.sha-3680aef');
+const VERSION = new Version('7.2.0-beta.2+34.sha-d32939d');
 
 /**
  * @fileoverview added by tsickle
