7108172e7b7c4f2af7dccbce376e6980f78e0cce
Miško Hevery <misko@hevery.com>
2018-12-14 19:04:30 +0000

    a954345 fix(ivy): prevent invalid forward references in setClassMetadata call (#27561)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+33.sha-d528e30
+ * @license Angular v7.2.0-beta.2+38.sha-a954345
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+33.sha-d528e30');
+const VERSION = new Version('7.2.0-beta.2+38.sha-a954345');
 
 /**
  * @fileoverview added by tsickle
