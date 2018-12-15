7bc5852caa260d173eadc6e413ee8b3399721f96
Miško Hevery <misko@hevery.com>
2018-12-15 00:07:42 +0000

    f8096d4 docs(animations): rename animateChild() to animate() (#26184)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+51.sha-7dab458
+ * @license Angular v7.2.0-beta.2+52.sha-f8096d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+51.sha-7dab458');
+const VERSION = new Version('7.2.0-beta.2+52.sha-f8096d4');
 
 /**
  * @fileoverview added by tsickle
