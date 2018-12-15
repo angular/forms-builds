8711b033793b80d4b5f02a842a58c06e666bc1d9
Miško Hevery <misko@hevery.com>
2018-12-15 00:06:43 +0000

    f1c9d6a fix(animations): do not truncate decimals for delay (#24455)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+48.sha-b00aeef
+ * @license Angular v7.2.0-beta.2+47.sha-f1c9d6a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+48.sha-b00aeef');
+var VERSION = new Version('7.2.0-beta.2+47.sha-f1c9d6a');
 
 /**
  * @license
