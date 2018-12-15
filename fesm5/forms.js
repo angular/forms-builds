b3b3944789e0f56f694c8c246870a0ac37863187
Miško Hevery <misko@hevery.com>
2018-12-15 00:03:34 +0000

    b39efdd fix(common): KeyValuePipe should return empty array for empty objects (#27258)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+42.sha-0b78f54
+ * @license Angular v7.2.0-beta.2+44.sha-b39efdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+42.sha-0b78f54');
+var VERSION = new Version('7.2.0-beta.2+44.sha-b39efdd');
 
 /**
  * @license
