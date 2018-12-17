6843ddb26115f6fbb282f9206fe3b168291230ff
Miško Hevery <misko@hevery.com>
2018-12-17 18:16:09 +0000

    a726338 docs: Add Smart UI to UI Components section in resources (#27515)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+62.sha-94f17e9
+ * @license Angular v7.2.0-beta.2+55.sha-a726338
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+62.sha-94f17e9');
+const VERSION = new Version('7.2.0-beta.2+55.sha-a726338');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+62.sha-94f17e9
+ * @license Angular v7.2.0-beta.2+55.sha-a726338
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+62.sha-94f17e9');
+var VERSION = new Version('7.2.0-beta.2+55.sha-a726338');
 
 /**
  * @license
