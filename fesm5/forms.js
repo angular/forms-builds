216b008a7224713ce168e207c293f78efd6a0897
Miško Hevery <misko@hevery.com>
2018-12-17 18:53:18 +0000

    d132bae test(ivy): add more test for content projection with ngIf / ngFor (#27705)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+61.sha-44ec66d
+ * @license Angular v7.2.0-beta.2+63.sha-d132bae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6524,7 +6524,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+61.sha-44ec66d');
+var VERSION = new Version('7.2.0-beta.2+63.sha-d132bae');
 
 /**
  * @license
