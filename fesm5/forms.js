94d40fddd82778c99285a3d95dcfc38342ca1f0f
Miško Hevery <misko@hevery.com>
2018-12-17 18:11:56 +0000

    ea10a3a test(ivy): enable all test for @angular/common (#27700)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+52.sha-f8096d4
+ * @license Angular v7.2.0-beta.2+53.sha-ea10a3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+52.sha-f8096d4');
+var VERSION = new Version('7.2.0-beta.2+53.sha-ea10a3a');
 
 /**
  * @license
