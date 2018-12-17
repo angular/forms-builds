8580074d12124f2f578e8d4afee5d9638d6450bd
Miško Hevery <misko@hevery.com>
2018-12-17 18:16:25 +0000

    44ec66d test(ivy): enable running cli-hello-world-ivy integration test suite (#27438)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+55.sha-a726338
+ * @license Angular v7.2.0-beta.2+61.sha-44ec66d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6824,7 +6824,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-beta.2+55.sha-a726338');
+const VERSION = new Version('7.2.0-beta.2+61.sha-44ec66d');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+55.sha-a726338
+ * @license Angular v7.2.0-beta.2+61.sha-44ec66d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+55.sha-a726338');
+var VERSION = new Version('7.2.0-beta.2+61.sha-44ec66d');
 
 /**
  * @license
