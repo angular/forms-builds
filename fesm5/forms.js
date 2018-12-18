6e04d62a0ec04fc2f3c7f80856b1cb7908044a19
Miško Hevery <misko@hevery.com>
2018-12-18 18:02:29 +0000

    8fdd702 docs: Fixed typo (#27669) PR Close #27669

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-d766ad0
+ * @license Angular v7.2.0-beta.2+79.sha-8fdd702
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6664,7 +6664,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+77.sha-d766ad0');
+var VERSION = new Version('7.2.0-beta.2+79.sha-8fdd702');
 
 /**
  * @license
