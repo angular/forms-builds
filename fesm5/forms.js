d8bc9a525079490e593959339a0df18e379358fd
Miško Hevery <misko@hevery.com>
2018-12-18 01:17:18 +0000

    87d7b74 build: improve error message for upgrade test helper (#27706)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+67.sha-85866de
+ * @license Angular v7.2.0-beta.2+68.sha-87d7b74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6268,7 +6268,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+67.sha-85866de');
+var VERSION = new Version('7.2.0-beta.2+68.sha-87d7b74');
 
 /**
  * @license
