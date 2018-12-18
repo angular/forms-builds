3cfece63439d61f19bfd1a283fbefba56b8f95a3
Miško Hevery <misko@hevery.com>
2018-12-18 01:11:53 +0000

    183f278 fix(bazel): Set module_name and enable ng test (#27715)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+32.sha-d1a14f6
+ * @license Angular v7.1.3+33.sha-183f278
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6262,7 +6262,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+32.sha-d1a14f6');
+var VERSION = new Version('7.1.3+33.sha-183f278');
 
 /**
  * @license
