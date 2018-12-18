94a0423bc7df9e499c1d3c13f7c00ff12be9e85a
Miško Hevery <misko@hevery.com>
2018-12-18 21:45:19 +0000

    b04bc5d test(ivy): re-enable passing upgrade tests (#27736)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+82.sha-1c93afe
+ * @license Angular v7.2.0-beta.2+83.sha-b04bc5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6664,7 +6664,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+82.sha-1c93afe');
+var VERSION = new Version('7.2.0-beta.2+83.sha-b04bc5d');
 
 /**
  * @license
