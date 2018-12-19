6baf39d4042e5a40d471fc2ab4efb83b8a0f461e
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:43:30 +0000

    8f8572f fix(ivy): @Host should behave as in View Engine (#27646)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+2.sha-e8f7241
+ * @license Angular v7.2.0-rc.0+3.sha-8f8572f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6408,7 +6408,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+2.sha-e8f7241');
+var VERSION = new Version('7.2.0-rc.0+3.sha-8f8572f');
 
 /**
  * @license
