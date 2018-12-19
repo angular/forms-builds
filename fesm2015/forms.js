82858c1731f08761b75b4ad13888b2ffdad30680
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:40:59 +0000

    a3debf6 Revert "build: update gulp-clang-format dependency (#27712)" (#27759)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0
+ * @license Angular v7.2.0-rc.0+2.sha-a3debf6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6955,7 +6955,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0');
+const VERSION = new Version('7.2.0-rc.0+2.sha-a3debf6');
 
 /**
  * @fileoverview added by tsickle
