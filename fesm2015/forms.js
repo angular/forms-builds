14e4dd4bc7cdc4d9d295c7c3930540901dbbbad7
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:39:09 +0000

    a20b2f7 fix(ivy): process creation mode deeply before running update mode (#27744)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0
+ * @license Angular v7.2.0-rc.0+1.sha-a20b2f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6955,7 +6955,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0');
+const VERSION = new Version('7.2.0-rc.0+1.sha-a20b2f7');
 
 /**
  * @fileoverview added by tsickle
