ec54093de0bed7ccb8760ea6067bb1128e172346
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:47:10 +0000

    07ada7f fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+13.sha-880e8a5
+ * @license Angular v7.2.0-rc.0+14.sha-07ada7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6955,7 +6955,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
+const VERSION = new Version('7.2.0-rc.0+14.sha-07ada7f');
 
 /**
  * @fileoverview added by tsickle
