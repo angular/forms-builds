3c4b5d6fc5148f5b346fcb6be8cdb386488d196a
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:42:40 +0000

    880e8a5 refactor: fix broken formatting

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+11.sha-a833b98
+ * @license Angular v7.2.0-rc.0+13.sha-880e8a5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6664,7 +6664,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+11.sha-a833b98');
+var VERSION = new Version('7.2.0-rc.0+13.sha-880e8a5');
 
 /**
  * @license
