97d9be1e43d17bbd17d46ccd5a9d6ed5c16e7f59
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:53:17 +0000

    50a91ba docs(upgrade): add gotchas/tips/example for multiple downgraded modules (#27217)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+14.sha-07ada7f
+ * @license Angular v7.2.0-rc.0+19.sha-50a91ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6955,7 +6955,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+14.sha-07ada7f');
+const VERSION = new Version('7.2.0-rc.0+19.sha-50a91ba');
 
 /**
  * @fileoverview added by tsickle
