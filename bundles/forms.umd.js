0b9e3681ce55e921b7f4009a6ea50fab006076ac
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 22:06:43 +0000

    4b67b0a fix(ivy): adding TestBed.overrideProvider support (#27693)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+22.sha-509aa61
+ * @license Angular v7.2.0-rc.0+25.sha-4b67b0a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -585,8 +585,12 @@
         /**
          * @description
          * Validator that requires the control's value to match a regex pattern. This validator is also
-         * provided
-         * by default if you use the HTML5 `pattern` attribute.
+         * provided by default if you use the HTML5 `pattern` attribute.
+         *
+         * Note that if a Regexp is provided, the Regexp is used as is to test the values. On the other
+         * hand, if a string is passed, the `^` character is prepended and the `$` character is
+         * appended to the provided string (if not already present), and the resulting regular
+         * expression is used to test the values.
          *
          * @usageNotes
          *
@@ -6738,7 +6742,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+22.sha-509aa61');
+    var VERSION = new i0.Version('7.2.0-rc.0+25.sha-4b67b0a');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+22.sha-509aa61
+ * @license Angular v7.2.0-rc.0+25.sha-4b67b0a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -511,8 +511,12 @@ var Validators = /** @class */ (function () {
     /**
      * @description
      * Validator that requires the control's value to match a regex pattern. This validator is also
-     * provided
-     * by default if you use the HTML5 `pattern` attribute.
+     * provided by default if you use the HTML5 `pattern` attribute.
+     *
+     * Note that if a Regexp is provided, the Regexp is used as is to test the values. On the other
+     * hand, if a string is passed, the `^` character is prepended and the `$` character is
+     * appended to the provided string (if not already present), and the resulting regular
+     * expression is used to test the values.
      *
      * @usageNotes
      *
@@ -6664,7 +6668,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+22.sha-509aa61');
+var VERSION = new Version('7.2.0-rc.0+25.sha-4b67b0a');
 
 /**
  * @license
