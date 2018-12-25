348598c8f0d1a1e56598b57c7e3167b53194f263
Matias Niemelä <matias@yearofmoo.com>
2018-12-25 01:19:49 +0000

    9129112 test(bazel): fix `bazel-workspace` schematics test

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+35.sha-be08611
+ * @license Angular v7.1.4+1.sha-9129112
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6402,7 +6402,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+35.sha-be08611');
+var VERSION = new Version('7.1.4+1.sha-9129112');
 
 /**
  * @license
