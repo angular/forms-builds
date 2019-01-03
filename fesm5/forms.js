8dc694f20e757f207d781b8b0851e7e0b6e5946c
Ben Lesh <ben@benlesh.com>
2019-01-03 18:17:39 +0000

    a0a3648 docs: add more info on what is being cached (#27343)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+43.sha-a0a3648
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6412,7 +6412,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+40.sha-755c809');
+var VERSION = new Version('7.2.0-rc.0+43.sha-a0a3648');
 
 /**
  * @license
