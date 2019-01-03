d22ad1422aed9fa94973e479e7f7a5cf98ff97d1
Ben Lesh <ben@benlesh.com>
2019-01-03 18:16:50 +0000

    791d865 fix(docs-infra): upgrade to latest dgeni-packages to fix linking problem (#27864)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+23.sha-5aecb2d
+ * @license Angular v7.2.0-rc.0+25.sha-791d865
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6412,7 +6412,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+23.sha-5aecb2d');
+var VERSION = new Version('7.2.0-rc.0+25.sha-791d865');
 
 /**
  * @license
