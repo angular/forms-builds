9de4ed00bc7c9829249416a183384eb8a208fa3d
Ben Lesh <ben@benlesh.com>
2019-01-03 18:19:07 +0000

    5638c1d docs: fix npm description in architecture modules guide (#27818)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+41.sha-5638c1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7786,7 +7786,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+40.sha-755c809');
+const VERSION = new Version('7.2.0-rc.0+41.sha-5638c1d');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+40.sha-755c809
+ * @license Angular v7.2.0-rc.0+41.sha-5638c1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6668,7 +6668,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+40.sha-755c809');
+var VERSION = new Version('7.2.0-rc.0+41.sha-5638c1d');
 
 /**
  * @license
