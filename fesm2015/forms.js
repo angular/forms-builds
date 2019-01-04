337189c3e05212c9337f3bc79db6c2729009f594
Kara Erickson <karakara@google.com>
2019-01-04 23:25:08 +0000

    929334b fix(ivy): should not throw when getting VCRef.parentInjector on the root view (#27909)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+63.sha-fb7816f
+ * @license Angular v7.2.0-rc.0+64.sha-929334b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7867,7 +7867,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+63.sha-fb7816f');
+const VERSION = new Version('7.2.0-rc.0+64.sha-929334b');
 
 /**
  * @fileoverview added by tsickle
