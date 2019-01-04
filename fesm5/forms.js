21e2fcf7bcd18249868792716bbf4864e5654931
Kara Erickson <karakara@google.com>
2019-01-04 23:21:44 +0000

    fb7816f fix(ivy): element injector on the host of a component should see viewProviders of that component (#27928)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+62.sha-5d3dcfc
+ * @license Angular v7.2.0-rc.0+63.sha-fb7816f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6499,7 +6499,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+62.sha-5d3dcfc');
+var VERSION = new Version('7.2.0-rc.0+63.sha-fb7816f');
 
 /**
  * @license
