6e5315e84bb2c97174fd435e4298072712ffc15c
Kara Erickson <karakara@google.com>
2019-01-05 01:02:24 +0000

    b61dafa refactor: remove redundant error in catch (#25478)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+61.sha-0bd9deb
+ * @license Angular v7.2.0-rc.0+65.sha-b61dafa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7040,7 +7040,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+61.sha-0bd9deb');
+const VERSION = new Version('7.2.0-rc.0+65.sha-b61dafa');
 
 /**
  * @fileoverview added by tsickle
