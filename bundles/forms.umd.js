b9caecb22cf9478849def975018866f1042a0460
Kara Erickson <karakara@google.com>
2019-01-05 03:03:39 +0000

    6cc864a build: enforce minimum Bazel version of 0.21.0 in WORKSPACE so cache works with `yarn bazel` (#27935)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+27.sha-0a54825
+ * @license Angular v7.2.0-rc.0+29.sha-6cc864a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6501,7 +6501,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0-rc.0+27.sha-0a54825');
+    var VERSION = new core.Version('7.2.0-rc.0+29.sha-6cc864a');
 
     /**
      * @license
