0dfc70e87c2ab8e8f1b252ae5f4752e7a5574bc8
Kara Erickson <karakara@google.com>
2019-01-07 23:16:49 +0000

    216bbd0 test(bazel): re-enable ng_package golden testing on ci (#27829)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+33.sha-bbe656e
+ * @license Angular v7.2.0-rc.0+37.sha-216bbd0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6412,7 +6412,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+33.sha-bbe656e');
+var VERSION = new Version('7.2.0-rc.0+37.sha-216bbd0');
 
 /**
  * @license
