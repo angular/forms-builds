f59c1985d3c5225cc4439f4343f5d1cacccf101d
Kara Erickson <karakara@google.com>
2019-01-07 16:24:48 +0000

    0efbb37 release: cut the v7.2.0 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+71.sha-6b0c1a7
+ * @license Angular v7.2.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6499,7 +6499,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+71.sha-6b0c1a7');
+var VERSION = new Version('7.2.0');
 
 /**
  * @license
