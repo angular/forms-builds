e6000f89aa6015a6e6aca8a2d7253afe6c21bc0a
Kara Erickson <karakara@google.com>
2019-01-07 23:09:35 +0000

    e775313 fix(ivy): ngOnDestroy hooks should be called on providers (#27955)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+7.sha-4588053
+ * @license Angular v7.2.0+9.sha-e775313
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6499,7 +6499,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+7.sha-4588053');
+var VERSION = new Version('7.2.0+9.sha-e775313');
 
 /**
  * @license
