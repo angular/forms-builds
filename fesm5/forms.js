842a6cc2ea5fda92bd29b4fd318891b9f3b20a1b
Kara Erickson <karakara@google.com>
2019-01-08 20:15:40 +0000

    f1c3eab ci: update payload size for cli-hello-world (#27994)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-1edb8af
+ * @license Angular v7.2.0-rc.0+55.sha-f1c3eab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6412,7 +6412,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+54.sha-1edb8af');
+var VERSION = new Version('7.2.0-rc.0+55.sha-f1c3eab');
 
 /**
  * @license
