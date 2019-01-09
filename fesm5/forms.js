27c121ae2edda5709653b99a1555b4ba192961da
Andrew Kushnir <akushnir@google.com>
2019-01-09 20:17:03 +0000

    76580b9 ci: add brandonroberts to @angular/docs-infra codeowners (#28023)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+67.sha-d6cfe2e
+ * @license Angular v7.2.0+71.sha-76580b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6581,7 +6581,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+67.sha-d6cfe2e');
+var VERSION = new Version('7.2.0+71.sha-76580b9');
 
 /**
  * @license
