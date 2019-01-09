7c9aa4827bb8cbcdc451fb3b7b9c819b19fd73b7
Andrew Kushnir <akushnir@google.com>
2019-01-09 20:16:04 +0000

    ecc304a ci: add brandonroberts to @angular/docs-infra codeowners (#28023)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+61.sha-7227b4a
+ * @license Angular v7.2.0-rc.0+70.sha-ecc304a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6959,7 +6959,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0-rc.0+61.sha-7227b4a');
+const VERSION = new Version('7.2.0-rc.0+70.sha-ecc304a');
 
 /**
  * @fileoverview added by tsickle
