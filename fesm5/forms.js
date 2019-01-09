aa542dc7c392b3f23e975dafe37dbfd892e59831
Kara Erickson <karakara@google.com>
2019-01-09 01:45:18 +0000

    7227b4a docs: use static zip function as an Observable creator. (#26790)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+60.sha-5a7dd73
+ * @license Angular v7.2.0-rc.0+61.sha-7227b4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6412,7 +6412,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+60.sha-5a7dd73');
+var VERSION = new Version('7.2.0-rc.0+61.sha-7227b4a');
 
 /**
  * @license
