ca30391e381221d2eb94665f06e42f39803077bc
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:56:17 +0000

    11325ba test(ivy): mark jit_summaries_integration_spec as obsolete in Ivy (#28027)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+79.sha-d0c3e25
+ * @license Angular v7.2.0+80.sha-11325ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6876,7 +6876,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+79.sha-d0c3e25');
+var VERSION = new Version('7.2.0+80.sha-11325ba');
 
 /**
  * @license
