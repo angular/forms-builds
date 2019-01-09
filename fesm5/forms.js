3743f3b8b8ebdfac7a8e02bdd631d8d239d6177a
Kara Erickson <karakara@google.com>
2019-01-09 00:43:33 +0000

    5a7dd73 Revert "test(ivy): re-enable passing animation tests (#27997)"

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+55.sha-f1c3eab
+ * @license Angular v7.2.0-rc.0+60.sha-5a7dd73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6412,7 +6412,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+55.sha-f1c3eab');
+var VERSION = new Version('7.2.0-rc.0+60.sha-5a7dd73');
 
 /**
  * @license
