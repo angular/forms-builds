c073b44e2fbbd9219956c44ae3083252cf7f1825
Kara Erickson <karakara@google.com>
2019-01-09 01:44:24 +0000

    c3aa24c fix(ivy): sanitization for Host Bindings (#27939)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+55.sha-1de4031
+ * @license Angular v7.2.0+56.sha-c3aa24c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6950,7 +6950,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+55.sha-1de4031');
+    var VERSION = new i0.Version('7.2.0+56.sha-c3aa24c');
 
     /**
      * @license
