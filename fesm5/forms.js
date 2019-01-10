ce712b15eff649cbab1137b083a019b6ca634c74
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:30:18 +0000

    a64a7a4 test(upgrade): log more info to help debug CI flakes (#28045)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+77.sha-9bda446
+ * @license Angular v7.2.0-rc.0+78.sha-a64a7a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6412,7 +6412,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+77.sha-9bda446');
+var VERSION = new Version('7.2.0-rc.0+78.sha-a64a7a4');
 
 /**
  * @license
