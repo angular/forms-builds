e8dfb3a133155e5eed0db1655d727ebb7cee3945
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:28:58 +0000

    c5ab3e8 fix(ivy): proper resolution of Enums in Component decorator (#27971)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+88.sha-142553a
+ * @license Angular v7.2.0+89.sha-c5ab3e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6670,7 +6670,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+88.sha-142553a');
+    var VERSION = new core.Version('7.2.0+89.sha-c5ab3e8');
 
     /**
      * @license
