cbf50ea9848023bc5e835176f8edcec99a1a89fe
Andrew Kushnir <akushnir@google.com>
2019-01-10 02:00:43 +0000

    5a0deb8 fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+84.sha-4694c93
+ * @license Angular v7.2.0+85.sha-5a0deb8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7994,7 +7994,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+84.sha-4694c93');
+const VERSION = new Version('7.2.0+85.sha-5a0deb8');
 
 /**
  * @fileoverview added by tsickle
