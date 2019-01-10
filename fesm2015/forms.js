7e163449242f516a29c0bcfd9e77f18d568adc7e
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:30:28 +0000

    c5ab3e8 fix(ivy): proper resolution of Enums in Component decorator (#27971)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+88.sha-142553a
+ * @license Angular v7.2.0+89.sha-c5ab3e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7994,7 +7994,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+88.sha-142553a');
+const VERSION = new Version('7.2.0+89.sha-c5ab3e8');
 
 /**
  * @fileoverview added by tsickle
