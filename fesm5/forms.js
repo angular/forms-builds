c605d84b03022f9fa655546f284a06cabff8bcff
Andrew Kushnir <akushnir@google.com>
2019-01-10 21:50:57 +0000

    feebe03 fix(ivy): pass ngContentSelectors through to `defineComponent()` calls (#27867)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+98.sha-a6ba789
+ * @license Angular v7.2.0+100.sha-feebe03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6581,7 +6581,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+98.sha-a6ba789');
+var VERSION = new Version('7.2.0+100.sha-feebe03');
 
 /**
  * @license
