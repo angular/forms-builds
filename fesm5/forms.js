847d940873ab775b8e045cc45670dd079774d7b5
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:33:30 +0000

    ee60c76 test(ivy): turn on passing i18n test in CI (#28035)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+92.sha-65e72e9
+ * @license Angular v7.2.0+94.sha-ee60c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6876,7 +6876,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+92.sha-65e72e9');
+var VERSION = new Version('7.2.0+94.sha-ee60c76');
 
 /**
  * @license
