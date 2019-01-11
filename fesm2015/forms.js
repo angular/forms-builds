4fb57de968154e9953b0023f09ea1f945eeac9a8
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:21 +0000

    acb2cac docs: fix typo (#27865) PR Close #27865

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+107.sha-96f4969
+ * @license Angular v7.2.0+108.sha-acb2cac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7994,7 +7994,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+107.sha-96f4969');
+const VERSION = new Version('7.2.0+108.sha-acb2cac');
 
 /**
  * @fileoverview added by tsickle
