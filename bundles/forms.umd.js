41b0608ee71c3530c5b6d6a3dda766a678681b00
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:31:19 +0000

    96f4969 docs: fix broken html for deployment.md (#27851)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+106.sha-aebec4b
+ * @license Angular v7.2.0+107.sha-96f4969
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6670,7 +6670,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+106.sha-aebec4b');
+    var VERSION = new core.Version('7.2.0+107.sha-96f4969');
 
     /**
      * @license
