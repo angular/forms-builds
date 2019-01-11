11c155fb49a12151cdfdd8b6420ad17d183370f6
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:43 +0000

    bccf8da docs(router): fix misnamed path (#27879)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+110.sha-26a8c09
+ * @license Angular v7.2.0+109.sha-bccf8da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6670,7 +6670,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.0+110.sha-26a8c09');
+    var VERSION = new core.Version('7.2.0+109.sha-bccf8da');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+110.sha-26a8c09
+ * @license Angular v7.2.0+109.sha-bccf8da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6581,7 +6581,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+110.sha-26a8c09');
+var VERSION = new Version('7.2.0+109.sha-bccf8da');
 
 /**
  * @license
