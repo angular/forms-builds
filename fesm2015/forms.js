02fdff6aa7c7c0f11278721f911b2b2f5e239809
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:37:30 +0000

    e31afb7 fix(ivy): ngcc - identify all ESM5 decorated classes (#27848)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+113.sha-d505468
+ * @license Angular v7.2.0+114.sha-e31afb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7994,7 +7994,7 @@ FormBuilder.ngInjectableDef = defineInjectable({ token: FormBuilder, factory: fu
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+113.sha-d505468');
+const VERSION = new Version('7.2.0+114.sha-e31afb7');
 
 /**
  * @fileoverview added by tsickle
