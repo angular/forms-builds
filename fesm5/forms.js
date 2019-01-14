5170d497a0f4761fec265f98b87d58bd86cd4722
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:29:34 +0000

    0897eba docs: fix typo in docs.md (#27484)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+157.sha-4bb5f63
+ * @license Angular v7.2.0+156.sha-0897eba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6603,7 +6603,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+157.sha-4bb5f63');
+var VERSION = new Version('7.2.0+156.sha-0897eba');
 
 /**
  * @license
