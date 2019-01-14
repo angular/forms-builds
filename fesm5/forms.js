c5b1e359e5db663406c4182e45145bed13a74fdf
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:21:06 +0000

    6fff74e test(ivy): re-enable passing tests and misc cleanup (#28093)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+143.sha-da2880d
+ * @license Angular v7.2.0+146.sha-6fff74e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6898,7 +6898,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+143.sha-da2880d');
+var VERSION = new Version('7.2.0+146.sha-6fff74e');
 
 /**
  * @license
