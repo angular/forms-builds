cf0d49fde76f3157c5c3d45505e88f4f5ba0b746
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:26:25 +0000

    5e6e96d docs: fix GitHub pages deployment command (#26896)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+150.sha-638375b
+ * @license Angular v7.2.0+152.sha-5e6e96d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6898,7 +6898,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+150.sha-638375b');
+var VERSION = new Version('7.2.0+152.sha-5e6e96d');
 
 /**
  * @license
