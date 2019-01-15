3b145f004ee451d5912b17b43d77523119f2a6af
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:32:53 +0000

    fa53150 test(ivy): NodeInjector should know how to get itself (INJECTOR) (#28009)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+173.sha-b5c2ef2
+ * @license Angular v7.2.0+174.sha-fa53150
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+173.sha-b5c2ef2');
+const VERSION = new Version('7.2.0+174.sha-fa53150');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+173.sha-b5c2ef2
+ * @license Angular v7.2.0+174.sha-fa53150
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6603,7 +6603,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+173.sha-b5c2ef2');
+var VERSION = new Version('7.2.0+174.sha-fa53150');
 
 /**
  * @license
