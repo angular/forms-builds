08a883600ce3f9589d894276a3631f52ca5badc9
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:44:36 +0000

    ce3a746 perf(platform-server): use shared `DomElementSchemaRegistry` instance (#28150) (#28151)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+187.sha-bc02e31
+ * @license Angular v7.2.0+188.sha-ce3a746
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8011,7 +8011,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+187.sha-bc02e31');
+const VERSION = new Version('7.2.0+188.sha-ce3a746');
 
 /**
  * @fileoverview added by tsickle
