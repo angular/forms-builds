4c13e2eb79bc22ed60a8fbd9cfbac46a267ae35f
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:01:33 +0000

    e62eeed fix(ivy): ensure component/directive `class` selectors are properly understood (#27849)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+177.sha-06e5bf1
+ * @license Angular v7.2.0+178.sha-e62eeed
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6603,7 +6603,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+177.sha-06e5bf1');
+var VERSION = new Version('7.2.0+178.sha-e62eeed');
 
 /**
  * @license
