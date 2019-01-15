6f0064ad660932fa2e5b956e567f78e20e3da8af
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:35:20 +0000

    60fecc1 fixup! refactor(ivy): simplify context discover based on feedback (#28101)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+174.sha-fa53150
+ * @license Angular v7.2.0+176.sha-60fecc1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6898,7 +6898,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+174.sha-fa53150');
+var VERSION = new Version('7.2.0+176.sha-60fecc1');
 
 /**
  * @license
