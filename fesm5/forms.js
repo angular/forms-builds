49b60addbdf1f4735ae6145a9f8fc1484e503594
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:10:14 +0000

    850b867 docs: add api doc for switch directives (#27378)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+183.sha-a241200
+ * @license Angular v7.2.0+184.sha-850b867
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6603,7 +6603,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+183.sha-a241200');
+var VERSION = new Version('7.2.0+184.sha-850b867');
 
 /**
  * @license
