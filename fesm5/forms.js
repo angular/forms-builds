72e41f60ceca5c40288039854b60f43b87bc2a08
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:58:46 +0000

    1e5e62e docs: release notes for the v7.2.1 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+198.sha-bac71ef
+ * @license Angular v7.2.0+199.sha-1e5e62e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6901,7 +6901,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+198.sha-bac71ef');
+var VERSION = new Version('7.2.0+199.sha-1e5e62e');
 
 /**
  * @license
