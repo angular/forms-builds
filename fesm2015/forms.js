ca2ac88579b3cdeafa6a60e7ce746c7866b21cf6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 01:57:43 +0000

    4ea6c27 build(bazel): Use @angular/cli from root package.json (#28139)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1
+ * @license Angular v7.2.1+3.sha-4ea6c27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.1');
+const VERSION = new Version('7.2.1+3.sha-4ea6c27');
 
 /**
  * @fileoverview added by tsickle
