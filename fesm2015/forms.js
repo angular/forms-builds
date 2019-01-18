578c02a167734350fbcf68d9a57993be25f653b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 17:22:40 +0000

    93d78c9 build(bazel): Use local rollup & build-optimizer for ng_rollup_bundle (#28215)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+6.sha-49bee4c
+ * @license Angular v7.2.1+7.sha-93d78c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.1+6.sha-49bee4c');
+const VERSION = new Version('7.2.1+7.sha-93d78c9');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+6.sha-49bee4c
+ * @license Angular v7.2.1+7.sha-93d78c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+6.sha-49bee4c');
+var VERSION = new Version('7.2.1+7.sha-93d78c9');
 
 /**
  * @license
