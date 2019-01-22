ab3798e32382d061f259fcb35ca04cd070a0a1ac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:11:25 +0000

    eb8ccf6 fix(docs-infra): removal of the use of the ChildNode.remove() method that it isn't supported by IE (#28188)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+11.sha-607fd8e
+ * @license Angular v7.2.1+13.sha-eb8ccf6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+11.sha-607fd8e');
+var VERSION = new Version('7.2.1+13.sha-eb8ccf6');
 
 /**
  * @license
