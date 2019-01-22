c275e8c46e2b91ebbd01a83c0bde3f719aea35bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:42:45 +0000

    ea1b5c1 fix(ivy): not applying camelCased style properties (#28276)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+62.sha-18a9afc
+ * @license Angular v8.0.0-beta.0+63.sha-ea1b5c1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6611,7 +6611,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+62.sha-18a9afc');
+var VERSION = new Version('8.0.0-beta.0+63.sha-ea1b5c1');
 
 /**
  * @license
