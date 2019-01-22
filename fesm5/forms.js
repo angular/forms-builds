db826556c11f99054466e102b6353e4c33b2fafb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 17:54:23 +0000

    f4ac96d style(docs-infra): Active tslint rule `semicolon` (#28282)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+8.sha-4c00059
+ * @license Angular v7.2.1+10.sha-f4ac96d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+8.sha-4c00059');
+var VERSION = new Version('7.2.1+10.sha-f4ac96d');
 
 /**
  * @license
