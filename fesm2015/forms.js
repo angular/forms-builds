3aae30654aa406f3ad83db23f6fc62cd2d7f3730
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 23:30:32 +0000

    32737a6 fix(router): `skipLocationChange` with named outlets (#28301)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+19.sha-7642308
+ * @license Angular v7.2.1+20.sha-32737a6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.1+19.sha-7642308');
+const VERSION = new Version('7.2.1+20.sha-32737a6');
 
 /**
  * @fileoverview added by tsickle
