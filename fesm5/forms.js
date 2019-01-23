4483485c8c260c483299d4f2e143e92670a90be4
Jason Aden <jasonaden@google.com>
2019-01-23 23:59:23 +0000

    d4ecffe refactor: remove obsolete font-smoothing property (#28174)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+18.sha-cbd6264
+ * @license Angular v8.0.0-beta.1+20.sha-d4ecffe
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6909,7 +6909,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+18.sha-cbd6264');
+var VERSION = new Version('8.0.0-beta.1+20.sha-d4ecffe');
 
 /**
  * @license
