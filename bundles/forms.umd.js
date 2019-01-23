bded820f511cbe3635a18d210143f29f86128210
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:35:54 +0000

    03c8528 docs(router): removed additional to (#25989) PR Close #25989

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+2.sha-d8f2318
+ * @license Angular v8.0.0-beta.1+11.sha-03c8528
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6700,7 +6700,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.1+2.sha-d8f2318');
+    var VERSION = new core.Version('8.0.0-beta.1+11.sha-03c8528');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+2.sha-d8f2318
+ * @license Angular v8.0.0-beta.1+11.sha-03c8528
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7136,7 +7136,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.1+2.sha-d8f2318');
+const VERSION = new Version('8.0.0-beta.1+11.sha-03c8528');
 
 /**
  * @fileoverview added by tsickle
