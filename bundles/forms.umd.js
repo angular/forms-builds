1de7f452523ef9f96c20260db015a7e5965e5ba8
Jason Aden <jasonaden@google.com>
2019-01-30 02:23:25 +0000

    a227c52 feat(compiler-cli): expose ngtsc as a TscPlugin (#28435)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+100.sha-ad49962
+ * @license Angular v8.0.0-beta.1+109.sha-a227c52
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7000,7 +7000,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+100.sha-ad49962');
+    var VERSION = new i0.Version('8.0.0-beta.1+109.sha-a227c52');
 
     /**
      * @license
