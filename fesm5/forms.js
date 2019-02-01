70c1a95d03a769ca487dfc4549d958e40be91a51
Matias Niemelä <matias@yearofmoo.com>
2019-02-01 19:13:56 +0000

    d85d396 fix(bazel): Bazel builder resolves with require.resolve() (#28478)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+1.sha-449da8c
+ * @license Angular v7.2.3+3.sha-d85d396
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.3+1.sha-449da8c');
+var VERSION = new Version('7.2.3+3.sha-d85d396');
 
 /**
  * @license
