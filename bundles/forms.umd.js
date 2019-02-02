938b2abbfdb5b751ce1b494f62a963618bbebae6
Matias Niemelä <matias@yearofmoo.com>
2019-02-02 01:39:10 +0000

    72c3695 ci: pin ChromeDriver to a version compatible with docker image's Chrome (#28494)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+12.sha-8930f60
+ * @license Angular v8.0.0-beta.2+17.sha-72c3695
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7000,7 +7000,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.2+12.sha-8930f60');
+    var VERSION = new i0.Version('8.0.0-beta.2+17.sha-72c3695');
 
     /**
      * @license
