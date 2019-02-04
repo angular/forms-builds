54b7ad84996c2255c4421058f830887e2b6a9d9f
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 20:16:45 +0000

    2109c30 docs: fix shadow variable in tutorial example (#28499) PR Close #28499

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+16.sha-c64e666
+ * @license Angular v7.2.3+18.sha-2109c30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.3+16.sha-c64e666');
+const VERSION = new Version('7.2.3+18.sha-2109c30');
 
 /**
  * @fileoverview added by tsickle
