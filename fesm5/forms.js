3e692df2eba21cc94d3d0f0117094c18635ec7f3
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 16:52:41 +0000

    4dfcbc6 build(docs-infra): update terser version in lockfile (#28512)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+17.sha-72c3695
+ * @license Angular v8.0.0-beta.2+21.sha-4dfcbc6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+17.sha-72c3695');
+var VERSION = new Version('8.0.0-beta.2+21.sha-4dfcbc6');
 
 /**
  * @license
