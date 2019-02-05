c7c0e495013ece0224f289b2687942bc458d2559
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:44:49 +0000

    a6fd118 build: switch to typescript's es2015 typings (#28134)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+24.sha-fe6e76c
+ * @license Angular v7.2.3+25.sha-a6fd118
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.3+24.sha-fe6e76c');
+var VERSION = new Version('7.2.3+25.sha-a6fd118');
 
 /**
  * @license
