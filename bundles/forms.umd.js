b38cf24961eb9ad2a8a4bd9441360023adb4e839
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:18:35 +0000

    fe6e76c ci: keep job alive when yarn request takes more than 10 minutes (#28458)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+20.sha-c1aa1bf
+ * @license Angular v7.2.3+24.sha-fe6e76c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6610,7 +6610,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.3+20.sha-c1aa1bf');
+    var VERSION = new core.Version('7.2.3+24.sha-fe6e76c');
 
     /**
      * @license
