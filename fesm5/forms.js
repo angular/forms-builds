3e1396d31db03171614ad0a7d6b2eaba61b149f7
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:46:21 +0000

    47f5b5f docs(service-worker): note about isStable/setInterval (#28102)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+21.sha-a0e3a8e
+ * @license Angular v7.2.4+23.sha-47f5b5f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+21.sha-a0e3a8e');
+var VERSION = new Version('7.2.4+23.sha-47f5b5f');
 
 /**
  * @license
