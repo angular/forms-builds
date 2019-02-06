a11c8c21716778881492f0b67b272fbde08db07b
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 07:48:02 +0000

    be8a403 feat(aio): show developer survey 2019 (#28552)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+10.sha-c378402
+ * @license Angular v7.2.4+11.sha-be8a403
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+10.sha-c378402');
+const VERSION = new Version('7.2.4+11.sha-be8a403');
 
 /**
  * @fileoverview added by tsickle
