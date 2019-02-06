9eb5b647d60f14c14a5e524c8a228e1db6947403
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:45:29 +0000

    ed0cf7e fix(router): set href when routerLink is used on an 'area' element (#28441)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+61.sha-5a6f0d0
+ * @license Angular v8.0.0-beta.3+1.sha-ed0cf7e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7159,7 +7159,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.2+61.sha-5a6f0d0');
+const VERSION = new Version('8.0.0-beta.3+1.sha-ed0cf7e');
 
 /**
  * @fileoverview added by tsickle
