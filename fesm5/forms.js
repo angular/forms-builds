ad78cfa521bd15a38ec6849a549c849b2503c2d0
Misko Hevery <misko@hevery.com>
2019-02-07 05:40:10 +0000

    da81c9c build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+23.sha-47f5b5f
+ * @license Angular v7.2.4+28.sha-da81c9c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+23.sha-47f5b5f');
+var VERSION = new Version('7.2.4+28.sha-da81c9c');
 
 /**
  * @license
