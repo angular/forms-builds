25150b1297a027e8283b54c0139489e2b5d55699
Misko Hevery <misko@hevery.com>
2019-02-07 05:39:20 +0000

    50732e1 fix(router): redirect to root url when returned as UrlTree from guard (#28271)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+29.sha-8d90142
+ * @license Angular v8.0.0-beta.3+35.sha-50732e1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6717,7 +6717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.3+29.sha-8d90142');
+    var VERSION = new core.Version('8.0.0-beta.3+35.sha-50732e1');
 
     /**
      * @license
