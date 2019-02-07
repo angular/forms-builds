9df13947d6c494a8953dfab5bb4cdf73a23224c4
Misko Hevery <misko@hevery.com>
2019-02-07 05:40:39 +0000

    1e58a21 fix(router): redirect to root url when returned as UrlTree from guard (#28271)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+28.sha-da81c9c
+ * @license Angular v7.2.4+29.sha-1e58a21
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+28.sha-da81c9c');
+var VERSION = new Version('7.2.4+29.sha-1e58a21');
 
 /**
  * @license
