730367b1486fd078fc8d00d2f91f17ded0b37683
Igor Minar <igor@angular.io>
2019-02-08 18:38:15 +0000

    e4b1bdb ci: do not install firebase-tools without cache (#28615)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+38.sha-6477c9e.with-local-changes
+ * @license Angular v7.2.4+39.sha-e4b1bdb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+38.sha-6477c9e.with-local-changes');
+var VERSION = new Version('7.2.4+39.sha-e4b1bdb.with-local-changes');
 
 /**
  * @license
