7b7be56af50232bf3b434716388fee28769d4b70
Miško Hevery <misko@hevery.com>
2019-02-09 00:54:52 +0000

    81329c8 test(ivy): enable passing material tests (#28560)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+63.sha-f7591f1
+ * @license Angular v8.0.0-beta.3+71.sha-81329c8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+63.sha-f7591f1');
+var VERSION = new Version('8.0.0-beta.3+71.sha-81329c8');
 
 /**
  * @license
