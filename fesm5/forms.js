b4e4703702b52527fadffbb383223375cf7172fb
Miško Hevery <misko@hevery.com>
2019-02-12 18:55:27 +0000

    0cb02d9 fix(ivy): mark query test as modified in Ivy (#28660)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+92.sha-1e64f37
+ * @license Angular v8.0.0-beta.3+107.sha-0cb02d9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+92.sha-1e64f37');
+var VERSION = new Version('8.0.0-beta.3+107.sha-0cb02d9');
 
 /**
  * @license
