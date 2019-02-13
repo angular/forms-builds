8f09160baf2e9863a50e38527ab066fec4e235ce
Miško Hevery <misko@hevery.com>
2019-02-13 20:51:33 +0000

    2f27a80 test(ivy): ivy change detection doesn't descend into CD-detached view trees (#28680)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+143.sha-77eee42
+ * @license Angular v8.0.0-beta.3+144.sha-2f27a80
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+143.sha-77eee42');
+var VERSION = new Version('8.0.0-beta.3+144.sha-2f27a80');
 
 /**
  * @license
