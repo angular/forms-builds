55a1badc76bff4e1df0c4d724f6e1e588db34d66
Miško Hevery <misko@hevery.com>
2019-02-13 20:44:33 +0000

    77eee42 fix(core): improve global variable detection (#28679)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+141.sha-5cafd44
+ * @license Angular v8.0.0-beta.3+143.sha-77eee42
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7159,7 +7159,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+141.sha-5cafd44');
+const VERSION = new Version('8.0.0-beta.3+143.sha-77eee42');
 
 /**
  * @fileoverview added by tsickle
