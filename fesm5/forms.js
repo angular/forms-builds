63f1edbbe7cbce6d867fb44f197b8988d3c7c06d
Misko Hevery <misko@hevery.com>
2019-02-13 05:54:06 +0000

    08de52b feat(ivy): add source mappings to compiled Angular templates (#28055)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+89.sha-872a365
+ * @license Angular v8.0.0-beta.3+124.sha-08de52b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+89.sha-872a365');
+var VERSION = new Version('8.0.0-beta.3+124.sha-08de52b');
 
 /**
  * @license
