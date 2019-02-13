0a60011865df42c560ac990a0e39fae266301081
Miško Hevery <misko@hevery.com>
2019-02-13 20:27:37 +0000

    39d0311 refactor(ivy): combine contentQueries and contentQueriesRefresh functions (#28503)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+135.sha-644e7a2
+ * @license Angular v8.0.0-beta.3+136.sha-39d0311
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+135.sha-644e7a2');
+var VERSION = new Version('8.0.0-beta.3+136.sha-39d0311');
 
 /**
  * @license
