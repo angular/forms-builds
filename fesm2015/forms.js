dbd5d6edb705e5b8f948a9dcda6b2c2e495e773c
Miško Hevery <misko@hevery.com>
2019-02-13 04:52:46 +0000

    73f9db5 ci: downgrade to sauce-connect v4.5.1 (#28659)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+47.sha-2bf526b
+ * @license Angular v7.2.4+48.sha-73f9db5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+47.sha-2bf526b');
+const VERSION = new Version('7.2.4+48.sha-73f9db5');
 
 /**
  * @fileoverview added by tsickle
