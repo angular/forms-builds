8518331e22ecdfa333c1dfc6f949ab32496d9383
Misko Hevery <misko@hevery.com>
2019-02-14 03:50:13 +0000

    8cec4b3 build: make api-extractor work in google3 (#28588)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+168.sha-b0afc4c
+ * @license Angular v8.0.0-beta.3+166.sha-8cec4b3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -4041,9 +4041,9 @@ class FormGroup extends AbstractControl {
         this._forEachChild((control, name) => {
             control.reset(value[name], { onlySelf: true, emitEvent: options.emitEvent });
         });
+        this.updateValueAndValidity(options);
         this._updatePristine(options);
         this._updateTouched(options);
-        this.updateValueAndValidity(options);
     }
     /**
      * The aggregate value of the `FormGroup`, including any disabled controls.
@@ -4463,9 +4463,9 @@ class FormArray extends AbstractControl {
         this._forEachChild((control, index) => {
             control.reset(value[index], { onlySelf: true, emitEvent: options.emitEvent });
         });
+        this.updateValueAndValidity(options);
         this._updatePristine(options);
         this._updateTouched(options);
-        this.updateValueAndValidity(options);
     }
     /**
      * The aggregate value of the array, including any disabled controls.
@@ -7159,7 +7159,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+168.sha-b0afc4c');
+const VERSION = new Version('8.0.0-beta.3+166.sha-8cec4b3');
 
 /**
  * @fileoverview added by tsickle
