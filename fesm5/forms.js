51b4eb56fed26c52dc4cbca6a9819cc7f759a9e2
Misko Hevery <misko@hevery.com>
2019-02-14 03:39:46 +0000

    b23582d docs(animations): fixed some grammar (#28708) PR Close #28708

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+73.sha-d3c6e51
+ * @license Angular v7.2.4+74.sha-b23582d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+73.sha-d3c6e51');
+var VERSION = new Version('7.2.4+74.sha-b23582d');
 
 /**
  * @license
