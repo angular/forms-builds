eacd7826ac22f4bd0a29287288fd001e95687b0d
Miško Hevery <misko@hevery.com>
2019-02-14 19:58:26 +0000

    80a5934 fix(ivy): support schemas at runtime (#28637)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+177.sha-63e5d27
+ * @license Angular v8.0.0-beta.3+179.sha-80a5934
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7000,7 +7000,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+177.sha-63e5d27');
+    var VERSION = new i0.Version('8.0.0-beta.3+179.sha-80a5934');
 
     /**
      * @license
