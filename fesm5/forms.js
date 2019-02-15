76fbd539268573a37f7907fc3739601bcc806353
Miško Hevery <misko@hevery.com>
2019-02-15 21:50:17 +0000

    a2ba444 release: cut the v release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+82.sha-ac6b2b4
+ * @license Angular v7.2.4+83.sha-a2ba444
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+82.sha-ac6b2b4');
+var VERSION = new Version('7.2.4+83.sha-a2ba444');
 
 /**
  * @license
