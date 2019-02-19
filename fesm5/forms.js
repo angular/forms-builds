6f02d82b12890f8a3b667eb5ddae086fedfc4ea9
Igor Minar <igor@angular.io>
2019-02-19 21:56:34 +0000

    ae16378 docs: fix wrong filename in text (#28832)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+29.sha-d8ae899
+ * @license Angular v8.0.0-beta.4+32.sha-ae16378
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+29.sha-d8ae899');
+var VERSION = new Version('8.0.0-beta.4+32.sha-ae16378');
 
 /**
  * @license
