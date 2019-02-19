b2d940778c49961bb2dbdac2c77efdaa4c5a88b2
Igor Minar <igor@angular.io>
2019-02-19 21:01:40 +0000

    dd3cab6 build(docs-infra): improve caching configuration for Firebase hosting (#28785)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+11.sha-ea1a3d3
+ * @license Angular v7.2.5+12.sha-dd3cab6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.5+11.sha-ea1a3d3');
+const VERSION = new Version('7.2.5+12.sha-dd3cab6');
 
 /**
  * @fileoverview added by tsickle
