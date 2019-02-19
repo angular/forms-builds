60a86a9980d6aa44fe090943d5c804294657b9f0
Igor Minar <igor@angular.io>
2019-02-19 21:08:57 +0000

    00a8b07 ci: combine "define_env_vars" and "download_yarn" anchor (#28788)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+20.sha-80c7aff
+ * @license Angular v8.0.0-beta.4+21.sha-00a8b07
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+20.sha-80c7aff');
+var VERSION = new Version('8.0.0-beta.4+21.sha-00a8b07');
 
 /**
  * @license
