dcbc1fd8424a5856020eab9cdaef1b940feafb84
Igor Minar <igor@angular.io>
2019-02-19 21:27:45 +0000

    d8ae899 docs(common): fix a typo in a debounce requests example header (#28829)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+28.sha-19afb79
+ * @license Angular v8.0.0-beta.4+29.sha-d8ae899
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+28.sha-19afb79');
+var VERSION = new Version('8.0.0-beta.4+29.sha-d8ae899');
 
 /**
  * @license
