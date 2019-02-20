bc0780f7d93903e538066cf278258c5df2964833
Igor Minar <igor@angular.io>
2019-02-20 00:13:22 +0000

    ad6475f fix(ivy): re-create node links after creating/moving/removing nodes with i18n (#28827)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+34.sha-3c1a162
+ * @license Angular v8.0.0-beta.4+35.sha-ad6475f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+34.sha-3c1a162');
+var VERSION = new Version('8.0.0-beta.4+35.sha-ad6475f');
 
 /**
  * @license
