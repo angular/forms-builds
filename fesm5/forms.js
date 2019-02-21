2fea55f71ccec8f36a91353b349a68a5ebcb4987
Igor Minar <igor@angular.io>
2019-02-21 17:53:48 +0000

    e1aaa7e fix(ivy): component destroy hook called twice when configured as provider (#28470)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+15.sha-ebffde7
+ * @license Angular v8.0.0-beta.5+17.sha-e1aaa7e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+15.sha-ebffde7');
+var VERSION = new Version('8.0.0-beta.5+17.sha-e1aaa7e');
 
 /**
  * @license
