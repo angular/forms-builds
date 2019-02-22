0b2d1216ba1e9c311b4db9a56ebac62fe2f8ca98
Igor Minar <igor@angular.io>
2019-02-22 02:19:13 +0000

    32ae84d fixup! fix(ivy): incorrectly remapping certain properties that refer to inputs (#28765)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+24.sha-9defc00.with-local-changes
+ * @license Angular v8.0.0-beta.5+26.sha-32ae84d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+24.sha-9defc00.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+26.sha-32ae84d.with-local-changes');
 
 /**
  * @license
