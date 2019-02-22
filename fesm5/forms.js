e70f24601dd3b2341d684bc2a0f27b19aecc4165
Igor Minar <igor@angular.io>
2019-02-22 07:49:24 +0000

    599e2e2 feat(docs-infra): add meta[name]=Description to angular.io's index.html (#28916)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+28.sha-94223a0.with-local-changes
+ * @license Angular v8.0.0-beta.5+32.sha-599e2e2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6926,7 +6926,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+28.sha-94223a0.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+32.sha-599e2e2.with-local-changes');
 
 /**
  * @license
