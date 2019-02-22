2a2b68c72ba1d9bc6081be636e3e71bfff2d40d6
Igor Minar <igor@angular.io>
2019-02-22 02:19:41 +0000

    9defc00 build(bazel): cleanup the jasmine bootstrap code (#28906)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+23.sha-1145bdb.with-local-changes
+ * @license Angular v8.0.0-beta.5+24.sha-9defc00.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6926,7 +6926,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+23.sha-1145bdb.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+24.sha-9defc00.with-local-changes');
 
 /**
  * @license
