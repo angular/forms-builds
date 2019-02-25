9d275f37b2f779eb876f3e2efcbc8b1947c3101d
Ben Lesh <ben@benlesh.com>
2019-02-25 18:36:31 +0000

    481f4b7 docs: Updated TOH Heros documentation (#28347)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+27.sha-4bc0084.with-local-changes
+ * @license Angular v7.2.6+28.sha-481f4b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.6+27.sha-4bc0084.with-local-changes');
+var VERSION = new Version('7.2.6+28.sha-481f4b7.with-local-changes');
 
 /**
  * @license
