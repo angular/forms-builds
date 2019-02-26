55d0b9acdf334529e4f964f2fcd92fb1687ddabb
Ben Lesh <ben@benlesh.com>
2019-02-26 19:19:35 +0000

    25a2fef fix(ivy): ViewRef.rootNodes not including projected nodes (#28951)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes
+ * @license Angular v8.0.0-beta.5+82.sha-25a2fef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+82.sha-25a2fef.with-local-changes');
 
 /**
  * @license
