06222e3013af77ab8977e056d17d38555aa9ad0e
Andrew Kushnir <akushnir@google.com>
2019-03-01 18:29:30 +0000

    c6eaaf5 docs: add how to create a minimal repo in bug report template (#29022)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+14.sha-5e38ec8.with-local-changes
+ * @license Angular v7.2.7+15.sha-c6eaaf5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.7+14.sha-5e38ec8.with-local-changes');
+var VERSION = new Version('7.2.7+15.sha-c6eaaf5.with-local-changes');
 
 /**
  * @license
