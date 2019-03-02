cb2f6d1872e451c277dbac2d6a3ff53a71950ec4
Andrew Kushnir <akushnir@google.com>
2019-03-02 01:12:17 +0000

    c0757d1 fix(ivy): attached flag not being reset when view is destroyed (#29064)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+49.sha-a06824a.with-local-changes
+ * @license Angular v8.0.0-beta.6+50.sha-c0757d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8051,7 +8051,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.6+49.sha-a06824a.with-local-changes');
+const VERSION = new Version('8.0.0-beta.6+50.sha-c0757d1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
