d9068610cca5287f1dea97fd67aeccf7c3bcf93e
Andrew Kushnir <akushnir@google.com>
2019-03-02 03:18:33 +0000

    dcafdde fix(ivy): change for-of to forEach for pipes represented with Map (#29068)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+50.sha-c0757d1.with-local-changes
+ * @license Angular v8.0.0-beta.6+51.sha-dcafdde.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+50.sha-c0757d1.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+51.sha-dcafdde.with-local-changes');
 
 /**
  * @license
