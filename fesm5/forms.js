4049d4ff45d27961c64ce3b001e70b7d1ed11dde
Andrew Kushnir <akushnir@google.com>
2019-03-04 21:04:14 +0000

    95989a1 docs: fix and add decorator api doc (#28986)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+33.sha-ea09430.with-local-changes
+ * @license Angular v8.0.0-beta.6+63.sha-95989a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6628,7 +6628,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+33.sha-ea09430.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+63.sha-95989a1.with-local-changes');
 
 /**
  * @license
