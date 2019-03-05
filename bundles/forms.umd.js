e6c856a01b0acf2ad60309987d3d874139626442
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:05:40 +0000

    72ecc45 fix(router): removed obsolete TODO comment (#29085)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+72.sha-7d17496.with-local-changes
+ * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6717,7 +6717,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.6+72.sha-7d17496.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
 
     /**
      * @license
