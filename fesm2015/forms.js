ac3c145fa7db99ef0b7cd7a71fa7e01471f3f6e5
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:21:22 +0000

    f96efd1 test(ivy): enable passing MatChipList tests (#29130)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+5.sha-dc6192c.with-local-changes
+ * @license Angular v8.0.0-beta.7+8.sha-f96efd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7159,7 +7159,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.7+5.sha-dc6192c.with-local-changes');
+const VERSION = new Version('8.0.0-beta.7+8.sha-f96efd1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
