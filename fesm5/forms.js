84deed80e5ff943deeb6b8520a4fe2d0a7f098d0
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:18:29 +0000

    c5d9035 test: fix dts path to the flattened file (#28834)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7.with-local-changes
+ * @license Angular v8.0.0-beta.7+2.sha-c5d9035.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6790,7 +6790,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+2.sha-c5d9035.with-local-changes');
 
 /**
  * @license
