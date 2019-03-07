14e5cd070d4a8b9b8dfdd65109570606852712be
Kara Erickson <karakara@google.com>
2019-03-07 19:44:26 +0000

    5ad2097 fix(ivy): teach template type checker about template attributes (#29041)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+22.sha-9a19592.with-local-changes
+ * @license Angular v8.0.0-beta.7+29.sha-5ad2097.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6790,7 +6790,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+22.sha-9a19592.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+29.sha-5ad2097.with-local-changes');
 
 /**
  * @license
