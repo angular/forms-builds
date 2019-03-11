4d9fe610b58e666205cf81f686341349365634d5
Kara Erickson <karakara@google.com>
2019-03-11 18:36:31 +0000

    1d88c2b fix(ivy): handle aliased Angular decorators (#29195)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+59.sha-99aa967.with-local-changes
+ * @license Angular v8.0.0-beta.7+60.sha-1d88c2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6827,7 +6827,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+59.sha-99aa967.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+60.sha-1d88c2b.with-local-changes');
 
 /**
  * @license
