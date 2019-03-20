593a8d496d11ab929b39038c214b82b2689c3872
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 18:04:04 +0000

    ddfdf3c docs: add alexeagle to @angular/fw-global-approvers (#29287)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+18.sha-d6d081e.with-local-changes
+ * @license Angular v8.0.0-beta.9+19.sha-ddfdf3c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6901,7 +6901,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.9+18.sha-d6d081e.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.9+19.sha-ddfdf3c.with-local-changes');
 
     /**
      * @license
