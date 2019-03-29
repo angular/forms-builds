6044ffa7f55de5576d86adb1475eac275fa72d85
Jason Aden <jasonaden@google.com>
2019-03-29 17:55:27 +0000

    6d7a4ff docs: move text that mention what is expected to fail up higher into the section (#27329)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+19.sha-72900ec.with-local-changes
+ * @license Angular v7.2.11+23.sha-6d7a4ff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6610,7 +6610,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('7.2.11+19.sha-72900ec.with-local-changes');
+    var VERSION = new core.Version('7.2.11+23.sha-6d7a4ff.with-local-changes');
 
     /**
      * @license
