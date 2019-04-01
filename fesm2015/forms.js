2ad997f766079659d6e5ca13fab857a53aa17cad
Jason Aden <jasonaden@google.com>
2019-04-01 19:27:38 +0000

    e884c0c fix(router): adjust setting navigationTransition when a new navigation cancels an existing one (#29636)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+44.sha-ab72e06.with-local-changes
+ * @license Angular v7.2.11+45.sha-e884c0c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.11+44.sha-ab72e06.with-local-changes');
+const VERSION = new Version('7.2.11+45.sha-e884c0c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
