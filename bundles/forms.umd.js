aff62e9429f2f945bbd2fa60f90c86c015cf1da9
Jason Aden <jasonaden@google.com>
2019-04-01 22:29:29 +0000

    3e56976 fix(ivy): avoid remote scoping if it's not actually required (#29404)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+83.sha-d7e5535.with-local-changes
+ * @license Angular v8.0.0-beta.10+87.sha-3e56976.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6901,7 +6901,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+83.sha-d7e5535.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+87.sha-3e56976.with-local-changes');
 
     /**
      * @license
