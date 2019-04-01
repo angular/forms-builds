15a27117e606a9320c907e9a4644bfcd2dbb4191
Jason Aden <jasonaden@google.com>
2019-04-01 18:48:24 +0000

    94b0e8f docs: update Jorge Cano GDE bio (#29622)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+40.sha-157b948.with-local-changes
+ * @license Angular v7.2.11+41.sha-94b0e8f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+40.sha-157b948.with-local-changes');
+var VERSION = new Version('7.2.11+41.sha-94b0e8f.with-local-changes');
 
 /**
  * @license
