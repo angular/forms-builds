e542ef2cd0146be3901515f36cb33a246ae7f906
Jason Aden <jasonaden@google.com>
2019-04-02 23:29:07 +0000

    ec4dced docs: update examples region to match the description in the guide (#29259)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+59.sha-69786c2.with-local-changes
+ * @license Angular v7.2.11+60.sha-ec4dced.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6521,7 +6521,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.11+59.sha-69786c2.with-local-changes');
+var VERSION = new Version('7.2.11+60.sha-ec4dced.with-local-changes');
 
 /**
  * @license
