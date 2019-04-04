a854acd27570223314ef43e71750c9315387fff3
Igor Minar <igor@angular.io>
2019-04-04 23:47:18 +0000

    b6f48db docs: correct route path description (#29669)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+24.sha-609024f.with-local-changes
+ * @license Angular v8.0.0-beta.11+25.sha-b6f48db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+24.sha-609024f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+25.sha-b6f48db.with-local-changes');
 
 /**
  * @license
