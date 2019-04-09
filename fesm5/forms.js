e54acd57e3e7f815bd2ecbff394ae84cfe2ae40d
Igor Minar <igor@angular.io>
2019-04-09 00:35:00 +0000

    e682b44 docs(router): use pipe in activatedRoute example (#29752)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+42.sha-d144a3b.with-local-changes
+ * @license Angular v8.0.0-beta.11+46.sha-e682b44.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+42.sha-d144a3b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+46.sha-e682b44.with-local-changes');
 
 /**
  * @license
