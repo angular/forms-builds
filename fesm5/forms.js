c49d29eba2e8dc3743c3bb525482882d109d9053
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 17:54:32 +0000

    6a8cca7 feat(ivy): use i18n locale data to determine the plural form of ICU expressions (#29249)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes
+ * @license Angular v8.0.0-beta.12+6.sha-6a8cca7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+6.sha-6a8cca7.with-local-changes');
 
 /**
  * @license
