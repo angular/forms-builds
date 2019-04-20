f2c82b3cb2e4b18c32dec39c9f63e282fe2fbec8
Ben Lesh <ben@benlesh.com>
2019-04-20 02:51:03 +0000

    3f6bf6d fix(common): properly check NaN value (#22305)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+60.sha-c3c0df9.with-local-changes
+ * @license Angular v8.0.0-beta.13+61.sha-3f6bf6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+60.sha-c3c0df9.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+61.sha-3f6bf6d.with-local-changes');
 
 /**
  * @license
