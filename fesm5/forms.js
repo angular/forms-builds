71a50494db386564fd0d5756078007ce086bb3ea
Ben Lesh <ben@benlesh.com>
2019-04-23 02:12:03 +0000

    9873356 docs: fix grammatical errors in the guides and API documentation (#29928)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+70.sha-63523f7.with-local-changes
+ * @license Angular v8.0.0-beta.13+80.sha-9873356.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6665,7 +6665,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+70.sha-63523f7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+80.sha-9873356.with-local-changes');
 
 /**
  * @license
