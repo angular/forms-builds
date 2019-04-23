815d5eac8d691681aba27a6e326e1315a402a8aa
Brandon Roberts <robertsbt@gmail.com>
2019-04-23 17:17:19 +0000

    b61c9df fixup! docs: explicitly state purpose of components

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+62.sha-5c8d156.with-local-changes
+ * @license Angular v8.0.0-beta.13+91.sha-b61c9df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6754,7 +6754,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.0.0-beta.13+62.sha-5c8d156.with-local-changes');
+    var VERSION = new core.Version('8.0.0-beta.13+91.sha-b61c9df.with-local-changes');
 
     /**
      * @license
