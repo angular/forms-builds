5b318ca4fe88cb450b8ef486032aa41902b80262
Ben Lesh <ben@benlesh.com>
2019-04-23 00:25:08 +0000

    0bcb232 feat(ivy): generate ɵɵpropertyInterpolateX instructions (#30008)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+76.sha-0cab437.with-local-changes
+ * @license Angular v8.0.0-beta.13+77.sha-0bcb232.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+76.sha-0cab437.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+77.sha-0bcb232.with-local-changes');
 
 /**
  * @license
