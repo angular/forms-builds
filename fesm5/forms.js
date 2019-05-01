ff62edf0b6f89aed5928bd2a606695cc81aa9568
Kara Erickson <karakara@google.com>
2019-05-01 04:51:03 +0000

    7d6f488 fix(ivy): properly tree-shake away StaticInjector (#30219)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+73.sha-b1506a3.with-local-changes
+ * @license Angular v8.0.0-rc.0+74.sha-7d6f488.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+73.sha-b1506a3.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+74.sha-7d6f488.with-local-changes');
 
 /**
  * @license
