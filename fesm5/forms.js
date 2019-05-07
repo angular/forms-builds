045595f41bb3a5856dfeb94e50d8138cae71eaf3
Kara Erickson <karakara@google.com>
2019-05-07 21:33:43 +0000

    be8fbac refactor(ivy): break apart stylingMap into styleMap and classMap instructions (#30293)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+91.sha-b40f6f3.with-local-changes
+ * @license Angular v8.0.0-rc.0+97.sha-be8fbac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6665,7 +6665,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+91.sha-b40f6f3.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+97.sha-be8fbac.with-local-changes');
 
 /**
  * @license
