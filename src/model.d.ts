7a374caa45417fb7a474c8685df2b10358746e39
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:11:26 +0000

    3aff79c fix(bazel): pass correct arguments to http_server in Windows (#30346)

--- src/model.d.ts
+++ src/model.d.ts
@@ -81,9 +81,7 @@ export declare abstract class AbstractControl {
      * The current value of the control.
      *
      * * For a `FormControl`, the current value.
-     * * For an enabled `FormGroup`, the values of enabled controls as an object
-     * with a key-value pair for each member of the group.
-     * * For a disabled `FormGroup`, the values of all controls as an object
+     * * For a `FormGroup`, the values of enabled controls as an object
      * with a key-value pair for each member of the group.
      * * For a `FormArray`, the values of enabled controls as an array.
      *
