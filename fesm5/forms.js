bb0ff41c6d45e6619717f2784b6e6755f92c18a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:09:47 +0000

    0cf09fc test(ivy): use `TestBed` for render3 i18n tests (#30352) PR Close #30352

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+151.sha-877b228.with-local-changes
+ * @license Angular v8.0.0-rc.0+152.sha-0cf09fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6665,7 +6665,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+151.sha-877b228.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+152.sha-0cf09fc.with-local-changes');
 
 /**
  * @license
