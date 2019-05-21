4d79835a5bb9c004aea0e4df95b44659efc61efa
Jason Aden <jasonaden@google.com>
2019-05-21 20:47:16 +0000

    f3c69e7 refactor(ivy): rewrite flatten function to be more memory efficient (#30468)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+274.sha-c9f5f3d.with-local-changes
+ * @license Angular v8.0.0-rc.0+275.sha-f3c69e7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+274.sha-c9f5f3d.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+275.sha-f3c69e7.with-local-changes');
 
 /**
  * @license
