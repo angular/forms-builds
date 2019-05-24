7678fd40f0b4d8d3442ebfe683c8906910c63f06
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:16:37 +0000

    b5756bc docs(bazel): remove out of date known issue with Webstorm (#30650)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+4.sha-60f0462.with-local-changes
+ * @license Angular v8.0.0-rc.5+5.sha-b5756bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.5+4.sha-60f0462.with-local-changes');
+const VERSION = new Version('8.0.0-rc.5+5.sha-b5756bc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
