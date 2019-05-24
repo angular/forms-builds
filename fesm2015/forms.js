1b3714d53a08afdf86bc50b6d8e1b0e0fff605c4
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:38:57 +0000

    45718b3 build(docs-infra): upgrade cli command docs sources to 9a3d36c98 (#30609)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+5.sha-b5756bc.with-local-changes
+ * @license Angular v8.0.0-rc.5+6.sha-45718b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.5+5.sha-b5756bc.with-local-changes');
+const VERSION = new Version('8.0.0-rc.5+6.sha-45718b3.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
