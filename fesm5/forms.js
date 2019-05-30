4c2f72c004ee7a4f671b4a86ba501181f107bdd6
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 20:15:44 +0000

    41f372f ci: fix payload limits value

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes
+ * @license Angular v8.0.0-rc.0+383.sha-41f372f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+383.sha-41f372f.with-local-changes');
 
 /**
  * @license
