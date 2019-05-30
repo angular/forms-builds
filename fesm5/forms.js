fe2a6a5a107bbc2922626df36d98bd901c2e050d
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:22:51 +0000

    1c67b90 docs: clarify usage of using /deep/ in component styles guide (#30452)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+9.sha-6d9f5fc.with-local-changes
+ * @license Angular v8.0.0+6.sha-1c67b90.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+9.sha-6d9f5fc.with-local-changes');
+var VERSION = new Version('8.0.0+6.sha-1c67b90.with-local-changes');
 
 /**
  * @license
