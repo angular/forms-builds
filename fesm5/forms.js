25f5bf4fca77a9d20a75c383fcdfcfd250cf5de5
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:21:17 +0000

    7757826 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+368.sha-0b23adc.with-local-changes
+ * @license Angular v8.0.0-rc.0+360.sha-7757826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+368.sha-0b23adc.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+360.sha-7757826.with-local-changes');
 
 /**
  * @license
