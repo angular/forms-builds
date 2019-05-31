d438d9cc42b58053f6add3aa02dcdd97bb96a7a7
Miško Hevery <misko@hevery.com>
2019-05-31 17:04:05 +0000

    cdf586d build(docs-infra): upgrade cli command docs sources to e8bdfe6b9 (#30777)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+17.sha-0f352b6.with-local-changes
+ * @license Angular v8.0.0+19.sha-cdf586d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+17.sha-0f352b6.with-local-changes');
+const VERSION = new Version('8.0.0+19.sha-cdf586d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
