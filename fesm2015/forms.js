fa78788b62c24ab9824c6be8c05759de2f8a727e
Misko Hevery <misko@hevery.com>
2019-06-03 16:31:39 +0000

    9e4b2f1 docs: fix a small typo in injectable decorator description (#30785)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+25.sha-81b7559.with-local-changes
+ * @license Angular v8.0.0+27.sha-9e4b2f1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0+25.sha-81b7559.with-local-changes');
+const VERSION = new Version('8.0.0+27.sha-9e4b2f1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
