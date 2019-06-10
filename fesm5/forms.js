ff071bdf5121f1c5ba72c92f69779362c698188a
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-10 19:15:12 +0000

    4f1008b test: modify for windows

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+32.sha-99331c0.with-local-changes
+ * @license Angular v8.1.0-beta.0+33.sha-4f1008b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-beta.0+32.sha-99331c0.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+33.sha-4f1008b.with-local-changes');
 
 /**
  * @license
