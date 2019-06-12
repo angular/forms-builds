40a68bd5b622c10423421f0cfb6e88c9e6af6a94
Andrew Kushnir <akushnir@google.com>
2019-06-12 19:09:03 +0000

    cbb3794 fix(docs-infra): enable only vertical scrolling for contributors bio (#30991)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+67.sha-77a5790.with-local-changes
+ * @license Angular v8.0.0+68.sha-cbb3794.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0+67.sha-77a5790.with-local-changes');
+var VERSION = new Version('8.0.0+68.sha-cbb3794.with-local-changes');
 
 /**
  * @license
