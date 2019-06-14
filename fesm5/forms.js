4b47a8df0a4e8ba21dd0203494dd8041b5f8f5e9
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:09:42 +0000

    0c3bb6a fix(ivy): ngcc - capture entry-points in top-level path-mapped folders (#31027)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+8.sha-5d24a75.with-local-changes
+ * @license Angular v8.1.0-next.2+10.sha-0c3bb6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.2+8.sha-5d24a75.with-local-changes');
+var VERSION = new Version('8.1.0-next.2+10.sha-0c3bb6a.with-local-changes');
 
 /**
  * @license
