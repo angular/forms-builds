3afb0f8ba9f40157b99b52bcdc5e88e23be936dc
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:15:41 +0000

    65544ac74 fix(ivy): reprojected ICU expression nodes when creating embedded views (#30979)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+32.sha-036294d.with-local-changes
+ * @license Angular v8.1.0-next.2+35.sha-65544ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+32.sha-036294d.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+35.sha-65544ac.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
