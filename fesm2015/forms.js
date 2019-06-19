8b396fd771100fdd24105eeb57b88d48d78654c8
Kara Erickson <karakara@google.com>
2019-06-19 23:20:01 +0000

    9c222350f ci: re-enable payload size tracking (#31138)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
+ * @license Angular v8.0.2+8.sha-9c22235.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.2+7.sha-c36edff.with-local-changes');
+const VERSION = new Version('8.0.2+8.sha-9c22235.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
