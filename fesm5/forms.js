4160ed92e821d7b49f096b3d203a245c61f59172
Kara Erickson <karakara@google.com>
2019-06-20 01:47:42 +0000

    029f1be20 perf(ivy): avoid unnecessary function call in i18n instruction (#31106)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+6.sha-f039583.with-local-changes
+ * @license Angular v8.1.0-next.3+8.sha-029f1be.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+6.sha-f039583.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+8.sha-029f1be.with-local-changes');
 
 /**
  * @license
