1edf23fddfc9742d513a1e47fb033013bfe9cd18
Kara Erickson <karakara@google.com>
2019-06-20 18:50:48 +0000

    7b3bcc23a build: user bazelrc should be able to overwrite all flags (#31164)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+17.sha-b356fb0.with-local-changes
+ * @license Angular v8.1.0-next.3+18.sha-7b3bcc2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+17.sha-b356fb0.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+18.sha-7b3bcc2.with-local-changes');
 
 /**
  * @license
