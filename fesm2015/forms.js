2143780eb92850ad8f57b95dd96f3df33f4acbab
Kara Erickson <karakara@google.com>
2019-06-21 17:54:41 +0000

    de0474186 docs: clarify wording in Template Syntax (#31119)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+12.sha-15d138f.with-local-changes
+ * @license Angular v8.0.2+15.sha-de04741.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.2+12.sha-15d138f.with-local-changes');
+const VERSION = new Version('8.0.2+15.sha-de04741.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
