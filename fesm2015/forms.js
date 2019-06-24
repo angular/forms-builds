593b639cb70946d112e8159f143bc34716dfdec3
Kara Erickson <karakara@google.com>
2019-06-24 22:19:40 +0000

    980bcaf17 fix(bazel): remove unsupported Css pre-processors from ng new (#31234)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+25.sha-f470e69.with-local-changes
+ * @license Angular v8.0.2+24.sha-980bcaf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.2+25.sha-f470e69.with-local-changes');
+const VERSION = new Version('8.0.2+24.sha-980bcaf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
