3fde231d23d731d50b66961aaa136776afbfc0b7
Kara Erickson <karakara@google.com>
2019-06-24 22:22:18 +0000

    987b18596 ci(docs-infra): re-enable payload size checking for `test_aio_local` and `test_aio_local_ivy` (#31243)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+24.sha-980bcaf.with-local-changes
+ * @license Angular v8.0.2+26.sha-987b185.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.2+24.sha-980bcaf.with-local-changes');
+const VERSION = new Version('8.0.2+26.sha-987b185.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
