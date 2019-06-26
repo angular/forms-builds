359841666f044c508a0c3f23ca7a515148210927
Kara Erickson <karakara@google.com>
2019-06-26 15:26:29 +0000

    3788ebb71 fix(ivy): ngcc - don't crash if entry-points have multiple invalid dependencies (#31276)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+86.sha-4fe0e75.with-local-changes
+ * @license Angular v8.1.0-next.3+98.sha-3788ebb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-next.3+86.sha-4fe0e75.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+98.sha-3788ebb.with-local-changes');
 
 /**
  * @license
