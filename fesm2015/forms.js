65475f71b88def4aa4b8c9790bd51c96dce5bbac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 23:05:38 +0000

    7ca611cd1 fix(ivy): properly handle re-projection with an empty set of nodes to re-project (#31306)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+22.sha-0de5d79.with-local-changes
+ * @license Angular v8.1.0-rc.0+25.sha-7ca611c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-rc.0+22.sha-0de5d79.with-local-changes');
+const VERSION = new Version('8.1.0-rc.0+25.sha-7ca611c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
