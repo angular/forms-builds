4ca7e6985a3b329531a9042903a300191937efe2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 19:01:21 +0000

    299a43c7f Revert "fix(router): adjust UrlTree redirect to replace URL if in eager update (#31168)" (#31344)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+12.sha-8b03418.with-local-changes
+ * @license Angular v8.1.0-rc.0+17.sha-299a43c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.0-rc.0+12.sha-8b03418.with-local-changes');
+const VERSION = new Version('8.1.0-rc.0+17.sha-299a43c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
