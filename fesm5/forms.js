39a54f039d731603500b306b7356ff78226036f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-03 16:15:55 +0000

    dcf9c13fc ci: update gcp_token (#31405)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+34.sha-376c5fc.with-local-changes
+ * @license Angular v8.1.0+1.sha-dcf9c13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.1.0-rc.0+34.sha-376c5fc.with-local-changes');
+var VERSION = new Version('8.1.0+1.sha-dcf9c13.with-local-changes');
 
 /**
  * @license
