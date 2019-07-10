26392fd480ae13bfe0533ee77451c9c059d5088c
Jason Aden <jasonaden@google.com>
2019-07-10 17:47:33 +0000

    c69e552a8 docs: release notes for the v8.1.1 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+24.sha-2b44be9.with-local-changes
+ * @license Angular v8.2.0-next.0+26.sha-c69e552.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6828,7 +6828,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.0+24.sha-2b44be9.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+26.sha-c69e552.with-local-changes');
 
 /**
  * @license
