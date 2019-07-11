23d95dbc3ac1d2bf1a0c7ad98f82302da39275d9
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 22:45:41 +0000

    d545bbeee fix(ivy): handle ModuleWithProviders in R3TestBed correctly (#31415)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+34.sha-4bb283c.with-local-changes
+ * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+35.sha-d545bbe.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
