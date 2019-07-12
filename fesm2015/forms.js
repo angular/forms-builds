f038d47d87d56dff8f92592e762ad6e358d86966
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 15:59:31 +0000

    32aa18be7 build: add tsconfig-test to dependency for tsconfig in core/test/strict_types (#31471)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+36.sha-63e458d.with-local-changes
+ * @license Angular v8.2.0-next.1+43.sha-32aa18b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7682,7 +7682,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+36.sha-63e458d.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+43.sha-32aa18b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
