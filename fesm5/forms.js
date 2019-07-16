152736986fb04a2270dace89374e931ae0dc5189
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 21:26:31 +0000

    09576e968 fix(ivy): use goog.LOCALE for Closure Compiler to define default LOCALE_ID (#31519)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+52.sha-31ea254.with-local-changes
+ * @license Angular v8.2.0-next.1+60.sha-09576e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6667,7 +6667,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.1+52.sha-31ea254.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+60.sha-09576e9.with-local-changes');
 
 /**
  * @license
