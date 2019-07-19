33dbf54b35140b75031177a04a7612097f9167e8
Kara Erickson <karakara@google.com>
2019-07-19 23:59:55 +0000

    9c954ebc6 refactor(ivy): make styling instructions use the new styling algorithm (#30742)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+32.sha-f14693b.with-local-changes
+ * @license Angular v8.2.0-next.2+33.sha-9c954eb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6709,7 +6709,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+32.sha-f14693b.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+33.sha-9c954eb.with-local-changes');
 
 /**
  * @license
