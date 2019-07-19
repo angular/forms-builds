c2c58ae5d2297d9e996c67de94b54ec1098ec9c7
Miško Hevery <misko@hevery.com>
2019-07-19 00:04:49 +0000

    7151eae36 fix(elements): handle falsy initial value (#31604)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+7.sha-1e9eeaf.with-local-changes
+ * @license Angular v8.2.0-next.2+25.sha-7151eae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.2+7.sha-1e9eeaf.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.2+25.sha-7151eae.with-local-changes');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+7.sha-1e9eeaf.with-local-changes
+ * @license Angular v8.2.0-next.2+25.sha-7151eae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6709,7 +6709,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0-next.2+7.sha-1e9eeaf.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+25.sha-7151eae.with-local-changes');
 
 /**
  * @license
