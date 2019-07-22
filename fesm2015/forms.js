d85c97b23002c56c2b073dabbd9a9c1968bb2f98
Miško Hevery <misko@hevery.com>
2019-07-22 17:15:01 +0000

    03050deca fix(docs-infra): improve small screen experience for docs cards layout (#31576)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+13.sha-157456b.with-local-changes
+ * @license Angular v8.1.2+16.sha-03050de.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7723,7 +7723,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.1.2+13.sha-157456b.with-local-changes');
+const VERSION = new Version('8.1.2+16.sha-03050de.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
