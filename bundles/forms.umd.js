59d060ae0fc9eb4f7feaba8a9c5b99f9665ed9a2
Kara Erickson <karakara@google.com>
2019-07-23 23:06:13 +0000

    f50dede8f refactor(ivy): remove all old styling code prior to refactor (#31193)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+46.sha-0e68c7e.with-local-changes
+ * @license Angular v8.2.0-next.2+47.sha-f50dede.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.2+46.sha-0e68c7e.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.2+47.sha-f50dede.with-local-changes');
 
     /**
      * @license
