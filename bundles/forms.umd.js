c38db2665e5c6236856a57f3f0bb00e280681405
Kara Erickson <karakara@google.com>
2019-07-23 23:10:18 +0000

    9eefe25e2 fix(ivy): components created with wrong context when passing root node to component factory (#31661)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+50.sha-b31a292.with-local-changes
+ * @license Angular v8.2.0-next.2+53.sha-9eefe25.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.0-next.2+50.sha-b31a292.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.2+53.sha-9eefe25.with-local-changes');
 
     /**
      * @license
