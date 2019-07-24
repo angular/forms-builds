3c7e1f8637265445c2e4812e50508a378be2e00d
Miško Hevery <misko@hevery.com>
2019-07-24 21:56:03 +0000

    0440dc948 fix(zone.js): don't wrap uncaught promise error. (#31443)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+22.sha-5407b43.with-local-changes
+ * @license Angular v8.1.2+25.sha-0440dc9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6798,7 +6798,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.1.2+22.sha-5407b43.with-local-changes');
+    var VERSION = new core.Version('8.1.2+25.sha-0440dc9.with-local-changes');
 
     /**
      * @license
