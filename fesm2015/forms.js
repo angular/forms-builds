7ac8373f153db8c13d71986c5d3722a1a79fcbe9
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:24:23 +0000

    859ebdd83 fix(ivy): correctly bind `targetToIdentifier` to the TemplateVisitor (#31861)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+97.sha-3067309.with-local-changes
+ * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8478,7 +8478,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+97.sha-3067309.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
