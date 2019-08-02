7baab1ec47bbcdb58df38544a4c2a88798252d0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-02 01:05:12 +0000

    44b5bf486 docs: add links to web.dev/angular guides (#31804)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+5.sha-04ebd59.with-local-changes
+ * @license Angular v8.2.0+7.sha-44b5bf4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6708,7 +6708,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.0+5.sha-04ebd59.with-local-changes');
+var VERSION = new Version('8.2.0+7.sha-44b5bf4.with-local-changes');
 
 /**
  * @license
