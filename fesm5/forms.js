ffbae9e2fa442486e8b7934c909c65f4af2ee917
Kara Erickson <karakara@google.com>
2019-08-12 17:56:11 +0000

    6eb9c2fab perf(ivy): don't read global state when interpolated values don't change (#32093)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+21.sha-7b94145.with-local-changes
+ * @license Angular v9.0.0-next.1+22.sha-6eb9c2f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6876,7 +6876,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.1+21.sha-7b94145.with-local-changes');
+var VERSION = new Version('9.0.0-next.1+22.sha-6eb9c2f.with-local-changes');
 
 /**
  * @license
