129e2f6da721c9543d8e0aa0c801dccfc55c3330
Kara Erickson <karakara@google.com>
2019-08-12 20:38:09 +0000

    76854287e Revert "fix(compiler): do not remove whitespace wrapping i18n expansions (#31962)" (#32110)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.1+11.sha-972550e.with-local-changes
+ * @license Angular v8.2.1+12.sha-7685428.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6714,7 +6714,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.1+11.sha-972550e.with-local-changes');
+var VERSION = new Version('8.2.1+12.sha-7685428.with-local-changes');
 
 /**
  * @license
