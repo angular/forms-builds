264455462eca24f6b65804e8bfa386dd04f91c29
Andrew Kushnir <akushnir@google.com>
2019-08-14 21:29:50 +0000

    5a562d8a0 refactor(language-service): Return ts.CompletionInfo for getCompletionsAt() (#32116)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+29.sha-4055150.with-local-changes
+ * @license Angular v9.0.0-next.2+31.sha-5a562d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8988,7 +8988,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.2+29.sha-4055150.with-local-changes');
+const VERSION = new Version('9.0.0-next.2+31.sha-5a562d8.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
