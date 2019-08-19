eeb753771c10158d964693cec4cc9404698a7370
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:34:20 +0000

    994264c0b refactor(ivy): simplify `walkTNodeTree` method for readability (#31065)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+55.sha-e85ec23.with-local-changes
+ * @license Angular v9.0.0-next.2+58.sha-994264c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6903,7 +6903,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.2+55.sha-e85ec23.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.2+58.sha-994264c.with-local-changes');
 
     /**
      * @license
