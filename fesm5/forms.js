192edff731b439f46f6aed0c2e45c4478047c9e4
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:30:20 +0000

    e85ec2303 docs(service-worker): mention that dataGroups only cache non-mutating requests (#32142)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+53.sha-adb4f7c.with-local-changes
+ * @license Angular v9.0.0-next.2+55.sha-e85ec23.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6716,7 +6716,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+53.sha-adb4f7c.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+55.sha-e85ec23.with-local-changes');
 
 /**
  * @license
