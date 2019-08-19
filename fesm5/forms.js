648345f8eae8d8c434e7383cc092b7faf0b059ba
Andrew Kushnir <akushnir@google.com>
2019-08-19 23:07:30 +0000

    5da5ca5c2 fix(bazel): pin `@microsoft/api-extractor` (#32187)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+65.sha-639b732.with-local-changes
+ * @license Angular v9.0.0-next.2+64.sha-5da5ca5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6878,7 +6878,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.2+65.sha-639b732.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+64.sha-5da5ca5.with-local-changes');
 
 /**
  * @license
