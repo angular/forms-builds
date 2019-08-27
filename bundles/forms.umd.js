cf462d7973e7fd2a4ac75d6c71e20a5126f5fb0d
atscott <atscott@google.com>
2019-08-27 16:28:17 +0000

    7033e7eb2 docs: Use ngOnInit to fetch data from services in the getting started guide (#32273)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+24.sha-8931239.with-local-changes
+ * @license Angular v8.2.3+25.sha-7033e7e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6901,7 +6901,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.3+24.sha-8931239.with-local-changes');
+    var VERSION = new core.Version('8.2.3+25.sha-7033e7e.with-local-changes');
 
     /**
      * @license
