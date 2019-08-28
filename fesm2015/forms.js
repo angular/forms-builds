82e348ccab15dc412349316166e4ea15a3983c24
atscott <atscott@google.com>
2019-08-28 18:23:15 +0000

    b7f95bec0 build(docs-infra): add empty `codeGenApi` JSDoc tag definition (#32207)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+34.sha-d02573d.with-local-changes
+ * @license Angular v8.2.3+35.sha-b7f95be.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8982,7 +8982,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.3+34.sha-d02573d.with-local-changes');
+const VERSION = new Version('8.2.3+35.sha-b7f95be.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
