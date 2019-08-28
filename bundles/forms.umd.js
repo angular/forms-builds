d9453f52f84132415aa2db678f4585434203df55
atscott <atscott@google.com>
2019-08-28 16:25:36 +0000

    6aaf4eac8 docs(docs-infra): add note about setting up examples on Windows (#32355)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+28.sha-aa8dd74.with-local-changes
+ * @license Angular v8.2.3+32.sha-6aaf4ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6901,7 +6901,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.3+28.sha-aa8dd74.with-local-changes');
+    var VERSION = new core.Version('8.2.3+32.sha-6aaf4ea.with-local-changes');
 
     /**
      * @license
