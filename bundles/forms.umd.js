42f07fe45b53212b6c05e6bc6ff3e105929b00dd
Miško Hevery <misko@hevery.com>
2019-08-29 19:56:02 +0000

    e7bca0751 build: update ibazel to version that supports windows (#32390)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+6.sha-4c5cfec.with-local-changes
+ * @license Angular v8.2.4+4.sha-e7bca07.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6901,7 +6901,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.4+6.sha-4c5cfec.with-local-changes');
+    var VERSION = new core.Version('8.2.4+4.sha-e7bca07.with-local-changes');
 
     /**
      * @license
