36b02b2495da4cb15b0e0cc55ea740d2aae10c9b
Misko Hevery <misko@hevery.com>
2019-08-30 20:08:03 +0000

    c56c2416a refactor(core): undecorated-classes-with-decorated-fields migration commits empty updates (#32391)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+60.sha-c8b0655.with-local-changes
+ * @license Angular v9.0.0-next.4+56.sha-c56c241.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6716,7 +6716,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+60.sha-c8b0655.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+56.sha-c56c241.with-local-changes');
 
 /**
  * @license
