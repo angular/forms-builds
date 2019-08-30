46b9b0243e772b4bc2e183b11311783125145a62
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:12:16 +0000

    3a45bef0e docs: add clarification regarding development in older browsers in Deployment guide (#32210)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+5.sha-9d2348b.with-local-changes
+ * @license Angular v8.2.4+8.sha-3a45bef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6714,7 +6714,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.4+5.sha-9d2348b.with-local-changes');
+var VERSION = new Version('8.2.4+8.sha-3a45bef.with-local-changes');
 
 /**
  * @license
