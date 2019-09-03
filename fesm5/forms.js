bda3d6d6a4aa6cb8f8cbdff8cdc552386ad03a40
Misko Hevery <misko@hevery.com>
2019-09-03 17:38:54 +0000

    f5bec3ff5 feat(common): HttpParams fromObject accepts ReadonlyArray<string> (#31072)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+69.sha-9a55eaf.with-local-changes
+ * @license Angular v9.0.0-next.4+70.sha-f5bec3f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6716,7 +6716,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.4+69.sha-9a55eaf.with-local-changes');
+var VERSION = new Version('9.0.0-next.4+70.sha-f5bec3f.with-local-changes');
 
 /**
  * @license
