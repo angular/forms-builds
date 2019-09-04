c54fa40504c39eb616052be284f53a21df6cf9e8
Miško Hevery <misko@hevery.com>
2019-09-04 19:08:11 +0000

    5112669d2 refactor(benchpress): remove two mutable exports from largetable/util.ts (#32425)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+18.sha-3d453fe.with-local-changes
+ * @license Angular v8.2.4+19.sha-5112669.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8982,7 +8982,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.4+18.sha-3d453fe.with-local-changes');
+const VERSION = new Version('8.2.4+19.sha-5112669.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
