e6e05c87b6501f23707b96a198eb67bdffd8f882
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 23:31:59 +0000

    ea6a2e9f2 fix(ivy): template compiler should render correct $localize placeholder names (#32509)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+48.sha-9166baf.with-local-changes
+ * @license Angular v9.0.0-next.5+49.sha-ea6a2e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8989,7 +8989,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.5+48.sha-9166baf.with-local-changes');
+const VERSION = new Version('9.0.0-next.5+49.sha-ea6a2e9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
