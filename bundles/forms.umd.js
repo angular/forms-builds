f11dbfa1ef46dc757423e94b086b9e63c2d92355
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 12:18:08 +0000

    39302ba92 docs: fix typos in the routing and testing documentation (#32329)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+13.sha-1c71db8.with-local-changes
+ * @license Angular v8.2.5+14.sha-39302ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.5+13.sha-1c71db8.with-local-changes');
+    var VERSION = new core.Version('8.2.5+14.sha-39302ba.with-local-changes');
 
     /**
      * @license
