90c9281488808617ffd7ec89f2fda092257b0fa6
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 10:49:52 +0000

    1c71db846 docs: describe your change... (#32552)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+12.sha-48e4b0e.with-local-changes
+ * @license Angular v8.2.5+13.sha-1c71db8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.5+12.sha-48e4b0e.with-local-changes');
+    var VERSION = new core.Version('8.2.5+13.sha-1c71db8.with-local-changes');
 
     /**
      * @license
