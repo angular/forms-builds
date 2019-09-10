6d4e3ebec41af898e6c72f4b8754677b25185a3a
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 19:41:44 +0000

    8a7129e21 ci: disable hammerjs tests of components repository (#32485)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+63.sha-2124588.with-local-changes
+ * @license Angular v9.0.0-next.5+67.sha-8a7129e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6712,7 +6712,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.5+63.sha-2124588.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+67.sha-8a7129e.with-local-changes');
 
 /**
  * @license
