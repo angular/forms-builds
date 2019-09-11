ccb5f83ca1c2e2f6c47487f2fd7fa5aa9e5fb6d5
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 21:02:27 +0000

    bb9e61202 docs: remove duplicate entry in changelog for 8.2.6

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+2.sha-c1ac21d.with-local-changes
+ * @license Angular v9.0.0-next.6+3.sha-bb9e612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6899,7 +6899,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.6+2.sha-c1ac21d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.6+3.sha-bb9e612.with-local-changes');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+2.sha-c1ac21d.with-local-changes
+ * @license Angular v9.0.0-next.6+3.sha-bb9e612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6712,7 +6712,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.6+2.sha-c1ac21d.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+3.sha-bb9e612.with-local-changes');
 
 /**
  * @license
