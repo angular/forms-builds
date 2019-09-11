2e8abb65d0e102b5678d08368a87050823149d01
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 23:46:42 +0000

    38fe47316 docs: fix doc links to current version (#32566)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+2.sha-9488da0.with-local-changes
+ * @license Angular v8.2.6+3.sha-38fe473.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8983,7 +8983,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.6+2.sha-9488da0.with-local-changes');
+const VERSION = new Version('8.2.6+3.sha-38fe473.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
