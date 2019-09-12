e3dce5622fb18de59d3011a1e105287e8be65275
Kara Erickson <karakara@google.com>
2019-09-12 19:37:41 +0000

    581b99143 docs: add undecorated classes migration faq (#32478)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+9.sha-4014aab.with-local-changes
+ * @license Angular v8.2.6+10.sha-581b991.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.6+9.sha-4014aab.with-local-changes');
+    var VERSION = new core.Version('8.2.6+10.sha-581b991.with-local-changes');
 
     /**
      * @license
