e64811f4ad88913ef72f8bd46df6fa5d9e6012b5
Matias Niemelä <matias@yearofmoo.com>
2019-09-12 00:43:35 +0000

    08a7c6f0b ci: update codeowners to reflect GitHub team members (#32569)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+4.sha-1c5b157.with-local-changes
+ * @license Angular v8.2.6+6.sha-08a7c6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6710,7 +6710,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.6+4.sha-1c5b157.with-local-changes');
+var VERSION = new Version('8.2.6+6.sha-08a7c6f.with-local-changes');
 
 /**
  * @license
