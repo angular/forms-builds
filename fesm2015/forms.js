19a48779e58a083bd4f0e951378f2c6d2653c728
Kara Erickson <karakara@google.com>
2019-09-13 22:33:39 +0000

    6600bea81 ci: run ts-api-guardian test remotely on CI (#32677)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+11.sha-6cd61ae.with-local-changes
+ * @license Angular v8.2.6+12.sha-6600bea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8983,7 +8983,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.6+11.sha-6cd61ae.with-local-changes');
+const VERSION = new Version('8.2.6+12.sha-6600bea.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+11.sha-6cd61ae.with-local-changes
+ * @license Angular v8.2.6+12.sha-6600bea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6710,7 +6710,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.6+11.sha-6cd61ae.with-local-changes');
+var VERSION = new Version('8.2.6+12.sha-6600bea.with-local-changes');
 
 /**
  * @license
