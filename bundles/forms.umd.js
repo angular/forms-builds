84ef13c923e5a403b93ecb9f3c8164e76d1a6ccd
Kara Erickson <karakara@google.com>
2019-09-16 17:05:00 +0000

    947c076ff fix(compiler-cli): fix typo in diagnostic template info. (#32684)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+12.sha-6600bea.with-local-changes
+ * @license Angular v8.2.6+13.sha-947c076.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.6+12.sha-6600bea.with-local-changes');
+    var VERSION = new core.Version('8.2.6+13.sha-947c076.with-local-changes');
 
     /**
      * @license
