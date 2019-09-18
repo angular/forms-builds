85c1b896f2019546862ed42f736cd0647fb15088
Andrew Kushnir <akushnir@google.com>
2019-09-18 22:00:38 +0000

    708ae4c8e fix(language-service): Fix failing tests after merge (#32758)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+2.sha-9d8dc79.with-local-changes
+ * @license Angular v9.0.0-next.7+5.sha-708ae4c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.7+2.sha-9d8dc79.with-local-changes');
+var VERSION = new Version('9.0.0-next.7+5.sha-708ae4c.with-local-changes');
 
 /**
  * @license
