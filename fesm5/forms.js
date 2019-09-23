8fc015fc1e0b820b41c5e182cfc45321b023c3ae
Andrew Kushnir <akushnir@google.com>
2019-09-23 21:52:35 +0000

    5651fa3a9 fix(ivy): ensure `window.ng.getDebugNode` returns debug info for component elements (#32780)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+14.sha-353368c.with-local-changes
+ * @license Angular v9.0.0-next.7+15.sha-5651fa3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6712,7 +6712,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.7+14.sha-353368c.with-local-changes');
+var VERSION = new Version('9.0.0-next.7+15.sha-5651fa3.with-local-changes');
 
 /**
  * @license
