910f13274dcaaf5b34f5de23c407b269032c2233
Andrew Kushnir <akushnir@google.com>
2019-09-24 21:22:22 +0000

    104c786b9 docs: removed duplicated spaces in upgrade guide (#32813)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.7+3.sha-e63390d.with-local-changes
+ * @license Angular v8.2.7+5.sha-104c786.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8983,7 +8983,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.7+3.sha-e63390d.with-local-changes');
+const VERSION = new Version('8.2.7+5.sha-104c786.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
