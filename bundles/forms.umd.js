2c3ef253be70611d8611db6baa512ca2ee6b60a2
Andrew Kushnir <akushnir@google.com>
2019-09-25 01:16:04 +0000

    39bc6f7be build: temporarily disable dynamic queries migration (#32837)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+24.sha-948714c.with-local-changes
+ * @license Angular v9.0.0-next.7+25.sha-39bc6f7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6899,7 +6899,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.0-next.7+24.sha-948714c.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.7+25.sha-39bc6f7.with-local-changes');
 
     /**
      * @license
