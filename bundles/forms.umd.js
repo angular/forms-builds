528bb947998cce6c8debf20704c7fd561af18fdd
atscott <atscott@google.com>
2019-09-27 23:18:34 +0000

    bdea243fe docs: add missing space between "Component" and "will" in guide (#32885)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+8.sha-2397144.with-local-changes
+ * @license Angular v8.2.8+9.sha-bdea243.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.8+8.sha-2397144.with-local-changes');
+    var VERSION = new core.Version('8.2.8+9.sha-bdea243.with-local-changes');
 
     /**
      * @license
