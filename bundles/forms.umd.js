3e4fc70234db86ddbad50cea1345bd8373b1d5d5
atscott <atscott@google.com>
2019-09-27 00:26:55 +0000

    e81fc14da fix(docs-infra): correctly run on IE11 (#32871)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+2.sha-d5c210a.with-local-changes
+ * @license Angular v8.2.8+3.sha-e81fc14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.8+2.sha-d5c210a.with-local-changes');
+    var VERSION = new core.Version('8.2.8+3.sha-e81fc14.with-local-changes');
 
     /**
      * @license
