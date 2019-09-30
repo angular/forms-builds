5db8cf02f200d351e37868ba09984da97539de92
atscott <atscott@google.com>
2019-09-30 19:27:19 +0000

    f438ae8a3 docs: update inline lazy loading example to use the import syntax (#32260)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+21.sha-8985233.with-local-changes
+ * @license Angular v9.0.0-next.8+23.sha-f438ae8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6712,7 +6712,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.8+21.sha-8985233.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+23.sha-f438ae8.with-local-changes');
 
 /**
  * @license
