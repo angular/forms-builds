84217922ede280213001870246cfec0cd9c44a15
atscott <atscott01@gmail.com>
2019-10-02 18:17:43 +0000

    d96167fc5 docs: add lib build note (#32940)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+22.sha-57457fb.with-local-changes
+ * @license Angular v8.2.8+24.sha-d96167f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6710,7 +6710,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.2.8+22.sha-57457fb.with-local-changes');
+var VERSION = new Version('8.2.8+24.sha-d96167f.with-local-changes');
 
 /**
  * @license
