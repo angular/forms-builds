990973d148cf9acea0cf05dd37314f7de895e21d
atscott <atscott01@gmail.com>
2019-10-02 20:43:45 +0000

    954b8e53a refactor(bazel): ng_module action description should include compile mode (#32955)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+2.sha-a3b6b10.with-local-changes
+ * @license Angular v9.0.0-next.9+3.sha-954b8e5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7086,7 +7086,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.9+2.sha-a3b6b10.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.9+3.sha-954b8e5.with-local-changes');
 
     /**
      * @license
