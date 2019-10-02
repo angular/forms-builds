68a7aa8a8773000369ee412ae43e84596b5408bd
atscott <atscott01@google.com>
2019-10-02 17:21:31 +0000

    57457fb7c fix(upgrade): fix AngularJsUrlCodec to support Safari (#32959)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+20.sha-c6041b9.with-local-changes
+ * @license Angular v8.2.8+22.sha-57457fb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8983,7 +8983,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.8+20.sha-c6041b9.with-local-changes');
+const VERSION = new Version('8.2.8+22.sha-57457fb.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
