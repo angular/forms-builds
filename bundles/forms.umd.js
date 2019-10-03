94ebd66944faa718859412da7980eed0847fc1fc
atscott <atscott01@gmail.com>
2019-10-03 17:47:33 +0000

    2742649a5 docs(core): mark `EventEmitter#__isAsync` as internal to hide from API docs (#31378)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+11.sha-e24393c.with-local-changes
+ * @license Angular v8.2.9+13.sha-2742649.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6897,7 +6897,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.9+11.sha-e24393c.with-local-changes');
+    var VERSION = new core.Version('8.2.9+13.sha-2742649.with-local-changes');
 
     /**
      * @license
