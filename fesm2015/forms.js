2ca0cc166a075aedc630f5883fabecbec3f1d49b
atscott <atscott01@gmail.com>
2019-10-03 17:18:53 +0000

    e24393c35 test(upgrade): add unit tests for AngularJSUrlCodec's parse method (#32976)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+10.sha-8237e95.with-local-changes
+ * @license Angular v8.2.9+11.sha-e24393c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8983,7 +8983,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.9+10.sha-8237e95.with-local-changes');
+const VERSION = new Version('8.2.9+11.sha-e24393c.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
