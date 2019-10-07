d523c49015b3a210de927270becb922d6a3378cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 20:33:41 +0000

    2952ea57a docs: correct sentence in AoT compiler guide (#33020)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.9+26.sha-3541e59.with-local-changes
+ * @license Angular v8.2.9+27.sha-2952ea5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6941,7 +6941,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('8.2.9+26.sha-3541e59.with-local-changes');
+    var VERSION = new core.Version('8.2.9+27.sha-2952ea5.with-local-changes');
 
     /**
      * @license
