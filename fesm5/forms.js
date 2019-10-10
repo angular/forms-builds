675e42561c940bd414cab220ac5bc9b9125b3cd1
Miško Hevery <misko@hevery.com>
2019-10-10 22:40:21 +0000

    6ab5f3648 refactor: utilize type narrowing (#33075)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+27.sha-1ae77da.with-local-changes
+ * @license Angular v9.0.0-next.10+28.sha-6ab5f36.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2463,7 +2463,7 @@ function _find(control, path, delimiter) {
     if (!(path instanceof Array)) {
         path = path.split(delimiter);
     }
-    if (path instanceof Array && (path.length === 0))
+    if (path instanceof Array && path.length === 0)
         return null;
     return path.reduce(function (v, name) {
         if (v instanceof FormGroup) {
@@ -6760,7 +6760,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+27.sha-1ae77da.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+28.sha-6ab5f36.with-local-changes');
 
 /**
  * @license
