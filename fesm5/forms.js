a6a65322e97979357f2b8fea04a724345ee57375
Miško Hevery <misko@hevery.com>
2019-10-14 16:44:07 +0000

    d8249d123 feat(ivy): better error messages for unknown components (#33064)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+42.sha-64fd0d6.with-local-changes
+ * @license Angular v9.0.0-next.10+44.sha-d8249d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6896,7 +6896,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.10+42.sha-64fd0d6.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+44.sha-d8249d1.with-local-changes');
 
 /**
  * @license
