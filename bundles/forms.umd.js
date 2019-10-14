23e4a19924813fa7e1f5cb3432033caad6a1f4d9
Miško Hevery <misko@hevery.com>
2019-10-14 20:42:14 +0000

    5557dec12 refactor(core): missing-injectable migration should respect providers of directives and components (#33011)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+58.sha-b04488d.with-local-changes
+ * @license Angular v9.0.0-next.10+61.sha-5557dec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7083,7 +7083,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-next.10+58.sha-b04488d.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.10+61.sha-5557dec.with-local-changes');
 
     /**
      * @license
