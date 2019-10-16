416e236dc3687af0231b469f5c9a022bce894048
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 21:03:29 +0000

    37cbcfaa7 fix(upgrade): remove unused version export (#33180)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+1.sha-ef0b8f4.with-local-changes
+ * @license Angular v8.2.11+2.sha-37cbcfa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9027,7 +9027,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+1.sha-ef0b8f4.with-local-changes');
+const VERSION = new Version('8.2.11+2.sha-37cbcfa.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
