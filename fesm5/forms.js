74b5a8b8785759d6537b603c463d396ae9231137
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 14:45:11 +0000

    67c914819 test(language-service): Move completions test to completions_spec.ts (#33159)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+2.sha-51b9ce4.with-local-changes
+ * @license Angular v9.0.0-next.11+3.sha-67c9148.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6896,7 +6896,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.11+2.sha-51b9ce4.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+3.sha-67c9148.with-local-changes');
 
 /**
  * @license
