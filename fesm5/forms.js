10428c546caa5991274ec530193bbc027f1b03d5
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:27:43 +0000

    1a8bd22fa refactor(core): rename ngLocaleIdDef to ɵloc (#33212)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+39.sha-bb53b65.with-local-changes
+ * @license Angular v9.0.0-next.11+41.sha-1a8bd22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6896,7 +6896,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.11+39.sha-bb53b65.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+41.sha-1a8bd22.with-local-changes');
 
 /**
  * @license
