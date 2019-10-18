dd2ef057f2f44a68c0f14582cd324f16f3e89f0f
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 15:52:55 +0000

    f13e40db1 docs: change "patchMatch" to "pathMatch" (#32935)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+5.sha-fab6ea0.with-local-changes
+ * @license Angular v8.2.11+6.sha-f13e40d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9027,7 +9027,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.2.11+5.sha-fab6ea0.with-local-changes');
+const VERSION = new Version('8.2.11+6.sha-f13e40d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
