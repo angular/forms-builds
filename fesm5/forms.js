715d0c22344997948dbe4bfdaa41d0ee207f50b0
Andrew Kushnir <akushnir@google.com>
2019-10-24 21:30:51 +0000

    93a0b1ba13 refactor(language-service): Remove AttrInfo interface (#33380)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+21.sha-73530a9.with-local-changes
+ * @license Angular v9.0.0-next.13+22.sha-93a0b1b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6898,7 +6898,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.13+21.sha-73530a9.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+22.sha-93a0b1b.with-local-changes');
 
 /**
  * @license
