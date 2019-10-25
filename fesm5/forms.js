2c5562627b7a9284aec37b0058a3eb3a282f2ab7
Andrew Kushnir <akushnir@google.com>
2019-10-25 19:32:44 +0000

    8d15bfa6ee fix(ivy): allow abstract directives to have an invalid constructor (#32987)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+51.sha-e4e8dbd.with-local-changes
+ * @license Angular v9.0.0-next.13+52.sha-8d15bfa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6898,7 +6898,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-next.13+51.sha-e4e8dbd.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+52.sha-8d15bfa.with-local-changes');
 
 /**
  * @license
