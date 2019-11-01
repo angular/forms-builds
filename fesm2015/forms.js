82cd983ff1a572915ca1891cdcbad5495edfeb04
atscott <atscott01@gmail.com>
2019-11-01 18:07:28 +0000

    f02213a661 build: restore functionality of the micro benchmarks `profile_all.js` script (#33494)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+24.sha-0953642.with-local-changes
+ * @license Angular v9.0.0-rc.0+25.sha-f02213a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8978,7 +8978,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+24.sha-0953642.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+25.sha-f02213a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
