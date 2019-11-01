f13df1c726cd09a5a6c79d07553e27c0e9269b34
atscott <atscott01@gmail.com>
2019-11-01 18:01:06 +0000

    9ebac71521 fix(language-service): Should not crash if expr ends unexpectedly (#33524)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
+ * @license Angular v9.0.0-rc.0+20.sha-9ebac71.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6894,7 +6894,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+22.sha-feb5f27.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+20.sha-9ebac71.with-local-changes');
 
 /**
  * @license
