18a9733f67269ed4d2feb0762758efb7cf7f2e6b
atscott <atscott01@gmail.com>
2019-11-04 20:30:05 +0000

    3992e7f05b docs: Update link to angular + bazel example app (#33409)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+35.sha-316b30b.with-local-changes
+ * @license Angular v9.0.0-rc.0+37.sha-3992e7f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.0+35.sha-316b30b.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+37.sha-3992e7f.with-local-changes');
 
 /**
  * @license
