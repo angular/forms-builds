8b8d9fc4d936e0d30ae05f5f1d0f755d9c26064b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 22:17:55 +0000

    213e3c3e49 fix(ivy): move setClassMetadata calls into a pure iife (#33337)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+96.sha-595375f.with-local-changes
+ * @license Angular v9.0.0-rc.2+98.sha-213e3c3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.2+96.sha-595375f.with-local-changes');
+var VERSION = new Version('9.0.0-rc.2+98.sha-213e3c3.with-local-changes');
 
 /**
  * @license
