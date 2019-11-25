5e49c6235e8d5909dc2df23654b6e8be8a645138
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 17:11:33 +0000

    d80eeb6959 build: remove compile_strategy from ng_module.bzl (#33999)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+252.sha-8969f35.with-local-changes
+ * @license Angular v9.0.0-rc.1+253.sha-d80eeb6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6869,7 +6869,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+252.sha-8969f35.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+253.sha-d80eeb6.with-local-changes');
 
 /**
  * @license
