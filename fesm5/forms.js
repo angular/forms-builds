3f0cb4189f274653183365d79e93608b59ea5707
Miško Hevery <misko@hevery.com>
2019-12-04 00:38:41 +0000

    bd820fdf84 docs(core): update render factory description (#34137)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+356.sha-627814c.with-local-changes
+ * @license Angular v9.0.0-rc.1+357.sha-bd820fd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6869,7 +6869,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+356.sha-627814c.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+357.sha-bd820fd.with-local-changes');
 
 /**
  * @license
