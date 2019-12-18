156c8dda4180ca393125e47eeb9166ddfab5622d
Kara Erickson <karakara@google.com>
2019-12-18 23:27:49 +0000

    cf37c003ff feat(ivy): error in ivy when inheriting a ctor from an undecorated base (#34460)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+513.sha-3959511
+ * @license Angular v9.0.0-rc.1+518.sha-cf37c00
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6869,7 +6869,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+513.sha-3959511');
+var VERSION = new Version('9.0.0-rc.1+518.sha-cf37c00');
 
 /**
  * @license
