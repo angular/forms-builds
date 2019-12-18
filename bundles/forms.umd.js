85f44999301302c4727becfddfe231674e05a646
Kara Erickson <karakara@google.com>
2019-12-18 18:44:26 +0000

    dfecca29da refactor: TypeScript 3.7 fixes. (#34372)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+499.sha-9d1175e
+ * @license Angular v9.0.0-rc.1+502.sha-dfecca2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7066,7 +7066,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+499.sha-9d1175e');
+    var VERSION = new i0.Version('9.0.0-rc.1+502.sha-dfecca2');
 
     /**
      * @license
