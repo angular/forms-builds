e52d062fbe5b6e4f5b2378a1afc5609dcd3d22b9
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:23:41 +0000

    0fae3d0395 build(docs-infra): bump handlebars from 4.0.12 to 4.5.3 in /aio/tools/examples/shared

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-1aed00d
+ * @license Angular v9.0.0-rc.1+522.sha-0fae3d0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9015,7 +9015,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+522.sha-1aed00d');
+const VERSION = new Version('9.0.0-rc.1+522.sha-0fae3d0');
 
 /**
  * @fileoverview added by tsickle
