923ea647aad679db2c3ba2f4c79f3baa8e3e771f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 19:01:08 +0000

    0254cba4b3 docs: Fix double 'as an' in description at line 2069 (#34373)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+534.sha-effb92d
+ * @license Angular v9.0.0-rc.1+535.sha-0254cba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9770,7 +9770,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+534.sha-effb92d');
+const VERSION = new Version('9.0.0-rc.1+535.sha-0254cba');
 
 /**
  * @fileoverview added by tsickle
