29b273dcd07b4353c883f7b09fc82c46949fb09f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 19:22:55 +0000

    3053e022d3 docs(ivy): minor changes to version 9 guide (#34498)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+537.sha-b9b8920
+ * @license Angular v9.0.0-rc.1+538.sha-3053e02
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7066,7 +7066,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+537.sha-b9b8920');
+    var VERSION = new i0.Version('9.0.0-rc.1+538.sha-3053e02');
 
     /**
      * @license
