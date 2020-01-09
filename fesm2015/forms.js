5e5763738d682370b496c5ed874144e107cb5882
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-09 20:19:36 +0000

    f2952403dd fix(ngcc): insert definitions after statement (#34677)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.7+39.sha-186d310
+ * @license Angular v9.0.0-rc.8+18.sha-f295240
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9015,7 +9015,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.7+39.sha-186d310');
+const VERSION = new Version('9.0.0-rc.8+18.sha-f295240');
 
 /**
  * @fileoverview added by tsickle
