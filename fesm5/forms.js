838c1ebe4dc4643a6b5ce434f5d41478eda5ddbd
atscott <atscott@google.com>
2020-01-13 18:07:54 +0000

    ff02ddfd9b fix(ngcc): correctly detect dependencies in CommonJS (#34528)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+85.sha-a0eb57f
+ * @license Angular v9.0.0-rc.8+88.sha-ff02ddf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.8+85.sha-a0eb57f');
+var VERSION = new Version('9.0.0-rc.8+88.sha-ff02ddf');
 
 /**
  * @license
