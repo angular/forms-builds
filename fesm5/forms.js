a659ccd546ace8872aa21e88acead51941c293a8
atscott <atscott@google.com>
2020-01-14 18:29:52 +0000

    280aae6fc5 docs: edit injectable decorator doc (#34218)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+658.sha-2776810
+ * @license Angular v9.0.0-rc.1+659.sha-280aae6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6869,7 +6869,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+658.sha-2776810');
+var VERSION = new Version('9.0.0-rc.1+659.sha-280aae6');
 
 /**
  * @license
