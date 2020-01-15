ba62fcae93a27c5e33dfbb4fc02e527d665b3773
atscott <atscott@google.com>
2020-01-15 17:54:22 +0000

    b6bc3887f0 release: cut the v9.0.0-rc.9 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+132.sha-76a84bf
+ * @license Angular v9.0.0-rc.9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9015,7 +9015,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.8+132.sha-76a84bf');
+const VERSION = new Version('9.0.0-rc.9');
 
 /**
  * @fileoverview added by tsickle
