021e4e07249f6f077c8b13a3aa4d9bcb38509bd8
GitHub <noreply@github.com>
2020-01-16 19:18:40 +0000

    6971c188d3 fixup! docs: fix public api tool name

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+696.sha-32e1dc7
+ * @license Angular v9.0.0-rc.1+697.sha-6971c18
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9015,7 +9015,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+696.sha-32e1dc7');
+const VERSION = new Version('9.0.0-rc.1+697.sha-6971c18');
 
 /**
  * @fileoverview added by tsickle
