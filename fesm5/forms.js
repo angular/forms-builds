d8013156cf8d65b0ea9b4f63dee3493c18f6b048
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 21:19:35 +0000

    32489c7426 revert: refactor(ivy): remove styleSanitizer instruction in favor of an inline param (#34480) (#34910)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+747.sha-61ad50d
+ * @license Angular v9.0.0-rc.1+748.sha-32489c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6869,7 +6869,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+747.sha-61ad50d');
+var VERSION = new Version('9.0.0-rc.1+748.sha-32489c7');
 
 /**
  * @license
