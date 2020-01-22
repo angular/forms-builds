0cb45190101c8722df363c8a81082eb9a0413589
Andrew Kushnir <akushnir@google.com>
2020-01-22 23:20:22 +0000

    92c411f86d docs: update payload size limits for angular.io application (#34913)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+750.sha-e1fc44f
+ * @license Angular v9.0.0-rc.1+755.sha-92c411f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9770,7 +9770,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+750.sha-e1fc44f');
+const VERSION = new Version('9.0.0-rc.1+755.sha-92c411f');
 
 /**
  * @fileoverview added by tsickle
