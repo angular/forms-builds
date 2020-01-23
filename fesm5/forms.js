08a5bf82312c7f6e5d51d1ced97b93d9e2aa65ee
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:53:35 +0000

    6b468f9b2e fix(ngcc): libraries using spread in object literals cannot be processed (#34661)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+766.sha-93ffc67
+ * @license Angular v9.0.0-rc.1+767.sha-6b468f9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6869,7 +6869,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+766.sha-93ffc67');
+var VERSION = new Version('9.0.0-rc.1+767.sha-6b468f9');
 
 /**
  * @license
