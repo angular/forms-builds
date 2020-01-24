13e86d6f11f38ffc543ab6e602c11c85133a4788
Miško Hevery <misko@hevery.com>
2020-01-24 22:25:32 +0000

    2e1a16bf95 refactor(ivy): change styling to use programmatic API on updates (#34804)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+54.sha-622737c
+ * @license Angular v9.0.0-rc.10+67.sha-2e1a16b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+54.sha-622737c');
+const VERSION = new Version('9.0.0-rc.10+67.sha-2e1a16b');
 
 /**
  * @fileoverview added by tsickle
