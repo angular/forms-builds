e9c9b6766c72b4ca82100819ae8766923de377b9
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:06:27 +0000

    8926f764a5 docs: release notes for the v9.0.0-rc.11 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+806.sha-9bd9590
+ * @license Angular v9.0.0-rc.1+807.sha-8926f76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9754,7 +9754,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+806.sha-9bd9590');
+const VERSION = new Version('9.0.0-rc.1+807.sha-8926f76');
 
 /**
  * @fileoverview added by tsickle
