65c84e72be68184e8c892214ad53276578d2974c
Andrew Kushnir <akushnir@google.com>
2020-01-27 17:47:36 +0000

    724ce8c22b build(docs-infra): update to @angular/material 9.0.0-rc.8 (#34966)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+814.sha-3d4b143
+ * @license Angular v9.0.0-rc.1+817.sha-724ce8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+814.sha-3d4b143');
+const VERSION = new Version('9.0.0-rc.1+817.sha-724ce8c');
 
 /**
  * @fileoverview added by tsickle
