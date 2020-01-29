350e712c6b7a971d4725350142003c1e61cfaeca
Andrew Kushnir <akushnir@google.com>
2020-01-29 21:29:20 +0000

    7e0a031543 docs: remove named node version (#35014)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+53.sha-eaa4a5a
+ * @license Angular v9.0.0-rc.11+54.sha-7e0a031
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+53.sha-eaa4a5a');
+const VERSION = new Version('9.0.0-rc.11+54.sha-7e0a031');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+53.sha-eaa4a5a
+ * @license Angular v9.0.0-rc.11+54.sha-7e0a031
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+53.sha-eaa4a5a');
+var VERSION = new Version('9.0.0-rc.11+54.sha-7e0a031');
 
 /**
  * @license
