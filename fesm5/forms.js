43ab2555c253fbc234250128779be5d0f3cad191
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:08:49 +0000

    17650523a8 build: remove unneeded style code for ngClass and ngStyle (#34998)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+864.sha-ee8b8f5
+ * @license Angular v9.0.0-rc.1+862.sha-1765052
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+864.sha-ee8b8f5');
+var VERSION = new Version('9.0.0-rc.1+862.sha-1765052');
 
 /**
  * @license
