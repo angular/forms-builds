03172f9ea4b4dc0fb70beb332374738be8cf2915
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:03:06 +0000

    ee8b8f52aa feat(ivy): Change static priority resolution to be same level as directive it belongs to (#34938)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+859.sha-a3de110
+ * @license Angular v9.0.0-rc.1+864.sha-ee8b8f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+859.sha-a3de110');
+const VERSION = new Version('9.0.0-rc.1+864.sha-ee8b8f5');
 
 /**
  * @fileoverview added by tsickle
