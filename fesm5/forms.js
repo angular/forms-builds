99e9cae140a145a04886fad43b49441aa35ff3f9
Andrew Kushnir <akushnir@google.com>
2020-01-30 20:26:34 +0000

    36dfca979f Revert "refactor(compiler): refactor and export Bazel host creation. (#34686)" (#35063)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+64.sha-7bca243
+ * @license Angular v9.0.0-rc.11+67.sha-36dfca9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+64.sha-7bca243');
+var VERSION = new Version('9.0.0-rc.11+67.sha-36dfca9');
 
 /**
  * @license
