74bb585749bb64cc4e92a09287c014d2b5d4ed58
Miško Hevery <misko@hevery.com>
2020-02-03 22:46:30 +0000

    22357d4796 fix(ngcc): correctly invalidate cache when moving/removing files/directories (#35106)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.13+6.sha-6a771d9
+ * @license Angular v9.0.0-rc.13+10.sha-22357d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.13+6.sha-6a771d9');
+var VERSION = new Version('9.0.0-rc.13+10.sha-22357d4');
 
 /**
  * @license
