90bb8528f303373ebe7edeebb896f4616b6bf431
Miško Hevery <misko@hevery.com>
2020-02-04 19:04:58 +0000

    5cada5cce1 fix(ivy): recompile on template change in ngc watch mode on Windows (#34015)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+914.sha-f7d4cc9
+ * @license Angular v9.0.0-rc.1+915.sha-5cada5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9754,7 +9754,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+914.sha-f7d4cc9');
+const VERSION = new Version('9.0.0-rc.1+915.sha-5cada5c');
 
 /**
  * @fileoverview added by tsickle
