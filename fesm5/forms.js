20590500229edaad0d60234d06f13924b1bf8527
Miško Hevery <misko@hevery.com>
2020-02-04 19:07:54 +0000

    1a23c79835 docs: changes AoT to AOT for consistency (#35112)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+2.sha-a320dc9
+ * @license Angular v9.0.0-rc.14+12.sha-1a23c79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.14+2.sha-a320dc9');
+var VERSION = new Version('9.0.0-rc.14+12.sha-1a23c79');
 
 /**
  * @license
