2d3f359d9ba63b9ccd609b0748001577da51d9e1
Miško Hevery <misko@hevery.com>
2020-02-04 17:21:33 +0000

    7369dd6f24 docs: fix animations example/remove 1st person (#35046)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+907.sha-128d4ac
+ * @license Angular v9.0.0-rc.1+909.sha-7369dd6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6852,7 +6852,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+907.sha-128d4ac');
+var VERSION = new Version('9.0.0-rc.1+909.sha-7369dd6');
 
 /**
  * @license
