c76c9d36e6dc742a889c3aa7c9cc7fcfa0e5e4a3
Miško Hevery <misko@hevery.com>
2020-02-05 00:21:59 +0000

    c35671c0a4 fix(ivy): template type-check errors from TS should not use NG error codes (#35146)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+926.sha-1f6da8c
+ * @license Angular v9.0.0-rc.1+927.sha-c35671c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+926.sha-1f6da8c');
+const VERSION = new Version('9.0.0-rc.1+927.sha-c35671c');
 
 /**
  * @fileoverview added by tsickle
