c19c4578be7fc54eadfb15dc4d43a7a4bfe542fc
Misko Hevery <misko@hevery.com>
2020-02-06 02:40:43 +0000

    4fc2678438 docs(ivy): document breaking change with host attribute priority (#35175)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+929.sha-c73d718
+ * @license Angular v9.0.0-rc.1+930.sha-4fc2678
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.1+929.sha-c73d718');
+var VERSION = new Version('9.0.0-rc.1+930.sha-4fc2678');
 
 /**
  * @license
