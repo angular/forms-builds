133a9da3fc842363872d8a42aabd21b1d69af415
Misko Hevery <misko@hevery.com>
2020-02-06 17:57:52 +0000

    becab76c4a build: update scripts/release/post-check with the latest versions (#35151)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+23.sha-483ba6a
+ * @license Angular v9.0.0+5.sha-becab76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.14+23.sha-483ba6a');
+var VERSION = new Version('9.0.0+5.sha-becab76');
 
 /**
  * @license
