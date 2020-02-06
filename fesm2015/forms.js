d7d979fbfba19a60f457794a7c1015a29e216dfb
Misko Hevery <misko@hevery.com>
2020-02-06 20:02:17 +0000

    22acdc2728 docs: consilidate release notes for the v9.0.0 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+936.sha-548b0e5
+ * @license Angular v9.0.0-rc.1+937.sha-22acdc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+936.sha-548b0e5');
+const VERSION = new Version('9.0.0-rc.1+937.sha-22acdc2');
 
 /**
  * @fileoverview added by tsickle
