298abc94cd726169e06cc38e937dd7692877a53f
Kara Erickson <karakara@google.com>
2020-02-07 19:57:26 +0000

    dce230eb00 docs: fix awkard space due to margin in sections with `alert` class (#35113)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+19.sha-14fc181
+ * @license Angular v9.0.0+30.sha-dce230e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+19.sha-14fc181');
+const VERSION = new Version('9.0.0+30.sha-dce230e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+19.sha-14fc181
+ * @license Angular v9.0.0+30.sha-dce230e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6715,7 +6715,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0+19.sha-14fc181');
+var VERSION = new Version('9.0.0+30.sha-dce230e');
 
 /**
  * @license
