b769d5b78a4b1f48673477cf939ce15164a10949
Kara Erickson <karakara@google.com>
2020-02-08 01:43:08 +0000

    480a4c3061 fix(ivy): set namespace for host elements of dynamically created components (#35136)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+36.sha-18ed9dc
+ * @license Angular v9.0.0+37.sha-480a4c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9016,7 +9016,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0+36.sha-18ed9dc');
+const VERSION = new Version('9.0.0+37.sha-480a4c3');
 
 /**
  * @fileoverview added by tsickle
