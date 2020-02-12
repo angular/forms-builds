39daad17da79ab6524db5973e8db5950a6fe1a76
Kara Erickson <karakara@google.com>
2020-02-12 19:12:58 +0000

    c3b5ce4bb2 release: cut the v9.0.1 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+53.sha-554c2cb
+ * @license Angular v9.0.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6740,7 +6740,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0+53.sha-554c2cb');
+var VERSION = new Version('9.0.1');
 
 /**
  * @license
