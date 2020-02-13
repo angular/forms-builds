0c2c11a1856121f2fa96ce95ebfcf8e99a9bcda3
Kara Erickson <karakara@google.com>
2020-02-13 18:36:43 +0000

    b2222e7e02 docs: minor typo fix (#35423) PR Close #35423

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+8.sha-dc63ceb
+ * @license Angular v9.1.0-next.0+16.sha-b2222e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6740,7 +6740,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+8.sha-dc63ceb');
+var VERSION = new Version('9.1.0-next.0+16.sha-b2222e7');
 
 /**
  * @license
