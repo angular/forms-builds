0f8d2115c450068fdc58d70aed44ea88804b39df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:17:51 +0000

    c013dd4ca6 fix(core): better handing of ICUs outside of i18n blocks (#35347)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+32.sha-1939676
+ * @license Angular v9.1.0-next.0+34.sha-c013dd4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.0+32.sha-1939676');
+var VERSION = new Version('9.1.0-next.0+34.sha-c013dd4');
 
 /**
  * @license
