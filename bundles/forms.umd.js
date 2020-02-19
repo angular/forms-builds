3c795fb76a81a93785cbc57df172d6e0823439d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 20:33:50 +0000

    5edeee69dd release: cut the v9.0.2 release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+44.sha-ce85cbf
+ * @license Angular v9.0.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6919,7 +6919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.1+44.sha-ce85cbf');
+    var VERSION = new core.Version('9.0.2');
 
     /**
      * @license
