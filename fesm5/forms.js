ba073480e1866a1a1af725251b51390f4e29ebc6
Miško Hevery <misko@hevery.com>
2020-02-25 21:48:01 +0000

    d6904882d0 fix(ivy): error when accessing NgModuleRef.componentFactoryResolver in constructor (#35637)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+47.sha-8cac5fe
+ * @license Angular v9.0.2+54.sha-d690488
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.2+47.sha-8cac5fe');
+var VERSION = new Version('9.0.2+54.sha-d690488');
 
 /**
  * @license
