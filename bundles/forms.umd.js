da37d7229bc84b7ee2c59279f9074fcd9dd6a4c2
atscott <atscott@google.com>
2020-02-28 16:04:00 +0000

    fcad075ade fix(ivy): narrow `NgIf` context variables in template type checker (#35125)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+4.sha-52fc087
+ * @license Angular v9.0.4+10.sha-fcad075
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6919,7 +6919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.4+4.sha-52fc087');
+    var VERSION = new core.Version('9.0.4+10.sha-fcad075');
 
     /**
      * @license
