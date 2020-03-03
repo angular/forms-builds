41155fee17221dbcf8b199c49885335025298799
atscott <atscott@google.com>
2020-03-03 17:15:04 +0000

    c195d22f68 fix(core): remove side effects from `ɵɵgetInheritedFactory()` (#35769)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+41.sha-47a1811
+ * @license Angular v9.1.0-next.2+46.sha-c195d22
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+41.sha-47a1811');
+var VERSION = new Version('9.1.0-next.2+46.sha-c195d22');
 
 /**
  * @license
