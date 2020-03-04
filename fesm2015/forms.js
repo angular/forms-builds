7806668a3ac176fd021c8caaaa467f80153d82e0
Matias Niemelä <matias@yearofmoo.com>
2020-03-04 22:15:53 +0000

    773d7b86aa fix(router): state data missing in routerLink (#33203)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5
+ * @license Angular v9.0.5+1.sha-773d7b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9022,7 +9022,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.5');
+const VERSION = new Version('9.0.5+1.sha-773d7b8');
 
 /**
  * @fileoverview added by tsickle
