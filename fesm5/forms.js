44912adbc3c15072e823b3c99c169efc89087dc0
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 23:42:36 +0000

    c55f900081 fix(ngcc): a new LockFile implementation that uses a child-process (#35861)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+80.sha-98a9daf
+ * @license Angular v9.1.0-next.2+84.sha-c55f900
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+80.sha-98a9daf');
+var VERSION = new Version('9.1.0-next.2+84.sha-c55f900');
 
 /**
  * @license
