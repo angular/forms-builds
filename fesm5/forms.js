bfd16a007ff19c935866e205076f9e39324e33ab
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:28:53 +0000

    716d50aa21 refactor(compiler): Break up parseTemplateBindings() for microsyntax (#35812)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+76.sha-8ef29b6
+ * @license Angular v9.1.0-next.2+79.sha-716d50a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+76.sha-8ef29b6');
+var VERSION = new Version('9.1.0-next.2+79.sha-716d50a');
 
 /**
  * @license
