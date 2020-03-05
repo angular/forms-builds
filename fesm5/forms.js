340f7fb2a976a49bc4bf4d957d957def33193116
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 23:06:28 +0000

    98a9daf4f4 Revert "fix(router): state data missing in routerLink (#33203)" (#35882)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+79.sha-716d50a
+ * @license Angular v9.1.0-next.2+80.sha-98a9daf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+79.sha-716d50a');
+var VERSION = new Version('9.1.0-next.2+80.sha-98a9daf');
 
 /**
  * @license
