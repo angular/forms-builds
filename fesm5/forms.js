31c9878d8d550f44be1c3b37ac654952aca2e35b
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:25:53 +0000

    86a45ff561 docs(zone.js): update coments of zone configuration API definition (#35849)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+94.sha-84c2458
+ * @license Angular v9.1.0-next.2+95.sha-86a45ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+94.sha-84c2458');
+var VERSION = new Version('9.1.0-next.2+95.sha-86a45ff');
 
 /**
  * @license
