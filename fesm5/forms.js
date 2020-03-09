8565454eee5ef621dc15c02005b4570e91e1ef1e
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 18:49:29 +0000

    fc4c3c3eb5 fix(localize): merge translation from all  XLIFF `<file>` elements (#35936)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+111.sha-9ff9a07
+ * @license Angular v9.1.0-next.2+110.sha-fc4c3c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+111.sha-9ff9a07');
+var VERSION = new Version('9.1.0-next.2+110.sha-fc4c3c3');
 
 /**
  * @license
