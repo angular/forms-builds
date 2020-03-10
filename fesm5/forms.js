886eeaec0a43cb06e426b86fa6f253cd09dca187
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:01:24 +0000

    9368053be5 build: set dev-infra package to be private (#35910)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+127.sha-e4b1e6c
+ * @license Angular v9.1.0-next.2+123.sha-9368053
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+127.sha-e4b1e6c');
+var VERSION = new Version('9.1.0-next.2+123.sha-9368053');
 
 /**
  * @license
