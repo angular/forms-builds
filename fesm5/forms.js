222f737a10a1934d6bb1c53b3fccc370911f0384
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 19:00:09 +0000

    0bf6e58db2 fix(compiler): process `imports` first and `declarations` second while calculating scopes (#35850)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+126.sha-65a6848
+ * @license Angular v9.1.0-next.2+125.sha-0bf6e58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+126.sha-65a6848');
+var VERSION = new Version('9.1.0-next.2+125.sha-0bf6e58');
 
 /**
  * @license
