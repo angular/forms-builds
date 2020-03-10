b0ce80e8760570d6b17fbc1d16fe7b0125bb2cd0
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:12:13 +0000

    81cb54fc15 refactor(compiler): optionalOperator -> consumeOptionalOperator (#35980)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+114.sha-a73e125
+ * @license Angular v9.1.0-next.2+117.sha-81cb54f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.2+114.sha-a73e125');
+var VERSION = new Version('9.1.0-next.2+117.sha-81cb54f');
 
 /**
  * @license
