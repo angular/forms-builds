bc776ce0f633fa17d7e9b1a65b1f655e6642db96
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:15:18 +0000

    64d6f13b87 fix(platform-browser): add missing peerDependency on `@angular/animations` (#35949)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+120.sha-287bfef
+ * @license Angular v9.1.0-next.2+119.sha-64d6f13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6919,7 +6919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-next.2+120.sha-287bfef');
+    var VERSION = new core.Version('9.1.0-next.2+119.sha-64d6f13');
 
     /**
      * @license
