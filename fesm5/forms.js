43db437c2c0f6a610c86da8e6e3d52c2fb5dd917
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 22:03:44 +0000

    ce728dd323 build: adding a script to compare commits in master and stable branches (#35130)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+43.sha-20dd0ac
+ * @license Angular v9.0.5+44.sha-ce728dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.5+43.sha-20dd0ac');
+var VERSION = new Version('9.0.5+44.sha-ce728dd');
 
 /**
  * @license
