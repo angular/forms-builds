f81cdab850dc21b496b0bbd32b01bfab08bbf107
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:38:34 +0000

    19cfaf7f4c fix(core): don't re-invoke pure pipes that throw and arguments are the same (#35827)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+1.sha-3aa5ae9
+ * @license Angular v9.1.0-next.4+4.sha-19cfaf7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+1.sha-3aa5ae9');
+var VERSION = new Version('9.1.0-next.4+4.sha-19cfaf7');
 
 /**
  * @license
