f166201e0617a5476793d165850995f82ba3c17d
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:39:18 +0000

    6f95bc915d build: update to rules_nodejs 1.4.1 (#35999)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+4.sha-19cfaf7
+ * @license Angular v9.1.0-next.4+3.sha-6f95bc9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+4.sha-19cfaf7');
+var VERSION = new Version('9.1.0-next.4+3.sha-6f95bc9');
 
 /**
  * @license
