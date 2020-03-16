ea5189ab71a2ebc74878e7d6412ed367a448011d
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:20:53 +0000

    31bec8ce61 feat(compiler): Propagate source span and value span to Variable AST (#36047)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+58.sha-e179c58
+ * @license Angular v9.1.0-next.4+59.sha-31bec8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9756,7 +9756,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+58.sha-e179c58');
+const VERSION = new Version('9.1.0-next.4+59.sha-31bec8c');
 
 /**
  * @fileoverview added by tsickle
