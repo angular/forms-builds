cd0e02a912aca5b61f6f30443e9f60b4ebd53aac
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:17:49 +0000

    e179c5827f fix(compiler): do not recurse to find static symbols of same module (#35262)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+57.sha-79659ee
+ * @license Angular v9.1.0-next.4+58.sha-e179c58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+57.sha-79659ee');
+var VERSION = new Version('9.1.0-next.4+58.sha-e179c58');
 
 /**
  * @license
