43fb659b7ae517d0d6335b7caf1bb386172d27ab
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:51:07 +0000

    8da4adc4dd feat(dev-infra): create common config file loading util (#36091)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+42.sha-ec750ca
+ * @license Angular v9.0.6+44.sha-8da4adc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6919,7 +6919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.0.6+42.sha-ec750ca');
+    var VERSION = new core.Version('9.0.6+44.sha-8da4adc');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+42.sha-ec750ca
+ * @license Angular v9.0.6+44.sha-8da4adc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.6+42.sha-ec750ca');
+var VERSION = new Version('9.0.6+44.sha-8da4adc');
 
 /**
  * @license
