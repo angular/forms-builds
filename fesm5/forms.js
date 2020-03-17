a3cacfbfa8951ba1653e224f3b8d1a79acc2bad5
Andrew Kushnir <akushnir@google.com>
2020-03-17 16:51:28 +0000

    61c37954c6 docs: remove browserstack badge from readme (#35684)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+62.sha-168a393
+ * @license Angular v9.1.0-next.4+64.sha-61c3795
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+62.sha-168a393');
+var VERSION = new Version('9.1.0-next.4+64.sha-61c3795');
 
 /**
  * @license
