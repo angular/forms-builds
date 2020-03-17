fb8806fc255ab68a206b8a85cb9e9d1122738291
Andrew Kushnir <akushnir@google.com>
2020-03-17 00:07:07 +0000

    168a393589 refactor(core): misc refactoring to support upcoming changes to improve synthetic host bindings (#35884)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+61.sha-e552591
+ * @license Angular v9.1.0-next.4+62.sha-168a393
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-next.4+61.sha-e552591');
+var VERSION = new Version('9.1.0-next.4+62.sha-168a393');
 
 /**
  * @license
