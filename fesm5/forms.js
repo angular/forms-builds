a38507a09914d983b27424f8984df33ee7f8ec2f
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:20:18 +0000

    647eb879ab docs: adding a note on extra text interpolations and HTML tags in translations to Ivy guide (#36029)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+58.sha-76d7188
+ * @license Angular v9.0.6+80.sha-647eb87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.6+58.sha-76d7188');
+var VERSION = new Version('9.0.6+80.sha-647eb87');
 
 /**
  * @license
