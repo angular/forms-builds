4da051ac94e1d921737e634209aca64a0711f319
Misko Hevery <misko@hevery.com>
2020-03-20 20:19:05 +0000

    ae3eaf8b16 test(compiler): remove whitespace in spans (#36169)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+6.sha-9126921
+ * @license Angular v9.1.0-rc.0+8.sha-ae3eaf8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+6.sha-9126921');
+var VERSION = new Version('9.1.0-rc.0+8.sha-ae3eaf8');
 
 /**
  * @license
