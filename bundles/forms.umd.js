c48ebd40af0e031bd11eb7e2cb8404ddb6fe9578
Misko Hevery <misko@hevery.com>
2020-03-20 20:17:17 +0000

    ae3eaf8b16 test(compiler): remove whitespace in spans (#36169)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+6.sha-9126921
+ * @license Angular v9.1.0-rc.0+8.sha-ae3eaf8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6919,7 +6919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+6.sha-9126921');
+    var VERSION = new core.Version('9.1.0-rc.0+8.sha-ae3eaf8');
 
     /**
      * @license
