851d3c6235844088ce06a85075c80ffbb1eb42d8
Misko Hevery <misko@hevery.com>
2020-03-20 21:19:50 +0000

    d80e51a6b1 build(docs-infra): upgrade cli command docs sources to 1bc653bac (#36160)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+8.sha-05eeb7d
+ * @license Angular v9.1.0-rc.0+12.sha-d80e51a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+8.sha-05eeb7d');
+var VERSION = new Version('9.1.0-rc.0+12.sha-d80e51a');
 
 /**
  * @license
