38defabcc44716d57c348f1e66ac7f7321fd499e
Misko Hevery <misko@hevery.com>
2020-03-23 19:05:22 +0000

    9d415f9c3f fix(docs-infra): change `app-list-item` to `app-item-list` (#35601)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+18.sha-1cb7b88
+ * @license Angular v9.1.0-rc.0+20.sha-9d415f9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6859,7 +6859,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+18.sha-1cb7b88');
+var VERSION = new Version('9.1.0-rc.0+20.sha-9d415f9');
 
 /**
  * @license
