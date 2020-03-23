fc661a3d5049ffa2094d0268f43903766599ace5
Misko Hevery <misko@hevery.com>
2020-03-23 19:43:25 +0000

    f71d132f7c fix(core): workaround Terser inlining bug (#36200)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+19.sha-ba3edda
+ * @license Angular v9.1.0-rc.0+20.sha-f71d132
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+19.sha-ba3edda');
+var VERSION = new Version('9.1.0-rc.0+20.sha-f71d132');
 
 /**
  * @license
