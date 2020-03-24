f2472f88491b361873d56d78c7ad85c8ceb61604
Misko Hevery <misko@hevery.com>
2020-03-24 23:37:24 +0000

    b08168bb90 release: cut the v9.1.0-rc.2 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.1+13.sha-407fa42
+ * @license Angular v9.1.0-rc.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9018,7 +9018,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.1+13.sha-407fa42');
+const VERSION = new Version('9.1.0-rc.2');
 
 /**
  * @fileoverview added by tsickle
