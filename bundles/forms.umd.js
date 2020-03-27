446917556d780c6e07f0b3602fe80771ef9c9f2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:22:32 +0000

    bfa7b1a494 docs: correct a misleading sentence (#36155) (#36158)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+38.sha-fc3e5cb
+ * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6919,7 +6919,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.0-rc.0+38.sha-fc3e5cb');
+    var VERSION = new core.Version('9.1.0-rc.0+46.sha-bfa7b1a');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+38.sha-fc3e5cb
+ * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0-rc.0+38.sha-fc3e5cb');
+var VERSION = new Version('9.1.0-rc.0+46.sha-bfa7b1a');
 
 /**
  * @license
