540de8f93d4e192f6e6b9eafb1d1e4bb160e8185
Kara Erickson <karakara@google.com>
2020-04-02 19:05:18 +0000

    9f486c31d2 build: enable `strictTemplates` in AIO (#36391)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+49.sha-c3ce190
+ * @license Angular v9.1.0+56.sha-9f486c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6722,7 +6722,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.0+49.sha-c3ce190');
+var VERSION = new Version('9.1.0+56.sha-9f486c3');
 
 /**
  * @license
