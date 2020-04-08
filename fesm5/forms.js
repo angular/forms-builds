ac5356630f534e41824b838f0771fbaac2624094
atscott <atscott@google.com>
2020-04-08 22:48:36 +0000

    5da621d3dd fix(language-service): remove circular dependency instance (#36463)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+11.sha-cbed582
+ * @license Angular v9.1.1+12.sha-5da621d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6817,7 +6817,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+11.sha-cbed582');
+var VERSION = new Version('9.1.1+12.sha-5da621d');
 
 /**
  * @license
