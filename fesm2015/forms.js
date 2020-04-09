9aba7cf69d5b744c102f2711b3e41ee7e5500536
atscott <atscott@google.com>
2020-04-09 21:03:45 +0000

    2e4244d521 fix(docs-infra): fix `elements` example when used with ES5 (#36536)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
+ * @license Angular v10.0.0-next.1+18.sha-2e4244d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+14.sha-4aa4e6f');
+const VERSION = new Version('10.0.0-next.1+18.sha-2e4244d');
 
 /**
  * @fileoverview added by tsickle
