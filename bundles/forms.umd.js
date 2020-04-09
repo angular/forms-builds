ad39a95baa73b16ff4032a7390c99a23cc813df8
atscott <atscott@google.com>
2020-04-09 17:06:07 +0000

    c8f9092364 fix(dev-infra): fix commit message validation in git worktrees (#36507)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+15.sha-1bfa908
+ * @license Angular v9.1.1+17.sha-c8f9092
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7031,7 +7031,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('9.1.1+15.sha-1bfa908');
+    var VERSION = new core.Version('9.1.1+17.sha-c8f9092');
 
     /**
      * @license
--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+15.sha-1bfa908
+ * @license Angular v9.1.1+17.sha-c8f9092
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6817,7 +6817,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+15.sha-1bfa908');
+var VERSION = new Version('9.1.1+17.sha-c8f9092');
 
 /**
  * @license
