ba3136c0461c31f8bef50389cca37054aab54dd2
atscott <atscott@google.com>
2020-04-14 21:19:20 +0000

    0147de9044 build: fix circular deps failure (#36629)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+34.sha-b010849
+ * @license Angular v10.0.0-next.1+35.sha-0147de9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6822,7 +6822,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1+34.sha-b010849');
+var VERSION = new Version('10.0.0-next.1+35.sha-0147de9');
 
 /**
  * @license
