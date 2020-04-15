f4fc6ab6cf6137cb62d664027229f0d1beb39483
atscott <atscott@google.com>
2020-04-15 16:43:13 +0000

    663b768780 fix(ngcc): force ngcc to exit on error (#36622)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+35.sha-0147de9
+ * @license Angular v10.0.0-next.1+36.sha-663b768
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+35.sha-0147de9');
+const VERSION = new Version('10.0.0-next.1+36.sha-663b768');
 
 /**
  * @fileoverview added by tsickle
