b12fff454eaf4482b6edb245b29ba780cf0ad88d
atscott <atscott@google.com>
2020-04-15 23:23:17 +0000

    c4dc642f99 docs: release notes for the v9.1.2 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+38.sha-e041ac6
+ * @license Angular v10.0.0-next.1+39.sha-c4dc642
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+38.sha-e041ac6');
+const VERSION = new Version('10.0.0-next.1+39.sha-c4dc642');
 
 /**
  * @fileoverview added by tsickle
