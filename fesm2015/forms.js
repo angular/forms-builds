773398f680443230174fd36f885fd0cb66c06c74
atscott <atscott@google.com>
2020-04-16 17:56:11 +0000

    305b059809 docs: refactor routing doc (#35566)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+17.sha-297b7da
+ * @license Angular v10.0.0-next.2+18.sha-305b059
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+17.sha-297b7da');
+const VERSION = new Version('10.0.0-next.2+18.sha-305b059');
 
 /**
  * @fileoverview added by tsickle
