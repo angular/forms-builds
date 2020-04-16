eddec264104e91ba10346fc48bbb046e4b42a6e5
atscott <atscott@google.com>
2020-04-16 17:06:36 +0000

    4a9f0bebc3 fix(core): prevent unknown property check for AOT-compiled components (#36072)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+3.sha-b1f1d3f
+ * @license Angular v10.0.0-next.2+14.sha-4a9f0be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+3.sha-b1f1d3f');
+const VERSION = new Version('10.0.0-next.2+14.sha-4a9f0be');
 
 /**
  * @fileoverview added by tsickle
