2c3ed90835ab5b425da8691ef35e0c7008749a4c
atscott <atscott@google.com>
2020-04-16 17:03:46 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+2.sha-03ff380
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+2.sha-03ff380');
+const VERSION = new Version('10.0.0-next.2+12.sha-1e3b8a1');
 
 /**
  * @fileoverview added by tsickle
