f495833bc5b06c081bae1cf4cc2a2efa6a45dd47
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 17:57:18 +0000

    28ff02b116 docs: remove version ^7.0.0 from LTS support (#36708)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+46.sha-f4ef8a9
+ * @license Angular v10.0.0-next.2+47.sha-28ff02b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6822,7 +6822,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+46.sha-f4ef8a9');
+var VERSION = new Version('10.0.0-next.2+47.sha-28ff02b');
 
 /**
  * @license
