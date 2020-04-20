244342bd8dc8e3676c14c8dff5e0d9543630a3ac
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:50:42 +0000

    4341743b4a fix(dev-infra): exit non-zero if commit message validation failed (#36723)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+48.sha-b5e92b9
+ * @license Angular v10.0.0-next.2+49.sha-4341743
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7173,7 +7173,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.2+48.sha-b5e92b9');
+    var VERSION = new i0.Version('10.0.0-next.2+49.sha-4341743');
 
     /**
      * @license
