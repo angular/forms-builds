c19811c296ddd96b4205199978496020a252e976
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:14:26 +0000

    51b09244ff fix(dev-infra): extract commit headers before checking commit message validity (#36733)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+53.sha-f186c32
+ * @license Angular v10.0.0-next.2+51.sha-51b0924
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6959,7 +6959,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.2+53.sha-f186c32');
+var VERSION = new Version('10.0.0-next.2+51.sha-51b0924');
 
 /**
  * @license
