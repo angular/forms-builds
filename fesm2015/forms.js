28207f82f3fdcee7fa1c0ebcc50e8a4a48ba988d
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:17:12 +0000

    a6e10ef869 build: update `REQUIRED_BASE_SHA` in merge script to `commit-message` script update commit (#36750)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+52.sha-c0ed57d
+ * @license Angular v9.1.2+54.sha-a6e10ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.2+52.sha-c0ed57d');
+const VERSION = new Version('9.1.2+54.sha-a6e10ef');
 
 /**
  * @fileoverview added by tsickle
