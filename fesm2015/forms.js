41268faaf7abcbc7dece1a1fea52a75bbd9939a1
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 16:48:36 +0000

    8bd5374cfd revert: fix(common): format day-periods that cross midnight (#36611) (#36751)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+54.sha-a6e10ef
+ * @license Angular v9.1.2+56.sha-8bd5374
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.2+54.sha-a6e10ef');
+const VERSION = new Version('9.1.2+56.sha-8bd5374');
 
 /**
  * @fileoverview added by tsickle
