bb7430f56a3389bda00f7b3e239e692b14d9e0a5
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:38:03 +0000

    4275b3412f build: add AndrewKushnir to the `fw-testing` group (#36744)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+6.sha-1b3ee17
+ * @license Angular v10.0.0-next.3+7.sha-4275b34
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+6.sha-1b3ee17');
+const VERSION = new Version('10.0.0-next.3+7.sha-4275b34');
 
 /**
  * @fileoverview added by tsickle
