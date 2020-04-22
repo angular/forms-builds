e2bc549b2eb695487741e600aa92d72442b36a8a
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 19:16:37 +0000

    e1071615c6 release: cut the v9.1.3 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+56.sha-8bd5374
+ * @license Angular v9.1.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.2+56.sha-8bd5374');
+const VERSION = new Version('9.1.3');
 
 /**
  * @fileoverview added by tsickle
