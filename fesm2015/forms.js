05566927e62fe83f11eecd91d84e920dc4981f3d
Andrew Kushnir <akushnir@google.com>
2020-04-23 20:54:19 +0000

    e6161ca307 build(core): use dev-infra's component_benchmark to show PoC (#36434)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+21.sha-e37f266
+ * @license Angular v10.0.0-next.3+24.sha-e6161ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+21.sha-e37f266');
+const VERSION = new Version('10.0.0-next.3+24.sha-e6161ca');
 
 /**
  * @fileoverview added by tsickle
