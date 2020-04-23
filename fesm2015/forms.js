3e56f373d71ad473f2ad6c9b12fc7d380e776106
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:43:03 +0000

    e37f2663c2 build: require a commit body in commit messages (#36632)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+19.sha-f0ec4ae
+ * @license Angular v10.0.0-next.3+21.sha-e37f266
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9862,7 +9862,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+19.sha-f0ec4ae');
+const VERSION = new Version('10.0.0-next.3+21.sha-e37f266');
 
 /**
  * @fileoverview added by tsickle
