82126c5a85565168f07983ed1c41c874a371594b
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:27:18 +0000

    b3fe9017f7 ci: migrate payload size tracking goldens to the golden directory (#36455)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+18.sha-d840503
+ * @license Angular v9.1.3+21.sha-b3fe901
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+18.sha-d840503');
+const VERSION = new Version('9.1.3+21.sha-b3fe901');
 
 /**
  * @fileoverview added by tsickle
