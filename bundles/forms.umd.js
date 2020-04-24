b492895b5aa5d2ec743b92ee758ba65c0e891a25
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:27:04 +0000

    dd049caf0a fix(language-service): disable update the `@angular/core` module (#36783)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+24.sha-e6161ca
+ * @license Angular v10.0.0-next.3+26.sha-dd049ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7173,7 +7173,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.3+24.sha-e6161ca');
+    var VERSION = new i0.Version('10.0.0-next.3+26.sha-dd049ca');
 
     /**
      * @license
