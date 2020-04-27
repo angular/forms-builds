d85c6c04ab99d95d8f529ae615a823ddd148c592
Andrew Kushnir <akushnir@google.com>
2020-04-27 22:18:50 +0000

    0f389fa5ae fix(core): handle synthetic props in Directive host bindings correctly (#35568)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+27.sha-2a27f69
+ * @license Angular v9.1.3+28.sha-0f389fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+27.sha-2a27f69');
+const VERSION = new Version('9.1.3+28.sha-0f389fa');
 
 /**
  * @fileoverview added by tsickle
