af5e436d4144456511d5c7796b5a5a404e6ba985
Andrew Kushnir <akushnir@google.com>
2020-04-27 21:32:48 +0000

    2a27f69522 docs: fix typo (#36786)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+26.sha-180a328
+ * @license Angular v9.1.3+27.sha-2a27f69
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+26.sha-180a328');
+const VERSION = new Version('9.1.3+27.sha-2a27f69');
 
 /**
  * @fileoverview added by tsickle
