b5ab65eb77fb22647cb0e907d5d43ee1ee0d933c
Andrew Kushnir <akushnir@google.com>
2020-04-27 21:06:09 +0000

    00e6cb1d62 feat(router): allow CanLoad guard to return UrlTree (#36610)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+33.sha-b34fb04
+ * @license Angular v10.0.0-next.3+35.sha-00e6cb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7036,7 +7036,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.3+33.sha-b34fb04');
+    var VERSION = new core.Version('10.0.0-next.3+35.sha-00e6cb1');
 
     /**
      * @license
