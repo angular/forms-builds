af8f67e6f2d13be26966e9903354c60e95629d0a
Andrew Kushnir <akushnir@google.com>
2020-04-28 16:42:18 +0000

    0ce96f1d78 refactor(localize): remove unused code (#36834)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+28.sha-0f389fa
+ * @license Angular v9.1.3+34.sha-0ce96f1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+28.sha-0f389fa');
+const VERSION = new Version('9.1.3+34.sha-0ce96f1');
 
 /**
  * @fileoverview added by tsickle
