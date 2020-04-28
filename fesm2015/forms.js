a17ebaf043c82e3c84198b835a9e87bafd86fcda
Andrew Kushnir <akushnir@google.com>
2020-04-28 23:19:48 +0000

    c440165384 fix(ngcc): recognize enum declarations emitted in JavaScript (#36550)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+34.sha-0ce96f1
+ * @license Angular v9.1.3+35.sha-c440165
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+34.sha-0ce96f1');
+const VERSION = new Version('9.1.3+35.sha-c440165');
 
 /**
  * @fileoverview added by tsickle
