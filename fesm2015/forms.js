42d8890ad9251695dbd43e6d61a5ab167e2becfc
Andrew Kushnir <akushnir@google.com>
2020-04-29 18:15:13 +0000

    62930aac7b Revert "refactor(localize): move `source_file_utils.ts` up (#36834)" (#36861)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+36.sha-d743331
+ * @license Angular v9.1.3+37.sha-62930aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.3+36.sha-d743331');
+const VERSION = new Version('9.1.3+37.sha-62930aa');
 
 /**
  * @fileoverview added by tsickle
