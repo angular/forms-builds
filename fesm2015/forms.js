e36f61f52a56dd83b0053872febc260ff70d2a06
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:24:15 +0000

    12fcc7cafe build(language-service): make test project a filegroup (#36865)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+15.sha-49be32c
+ * @license Angular v10.0.0-next.4+31.sha-12fcc7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+15.sha-49be32c');
+const VERSION = new Version('10.0.0-next.4+31.sha-12fcc7c');
 
 /**
  * @fileoverview added by tsickle
