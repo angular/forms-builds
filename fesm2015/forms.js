9b06f2de9995ef4434ed7972ff50fe0717cb7efd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:20:02 +0000

    b3713a112f test(language-service): Add method to override inline template (#36890)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+37.sha-6046e86
+ * @license Angular v10.0.0-next.4+42.sha-b3713a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+37.sha-6046e86');
+const VERSION = new Version('10.0.0-next.4+42.sha-b3713a1');
 
 /**
  * @fileoverview added by tsickle
