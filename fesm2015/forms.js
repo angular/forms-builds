6efc3ddd0ca11aaa5c138ac774f7665443ec9f74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:04:46 +0000

    9d9d46f52b fix(core): log error instead of warning for unknown properties and elements (#36399)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+32.sha-d9c4840
+ * @license Angular v10.0.0-next.4+33.sha-9d9d46f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9862,7 +9862,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+32.sha-d9c4840');
+const VERSION = new Version('10.0.0-next.4+33.sha-9d9d46f');
 
 /**
  * @fileoverview added by tsickle
