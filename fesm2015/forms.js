fae45ef7d5f5674861fea153bf27de6263860fab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:17:33 +0000

    306f46ce92 build(docs-infra): upgrade docs examples to latest Angular CLI and framework (#36145)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+9.sha-70b25a3
+ * @license Angular v10.0.0-next.5+7.sha-306f46c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9130,7 +9130,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+9.sha-70b25a3');
+const VERSION = new Version('10.0.0-next.5+7.sha-306f46c');
 
 /**
  * @fileoverview added by tsickle
