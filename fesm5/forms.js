e5428df4ed5a12726bbcf1f5f18d3777da4533bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:42:07 +0000

    35d61c11fd test(core): Add benchmark for transplanted views when insertion is dirty (#36722)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+21.sha-e30e132
+ * @license Angular v10.0.0-next.5+22.sha-35d61c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6826,7 +6826,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.5+21.sha-e30e132');
+var VERSION = new Version('10.0.0-next.5+22.sha-35d61c1');
 
 /**
  * @license
