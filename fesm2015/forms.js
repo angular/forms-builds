4725aa1e40a07fc49dcb38e0ff5171052176e58d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:22:41 +0000

    420b9be1c1 refactor: disable sanitization for [style] and [style.prop] bindings (#35621)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+53.sha-3c6c00d
+ * @license Angular v10.0.0-next.5+54.sha-420b9be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9868,7 +9868,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+53.sha-3c6c00d');
+const VERSION = new Version('10.0.0-next.5+54.sha-420b9be');
 
 /**
  * @fileoverview added by tsickle
