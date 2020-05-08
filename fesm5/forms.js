7db99f2850102f5eb1f650696376476bdd2c6be1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 00:29:14 +0000

    d1b3af697c release: cut the v9.1.5 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+67.sha-dcf1dcb
+ * @license Angular v9.1.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6822,7 +6822,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.4+67.sha-dcf1dcb');
+var VERSION = new Version('9.1.5');
 
 /**
  * @license
