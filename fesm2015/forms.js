bad9e2c9c215a8233012f9c52f2313d4affd4e03
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 18:59:27 +0000

    de0c71c467 docs: release notes for the v9.1.6 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+6.sha-178e750
+ * @license Angular v10.0.0-next.6+8.sha-de0c71c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9129,7 +9129,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+6.sha-178e750');
+const VERSION = new Version('10.0.0-next.6+8.sha-de0c71c');
 
 /**
  * @fileoverview added by tsickle
