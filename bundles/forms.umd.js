eb1d9c6d88aad2549edba89a009730f7f842da5d
Misko Hevery <misko@hevery.com>
2020-05-08 22:06:12 +0000

    eb34aa551a feat(compiler): add name spans for property reads and method calls (#36826)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+11.sha-df71c7c
+ * @license Angular v10.0.0-next.6+16.sha-eb34aa5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7176,7 +7176,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.6+11.sha-df71c7c');
+    var VERSION = new i0.Version('10.0.0-next.6+16.sha-eb34aa5');
 
     /**
      * @license
