d9b0bf2b7ff806a37e3a317ca8534fcb11267fa3
Misko Hevery <misko@hevery.com>
2020-05-11 18:29:24 +0000

    286fbf42c6 fix: add aikidave as reviewer for DOCS: Marketing (#37014)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+21.sha-7eb1a58
+ * @license Angular v10.0.0-next.6+23.sha-286fbf4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7176,7 +7176,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.6+21.sha-7eb1a58');
+    var VERSION = new i0.Version('10.0.0-next.6+23.sha-286fbf4');
 
     /**
      * @license
