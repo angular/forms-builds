398f1db36bca0ad7112810f55e0f4c85f932a999
Misko Hevery <misko@hevery.com>
2020-05-13 23:19:53 +0000

    14c0ec97d8 feat(dev-infra): introduce validators for ng-dev config loading (#37049)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+9.sha-7d3f504
+ * @license Angular v10.0.0-next.7+8.sha-14c0ec9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7176,7 +7176,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.7+9.sha-7d3f504');
+    var VERSION = new i0.Version('10.0.0-next.7+8.sha-14c0ec9');
 
     /**
      * @license
