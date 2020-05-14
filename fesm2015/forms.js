0668c5fe95724777ea450378649ba3239849948b
Kara Erickson <karakara@google.com>
2020-05-14 22:41:43 +0000

    4de546eef5 test(core): verify that Ivy i18n works correctly with HTML namespaces (#36943)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+36.sha-719ca52
+ * @license Angular v9.1.6+37.sha-4de546e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.6+36.sha-719ca52');
+const VERSION = new Version('9.1.6+37.sha-4de546e');
 
 /**
  * @fileoverview added by tsickle
