c49d3b308499a272c324bd2afcf50608041f150f
Kara Erickson <karakara@google.com>
2020-05-14 20:25:42 +0000

    7acd33007d fix(dev-infra): do not require a commit body for release commits (#37110)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+48.sha-8a56c99
+ * @license Angular v10.0.0-next.7+49.sha-7acd330
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9974,7 +9974,7 @@ let FormBuilder = /** @class */ (() => {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+48.sha-8a56c99');
+const VERSION = new Version('10.0.0-next.7+49.sha-7acd330');
 
 /**
  * @fileoverview added by tsickle
