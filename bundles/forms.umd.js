2b5e379eb175d184f15bd084b83ffbd95c41993c
Kara Erickson <karakara@google.com>
2020-05-14 18:09:00 +0000

    2418c6aca1 refactor(docs-infra): refactors `addImageDimensionsImpl` (#37046)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+16.sha-9020507
+ * @license Angular v10.0.0-next.7+17.sha-2418c6a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7176,7 +7176,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-next.7+16.sha-9020507');
+    var VERSION = new i0.Version('10.0.0-next.7+17.sha-2418c6a');
 
     /**
      * @license
