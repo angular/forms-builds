988fd24569e22ab806e6bf932d4115646cd9e4ec
Kara Erickson <karakara@google.com>
2020-05-15 18:07:36 +0000

    9548c7cb94 release: cut the v9.1.7 release (#37116)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+47.sha-5ffa4f1
+ * @license Angular v9.1.6+49.sha-9548c7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.6+47.sha-5ffa4f1');
+const VERSION = new Version('9.1.6+49.sha-9548c7c');
 
 /**
  * @fileoverview added by tsickle
