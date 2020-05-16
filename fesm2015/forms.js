952c856d4d8b599c05cc3d947e4002c58aa51d8c
Kara Erickson <karakara@google.com>
2020-05-16 00:18:49 +0000

    3f9b3d7666 build: add kara to public-api approvers (#37143)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+64.sha-7f58c57
+ * @license Angular v10.0.0-next.7+66.sha-3f9b3d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9974,7 +9974,7 @@ let FormBuilder = /** @class */ (() => {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+64.sha-7f58c57');
+const VERSION = new Version('10.0.0-next.7+66.sha-3f9b3d7');
 
 /**
  * @fileoverview added by tsickle
