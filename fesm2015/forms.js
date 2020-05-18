5e864b7a96056f1f41e815eb77ac8ed49633e3da
Kara Erickson <karakara@google.com>
2020-05-18 17:49:10 +0000

    593e07bd21 build(docs-infra): upgrade cli command docs sources to dbde51375 (#37154)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+7.sha-2ca2c8f
+ * @license Angular v10.0.0-next.8+9.sha-593e07b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9974,7 +9974,7 @@ let FormBuilder = /** @class */ (() => {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+7.sha-2ca2c8f');
+const VERSION = new Version('10.0.0-next.8+9.sha-593e07b');
 
 /**
  * @fileoverview added by tsickle
