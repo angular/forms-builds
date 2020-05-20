2417f3228396a5c621eedb43f013658ab346790d
Kara Erickson <karakara@google.com>
2020-05-20 17:56:32 +0000

    aedac71491 docs: release notes for the v9.1.8 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+11.sha-89b44d1
+ * @license Angular v10.0.0-next.9+12.sha-aedac71
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9974,7 +9974,7 @@ let FormBuilder = /** @class */ (() => {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.9+11.sha-89b44d1');
+const VERSION = new Version('10.0.0-next.9+12.sha-aedac71');
 
 /**
  * @fileoverview added by tsickle
