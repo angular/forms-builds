aea878108535163243f2dda0e5462e16210415c4
Kara Erickson <karakara@google.com>
2020-05-20 23:06:35 +0000

    6ca3fb19fd docs: release notes for the v${releaseVersion} release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+21.sha-267f844
+ * @license Angular v10.0.0-next.9+22.sha-6ca3fb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7053,7 +7053,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-next.9+21.sha-267f844');
+    var VERSION = new core.Version('10.0.0-next.9+22.sha-6ca3fb1');
 
     /**
      * @license
