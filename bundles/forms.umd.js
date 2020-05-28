858591f424c183962896782419173de735190384
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 23:09:46 +0000

    df082307b9 docs: fix typo in committer.md (#37171)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+21.sha-f001aef
+ * @license Angular v10.0.0-rc.0+23.sha-df08230
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7190,7 +7190,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+21.sha-f001aef');
+    var VERSION = new i0.Version('10.0.0-rc.0+23.sha-df08230');
 
     /**
      * @license
