b68befc0188618ac55e8e45ffa0e0aaaaf5bd7da
atscott <atscott@google.com>
2020-06-08 22:25:29 +0000

    a1616ce181 docs: add note on publishing libraries in ivy (#36556)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+70.sha-1c22dff
+ * @license Angular v10.0.0-rc.2+71.sha-a1616ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6503,7 +6503,7 @@ let FormBuilder = /** @class */ (() => {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+70.sha-1c22dff');
+const VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
 
 /**
  * @license
