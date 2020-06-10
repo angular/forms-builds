5a7138a5459419e66d5ac2e372d50533b3d3c830
atscott <atscott@google.com>
2020-06-10 19:15:05 +0000

    ae364864f6 fix(ngcc): do not scan import expressions in d.ts files (#37503)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+122.sha-d76a872
+ * @license Angular v10.0.0-rc.0+119.sha-ae36486
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7188,7 +7188,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+122.sha-d76a872');
+    var VERSION = new i0.Version('10.0.0-rc.0+119.sha-ae36486');
 
     /**
      * @license
