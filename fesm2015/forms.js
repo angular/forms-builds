4fe43d6d42da331b1101990a6ef27cb62f26b7f6
Misko Hevery <misko@hevery.com>
2020-06-12 16:10:03 +0000

    a075260751 docs: document how external link icons work (#37025)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+138.sha-284123c
+ * @license Angular v10.0.0-rc.0+159.sha-a075260
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6394,7 +6394,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+138.sha-284123c');
+const VERSION = new Version('10.0.0-rc.0+159.sha-a075260');
 
 /**
  * @license
