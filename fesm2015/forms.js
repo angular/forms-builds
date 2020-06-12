8123faba43b05600cebe09a50d07ad4dd485deec
Misko Hevery <misko@hevery.com>
2020-06-12 16:16:47 +0000

    8dcf618411 docs: add redirects for cli generated config files (#37533)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+158.sha-aba33de
+ * @license Angular v10.0.0-rc.0+161.sha-8dcf618
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6663,7 +6663,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+158.sha-aba33de');
+const VERSION = new Version('10.0.0-rc.0+161.sha-8dcf618');
 
 /**
  * @license
