69e41e707fe876decd4de058ee75b3efc571ef69
Misko Hevery <misko@hevery.com>
2020-06-15 16:57:48 +0000

    7ac166ff08 feat(bazel): expose explicit mapping from closure to devmode files (#36262)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+170.sha-ae4f81c
+ * @license Angular v10.0.0-rc.0+171.sha-7ac166f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6663,7 +6663,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+170.sha-ae4f81c');
+const VERSION = new Version('10.0.0-rc.0+171.sha-7ac166f');
 
 /**
  * @license
