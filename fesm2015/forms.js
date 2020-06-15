495e0d552b935610359b90886db26164c851ca58
Misko Hevery <misko@hevery.com>
2020-06-15 17:12:53 +0000

    adc9d5cdcb docs: describe how to configure CommonJS modules (#37331)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+173.sha-d330cd8
+ * @license Angular v10.0.0-rc.0+174.sha-adc9d5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6663,7 +6663,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+173.sha-d330cd8');
+const VERSION = new Version('10.0.0-rc.0+174.sha-adc9d5c');
 
 /**
  * @license
