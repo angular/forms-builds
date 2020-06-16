10e55fa945a9f7839ccdb1b0062562aa7163ce66
Misko Hevery <misko@hevery.com>
2020-06-16 17:05:37 +0000

    1185aa07a3 docs: wrong example in routerLink (#37590)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+186.sha-ceca5dd
+ * @license Angular v10.0.0-rc.0+187.sha-1185aa0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7015,7 +7015,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+186.sha-ceca5dd');
+    var VERSION = new core.Version('10.0.0-rc.0+187.sha-1185aa0');
 
     /**
      * @license
