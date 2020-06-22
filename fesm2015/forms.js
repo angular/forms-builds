f2af9c1b03a58fb94daa5264e88ebfd44b950d15
Misko Hevery <misko@hevery.com>
2020-06-22 18:13:04 +0000

    7e0eccc4ef docs: Refactor-i18n (#36924)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+199.sha-2a970a0
+ * @license Angular v10.0.0-rc.0+200.sha-7e0eccc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+199.sha-2a970a0');
+const VERSION = new Version('10.0.0-rc.0+200.sha-7e0eccc');
 
 /**
  * @license
