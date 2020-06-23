967b5ef648b758d241cb1a96d3e02e1b5d44de41
Misko Hevery <misko@hevery.com>
2020-06-23 20:36:57 +0000

    71c0363bb5 docs: add tslib update migration docs (#37402)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+222.sha-e03adaf
+ * @license Angular v10.0.0-rc.0+223.sha-71c0363
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+222.sha-e03adaf');
+const VERSION = new Version('10.0.0-rc.0+223.sha-71c0363');
 
 /**
  * @license
