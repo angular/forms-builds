b7d19eb884248f2b63d71afbde1b2eebb10fa761
Misko Hevery <misko@hevery.com>
2020-06-24 23:23:53 +0000

    96891a076f release: sort the v10.0.0 release in `CHANGELOG.md`

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+23.sha-c49507b
+ * @license Angular v10.0.0+2.sha-96891a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.6+23.sha-c49507b');
+const VERSION = new Version('10.0.0+2.sha-96891a0');
 
 /**
  * @license
