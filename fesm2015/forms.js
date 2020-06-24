f8e2a468ab1225ac0cb106c3b0ee42ebcbeb9300
Misko Hevery <misko@hevery.com>
2020-06-24 17:01:50 +0000

    243824ef74 docs: updating guides and docs related to the v10 release (#37705)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+224.sha-c95802f
+ * @license Angular v10.0.0-rc.0+225.sha-243824e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+224.sha-c95802f');
+const VERSION = new Version('10.0.0-rc.0+225.sha-243824e');
 
 /**
  * @license
