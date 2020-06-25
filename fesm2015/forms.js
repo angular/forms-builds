a289986dc343c836b90224a948ffba2801ed4ff1
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:13:26 +0000

    2cbe53a9ba docs: Uses correct component in the `MessageService` (#37666)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+15.sha-a5d5f67
+ * @license Angular v10.0.0+17.sha-2cbe53a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0+15.sha-a5d5f67');
+const VERSION = new Version('10.0.0+17.sha-2cbe53a');
 
 /**
  * @license
