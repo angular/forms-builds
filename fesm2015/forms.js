429868c26d0a881ee4aae445b2423b494449f40b
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:03:50 +0000

    29490cce30 build: set up caretaker note label in merge tooling (#37675)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+230.sha-a0595f4
+ * @license Angular v10.0.0-rc.0+232.sha-29490cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6665,7 +6665,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+230.sha-a0595f4');
+const VERSION = new Version('10.0.0-rc.0+232.sha-29490cc');
 
 /**
  * @license
