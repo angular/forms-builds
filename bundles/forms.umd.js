4d850c400a06518b4209cac73ef9cd50a1b8c033
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:19:55 +0000

    2170ea270a fix(router): fix error when calling ParamMap.get function (#31599)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+239.sha-80d0067
+ * @license Angular v10.0.0-rc.0+240.sha-2170ea2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6976,7 +6976,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+239.sha-80d0067');
+    var VERSION = new core.Version('10.0.0-rc.0+240.sha-2170ea2');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+239.sha-80d0067
+ * @license Angular v10.0.0-rc.0+240.sha-2170ea2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+239.sha-80d0067');
+const VERSION = new Version('10.0.0-rc.0+240.sha-2170ea2');
 
 /**
  * @license
