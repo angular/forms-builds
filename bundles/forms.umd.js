baf9298667e8869de516ed956e24dc6f7a583c9a
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:19:15 +0000

    80d0067048 fix(router): `RouterLinkActive` should run CD when setting `isActive` (#21411)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+238.sha-5218916
+ * @license Angular v10.0.0-rc.0+239.sha-80d0067
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6976,7 +6976,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+238.sha-5218916');
+    var VERSION = new core.Version('10.0.0-rc.0+239.sha-80d0067');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+238.sha-5218916
+ * @license Angular v10.0.0-rc.0+239.sha-80d0067
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+238.sha-5218916');
+const VERSION = new Version('10.0.0-rc.0+239.sha-80d0067');
 
 /**
  * @license
