2ce7d380a5a025c78ce370b036aeb109e4f52c47
Andrew Kushnir <akushnir@google.com>
2020-06-25 23:08:11 +0000

    8572e885b4 test(compiler-cli): fix assertion of diagnostic filename on Windows (#37758)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+263.sha-0e3e9cd
+ * @license Angular v10.0.0-rc.0+264.sha-8572e88
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+263.sha-0e3e9cd');
+const VERSION = new Version('10.0.0-rc.0+264.sha-8572e88');
 
 /**
  * @license
