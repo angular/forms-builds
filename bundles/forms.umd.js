49089a3c3a9433e38088faa4bdad3f9609b6f4d0
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:21:22 +0000

    616543ded0 fix(router): add null support for RouterLink directive (#32616)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+240.sha-2170ea2
+ * @license Angular v10.0.0-rc.0+241.sha-616543d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6976,7 +6976,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.0-rc.0+240.sha-2170ea2');
+    var VERSION = new core.Version('10.0.0-rc.0+241.sha-616543d');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+240.sha-2170ea2
+ * @license Angular v10.0.0-rc.0+241.sha-616543d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+240.sha-2170ea2');
+const VERSION = new Version('10.0.0-rc.0+241.sha-616543d');
 
 /**
  * @license
