8bee2c61325aae3dc441b01a64943b9cf4de3301
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:11:05 +0000

    1c1eb3045c docs: Add support schedule for v10 (#37745)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+235.sha-666e6d1
+ * @license Angular v10.0.0-rc.0+237.sha-1c1eb30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+235.sha-666e6d1');
+const VERSION = new Version('10.0.0-rc.0+237.sha-1c1eb30');
 
 /**
  * @license
