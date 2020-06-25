c246fc0b7b0c9d2c5fce43425a496059795a4471
Misko Hevery <misko@hevery.com>
2020-06-25 03:19:26 +0000

    4481bebb38 docs: add v9.angular.io to the angular.io version picker (#37719)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
+ * @license Angular v10.0.0-rc.0+228.sha-4481beb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6665,7 +6665,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+227.sha-f5bbfd7');
+const VERSION = new Version('10.0.0-rc.0+228.sha-4481beb');
 
 /**
  * @license
