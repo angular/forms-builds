3f07ee54513074bd346e5b25938a09a660fedcc1
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:53:31 +0000

    eee2fd22e0 ci(docs-infra): store JS bundles as CI artifacts to debug size check flakes (#37703)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+269.sha-638dd15
+ * @license Angular v10.0.0-rc.0+270.sha-eee2fd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+269.sha-638dd15');
+const VERSION = new Version('10.0.0-rc.0+270.sha-eee2fd2');
 
 /**
  * @license
