b0a19f367215176f35b8435272b4c3972cdf8a84
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:27:25 +0000

    6341a837c1 docs: correct outdated dev instructions for public api golds (#37026)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+274.sha-6341a83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
+const VERSION = new Version('10.0.0-rc.0+274.sha-6341a83');
 
 /**
  * @license
