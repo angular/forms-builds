9f7e42d28e11c6705d412125caed2ad5d1396c87
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:27:09 +0000

    a177b1b7b1 docs: change definition of providedIn any (#35292)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+303.sha-e84539f
+ * @license Angular v10.0.0-rc.0+301.sha-a177b1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+303.sha-e84539f');
+const VERSION = new Version('10.0.0-rc.0+301.sha-a177b1b');
 
 /**
  * @license
