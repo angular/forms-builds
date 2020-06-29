2c6c2fb3d9c2afa09b2ee0603b3a706a6da61f22
Andrew Kushnir <akushnir@google.com>
2020-06-29 23:58:53 +0000

    57a518a36d perf(compiler-cli): fix memory leak in retained incremental state (#37835)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+20.sha-29b8318
+ * @license Angular v10.0.1+21.sha-57a518a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.1+20.sha-29b8318');
+const VERSION = new Version('10.0.1+21.sha-57a518a');
 
 /**
  * @license
