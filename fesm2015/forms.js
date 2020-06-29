f934da1299cf39c6bf57617586325b866be02dee
Andrew Kushnir <akushnir@google.com>
2020-06-29 18:00:59 +0000

    89e16ed6a5 fix(elements): fire custom element output events during component initialization (#37570)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+4.sha-78460c1
+ * @license Angular v10.0.1+10.sha-89e16ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.1+4.sha-78460c1');
+const VERSION = new Version('10.0.1+10.sha-89e16ed');
 
 /**
  * @license
