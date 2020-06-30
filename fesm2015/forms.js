7afdae10bc95fb7176bd898969a39caa69763d30
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:35:25 +0000

    9206a26e1d docs: clean up api doc in core (#37053)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+308.sha-11dd2be
+ * @license Angular v10.0.0-rc.0+309.sha-9206a26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+308.sha-11dd2be');
+const VERSION = new Version('10.0.0-rc.0+309.sha-9206a26');
 
 /**
  * @license
