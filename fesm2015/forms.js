ccd7f814c1ec9ba1d60db1713fb095dbe54f57e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-01 19:18:14 +0000

    196bfa8fae docs: restore link to lightweight token page in left nav (#37855)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+311.sha-1d5ca55
+ * @license Angular v10.0.0-rc.0+312.sha-196bfa8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+311.sha-1d5ca55');
+const VERSION = new Version('10.0.0-rc.0+312.sha-196bfa8');
 
 /**
  * @license
