e016d16fd98a80b3f50de782492343c61b3df58b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:18:19 +0000

    1801d0c650 fix(router): properly compare array queryParams for equality (#37709) (#37860)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+314.sha-d7dd295
+ * @license Angular v10.0.0-rc.0+316.sha-1801d0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7378,7 +7378,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+314.sha-d7dd295');
+    var VERSION = new i0.Version('10.0.0-rc.0+316.sha-1801d0c');
 
     /**
      * @license
