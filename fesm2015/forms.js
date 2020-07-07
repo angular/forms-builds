187f67965b83ae12e3d0f060cad19f8620d434da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:42:39 +0000

    cb2ab26296 feat(dev-infra): merge script should link to original commit when cherry-picking with API strategy (#37889)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+320.sha-ec32eba
+ * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+320.sha-ec32eba');
+const VERSION = new Version('10.0.0-rc.0+324.sha-cb2ab26');
 
 /**
  * @license
