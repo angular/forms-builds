07350ce20726a9cd149a75f2fcbe02ae70524fd5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 21:24:32 +0000

    1f0c1f3ff2 release: cut the v9.1.12 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+5.sha-a86a412
+ * @license Angular v9.1.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.11+5.sha-a86a412');
+const VERSION = new Version('9.1.12');
 
 /**
  * @fileoverview added by tsickle
