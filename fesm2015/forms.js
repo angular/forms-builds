253deb3e3c640c8a977ebfba9e149e018bd4ccd8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 21:30:21 +0000

    1237d9cbea docs: release notes for the v10.0.3 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+327.sha-6eb868b
+ * @license Angular v10.0.0-rc.0+329.sha-1237d9c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+327.sha-6eb868b');
+const VERSION = new Version('10.0.0-rc.0+329.sha-1237d9c');
 
 /**
  * @license
