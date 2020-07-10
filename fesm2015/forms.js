e6f9f1914c5151351b660089fbc45dca8e19f92e
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:24:48 +0000

    a94383f168 fix(compiler): check more cases for pipe usage inside host bindings (#37883)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+33.sha-c776825
+ * @license Angular v10.0.3+36.sha-a94383f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6396,7 +6396,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.3+33.sha-c776825');
+const VERSION = new Version('10.0.3+36.sha-a94383f');
 
 /**
  * @license
