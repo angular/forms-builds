ea9de89cd184a0b0dbf7875879c0b12638239279
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:49:18 +0000

    9185c6e971 fix(router): ensure duplicate popstate/hashchange events are handled correctly (#37674)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+39.sha-69472a1
+ * @license Angular v10.1.0-next.0+40.sha-9185c6e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6678,7 +6678,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+39.sha-69472a1');
+const VERSION = new Version('10.1.0-next.0+40.sha-9185c6e');
 
 /**
  * @license
