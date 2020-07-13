0755089672abd3148200791a81881a95dd9bb575
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:35:05 +0000

    62e19c8813 docs: Add Scully to resources.json (#37678)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+34.sha-95669df
+ * @license Angular v10.1.0-next.0+35.sha-62e19c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7378,7 +7378,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.0+34.sha-95669df');
+    var VERSION = new i0.Version('10.1.0-next.0+35.sha-62e19c8');
 
     /**
      * @license
