539cf09c8ef3337e1eaffb7a37c3bbecd65ce070
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:39:31 +0000

    69472a1ed0 refactor(dev-infra): allow for consumption with rules_nodejs v2.0.0 (#37968)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+38.sha-10aba15
+ * @license Angular v10.1.0-next.0+39.sha-69472a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6409,7 +6409,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+38.sha-10aba15');
+const VERSION = new Version('10.1.0-next.0+39.sha-69472a1');
 
 /**
  * @license
