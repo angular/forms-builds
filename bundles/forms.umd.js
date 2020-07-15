dc0392416cd5cdea2b3f2c49d9483fe56071b390
Andrew Kushnir <akushnir@google.com>
2020-07-15 19:59:12 +0000

    09eee5b312 fix(dev-infra): add missing BUILD file to dev-infra/bazel:files (#38026)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+3.sha-b00121c
+ * @license Angular v10.1.0-next.1+6.sha-09eee5b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6994,7 +6994,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.1+3.sha-b00121c');
+    var VERSION = new core.Version('10.1.0-next.1+6.sha-09eee5b');
 
     /**
      * @license
