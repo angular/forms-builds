5ef540007512301ac3fc68c66c175a09c8526159
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:10:53 +0000

    17ddab98fb fix(core): incorrectly validating properties on ng-content and ng-container (#37773)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4
+ * @license Angular v10.0.4+9.sha-17ddab9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4');
+const VERSION = new Version('10.0.4+9.sha-17ddab9');
 
 /**
  * @license
