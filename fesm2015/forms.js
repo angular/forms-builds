159220810aca38d28923af2669d84be7b969c636
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:01:54 +0000

    09eee5b312 fix(dev-infra): add missing BUILD file to dev-infra/bazel:files (#38026)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+3.sha-b00121c
+ * @license Angular v10.1.0-next.1+6.sha-09eee5b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6693,7 +6693,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+3.sha-b00121c');
+const VERSION = new Version('10.1.0-next.1+6.sha-09eee5b');
 
 /**
  * @license
