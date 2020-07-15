b8d3cc37bf1a26ce5e12db83facf8111164d989f
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:03:37 +0000

    45d79efc3f docs: remove all references to Angular Console (#37608)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+6.sha-09eee5b
+ * @license Angular v10.1.0-next.1+2.sha-45d79ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6693,7 +6693,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+6.sha-09eee5b');
+const VERSION = new Version('10.1.0-next.1+2.sha-45d79ef');
 
 /**
  * @license
