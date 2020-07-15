1445d70ae5586d53368adddf446d571ae900fe4a
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:35:25 +0000

    739bf5c325 docs: fix typo in "Creating libraries" guide (`by publishing...ensures` --> `publishing...ensures`) (#38032)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+9.sha-17ddab9
+ * @license Angular v10.0.4+11.sha-739bf5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+9.sha-17ddab9');
+const VERSION = new Version('10.0.4+11.sha-739bf5c');
 
 /**
  * @license
