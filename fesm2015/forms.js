8b292751194c97b383317cd6b4992caa1b54342f
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:34:04 +0000

    24b420527a docs(service-worker): fix typos in `SwRegistrationOptions` API docs (#38047)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+9.sha-406f801
+ * @license Angular v10.1.0-next.1+11.sha-24b4205
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6693,7 +6693,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+9.sha-406f801');
+const VERSION = new Version('10.1.0-next.1+11.sha-24b4205');
 
 /**
  * @license
