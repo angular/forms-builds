6bc6b3f808d301e4311e7c9a49a0c608180cb69a
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:09:54 +0000

    c8e77d8536 ci(docs-infra): increase minimum a11y scores for various pages (#37899)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+10.sha-bf641e1
+ * @license Angular v10.1.0-next.1+8.sha-c8e77d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6693,7 +6693,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+10.sha-bf641e1');
+const VERSION = new Version('10.1.0-next.1+8.sha-c8e77d8');
 
 /**
  * @license
