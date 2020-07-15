0bee02e49d05b108d76c82f0e5411f5f4d9f78e0
atscott <atscott01@gmail.com>
2020-07-15 18:26:57 +0000

    e7c722574f release: cut the v10.1.0-next.1 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+49.sha-b358495
+ * @license Angular v10.1.0-next.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6424,7 +6424,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.0+49.sha-b358495');
+const VERSION = new Version('10.1.0-next.1');
 
 /**
  * @license
