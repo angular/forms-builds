5210b5aa95ef4c1a38f49314fd41206ab57fc3de
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:25:35 +0000

    e382632473 build: fix symbol extractor not dealing with ES2015 classes (#38093)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+16.sha-e0b35bf
+ * @license Angular v10.1.0-next.1+23.sha-e382632
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7383,7 +7383,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.1+16.sha-e0b35bf');
+    var VERSION = new i0.Version('10.1.0-next.1+23.sha-e382632');
 
     /**
      * @license
