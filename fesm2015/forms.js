acf7db8bd424051a7b32f826ca3cb17458b8c7ee
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:32:28 +0000

    8b25a64200 build(language-service): remove typescript from ivy bundle (#38088)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+19.sha-019a696
+ * @license Angular v10.1.0-next.1+18.sha-8b25a64
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6424,7 +6424,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+19.sha-019a696');
+const VERSION = new Version('10.1.0-next.1+18.sha-8b25a64');
 
 /**
  * @license
