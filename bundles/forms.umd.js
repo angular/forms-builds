c52ecb5c8fdd1166a65c02daa275aa12fccd164d
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:34:09 +0000

    bd71f10234 build(language-service): Remove ls_rollup_bundle (#38086)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+18.sha-8b25a64
+ * @license Angular v10.1.0-next.1+17.sha-bd71f10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6994,7 +6994,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.1+18.sha-8b25a64');
+    var VERSION = new core.Version('10.1.0-next.1+17.sha-bd71f10');
 
     /**
      * @license
