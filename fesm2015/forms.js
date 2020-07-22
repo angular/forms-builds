3f8ece88ca4465ecbccff7b28317034ad5e41b2a
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:29:15 +0000

    970d10c671 release: cut the v10.1.0-next.2 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+46.sha-8851ba9
+ * @license Angular v10.1.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6463,7 +6463,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+46.sha-8851ba9');
+const VERSION = new Version('10.1.0-next.2');
 
 /**
  * @license
