0b664fa6bcc312c8b5ea8ee22c210d70aae912ee
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:40:43 +0000

    5a733629b5 build(docs-infra): remove boilerplate file listings in `example-boilerplate.js` (#38173)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+1.sha-5e742d2
+ * @license Angular v10.1.0-next.2+8.sha-5a73362
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -868,7 +868,7 @@
     }
     /**
      * @description
-     * A base class that all control `FormControl`-based directives extend. It binds a `FormControl`
+     * A base class that all `FormControl`-based directives extend. It binds a `FormControl`
      * object to a DOM element.
      *
      * @publicApi
@@ -7430,7 +7430,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.2+1.sha-5e742d2');
+    var VERSION = new i0.Version('10.1.0-next.2+8.sha-5a73362');
 
     /**
      * @license
