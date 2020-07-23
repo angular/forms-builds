58533ec2fc3c53109ea257153a3151f71f00b891
Misko Hevery <misko@hevery.com>
2020-07-23 18:22:55 +0000

    8bb8f98c28 docs: remove duplicate `https://` (#38199)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+12.sha-3c1866b
+ * @license Angular v10.1.0-next.2+13.sha-8bb8f98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+12.sha-3c1866b');
+const VERSION = new Version('10.1.0-next.2+13.sha-8bb8f98');
 
 /**
  * @license
