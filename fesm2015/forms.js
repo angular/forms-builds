e3d1e3b33c9e6cfcd22f4bdef8fb2d7833ced534
Misko Hevery <misko@hevery.com>
2020-07-23 04:08:24 +0000

    062b8d90af refactor(forms): refactor common validators used in unit tests (#38020)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+10.sha-8df888d
+ * @license Angular v10.1.0-next.2+11.sha-062b8d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+10.sha-8df888d');
+const VERSION = new Version('10.1.0-next.2+11.sha-062b8d9');
 
 /**
  * @license
