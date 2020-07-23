98e3d48bea11cce123dc06570930864d4eed184f
Misko Hevery <misko@hevery.com>
2020-07-23 04:01:55 +0000

    8df888dfb4 fix(elements): run strategy methods in correct zone (#37814)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+9.sha-778ad37
+ * @license Angular v10.1.0-next.2+10.sha-8df888d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+9.sha-778ad37');
+const VERSION = new Version('10.1.0-next.2+10.sha-8df888d');
 
 /**
  * @license
