f52c9b632ca68cb52ac7bf62f2b5243ad7eccf9c
Misko Hevery <misko@hevery.com>
2020-07-23 18:26:54 +0000

    b518b30dae docs: add Kevin Kreuzer to GDE page (#37929)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+16.sha-7b2e2f5
+ * @license Angular v10.1.0-next.2+15.sha-b518b30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6463,7 +6463,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+16.sha-7b2e2f5');
+const VERSION = new Version('10.1.0-next.2+15.sha-b518b30');
 
 /**
  * @license
