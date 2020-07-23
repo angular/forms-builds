724397ea4e7d88a52543fe031784f224c8a1302c
Misko Hevery <misko@hevery.com>
2020-07-23 18:29:29 +0000

    b518b30dae docs: add Kevin Kreuzer to GDE page (#37929)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+16.sha-7b2e2f5
+ * @license Angular v10.1.0-next.2+15.sha-b518b30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+16.sha-7b2e2f5');
+const VERSION = new Version('10.1.0-next.2+15.sha-b518b30');
 
 /**
  * @license
