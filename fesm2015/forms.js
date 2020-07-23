f129de264fb722eed545b83a73b4bf813d9571aa
Misko Hevery <misko@hevery.com>
2020-07-23 04:11:32 +0000

    a50a688aaf docs: update api reference for router outlet directive (#38166)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+7.sha-6ec7297
+ * @license Angular v10.0.5+8.sha-a50a688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+7.sha-6ec7297');
+const VERSION = new Version('10.0.5+8.sha-a50a688');
 
 /**
  * @license
