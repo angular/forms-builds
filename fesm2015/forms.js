59cfcab87895972ed9cac249a3bfac14b9fc7100
Misko Hevery <misko@hevery.com>
2020-07-24 17:16:12 +0000

    6e2db228af docs: Fix link by removing a space (#38214) PR Close #38214

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+22.sha-cce5583
+ * @license Angular v10.1.0-next.2+23.sha-6e2db22
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+22.sha-cce5583');
+const VERSION = new Version('10.1.0-next.2+23.sha-6e2db22');
 
 /**
  * @license
