9680fb8312fb92a98be4ea890dc8b53748b88173
Misko Hevery <misko@hevery.com>
2020-07-24 23:07:20 +0000

    1822cbcd46 fix(zone.js): patch nodejs EventEmtter.prototype.off (#37863)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+25.sha-a71f114
+ * @license Angular v10.1.0-next.2+26.sha-1822cbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+25.sha-a71f114');
+const VERSION = new Version('10.1.0-next.2+26.sha-1822cbc');
 
 /**
  * @license
