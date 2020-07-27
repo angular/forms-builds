3f0bbcbea5f1f7074934f79962ee3af9aa641cb6
Misko Hevery <misko@hevery.com>
2020-07-27 19:38:37 +0000

    96aa14df01 fix(zone.js): zone patch rxjs should return null _unsubscribe correctly. (#37091)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+38.sha-2b6ab57
+ * @license Angular v10.1.0-next.2+39.sha-96aa14d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+38.sha-2b6ab57');
+const VERSION = new Version('10.1.0-next.2+39.sha-96aa14d');
 
 /**
  * @license
