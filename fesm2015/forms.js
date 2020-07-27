a22b8f34a5c2fade62c6b3df8305041d9a963117
Misko Hevery <misko@hevery.com>
2020-07-27 19:28:44 +0000

    2b6ab57d78 docs: add template ref var to glossary (#36743)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+37.sha-b65b616
+ * @license Angular v10.1.0-next.2+38.sha-2b6ab57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+37.sha-b65b616');
+const VERSION = new Version('10.1.0-next.2+38.sha-2b6ab57');
 
 /**
  * @license
