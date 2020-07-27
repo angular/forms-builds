b905e9ccb267a88a8e46c0899cffa6705547c2bb
Misko Hevery <misko@hevery.com>
2020-07-27 22:50:45 +0000

    65cc0c8bd6 fix(compiler-cli): Add support for string literal class members (#38226)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+44.sha-03a6252
+ * @license Angular v10.1.0-next.2+45.sha-65cc0c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6463,7 +6463,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+44.sha-03a6252');
+const VERSION = new Version('10.1.0-next.2+45.sha-65cc0c8');
 
 /**
  * @license
