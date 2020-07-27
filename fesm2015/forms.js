513df665bf8242328b48afd470ee452d39a3c02f
Misko Hevery <misko@hevery.com>
2020-07-27 17:28:21 +0000

    8e5969bb52 fix(compiler): share identical stylesheets between components in the same file (#38213)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+31.sha-4dfc3fe
+ * @license Angular v10.1.0-next.2+35.sha-8e5969b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+31.sha-4dfc3fe');
+const VERSION = new Version('10.1.0-next.2+35.sha-8e5969b');
 
 /**
  * @license
