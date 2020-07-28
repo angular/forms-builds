029664bd735bdcf510e62a6234957dcc89005426
Misko Hevery <misko@hevery.com>
2020-07-28 23:03:17 +0000

    57575e379f release: cut the v10.1.0-next.3 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+59.sha-28c3e2d
+ * @license Angular v10.1.0-next.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+59.sha-28c3e2d');
+const VERSION = new Version('10.1.0-next.3');
 
 /**
  * @license
