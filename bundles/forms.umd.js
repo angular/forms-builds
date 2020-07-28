9597eab2d435c2c47c6a522b19d03a51be840018
Misko Hevery <misko@hevery.com>
2020-07-28 19:58:33 +0000

    2a45b932e2 build: fix broken build (#38274)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+45.sha-65cc0c8
+ * @license Angular v10.1.0-next.2+58.sha-2a45b93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7430,7 +7430,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('10.1.0-next.2+45.sha-65cc0c8');
+    var VERSION = new i0.Version('10.1.0-next.2+58.sha-2a45b93');
 
     /**
      * @license
