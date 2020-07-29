ca0f750174d9db3df8f24f93417fe850202d2173
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 20:58:10 +0000

    8effc83b92 docs(router): clarify how base href is used to construct targets (#38123)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+15.sha-887c350
+ * @license Angular v10.1.0-next.3+17.sha-8effc83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7041,7 +7041,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.3+15.sha-887c350');
+    var VERSION = new core.Version('10.1.0-next.3+17.sha-8effc83');
 
     /**
      * @license
