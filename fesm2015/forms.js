ad8705801316969cc677b7459e8b88316e711c40
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:14:20 +0000

    1eebb7f189 test(compiler-cli): disable one TypeChecker test on Windows due to path sensitivity issue (#38294)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+16.sha-7f8c222
+ * @license Angular v10.1.0-next.3+18.sha-1eebb7f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+16.sha-7f8c222');
+const VERSION = new Version('10.1.0-next.3+18.sha-1eebb7f');
 
 /**
  * @license
