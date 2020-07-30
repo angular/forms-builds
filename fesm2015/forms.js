b2a11a375e0672033fb623eb9fd358c18472fa5e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 19:47:13 +0000

    3a525d196b Revert "fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)" (#38303)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+18.sha-1eebb7f
+ * @license Angular v10.1.0-next.3+19.sha-3a525d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+18.sha-1eebb7f');
+const VERSION = new Version('10.1.0-next.3+19.sha-3a525d1');
 
 /**
  * @license
