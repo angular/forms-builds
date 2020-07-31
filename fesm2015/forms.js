8d3abaebe75b78d819a4a67104e1e273f0975919
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:59:52 +0000

    cea46786a2 fix(compiler): update unparsable character reference entity error messages (#38319)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+17.sha-9c5fc16
+ * @license Angular v10.0.7+18.sha-cea4678
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.7+17.sha-9c5fc16');
+const VERSION = new Version('10.0.7+18.sha-cea4678');
 
 /**
  * @license
