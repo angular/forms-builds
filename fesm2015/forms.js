f712bd98c3e9e1e1b69a799ec0a1d618da3a8bd1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:21:25 +0000

    e8896b9de3 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+22.sha-6f6102d
+ * @license Angular v10.1.0-next.3+23.sha-e8896b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+22.sha-6f6102d');
+const VERSION = new Version('10.1.0-next.3+23.sha-e8896b9');
 
 /**
  * @license
