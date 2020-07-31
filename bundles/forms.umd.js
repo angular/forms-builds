64a47aeafad20d573172468a44fff0c8c22c66b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:54:36 +0000

    ff9f4de4f1 fix(compiler): update unparsable character reference entity error messages (#38319)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+38.sha-3a46c2d
+ * @license Angular v10.1.0-next.3+39.sha-ff9f4de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7041,7 +7041,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.3+38.sha-3a46c2d');
+    var VERSION = new core.Version('10.1.0-next.3+39.sha-ff9f4de');
 
     /**
      * @license
