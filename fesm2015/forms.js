b636a7444a3c4c48df971f6e854d01c113dc90df
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:11:35 +0000

    585ef2bdd7 docs: fix typo in glossary (#38318)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4
+ * @license Angular v10.1.0-next.4+1.sha-585ef2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6463,7 +6463,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4');
+const VERSION = new Version('10.1.0-next.4+1.sha-585ef2b');
 
 /**
  * @license
