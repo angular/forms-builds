771e23a6bcfeb9f0633828d93bc8babbc8723bbc
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:20:43 +0000

    f0766a4474 feat(dev-infra): provide organization-wide merge-tool label configuration (#38223)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+1.sha-585ef2b
+ * @license Angular v10.1.0-next.4+4.sha-f0766a4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6732,7 +6732,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+1.sha-585ef2b');
+const VERSION = new Version('10.1.0-next.4+4.sha-f0766a4');
 
 /**
  * @license
