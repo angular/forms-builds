4de5f0e644a5ba321056a470cd811972b3bc3deb
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:20:05 +0000

    415131413d fix(router): prevent calling unsubscribe on undefined subscription in RouterPreloader (#38344)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8
+ * @license Angular v10.0.8+6.sha-4151314
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6981,7 +6981,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.8');
+    var VERSION = new core.Version('10.0.8+6.sha-4151314');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8
+ * @license Angular v10.0.8+6.sha-4151314
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6411,7 +6411,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8');
+const VERSION = new Version('10.0.8+6.sha-4151314');
 
 /**
  * @license
