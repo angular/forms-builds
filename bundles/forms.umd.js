b47593cef0a90ce83fe757a04c8e7ba81121b450
Andrew Kushnir <akushnir@google.com>
2020-08-07 00:22:49 +0000

    702958e968 refactor(core): add debug ranges to `LViewDebug` with matchers (#38359)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+22.sha-df7f3b0
+ * @license Angular v10.1.0-next.4+23.sha-702958e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7041,7 +7041,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.4+22.sha-df7f3b0');
+    var VERSION = new core.Version('10.1.0-next.4+23.sha-702958e');
 
     /**
      * @license
