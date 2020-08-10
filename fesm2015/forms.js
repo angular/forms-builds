75f1a5e35d7ded2c6a90c6222dba051a5c53b629
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:23:07 +0000

    0de93fd402 docs: update team contributors page (#38384)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+28.sha-6ff28ac
+ * @license Angular v10.1.0-next.4+27.sha-0de93fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+28.sha-6ff28ac');
+const VERSION = new Version('10.1.0-next.4+27.sha-0de93fd');
 
 /**
  * @license
