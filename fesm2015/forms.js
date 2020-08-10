79a64f7ad5dfc2558b2f84d804123c998d3b4b08
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:52:20 +0000

    5dc8d287aa fix(core): queries not matching string injection tokens (#38321)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+33.sha-6da9e58
+ * @license Angular v10.1.0-next.4+34.sha-5dc8d28
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+33.sha-6da9e58');
+const VERSION = new Version('10.1.0-next.4+34.sha-5dc8d28');
 
 /**
  * @license
