e7966241c5ab72390ccc0234442c3541a8187b4f
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:35:58 +0000

    e34c33cd46 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+29.sha-6d8c73a
+ * @license Angular v10.1.0-next.4+30.sha-e34c33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7034,7 +7034,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.4+29.sha-6d8c73a');
+    var VERSION = new core.Version('10.1.0-next.4+30.sha-e34c33c');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+29.sha-6d8c73a
+ * @license Angular v10.1.0-next.4+30.sha-e34c33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+29.sha-6d8c73a');
+const VERSION = new Version('10.1.0-next.4+30.sha-e34c33c');
 
 /**
  * @license
