e73d4a215802edd5a450d1ab7f0be35695d7d3a1
Andrew Kushnir <akushnir@google.com>
2020-08-11 18:23:49 +0000

    e2e5f83869 ci: update payload size limits for Closure tests (#38411)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+36.sha-df76a20
+ * @license Angular v10.1.0-next.4+43.sha-e2e5f83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+36.sha-df76a20');
+const VERSION = new Version('10.1.0-next.4+43.sha-e2e5f83');
 
 /**
  * @license
