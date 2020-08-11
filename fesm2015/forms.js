0b040decf0d7a48152bb1b874cdf129fda245435
Andrew Kushnir <akushnir@google.com>
2020-08-11 23:56:36 +0000

    a2e069fdda build: run formatting automatically on pre-commit hook (#38402)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+43.sha-e2e5f83
+ * @license Angular v10.1.0-next.4+46.sha-a2e069f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+43.sha-e2e5f83');
+const VERSION = new Version('10.1.0-next.4+46.sha-a2e069f');
 
 /**
  * @license
