4965832218a9153fdbf4c666bd4b66a010f28e72
Andrew Kushnir <akushnir@google.com>
2020-08-11 00:51:17 +0000

    3d156162af fix(dev-infra): update i18n-related file locations in PullApprove config (#38403)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+34.sha-5dc8d28
+ * @license Angular v10.1.0-next.4+35.sha-3d15616
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+34.sha-5dc8d28');
+const VERSION = new Version('10.1.0-next.4+35.sha-3d15616');
 
 /**
  * @license
