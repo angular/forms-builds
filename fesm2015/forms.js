64be80dcc0f283fa00f67e0f5c036cbce4b3f4c1
Andrew Scott <atscott01@gmail.com>
2020-08-12 23:34:51 +0000

    8366effeec refactor(dev-infra): extract the commit message parsing function into its own file (#38429)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+3.sha-5f2e475
+ * @license Angular v10.1.0-next.5+4.sha-8366eff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+3.sha-5f2e475');
+const VERSION = new Version('10.1.0-next.5+4.sha-8366eff');
 
 /**
  * @license
