899e38af5967f581291585478d05fd582c08935b
Andrew Scott <atscott01@gmail.com>
2020-08-12 18:50:06 +0000

    aa3520eb7d refactor(dev-infra): use promptConfirm util in ng-dev's formatter (#38419)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5
+ * @license Angular v10.1.0-next.5+2.sha-aa3520e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5');
+const VERSION = new Version('10.1.0-next.5+2.sha-aa3520e');
 
 /**
  * @license
