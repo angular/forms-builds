a812e49e8535176e1d36f5effa7213711f3eb534
Andrew Scott <atscott01@gmail.com>
2020-08-12 18:58:44 +0000

    5f2e475abf docs: remove unused Input decorator (#38306)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+2.sha-aa3520e
+ * @license Angular v10.1.0-next.5+3.sha-5f2e475
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+2.sha-aa3520e');
+const VERSION = new Version('10.1.0-next.5+3.sha-5f2e475');
 
 /**
  * @license
