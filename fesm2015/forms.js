e1b215f5cca2c2d35815fc00547bcf358a51afbc
Andrew Kushnir <akushnir@google.com>
2020-08-12 17:35:55 +0000

    d6d7caa2a8 release: cut the v10.1.0-next.5 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+48.sha-dcf7baf
+ * @license Angular v10.1.0-next.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4+48.sha-dcf7baf');
+const VERSION = new Version('10.1.0-next.5');
 
 /**
  * @license
