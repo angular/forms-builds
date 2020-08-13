9a7db2c30bac8b84d42b387333334424863f8ead
Andrew Scott <atscott01@gmail.com>
2020-08-13 18:58:55 +0000

    b769771d60 refactor(router): Add annotations to correct Router documentation (#38448)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+10.sha-a80f654
+ * @license Angular v10.1.0-next.5+11.sha-b769771
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+10.sha-a80f654');
+const VERSION = new Version('10.1.0-next.5+11.sha-b769771');
 
 /**
  * @license
