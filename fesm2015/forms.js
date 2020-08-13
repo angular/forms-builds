600d7ccead67ea10d66dcd8c68e14041e524c1ed
Andrew Scott <atscott01@gmail.com>
2020-08-13 23:29:47 +0000

    ca798804b2 fix(router): export DefaultRouteReuseStrategy to Router public_api (#31575)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+18.sha-b071495
+ * @license Angular v10.1.0-next.5+19.sha-ca79880
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+18.sha-b071495');
+const VERSION = new Version('10.1.0-next.5+19.sha-ca79880');
 
 /**
  * @license
