1488a82d352ccee5848792d2611dd8f2a73c8c22
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:59:10 +0000

    d5f819ebc1 style(compiler-cli): remove unused constant (#38441)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+16.sha-1388c17
+ * @license Angular v10.1.0-next.5+17.sha-d5f819e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+16.sha-1388c17');
+const VERSION = new Version('10.1.0-next.5+17.sha-d5f819e');
 
 /**
  * @license
