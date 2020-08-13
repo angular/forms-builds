2599ee8e3ee066a68a2f62c3e3ebd26f39c356e3
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:34:52 +0000

    773f7908c0 feat(dev-infra): update to latest benchpress version (#38440)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+5.sha-f4ced74
+ * @license Angular v10.1.0-next.5+6.sha-773f790
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+5.sha-f4ced74');
+const VERSION = new Version('10.1.0-next.5+6.sha-773f790');
 
 /**
  * @license
