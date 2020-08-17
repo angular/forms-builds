7493a8babf28f456003eba110875a68e86c553c8
atscott <atscott01@gmail.com>
2020-08-17 20:44:17 +0000

    fec9dcbeb0 docs: release notes for the v10.0.10 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+28.sha-e0e5c9f
+ * @license Angular v10.1.0-next.5+29.sha-fec9dcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+28.sha-e0e5c9f');
+const VERSION = new Version('10.1.0-next.5+29.sha-fec9dcb');
 
 /**
  * @license
