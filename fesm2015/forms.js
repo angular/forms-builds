54b8b76d9c22c5d4aa5834275077b07fa3794a63
Andrew Scott <atscott@google.com>
2020-08-17 19:59:06 +0000

    e0e5c9f195 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38349)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+27.sha-cfe424e
+ * @license Angular v10.1.0-next.5+28.sha-e0e5c9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+27.sha-cfe424e');
+const VERSION = new Version('10.1.0-next.5+28.sha-e0e5c9f');
 
 /**
  * @license
