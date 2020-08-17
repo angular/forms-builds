5575ebe5ea94d934dc5532a27322ae636e7aeed8
Andrew Scott <atscott@google.com>
2020-08-17 18:22:33 +0000

    3b9c802dee fix(ngcc): detect synthesized delegate constructors for downleveled ES2015 classes (#38463)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+24.sha-81c3e80
+ * @license Angular v10.1.0-next.5+26.sha-3b9c802
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+24.sha-81c3e80');
+const VERSION = new Version('10.1.0-next.5+26.sha-3b9c802');
 
 /**
  * @license
