9680cc55c7cad4c91d7c8f7f31f4adaeb3e6eda3
atscott <atscott01@gmail.com>
2020-08-17 20:56:12 +0000

    64cf087ae5 release: cut the v10.1.0-next.6 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+29.sha-fec9dcb
+ * @license Angular v10.1.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.5+29.sha-fec9dcb');
+const VERSION = new Version('10.1.0-next.6');
 
 /**
  * @license
