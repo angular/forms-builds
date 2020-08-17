e72c2623ee035c8c247807cd555d97776b7845f8
atscott <atscott01@gmail.com>
2020-08-17 22:58:44 +0000

    e472f5f688 refactor(ngcc): update yargs and typings for yargs (#38470)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6
+ * @license Angular v10.1.0-next.6+3.sha-e472f5f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6');
+const VERSION = new Version('10.1.0-next.6+3.sha-e472f5f');
 
 /**
  * @license
