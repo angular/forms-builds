5c4b9f8a8f33504dcf9d9766c1953f8a9443de98
atscott <atscott01@gmail.com>
2020-08-17 20:41:49 +0000

    fec9dcbeb0 docs: release notes for the v10.0.10 release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+28.sha-e0e5c9f
+ * @license Angular v10.1.0-next.5+29.sha-fec9dcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7034,7 +7034,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.1.0-next.5+28.sha-e0e5c9f');
+    var VERSION = new core.Version('10.1.0-next.5+29.sha-fec9dcb');
 
     /**
      * @license
