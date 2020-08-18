8275152178abac279a33970b9bb7086e699cb995
Andrew Scott <atscott@google.com>
2020-08-18 19:15:32 +0000

    aaa1d8e2fe release: cut the zone.js-0.11.0 release (#38473)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+6.sha-dbfb50e
+ * @license Angular v10.1.0-next.6+7.sha-aaa1d8e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+6.sha-dbfb50e');
+const VERSION = new Version('10.1.0-next.6+7.sha-aaa1d8e');
 
 /**
  * @license
