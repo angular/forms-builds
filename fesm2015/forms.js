92d0d3037c9a5d1a0462340e1be746930ba77a47
Andrew Scott <atscott@google.com>
2020-08-18 17:32:16 +0000

    723a9ff095 docs(common): Wrong parameter description on TrackBy (#38495)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+3.sha-e472f5f
+ * @license Angular v10.1.0-next.6+4.sha-723a9ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+3.sha-e472f5f');
+const VERSION = new Version('10.1.0-next.6+4.sha-723a9ff');
 
 /**
  * @license
