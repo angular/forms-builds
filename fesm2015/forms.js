82c9387c677e5bd68bb45205919cbf7f6e5eb65e
Misko Hevery <misko@hevery.com>
2020-08-19 21:44:35 +0000

    ac461e1efd fix(localize): extract the correct message ids (#38498)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+7.sha-f245c6b
+ * @license Angular v10.1.0-next.7+8.sha-ac461e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+7.sha-f245c6b');
+const VERSION = new Version('10.1.0-next.7+8.sha-ac461e1');
 
 /**
  * @license
