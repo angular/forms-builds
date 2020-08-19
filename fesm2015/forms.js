6523ce5ef230c9270719d1dc55314538e9e83b09
Andrew Scott <atscott@google.com>
2020-08-19 15:49:28 +0000

    0570c240e4 docs: Fix typo in the inputs and outputs guide (#38524)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+8.sha-398118f
+ * @license Angular v10.0.10+9.sha-0570c24
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6404,7 +6404,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10+8.sha-398118f');
+const VERSION = new Version('10.0.10+9.sha-0570c24');
 
 /**
  * @license
