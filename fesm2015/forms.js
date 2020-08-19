d8db69908f7c2298f3d43912396ee9570c025018
Andrew Scott <atscott@google.com>
2020-08-19 00:27:00 +0000

    398118f708 feat(dev-infra): create a wizard for building commit messages (#38457)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+7.sha-dcc3f6d
+ * @license Angular v10.0.10+8.sha-398118f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6404,7 +6404,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.10+7.sha-dcc3f6d');
+const VERSION = new Version('10.0.10+8.sha-398118f');
 
 /**
  * @license
