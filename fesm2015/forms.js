37b1744280c5b31d7a71f6a3a0b01dac788a2024
Misko Hevery <misko@hevery.com>
2020-08-20 17:03:30 +0000

    1775f351d6 fix(docs-infra): fix vertical alignment of external link icons (#38410)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+7.sha-c90262e
+ * @license Angular v10.0.11+10.sha-1775f35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6404,7 +6404,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.11+7.sha-c90262e');
+const VERSION = new Version('10.0.11+10.sha-1775f35');
 
 /**
  * @license
