d41da5e1c8c68c0072e7013d4243d27806382c88
Misko Hevery <misko@hevery.com>
2020-08-20 17:02:09 +0000

    22f1ac3e37 docs: udpate the details (#37967)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+13.sha-8017ca4
+ * @license Angular v10.1.0-next.7+12.sha-22f1ac3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6456,7 +6456,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+13.sha-8017ca4');
+const VERSION = new Version('10.1.0-next.7+12.sha-22f1ac3');
 
 /**
  * @license
