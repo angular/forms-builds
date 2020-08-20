32c5f5a01e48a2bf7c533cd37c4495ab820d5fc4
Misko Hevery <misko@hevery.com>
2020-08-20 16:57:46 +0000

    6442875c99 docs(core): Fix typo in JSDoc for AbstractType<T> (#38541)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+11.sha-4ee5e73
+ * @license Angular v10.1.0-next.7+10.sha-6442875
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+11.sha-4ee5e73');
+const VERSION = new Version('10.1.0-next.7+10.sha-6442875');
 
 /**
  * @license
