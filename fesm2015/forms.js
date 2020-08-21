e3c65f38fcdffd7dcf1327426de907332e9b5d74
Misko Hevery <misko@hevery.com>
2020-08-21 19:53:28 +0000

    874792dc43 feat(compiler): support unary operators for more accurate type checking (#37918)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+17.sha-e7da404
+ * @license Angular v10.1.0-next.7+18.sha-874792d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6725,7 +6725,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+17.sha-e7da404');
+const VERSION = new Version('10.1.0-next.7+18.sha-874792d');
 
 /**
  * @license
