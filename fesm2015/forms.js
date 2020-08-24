824f416cae3bb20870d4ad7532e2442ea684a356
Misko Hevery <misko@hevery.com>
2020-08-24 22:53:49 +0000

    81db6a3171 release: cut the v10.0.12 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+16.sha-8224e65
+ * @license Angular v10.0.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6404,7 +6404,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.11+16.sha-8224e65');
+const VERSION = new Version('10.0.12');
 
 /**
  * @license
