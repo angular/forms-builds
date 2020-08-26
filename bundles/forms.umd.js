0baa2445ff20dc92610b4b611fbb7d95e517d6b5
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:17:49 +0000

    89ff4467fd docs: add Sam Vloeberghs to GDE list (#37970)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14
+ * @license Angular v10.0.14+3.sha-89ff446
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6974,7 +6974,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.14');
+    var VERSION = new core.Version('10.0.14+3.sha-89ff446');
 
     /**
      * @license
