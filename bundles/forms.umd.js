99544fb44539f2c2e8075dc4457038c024a2da5f
Misko Hevery <misko@hevery.com>
2020-08-26 19:15:00 +0000

    7d8dce11c0 release: cut the v10.0.14 release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12+4.sha-9168919
+ * @license Angular v10.0.14
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6974,7 +6974,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.0.12+4.sha-9168919');
+    var VERSION = new core.Version('10.0.14');
 
     /**
      * @license
