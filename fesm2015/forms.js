0ad34e40b84629f18a2ef78cdd46a62633eb625e
Joey Perrott <josephperrott@gmail.com>
2020-08-27 16:27:21 +0000

    164cd274a4 test(docs-infra): add commands to run the unit tests (#34537)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+2.sha-4aee008
+ * @license Angular v10.1.0-rc.0+5.sha-164cd27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6480,7 +6480,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+2.sha-4aee008');
+const VERSION = new Version('10.1.0-rc.0+5.sha-164cd27');
 
 /**
  * @license
