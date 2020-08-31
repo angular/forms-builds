247642ece6ce4e74866c277980af07b6023bd4eb
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:06:32 +0000

    b2857b4e3a docs: remove double space in start-data. (#38642)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-827ba05
+ * @license Angular v10.1.0-rc.0+18.sha-b2857b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+19.sha-827ba05');
+const VERSION = new Version('10.1.0-rc.0+18.sha-b2857b4');
 
 /**
  * @license
