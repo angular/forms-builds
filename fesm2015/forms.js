955b3a29b6d827559362754a0510233996807340
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:52:14 +0000

    b060837e70 docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+21.sha-9a64980
+ * @license Angular v10.0.14+22.sha-b060837
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6278,7 +6278,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+21.sha-9a64980');
+const VERSION = new Version('10.0.14+22.sha-b060837');
 
 /**
  * @license
