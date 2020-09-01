42a1ca8ef4f7fed2fc0d537e9485a892259ae059
Joey Perrott <josephperrott@gmail.com>
2020-09-01 00:32:39 +0000

    626ac45482 docs: Restructure table of contents to provide a more streamlined experience (#38658)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+24.sha-a938180
+ * @license Angular v10.0.14+25.sha-626ac45
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6278,7 +6278,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.14+24.sha-a938180');
+const VERSION = new Version('10.0.14+25.sha-626ac45');
 
 /**
  * @license
