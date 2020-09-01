b86b8bf037eca9c41208ee58b6f44e19363bdb13
Joey Perrott <josephperrott@gmail.com>
2020-09-01 20:34:02 +0000

    59c234cfb4 build: add configuration for the caretaker command (#38601)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+35.sha-d9fea85
+ * @license Angular v10.1.0-rc.0+37.sha-59c234c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+35.sha-d9fea85');
+const VERSION = new Version('10.1.0-rc.0+37.sha-59c234c');
 
 /**
  * @license
