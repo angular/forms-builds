7cc71c1c0419e235f096567ff3296e6d18f39bfd
Andrew Scott <atscott@google.com>
2020-09-02 22:11:21 +0000

    32f33f095f fix(localize): render context of translation file parse errors (#38673)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0
+ * @license Angular v10.1.0+2.sha-32f33f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0');
+const VERSION = new Version('10.1.0+2.sha-32f33f0');
 
 /**
  * @license
