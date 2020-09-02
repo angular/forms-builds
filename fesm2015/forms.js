cc062ba5df05512f16524f2d151823d5aba2a056
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:21:08 +0000

    c01bd0fe8e release: cut the v10.1.0 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-5588324
+ * @license Angular v10.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-rc.0+19.sha-5588324');
+const VERSION = new Version('10.1.0');
 
 /**
  * @license
