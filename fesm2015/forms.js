9c6649c992f52f7e150fec6b01795790b90e7fe5
atscott <atscott01@gmail.com>
2020-09-03 19:22:50 +0000

    25afbcc459 docs: add dayjs date adapter to resources page (#38031)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+6.sha-29c89c9
+ * @license Angular v10.1.0+7.sha-25afbcc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0+6.sha-29c89c9');
+const VERSION = new Version('10.1.0+7.sha-25afbcc');
 
 /**
  * @license
