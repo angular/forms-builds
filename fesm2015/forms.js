796128a09647700f47f30a25f35ea796edcdfea3
atscott <atscott01@gmail.com>
2020-09-08 19:05:59 +0000

    2c4a98a285 fix(localize): do not expose NodeJS typings in $localize runtime code (#38700)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+25.sha-926ffcd
+ * @license Angular v11.0.0-next.0+28.sha-2c4a98a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+25.sha-926ffcd');
+const VERSION = new Version('11.0.0-next.0+28.sha-2c4a98a');
 
 /**
  * @license
