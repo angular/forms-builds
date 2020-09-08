aff4680c88d9bc216f7b947939b3343d1005daa2
atscott <atscott01@gmail.com>
2020-09-08 17:33:03 +0000

    d9da7e5a18 docs: fix result of sanitization example (#38724)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+24.sha-97475d7
+ * @license Angular v11.0.0-next.0+19.sha-d9da7e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+24.sha-97475d7');
+const VERSION = new Version('11.0.0-next.0+19.sha-d9da7e5');
 
 /**
  * @license
