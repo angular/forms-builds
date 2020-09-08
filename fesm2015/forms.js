a5ccc7063954b961af90e13e1fb16ca205809e9b
atscott <atscott01@gmail.com>
2020-09-08 17:31:05 +0000

    261f689e9b docs: remove duplicate trans-unit element closing tag (#38715)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+19.sha-d9da7e5
+ * @license Angular v11.0.0-next.0+21.sha-261f689
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+19.sha-d9da7e5');
+const VERSION = new Version('11.0.0-next.0+21.sha-261f689');
 
 /**
  * @license
