5f989ffffa42e2ab21aaf89ef867ed7506272cce
atscott <atscott01@gmail.com>
2020-09-08 17:31:46 +0000

    97475d7408 build: upgrade all preview-server JS dependencies to latest versions (#38736)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+22.sha-9f28f82
+ * @license Angular v11.0.0-next.0+24.sha-97475d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+22.sha-9f28f82');
+const VERSION = new Version('11.0.0-next.0+24.sha-97475d7');
 
 /**
  * @license
