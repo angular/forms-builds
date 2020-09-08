989c23db9ff673f49a40002e2d261627831743b2
atscott <atscott01@gmail.com>
2020-09-08 17:31:39 +0000

    9f28f82598 docs: add space between icon and text in issue template (#38712)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+16.sha-c2d017d
+ * @license Angular v11.0.0-next.0+22.sha-9f28f82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+16.sha-c2d017d');
+const VERSION = new Version('11.0.0-next.0+22.sha-9f28f82');
 
 /**
  * @license
