f88f6196dea1c102b3191166d173f89a62a8c14a
atscott <atscott01@gmail.com>
2020-09-08 17:27:37 +0000

    c2d017de83 docs: word correction (#38729) PR Close #38729

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+23.sha-a29f9a9
+ * @license Angular v11.0.0-next.0+16.sha-c2d017d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+23.sha-a29f9a9');
+const VERSION = new Version('11.0.0-next.0+16.sha-c2d017d');
 
 /**
  * @license
