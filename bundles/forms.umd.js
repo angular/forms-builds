c183589625b9254682f0a586b1934823aeec7435
atscott <atscott01@gmail.com>
2020-09-09 16:31:51 +0000

    d896c33b0e fix(dev-infra): set build commit message type to allow an optional scope (#38745)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+52.sha-f645d26
+ * @license Angular v11.0.0-next.0+48.sha-d896c33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6952,7 +6952,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.0+52.sha-f645d26');
+    var VERSION = new core.Version('11.0.0-next.0+48.sha-d896c33');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+52.sha-f645d26
+ * @license Angular v11.0.0-next.0+48.sha-d896c33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+52.sha-f645d26');
+const VERSION = new Version('11.0.0-next.0+48.sha-d896c33');
 
 /**
  * @license
