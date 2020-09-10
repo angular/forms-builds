58c0afeb6bacdd52f153284025c360f7d5d2203a
Andrew Kushnir <akushnir@google.com>
2020-09-10 20:09:57 +0000

    19598b47ca feat(compiler-cli): add ability to get symbol of reference or variable (#38618)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+13.sha-26f2820
+ * @license Angular v11.0.0-next.1+19.sha-19598b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+13.sha-26f2820');
+const VERSION = new Version('11.0.0-next.1+19.sha-19598b4');
 
 /**
  * @license
