23ebd7af6fe4f96cbe66706bb3b4a615c41f4f54
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:56:26 +0000

    03447ba52f docs: Move Displaying data topic and ToH tutorial (#38774)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+19.sha-19598b4
+ * @license Angular v11.0.0-next.1+21.sha-03447ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+19.sha-19598b4');
+const VERSION = new Version('11.0.0-next.1+21.sha-03447ba');
 
 /**
  * @license
