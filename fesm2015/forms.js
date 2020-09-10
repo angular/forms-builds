a5b36175f123fd3de8cad9a2cad77f9b3e695710
Andrew Kushnir <akushnir@google.com>
2020-09-10 16:22:20 +0000

    281865bbcf refactor(router): Remove `defer` as it's not needed anymore (#38781)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+5.sha-1373a98
+ * @license Angular v11.0.0-next.1+9.sha-281865b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+5.sha-1373a98');
+const VERSION = new Version('11.0.0-next.1+9.sha-281865b');
 
 /**
  * @license
