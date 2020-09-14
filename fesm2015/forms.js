b9ec29ab5bb10cf523a4b85231e34a1d78f09a02
Andrew Kushnir <akushnir@google.com>
2020-09-14 22:10:02 +0000

    78e1ecb161 feat(dev-infra): allow local ng-dev configuration to error on invalid commit messages (#38784)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+35.sha-3934f0a
+ * @license Angular v11.0.0-next.1+36.sha-78e1ecb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+35.sha-3934f0a');
+const VERSION = new Version('11.0.0-next.1+36.sha-78e1ecb');
 
 /**
  * @license
