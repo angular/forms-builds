087b7f919659031d0532878b81ad8f26144ae1c5
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:52:42 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+34.sha-297c060
+ * @license Angular v11.0.0-next.1+35.sha-3934f0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+34.sha-297c060');
+const VERSION = new Version('11.0.0-next.1+35.sha-3934f0a');
 
 /**
  * @license
