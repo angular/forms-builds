4afd1cb3178fc57825fae85ddef02d8edd42acad
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:54:09 +0000

    3817e5f1df fix(router): Fix arguments order for call to shouldReuseRoute (#26949)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+44.sha-171a0d0
+ * @license Angular v11.0.0-next.1+45.sha-3817e5f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6952,7 +6952,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.1+44.sha-171a0d0');
+    var VERSION = new core.Version('11.0.0-next.1+45.sha-3817e5f');
 
     /**
      * @license
