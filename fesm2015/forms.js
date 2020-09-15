361cce88dbfabd9fb6636821a98ac6daf8bce1dc
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:10:51 +0000

    e00535a2a4 fix(dev-infra): remove ANSI escape codes from log file outputs (#38792)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+38.sha-593bd59
+ * @license Angular v11.0.0-next.1+39.sha-e00535a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+38.sha-593bd59');
+const VERSION = new Version('11.0.0-next.1+39.sha-e00535a');
 
 /**
  * @license
