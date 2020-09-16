c8d7faacc2dc8f4e89bd6396707774c8559bbee3
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:46:52 +0000

    49ee90b1b5 docs: fix typo in architecture.md guide (#38853)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+9.sha-e62a918
+ * @license Angular v11.0.0-next.2+8.sha-49ee90b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3682,7 +3682,7 @@ class FormGroup extends AbstractControl {
         this.updateValueAndValidity(options);
     }
     /**
-     * Resets the `FormGroup`, marks all descendants `pristine` and `untouched` and sets
+     * Resets the `FormGroup`, marks all descendants are marked `pristine` and `untouched`, and
      * the value of all descendants to null.
      *
      * You reset to a specific form state by passing in a map of states
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+9.sha-e62a918');
+const VERSION = new Version('11.0.0-next.2+8.sha-49ee90b');
 
 /**
  * @license
