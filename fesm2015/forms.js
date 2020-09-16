dad124b41f0cf182b6ad5ab4094ee93d1445f71d
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:53:07 +0000

    dd8d8c8289 fix(common): add `params` and `reportProgress` options to `HttpClient.put()` overload (#37873)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+11.sha-722699f
+ * @license Angular v11.0.0-next.2+13.sha-dd8d8c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+11.sha-722699f');
+const VERSION = new Version('11.0.0-next.2+13.sha-dd8d8c8');
 
 /**
  * @license
