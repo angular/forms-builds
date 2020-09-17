6ed21cf31c7d324b45e9e83626a412cda8e78b85
Misko Hevery <misko@hevery.com>
2020-09-17 16:58:08 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+12.sha-1291071
+ * @license Angular v11.0.0-next.2+15.sha-a33d630
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+12.sha-1291071');
+const VERSION = new Version('11.0.0-next.2+15.sha-a33d630');
 
 /**
  * @license
