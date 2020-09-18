fdc49a1f1169cf420f97062811cee59636059c9c
Misko Hevery <misko@hevery.com>
2020-09-18 18:45:20 +0000

    2d6105a784 feat(dev-infra): output the number of new and fixed cycles (#38805)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+22.sha-5548571
+ * @license Angular v11.0.0-next.2+25.sha-2d6105a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+22.sha-5548571');
+const VERSION = new Version('11.0.0-next.2+25.sha-2d6105a');
 
 /**
  * @license
