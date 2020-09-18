40a5d72c056ad8b985023e3da102ae38f0a929e6
Misko Hevery <misko@hevery.com>
2020-09-18 18:49:12 +0000

    d92a0dd72f fix(zone.js): should invoke xhr send task when no response error occurs (#38836)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+26.sha-88d7bb8
+ * @license Angular v11.0.0-next.2+23.sha-d92a0dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+26.sha-88d7bb8');
+const VERSION = new Version('11.0.0-next.2+23.sha-d92a0dd');
 
 /**
  * @license
