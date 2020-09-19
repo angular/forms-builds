cc64e2d7aef661d9e1fe9d863a22821174b286a1
Misko Hevery <misko@hevery.com>
2020-09-19 00:25:44 +0000

    a91f0f6b82 refactor(compiler): refactor ast spans test to be more human readable (#38902)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+28.sha-b68fab5
+ * @license Angular v11.0.0-next.2+30.sha-a91f0f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+28.sha-b68fab5');
+const VERSION = new Version('11.0.0-next.2+30.sha-a91f0f6');
 
 /**
  * @license
