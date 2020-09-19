61c337610e53b13b4c53beea82deb7efbd55fc54
Misko Hevery <misko@hevery.com>
2020-09-19 00:11:58 +0000

    b68fab547a ci: do not require g3 checks for the changes in `packages/zone.js/dist` folder (#38901)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+23.sha-d92a0dd
+ * @license Angular v11.0.0-next.2+28.sha-b68fab5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+23.sha-d92a0dd');
+const VERSION = new Version('11.0.0-next.2+28.sha-b68fab5');
 
 /**
  * @license
