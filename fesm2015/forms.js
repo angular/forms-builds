2628a379063368d2518ad428e6063845cad21216
Misko Hevery <misko@hevery.com>
2020-09-21 19:51:56 +0000

    2b1b7180db fix(forms): type NG_VALUE_ACCESSOR injection token as array (#29723)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
+ * @license Angular v11.0.0-next.2+37.sha-2b1b718
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+36.sha-fdd4fa0');
+const VERSION = new Version('11.0.0-next.2+37.sha-2b1b718');
 
 /**
  * @license
