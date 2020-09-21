38bd1032f3bc24ed93e4cbfeb3bdbc0d04a1b975
Misko Hevery <misko@hevery.com>
2020-09-21 23:59:13 +0000

    145ab3d7e0 docs: Update titles to getting started topics (#38887)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+48.sha-3082f73
+ * @license Angular v11.0.0-next.2+49.sha-145ab3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+48.sha-3082f73');
+const VERSION = new Version('11.0.0-next.2+49.sha-145ab3d');
 
 /**
  * @license
