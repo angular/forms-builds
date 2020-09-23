7fdf5ce38cf063f3f98f518ef20bcc193182521b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:14:33 +0000

    ffe89fb07d test(docs-infra): remove usage of blacklist in test naming (#38927)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+1.sha-15ea811
+ * @license Angular v11.0.0-next.3+2.sha-ffe89fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+1.sha-15ea811');
+const VERSION = new Version('11.0.0-next.3+2.sha-ffe89fb');
 
 /**
  * @license
