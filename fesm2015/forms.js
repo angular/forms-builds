955525e464f8df5d29c3565826ded5d675a81c85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:53:45 +0000

    cf9176eb46 docs(docs-infra): remove usage of whitelist in aio-builds-setup docs (#38964)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+18.sha-2f6777d
+ * @license Angular v11.0.0-next.3+19.sha-cf9176e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+18.sha-2f6777d');
+const VERSION = new Version('11.0.0-next.3+19.sha-cf9176e');
 
 /**
  * @license
