985157822799c524ff0e0b1ed739f2a3d3fa25c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:52:42 +0000

    2f6777df18 docs(docs-infra): add upcoming conference (#38956)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+12.sha-7f6f8bb
+ * @license Angular v11.0.0-next.3+18.sha-2f6777d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+12.sha-7f6f8bb');
+const VERSION = new Version('11.0.0-next.3+18.sha-2f6777d');
 
 /**
  * @license
