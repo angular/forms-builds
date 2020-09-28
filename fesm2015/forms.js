6f9d87e4f3b06c9c33378ded2c620368a1af1b8c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:57:30 +0000

    06525cfed3 test(compiler-cli): fix tests to have at least one component (#39011)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+77.sha-3ba97ab
+ * @license Angular v11.0.0-next.3+72.sha-06525cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+77.sha-3ba97ab');
+const VERSION = new Version('11.0.0-next.3+72.sha-06525cf');
 
 /**
  * @license
