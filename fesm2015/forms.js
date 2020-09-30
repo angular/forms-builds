ac206c7c9632be60770e432ed196863857106aa9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:41:59 +0000

    1b70dc931d docs: edit template-statements doc (#38742)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+80.sha-a6971ba
+ * @license Angular v11.0.0-next.3+78.sha-1b70dc9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+80.sha-a6971ba');
+const VERSION = new Version('11.0.0-next.3+78.sha-1b70dc9');
 
 /**
  * @license
