10114db8ee24f709937c1e5a73166789a143ab6f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:38:11 +0000

    a6971ba89a release: cut the zone.js-0.11.2 release (#38911)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+76.sha-e2997ed
+ * @license Angular v11.0.0-next.3+80.sha-a6971ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+76.sha-e2997ed');
+const VERSION = new Version('11.0.0-next.3+80.sha-a6971ba');
 
 /**
  * @license
