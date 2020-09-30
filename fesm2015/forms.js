2b55c138bb2cb9cde41aceb5e6ae73ffa0b58e3b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 17:30:44 +0000

    a8134dcfd4 release: cut the v10.1.4 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+72.sha-a1bf0de
+ * @license Angular v10.1.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.3+72.sha-a1bf0de');
+const VERSION = new Version('10.1.4');
 
 /**
  * @license
