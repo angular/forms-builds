a38d2f452d987b84021415427d8b3726d3891f61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:51:29 +0000

    8f11b516f8 refactor(compiler-cli): API for getting components from a template file (#39002)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+90.sha-393ce55
+ * @license Angular v11.0.0-next.3+87.sha-8f11b51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+90.sha-393ce55');
+const VERSION = new Version('11.0.0-next.3+87.sha-8f11b51');
 
 /**
  * @license
