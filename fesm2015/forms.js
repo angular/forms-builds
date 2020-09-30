edaeed27740e18c080522a0e32bc05275d98efad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:43:58 +0000

    c8958d76db build(docs-infra): simplify updating dependencies in docs examples boilerplate (#38992)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+81.sha-b16a69c
+ * @license Angular v11.0.0-next.3+86.sha-c8958d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6643,7 +6643,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+81.sha-b16a69c');
+const VERSION = new Version('11.0.0-next.3+86.sha-c8958d7');
 
 /**
  * @license
