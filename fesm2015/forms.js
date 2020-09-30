513d3834b4ae3d67ff0841436b0b3f800b46748e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:57:09 +0000

    8b7acc4f8f refactor(compiler): Binding parser sets binding span as source span in Ivy (#39036)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+93.sha-e8084ff
+ * @license Angular v11.0.0-next.3+91.sha-8b7acc4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+93.sha-e8084ff');
+const VERSION = new Version('11.0.0-next.3+91.sha-8b7acc4');
 
 /**
  * @license
