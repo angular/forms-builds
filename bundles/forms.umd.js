62ddabba2a940c2b16487e37457503358ebb268a
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:59:20 +0000

    ac54019315 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+23.sha-8894706
+ * @license Angular v11.0.0-next.4+22.sha-ac54019
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6952,7 +6952,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.4+23.sha-8894706');
+    var VERSION = new core.Version('11.0.0-next.4+22.sha-ac54019');
 
     /**
      * @license
