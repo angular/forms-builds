49bab3bf8b8b836aa7adb953fc44cd4256cd4ede
Joey Perrott <josephperrott@gmail.com>
2020-10-01 17:38:51 +0000

    d66d82a9b5 Revert "fix(core): use single quotes for relative link resolution migration to align with style guide (#39070)" (#39082)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+22.sha-ac54019
+ * @license Angular v11.0.0-next.4+24.sha-d66d82a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6374,7 +6374,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+22.sha-ac54019');
+const VERSION = new Version('11.0.0-next.4+24.sha-d66d82a');
 
 /**
  * @license
