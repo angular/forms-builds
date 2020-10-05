fe03970137348852e3746bcefc4340aaf021d992
Joey Perrott <josephperrott@gmail.com>
2020-10-05 21:46:04 +0000

    57f8dd2978 test(compiler): add tests for parsing of malformed property reads (#38998)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+44.sha-bc69182
+ * @license Angular v11.0.0-next.4+45.sha-57f8dd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6953,7 +6953,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.0-next.4+44.sha-bc69182');
+    var VERSION = new core.Version('11.0.0-next.4+45.sha-57f8dd2');
 
     /**
      * @license
