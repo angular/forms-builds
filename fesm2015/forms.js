347201207c4988db5639f12dd0295a3a6d907ed4
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:20:27 +0000

    072b707b38 ci: update to latest version of sauce-connect (#39073)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+24.sha-878e2f0
+ * @license Angular v10.1.4+27.sha-072b707
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.4+24.sha-878e2f0');
+const VERSION = new Version('10.1.4+27.sha-072b707');
 
 /**
  * @license
