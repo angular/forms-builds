5f73226e7ce2c21b4f4a3b2fb1bb51ab618c735a
Joey Perrott <josephperrott@gmail.com>
2020-10-06 18:59:43 +0000

    70e13dc31e fix(language-service): [Ivy] hybrid visitor should not locate let keyword (#39061)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+54.sha-246de9a
+ * @license Angular v11.0.0-next.4+55.sha-70e13dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+54.sha-246de9a');
+const VERSION = new Version('11.0.0-next.4+55.sha-70e13dc');
 
 /**
  * @license
