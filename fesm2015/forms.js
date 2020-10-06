a16f2ee49c81b8e654a6d3f250159d8308f07f0d
Joey Perrott <josephperrott@gmail.com>
2020-10-06 21:21:16 +0000

    f2fca6d58e refactor(compiler-cli): add a global autocompletion API (#39048)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+60.sha-aeec223
+ * @license Angular v11.0.0-next.4+59.sha-f2fca6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+60.sha-aeec223');
+const VERSION = new Version('11.0.0-next.4+59.sha-f2fca6d');
 
 /**
  * @license
