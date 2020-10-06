89614639348df358bc84d8dc8ce812651d8d888f
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:36:58 +0000

    21dfb33b27 build: update bazelversion (#39123)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+50.sha-3c474ec
+ * @license Angular v11.0.0-next.4+51.sha-21dfb33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+50.sha-3c474ec');
+const VERSION = new Version('11.0.0-next.4+51.sha-21dfb33');
 
 /**
  * @license
