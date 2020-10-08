45122261939e8d8fab075aa633164a791318690a
atscott <atscott01@gmail.com>
2020-10-08 16:07:20 +0000

    01ec0f0933 feat(docs-infra): add short url for strict mode guide (#39129)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+1.sha-4ede190
+ * @license Angular v11.0.0-next.5+3.sha-01ec0f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+1.sha-4ede190');
+const VERSION = new Version('11.0.0-next.5+3.sha-01ec0f0');
 
 /**
  * @license
