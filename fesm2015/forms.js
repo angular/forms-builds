7c0a51a493aa27b5fe3b086877d5675531ee73d6
atscott <atscott01@gmail.com>
2020-10-08 17:03:27 +0000

    b417dd7b9c docs: fix for HttpRequest expansion panel (#39166)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+4.sha-8cf16b4
+ * @license Angular v10.1.5+5.sha-b417dd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+4.sha-8cf16b4');
+const VERSION = new Version('10.1.5+5.sha-b417dd7');
 
 /**
  * @license
