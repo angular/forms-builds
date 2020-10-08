35920af76c0dd4f53b6af7840dcdefbb1f358d6b
atscott <atscott01@gmail.com>
2020-10-08 17:35:15 +0000

    049b453149 fix(core): migrate relative link resolution with single quotes (#39102)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+6.sha-dace4aa
+ * @license Angular v11.0.0-next.5+7.sha-049b453
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+6.sha-dace4aa');
+const VERSION = new Version('11.0.0-next.5+7.sha-049b453');
 
 /**
  * @license
