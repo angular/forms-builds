4f3bab1a56961d71d0ab0ecb7c91ff82125659d8
atscott <atscott01@gmail.com>
2020-10-08 19:22:47 +0000

    4a1c12c773 feat(core): remove ViewEncapsulation.Native (#38882)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+8.sha-42a164f
+ * @license Angular v11.0.0-next.5+13.sha-4a1c12c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+8.sha-42a164f');
+const VERSION = new Version('11.0.0-next.5+13.sha-4a1c12c');
 
 /**
  * @license
