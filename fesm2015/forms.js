e52c6ad6202368d7ccca56dbddf41993648e25ea
atscott <atscott01@gmail.com>
2020-10-09 15:22:23 +0000

    8913aee527 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+6.sha-7628c36
+ * @license Angular v10.1.5+7.sha-8913aee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+6.sha-7628c36');
+const VERSION = new Version('10.1.5+7.sha-8913aee');
 
 /**
  * @license
