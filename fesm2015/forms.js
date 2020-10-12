e3a7d3ec327b26b90f6d0c767a31e044e22e157d
atscott <atscott01@gmail.com>
2020-10-12 16:10:22 +0000

    e930b0cd19 build(docs-infra): support passing args to `ng serve` via `serve-and-sync` (#39201)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+20.sha-4cc5982
+ * @license Angular v11.0.0-next.5+22.sha-e930b0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+20.sha-4cc5982');
+const VERSION = new Version('11.0.0-next.5+22.sha-e930b0c');
 
 /**
  * @license
