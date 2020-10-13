49cb654c37d79cd316f488f593be805c7b726e20
atscott <atscott01@gmail.com>
2020-10-13 17:23:27 +0000

    5ce71e0fbc feat(core): add automated migration to replace async with waitForAsync (#39212)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+48.sha-437e563
+ * @license Angular v11.0.0-next.5+49.sha-5ce71e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+48.sha-437e563');
+const VERSION = new Version('11.0.0-next.5+49.sha-5ce71e0');
 
 /**
  * @license
