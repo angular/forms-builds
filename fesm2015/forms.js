58f140679834b18be4813ed19bee34b062b87ffa
atscott <atscott01@gmail.com>
2020-10-13 22:22:11 +0000

    23fc2b43ac updated sajee's profile (#39019)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+18.sha-3487d5a
+ * @license Angular v10.1.5+20.sha-23fc2b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+18.sha-3487d5a');
+const VERSION = new Version('10.1.5+20.sha-23fc2b4');
 
 /**
  * @license
