965ba2ce2823ae6f896fa523a76cfbf50b6fbe93
atscott <atscott01@gmail.com>
2020-10-14 16:58:23 +0000

    5221df8e92 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+20.sha-23fc2b4
+ * @license Angular v10.1.5+22.sha-5221df8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+20.sha-23fc2b4');
+const VERSION = new Version('10.1.5+22.sha-5221df8');
 
 /**
  * @license
