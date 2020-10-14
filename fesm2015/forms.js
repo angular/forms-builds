4965c767ded83254be88d2661cd995548d130095
atscott <atscott01@gmail.com>
2020-10-14 17:48:20 +0000

    df0941554b docs: add Discord as an official communication channel (#39149)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+22.sha-5221df8
+ * @license Angular v10.1.5+23.sha-df09415
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.5+22.sha-5221df8');
+const VERSION = new Version('10.1.5+23.sha-df09415');
 
 /**
  * @license
