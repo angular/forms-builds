ddb9844239c4912b718131f9a1de42710383f760
atscott <atscott01@gmail.com>
2020-10-14 22:03:47 +0000

    822b838fbc fix(ngcc): ensure that "inline exports" can be interpreted correctly (#39267)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+5.sha-a83693d
+ * @license Angular v11.0.0-next.6+10.sha-822b838
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+5.sha-a83693d');
+const VERSION = new Version('11.0.0-next.6+10.sha-822b838');
 
 /**
  * @license
