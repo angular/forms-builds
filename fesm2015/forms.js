b894069b8de31c3caa7d1c3aa50dee273b5905cd
Andrew Kushnir <akushnir@google.com>
2020-10-15 23:30:56 +0000

    5bda62c51d refactor(language-service): Return directive defs when input name is part of selector (#39243)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+22.sha-7669827
+ * @license Angular v11.0.0-next.6+24.sha-5bda62c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+22.sha-7669827');
+const VERSION = new Version('11.0.0-next.6+24.sha-5bda62c');
 
 /**
  * @license
