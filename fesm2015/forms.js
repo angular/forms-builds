76444811fe93ed09941e238dad851fb74e727ecc
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:32:45 +0000

    492e236ae6 docs: Typos fixes in built-in directives guide (#38520)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+36.sha-4dc8d83
+ * @license Angular v11.0.0-next.6+35.sha-492e236
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+36.sha-4dc8d83');
+const VERSION = new Version('11.0.0-next.6+35.sha-492e236');
 
 /**
  * @license
