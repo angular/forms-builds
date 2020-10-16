774ccf026fc2fa5b998e13eadde22c9304c0ce45
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:44:25 +0000

    a38293d213 release: cut the zone.js-0.11.2 release to fix several new issues (#39298)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+35.sha-492e236
+ * @license Angular v11.0.0-next.6+37.sha-a38293d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+35.sha-492e236');
+const VERSION = new Version('11.0.0-next.6+37.sha-a38293d');
 
 /**
  * @license
