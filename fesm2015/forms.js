70c6235686c54348c65bcb9241303c0cbcf5bc37
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:45:30 +0000

    4a5f17cd8e build(docs-infra): add a tool to create new examples (#39283)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+16.sha-94d7ef3
+ * @license Angular v10.1.6+18.sha-4a5f17c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+16.sha-94d7ef3');
+const VERSION = new Version('10.1.6+18.sha-4a5f17c');
 
 /**
  * @license
