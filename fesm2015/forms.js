f960615a2d6df31387c79c75566d40a7b415f733
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:16:17 +0000

    b5ec5a7fca ci: separate the windows CI tests into build and test (#39289)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+39.sha-563fb6c
+ * @license Angular v11.0.0-next.6+42.sha-b5ec5a7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+39.sha-563fb6c');
+const VERSION = new Version('11.0.0-next.6+42.sha-b5ec5a7');
 
 /**
  * @license
