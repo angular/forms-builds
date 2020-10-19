3abce878ec4c782c7eda200a2958860f706c8d88
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:54:56 +0000

    02b87505ee docs: clarify grammatical error (#39279)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+26.sha-32a8e72
+ * @license Angular v10.1.6+27.sha-02b8750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+26.sha-32a8e72');
+const VERSION = new Version('10.1.6+27.sha-02b8750');
 
 /**
  * @license
