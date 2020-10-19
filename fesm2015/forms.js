07c1b5ea816a401ac519292d2f9a87c9a83db5e3
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:54:06 +0000

    d3087be237 feat(dev-infra): prompt caretaker to confirm the merge branches on merge (#39333)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+38.sha-aba1cb4
+ * @license Angular v11.0.0-next.6+39.sha-d3087be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6375,7 +6375,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+38.sha-aba1cb4');
+const VERSION = new Version('11.0.0-next.6+39.sha-d3087be');
 
 /**
  * @license
