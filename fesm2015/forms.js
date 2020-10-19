99f18cbdd802cea79669b629c69fc28e535a8b15
Andrew Kushnir <akushnir@google.com>
2020-10-19 15:34:27 +0000

    a93b7fd674 build(docs-infra): upgrade cli command docs sources to 04ae7cbf5 (#39318)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+43.sha-79d67dc
+ * @license Angular v11.0.0-next.6+47.sha-a93b7fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+43.sha-79d67dc');
+const VERSION = new Version('11.0.0-next.6+47.sha-a93b7fd');
 
 /**
  * @license
