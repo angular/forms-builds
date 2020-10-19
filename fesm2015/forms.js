c68938466b676793806b7557f0068490e0e6f5a8
Andrew Kushnir <akushnir@google.com>
2020-10-19 18:47:46 +0000

    7827d3ee73 docs: change definition of NgModules in Angular concepts guide (#36179)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+24.sha-38db976
+ * @license Angular v10.1.6+25.sha-7827d3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6354,7 +6354,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+24.sha-38db976');
+const VERSION = new Version('10.1.6+25.sha-7827d3e');
 
 /**
  * @license
