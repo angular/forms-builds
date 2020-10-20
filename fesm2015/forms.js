a7442f73f775a90fa030dfe9f94564f46dad3058
Andrew Kushnir <akushnir@google.com>
2020-10-20 00:10:41 +0000

    7040f1714b docs: add Jessica Janiuk to contributors.json (#39253)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+52.sha-41c2228
+ * @license Angular v11.0.0-next.6+54.sha-7040f17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6644,7 +6644,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+52.sha-41c2228');
+const VERSION = new Version('11.0.0-next.6+54.sha-7040f17');
 
 /**
  * @license
