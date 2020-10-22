2f7a013203a68f5cac177f585bc8b62956aa89bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-22 01:55:31 +0000

    08f3d62391 refactor(core): clean up circular dependencies (#39233)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+69.sha-eb4c05d
+ * @license Angular v11.0.0-next.6+81.sha-08f3d62
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6375,7 +6375,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+69.sha-eb4c05d');
+const VERSION = new Version('11.0.0-next.6+81.sha-08f3d62');
 
 /**
  * @license
