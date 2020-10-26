7117942fe3ceb8554efae11e648eaf27460d7120
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:17:43 +0000

    3236ae0ee1 docs: fix typo in component overview (#39425)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+103.sha-35fdc6d
+ * @license Angular v11.0.0-next.6+105.sha-3236ae0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6656,7 +6656,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+103.sha-35fdc6d');
+const VERSION = new Version('11.0.0-next.6+105.sha-3236ae0');
 
 /**
  * @license
