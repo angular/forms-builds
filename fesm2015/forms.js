e0447e4a22a2bf7bab5d0aadb79a5870c68a6926
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:46:06 +0000

    bdec818b3a docs: release notes for the v11.0.0-rc.1 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+146.sha-e42d3ec
+ * @license Angular v11.0.0-next.6+147.sha-bdec818
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6615,7 +6615,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+146.sha-e42d3ec');
+const VERSION = new Version('11.0.0-next.6+147.sha-bdec818');
 
 /**
  * @license
