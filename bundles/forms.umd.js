ad50cd6f3ffbdfecc024ee9ddb366822d877b5ce
Joey Perrott <josephperrott@gmail.com>
2020-10-28 22:03:35 +0000

    6b3db15080 docs: move template ref vars doc to concepts section clarify, add scope section, and update headers (#31195)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1
+ * @license Angular v10.2.1+2.sha-6b3db15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6963,7 +6963,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('10.2.1');
+    var VERSION = new core.Version('10.2.1+2.sha-6b3db15');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1
+ * @license Angular v10.2.1+2.sha-6b3db15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6385,7 +6385,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1');
+const VERSION = new Version('10.2.1+2.sha-6b3db15');
 
 /**
  * @license
