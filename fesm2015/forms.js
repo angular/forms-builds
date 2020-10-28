52eb5a7581410c26b9c15dc184712a0424b42961
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:23:11 +0000

    20c26cd49b build: add PullApprove: disable as a caretaker note label (#39430)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+137.sha-1508fd2
+ * @license Angular v11.0.0-next.6+140.sha-20c26cd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6346,7 +6346,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+137.sha-1508fd2');
+const VERSION = new Version('11.0.0-next.6+140.sha-20c26cd');
 
 /**
  * @license
