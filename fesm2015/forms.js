37d8a1bca84e9dcfc9cfc4d4a32fe433c98b45b1
Joey Perrott <josephperrott@gmail.com>
2020-10-29 17:17:25 +0000

    cff61b2abe fix(dev-infra): update generated ng-dev compiled output (#39474)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+4.sha-9205fce
+ * @license Angular v10.2.1+8.sha-cff61b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6385,7 +6385,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1+4.sha-9205fce');
+const VERSION = new Version('10.2.1+8.sha-cff61b2');
 
 /**
  * @license
