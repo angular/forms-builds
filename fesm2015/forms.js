11edb1a34c229ce1035435af2ad075863d92a5c7
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:16:40 +0000

    17070af417 build: update to CLI 11.0.0-rc.1 (#39432)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+159.sha-e336572
+ * @license Angular v11.0.0-next.6+161.sha-17070af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6348,7 +6348,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+159.sha-e336572');
+const VERSION = new Version('11.0.0-next.6+161.sha-17070af');
 
 /**
  * @license
