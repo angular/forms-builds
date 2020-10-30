36565d502813cf1ebb5d19b0076f008a324851f3
Joey Perrott <josephperrott@gmail.com>
2020-10-30 23:46:42 +0000

    3af585c004 fix(router): Ensure all outlets are used when commands have a prefix (#39456)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+170.sha-f12157c
+ * @license Angular v11.0.0-next.6+172.sha-3af585c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6617,7 +6617,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+170.sha-f12157c');
+const VERSION = new Version('11.0.0-next.6+172.sha-3af585c');
 
 /**
  * @license
