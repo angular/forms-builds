480ede3c52dff066f30efc0e1526d0b105b12035
Joey Perrott <josephperrott@gmail.com>
2020-10-30 20:04:21 +0000

    eb625b993e docs: update CLI workspace config documentation (#39427)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+18.sha-a8e0db7
+ * @license Angular v11.0.0-rc.1+19.sha-eb625b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6348,7 +6348,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+18.sha-a8e0db7');
+const VERSION = new Version('11.0.0-rc.1+19.sha-eb625b9');
 
 /**
  * @license
