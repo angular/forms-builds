d8c7311991087e9d4b250a49a89099d9b1b75760
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:25:26 +0000

    67ea7b6098 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+21.sha-b2f3952
+ * @license Angular v11.0.0-rc.1+22.sha-67ea7b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6348,7 +6348,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+21.sha-b2f3952');
+const VERSION = new Version('11.0.0-rc.1+22.sha-67ea7b6');
 
 /**
  * @license
