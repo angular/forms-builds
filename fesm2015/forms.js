f9c5662ebbe628c2104f2bdcfb9c349ef17a9a34
Joey Perrott <josephperrott@gmail.com>
2020-11-02 21:00:04 +0000

    a0b6548a94 build(docs-infra): ensure that deployment works on CI (#39535)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+33.sha-d386fb3
+ * @license Angular v11.0.0-rc.1+35.sha-a0b6548
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6348,7 +6348,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-rc.1+33.sha-d386fb3');
+const VERSION = new Version('11.0.0-rc.1+35.sha-a0b6548');
 
 /**
  * @license
