649aa822524ffc691f2bfe9a40b6d56d318df6bb
Joey Perrott <josephperrott@gmail.com>
2020-11-02 16:26:53 +0000

    cf48d508af build(docs-infra): upgrade `firebase-tools` to v8.14.1 (#39470)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+173.sha-3b0b7d2
+ * @license Angular v11.0.0-next.6+184.sha-cf48d50
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6348,7 +6348,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+173.sha-3b0b7d2');
+const VERSION = new Version('11.0.0-next.6+184.sha-cf48d50');
 
 /**
  * @license
