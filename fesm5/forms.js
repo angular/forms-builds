7d32b48cfd59089a5b7ca0b97ea41f91f289bb8b
Joey Perrott <josephperrott@gmail.com>
2020-11-02 21:03:12 +0000

    2d0bf68380 build(docs-infra): ensure that deployment works on CI (#39541)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+8.sha-e9ff09a
+ * @license Angular v9.1.12+9.sha-2d0bf68
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6822,7 +6822,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12+8.sha-e9ff09a');
+var VERSION = new Version('9.1.12+9.sha-2d0bf68');
 
 /**
  * @license
