09c69906bf2a8f858c328bc856237644d309a936
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:21:36 +0000

    d0dd0e80f6 feat(dev-infra): provide tooling to check what branches a pr targets (#39504)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+192.sha-7e33cb9
+ * @license Angular v11.0.0-next.6+193.sha-d0dd0e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6617,7 +6617,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+192.sha-7e33cb9');
+const VERSION = new Version('11.0.0-next.6+193.sha-d0dd0e8');
 
 /**
  * @license
