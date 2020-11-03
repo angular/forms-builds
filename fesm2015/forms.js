2deafaf2e547475cbb9378ea021cf637bf02d402
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:07:17 +0000

    be64f98076 fix(dev-infra): run fetches in the git client in quiet mode (#39503)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+189.sha-3cab823
+ * @license Angular v11.0.0-next.6+191.sha-be64f98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6617,7 +6617,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+189.sha-3cab823');
+const VERSION = new Version('11.0.0-next.6+191.sha-be64f98');
 
 /**
  * @license
