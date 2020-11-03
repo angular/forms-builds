2a9d197eca672307e65402e452032b7a0c051c54
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:04:56 +0000

    be64f98076 fix(dev-infra): run fetches in the git client in quiet mode (#39503)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+189.sha-3cab823
+ * @license Angular v11.0.0-next.6+191.sha-be64f98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6348,7 +6348,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+189.sha-3cab823');
+const VERSION = new Version('11.0.0-next.6+191.sha-be64f98');
 
 /**
  * @license
