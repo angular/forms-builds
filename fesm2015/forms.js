6e97e166dfce439f3078f25fb9dd432fc43163af
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:56:52 +0000

    2d79780384 fix(dev-infra): run caretaker checks asyncronously (#39086)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+194.sha-1e91f84
+ * @license Angular v11.0.0-next.6+195.sha-2d79780
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6617,7 +6617,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+194.sha-1e91f84');
+const VERSION = new Version('11.0.0-next.6+195.sha-2d79780');
 
 /**
  * @license
