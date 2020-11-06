90dfac1c022a9c63d85103c96f346c85c2802da7
Misko Hevery <misko@hevery.com>
2020-11-06 17:52:56 +0000

    b9b9178458 fix(compiler-cli): do not drop non-Angular decorators when downleveling (#39577)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+216.sha-3a1d36c
+ * @license Angular v11.0.0-next.6+217.sha-b9b9178
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6701,7 +6701,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+216.sha-3a1d36c');
+const VERSION = new Version('11.0.0-next.6+217.sha-b9b9178');
 
 /**
  * @license
