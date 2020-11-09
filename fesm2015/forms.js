42605c38f612dbc5e37ab13c1a237f0599245b53
Misko Hevery <misko@hevery.com>
2020-11-09 16:09:48 +0000

    0edf6fc7f6 ci: log commands output when deploying angular.io to Firebase (#39596)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+226.sha-cf88ea0
+ * @license Angular v11.0.0-next.6+229.sha-0edf6fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6701,7 +6701,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+226.sha-cf88ea0');
+const VERSION = new Version('11.0.0-next.6+229.sha-0edf6fc');
 
 /**
  * @license
