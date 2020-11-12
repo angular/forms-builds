7b03e040a87cf780347a1bec2f6373b1a90382c5
atscott <atscott01@gmail.com>
2020-11-12 17:29:41 +0000

    3be344cdc2 docs: correct appearance of numbering (#39638)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+234.sha-6d13473
+ * @license Angular v11.0.0-next.6+235.sha-3be344c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6701,7 +6701,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+234.sha-6d13473');
+const VERSION = new Version('11.0.0-next.6+235.sha-3be344c');
 
 /**
  * @license
