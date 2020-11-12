1c97ee3fcb196ff4f51c08f2e18e1d759394d27c
atscott <atscott01@gmail.com>
2020-11-12 17:30:38 +0000

    d1355caa74 docs: added missing slash to close tag (#39626) PR Close #39626

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+235.sha-3be344c
+ * @license Angular v11.0.0-next.6+237.sha-d1355ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6701,7 +6701,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+235.sha-3be344c');
+const VERSION = new Version('11.0.0-next.6+237.sha-d1355ca');
 
 /**
  * @license
