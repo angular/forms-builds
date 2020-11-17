99c50791bb802c36c2a52c314b81138c98445576
atscott <atscott01@gmail.com>
2020-11-17 21:37:29 +0000

    39266654e6 fix(http): queue jsonp <script> tag onLoad event handler in microtask (#39512)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+290.sha-49410f8
+ * @license Angular v11.0.0-next.6+289.sha-3926665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6752,7 +6752,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+290.sha-49410f8');
+const VERSION = new Version('11.0.0-next.6+289.sha-3926665');
 
 /**
  * @license
