6eb38173f07d327edb78dbc287de984d0d5190b6
atscott <atscott01@gmail.com>
2020-11-17 18:39:39 +0000

    882ff8fc37 perf(compiler): use raw bytes to represent utf-8 encoded strings (#39694)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+35.sha-9e87fc8
+ * @license Angular v11.0.0+37.sha-882ff8f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6483,7 +6483,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0+35.sha-9e87fc8');
+const VERSION = new Version('11.0.0+37.sha-882ff8f');
 
 /**
  * @license
