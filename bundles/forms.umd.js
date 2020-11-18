c007910fb44f49ba5320fac316ce714ea200f4c8
atscott <atscott01@gmail.com>
2020-11-18 19:31:43 +0000

    ea2312ea85 release: cut the v11.1.0-next.0 release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+307.sha-79bd1bd
+ * @license Angular v11.1.0-next.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7401,7 +7401,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+307.sha-79bd1bd');
+    var VERSION = new i0.Version('11.1.0-next.0');
 
     /**
      * @license
