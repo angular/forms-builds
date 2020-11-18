04c6169b3e54a982b1a48e172ac1662c4d43a8a9
atscott <atscott01@gmail.com>
2020-11-18 20:35:16 +0000

    3273856d33 docs: fixes on paragraph (#39664)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+3.sha-cc509d6
+ * @license Angular v11.1.0-next.0+4.sha-3273856
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6752,7 +6752,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+3.sha-cc509d6');
+const VERSION = new Version('11.1.0-next.0+4.sha-3273856');
 
 /**
  * @license
