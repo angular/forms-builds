fb6cd68c35adcccbd769faa0bb613d12d2863315
atscott <atscott01@gmail.com>
2020-11-18 17:35:31 +0000

    b33b89d441 fix(common): add `HttpParamsOptions` to the public api (#35829)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+294.sha-0ab833f
+ * @license Angular v11.0.0-next.6+295.sha-b33b89d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6483,7 +6483,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+294.sha-0ab833f');
+const VERSION = new Version('11.0.0-next.6+295.sha-b33b89d');
 
 /**
  * @license
