d479b8a097c49809bf528a3a5942debe05af83dd
atscott <atscott01@gmail.com>
2020-11-18 17:49:12 +0000

    6fbe21941d refactor(core): Replace non-null assertion operator with property initialization (#39730)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+299.sha-3b2e5be
+ * @license Angular v11.0.0-next.6+298.sha-6fbe219
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6752,7 +6752,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+299.sha-3b2e5be');
+const VERSION = new Version('11.0.0-next.6+298.sha-6fbe219');
 
 /**
  * @license
