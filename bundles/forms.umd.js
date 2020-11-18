b4e8f54e1432c54076dc13b51e5238735a8f3ea6
atscott <atscott01@gmail.com>
2020-11-18 20:34:15 +0000

    26533a0954 docs: fixes on paragraph (#39664)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1
+ * @license Angular v11.0.1+3.sha-26533a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7012,7 +7012,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.1');
+    var VERSION = new core.Version('11.0.1+3.sha-26533a0');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1
+ * @license Angular v11.0.1+3.sha-26533a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6483,7 +6483,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.1');
+const VERSION = new Version('11.0.1+3.sha-26533a0');
 
 /**
  * @license
