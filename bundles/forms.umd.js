0c742ee1722d1906ae81b4b88cae9e3eea76c379
Andrew Kushnir <akushnir@google.com>
2020-11-20 16:59:13 +0000

    66d863febe fix(animations): getAnimationStyle causes exceptions in older browsers (#29709)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+36.sha-fe1ada8
+ * @license Angular v11.1.0-next.0+38.sha-66d863f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7401,7 +7401,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.0+36.sha-fe1ada8');
+    var VERSION = new i0.Version('11.1.0-next.0+38.sha-66d863f');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+36.sha-fe1ada8
+ * @license Angular v11.1.0-next.0+38.sha-66d863f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6752,7 +6752,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+36.sha-fe1ada8');
+const VERSION = new Version('11.1.0-next.0+38.sha-66d863f');
 
 /**
  * @license
