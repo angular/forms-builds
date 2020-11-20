b4d85be60b8afeed905ccd0765067f54272f036f
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:05:07 +0000

    c33a8235d9 fix(router): correctly handle string command in outlets (#39728)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+39.sha-1cba56e
+ * @license Angular v11.1.0-next.0+43.sha-c33a823
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6483,7 +6483,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+39.sha-1cba56e');
+const VERSION = new Version('11.1.0-next.0+43.sha-c33a823');
 
 /**
  * @license
