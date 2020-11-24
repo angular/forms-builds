321b479ccd890ce1c293e6605d085f3edaed3f43
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:04:04 +0000

    2c275f6d7a docs: archive template expression operators doc, move pipes precedence section (#39170)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+64.sha-6046419
+ * @license Angular v11.1.0-next.0+67.sha-2c275f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7012,7 +7012,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.0+64.sha-6046419');
+    var VERSION = new core.Version('11.1.0-next.0+67.sha-2c275f6');
 
     /**
      * @license
