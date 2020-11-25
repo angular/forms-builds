aac96cb5203e5d0f4137b7e1178fbea5717a6607
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:32:49 +0000

    2786292780 test(compiler-cli): migrate i18n compliance tests (#39661)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+94.sha-6653028
+ * @license Angular v11.1.0-next.0+96.sha-2786292
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6752,7 +6752,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+94.sha-6653028');
+const VERSION = new Version('11.1.0-next.0+96.sha-2786292');
 
 /**
  * @license
