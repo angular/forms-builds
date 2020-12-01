6fa02bc228981e252f2fbaed8d6f8358892acff1
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 19:31:19 +0000

    425e3297f8 build(docs-infra): upgrade cli command docs sources to 4f756ce2f (#39907)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+72.sha-75e22ab
+ * @license Angular v11.0.2+74.sha-425e329
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6462,7 +6462,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+72.sha-75e22ab');
+const VERSION = new Version('11.0.2+74.sha-425e329');
 
 /**
  * @license
