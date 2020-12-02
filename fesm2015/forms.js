2f87f1cb1e41175daad916aa9b067887caf3a0cd
Misko Hevery <misko@hevery.com>
2020-12-02 23:30:03 +0000

    efd0d33391 test(compiler-cli): convert bindings compliance tests (#39862)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+21.sha-790ca09
+ * @license Angular v11.1.0-next.1+24.sha-efd0d33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6731,7 +6731,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+21.sha-790ca09');
+const VERSION = new Version('11.1.0-next.1+24.sha-efd0d33');
 
 /**
  * @license
