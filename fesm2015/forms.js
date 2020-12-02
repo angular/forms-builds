2a2e2d7ac42437218425d3a5109a46e569891fe1
Joey Perrott <josephperrott@gmail.com>
2020-12-02 19:03:37 +0000

    784667ca12 release: cut the v11.1.0-next.1 release (#39932)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+126.sha-eac85b4
+ * @license Angular v11.1.0-next.0+127.sha-784667c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6731,7 +6731,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+126.sha-eac85b4');
+const VERSION = new Version('11.1.0-next.0+127.sha-784667c');
 
 /**
  * @license
