e1841909f2e1cde24cc44c0ac1d483814032ce98
Misko Hevery <misko@hevery.com>
2020-12-02 21:21:38 +0000

    eba185edc8 test(docs-infra): reduce ambiguity in `deploy-to-firebase.js` test descriptions (#39853)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+18.sha-75fc893
+ * @license Angular v11.1.0-next.1+12.sha-eba185e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6731,7 +6731,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+18.sha-75fc893');
+const VERSION = new Version('11.1.0-next.1+12.sha-eba185e');
 
 /**
  * @license
