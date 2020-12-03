f7401140812d91c9893310a131e1ad0abcff25c7
Misko Hevery <misko@hevery.com>
2020-12-03 00:59:00 +0000

    3680ad147b fix(core): remove application from the testability registry when the root view is removed (#39876)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+5.sha-57642e8
+ * @license Angular v11.0.3+17.sha-3680ad1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6462,7 +6462,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+5.sha-57642e8');
+const VERSION = new Version('11.0.3+17.sha-3680ad1');
 
 /**
  * @license
