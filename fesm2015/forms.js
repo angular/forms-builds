70dd1f71baa6dfdd256e25bba13951e3e9eaf63b
Misko Hevery <misko@hevery.com>
2020-12-03 15:42:56 +0000

    2b84882ab0 fix(language-service): do not return external template that does not exist (#39898)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+24.sha-efd0d33
+ * @license Angular v11.1.0-next.1+26.sha-2b84882
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6462,7 +6462,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+24.sha-efd0d33');
+const VERSION = new Version('11.1.0-next.1+26.sha-2b84882');
 
 /**
  * @license
