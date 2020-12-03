d762c3a2b6aec622dd38be64c3e36d9fe6b3e37d
Misko Hevery <misko@hevery.com>
2020-12-03 22:05:43 +0000

    aa8bd733fa ci: update l2 triage label configuration for pull requests (#39954)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+28.sha-76ae874
+ * @license Angular v11.1.0-next.1+33.sha-aa8bd73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6462,7 +6462,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+28.sha-76ae874');
+const VERSION = new Version('11.1.0-next.1+33.sha-aa8bd73');
 
 /**
  * @license
