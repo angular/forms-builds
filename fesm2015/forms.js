eb9f08d3f6c8bc31fe3d3d3a845422037424ec46
Misko Hevery <misko@hevery.com>
2020-12-07 18:16:18 +0000

    6d19c16d32 build: remove check-env script (#39980)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+51.sha-d9356f2
+ * @license Angular v11.1.0-next.1+52.sha-6d19c16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+51.sha-d9356f2');
+const VERSION = new Version('11.1.0-next.1+52.sha-6d19c16');
 
 /**
  * @license
