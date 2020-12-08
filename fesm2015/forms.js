f45beabc325f92f91e01184709f46f2ddb71b67d
Misko Hevery <misko@hevery.com>
2020-12-08 19:33:40 +0000

    19309b5797 Revert "build: update bazel rules_nodejs to version 2.3.1 (#39636)"

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+54.sha-8ab1231
+ * @license Angular v11.0.3+56.sha-19309b5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+54.sha-8ab1231');
+const VERSION = new Version('11.0.3+56.sha-19309b5');
 
 /**
  * @license
