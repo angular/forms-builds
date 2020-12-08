0e97636e2a0bdbd389cec37b1d2fca69b69d8c3a
Misko Hevery <misko@hevery.com>
2020-12-08 20:37:01 +0000

    65e93ea7b3 ci: add atscott to language-service approvers (#40027)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+56.sha-19309b5
+ * @license Angular v11.0.3+63.sha-65e93ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+56.sha-19309b5');
+const VERSION = new Version('11.0.3+63.sha-65e93ea');
 
 /**
  * @license
