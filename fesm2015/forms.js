eb2b9cf217d8da1fcf47f87900f5e94dc73172a2
Misko Hevery <misko@hevery.com>
2020-12-08 17:49:59 +0000

    fbfc7dff48 build: update to use npm_package.pack rule (#39636)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+72.sha-86fdc77
+ * @license Angular v11.1.0-next.1+75.sha-fbfc7df
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+72.sha-86fdc77');
+const VERSION = new Version('11.1.0-next.1+75.sha-fbfc7df');
 
 /**
  * @license
