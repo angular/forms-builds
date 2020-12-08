be4fb0310e1b01c9bf44b8efefdee28aa242ddeb
Misko Hevery <misko@hevery.com>
2020-12-08 00:50:21 +0000

    5631daa05c feat(compiler): support tagged template literals in code generator (#39122)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+41.sha-6bb6dfd
+ * @license Angular v11.0.3+42.sha-5631daa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+41.sha-6bb6dfd');
+const VERSION = new Version('11.0.3+42.sha-5631daa');
 
 /**
  * @license
