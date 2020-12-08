8818e995959493940bc15827f59ec2939310906e
Misko Hevery <misko@hevery.com>
2020-12-08 20:30:19 +0000

    6dc43a475b fix(upgrade): avoid memory leak when removing downgraded components (#39965)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+83.sha-e692884
+ * @license Angular v11.1.0-next.1+85.sha-6dc43a4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+83.sha-e692884');
+const VERSION = new Version('11.1.0-next.1+85.sha-6dc43a4');
 
 /**
  * @license
