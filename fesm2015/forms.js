cfb54cffb26ab55e64f960fc3ae82894d6842d5f
Misko Hevery <misko@hevery.com>
2020-12-08 17:15:57 +0000

    f408af30bb docs: removes the outdated content (#39242)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+64.sha-6e7a57f
+ * @license Angular v11.1.0-next.1+66.sha-f408af3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+64.sha-6e7a57f');
+const VERSION = new Version('11.1.0-next.1+66.sha-f408af3');
 
 /**
  * @license
