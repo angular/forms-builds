fb5bb48a6627fff97fad1a38db874d38ab488401
Misko Hevery <misko@hevery.com>
2020-12-09 17:33:55 +0000

    b2090fdf93 docs(common): use table layout for Pre-defined format options (#40036)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+66.sha-5a765f0
+ * @license Angular v11.0.3+73.sha-b2090fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+66.sha-5a765f0');
+const VERSION = new Version('11.0.3+73.sha-b2090fd');
 
 /**
  * @license
