e86a685b703e359bfad320fb607c47007e838619
Misko Hevery <misko@hevery.com>
2020-12-09 00:49:50 +0000

    5a765f0be9 fix(animations): implement getPosition in browser animation builder (#39983)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+63.sha-65e93ea
+ * @license Angular v11.0.3+66.sha-5a765f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+63.sha-65e93ea');
+const VERSION = new Version('11.0.3+66.sha-5a765f0');
 
 /**
  * @license
