ebf43e2df67f1e6c01637c71edcdad72ae421e1f
Misko Hevery <misko@hevery.com>
2020-12-09 17:21:44 +0000

    bb3f207f44 docs: add reason to use Angular in README (#36927)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+95.sha-cfb094d
+ * @license Angular v11.1.0-next.1+96.sha-bb3f207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+95.sha-cfb094d');
+const VERSION = new Version('11.1.0-next.1+96.sha-bb3f207');
 
 /**
  * @license
