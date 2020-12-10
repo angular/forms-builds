fa378a00b87e88d25674ab7a2856338a09e9bd2f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:47:24 +0000

    112324a614 feat(router): add `relativeTo` as an input to `routerLink` (#39720)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+6.sha-dc6d40e
+ * @license Angular v11.1.0-next.2+7.sha-112324a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+6.sha-dc6d40e');
+const VERSION = new Version('11.1.0-next.2+7.sha-112324a');
 
 /**
  * @license
