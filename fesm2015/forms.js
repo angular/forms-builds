b2b3b5b48450c6368dccce665f26fcb3c7364364
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:03:54 +0000

    76e3de253c docs: remove -webkit-box-sizing and -moz-box-sizing (#40039)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+9.sha-1f73af7
+ * @license Angular v11.1.0-next.2+10.sha-76e3de2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+9.sha-1f73af7');
+const VERSION = new Version('11.1.0-next.2+10.sha-76e3de2');
 
 /**
  * @license
