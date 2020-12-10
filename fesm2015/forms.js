dfc7ecaa053a4456d66a66c836fcefe8b28006d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:32:20 +0000

    269a775287 refactor(compiler-cli): produce binding access when checkTypeOfOutputEvents is false (#39515)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+2.sha-29298f4
+ * @license Angular v11.1.0-next.2+4.sha-269a775
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+2.sha-29298f4');
+const VERSION = new Version('11.1.0-next.2+4.sha-269a775');
 
 /**
  * @license
