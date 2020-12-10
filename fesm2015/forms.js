be604b38abcb23701a681759837dc9df639aae7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:42:16 +0000

    fea95059d5 build(docs-infra): upgrade cli command docs sources to f6f0cde41 (#40064)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+21.sha-79a42d9
+ * @license Angular v11.1.0-next.2+22.sha-fea9505
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+21.sha-79a42d9');
+const VERSION = new Version('11.1.0-next.2+22.sha-fea9505');
 
 /**
  * @license
