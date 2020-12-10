0b34a73345be4f2724634fafeb303f5449cce8ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:10:10 +0000

    0817758b6a feat(docs-infra): add no upcoming events message (#40055)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+16.sha-973f797
+ * @license Angular v11.1.0-next.2+17.sha-0817758
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+16.sha-973f797');
+const VERSION = new Version('11.1.0-next.2+17.sha-0817758');
 
 /**
  * @license
