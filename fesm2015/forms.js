7e7857b71cc48fb0e2f2e5b786b7606ee1dc9851
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:04:23 +0000

    c977e1b629 test(compiler): use `yarn bazel` instead of `bazel` in error message (#40078)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+32.sha-044cd52
+ * @license Angular v11.1.0-next.2+33.sha-c977e1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+32.sha-044cd52');
+const VERSION = new Version('11.1.0-next.2+33.sha-c977e1b');
 
 /**
  * @license
