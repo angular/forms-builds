b2aa41e62d1659525dd3b1660c3f3e46834e7105
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:03:55 +0000

    044cd52996 docs: update CLI MAN page format (#40038)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+35.sha-4f4f318
+ * @license Angular v11.1.0-next.2+32.sha-044cd52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6733,7 +6733,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+35.sha-4f4f318');
+const VERSION = new Version('11.1.0-next.2+32.sha-044cd52');
 
 /**
  * @license
