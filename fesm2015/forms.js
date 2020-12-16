5b15bbbe7a8d33cc2c7df5a7123c1714ebf38ce0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 18:05:03 +0000

    0aa220bc00 fix(core): fix possible XSS attack in development through SSR. (#40136)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+24.sha-a1c82e7
+ * @license Angular v11.0.4+26.sha-0aa220b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.4+24.sha-a1c82e7');
+const VERSION = new Version('11.0.4+26.sha-0aa220b');
 
 /**
  * @license
