2df18f1f69f7417dd44467170edaa26662a293b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 22:56:42 +0000

    9ae216ced5 build: upgrade typescript (dev dep) in zone.js to 3.8.3 (#40159)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+10.sha-5b38050
+ * @license Angular v9.1.12+13.sha-9ae216c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -9124,7 +9124,7 @@ FormBuilder.decorators = [
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.1.12+10.sha-5b38050');
+const VERSION = new Version('9.1.12+13.sha-9ae216c');
 
 /**
  * @fileoverview added by tsickle
