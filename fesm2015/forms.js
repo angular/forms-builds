bac28d474a9a677330671e484c05796d3d0a31a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 21:42:23 +0000

    922f492a3b fix(core): set `ngDevMode` to `false` when calling `enableProdMode()` (#40124)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+26.sha-0aa220b
+ * @license Angular v11.0.4+27.sha-922f492
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.4+26.sha-0aa220b');
+const VERSION = new Version('11.0.4+27.sha-922f492');
 
 /**
  * @license
