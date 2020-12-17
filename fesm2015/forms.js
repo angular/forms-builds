c229d2006bca5a04c925a71aa56c693a90c0d68d
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:27:05 +0000

    362f45c4bf docs: use an explicit product id in the getting started example (#34934)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+5.sha-475468c
+ * @license Angular v11.1.0-next.3+6.sha-362f45c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+5.sha-475468c');
+const VERSION = new Version('11.1.0-next.3+6.sha-362f45c');
 
 /**
  * @license
