7d8413fa05e009b798faa742d85885ddb4434e2c
Joey Perrott <josephperrott@gmail.com>
2020-12-17 00:31:18 +0000

    4a11226be7 release: cut the v10.2.4 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.3+3.sha-90570c0
+ * @license Angular v10.2.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6387,7 +6387,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.3+3.sha-90570c0');
+const VERSION = new Version('10.2.4');
 
 /**
  * @license
