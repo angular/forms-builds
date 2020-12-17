9fb7eb4a0f5d661014169ce13f9c9f79e8b0fc9c
Joey Perrott <josephperrott@gmail.com>
2020-12-17 00:49:59 +0000

    31448197e4 release: cut the v9.1.13 release

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+13.sha-9ae216c
+ * @license Angular v9.1.13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6822,7 +6822,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.12+13.sha-9ae216c');
+var VERSION = new Version('9.1.13');
 
 /**
  * @license
