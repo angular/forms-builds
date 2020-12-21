540e16aba34e37c0d048be70ee86faad30d8bd0e
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:36:04 +0000

    5831d2b685 ci: correctly rebase PRs for branches contain a slash (/) (#40184)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+6.sha-362f45c
+ * @license Angular v11.1.0-next.3+8.sha-5831d2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+6.sha-362f45c');
+const VERSION = new Version('11.1.0-next.3+8.sha-5831d2b');
 
 /**
  * @license
