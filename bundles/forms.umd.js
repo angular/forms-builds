c4f58b28aed01e630bf95931bfc9ff2f57dc4215
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:29:21 +0000

    c22d00547a docs(router): fix typo (#40244)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+31.sha-9186f1f
+ * @license Angular v11.1.0-next.3+32.sha-c22d005
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6993,7 +6993,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.3+31.sha-9186f1f');
+    var VERSION = new core.Version('11.1.0-next.3+32.sha-c22d005');
 
     /**
      * @license
