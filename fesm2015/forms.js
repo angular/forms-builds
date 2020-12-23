4e99f4927fcd17153123e1eaa1dac8f50657694f
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:26:26 +0000

    c83163ecfe docs(router): fix typo (#40244)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+24.sha-9a149d4
+ * @license Angular v11.0.5+26.sha-c83163e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+24.sha-9a149d4');
+const VERSION = new Version('11.0.5+26.sha-c83163e');
 
 /**
  * @license
