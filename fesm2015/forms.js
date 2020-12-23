513b4c05006c7489ca96fac2b3f3092d6823dfcf
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:13:43 +0000

    e54261b8d8 docs: update boolean and enumerated CLI section (#40224)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+29.sha-53b64fb
+ * @license Angular v11.1.0-next.3+30.sha-e54261b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+29.sha-53b64fb');
+const VERSION = new Version('11.1.0-next.3+30.sha-e54261b');
 
 /**
  * @license
