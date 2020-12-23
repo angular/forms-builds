bfb3e9c68b6ed59686573bbe2f522b33fc70207a
Joey Perrott <josephperrott@gmail.com>
2020-12-23 17:01:14 +0000

    9a149d4c49 docs: use kebab-case for CLI commands (#40240)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+23.sha-ef13e83
+ * @license Angular v11.0.5+24.sha-9a149d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+23.sha-ef13e83');
+const VERSION = new Version('11.0.5+24.sha-9a149d4');
 
 /**
  * @license
