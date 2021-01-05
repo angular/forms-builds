2cb95220a26ef5f74abb74efb9479f4f49aa99c3
Joey Perrott <josephperrott@gmail.com>
2021-01-05 21:12:22 +0000

    e43f7e26fe fix(router): apply redirects should match named outlets with empty path parents (#40029)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+44.sha-b473bc2
+ * @license Angular v11.1.0-next.3+50.sha-e43f7e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6833,7 +6833,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.3+44.sha-b473bc2');
+const VERSION = new Version('11.1.0-next.3+50.sha-e43f7e2');
 
 /**
  * @license
