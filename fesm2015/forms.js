bcd3ffc79071c6ff7d4293755708f50625840d8a
Joey Perrott <josephperrott@gmail.com>
2021-01-05 19:22:13 +0000

    45838c033e fix(core): take @Host into account while processing `useFactory` arguments (#40122) (#40313)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+31.sha-ec5b09b
+ * @license Angular v11.0.5+32.sha-45838c0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+31.sha-ec5b09b');
+const VERSION = new Version('11.0.5+32.sha-45838c0');
 
 /**
  * @license
