5fa4a28cca9612fbe9b439372433c59395ed2a58
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:25:15 +0000

    ec5b09b6f3 build(docs-infra): upgrade cli command docs sources to 5dd818279 (#40251)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+26.sha-c83163e
+ * @license Angular v11.0.5+31.sha-ec5b09b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6464,7 +6464,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+26.sha-c83163e');
+const VERSION = new Version('11.0.5+31.sha-ec5b09b');
 
 /**
  * @license
