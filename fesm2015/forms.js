d617cf20b9362dfc004dc6de6789b6058805a94f
Andrew Scott <atscott@google.com>
2021-01-08 18:01:23 +0000

    e27b920ac3 refactor(compiler-cli): split up `NodeJSFileSystem` class (#40281)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+22.sha-1045465
+ * @license Angular v11.1.0-next.4+35.sha-e27b920
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6833,7 +6833,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+22.sha-1045465');
+const VERSION = new Version('11.1.0-next.4+35.sha-e27b920');
 
 /**
  * @license
