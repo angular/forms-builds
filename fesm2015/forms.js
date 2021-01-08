e307b733835c1fd89a6d36a2be4b0ffaecfbdaf7
atscott <atscott01@gmail.com>
2021-01-08 20:13:37 +0000

    b36bece17c docs(router): Clearly document how to retrieve all params in the router tree (#40306)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+43.sha-e05ac39
+ * @license Angular v11.1.0-next.4+44.sha-b36bece
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6842,7 +6842,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+43.sha-e05ac39');
+const VERSION = new Version('11.1.0-next.4+44.sha-b36bece');
 
 /**
  * @license
