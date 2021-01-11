31095b25ffbfe1507d83e51cc4c3bf844783cb60
atscott <atscott01@gmail.com>
2021-01-11 19:35:42 +0000

    adf42daed6 fix(router): better ngZone checking for warning (#25839)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+6.sha-6438a29
+ * @license Angular v11.0.8+19.sha-adf42da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7000,7 +7000,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.8+6.sha-6438a29');
+    var VERSION = new core.Version('11.0.8+19.sha-adf42da');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+6.sha-6438a29
+ * @license Angular v11.0.8+19.sha-adf42da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6471,7 +6471,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+6.sha-6438a29');
+const VERSION = new Version('11.0.8+19.sha-adf42da');
 
 /**
  * @license
