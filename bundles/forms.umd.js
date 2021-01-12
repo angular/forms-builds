d94741423da8af0f3d5e982939f377500cae73bf
atscott <atscott01@gmail.com>
2021-01-12 00:04:51 +0000

    642c45bbf1 fix(compiler): incorrectly inferring content type of SVG-specific title tag (#40259)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+20.sha-f8495ca
+ * @license Angular v11.0.8+30.sha-642c45b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7000,7 +7000,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.0.8+20.sha-f8495ca');
+    var VERSION = new core.Version('11.0.8+30.sha-642c45b');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.8+20.sha-f8495ca
+ * @license Angular v11.0.8+30.sha-642c45b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6471,7 +6471,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.8+20.sha-f8495ca');
+const VERSION = new Version('11.0.8+30.sha-642c45b');
 
 /**
  * @license
