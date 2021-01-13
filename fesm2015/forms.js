d2a587feda13402237c48160b9da3b3d76ca7508
atscott <atscott01@gmail.com>
2021-01-13 23:14:37 +0000

    5aa264f17c docs: update identity-revealed.directive.ts to use `AbstractControl` instead of `FormGroup` (#40132)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+7.sha-3f6279d
+ * @license Angular v11.0.9+9.sha-5aa264f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6471,7 +6471,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.9+7.sha-3f6279d');
+const VERSION = new Version('11.0.9+9.sha-5aa264f');
 
 /**
  * @license
