2c39f923046b747b4a784064d7b616e262db8fa0
Andrew Scott <atscott@google.com>
2021-01-14 17:45:42 +0000

    f3a96a50b6 build(docs-infra): upgrade cli command docs sources to f67800842 (#40426)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+9.sha-5aa264f
+ * @license Angular v11.0.9+11.sha-f3a96a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6471,7 +6471,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.9+9.sha-5aa264f');
+const VERSION = new Version('11.0.9+11.sha-f3a96a5');
 
 /**
  * @license
