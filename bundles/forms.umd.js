0735bb886b03ac7d9df7861d4ba0dca43a500765
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:39:00 +0000

    b37a9eba1a fix(router): lazy loaded modules without RouterModule.forChild() won't cause an infinite loop (#36605)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+108.sha-5457936
+ * @license Angular v11.1.0-next.4+111.sha-b37a9eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7100,7 +7100,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+108.sha-5457936');
+    var VERSION = new core.Version('11.1.0-next.4+111.sha-b37a9eb');
 
     /**
      * @license
