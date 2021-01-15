8a4ec9142afe4ab80c6ea2cff07dddf72e3a88bb
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:39:50 +0000

    96690ed3a4 feat(docs-infra): add shortcuts for the angular.io PWA (#40393)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+111.sha-b37a9eb
+ * @license Angular v11.1.0-next.4+110.sha-96690ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7100,7 +7100,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+111.sha-b37a9eb');
+    var VERSION = new core.Version('11.1.0-next.4+110.sha-96690ed');
 
     /**
      * @license
