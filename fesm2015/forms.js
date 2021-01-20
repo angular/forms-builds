6b5dde3ad5483ebd3fea00ad309cf10e0784ac2b
Andrew Kushnir <akushnir@google.com>
2021-01-20 17:33:03 +0000

    34092a5052 docs: add embedded videos to error guides (#40453)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+37.sha-2731a4b
+ * @license Angular v11.1.0-rc.0+39.sha-34092a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6577,7 +6577,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-rc.0+37.sha-2731a4b');
+const VERSION = new Version('11.1.0-rc.0+39.sha-34092a5');
 
 /**
  * @license
