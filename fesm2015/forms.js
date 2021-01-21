033f79e5b490759eff8fe7c8eb6b1321f2797da5
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:32:19 +0000

    566206b519 fix(compiler-cli): handle pseudo cycles in inline source-maps (#40435)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+14.sha-69fd942
+ * @license Angular v11.1.0+19.sha-566206b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6577,7 +6577,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+14.sha-69fd942');
+const VERSION = new Version('11.1.0+19.sha-566206b');
 
 /**
  * @license
