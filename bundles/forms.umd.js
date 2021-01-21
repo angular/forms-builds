50ed3e81c9643f9fac2c06cdb2f04bd24f5b615a
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:55:52 +0000

    3cf4e3c7c5 fix(router): always stringify matrix parameters (#25095)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+137.sha-315b272
+ * @license Angular v11.1.0-next.4+139.sha-3cf4e3c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7497,7 +7497,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.1.0-next.4+137.sha-315b272');
+    var VERSION = new i0.Version('11.1.0-next.4+139.sha-3cf4e3c');
 
     /**
      * @license
