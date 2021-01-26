28d44cd3fd3c070cfbeb713e65c19c78bdad2f0b
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:57:05 +0000

    97ec6e4849 fix(core): fix possible XSS attack in development through SSR (#40525)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+43.sha-a87e900
+ * @license Angular v11.1.0+45.sha-97ec6e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+43.sha-a87e900');
+const VERSION = new Version('11.1.0+45.sha-97ec6e4');
 
 /**
  * @license
