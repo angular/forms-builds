404559e41a027968c3c3b64329d75c02f1e29df7
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 20:05:08 +0000

    39b3cbbdf5 build: update .bazelversion (#40579)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+181.sha-ffad040
+ * @license Angular v11.1.0-next.4+182.sha-39b3cbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+181.sha-ffad040');
+const VERSION = new Version('11.1.0-next.4+182.sha-39b3cbb');
 
 /**
  * @license
