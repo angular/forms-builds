63f92f7d02458b8432e21e7470a4e89528972614
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:19:19 +0000

    be979c907b perf(compiler-cli): introduce fast path for resource-only updates (#40561)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+189.sha-e43cba5
+ * @license Angular v11.1.0-next.4+194.sha-be979c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6863,7 +6863,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+189.sha-e43cba5');
+const VERSION = new Version('11.1.0-next.4+194.sha-be979c9');
 
 /**
  * @license
