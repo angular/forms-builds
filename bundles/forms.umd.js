c4eb51c5635937305cde529a41775c87481fa659
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:16:45 +0000

    be979c907b perf(compiler-cli): introduce fast path for resource-only updates (#40561)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+189.sha-e43cba5
+ * @license Angular v11.1.0-next.4+194.sha-be979c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7121,7 +7121,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.1.0-next.4+189.sha-e43cba5');
+    var VERSION = new core.Version('11.1.0-next.4+194.sha-be979c9');
 
     /**
      * @license
