9e79d75577a43d302b80751427c8c4218d181170
Misko Hevery <misko@hevery.com>
2021-02-02 20:25:09 +0000

    21994ee985 refactor(docs-infra): remove unused styles for `.heading` CSS class (#40427)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+18.sha-caafac2
+ * @license Angular v11.2.0-next.0+30.sha-21994ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7121,7 +7121,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.0-next.0+18.sha-caafac2');
+    var VERSION = new core.Version('11.2.0-next.0+30.sha-21994ee');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+18.sha-caafac2
+ * @license Angular v11.2.0-next.0+30.sha-21994ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0-next.0+18.sha-caafac2');
+const VERSION = new Version('11.2.0-next.0+30.sha-21994ee');
 
 /**
  * @license
