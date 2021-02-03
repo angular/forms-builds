e0ad4061a1027300d5286cc0d58920ed4ca17ada
Misko Hevery <misko@hevery.com>
2021-02-03 18:26:41 +0000

    950875c1ba refactor(language-service): pull out interfaces on package toplevel (#40621)

--- forms.d.ts
+++ forms.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+231.sha-a3b0864
+ * @license Angular v11.1.0-next.4+237.sha-950875c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -758,6 +758,8 @@ export declare class AbstractFormGroupDirective extends ControlContainer impleme
      */
     get formDirective(): Form | null;
 }
+
+declare type AnyControlStatus = 'untouched' | 'touched' | 'pristine' | 'dirty' | 'valid' | 'invalid' | 'pending';
 
 /**
  * @description
@@ -4113,13 +4115,7 @@ export declare const ɵangular_packages_forms_forms_f: any;
 export declare class ɵangular_packages_forms_forms_g {
     private _cd;
     constructor(cd: AbstractControlDirective | null);
-    get ngClassUntouched(): boolean;
-    get ngClassTouched(): boolean;
-    get ngClassPristine(): boolean;
-    get ngClassDirty(): boolean;
-    get ngClassValid(): boolean;
-    get ngClassInvalid(): boolean;
-    get ngClassPending(): boolean;
+    is(status: AnyControlStatus): boolean;
 }
 
 export declare const ɵangular_packages_forms_forms_h: {
