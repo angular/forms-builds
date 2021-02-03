e97819fcc419797071dce340ff07b2b53b122331
Misko Hevery <misko@hevery.com>
2021-02-03 00:47:42 +0000

    a3b0864428 refactor(compiler-cli): remove the overrideComponentTemplate API (#40585)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+229.sha-11ca2f0
+ * @license Angular v11.1.0-next.4+231.sha-a3b0864
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6592,7 +6592,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+229.sha-11ca2f0');
+const VERSION = new Version('11.1.0-next.4+231.sha-a3b0864');
 
 /**
  * @license
