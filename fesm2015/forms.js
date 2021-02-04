424b2368804356cc0257981582620470200b3337
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:31:10 +0000

    c9fa59ac1f fix(docs-infra): switch code font from `Droid Sans Mono` to `Roboto Mono` (#40667)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+245.sha-d067dc0
+ * @license Angular v11.1.0-next.4+244.sha-c9fa59a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6839,7 +6839,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+245.sha-d067dc0');
+const VERSION = new Version('11.1.0-next.4+244.sha-c9fa59a');
 
 /**
  * @license
