fe4a2a2c48395e2bff56057aea665311f0c18db6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:22:36 +0000

    5324a62ebe docs: add guide for branching process (#39924)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+240.sha-836b176
+ * @license Angular v11.1.0-next.4+241.sha-5324a62
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6839,7 +6839,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+240.sha-836b176');
+const VERSION = new Version('11.1.0-next.4+241.sha-5324a62');
 
 /**
  * @license
