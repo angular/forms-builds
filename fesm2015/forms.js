2182757b8a5caa68b6072d8d643d82d513aa9e53
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:21:39 +0000

    9cb43fb507 refactor(compiler-cli): implement `ɵɵngDeclarePipe()` (#40803)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+26.sha-6425a6d
+ * @license Angular v12.0.0-next.0+27.sha-9cb43fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7012,7 +7012,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+26.sha-6425a6d');
+const VERSION = new Version('12.0.0-next.0+27.sha-9cb43fb');
 
 /**
  * @license
