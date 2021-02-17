c6284b95334f1c5644d69c4d146f2a15d2e09d04
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:19:10 +0000

    13d82ce248 docs(core): add documentation for HostListener (#40720)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+72.sha-a4c00c2
+ * @license Angular v12.0.0-next.0+74.sha-13d82ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7054,7 +7054,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+72.sha-a4c00c2');
+const VERSION = new Version('12.0.0-next.0+74.sha-13d82ce');
 
 /**
  * @license
