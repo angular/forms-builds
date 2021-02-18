6d21e4831042df1800e66fa09c63b6b735b8598f
Zach Arend <zachzach@google.com>
2021-02-18 01:23:52 +0000

    f57cd19e0c refactor(localize): avoid computing source-maps in extraction unnecessarily (#40891)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+4.sha-1de999b
+ * @license Angular v12.0.0-next.1+6.sha-f57cd19
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7700,7 +7700,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+4.sha-1de999b');
+    var VERSION = new i0.Version('12.0.0-next.1+6.sha-f57cd19');
 
     /**
      * @license
