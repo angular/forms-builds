46fad8ae87269f9d5c36ac62bf2d4acb5dd22954
Andrew Scott <atscott@google.com>
2021-02-19 20:29:23 +0000

    3b884ed1fc fix(http): ignore question mark when params are parsed (#40610)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+30.sha-dcbf600
+ * @license Angular v11.2.1+32.sha-3b884ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6568,7 +6568,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+30.sha-dcbf600');
+const VERSION = new Version('11.2.1+32.sha-3b884ed');
 
 /**
  * @license
