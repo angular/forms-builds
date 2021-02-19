1106a3e1e64c91a640f3df1d5c2fb22a53ef92b3
Andrew Scott <atscott@google.com>
2021-02-19 16:35:20 +0000

    30d5e25a28 docs: edit attribute-directives.md, move best practice to styleguide (#39849)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+10.sha-542ba1f
+ * @license Angular v12.0.0-next.1+11.sha-30d5e25
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7700,7 +7700,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.1+10.sha-542ba1f');
+    var VERSION = new i0.Version('12.0.0-next.1+11.sha-30d5e25');
 
     /**
      * @license
