edb907f7cef70a18e115f022b8d2e2a71b576551
GitHub <noreply@github.com>
2021-02-22 22:06:35 +0000

    263bbd43c1 release: cut the v11.2.2 release (#40951)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+39.sha-73a3ff1
+ * @license Angular v11.2.1+40.sha-263bbd4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6568,7 +6568,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+39.sha-73a3ff1');
+const VERSION = new Version('11.2.1+40.sha-263bbd4');
 
 /**
  * @license
