2a0b643ac4d49b3363c586af9e326139a7a77a53
Zach Arend <zachzach@google.com>
2021-02-24 23:31:40 +0000

    29d8a0ab09 feat(animations): add support for disabling animations through BrowserAnimationsModule.withConfig (#40731)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+5.sha-3c24136
+ * @license Angular v12.0.0-next.2+6.sha-29d8a0a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7055,7 +7055,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+5.sha-3c24136');
+const VERSION = new Version('12.0.0-next.2+6.sha-29d8a0a');
 
 /**
  * @license
