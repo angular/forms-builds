013d544c7a8fc2a5f2b633b583704abdf5874dfe
Andrew Scott <atscott@google.com>
2021-02-24 16:18:04 +0000

    03de2f2b78 docs: fix ch 4 so it compiles in strict mode (#40970)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+59.sha-011a527
+ * @license Angular v12.0.0-next.1+60.sha-03de2f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7055,7 +7055,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+59.sha-011a527');
+const VERSION = new Version('12.0.0-next.1+60.sha-03de2f2');
 
 /**
  * @license
