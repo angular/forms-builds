44425ddc9f2846087e390e9fd10c9985417a33c8
atscott <atscott01@gmail.com>
2021-02-24 17:47:53 +0000

    d1535a1a77 test(language-service): remove circular dependency (#40966)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+64.sha-f93eae7
+ * @license Angular v12.0.0-next.1+74.sha-d1535a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7055,7 +7055,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+64.sha-f93eae7');
+const VERSION = new Version('12.0.0-next.1+74.sha-d1535a1');
 
 /**
  * @license
