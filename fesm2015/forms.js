7a79e7cf0a034baf6e0fff980a16cdffb35a78c6
Andrew Scott <atscott@google.com>
2021-02-24 16:21:36 +0000

    aaf9b31fb4 feat(core): drop support for zone.js 0.10.x (#40823)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+60.sha-03de2f2
+ * @license Angular v12.0.0-next.1+63.sha-aaf9b31
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7055,7 +7055,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.1+60.sha-03de2f2');
+const VERSION = new Version('12.0.0-next.1+63.sha-aaf9b31');
 
 /**
  * @license
