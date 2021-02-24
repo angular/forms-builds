7542f002db9cc4b9feb09fa0a35ca5663d279d8c
GitHub <noreply@github.com>
2021-02-24 17:53:09 +0000

    d7133a8e4e release: cut the v11.2.3 release (#40981)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+25.sha-3fe759e
+ * @license Angular v11.2.2+26.sha-d7133a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7069,7 +7069,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.2+25.sha-3fe759e');
+    var VERSION = new core.Version('11.2.2+26.sha-d7133a8');
 
     /**
      * @license
