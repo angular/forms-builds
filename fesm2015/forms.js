0d1db77e90a0583bbffe358a71c57e44d0c182ec
Zach Arend <zachzach@google.com>
2021-02-26 23:33:48 +0000

    fb82558d78 docs: Change lexical in security guide (#41016)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+14.sha-91cdc11
+ * @license Angular v12.0.0-next.2+15.sha-fb82558
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7066,7 +7066,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+14.sha-91cdc11');
+const VERSION = new Version('12.0.0-next.2+15.sha-fb82558');
 
 /**
  * @license
