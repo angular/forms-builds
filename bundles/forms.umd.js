1c6b42b88a79232197d742dabea7d9f2fdcd1f28
Zach Arend <zachzach@google.com>
2021-02-26 23:57:13 +0000

    272b5645c4 fix(docs-infra): improve the layout of the "Resources" page (#40944)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+15.sha-fb82558
+ * @license Angular v12.0.0-next.2+29.sha-272b564
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7712,7 +7712,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.2+15.sha-fb82558');
+    var VERSION = new i0.Version('12.0.0-next.2+29.sha-272b564');
 
     /**
      * @license
