f5696e0c1afa7bd862e8b062355551e209f183c5
Zach Arend <zachzach@google.com>
2021-03-01 23:53:16 +0000

    6c783c7fcb docs: rewrite structural-directives.md (#40015)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+40.sha-356d9a0
+ * @license Angular v12.0.0-next.2+41.sha-6c783c7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7066,7 +7066,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.2+40.sha-356d9a0');
+const VERSION = new Version('12.0.0-next.2+41.sha-6c783c7');
 
 /**
  * @license
