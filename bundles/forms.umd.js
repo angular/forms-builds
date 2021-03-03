b233b9a8c8455681118d73f46dd2324871a682cf
Zach Arend <zachzach@google.com>
2021-03-03 23:12:50 +0000

    2028a77dc8 release: cut the v11.2.4 release

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+49.sha-9322e6a
+ * @license Angular v11.2.4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7080,7 +7080,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.3+49.sha-9322e6a');
+    var VERSION = new core.Version('11.2.4');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.3+49.sha-9322e6a
+ * @license Angular v11.2.4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6579,7 +6579,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.3+49.sha-9322e6a');
+const VERSION = new Version('11.2.4');
 
 /**
  * @license
