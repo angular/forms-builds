5cf5789726018e1278f1bbf22a7f451f1c78c6fb
Andrew Kushnir <akushnir@google.com>
2021-03-04 23:10:59 +0000

    a05eb13caa fix(language-service): Only provide dom completions for inline templates (#41078)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+11.sha-1d4fc94
+ * @license Angular v11.2.4+12.sha-a05eb13
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7080,7 +7080,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.4+11.sha-1d4fc94');
+    var VERSION = new core.Version('11.2.4+12.sha-a05eb13');
 
     /**
      * @license
