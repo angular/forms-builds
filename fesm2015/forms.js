8e0b8e8aaf6cca8eaeb0069cf6dfaeb25c3694a6
Andrew Kushnir <akushnir@google.com>
2021-03-04 20:22:58 +0000

    ca721c2972 feat(core): more precise type for `APP_INITIALIZER` token (#40986)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+11.sha-bf158e7
+ * @license Angular v12.0.0-next.3+12.sha-ca721c2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7066,7 +7066,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+11.sha-bf158e7');
+const VERSION = new Version('12.0.0-next.3+12.sha-ca721c2');
 
 /**
  * @license
