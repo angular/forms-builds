2d813d05b5822e4ff9fd8caa418cd085e790fcbc
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:30:19 +0000

    bf158e7ff0 fix(core): remove duplicated EMPTY_OBJ constant (#41066)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+10.sha-d44c7c2
+ * @license Angular v12.0.0-next.3+11.sha-bf158e7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7066,7 +7066,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+10.sha-d44c7c2');
+const VERSION = new Version('12.0.0-next.3+11.sha-bf158e7');
 
 /**
  * @license
