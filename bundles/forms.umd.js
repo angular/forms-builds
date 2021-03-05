183311bed45a198606bb7391e0ec778d92db931c
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:05:17 +0000

    2de7eef92e docs: fix issue #40941 (#40942)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+12.sha-a05eb13
+ * @license Angular v11.2.4+14.sha-2de7eef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7080,7 +7080,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.4+12.sha-a05eb13');
+    var VERSION = new core.Version('11.2.4+14.sha-2de7eef');
 
     /**
      * @license
