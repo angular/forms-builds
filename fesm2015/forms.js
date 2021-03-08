e7632382e7686f488ab602e8ced68fc2730bb190
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:20:06 +0000

    c1a93fcf32 docs: remove augury resource (#41107)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+29.sha-fed6a7c
+ * @license Angular v12.0.0-next.3+31.sha-c1a93fc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7066,7 +7066,7 @@ FormBuilder.ɵprov = ɵɵdefineInjectable({ token: FormBuilder, factory: FormBui
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+29.sha-fed6a7c');
+const VERSION = new Version('12.0.0-next.3+31.sha-c1a93fc');
 
 /**
  * @license
