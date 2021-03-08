8f2de3644beaa75292f4a309a14e6b8d24f5bcac
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:27:39 +0000

    925746b6d8 fix(bazel): fix incorrect rollup plugin method signature (#41101)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+24.sha-e35ecea
+ * @license Angular v11.2.4+26.sha-925746b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7080,7 +7080,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new core.Version('11.2.4+24.sha-e35ecea');
+    var VERSION = new core.Version('11.2.4+26.sha-925746b');
 
     /**
      * @license
