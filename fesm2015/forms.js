d8aeba0f279c7452a5caa8f248fe6070cb1b3264
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:01:33 +0000

    b50d88073e perf(compiler-cli): ensure module resolution cache is reused for type-check program (#39693)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+59.sha-a730315
+ * @license Angular v12.0.0-next.3+61.sha-b50d880
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+59.sha-a730315');
+const VERSION = new Version('12.0.0-next.3+61.sha-b50d880');
 
 /**
  * @license
