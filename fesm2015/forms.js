b54d9142d081e90f6983938945b73438d8d16a2f
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:24:06 +0000

    a730315fb2 docs: use double quotes in Tour of Heroes (pt1) (#41134)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+60.sha-e29a29c
+ * @license Angular v12.0.0-next.3+59.sha-a730315
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+60.sha-e29a29c');
+const VERSION = new Version('12.0.0-next.3+59.sha-a730315');
 
 /**
  * @license
