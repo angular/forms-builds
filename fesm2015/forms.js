30e659566579ee1bbd7bc90a80fd015f6d35dc5a
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:15:41 +0000

    7a27cd262f refactor(docs-infra): simplify code snippet indentation in the "Cheat sheet" guide (#41051)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+43.sha-fa3689f
+ * @license Angular v12.0.0-next.3+57.sha-7a27cd2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+43.sha-fa3689f');
+const VERSION = new Version('12.0.0-next.3+57.sha-7a27cd2');
 
 /**
  * @license
