f775e4b452667c1d1d65b87bb93d5be583321f13
Andrew Kushnir <akushnir@google.com>
2021-03-10 20:16:45 +0000

    3c66b100dd perf(common): remove unused methods from DomAdapter (#41102)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+3.sha-4c79b8a
+ * @license Angular v12.0.0-next.4+4.sha-3c66b10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+3.sha-4c79b8a');
+const VERSION = new Version('12.0.0-next.4+4.sha-3c66b10');
 
 /**
  * @license
