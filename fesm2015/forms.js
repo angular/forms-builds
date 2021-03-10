bffffb7ee26e38fda897ce45723a9103dd2d3d10
Andrew Kushnir <akushnir@google.com>
2021-03-10 18:22:07 +0000

    ab15e61656 docs: release notes for the v11.2.5 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+62.sha-41b8f98
+ * @license Angular v12.0.0-next.3+63.sha-ab15e61
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+62.sha-41b8f98');
+const VERSION = new Version('12.0.0-next.3+63.sha-ab15e61');
 
 /**
  * @license
