42a0ff2159822f207daf79e905061d586d444004
GitHub <noreply@github.com>
2021-03-10 18:24:43 +0000

    1fb0dbb300 release: cut the v12.0.0-next.4 release (#41156)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+63.sha-ab15e61
+ * @license Angular v12.0.0-next.3+64.sha-1fb0dbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7067,7 +7067,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.3+63.sha-ab15e61');
+const VERSION = new Version('12.0.0-next.3+64.sha-1fb0dbb');
 
 /**
  * @license
