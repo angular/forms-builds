63853984dc0e2bc4c3264bdd6963a13576b3c0ba
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 19:35:55 +0000

    cc75e1d17b docs: add contributors guide to aio (#41061)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+25.sha-3bd1992
+ * @license Angular v12.0.0-next.4+26.sha-cc75e1d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7158,7 +7158,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+25.sha-3bd1992');
+const VERSION = new Version('12.0.0-next.4+26.sha-cc75e1d');
 
 /**
  * @license
