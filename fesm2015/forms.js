44904ac8d58ff5afce71d74179d980ffdaa2298a
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 18:29:33 +0000

    66e9970691 feat(ngcc): support `__read` helper as used by TypeScript 4.2 (#41201)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+40.sha-6bdad5e
+ * @license Angular v12.0.0-next.4+42.sha-66e9970
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+40.sha-6bdad5e');
+const VERSION = new Version('12.0.0-next.4+42.sha-66e9970');
 
 /**
  * @license
