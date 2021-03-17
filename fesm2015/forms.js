f908b18c368e34d510ee76d2ea92a172e407377c
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:29:47 +0000

    98de4cb1bf docs: add deprecation note next to IE 11 in browser-support page (#41147)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+44.sha-0f0f34f
+ * @license Angular v12.0.0-next.4+47.sha-98de4cb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.4+44.sha-0f0f34f');
+const VERSION = new Version('12.0.0-next.4+47.sha-98de4cb');
 
 /**
  * @license
