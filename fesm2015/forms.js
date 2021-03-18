a796e0b183bbae16fbb3aadac27879bb128b42f2
Misko Hevery <misko@hevery.com>
2021-03-18 17:43:48 +0000

    655507012b build(docs-infra): upgrade cli command docs sources to b6effecae (#41259)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+5.sha-e239941
+ * @license Angular v12.0.0-next.5+8.sha-6555070
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+5.sha-e239941');
+const VERSION = new Version('12.0.0-next.5+8.sha-6555070');
 
 /**
  * @license
