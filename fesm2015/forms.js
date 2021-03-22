5e5d34e8721fc6188189f78fc90bd6cbc7ec6981
Misko Hevery <misko@hevery.com>
2021-03-22 16:15:43 +0000

    5bc0a472bd refactor(docs-infra): remove image size exclusion list (#41292)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+15.sha-5eb7f34
+ * @license Angular v12.0.0-next.5+18.sha-5bc0a47
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+15.sha-5eb7f34');
+const VERSION = new Version('12.0.0-next.5+18.sha-5bc0a47');
 
 /**
  * @license
