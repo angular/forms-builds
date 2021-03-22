1ef7870feb875ef1b9df24bb68b485a1a78888bc
Misko Hevery <misko@hevery.com>
2021-03-22 16:16:46 +0000

    ed5bfe8173 docs: simplify deleteHero method in tutorial (#41303)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+18.sha-5bc0a47
+ * @license Angular v12.0.0-next.5+16.sha-ed5bfe8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7906,7 +7906,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.5+18.sha-5bc0a47');
+    var VERSION = new i0.Version('12.0.0-next.5+16.sha-ed5bfe8');
 
     /**
      * @license
