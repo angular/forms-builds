6b5c6577a9a14bf7bb78e49350595dc2c0553608
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:38:34 +0000

    6a6d332c06 docs: add Alexander Inkin to GDE list (#41310)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+30.sha-1eba57e
+ * @license Angular v12.0.0-next.5+32.sha-6a6d332
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+30.sha-1eba57e');
+const VERSION = new Version('12.0.0-next.5+32.sha-6a6d332');
 
 /**
  * @license
