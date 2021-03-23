411a6e0b46f1a0586d279fe40616d4c56cd2e650
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:10:39 +0000

    31dc483206 docs: add alisa duncan to GDE resources (#41295)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+25.sha-c49b280
+ * @license Angular v12.0.0-next.5+26.sha-31dc483
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+25.sha-c49b280');
+const VERSION = new Version('12.0.0-next.5+26.sha-31dc483');
 
 /**
  * @license
