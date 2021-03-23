0e389d7b4078ab3758363f9f1651b18c60c23e16
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:12:21 +0000

    18cd7a0c69 fix(language-service): show suggestion when type inference is suboptimal (#41072)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+17.sha-17c5dc7
+ * @license Angular v11.2.6+22.sha-18cd7a0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6749,7 +6749,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.6+17.sha-17c5dc7');
+const VERSION = new Version('11.2.6+22.sha-18cd7a0');
 
 /**
  * @license
