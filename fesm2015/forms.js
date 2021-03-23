1718be46914435f47517c0a09ee6faba5ff0954d
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:43:37 +0000

    65f7d5380d docs: add link to fork repo how-to (#41266)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+31.sha-190fa07
+ * @license Angular v12.0.0-next.5+33.sha-65f7d53
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+31.sha-190fa07');
+const VERSION = new Version('12.0.0-next.5+33.sha-65f7d53');
 
 /**
  * @license
