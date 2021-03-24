d06c54b03a661f5b6f09e759e02f4b7e2df65175
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-24 20:57:01 +0000

    dd82c8e9f4 docs(docs-infra): add Zach Arend to contributors.json (#41325)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+41.sha-9da7ea9
+ * @license Angular v12.0.0-next.6+1.sha-dd82c8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7250,7 +7250,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+41.sha-9da7ea9');
+const VERSION = new Version('12.0.0-next.6+1.sha-dd82c8e');
 
 /**
  * @license
