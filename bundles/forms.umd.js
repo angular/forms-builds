c7a9bd4ef241f97c1f9fe8b178ed5229965449a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-29 22:33:54 +0000

    81a88c009c fix(dev-infra): use all non-header content for checking commit body length (#41367)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+6.sha-95ff5ec
+ * @license Angular v12.0.0-next.6+7.sha-81a88c0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7469,7 +7469,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.6+6.sha-95ff5ec');
+    var VERSION = new i0.Version('12.0.0-next.6+7.sha-81a88c0');
 
     /**
      * @license
