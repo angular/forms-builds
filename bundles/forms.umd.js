21974375c307ce2c2e99f368d715f4c72b74a7ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 01:48:26 +0000

    a371646a37 build: update yargs dependency to 16.2.0. (#41351)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+23.sha-e112e32
+ * @license Angular v12.0.0-next.6+25.sha-a371646
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7469,7 +7469,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.6+23.sha-e112e32');
+    var VERSION = new i0.Version('12.0.0-next.6+25.sha-a371646');
 
     /**
      * @license
