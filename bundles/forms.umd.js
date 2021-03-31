f51f35776588ef943e89de534e3c990ef635cc31
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:18:03 +0000

    31370f4103 feat(bazel): allow setting `compilationMode` in `ng_module` rule (#41366)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+20.sha-9f31cdd
+ * @license Angular v12.0.0-next.6+21.sha-31370f4
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7469,7 +7469,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.6+20.sha-9f31cdd');
+    var VERSION = new i0.Version('12.0.0-next.6+21.sha-31370f4');
 
     /**
      * @license
