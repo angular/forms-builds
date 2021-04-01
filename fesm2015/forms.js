bc09e7b84b9ee70ef189cb6929ad1ec5ef4c9a0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:15:20 +0000

    c6d9a193d6 fix(dev-infra): Correctly place comments in dockerfile (#41374)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+42.sha-ff9470b
+ * @license Angular v12.0.0-next.6+43.sha-c6d9a19
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7246,7 +7246,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+42.sha-ff9470b');
+const VERSION = new Version('12.0.0-next.6+43.sha-c6d9a19');
 
 /**
  * @license
