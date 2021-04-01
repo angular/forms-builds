ad4f325a36582600ee3d3e814111848f2682820d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:44:49 +0000

    18bc9ffb51 ci: do not try to publish directories that are not packages (#41393)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+34.sha-6391a4a
+ * @license Angular v12.0.0-next.6+36.sha-18bc9ff
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7469,7 +7469,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.6+34.sha-6391a4a');
+    var VERSION = new i0.Version('12.0.0-next.6+36.sha-18bc9ff');
 
     /**
      * @license
