7a613d51c0b1d466b2f5e006d9872e39c3f88fbb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:53:14 +0000

    db55f8e671 ci: update commit-message verification range parameters (#41341)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+36.sha-18bc9ff
+ * @license Angular v12.0.0-next.6+38.sha-db55f8e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7246,7 +7246,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+36.sha-18bc9ff');
+const VERSION = new Version('12.0.0-next.6+38.sha-db55f8e');
 
 /**
  * @license
