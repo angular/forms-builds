4e17bd2aa05030a5f98f919961442a79904af47f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:57:53 +0000

    3470ea17d5 fix(docs-infra): increase the max width of the `file-not-found` page (#41275)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.6+38.sha-db55f8e
+ * @license Angular v12.0.0-next.6+40.sha-3470ea1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7246,7 +7246,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.6+38.sha-db55f8e');
+const VERSION = new Version('12.0.0-next.6+40.sha-3470ea1');
 
 /**
  * @license
