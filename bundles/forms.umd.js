23d34fa60408b5de80644a3fcf457a3e65dded39
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 22:05:39 +0000

    154129b9de release: cut the v11.2.8 release (#41419)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+32.sha-4bac297
+ * @license Angular v11.2.7+33.sha-154129b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.7+32.sha-4bac297');
+    var VERSION = new i0.Version('11.2.7+33.sha-154129b');
 
     /**
      * @license
