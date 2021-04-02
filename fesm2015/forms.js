dc6b57e76fb7566fce2189874c1c715828137df7
atscott <atscott01@gmail.com>
2021-04-02 23:08:53 +0000

    64b979e12b fix(dev-infra): exclude node_modules from circular dependency checks (#41407)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+5.sha-03a46af
+ * @license Angular v12.0.0-next.7+7.sha-64b979e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+5.sha-03a46af');
+const VERSION = new Version('12.0.0-next.7+7.sha-64b979e');
 
 /**
  * @license
