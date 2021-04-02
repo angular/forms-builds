3287c93ac119a24fd1ffbdb8a397149fba1ef6f4
atscott <atscott01@gmail.com>
2021-04-02 18:11:47 +0000

    03a46afc91 docs: Including Visual Studio (#41414)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+3.sha-a43f36b
+ * @license Angular v12.0.0-next.7+5.sha-03a46af
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6947,7 +6947,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+3.sha-a43f36b');
+const VERSION = new Version('12.0.0-next.7+5.sha-03a46af');
 
 /**
  * @license
