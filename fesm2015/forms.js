9e4a23a2d7d184b5844c241cda7ad4b33e4393c6
atscott <atscott01@gmail.com>
2021-04-03 20:13:09 +0000

    1de04b124e feat(compiler-cli): support transforming component style resources (#41307)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+8.sha-dc65526
+ * @license Angular v12.0.0-next.7+9.sha-1de04b1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+8.sha-dc65526');
+const VERSION = new Version('12.0.0-next.7+9.sha-1de04b1');
 
 /**
  * @license
