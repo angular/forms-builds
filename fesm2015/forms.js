f62a457187e3240e4b568833b80b992e1380046f
atscott <atscott01@gmail.com>
2021-04-05 16:16:27 +0000

    754247fefd feat(dev-infra): prevent `git push` from being called in dryRun mode (#41387)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+6.sha-103f05a
+ * @license Angular v11.2.8+10.sha-754247f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6747,7 +6747,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+6.sha-103f05a');
+const VERSION = new Version('11.2.8+10.sha-754247f');
 
 /**
  * @license
