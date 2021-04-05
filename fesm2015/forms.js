4863d068e8c9fb00922ea702cebd85998f9eea11
atscott <atscott01@gmail.com>
2021-04-05 16:17:56 +0000

    180c1b555f fix(docs-infra): avoid auto-linking generic word `state` (#41438)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+10.sha-fcd190a
+ * @license Angular v12.0.0-next.7+12.sha-180c1b5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+10.sha-fcd190a');
+const VERSION = new Version('12.0.0-next.7+12.sha-180c1b5');
 
 /**
  * @license
