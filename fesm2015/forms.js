14629a5d4bf1edde915bb6a501b75e2f4d798b92
atscott <atscott01@gmail.com>
2021-04-06 08:38:51 +0000

    3a823abcc5 refactor(compiler): remove dependency on fs-extra (#41445)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+16.sha-2a76d84
+ * @license Angular v12.0.0-next.7+18.sha-3a823ab
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+16.sha-2a76d84');
+const VERSION = new Version('12.0.0-next.7+18.sha-3a823ab');
 
 /**
  * @license
