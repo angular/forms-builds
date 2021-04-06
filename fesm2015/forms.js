a6a7310256789988ae9ed017529f79c20265e5f1
atscott <atscott01@gmail.com>
2021-04-06 17:28:12 +0000

    575f537473 docs(router): make getCurrentNavigation() jsdoc more explicit (#41417)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.7+21.sha-acebe92
+ * @license Angular v12.0.0-next.7+25.sha-575f537
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.7+21.sha-acebe92');
+const VERSION = new Version('12.0.0-next.7+25.sha-575f537');
 
 /**
  * @license
