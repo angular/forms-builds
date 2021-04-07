ddef3473a65d78285496262832d2ccbe6f9e765a
atscott <atscott01@gmail.com>
2021-04-07 19:14:30 +0000

    d641542587 refactor(core): remove deprecated rxjs signature usage in event emitter and make more minifier-friendly (#41450)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+6.sha-7a49aa5
+ * @license Angular v12.0.0-next.8+7.sha-d641542
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6947,7 +6947,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+6.sha-7a49aa5');
+const VERSION = new Version('12.0.0-next.8+7.sha-d641542');
 
 /**
  * @license
