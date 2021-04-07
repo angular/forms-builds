3ba656ecbcc1329d2320b9811dc475b5c78f221d
atscott <atscott01@gmail.com>
2021-04-07 19:13:59 +0000

    7a49aa5136 build: add `dev-infra` and `docs-infra` as targetLabelExemptScopes (#41459)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+2.sha-18d4446
+ * @license Angular v12.0.0-next.8+6.sha-7a49aa5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+2.sha-18d4446');
+const VERSION = new Version('12.0.0-next.8+6.sha-7a49aa5');
 
 /**
  * @license
