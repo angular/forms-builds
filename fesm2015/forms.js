d9752323687e57abb851428f02a1c21cc64aa105
atscott <atscott01@gmail.com>
2021-04-07 19:13:17 +0000

    18d4446dc6 docs: add Zack DeRose to GDE resources (#41464)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+1.sha-5a81490
+ * @license Angular v12.0.0-next.8+2.sha-18d4446
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+1.sha-5a81490');
+const VERSION = new Version('12.0.0-next.8+2.sha-18d4446');
 
 /**
  * @license
