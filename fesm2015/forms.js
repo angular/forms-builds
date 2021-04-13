e173e1d3ccfb232cf0c4c84986fa0a293181b901
Zach Arend <zachzach@google.com>
2021-04-13 23:31:30 +0000

    f7e391a912 perf(core): optimize getDirectives (#41525)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+72.sha-1d12c50
+ * @license Angular v12.0.0-next.8+74.sha-f7e391a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+72.sha-1d12c50');
+const VERSION = new Version('12.0.0-next.8+74.sha-f7e391a');
 
 /**
  * @license
