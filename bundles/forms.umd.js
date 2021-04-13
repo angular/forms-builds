0784cf89b2fec1cc115e2b6400a6a0cdfbf85b12
Zach Arend <zachzach@google.com>
2021-04-13 04:35:53 +0000

    9cda866703 docs: change (+) operator to `Number` function to match code (#41570) PR Close #41570

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+23.sha-99c9f7c
+ * @license Angular v11.2.9+30.sha-9cda866
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.9+23.sha-99c9f7c');
+    var VERSION = new i0.Version('11.2.9+30.sha-9cda866');
 
     /**
      * @license
