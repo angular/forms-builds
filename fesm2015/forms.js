24bf2ef88c89bf02240a8db0f956e69d930d0d01
Zach Arend <zachzach@google.com>
2021-04-13 04:34:33 +0000

    fe6002977e docs: change (+) operator to `Number` function to match code (#41570) PR Close #41570

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+54.sha-3a61bb8
+ * @license Angular v12.0.0-next.8+56.sha-fe60029
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+54.sha-3a61bb8');
+const VERSION = new Version('12.0.0-next.8+56.sha-fe60029');
 
 /**
  * @license
