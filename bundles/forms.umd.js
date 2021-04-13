870047d6dc73b51bf6d447f59969643d950e4866
Zach Arend <zachzach@google.com>
2021-04-13 00:02:17 +0000

    99c9f7c709 ci: remove old compliance tests (#41556) (#41585)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+21.sha-419c189
+ * @license Angular v11.2.9+23.sha-99c9f7c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.9+21.sha-419c189');
+    var VERSION = new i0.Version('11.2.9+23.sha-99c9f7c');
 
     /**
      * @license
