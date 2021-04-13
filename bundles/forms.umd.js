31305bdd7b1f22e82a6f38abf61a293915f0eabf
Zach Arend <zachzach@google.com>
2021-04-13 15:08:46 +0000

    cfd67c1c81 refactor(compiler-cli): track a dependency on a default import on `WrappedNodeExpr` (#41586)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.9+30.sha-9cda866
+ * @license Angular v11.2.9+35.sha-cfd67c1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.9+30.sha-9cda866');
+    var VERSION = new i0.Version('11.2.9+35.sha-cfd67c1');
 
     /**
      * @license
