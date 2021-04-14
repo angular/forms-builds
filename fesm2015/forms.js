e6316b3db68ad632ab94745d74870ed2ccbca458
Zach Arend <zachzach@google.com>
2021-04-14 01:44:13 +0000

    bd34bc9e89 fix(language-service): bound attributes should not break directive matching (#41597)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+75.sha-fd9a7ca
+ * @license Angular v12.0.0-next.8+76.sha-bd34bc9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+75.sha-fd9a7ca');
+const VERSION = new Version('12.0.0-next.8+76.sha-bd34bc9');
 
 /**
  * @license
