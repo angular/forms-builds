67730e6582b5495603997bdf04de3574cb082236
Zach Arend <zachzach@google.com>
2021-04-14 23:14:57 +0000

    a1b2718b92 fix(router): recursively merge empty path matches (#41584)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+102.sha-1043aa4
+ * @license Angular v12.0.0-next.8+105.sha-a1b2718
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7771,7 +7771,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+102.sha-1043aa4');
+    var VERSION = new i0.Version('12.0.0-next.8+105.sha-a1b2718');
 
     /**
      * @license
