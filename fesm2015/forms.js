f6df7c5465d53140a88232d328b4f1e971a5b59c
Zach Arend <zachzach@google.com>
2021-04-14 21:59:03 +0000

    74eed46477 release: cut the v10.2.5 release

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.4+2.sha-f630f33
+ * @license Angular v10.2.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6387,7 +6387,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.4+2.sha-f630f33');
+const VERSION = new Version('10.2.5');
 
 /**
  * @license
