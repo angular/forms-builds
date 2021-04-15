b53a36b1b41529276d68a8fe978857e843ea53a2
Zach Arend <zachzach@google.com>
2021-04-15 02:12:58 +0000

    9438d48b89 refactor: update `$schema` and `id` keywords (#41574)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+9.sha-e655846
+ * @license Angular v12.0.0-next.9+11.sha-9438d48
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.9+9.sha-e655846');
+    var VERSION = new i0.Version('12.0.0-next.9+11.sha-9438d48');
 
     /**
      * @license
