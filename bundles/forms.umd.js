dcfa67f2c04ce36414cebe8133160cbf39f5e90a
Zach Arend <zachzach@google.com>
2021-04-15 02:08:47 +0000

    c4b0753908 test(router): add Router regression test for hash location strategy (#40409)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+4.sha-b616f73
+ * @license Angular v11.2.10+5.sha-c4b0753
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.10+4.sha-b616f73');
+    var VERSION = new i0.Version('11.2.10+5.sha-c4b0753');
 
     /**
      * @license
