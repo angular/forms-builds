fb26bccc41f5eea3e26c2aafd8ed88118260a274
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:05:52 +0000

    a99aa29040 fix(animations): allow animations on elements in the shadow DOM (#40134)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+116.sha-1aebf16
+ * @license Angular v12.0.0-next.8+115.sha-a99aa29
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+116.sha-1aebf16');
+const VERSION = new Version('12.0.0-next.8+115.sha-a99aa29');
 
 /**
  * @license
