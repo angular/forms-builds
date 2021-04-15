5e1b600059201476f5b4b66d0eeca5ebac1101ec
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:11:33 +0000

    f4376fc0d4 fix(router): Only retrieve stored route when reuse strategy indicates it should reattach (#30263)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+5.sha-c4b0753
+ * @license Angular v11.2.10+6.sha-f4376fc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.10+5.sha-c4b0753');
+    var VERSION = new i0.Version('11.2.10+6.sha-f4376fc');
 
     /**
      * @license
