9c7f83584851bd9ff6913c31b95bc1d9fdfb30f1
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:46:54 +0000

    de25cdac54 docs: move lazy loading section to the best practices section (#41378)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+136.sha-96175fd
+ * @license Angular v12.0.0-next.8+138.sha-de25cda
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+136.sha-96175fd');
+const VERSION = new Version('12.0.0-next.8+138.sha-de25cda');
 
 /**
  * @license
