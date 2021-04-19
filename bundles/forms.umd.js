fa7b3b48689a41de2cc19978358d85f52f1db629
Andrew Kushnir <akushnir@google.com>
2021-04-19 17:03:25 +0000

    8c831a257c test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+16.sha-095bb52
+ * @license Angular v11.2.10+29.sha-8c831a2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.10+16.sha-095bb52');
+    var VERSION = new i0.Version('11.2.10+29.sha-8c831a2');
 
     /**
      * @license
