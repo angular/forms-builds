2d21e6f3465c4c8ea13c8806513927777a469d78
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:06:14 +0000

    d58747de8a Revert "fix(zone.js): should continue to executue listeners when throw error (#41562)" (#41707)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+165.sha-e256d92
+ * @license Angular v12.0.0-next.8+167.sha-d58747d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+165.sha-e256d92');
+const VERSION = new Version('12.0.0-next.8+167.sha-d58747d');
 
 /**
  * @license
