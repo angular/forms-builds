240d8d6527721ff2742a6c5b2abdc29989ac0d0d
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:01:03 +0000

    e238f2c33a docs: rewrites animation query () method use (#41710)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+179.sha-03f3dcb
+ * @license Angular v12.0.0-next.8+182.sha-e238f2c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+179.sha-03f3dcb');
+const VERSION = new Version('12.0.0-next.8+182.sha-e238f2c');
 
 /**
  * @license
