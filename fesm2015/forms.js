0513bbc3f94843ea2e1de02576afec746b00a0b4
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:07:57 +0000

    73824d5337 fix(compiler): not generating update instructions for ng-template inside alternate namespaces (#41669)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+185.sha-364ff96
+ * @license Angular v12.0.0-next.8+186.sha-73824d5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+185.sha-364ff96');
+const VERSION = new Version('12.0.0-next.8+186.sha-73824d5');
 
 /**
  * @license
