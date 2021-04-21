040c3c045db460c56e7883494f1f22920645ed7e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-21 23:14:48 +0000

    0830885c95 test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+89.sha-897d5ad
+ * @license Angular v12.0.0-rc.0+2.sha-0830885
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.9+89.sha-897d5ad');
+const VERSION = new Version('12.0.0-rc.0+2.sha-0830885');
 
 /**
  * @license
