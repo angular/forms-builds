10c340607ecb0aeef15e502d95f37ed208b37014
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:08:57 +0000

    5895ddbcd1 refactor(core): add single type for injector token (#41580)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+10.sha-6221227
+ * @license Angular v12.0.0-rc.0+16.sha-5895ddb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-rc.0+10.sha-6221227');
+    var VERSION = new i0.Version('12.0.0-rc.0+16.sha-5895ddb');
 
     /**
      * @license
