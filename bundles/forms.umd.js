fb1712f798bb28fabccda2dfbb1d8fb9b2680b08
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 17:34:38 +0000

    42c64aa376 Revert "build(docs-infra): fix `StackblitzBuilder` after `jsdom` update (#41725)" (#41768)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+4.sha-d061be9
+ * @license Angular v11.2.11+8.sha-42c64aa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.11+4.sha-d061be9');
+    var VERSION = new i0.Version('11.2.11+8.sha-42c64aa');
 
     /**
      * @license
