3ddc2556ebcc3ed329ddf6a33001637d60fd9584
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:51:41 +0000

    d061be99a9 fix(dev-infra): verify python3 is available before release (#41753)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+2.sha-1d1fa1a
+ * @license Angular v11.2.11+4.sha-d061be9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.11+2.sha-1d1fa1a');
+    var VERSION = new i0.Version('11.2.11+4.sha-d061be9');
 
     /**
      * @license
