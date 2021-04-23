f4c4171d5de62b3a2604bb695b6a9f9b14c77ea9
GitHub <noreply@github.com>
2021-04-23 18:51:07 +0000

    45ffab59f1 ci: remove trailing comma from renovate config

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+139.sha-aa35a1a
+ * @license Angular v12.0.0-next.8+213.sha-45ffab5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+139.sha-aa35a1a');
+const VERSION = new Version('12.0.0-next.8+213.sha-45ffab5');
 
 /**
  * @license
