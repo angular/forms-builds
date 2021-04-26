03166a99b889efaa2a4e3aa97953d5665881985b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 17:04:10 +0000

    21f1466d84 build(docs-infra): fix e2e testing SystemJS-based docs examples (#41796)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+10.sha-163c076
+ * @license Angular v11.2.11+15.sha-21f1466
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.11+10.sha-163c076');
+    var VERSION = new i0.Version('11.2.11+15.sha-21f1466');
 
     /**
      * @license
