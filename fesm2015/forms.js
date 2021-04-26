64be6c9d7fb89e4419fa9f5757fc0b048ad8be33
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:31:32 +0000

    326884736e docs: make inputs-outputs example more accessible (#41288)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+215.sha-70fb3e9
+ * @license Angular v12.0.0-next.8+216.sha-3268847
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+215.sha-70fb3e9');
+const VERSION = new Version('12.0.0-next.8+216.sha-3268847');
 
 /**
  * @license
