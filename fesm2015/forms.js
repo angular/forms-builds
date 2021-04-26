2e187d3a01507f8366896d51672afd52a3bb3ef8
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:36:53 +0000

    01e546f116 docs: improve accessibility of architecture example (#41314)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+216.sha-3268847
+ * @license Angular v12.0.0-next.8+217.sha-01e546f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+216.sha-3268847');
+const VERSION = new Version('12.0.0-next.8+217.sha-01e546f');
 
 /**
  * @license
