ac502dba32354e354a7695c770d31a4a1d9ba522
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 23:20:04 +0000

    7756ead0bf docs: add metadata documentation to httpClient (#41706)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+18.sha-aa578f5
+ * @license Angular v11.2.11+20.sha-7756ead
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.11+18.sha-aa578f5');
+    var VERSION = new i0.Version('11.2.11+20.sha-7756ead');
 
     /**
      * @license
