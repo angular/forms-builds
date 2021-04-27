10550578650953176e9232c36cd564c63cb1606e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:37:35 +0000

    3783976543 docs: remove the section "Using components vs services from other modules" (#41835)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+253.sha-f978976
+ * @license Angular v12.0.0-next.8+255.sha-3783976
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+253.sha-f978976');
+const VERSION = new Version('12.0.0-next.8+255.sha-3783976');
 
 /**
  * @license
