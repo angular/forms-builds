4fdaeed4249850b4c99cef2aef817beae326ba77
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:14:24 +0000

    eedbb758d2 build(docs-infra): update dgeni-packages to fix version list (#41841)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+24.sha-e758658
+ * @license Angular v11.2.11+25.sha-eedbb75
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.11+24.sha-e758658');
+    var VERSION = new i0.Version('11.2.11+25.sha-eedbb75');
 
     /**
      * @license
