9fec0e817a1ac86d784081c9458ba84020571e2f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:37:30 +0000

    f978976970 build: update @types/jasmine to version 3.6.10 (#41830)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+252.sha-da6ed15
+ * @license Angular v12.0.0-next.8+253.sha-f978976
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+252.sha-da6ed15');
+const VERSION = new Version('12.0.0-next.8+253.sha-f978976');
 
 /**
  * @license
