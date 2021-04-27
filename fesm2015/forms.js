2527444650dd0419f7d4b9249719154af5d67e6b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 21:00:21 +0000

    30db14b5db ci: several updates to renovate configuration (#41834)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+68.sha-947b451
+ * @license Angular v12.0.0-rc.0+69.sha-30db14b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.0+68.sha-947b451');
+const VERSION = new Version('12.0.0-rc.0+69.sha-30db14b');
 
 /**
  * @license
