5d48935294c15257219e7ede5541e0cb00c858aa
GitHub <noreply@github.com>
2021-04-28 19:24:51 +0000

    083ceecc51 ci: enable Renovate for remark packages

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+213.sha-45ffab5
+ * @license Angular v12.0.0-next.8+272.sha-083ceec
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+213.sha-45ffab5');
+    var VERSION = new i0.Version('12.0.0-next.8+272.sha-083ceec');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+213.sha-45ffab5
+ * @license Angular v12.0.0-next.8+272.sha-083ceec
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+213.sha-45ffab5');
+const VERSION = new Version('12.0.0-next.8+272.sha-083ceec');
 
 /**
  * @license
