4a033adde2f6ae8afcd358a325141eeac90b7f1c
GitHub <noreply@github.com>
2021-04-28 19:18:07 +0000

    98de217640 release: cut the v12.0.0-rc.1 release (#41864)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+80.sha-a66421e
+ * @license Angular v12.0.0-rc.0+81.sha-98de217
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-rc.0+80.sha-a66421e');
+    var VERSION = new i0.Version('12.0.0-rc.0+81.sha-98de217');
 
     /**
      * @license
