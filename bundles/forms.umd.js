c6fc4c61a1fb4f281041c3fdc84a4c58a0b29ba1
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 19:56:30 +0000

    f555a557a3 docs: remove mention of scheduleTick in markForCheck (#41845)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12
+ * @license Angular v11.2.12+1.sha-f555a55
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.12');
+    var VERSION = new i0.Version('11.2.12+1.sha-f555a55');
 
     /**
      * @license
