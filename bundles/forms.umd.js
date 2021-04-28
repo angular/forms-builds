2b71c59e925937aac7a6008cdb1bb22dfb2c4574
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:18:50 +0000

    a66421e643 ci: re-enable the `@angular/components` unit tests (#41816)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.0+79.sha-77a5157
+ * @license Angular v12.0.0-rc.0+80.sha-a66421e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-rc.0+79.sha-77a5157');
+    var VERSION = new i0.Version('12.0.0-rc.0+80.sha-a66421e');
 
     /**
      * @license
