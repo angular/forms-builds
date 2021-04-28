350786ab8a2baecfa3e1f99617a39d08abc7b016
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:53:36 +0000

    c31d6ea24e docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+279.sha-3ccad85
+ * @license Angular v12.0.0-next.8+281.sha-c31d6ea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+279.sha-3ccad85');
+const VERSION = new Version('12.0.0-next.8+281.sha-c31d6ea');
 
 /**
  * @license
