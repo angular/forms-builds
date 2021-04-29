966bf9f2e67fbbe581a6626cbc2ee85568f16997
Misko Hevery <misko@hevery.com>
2021-04-29 17:46:02 +0000

    05209fa397 ci: exclude packages which don't have a major from being batched (#41865)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+288.sha-f3331a8
+ * @license Angular v12.0.0-next.8+286.sha-05209fa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+288.sha-f3331a8');
+const VERSION = new Version('12.0.0-next.8+286.sha-05209fa');
 
 /**
  * @license
