25257527e51262352fb914d590d5a048acd0b55a
Alan Agius <alan.agius4@gmail.com>
2021-04-29 09:06:23 +0000

    c01002dd7a test: update `runtime-es2015` sizes follow CLI updates

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+278.sha-c0be765
+ * @license Angular v12.0.0-next.8+284.sha-c01002d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-next.8+278.sha-c0be765');
+    var VERSION = new i0.Version('12.0.0-next.8+284.sha-c01002d');
 
     /**
      * @license
