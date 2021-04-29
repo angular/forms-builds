9132c0e442e4f09748a6ea79a13f2fc28862e24d
Misko Hevery <misko@hevery.com>
2021-04-29 17:51:32 +0000

    77b77ff525 ci: only run `publish_snapshot` when not PR (#41877)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+10.sha-ede0cfe
+ * @license Angular v12.0.0-rc.1+16.sha-77b77ff
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-rc.1+10.sha-ede0cfe');
+    var VERSION = new i0.Version('12.0.0-rc.1+16.sha-77b77ff');
 
     /**
      * @license
