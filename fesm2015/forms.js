ee9abb3a420629ece800a6fdaca2e89fef86ce5d
Misko Hevery <misko@hevery.com>
2021-04-30 21:55:59 +0000

    c0694bf538 fix(dev-infra): remove getBaseDir method from GitClient (#41898)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+301.sha-9b4b281
+ * @license Angular v12.0.0-next.8+306.sha-c0694bf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+301.sha-9b4b281');
+const VERSION = new Version('12.0.0-next.8+306.sha-c0694bf');
 
 /**
  * @license
