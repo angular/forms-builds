d75f0a40bff5e3b3250382dbde5c961a289a0677
Misko Hevery <misko@hevery.com>
2021-04-30 21:33:48 +0000

    2dd96e08ae Revert "perf(core): avoid storing LView in __ngContext__ (#41358)" (#41901)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+294.sha-0dfd940
+ * @license Angular v12.0.0-next.8+295.sha-2dd96e0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+294.sha-0dfd940');
+const VERSION = new Version('12.0.0-next.8+295.sha-2dd96e0');
 
 /**
  * @license
