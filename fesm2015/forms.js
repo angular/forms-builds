53695333695e4fd2964afd4266789c569d790d81
Misko Hevery <misko@hevery.com>
2021-04-30 21:35:52 +0000

    6581a1b48d perf(core): minor improvements to listener instructions (#41807)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+295.sha-2dd96e0
+ * @license Angular v12.0.0-next.8+296.sha-6581a1b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+295.sha-2dd96e0');
+const VERSION = new Version('12.0.0-next.8+296.sha-6581a1b');
 
 /**
  * @license
