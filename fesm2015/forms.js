f68edc6cf413b9a2081017d72978ea3f8a89ba8e
Misko Hevery <misko@hevery.com>
2021-05-03 21:49:39 +0000

    f42796a8f5 build: lock file maintenance (#41924)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+316.sha-3777cf5
+ * @license Angular v12.0.0-next.8+315.sha-f42796a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+316.sha-3777cf5');
+const VERSION = new Version('12.0.0-next.8+315.sha-f42796a');
 
 /**
  * @license
