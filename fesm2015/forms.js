f9b56e87796fe3716086bb4de7e2486c4fa9c8e0
Misko Hevery <misko@hevery.com>
2021-05-03 17:27:16 +0000

    38f47be7af docs: update tagline (#41922)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+309.sha-f12d067
+ * @license Angular v12.0.0-next.8+310.sha-38f47be
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+309.sha-f12d067');
+const VERSION = new Version('12.0.0-next.8+310.sha-38f47be');
 
 /**
  * @license
