659658ed8a2ee7b696fefcc00981bd45a0a65429
Misko Hevery <misko@hevery.com>
2021-05-03 21:49:55 +0000

    741c295129 docs: update file structure document (#41923)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+315.sha-f42796a
+ * @license Angular v12.0.0-next.8+314.sha-741c295
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+315.sha-f42796a');
+const VERSION = new Version('12.0.0-next.8+314.sha-741c295');
 
 /**
  * @license
