04ca6fb89b79b669fec11cce6914b783be8a7e7f
Misko Hevery <misko@hevery.com>
2021-05-04 15:35:32 +0000

    b7db98fd92 build: remove emojis from github issue templates (#41902)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+314.sha-741c295
+ * @license Angular v12.0.0-next.8+318.sha-b7db98f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+314.sha-741c295');
+const VERSION = new Version('12.0.0-next.8+318.sha-b7db98f');
 
 /**
  * @license
