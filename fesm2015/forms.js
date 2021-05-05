94a1014465e622c0c6a1a9a4740a43fa69cf739a
Misko Hevery <misko@hevery.com>
2021-05-05 04:35:00 +0000

    33c9579212 refactor(dev-infra): switch away from deprecated platform execution properties (#41941)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+44.sha-b5b09dc
+ * @license Angular v12.0.0-rc.1+58.sha-33c9579
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.1+44.sha-b5b09dc');
+const VERSION = new Version('12.0.0-rc.1+58.sha-33c9579');
 
 /**
  * @license
