7bce953c154794e4eefe56bc64c09f5a7a79774c
Misko Hevery <misko@hevery.com>
2021-05-05 21:16:06 +0000

    c472c5b872 docs: initial commit of content projection topic. (#41143)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+336.sha-530e45f
+ * @license Angular v12.0.0-next.8+339.sha-c472c5b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+336.sha-530e45f');
+const VERSION = new Version('12.0.0-next.8+339.sha-c472c5b');
 
 /**
  * @license
