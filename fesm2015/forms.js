41fab0c7b14b4a359a2a0fd22cdd4bab192a4859
Misko Hevery <misko@hevery.com>
2021-05-05 04:27:00 +0000

    43cc5a126b docs: add dylhunn to contributors (#41936)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+325.sha-fdf2e02
+ * @license Angular v12.0.0-next.8+327.sha-43cc5a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+325.sha-fdf2e02');
+const VERSION = new Version('12.0.0-next.8+327.sha-43cc5a1');
 
 /**
  * @license
