d130a01c6f203e4e320688e701ecff1cfc5980aa
Misko Hevery <misko@hevery.com>
2021-05-05 21:10:23 +0000

    530e45f093 fix(dev-infra): publish script python check not working on windows (#41943)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+338.sha-931d40b
+ * @license Angular v12.0.0-next.8+336.sha-530e45f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+338.sha-931d40b');
+const VERSION = new Version('12.0.0-next.8+336.sha-530e45f');
 
 /**
  * @license
