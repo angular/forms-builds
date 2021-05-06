be20730870c486ca63ef0645c60907247dd486a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:00:38 +0000

    5c9bd237e3 build: update release configuration to use new release notes system (#41905)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+345.sha-35fbd4f
+ * @license Angular v12.0.0-next.8+350.sha-5c9bd23
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+345.sha-35fbd4f');
+const VERSION = new Version('12.0.0-next.8+350.sha-5c9bd23');
 
 /**
  * @license
