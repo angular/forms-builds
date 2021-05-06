8f7489674180f4fac7bc373211c79d45b71afaf4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:05:42 +0000

    640ec7828f refactor(common): change missing NgSwitch provider error message (#41704)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+352.sha-3a5f006
+ * @license Angular v12.0.0-next.8+354.sha-640ec78
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+352.sha-3a5f006');
+const VERSION = new Version('12.0.0-next.8+354.sha-640ec78');
 
 /**
  * @license
