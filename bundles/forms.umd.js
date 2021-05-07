3fa06a26ac41eb0099c9fe67d68eb7f06a529670
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 14:38:18 +0000

    ed20342404 docs: change the documentation of creating-libraries by providing material links (#41982)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.13+9.sha-9883dab
+ * @license Angular v11.2.13+12.sha-ed20342
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7244,7 +7244,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.2.13+9.sha-9883dab');
+    var VERSION = new i0.Version('11.2.13+12.sha-ed20342');
 
     /**
      * @license
