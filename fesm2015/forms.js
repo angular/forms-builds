bd6b9848cc3a27a26cbb63822034251989b00cca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 15:06:07 +0000

    4bc3cf216d feat(common): add URLSearchParams to request body (#37852)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+367.sha-cb418bd
+ * @license Angular v12.0.0-next.8+370.sha-4bc3cf2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+367.sha-cb418bd');
+const VERSION = new Version('12.0.0-next.8+370.sha-4bc3cf2');
 
 /**
  * @license
