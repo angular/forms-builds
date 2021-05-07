3e733cad19f82c5d371c8264f22c1e39dbc010df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 18:30:52 +0000

    c580fbb60c fix(dev-infra): merge tool should ensure that token has `workflow` oauth scope (#41989)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+22.sha-89c0298
+ * @license Angular v12.0.0-rc.2+23.sha-c580fbb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.2+22.sha-89c0298');
+const VERSION = new Version('12.0.0-rc.2+23.sha-c580fbb');
 
 /**
  * @license
