2f45aefabd0aa42c8a549032aa844cb823a8ed15
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 21:07:15 +0000

    d5555558d0 fix(platform-browser): prevent memory leak of style nodes if shadow DOM encapsulation is used (#42005)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+42.sha-d43e351
+ * @license Angular v12.0.0-rc.2+45.sha-d555555
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.2+42.sha-d43e351');
+const VERSION = new Version('12.0.0-rc.2+45.sha-d555555');
 
 /**
  * @license
