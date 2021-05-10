29469a2ced0bb51bfd530ef924cf15663ba5fc73
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:58:55 +0000

    089daea98b refactor(compiler-cli): remove redundant `potentialSourceMapUrl` property (#42000)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+387.sha-ddf2a6c
+ * @license Angular v12.0.0-next.8+385.sha-089daea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+387.sha-ddf2a6c');
+const VERSION = new Version('12.0.0-next.8+385.sha-089daea');
 
 /**
  * @license
