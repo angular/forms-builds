2e5cfb7b88a67e1998fb92c9406cd3ba652ad17c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 16:31:24 +0000

    388f4056e6 docs: fix a grammatical error in lazy-loading-ngmodules guide (#42008)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+38.sha-cffba6f
+ * @license Angular v12.0.0-rc.2+39.sha-388f405
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.2+38.sha-cffba6f');
+const VERSION = new Version('12.0.0-rc.2+39.sha-388f405');
 
 /**
  * @license
