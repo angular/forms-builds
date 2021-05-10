77392c592ecb56a155e89c4a4208b318f53df7ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 22:07:20 +0000

    1f84ac1d5f release: cut the v12.0.0-rc.3 release (#42031)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+45.sha-d555555
+ * @license Angular v12.0.0-rc.2+46.sha-1f84ac1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7314,7 +7314,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0-rc.2+45.sha-d555555');
+    var VERSION = new i0.Version('12.0.0-rc.2+46.sha-1f84ac1');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.2+45.sha-d555555
+ * @license Angular v12.0.0-rc.2+46.sha-1f84ac1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6794,7 +6794,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.2+45.sha-d555555');
+const VERSION = new Version('12.0.0-rc.2+46.sha-1f84ac1');
 
 /**
  * @license
