12e5f3fefa66f2e8091a7de059b17cabef53ac3c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 01:06:20 +0000

    53cd7e5eb5 docs: update the angular roadmap (#42050)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+399.sha-d34ce6b
+ * @license Angular v12.0.0-next.8+401.sha-53cd7e5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+399.sha-d34ce6b');
+const VERSION = new Version('12.0.0-next.8+401.sha-53cd7e5');
 
 /**
  * @license
