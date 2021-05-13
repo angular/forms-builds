8523f8f64f1f84ce73f8873f946a36a9c04562bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 17:52:16 +0000

    45083bc302 build: lock file maintenance (#42015)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+401.sha-53cd7e5
+ * @license Angular v12.0.0-next.8+403.sha-45083bc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7111,7 +7111,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+401.sha-53cd7e5');
+const VERSION = new Version('12.0.0-next.8+403.sha-45083bc');
 
 /**
  * @license
