d0a3ddaf57c8411205be99b1d81108e7822cc3e3
Zach Arend <zachzach@google.com>
2021-05-14 15:13:28 +0000

    4bc5b4d93b fix(core): global listeners not being bound on non-node host elements (#42014)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+410.sha-2e7eb27
+ * @license Angular v12.0.0-next.8+411.sha-4bc5b4d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+410.sha-2e7eb27');
+const VERSION = new Version('12.0.0-next.8+411.sha-4bc5b4d');
 
 /**
  * @license
