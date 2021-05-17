53636f7380200987dc9acfaf82e03cc6d747c9f8
atscott <atscott01@gmail.com>
2021-05-17 18:17:13 +0000

    c32bea6ccf docs: add link to the blog post in the changelog (#42109) PR Close #42109

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+418.sha-fc1bc0e
+ * @license Angular v12.0.0-next.8+420.sha-c32bea6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+418.sha-fc1bc0e');
+const VERSION = new Version('12.0.0-next.8+420.sha-c32bea6');
 
 /**
  * @license
