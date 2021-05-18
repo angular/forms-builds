59d82fa780209159b1c14323d9b2bc5fe813b699
atscott <atscott01@gmail.com>
2021-05-18 16:02:01 +0000

    fd4dbcb0c7 docs: decorate input properties with @Input (#42128) PR Close #42128

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+425.sha-2dfa010
+ * @license Angular v12.0.0-next.8+423.sha-fd4dbcb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+425.sha-2dfa010');
+const VERSION = new Version('12.0.0-next.8+423.sha-fd4dbcb');
 
 /**
  * @license
