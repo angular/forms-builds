74c9602c772ee0fbd6a0fc660a0e69d04ca1abc4
Andrew Scott <atscott@google.com>
2021-05-18 18:39:01 +0000

    c58e15a4b0 ci: add pullapprove entries for devtools (#42152)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+26.sha-6861911
+ * @license Angular v12.0.0+32.sha-c58e15a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+26.sha-6861911');
+const VERSION = new Version('12.0.0+32.sha-c58e15a');
 
 /**
  * @license
