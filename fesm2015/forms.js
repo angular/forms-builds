9867154084f850edd7615684029aaa9261cf2d4f
Andrew Scott <atscott@google.com>
2021-05-18 23:02:09 +0000

    2dd956421c build: remove publishConfig entry from package.json entries (#42104)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+437.sha-ec78e68
+ * @license Angular v12.0.0-next.8+438.sha-2dd9564
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+437.sha-ec78e68');
+const VERSION = new Version('12.0.0-next.8+438.sha-2dd9564');
 
 /**
  * @license
