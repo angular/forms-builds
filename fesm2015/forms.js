13974d3a03b37891e96476eabd33eeb6edab6bc4
Zach Arend <zachzach@google.com>
2021-05-20 17:15:51 +0000

    a01fe8d9b4 ci: add Teri to global approvers for docs changes list (#42156)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+3.sha-04ab5ed
+ * @license Angular v12.1.0-next.2+6.sha-a01fe8d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+3.sha-04ab5ed');
+const VERSION = new Version('12.1.0-next.2+6.sha-a01fe8d');
 
 /**
  * @license
