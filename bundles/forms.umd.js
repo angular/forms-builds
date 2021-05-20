c8e48ee7a6fc15823107bd9f0be0788b71a61c7c
Zach Arend <zachzach@google.com>
2021-05-20 16:26:59 +0000

    390d24b75f test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+42.sha-43525ad
+ * @license Angular v12.0.1+3.sha-390d24b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.0+42.sha-43525ad');
+    var VERSION = new i0.Version('12.0.1+3.sha-390d24b');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+42.sha-43525ad
+ * @license Angular v12.0.1+3.sha-390d24b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+42.sha-43525ad');
+const VERSION = new Version('12.0.1+3.sha-390d24b');
 
 /**
  * @license
