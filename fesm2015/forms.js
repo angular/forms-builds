a14d4dbd065144ccf26e49f11f5babaaf828ff46
Zach Arend <zachzach@google.com>
2021-05-21 18:26:54 +0000

    03825cfab6 docs: updated animation docs intro section and example (#42046)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+15.sha-62becc8
+ * @license Angular v12.1.0-next.2+17.sha-03825cf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+15.sha-62becc8');
+const VERSION = new Version('12.1.0-next.2+17.sha-03825cf');
 
 /**
  * @license
