9a4293f89dc15a3adab1b26a80d73e2d12fb3112
Zach Arend <zachzach@google.com>
2021-05-21 18:25:01 +0000

    62becc88f9 fix(docs-infra): fix example compilation issues (#42199)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+14.sha-8c158ba
+ * @license Angular v12.1.0-next.2+15.sha-62becc8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+14.sha-8c158ba');
+const VERSION = new Version('12.1.0-next.2+15.sha-62becc8');
 
 /**
  * @license
