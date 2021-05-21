c90f66e189640d917aa2adf3d4a379dc2a05a671
Joey Perrott <josephperrott@gmail.com>
2021-05-21 21:02:01 +0000

    30c461d862 ci: update to latest feature request triage bot commit (#42229)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+17.sha-03825cf
+ * @license Angular v12.1.0-next.2+18.sha-30c461d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+17.sha-03825cf');
+const VERSION = new Version('12.1.0-next.2+18.sha-30c461d');
 
 /**
  * @license
