f13ef9d08d33bbe9f645357127f96823a7f796d2
Zach Arend <zachzach@google.com>
2021-05-24 19:43:54 +0000

    c820066d98 docs: align code in `product-example.component.*.ts` files (#42155)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+25.sha-bf505ad
+ * @license Angular v12.1.0-next.2+31.sha-c820066
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+25.sha-bf505ad');
+const VERSION = new Version('12.1.0-next.2+31.sha-c820066');
 
 /**
  * @license
