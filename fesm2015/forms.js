e19219b74d6975746970772814cace3896e6b554
Zach Arend <zachzach@google.com>
2021-05-24 23:01:34 +0000

    1040697787 docs: remove sentence about how the not reloading the page. (#42290)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+33.sha-ad12b31
+ * @license Angular v12.1.0-next.2+37.sha-1040697
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+33.sha-ad12b31');
+const VERSION = new Version('12.1.0-next.2+37.sha-1040697');
 
 /**
  * @license
