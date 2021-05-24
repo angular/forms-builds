4be25aaf24b1a48c4a85519adc8ef1fa7f5cff0c
Zach Arend <zachzach@google.com>
2021-05-24 17:27:32 +0000

    bf505ad707 docs(core): tighten up PipeTransform example types (#42240)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+24.sha-4ac55ca
+ * @license Angular v12.1.0-next.2+25.sha-bf505ad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+24.sha-4ac55ca');
+const VERSION = new Version('12.1.0-next.2+25.sha-bf505ad');
 
 /**
  * @license
