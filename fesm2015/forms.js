639eb69ff4dfe4b0514e8132f637458fb3352819
Zach Arend <zachzach@google.com>
2021-05-24 15:04:36 +0000

    c03280fd54 docs: update the intro image in the devtools guide (#42162)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+21.sha-3293dd3
+ * @license Angular v12.1.0-next.2+23.sha-c03280f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+21.sha-3293dd3');
+const VERSION = new Version('12.1.0-next.2+23.sha-c03280f');
 
 /**
  * @license
