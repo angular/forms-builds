e824d98352ab9c51c83d7d5d63a54121f233b77d
Zach Arend <zachzach@google.com>
2021-05-24 16:18:07 +0000

    4ac55ca676 fix(dev-infra): use template strings for release note templates (#42224)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+22.sha-d7a1da9
+ * @license Angular v12.1.0-next.2+24.sha-4ac55ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+22.sha-d7a1da9');
+const VERSION = new Version('12.1.0-next.2+24.sha-4ac55ca');
 
 /**
  * @license
