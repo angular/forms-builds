5bad48266e6644aae4a1d85a9a285b465f98c4a5
Zach Arend <zachzach@google.com>
2021-05-24 15:04:57 +0000

    d7a1da9d4f docs: fix incorrect point in universal-in-action (#42262)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+23.sha-c03280f
+ * @license Angular v12.1.0-next.2+22.sha-d7a1da9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+23.sha-c03280f');
+const VERSION = new Version('12.1.0-next.2+22.sha-d7a1da9');
 
 /**
  * @license
