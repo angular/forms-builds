b41913a77c81f72a5c1b834eb33fc1c4d9e7ca9d
Zach Arend <zachzach@google.com>
2021-05-25 18:15:17 +0000

    d15a6a3a71 docs: update web-worker syntax (#42306)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+59.sha-1955dbb
+ * @license Angular v12.1.0-next.2+58.sha-d15a6a3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+59.sha-1955dbb');
+const VERSION = new Version('12.1.0-next.2+58.sha-d15a6a3');
 
 /**
  * @license
