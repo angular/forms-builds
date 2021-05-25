0c5c72268c37f331936e3617bdd69b9cc4a493d9
Zach Arend <zachzach@google.com>
2021-05-25 22:33:19 +0000

    b0581f3117 docs: add link to the style precedence guide in ivy compatibility (#42317)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+73.sha-9f50495
+ * @license Angular v12.1.0-next.2+78.sha-b0581f3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+73.sha-9f50495');
+const VERSION = new Version('12.1.0-next.2+78.sha-b0581f3');
 
 /**
  * @license
