b53c62944933c64b9a4e15a155592a3c5cfee85a
Zach Arend <zachzach@google.com>
2021-05-25 18:20:56 +0000

    c3cbfc22e0 fix(dev-infra): format command incorrectly prints outdated files (#42252)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+65.sha-f74fd64
+ * @license Angular v12.1.0-next.2+63.sha-c3cbfc2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+65.sha-f74fd64');
+const VERSION = new Version('12.1.0-next.2+63.sha-c3cbfc2');
 
 /**
  * @license
