4f547f9ab42db7c53c7f076faeac5ddb4cefbe9e
Zach Arend <zachzach@google.com>
2021-05-25 18:24:07 +0000

    594e63315e feat(dev-infra): add release notes generation to ng-dev (#42225)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+63.sha-c3cbfc2
+ * @license Angular v12.1.0-next.2+67.sha-594e633
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+63.sha-c3cbfc2');
+const VERSION = new Version('12.1.0-next.2+67.sha-594e633');
 
 /**
  * @license
