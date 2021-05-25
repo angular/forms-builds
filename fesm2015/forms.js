4e2abd3f679264d13a823ddb999b258ae50378b3
Zach Arend <zachzach@google.com>
2021-05-25 22:38:16 +0000

    b7721aa630 docs: Edit correction to remove jargon in Release section (#42029)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+80.sha-6da0c97
+ * @license Angular v12.1.0-next.2+84.sha-b7721aa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+80.sha-6da0c97');
+const VERSION = new Version('12.1.0-next.2+84.sha-b7721aa');
 
 /**
  * @license
