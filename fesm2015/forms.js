d09188cd0d924ff514af73c51a647b5f18ed47da
Zach Arend <zachzach@google.com>
2021-05-25 18:08:50 +0000

    9f09c3b9b1 build(core): update supported range of node versions to be less restrictive (#42205)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.2+40.sha-1c3fe69
+ * @license Angular v12.1.0-next.2+56.sha-9f09c3b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.2+40.sha-1c3fe69');
+const VERSION = new Version('12.1.0-next.2+56.sha-9f09c3b');
 
 /**
  * @license
