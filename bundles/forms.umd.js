652b30d45035f7394c0f9495c20bffba27661976
Zach Arend <zachzach@google.com>
2021-05-25 17:44:06 +0000

    12b1e87e6c build: update CLI packages to 12.0.1 (#42307)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+15.sha-99802dc
+ * @license Angular v12.0.1+38.sha-12b1e87
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.1+15.sha-99802dc');
+    var VERSION = new i0.Version('12.0.1+38.sha-12b1e87');
 
     /**
      * @license
