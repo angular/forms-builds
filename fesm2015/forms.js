5cf79a450998d6b18d3ec9bcd08dc53d7904ad2c
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:43:29 +0000

    516923e3dc docs: clarify when the `ngDoBootstrap` hook is invoked (#42362)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+16.sha-1572659
+ * @license Angular v12.1.0-next.3+18.sha-516923e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+16.sha-1572659');
+const VERSION = new Version('12.1.0-next.3+18.sha-516923e');
 
 /**
  * @license
