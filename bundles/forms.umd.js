51f62773a43d34974e38da9e9464753efa3136dd
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:45:16 +0000

    765854ed8f build: allow node v15 and above for aio (#42367)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+15.sha-12e7968
+ * @license Angular v12.0.2+21.sha-765854e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.2+15.sha-12e7968');
+    var VERSION = new i0.Version('12.0.2+21.sha-765854e');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+15.sha-12e7968
+ * @license Angular v12.0.2+21.sha-765854e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+15.sha-12e7968');
+const VERSION = new Version('12.0.2+21.sha-765854e');
 
 /**
  * @license
