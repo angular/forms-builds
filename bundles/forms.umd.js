f44afbd6eec597b36ffee6f0c29693c6530147c7
Zach Arend <zachzach@google.com>
2021-05-26 18:55:10 +0000

    5a51884069 fix(dev-infra): build with stamping for release builds (#42354)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+84.sha-037b636
+ * @license Angular v12.0.2+2.sha-5a51884
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.1+84.sha-037b636');
+    var VERSION = new i0.Version('12.0.2+2.sha-5a51884');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.1+84.sha-037b636
+ * @license Angular v12.0.2+2.sha-5a51884
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.1+84.sha-037b636');
+const VERSION = new Version('12.0.2+2.sha-5a51884');
 
 /**
  * @license
