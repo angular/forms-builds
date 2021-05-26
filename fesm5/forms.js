3b88b47975244fd4e487aed7c68f18b53b2a09b8
Zach Arend <zachzach@google.com>
2021-05-26 21:01:57 +0000

    fc25f94f2f build: mark //integration:bazel-schematics_test as manual (#42042)

--- fesm5/forms.js
+++ fesm5/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.13
+ * @license Angular v9.1.13+2.sha-fc25f94
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6822,7 +6822,7 @@ var FormBuilder = /** @class */ (function () {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.13');
+var VERSION = new Version('9.1.13+2.sha-fc25f94');
 
 /**
  * @license
