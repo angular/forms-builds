03e8d95615243c94f0c28bd1367110a58aa757cd
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:40:26 +0000

    4cb11cc4e7 docs: add note about why module import order is important (#42323)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+14.sha-4e9b61a
+ * @license Angular v12.1.0-next.3+15.sha-4cb11cc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+14.sha-4e9b61a');
+const VERSION = new Version('12.1.0-next.3+15.sha-4cb11cc');
 
 /**
  * @license
