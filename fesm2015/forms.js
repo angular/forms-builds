4f470ddc5130bc8ec691914b32d1e6ef72b1a5b8
Andrew Kushnir <akushnir@google.com>
2021-05-27 22:56:58 +0000

    635aaaa27d docs: Edited to remove jargon. (#41978)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+44.sha-3a95915
+ * @license Angular v12.1.0-next.3+42.sha-635aaaa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7103,7 +7103,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+44.sha-3a95915');
+const VERSION = new Version('12.1.0-next.3+42.sha-635aaaa');
 
 /**
  * @license
