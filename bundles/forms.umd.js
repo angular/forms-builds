eb7a3cf16dbafdc8cc8befd2aeca7dbaefb53d51
Andrew Kushnir <akushnir@google.com>
2021-06-01 19:39:33 +0000

    8bdcca1e08 fix(compiler-cli): better detect classes that are indirectly exported (#42207)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+62.sha-b0293c6
+ * @license Angular v12.0.2+63.sha-8bdcca1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.2+62.sha-b0293c6');
+    var VERSION = new i0.Version('12.0.2+63.sha-8bdcca1');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+62.sha-b0293c6
+ * @license Angular v12.0.2+63.sha-8bdcca1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+62.sha-b0293c6');
+const VERSION = new Version('12.0.2+63.sha-8bdcca1');
 
 /**
  * @license
