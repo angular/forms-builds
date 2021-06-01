b9760c34ea2a42345f1cb54b949b8561bbe91ad1
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:41:33 +0000

    b0293c656e docs: clarify how declaration files can be installed (#42417)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+42.sha-b60c9c1
+ * @license Angular v12.0.2+62.sha-b0293c6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.2+42.sha-b60c9c1');
+    var VERSION = new i0.Version('12.0.2+62.sha-b0293c6');
 
     /**
      * @license
