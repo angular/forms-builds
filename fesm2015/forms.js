228fc8aae0a8454c04c44cb60ce937606c0bfac1
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:31:17 +0000

    e0381a87c9 docs: update CLI builder to be platform agnostic (#42371)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.3+49.sha-e798bfa
+ * @license Angular v12.1.0-next.3+52.sha-e0381a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.3+49.sha-e798bfa');
+const VERSION = new Version('12.1.0-next.3+52.sha-e0381a8');
 
 /**
  * @license
