9ead7a418aa60f69abfcca29c1848a317a14e901
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 23:24:10 +0000

    beae4250fe ci: extend default timeout of karma tests for legacy saucelab tests (#42473)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+30.sha-a3b01e8
+ * @license Angular v12.0.3+31.sha-beae425
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.3+30.sha-a3b01e8');
+    var VERSION = new i0.Version('12.0.3+31.sha-beae425');
 
     /**
      * @license
