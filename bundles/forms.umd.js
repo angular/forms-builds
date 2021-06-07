4d179fcf035e3c2f4182156f2af086810ffc4397
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:16:14 +0000

    8bb6fb7704 docs(router): fix code-example headers (#42507)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+41.sha-65d5930
+ * @license Angular v12.0.3+42.sha-8bb6fb7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.3+41.sha-65d5930');
+    var VERSION = new i0.Version('12.0.3+42.sha-8bb6fb7');
 
     /**
      * @license
