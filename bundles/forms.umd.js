2774734de789cdc292f9ffee81f056f69f1b02a9
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:44:24 +0000

    34dd3c360b fix(language-service): fix autocomplete info display for some cases (#42472)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+42.sha-8bb6fb7
+ * @license Angular v12.0.3+43.sha-34dd3c3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.3+42.sha-8bb6fb7');
+    var VERSION = new i0.Version('12.0.3+43.sha-34dd3c3');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+42.sha-8bb6fb7
+ * @license Angular v12.0.3+43.sha-34dd3c3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+42.sha-8bb6fb7');
+const VERSION = new Version('12.0.3+43.sha-34dd3c3');
 
 /**
  * @license
