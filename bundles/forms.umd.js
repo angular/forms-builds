353c4e06c9530f85146855af9d678743e1c078d8
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:55:46 +0000

    770883ff4a fix(docs-infra): fix search results font color (#42488)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+34.sha-ad9dd2a
+ * @license Angular v12.0.3+37.sha-770883f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.3+34.sha-ad9dd2a');
+    var VERSION = new i0.Version('12.0.3+37.sha-770883f');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+34.sha-ad9dd2a
+ * @license Angular v12.0.3+37.sha-770883f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+34.sha-ad9dd2a');
+const VERSION = new Version('12.0.3+37.sha-770883f');
 
 /**
  * @license
