c45b57b945b0a81008835e97e00e60e861323043
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:14:13 +0000

    38c592ea0a build: remove unused no-strict tsconfig file (#42506)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+54.sha-64c8027
+ * @license Angular v12.1.0-next.4+55.sha-38c592e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7105,7 +7105,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+54.sha-64c8027');
+const VERSION = new Version('12.1.0-next.4+55.sha-38c592e');
 
 /**
  * @license
