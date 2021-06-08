b077294407267c1635857a3ad0c6ed5b8b2abb89
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:03:52 +0000

    5c0541f351 docs: fix issue 24571 in ngOnChanges (#42517)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+82.sha-386550c
+ * @license Angular v12.1.0-next.4+84.sha-5c0541f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7105,7 +7105,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+82.sha-386550c');
+const VERSION = new Version('12.1.0-next.4+84.sha-5c0541f');
 
 /**
  * @license
