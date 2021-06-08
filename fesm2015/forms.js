a3aa51ec1741314cd5054fa203ec21914d931f21
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:08:02 +0000

    4975a106dc docs(platform-server): added commands for universal and add prerendering info (#36005)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+85.sha-43b4a8c
+ * @license Angular v12.1.0-next.4+87.sha-4975a10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+85.sha-43b4a8c');
+const VERSION = new Version('12.1.0-next.4+87.sha-4975a10');
 
 /**
  * @license
