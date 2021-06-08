518bcd0315d09ac3acdace8638cefc7948c75cab
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:59:37 +0000

    645cad5614 docs(core): describe interactions between view-encapsulated components (#42397)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+71.sha-751f68d
+ * @license Angular v12.1.0-next.4+73.sha-645cad5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7105,7 +7105,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+71.sha-751f68d');
+const VERSION = new Version('12.1.0-next.4+73.sha-645cad5');
 
 /**
  * @license
