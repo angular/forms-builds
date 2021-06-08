0089fab69d3c16d4d30ba6b6ce8828467128123a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:02:31 +0000

    d10c38a8f8 docs: add Roman Sedov to contributors page (#42515)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+73.sha-645cad5
+ * @license Angular v12.1.0-next.4+72.sha-d10c38a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7105,7 +7105,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+73.sha-645cad5');
+const VERSION = new Version('12.1.0-next.4+72.sha-d10c38a');
 
 /**
  * @license
