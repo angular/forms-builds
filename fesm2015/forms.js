539084332b6dcb2ff50fb07d64d74546f3dfe367
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:04:22 +0000

    f3fde4bbb5 docs: Add selector for `PeekABooDirective` (#42146)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+84.sha-5c0541f
+ * @license Angular v12.1.0-next.4+86.sha-f3fde4b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+84.sha-5c0541f');
+const VERSION = new Version('12.1.0-next.4+86.sha-f3fde4b');
 
 /**
  * @license
