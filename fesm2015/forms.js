10bf80504eafa0fffe3f72fdeee8741362c774b2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 19:07:23 +0000

    93de5a0c5c docs: clarify documentation regarding passing multiple configurations to ng build (#41333)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+50.sha-a77ec5b
+ * @license Angular v12.0.3+53.sha-93de5a0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+50.sha-a77ec5b');
+const VERSION = new Version('12.0.3+53.sha-93de5a0');
 
 /**
  * @license
