38e7490d65017d00fb4c133f50de82a3c9c832b4
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:03:37 +0000

    4cf442c879 docs: add links to examples for ComponentFactoryResolver (#42496)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+76.sha-c44ab4f
+ * @license Angular v12.1.0-next.4+74.sha-4cf442c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+76.sha-c44ab4f');
+const VERSION = new Version('12.1.0-next.4+74.sha-4cf442c');
 
 /**
  * @license
