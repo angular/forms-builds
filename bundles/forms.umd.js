5ba794748f7431860188d8db23eaa7e18be3dfdf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 19:34:02 +0000

    785da0f1bf fix(core): ensure that autoRegisterModuleById registration in ɵɵdefineNgModule is not DCE-ed by closure (#42529)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+66.sha-dfb072a
+ * @license Angular v12.0.4+2.sha-785da0f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.3+66.sha-dfb072a');
+    var VERSION = new i0.Version('12.0.4+2.sha-785da0f');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+66.sha-dfb072a
+ * @license Angular v12.0.4+2.sha-785da0f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+66.sha-dfb072a');
+const VERSION = new Version('12.0.4+2.sha-785da0f');
 
 /**
  * @license
