9e24e4493ac9792260df2df55353e9cff19db11c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:37:08 +0000

    665b986896 docs: CLI users polyfills (#42263)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+3.sha-22bda22
+ * @license Angular v12.1.0-next.5+4.sha-665b986
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+3.sha-22bda22');
+const VERSION = new Version('12.1.0-next.5+4.sha-665b986');
 
 /**
  * @license
