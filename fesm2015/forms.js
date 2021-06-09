cfb6515185caacea4075a99437af73bb636f3ca5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 01:13:19 +0000

    3de774e778 docs: add guidance on adding filtering and sorting logic to componetns instead of pipes (#42368)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+92.sha-e1a80d4
+ * @license Angular v12.1.0-next.4+93.sha-3de774e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6808,7 +6808,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+92.sha-e1a80d4');
+const VERSION = new Version('12.1.0-next.4+93.sha-3de774e');
 
 /**
  * @license
