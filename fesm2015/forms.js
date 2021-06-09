39c52f17eb2ded0280dafc3cef1b2a3586cd653c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 16:36:14 +0000

    3c25242ada build(docs-infra): upgrade cli command docs sources to 18bbd044d (#42525)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+93.sha-3de774e
+ * @license Angular v12.1.0-next.4+94.sha-3c25242
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+93.sha-3de774e');
+const VERSION = new Version('12.1.0-next.4+94.sha-3c25242');
 
 /**
  * @license
