15e373bb95872e19a82bff4b39c39adc0fc02516
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:42:50 +0000

    e0158a1515 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+4.sha-a75874c
+ * @license Angular v12.0.4+5.sha-e0158a1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.4+4.sha-a75874c');
+const VERSION = new Version('12.0.4+5.sha-e0158a1');
 
 /**
  * @license
