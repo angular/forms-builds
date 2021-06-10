29823ab5d1beab2fe061cab78d2d92a85cd98078
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:17:33 +0000

    aef489f139 docs: add note for naming libraries (#42335)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+6.sha-e299683
+ * @license Angular v12.1.0-next.5+7.sha-aef489f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6808,7 +6808,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+6.sha-e299683');
+const VERSION = new Version('12.1.0-next.5+7.sha-aef489f');
 
 /**
  * @license
