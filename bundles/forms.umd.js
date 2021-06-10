3f01b8ffc50ff1b73b2e5653a27085ae257cee37
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:28:53 +0000

    efb440eb2f refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+11.sha-52e0987
+ * @license Angular v12.1.0-next.5+12.sha-efb440e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7787,7 +7787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.5+11.sha-52e0987');
+    var VERSION = new i0.Version('12.1.0-next.5+12.sha-efb440e');
 
     /**
      * @license
