0f2645f66582780171aea5204f1b2235e98f7e4e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:28:02 +0000

    013d1cb010 refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.4+6.sha-5185795
+ * @license Angular v12.0.4+11.sha-013d1cb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.4+6.sha-5185795');
+const VERSION = new Version('12.0.4+11.sha-013d1cb');
 
 /**
  * @license
