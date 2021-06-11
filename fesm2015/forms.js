7664f2cdcd0b1c0347328c72f74ada3962dca80f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 19:06:48 +0000

    5161084917 docs: remove section on tables from docs style guide (#42330)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+13.sha-0c13e2b
+ * @license Angular v12.1.0-next.5+14.sha-5161084
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+13.sha-0c13e2b');
+const VERSION = new Version('12.1.0-next.5+14.sha-5161084');
 
 /**
  * @license
