83af78ee43eba5c040c3b57d8a948a65d2c2eee1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:14:33 +0000

    828fde6e0d feat(docs-infra): implement popup to inform about the use of cookies (#42259)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+14.sha-5161084
+ * @license Angular v12.1.0-next.5+18.sha-828fde6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+14.sha-5161084');
+const VERSION = new Version('12.1.0-next.5+18.sha-828fde6');
 
 /**
  * @license
