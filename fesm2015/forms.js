3e9cf51a92747f6b0d7d62d558d975e058ca507d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:30:25 +0000

    228beeabd1 fix(language-service): Use last child end span for parent without close tag (#42554)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+28.sha-044e022
+ * @license Angular v12.1.0-next.5+30.sha-228beea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6808,7 +6808,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+28.sha-044e022');
+const VERSION = new Version('12.1.0-next.5+30.sha-228beea');
 
 /**
  * @license
