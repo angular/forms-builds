f387fca9c929475f81e452bbf5e9c972f9973e38
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:21:10 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+40.sha-2d1347b
+ * @license Angular v12.1.0-next.5+41.sha-62aca30
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+40.sha-2d1347b');
+const VERSION = new Version('12.1.0-next.5+41.sha-62aca30');
 
 /**
  * @license
