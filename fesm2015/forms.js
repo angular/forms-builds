33ff192ca91826d4bb4e7193244d69dfad1702a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:21:27 +0000

    5c25595647 docs: release notes for the v12.0.5 release (#42586)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+47.sha-eb04684
+ * @license Angular v12.1.0-next.5+48.sha-5c25595
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+47.sha-eb04684');
+const VERSION = new Version('12.1.0-next.5+48.sha-5c25595');
 
 /**
  * @license
