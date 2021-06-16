313a5045b174b2aae8b59b2bed218ac6545db716
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:25:16 +0000

    eb04684ed3 fix(docs-infra): make anchor in list item inherit line-height (#42572)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+46.sha-7b85a4d
+ * @license Angular v12.1.0-next.5+47.sha-eb04684
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+46.sha-7b85a4d');
+const VERSION = new Version('12.1.0-next.5+47.sha-eb04684');
 
 /**
  * @license
