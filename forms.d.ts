d4d27ca113bcdc723879b93be0f4a02b3d2d7f27
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:06 +0000

    f3a79878af docs(forms): correct sample code for FormArray.reset (#42477)

--- forms.d.ts
+++ forms.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+41.sha-62aca30
+ * @license Angular v12.1.0-next.5+42.sha-f3a7987
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1447,19 +1447,19 @@ export declare class FormArray extends AbstractControl {
      * ]);
      * arr.reset(['name', 'last name']);
      *
-     * console.log(this.arr.value);  // ['name', 'last name']
+     * console.log(arr.value);  // ['name', 'last name']
      * ```
      *
      * ### Reset the values in a form array and the disabled status for the first control
      *
      * ```
-     * this.arr.reset([
+     * arr.reset([
      *   {value: 'name', disabled: true},
      *   'last'
      * ]);
      *
-     * console.log(this.arr.value);  // ['name', 'last name']
-     * console.log(this.arr.get(0).status);  // 'DISABLED'
+     * console.log(arr.value);  // ['last']
+     * console.log(arr.at(0).status);  // 'DISABLED'
      * ```
      *
      * @param value Array of values for the controls
