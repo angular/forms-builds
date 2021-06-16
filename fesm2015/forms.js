c38d432478113a68f0fd132c766fdf307cbf02ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 17:11:46 +0000

    2d1347b2ce Revert "refactor: remove checked-in locale files (#42230)" (#42583)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+33.sha-b52e935
+ * @license Angular v12.1.0-next.5+40.sha-2d1347b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+33.sha-b52e935');
+const VERSION = new Version('12.1.0-next.5+40.sha-2d1347b');
 
 /**
  * @license
