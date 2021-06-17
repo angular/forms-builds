3c2b0ca4f608b0299b75b29640837481edceb79d
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:31:36 +0000

    07c1ddc487 fix(router): error if module is destroyed before location is initialized (#42560)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+3.sha-166e98a
+ * @license Angular v12.1.0-next.6+4.sha-07c1ddc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7787,7 +7787,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0-next.6+3.sha-166e98a');
+    var VERSION = new i0.Version('12.1.0-next.6+4.sha-07c1ddc');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+3.sha-166e98a
+ * @license Angular v12.1.0-next.6+4.sha-07c1ddc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+3.sha-166e98a');
+const VERSION = new Version('12.1.0-next.6+4.sha-07c1ddc');
 
 /**
  * @license
