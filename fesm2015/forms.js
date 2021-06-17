c7c3c513d986f1ae216d1682ac20edbd07c38b5e
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:29:30 +0000

    166e98a594 build: update all non-major dependencies (#42544)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+2.sha-0af9b89
+ * @license Angular v12.1.0-next.6+3.sha-166e98a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+2.sha-0af9b89');
+const VERSION = new Version('12.1.0-next.6+3.sha-166e98a');
 
 /**
  * @license
