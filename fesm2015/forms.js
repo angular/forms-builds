16e8e5b61c55602448b16f1284f6bb8a4566985b
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 17:04:08 +0000

    d77f560403 build: update to typescript 4.3.4 (#42600)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+23.sha-4429188
+ * @license Angular v12.1.0-next.6+25.sha-d77f560
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+23.sha-4429188');
+const VERSION = new Version('12.1.0-next.6+25.sha-d77f560');
 
 /**
  * @license
