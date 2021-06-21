623e88850634b045db1b1158f29e826162ab2160
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 19:06:43 +0000

    1ce5c20656 docs: add new i18n formats to i18n guide (#42597)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.5+23.sha-1cb85ba
+ * @license Angular v12.0.5+28.sha-1ce5c20
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7306,7 +7306,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.0.5+23.sha-1cb85ba');
+    var VERSION = new i0.Version('12.0.5+28.sha-1ce5c20');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.5+23.sha-1cb85ba
+ * @license Angular v12.0.5+28.sha-1ce5c20
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.5+23.sha-1cb85ba');
+const VERSION = new Version('12.0.5+28.sha-1ce5c20');
 
 /**
  * @license
