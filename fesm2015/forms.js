440a80f797b71274dc75203305a2ccecc4d18943
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 20:15:12 +0000

    b31e905754 build(docs-infra): disambiguate doc paths for global APIs (#42648)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.5+39.sha-4459741
+ * @license Angular v12.0.5+42.sha-b31e905
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.5+39.sha-4459741');
+const VERSION = new Version('12.0.5+42.sha-b31e905');
 
 /**
  * @license
