a6c1da7d0ecdcb5a5b4f35f87db5b7b0481732a6
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 16:41:23 +0000

    874de59d35 fix(compiler-cli): change default ngcc  hash algorithm to be FIPS compliant (#42582)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+45.sha-8da6f66
+ * @license Angular v12.1.0-next.6+48.sha-874de59
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+45.sha-8da6f66');
+const VERSION = new Version('12.1.0-next.6+48.sha-874de59');
 
 /**
  * @license
