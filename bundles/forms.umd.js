d6c194714fe873ca14d1ac8be837383a2cd2aeab
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 18:03:12 +0000

    a1e036cfd1 docs: correct literal string example (#42623)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+2.sha-f6b828e
+ * @license Angular v12.1.0+11.sha-a1e036c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7334,7 +7334,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0+2.sha-f6b828e');
+    var VERSION = new i0.Version('12.1.0+11.sha-a1e036c');
 
     /**
      * @license
