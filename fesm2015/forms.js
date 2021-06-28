b4674f1530c191bb9d81d20ad39193f9f264068e
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 18:11:10 +0000

    c93df7dc6d docs: correct literal string example (#42623)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+11.sha-1142b51
+ * @license Angular v12.2.0-next.0+12.sha-c93df7d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+11.sha-1142b51');
+const VERSION = new Version('12.2.0-next.0+12.sha-c93df7d');
 
 /**
  * @license
