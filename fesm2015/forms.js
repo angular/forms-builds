02e4d024cf2096438cb16427991fdc2efcab62da
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:54:43 +0000

    f29fe5ced0 fix(dev-infra): use API pagination for retrieving project branches (#42666)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+17.sha-0274255
+ * @license Angular v12.2.0-next.0+19.sha-f29fe5c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+17.sha-0274255');
+const VERSION = new Version('12.2.0-next.0+19.sha-f29fe5c');
 
 /**
  * @license
