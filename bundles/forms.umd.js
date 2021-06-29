56f18f1cbd25465fc49a8c6ed072825c97d77a5d
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:50:24 +0000

    78cf10b28b refactor(docs-infra): make angular.io and docs examples compatible with RxJS v7 (#42660)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+13.sha-ace0a50
+ * @license Angular v12.1.0+15.sha-78cf10b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7334,7 +7334,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0+13.sha-ace0a50');
+    var VERSION = new i0.Version('12.1.0+15.sha-78cf10b');
 
     /**
      * @license
