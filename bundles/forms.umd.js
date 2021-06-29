ba5dec9f8fea9f4a38006c0ecb3d0b8555007c4f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 16:46:25 +0000

    ace0a50646 docs: remove duplicate link to `guide/inputs-outputs.md` (#42654)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+12.sha-0f27f89
+ * @license Angular v12.1.0+13.sha-ace0a50
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7334,7 +7334,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0+12.sha-0f27f89');
+    var VERSION = new i0.Version('12.1.0+13.sha-ace0a50');
 
     /**
      * @license
