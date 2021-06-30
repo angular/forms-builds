95badbe36e07cb8165f63a5d07a88c4059d7b8ef
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:05:51 +0000

    59fe159b78 build: update API goldens to reflect new tool (#42688)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+24.sha-0e20b05
+ * @license Angular v12.2.0-next.0+27.sha-59fe159
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+24.sha-0e20b05');
+const VERSION = new Version('12.2.0-next.0+27.sha-59fe159');
 
 /**
  * @license
