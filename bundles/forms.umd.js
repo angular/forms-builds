0017cdb8a477c6727db0bd4753bae26e355b57d2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:54:30 +0000

    d19ddd1a87 fix(platform-browser): in `Meta.addTag()` do not add duplicate meta tags (#42703)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+17.sha-c380d56
+ * @license Angular v12.1.0+18.sha-d19ddd1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7334,7 +7334,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.0+17.sha-c380d56');
+    var VERSION = new i0.Version('12.1.0+18.sha-d19ddd1');
 
     /**
      * @license
