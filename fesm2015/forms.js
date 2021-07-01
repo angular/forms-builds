154ecb832cd1bef1532a3f18ce299e7328e67a18
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:55:38 +0000

    bfa1b5d9eb fix(docs-infra): move dots inside Usage Notes anchor tag (#42725)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+2.sha-d997303
+ * @license Angular v12.2.0-next.1+3.sha-bfa1b5d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+2.sha-d997303');
+const VERSION = new Version('12.2.0-next.1+3.sha-bfa1b5d');
 
 /**
  * @license
