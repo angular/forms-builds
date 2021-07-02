d38efe54cb22103527f68ba8f02e07317533891d
atscott <atscott01@gmail.com>
2021-07-02 17:29:56 +0000

    e99af184ab build: update API goldens after api-extractor update (#42737)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+9.sha-e81efe6
+ * @license Angular v12.2.0-next.1+12.sha-e99af18
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7793,7 +7793,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.1+9.sha-e81efe6');
+    var VERSION = new i0.Version('12.2.0-next.1+12.sha-e99af18');
 
     /**
      * @license
