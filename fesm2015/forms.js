63ca5f1c03911b83dd2444411cfeaafe18f66c55
atscott <atscott01@gmail.com>
2021-07-02 22:34:53 +0000

    4c78984ad2 fix(compiler-cli): support reflecting namespace declarations (#42728)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+12.sha-e99af18
+ * @license Angular v12.2.0-next.1+15.sha-4c78984
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+12.sha-e99af18');
+const VERSION = new Version('12.2.0-next.1+15.sha-4c78984');
 
 /**
  * @license
