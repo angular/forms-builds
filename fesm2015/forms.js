7326ba39be40a1c228386ece9abb93072ca7d66d
atscott <atscott01@gmail.com>
2021-07-03 00:36:10 +0000

    c9b47f2182 fix(dev-infra): missing `pkg_npm` substitution for top-level targets being accessed (#42754)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+15.sha-4c78984
+ * @license Angular v12.2.0-next.1+16.sha-c9b47f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+15.sha-4c78984');
+const VERSION = new Version('12.2.0-next.1+16.sha-c9b47f2');
 
 /**
  * @license
