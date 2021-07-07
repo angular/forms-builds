643b99dcdc08cbb7a81a8ed35a47cdc11cf9ee20
Andrew Scott <atscott@google.com>
2021-07-07 20:57:16 +0000

    24ac37891c fix(docs-infra): slightly improve top-menu responsiveness (#42753)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+18.sha-e064f17
+ * @license Angular v12.2.0-next.1+19.sha-24ac378
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+18.sha-e064f17');
+const VERSION = new Version('12.2.0-next.1+19.sha-24ac378');
 
 /**
  * @license
