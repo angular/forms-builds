04ba289c11d0c8c251da946087a9b8213f6af63a
Andrew Scott <atscott@google.com>
2021-07-07 17:32:49 +0000

    0c0c32d539 docs: change misspelled function (#42742)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+16.sha-c9b47f2
+ * @license Angular v12.2.0-next.1+17.sha-0c0c32d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7127,7 +7127,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+16.sha-c9b47f2');
+const VERSION = new Version('12.2.0-next.1+17.sha-0c0c32d');
 
 /**
  * @license
