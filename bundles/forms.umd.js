e5779b04ffb35d1dbfc0d2905f7ed4c9982789eb
atscott <atscott01@gmail.com>
2021-07-08 22:26:11 +0000

    a8d5a31356 docs(core): initializeApp method should return function (#42743)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+41.sha-f6bce31
+ * @license Angular v12.1.1+43.sha-a8d5a31
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7338,7 +7338,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.1+41.sha-f6bce31');
+    var VERSION = new i0.Version('12.1.1+43.sha-a8d5a31');
 
     /**
      * @license
