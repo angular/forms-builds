83066d02e335f84edfff647a271afaed853f851d
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:26:40 +0000

    e8be045cbd refactor(core): optimize the implementation about finding context from error in ErrorHandler (#42581)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+63.sha-4c482bf
+ * @license Angular v12.2.0-next.1+67.sha-e8be045
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7131,7 +7131,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+63.sha-4c482bf');
+const VERSION = new Version('12.2.0-next.1+67.sha-e8be045');
 
 /**
  * @license
