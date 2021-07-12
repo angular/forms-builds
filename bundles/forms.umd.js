ab053b6dec62e503039179968056cab7e4361ec1
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:24:07 +0000

    91a576db20 refactor(core): optimize the implementation about finding context from error in ErrorHandler (#42581)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+55.sha-2ed5a2d
+ * @license Angular v12.1.1+60.sha-91a576d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7338,7 +7338,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.1+55.sha-2ed5a2d');
+    var VERSION = new i0.Version('12.1.1+60.sha-91a576d');
 
     /**
      * @license
