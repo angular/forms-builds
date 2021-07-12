be073d83f4318afb6a9551309998d649d74d1b0d
Andrew Kushnir <akushnir@google.com>
2021-07-12 20:42:31 +0000

    4f8a7280c1 docs: move start tutorial alert for clarity (#42764)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+60.sha-91a576d
+ * @license Angular v12.1.1+83.sha-4f8a728
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7338,7 +7338,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.1+60.sha-91a576d');
+    var VERSION = new i0.Version('12.1.1+83.sha-4f8a728');
 
     /**
      * @license
