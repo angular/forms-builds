04db87c62e4e79c21d176049f3850e6b828a436b
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:33:48 +0000

    64bb7cd704 docs: update incorrect bullet text (#42839)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+106.sha-0b429d3
+ * @license Angular v12.2.0-next.2+1.sha-64bb7cd
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7131,7 +7131,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+106.sha-0b429d3');
+const VERSION = new Version('12.2.0-next.2+1.sha-64bb7cd');
 
 /**
  * @license
