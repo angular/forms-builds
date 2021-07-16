a92891ab0ff16c9105f70e6fdaefb30361f669b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-16 17:24:04 +0000

    3da71467e9 build(docs-infra): upgrade Lighthouse to version 8.1.0 (#42846)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+13.sha-7422821
+ * @license Angular v12.2.0-next.2+16.sha-3da7146
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7131,7 +7131,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.2+13.sha-7422821');
+const VERSION = new Version('12.2.0-next.2+16.sha-3da7146');
 
 /**
  * @license
