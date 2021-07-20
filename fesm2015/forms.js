a2085a616c1ac4f2e2d86bc0237497879b40ed82
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:56:22 +0000

    722eb5dd45 fix(docs-infra): prevent main content from overlapping with ToC (#42884)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+35.sha-7271ad1
+ * @license Angular v12.2.0-next.2+38.sha-722eb5d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7131,7 +7131,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.2+35.sha-7271ad1');
+const VERSION = new Version('12.2.0-next.2+38.sha-722eb5d');
 
 /**
  * @license
