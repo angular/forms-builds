a48160c8811ab8c3c18b2423ab1052b5b3fc193e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-21 21:56:34 +0000

    b0854e2ec6 release: cut the v12.1.3 release (#42914)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+33.sha-29f5097
+ * @license Angular v12.1.2+34.sha-b0854e2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7338,7 +7338,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.2+33.sha-29f5097');
+    var VERSION = new i0.Version('12.1.2+34.sha-b0854e2');
 
     /**
      * @license
