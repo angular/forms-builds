9bb05be472d58dc6291709e7c0efc3f6be5b656f
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 23:03:48 +0000

    f589b01672 perf(compiler-cli): skip analysis in incremental builds for files without Angular behavior (#42562)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+34.sha-b0854e2
+ * @license Angular v12.1.3+5.sha-f589b01.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7338,7 +7338,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.2+34.sha-b0854e2');
+    var VERSION = new i0.Version('12.1.3+5.sha-f589b01.with-local-changes');
 
     /**
      * @license
