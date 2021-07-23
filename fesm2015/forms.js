e08da191766fdfa71df108b5d092531845a59f19
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:30:41 +0000

    8fc08c5ab1 ci: update pullapprove config to properly handle global approvals (#42929)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+71.sha-9073633.with-local-changes
+ * @license Angular v12.2.0-next.1+73.sha-c74927d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+71.sha-9073633.with-local-changes');
+const VERSION = new Version('12.2.0-next.1+73.sha-c74927d.with-local-changes');
 
 /**
  * @license
