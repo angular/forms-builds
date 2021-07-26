f6bfdb2a85e89f03dc3ebcc6b49c2a71867cd80c
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 20:38:35 +0000

    ed9cfb674f fix(compiler-cli): use correct module resolution context for absolute imports in .d.ts files (#42879)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+80.sha-22290af.with-local-changes
+ * @license Angular v12.2.0-next.1+81.sha-c13ccc3.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+80.sha-22290af.with-local-changes');
+const VERSION = new Version('12.2.0-next.1+81.sha-c13ccc3.with-local-changes');
 
 /**
  * @license
