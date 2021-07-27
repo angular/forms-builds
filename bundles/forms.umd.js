dc9efe41e5ac66735320c5be17680c6e8c981aa7
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 21:40:17 +0000

    5474f80f6a Revert "refactor(compiler-cli): add makeTemplateDiagnostic wrapper (#42937)" (#42971)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.1+55.sha-2ed5a2d.with-local-changes
+ * @license Angular v12.1.1+60.sha-91a576d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7338,7 +7338,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.1.1+55.sha-2ed5a2d.with-local-changes');
+    var VERSION = new i0.Version('12.1.1+60.sha-91a576d.with-local-changes');
 
     /**
      * @license
