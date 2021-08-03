a886ca5835dcc3258592606d5ac927fc65dddd98
atscott <atscott01@gmail.com>
2021-08-03 00:30:32 +0000

    aace1e71d8 fix(language-service): global autocomplete doesn't work when the user tries to modify the symbol (#42923)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.2+8.sha-c556128.with-local-changes
+ * @license Angular v12.2.0-next.2+9.sha-ea93672.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7490,7 +7490,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0-next.2+8.sha-c556128.with-local-changes');
+    var VERSION = new i0.Version('12.2.0-next.2+9.sha-ea93672.with-local-changes');
 
     /**
      * @license
