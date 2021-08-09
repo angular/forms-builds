c5a815746fc3c98593ad676aee7bb66593a5506b
Andrew Kushnir <akushnir@google.com>
2021-08-09 19:45:40 +0000

    8a3d2aad30 fix(docs-infra): correctly detect chunk load errors (#43088)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+8.sha-dd3abdb.with-local-changes
+ * @license Angular v12.2.0+9.sha-8a3d2aa.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7490,7 +7490,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0+8.sha-dd3abdb.with-local-changes');
+    var VERSION = new i0.Version('12.2.0+9.sha-8a3d2aa.with-local-changes');
 
     /**
      * @license
