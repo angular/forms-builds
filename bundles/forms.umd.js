617f0705471d99d889b468e32d8c17cbd4cbc3f8
Andrew Kushnir <akushnir@google.com>
2021-08-10 18:03:44 +0000

    7da3570be8 build(dev-infra): exlude commit message filter from formatting (#43091)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+9.sha-8a3d2aa.with-local-changes
+ * @license Angular v12.2.0+12.sha-7da3570.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7490,7 +7490,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.0+9.sha-8a3d2aa.with-local-changes');
+    var VERSION = new i0.Version('12.2.0+12.sha-7da3570.with-local-changes');
 
     /**
      * @license
--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0+9.sha-8a3d2aa.with-local-changes
+ * @license Angular v12.2.0+12.sha-7da3570.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6964,7 +6964,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0+9.sha-8a3d2aa.with-local-changes');
+const VERSION = new Version('12.2.0+12.sha-7da3570.with-local-changes');
 
 /**
  * @license
