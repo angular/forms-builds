3ea512f43efab936292dcc6d7d12790bf7b377a9
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 20:30:51 +0000

    cd10a4cc13 test(compiler): add a test for parsing multiline expressions in attributes (#43129)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.1+12.sha-83fa3ee.with-local-changes
+ * @license Angular v12.2.1+19.sha-cd10a4c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7490,7 +7490,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.1+12.sha-83fa3ee.with-local-changes');
+    var VERSION = new i0.Version('12.2.1+19.sha-cd10a4c.with-local-changes');
 
     /**
      * @license
