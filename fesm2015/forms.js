2d7196880bd164aa62d29cc663b0c9ccbfa6b67f
GitHub <noreply@github.com>
2021-08-18 18:11:04 +0000

    edde058e5e docs: release notes for the v12.2.2 release (#43196)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.1+41.sha-c366d1c.with-local-changes
+ * @license Angular v13.0.0-next.1+42.sha-edde058.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7283,7 +7283,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.1+41.sha-c366d1c.with-local-changes');
+const VERSION = new Version('13.0.0-next.1+42.sha-edde058.with-local-changes');
 
 /**
  * @license
