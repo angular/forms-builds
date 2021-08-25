3fd9b706b4f3f72fcb262747aba2d34b309aca1f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 21:40:33 +0000

    b8183614f5 release: cut the v12.2.3 release (#43257)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.2+16.sha-b46ce7d.with-local-changes
+ * @license Angular v12.2.2+17.sha-b818361.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6964,7 +6964,7 @@ FormBuilder.decorators = [
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.2+16.sha-b46ce7d.with-local-changes');
+const VERSION = new Version('12.2.2+17.sha-b818361.with-local-changes');
 
 /**
  * @license
