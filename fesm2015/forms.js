fd8a59098ab67a2d6f30b1acffef54bce033be05
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-30 20:57:42 +0000

    dcfabf08af docs: add step to declare `ProductAlertsComponent` in tutorial (#43229)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.3+18.sha-f3c8408.with-local-changes
+ * @license Angular v13.0.0-next.3+19.sha-dcfabf0.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7274,7 +7274,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.3+18.sha-f3c8408.with-local-changes');
+const VERSION = new Version('13.0.0-next.3+19.sha-dcfabf0.with-local-changes');
 
 /**
  * @license
