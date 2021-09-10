5d680f007716980f2f375052e5d30875576fa091
Andrew Kushnir <akushnir@google.com>
2021-09-10 16:43:22 +0000

    578aba1939 docs: add ngUpgrade maintenance timeline (#43379)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.5+27.sha-df177d9.with-local-changes
+ * @license Angular v13.0.0-next.5+28.sha-578aba1.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7274,7 +7274,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.5+27.sha-df177d9.with-local-changes');
+const VERSION = new Version('13.0.0-next.5+28.sha-578aba1.with-local-changes');
 
 /**
  * @license
