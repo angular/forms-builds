74faf742da23d6920f4c14e1669849079a1a390d
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 20:41:00 +0000

    79700822fd refactor(router): remove unnecessary getTransition function (#43436)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.6+27.sha-e55d052.with-local-changes
+ * @license Angular v13.0.0-next.6+28.sha-7970082.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7274,7 +7274,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.6+27.sha-e55d052.with-local-changes');
+const VERSION = new Version('13.0.0-next.6+28.sha-7970082.with-local-changes');
 
 /**
  * @license
