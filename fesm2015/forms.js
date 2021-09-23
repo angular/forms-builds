37f43bd3949f97f1504fbdd89bc3ff1abc339df8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 22:11:30 +0000

    ea61ec2562 feat(core): support TypeScript 4.4 (#43281)

--- fesm2015/forms.js
+++ fesm2015/forms.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v13.0.0-next.7+10.sha-cc427d1.with-local-changes
+ * @license Angular v13.0.0-next.7+11.sha-ea61ec2.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7274,7 +7274,7 @@ FormBuilder.ɵprov = /*@__PURE__*/ ɵɵdefineInjectable({ token: FormBuilder, fa
 /**
  * @publicApi
  */
-const VERSION = new Version('13.0.0-next.7+10.sha-cc427d1.with-local-changes');
+const VERSION = new Version('13.0.0-next.7+11.sha-ea61ec2.with-local-changes');
 
 /**
  * @license
