aa85aab1c9b2f056964588c6202b4026dc2bbb99
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-30 00:01:32 +0000

    cadef4caa8 ci: correctly handle commit message with carriage returns in `payload-size.sh` (#43569)

--- bundles/forms.umd.js
+++ bundles/forms.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.7+9.sha-2306a32.with-local-changes
+ * @license Angular v12.2.7+32.sha-cadef4c.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7475,7 +7475,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('12.2.7+9.sha-2306a32.with-local-changes');
+    var VERSION = new i0.Version('12.2.7+32.sha-cadef4c.with-local-changes');
 
     /**
      * @license
