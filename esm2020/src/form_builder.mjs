13fa8db3460f168779b11adbd8e3cc422c0c1fe4
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:07:36 +0000

    ec0a0e0669 feat(service-worker): add `cacheOpaqueResponses` option for data-groups (#44723)

--- esm2020/src/form_builder.mjs
+++ esm2020/src/form_builder.mjs
@@ -115,9 +115,9 @@ export class FormBuilder {
         }
     }
 }
-FormBuilder.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.2.0-next.2+58.sha-a534a78.with-local-changes", ngImport: i0, type: FormBuilder, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-FormBuilder.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.2.0-next.2+58.sha-a534a78.with-local-changes", ngImport: i0, type: FormBuilder, providedIn: ReactiveFormsModule });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.2.0-next.2+58.sha-a534a78.with-local-changes", ngImport: i0, type: FormBuilder, decorators: [{
+FormBuilder.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.2.0-next.2+62.sha-ec0a0e0.with-local-changes", ngImport: i0, type: FormBuilder, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+FormBuilder.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.2.0-next.2+62.sha-ec0a0e0.with-local-changes", ngImport: i0, type: FormBuilder, providedIn: ReactiveFormsModule });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.2.0-next.2+62.sha-ec0a0e0.with-local-changes", ngImport: i0, type: FormBuilder, decorators: [{
             type: Injectable,
             args: [{ providedIn: ReactiveFormsModule }]
         }] });
