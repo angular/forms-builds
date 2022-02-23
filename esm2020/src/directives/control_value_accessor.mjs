e18b6b81f8037ac123990a4789b2c78030a2718e
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:14:43 +0000

    f8dc660605 fix(animations): allow animations with unsupported CSS properties (#44729)

--- esm2020/src/directives/control_value_accessor.mjs
+++ esm2020/src/directives/control_value_accessor.mjs
@@ -60,9 +60,9 @@ export class BaseControlValueAccessor {
         this.setProperty('disabled', isDisabled);
     }
 }
-BaseControlValueAccessor.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.3+18.sha-4fa5307.with-local-changes", ngImport: i0, type: BaseControlValueAccessor, deps: [{ token: i0.Renderer2 }, { token: i0.ElementRef }], target: i0.ɵɵFactoryTarget.Directive });
-BaseControlValueAccessor.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "12.0.0", version: "14.0.0-next.3+18.sha-4fa5307.with-local-changes", type: BaseControlValueAccessor, ngImport: i0 });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.3+18.sha-4fa5307.with-local-changes", ngImport: i0, type: BaseControlValueAccessor, decorators: [{
+BaseControlValueAccessor.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.3+20.sha-f8dc660.with-local-changes", ngImport: i0, type: BaseControlValueAccessor, deps: [{ token: i0.Renderer2 }, { token: i0.ElementRef }], target: i0.ɵɵFactoryTarget.Directive });
+BaseControlValueAccessor.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "12.0.0", version: "14.0.0-next.3+20.sha-f8dc660.with-local-changes", type: BaseControlValueAccessor, ngImport: i0 });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.3+20.sha-f8dc660.with-local-changes", ngImport: i0, type: BaseControlValueAccessor, decorators: [{
             type: Directive
         }], ctorParameters: function () { return [{ type: i0.Renderer2 }, { type: i0.ElementRef }]; } });
 /**
@@ -76,9 +76,9 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.3+18
  */
 export class BuiltInControlValueAccessor extends BaseControlValueAccessor {
 }
-BuiltInControlValueAccessor.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.3+18.sha-4fa5307.with-local-changes", ngImport: i0, type: BuiltInControlValueAccessor, deps: null, target: i0.ɵɵFactoryTarget.Directive });
-BuiltInControlValueAccessor.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "12.0.0", version: "14.0.0-next.3+18.sha-4fa5307.with-local-changes", type: BuiltInControlValueAccessor, usesInheritance: true, ngImport: i0 });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.3+18.sha-4fa5307.with-local-changes", ngImport: i0, type: BuiltInControlValueAccessor, decorators: [{
+BuiltInControlValueAccessor.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "14.0.0-next.3+20.sha-f8dc660.with-local-changes", ngImport: i0, type: BuiltInControlValueAccessor, deps: null, target: i0.ɵɵFactoryTarget.Directive });
+BuiltInControlValueAccessor.ɵdir = i0.ɵɵngDeclareDirective({ minVersion: "12.0.0", version: "14.0.0-next.3+20.sha-f8dc660.with-local-changes", type: BuiltInControlValueAccessor, usesInheritance: true, ngImport: i0 });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "14.0.0-next.3+20.sha-f8dc660.with-local-changes", ngImport: i0, type: BuiltInControlValueAccessor, decorators: [{
             type: Directive
         }] });
 /**
