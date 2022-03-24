acf968092ecdf7e9bf4602689cfd8bb5927d2d1c
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:13:07 +0000

    b481eab39d fix(docs-infra): update (and unpin) dependency versions for preview server (#45390)

--- esm2020/src/form_builder.mjs
+++ esm2020/src/form_builder.mjs
@@ -115,9 +115,9 @@ export class FormBuilder {
         }
     }
 }
-FormBuilder.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.3.0+6.sha-ee20410", ngImport: i0, type: FormBuilder, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-FormBuilder.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.3.0+6.sha-ee20410", ngImport: i0, type: FormBuilder, providedIn: ReactiveFormsModule });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.3.0+6.sha-ee20410", ngImport: i0, type: FormBuilder, decorators: [{
+FormBuilder.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.3.0+11.sha-b481eab", ngImport: i0, type: FormBuilder, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+FormBuilder.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.3.0+11.sha-b481eab", ngImport: i0, type: FormBuilder, providedIn: ReactiveFormsModule });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.3.0+11.sha-b481eab", ngImport: i0, type: FormBuilder, decorators: [{
             type: Injectable,
             args: [{ providedIn: ReactiveFormsModule }]
         }] });
