3778737907b05df02b5f31daefe1026659addc00
Dylan Hunn <dylhunn@gmail.com>
2022-03-25 21:04:32 +0000

    88dbeda931 build: shorten partial compilation test case target names for windows (#45443)

--- esm2020/src/form_builder.mjs
+++ esm2020/src/form_builder.mjs
@@ -115,9 +115,9 @@ export class FormBuilder {
         }
     }
 }
-FormBuilder.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.3.0+14.sha-f869b91", ngImport: i0, type: FormBuilder, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-FormBuilder.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.3.0+14.sha-f869b91", ngImport: i0, type: FormBuilder, providedIn: ReactiveFormsModule });
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.3.0+14.sha-f869b91", ngImport: i0, type: FormBuilder, decorators: [{
+FormBuilder.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "13.3.0+28.sha-88dbeda", ngImport: i0, type: FormBuilder, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+FormBuilder.ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "13.3.0+28.sha-88dbeda", ngImport: i0, type: FormBuilder, providedIn: ReactiveFormsModule });
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "13.3.0+28.sha-88dbeda", ngImport: i0, type: FormBuilder, decorators: [{
             type: Injectable,
             args: [{ providedIn: ReactiveFormsModule }]
         }] });
