9ba7fa05aac4e4fd0f3471e0c8409fb7f3fff8d1
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 18:06:01 +0000

    c0ca3fc71a fix(forms): expose ControlConfig in public API (#46594)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v14.1.0-next.4+sha-a6d5fe2
+ * @license Angular v14.1.0-next.4+sha-c0ca3fc
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1108,7 +1108,7 @@ export declare const COMPOSITION_BUFFER_MODE: InjectionToken<boolean>;
  *
  * @publicApi
  */
-declare type ControlConfig<T> = [T | FormControlState<T>, (ValidatorFn | (ValidatorFn[]))?, (AsyncValidatorFn | AsyncValidatorFn[])?];
+export declare type ControlConfig<T> = [T | FormControlState<T>, (ValidatorFn | (ValidatorFn[]))?, (AsyncValidatorFn | AsyncValidatorFn[])?];
 
 /**
  * @description
