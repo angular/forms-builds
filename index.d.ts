ba0989f28f242736ed04f95959461946497e8256
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:55:57 +0000

    b342e55509 fix(forms): don't mutate validators array (#47830)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.0+sha-2551c78
+ * @license Angular v15.0.0+sha-b342e55
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -626,6 +626,18 @@ export declare abstract class AbstractControl<TValue = any, TRawValue extends TV
      */
     get root(): AbstractControl;
     private _calculateStatus;
+    /**
+     * Internal implementation of the `setValidators` method. Needs to be separated out into a
+     * different method, because it is called in the constructor and it can break cases where
+     * a control is extended.
+     */
+    private _assignValidators;
+    /**
+     * Internal implementation of the `setAsyncValidators` method. Needs to be separated out into a
+     * different method, because it is called in the constructor and it can break cases where
+     * a control is extended.
+     */
+    private _assignAsyncValidators;
 }
 
 /**
