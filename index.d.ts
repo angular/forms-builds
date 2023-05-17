ae498dba43b44f33c92e1e50b4bf3b1305c94ed6
Andrew Kushnir <akushnir@google.com>
2023-05-17 15:33:20 +0000

    656c9537f0 docs: remove plnkr link from markForCheck example (#50328)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.1+sha-3eafb4b
+ * @license Angular v16.0.1+sha-656c953
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3913,8 +3913,8 @@ declare const ngGroupStatusHost: {
 
 /**
  * @description
- * Creates a `FormControl` instance from a domain model and binds it
- * to a form control element.
+ * Creates a `FormControl` instance from a [domain
+ * model](https://en.wikipedia.org/wiki/Domain_model) and binds it to a form control element.
  *
  * The `FormControl` instance tracks the value, user interaction, and
  * validation status of the control and keeps the view synced with the model. If used
