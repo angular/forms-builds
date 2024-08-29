9c41781f404a0cda193c0a405c69fd48260d0c1d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-29 15:56:18 +0000

    5e9661d fix(docs-infra): Fix scrolling in application (#57554)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.2+sha-d9c1004
+ * @license Angular v18.2.1+sha-5e9661d
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1354,6 +1354,7 @@ declare const DEFAULT_VALUE_ACCESSOR: Provider;
  * elements. The accessor is used by the `FormControlDirective`, `FormControlName`, and
  * `NgModel` directives.
  *
+ * {@searchKeywords ngDefaultControl}
  *
  * @usageNotes
  *
