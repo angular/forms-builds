224c7bc87d886b2b560e9c8153d81b377765b4e4
Andrew Kushnir <akushnir@google.com>
2024-08-29 17:23:42 +0000

    93bdbbc docs(docs-infra): Add dev-mode only mention for `core/global` (#57365)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.2.1+sha-5e9661d
+ * @license Angular v18.2.2+sha-93bdbbc
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1354,7 +1354,6 @@ declare const DEFAULT_VALUE_ACCESSOR: Provider;
  * elements. The accessor is used by the `FormControlDirective`, `FormControlName`, and
  * `NgModel` directives.
  *
- * {@searchKeywords ngDefaultControl}
  *
  * @usageNotes
  *
