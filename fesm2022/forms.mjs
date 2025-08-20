a064c1e302427900ba86e09bbce5843cd5a0e3c7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:10:32 +0000

    416878b build: update cross-repo angular dependencies (#63265)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-f9d73cc
+ * @license Angular v20.2.0-rc.1+sha-416878b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-f9d73cc');
+const VERSION = new Version('20.2.0-rc.1+sha-416878b');
 
 /**
  * Exports the required providers and directives for template-driven forms,
