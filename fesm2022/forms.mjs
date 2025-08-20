b4dbbad953677ec77b739b88fef8e18e6c502b39
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:48:14 +0000

    eeeaadc fix(language-service): Support to resolve the re-export component. (#62585)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-6a6cb01
+ * @license Angular v21.0.0-next.0+sha-eeeaadc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-6a6cb01');
+const VERSION = new Version('21.0.0-next.0+sha-eeeaadc');
 
 /**
  * Exports the required providers and directives for template-driven forms,
