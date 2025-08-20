117a35acd7cb6aaaac6cb74ca97738808f6a4a42
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:35:48 +0000

    878b99a fix(compiler-cli): account for expression with type arguments during HMR extraction (#63261)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.1+sha-9655ea7
+ * @license Angular v20.2.0-rc.1+sha-878b99a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.1+sha-9655ea7');
+const VERSION = new Version('20.2.0-rc.1+sha-878b99a');
 
 /**
  * Exports the required providers and directives for template-driven forms,
