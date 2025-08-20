76b346b988ce97a133d0e0e5986aa7e567df81ad
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 13:30:44 +0000

    25f593c refactor(common): remove`ngModuleFactory` input of `NgComponentOutlet` (#62838)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-4dfa6f1
+ * @license Angular v21.0.0-next.0+sha-25f593c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-4dfa6f1');
+const VERSION = new Version('21.0.0-next.0+sha-25f593c');
 
 /**
  * Exports the required providers and directives for template-driven forms,
