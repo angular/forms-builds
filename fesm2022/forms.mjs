8cb9ed0e9ae97f55a3c73a2526c7a19e9b0966d8
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-08-21 10:56:36 +0000

    1c27fa9 release: cut the v20.2.1 release

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-a28672f
+ * @license Angular v20.2.1+sha-1c27fa9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0+sha-a28672f');
+const VERSION = new Version('20.2.1+sha-1c27fa9');
 
 /**
  * Exports the required providers and directives for template-driven forms,
