8239b33ce63b21d8e0995f8a1923f6106a20daaf
Andrew Kushnir <akushnir@google.com>
2025-08-22 21:50:27 +0000

    10b0927 refactor: various build and import specificer fixes for strict deps (#63324)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-e1481d8
+ * @license Angular v20.2.1+sha-10b0927
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-e1481d8');
+const VERSION = new Version('20.2.1+sha-10b0927');
 
 /**
  * Exports the required providers and directives for template-driven forms,
