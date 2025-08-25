10419535adbfb9e5b36f7ee9f628a45377d32eec
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:39:32 +0000

    fca6122 build: lock file maintenance (#63359)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5a11106
+ * @license Angular v21.0.0-next.0+sha-fca6122
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7742,7 +7742,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5a11106');
+const VERSION = new Version('21.0.0-next.0+sha-fca6122');
 
 /**
  * Exports the required providers and directives for template-driven forms,
