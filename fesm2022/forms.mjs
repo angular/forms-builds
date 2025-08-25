31225fc25317fdbca5eaaa4aaf4b48c49edb8de4
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:44:54 +0000

    d025a07 build: update all non-major dependencies (#63344)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-5af3222
+ * @license Angular v21.0.0-next.0+sha-d025a07
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7742,7 +7742,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-5af3222');
+const VERSION = new Version('21.0.0-next.0+sha-d025a07');
 
 /**
  * Exports the required providers and directives for template-driven forms,
