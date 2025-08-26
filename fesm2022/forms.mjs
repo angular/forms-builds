0b7cae07b19bd405b745b926f97baf110be23648
Andrew Kushnir <akushnir@google.com>
2025-08-26 21:57:17 +0000

    a43057c refactor(core): Create a base effect interface and prototype to be used by both angular and wiz. (#62931)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-dfa2044
+ * @license Angular v21.0.0-next.0+sha-a43057c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7742,7 +7742,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-dfa2044');
+const VERSION = new Version('21.0.0-next.0+sha-a43057c');
 
 /**
  * Exports the required providers and directives for template-driven forms,
