bef912d19ec031b9114f76f5b75e3dfe3f58f028
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:34:02 +0000

    3a3bd36 docs: update to new standalone default behavior (#63329)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-f868465
+ * @license Angular v21.0.0-next.1+sha-3a3bd36
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7742,7 +7742,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-f868465');
+const VERSION = new Version('21.0.0-next.1+sha-3a3bd36');
 
 /**
  * Exports the required providers and directives for template-driven forms,
