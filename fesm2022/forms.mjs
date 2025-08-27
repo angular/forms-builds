5abba9ddbc41bb6befb6dd282119fd9d13512ce8
Miles Malerba <mmalerba@google.com>
2025-08-27 23:59:58 +0000

    a6a46e9 docs(docs-infra): tooltip on hover and message after copied (#63167)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-9de2f07
+ * @license Angular v20.2.2+sha-a6a46e9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-9de2f07');
+const VERSION = new Version('20.2.2+sha-a6a46e9');
 
 /**
  * Exports the required providers and directives for template-driven forms,
