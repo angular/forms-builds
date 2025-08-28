9efe564e20af49fe42b69740f973124968fe883d
Miles Malerba <mmalerba@google.com>
2025-08-28 16:25:50 +0000

    98468c0 build: update cross-repo angular dependencies (#63444)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-cef1449
+ * @license Angular v20.2.2+sha-98468c0
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-cef1449');
+const VERSION = new Version('20.2.2+sha-98468c0');
 
 /**
  * Exports the required providers and directives for template-driven forms,
