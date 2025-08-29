1e3193f371648b4cdae591826949d69cb4c9a941
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-08-29 12:38:20 +0000

    1875d5c docs: release notes for the v20.2.3 release

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-0a82138
+ * @license Angular v21.0.0-next.1+sha-1875d5c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7742,7 +7742,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-0a82138');
+const VERSION = new Version('21.0.0-next.1+sha-1875d5c');
 
 /**
  * Exports the required providers and directives for template-driven forms,
