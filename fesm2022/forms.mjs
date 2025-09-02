fd3966cc2fef7861eba77f64ac7fb6046e3ca3b9
Miles Malerba <mmalerba@google.com>
2025-09-02 16:48:07 +0000

    d6f7f68 docs(docs-infra): signal property should be readonly (#63166)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-3b42314
+ * @license Angular v20.2.3+sha-d6f7f68
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-3b42314');
+const VERSION = new Version('20.2.3+sha-d6f7f68');
 
 /**
  * Exports the required providers and directives for template-driven forms,
