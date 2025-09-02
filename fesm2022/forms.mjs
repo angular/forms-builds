60b27f7b55b5e9f9713c0f731bf71dce05273bc7
Miles Malerba <mmalerba@google.com>
2025-09-02 16:37:03 +0000

    15fa2ce docs(docs-infra): apply migration script changes (#63514)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-dddce83
+ * @license Angular v20.2.3+sha-15fa2ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-dddce83');
+const VERSION = new Version('20.2.3+sha-15fa2ce');
 
 /**
  * Exports the required providers and directives for template-driven forms,
