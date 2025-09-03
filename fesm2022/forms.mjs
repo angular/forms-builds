27039263e41fd3fb1e37bfa78a40106452e1d548
Andrew Scott <atscott@google.com>
2025-09-03 16:55:53 +0000

    320f1d6 build: update @angular/ng-dev digest to 435c59b (#63571)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-991785f
+ * @license Angular v20.2.3+sha-320f1d6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-991785f');
+const VERSION = new Version('20.2.3+sha-320f1d6');
 
 /**
  * Exports the required providers and directives for template-driven forms,
