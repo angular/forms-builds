65d025cfc9e9c46edb8003e4f7762d84168d14bb
Andrew Scott <atscott@google.com>
2025-09-04 22:29:38 +0000

    f697995 docs(docs-infra): allow collapse code example (#63559)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-e1dd3b1
+ * @license Angular v20.2.4+sha-f697995
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-e1dd3b1');
+const VERSION = new Version('20.2.4+sha-f697995');
 
 /**
  * Exports the required providers and directives for template-driven forms,
