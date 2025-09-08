7aa30d095eb0a2b7ed4eda06aba9586afd3b8df7
Andrew Scott <atscott@google.com>
2025-09-08 19:13:55 +0000

    d124fba docs: improve headings of docs (#63604)

--- fesm2022/forms.mjs
+++ fesm2022/forms.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.4+sha-a6640e3
+ * @license Angular v20.2.4+sha-d124fba
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7651,7 +7651,7 @@ i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.2.0-next.2", 
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.4+sha-a6640e3');
+const VERSION = new Version('20.2.4+sha-d124fba');
 
 /**
  * Exports the required providers and directives for template-driven forms,
