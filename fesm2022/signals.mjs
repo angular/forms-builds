6b4aaee74729905b873380c7880c3f04410cc224
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 16:00:10 +0000

    d1d5c72 docs(docs-infra): allow cors for json files

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-4f21ea7
+ * @license Angular v21.0.0-next.9+sha-d1d5c72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -930,7 +930,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-next.9+sha-4f21ea7",
+    version: "21.0.0-next.9+sha-d1d5c72",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -938,7 +938,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-next.9+sha-4f21ea7",
+    version: "21.0.0-next.9+sha-d1d5c72",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -963,7 +963,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-next.9+sha-4f21ea7",
+  version: "21.0.0-next.9+sha-d1d5c72",
   ngImport: i0,
   type: Field,
   decorators: [{
