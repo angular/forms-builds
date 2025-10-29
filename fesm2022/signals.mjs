d7deaaf8d0c43866834420c434e4e85056050655
Andrew Scott <atscott01@gmail.com>
2025-10-29 21:02:03 +0000

    fb569ef refactor(core): Move patch toggles inside functions to allow tree shaking

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-cfba831
+ * @license Angular v21.1.0-next.0+sha-fb569ef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-cfba831",
+    version: "21.1.0-next.0+sha-fb569ef",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-cfba831",
+    version: "21.1.0-next.0+sha-fb569ef",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-cfba831",
+  version: "21.1.0-next.0+sha-fb569ef",
   ngImport: i0,
   type: Field,
   decorators: [{
