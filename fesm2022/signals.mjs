12f58cc04c02e7df49976ab2c9548df384c952c5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-29 11:42:25 +0000

    aaa3020 refactor(devtools): Improves error messaging for profiler file import

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-64cb085
+ * @license Angular v21.1.0-next.0+sha-aaa3020
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-64cb085",
+    version: "21.1.0-next.0+sha-aaa3020",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-64cb085",
+    version: "21.1.0-next.0+sha-aaa3020",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-64cb085",
+  version: "21.1.0-next.0+sha-aaa3020",
   ngImport: i0,
   type: Field,
   decorators: [{
