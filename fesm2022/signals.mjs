d4fb9a6781e2bb99754a7d0699e12ae434c5ea49
Andrew Scott <atscott01@gmail.com>
2025-11-04 23:02:04 +0000

    b5e37ff docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b7839e8
+ * @license Angular v21.1.0-next.0+sha-b5e37ff
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-b7839e8",
+    version: "21.1.0-next.0+sha-b5e37ff",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-b7839e8",
+    version: "21.1.0-next.0+sha-b5e37ff",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-b7839e8",
+  version: "21.1.0-next.0+sha-b5e37ff",
   ngImport: i0,
   type: Field,
   decorators: [{
