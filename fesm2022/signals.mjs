fbf2801dbcc890da69c8df18534639f4b71a3c5f
GitHub <noreply@github.com>
2025-11-05 19:59:46 +0000

    e1dfe32 build: add VSCode extension release script

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e18aac8
+ * @license Angular v21.1.0-next.0+sha-e1dfe32
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-e18aac8",
+    version: "21.1.0-next.0+sha-e1dfe32",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-e18aac8",
+    version: "21.1.0-next.0+sha-e1dfe32",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-e18aac8",
+  version: "21.1.0-next.0+sha-e1dfe32",
   ngImport: i0,
   type: Field,
   decorators: [{
