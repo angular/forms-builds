bf655a88c4b74cce2ed7a99c4ac650b6cd6cabf1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 21:26:08 +0000

    86a9feb build: update cross-repo angular dependencies to v21.0.0-rc.1

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-dcfd7fe
+ * @license Angular v21.0.0-rc.1+sha-86a9feb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.1+sha-dcfd7fe",
+    version: "21.0.0-rc.1+sha-86a9feb",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-rc.1+sha-dcfd7fe",
+    version: "21.0.0-rc.1+sha-86a9feb",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.1+sha-dcfd7fe",
+  version: "21.0.0-rc.1+sha-86a9feb",
   ngImport: i0,
   type: Field,
   decorators: [{
