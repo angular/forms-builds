e87f0ed4c8741d11997f281a775c134e31c1b9d9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 20:12:57 +0000

    70d8ed4 build: restrain visibility to compiler-cli subpackages (#64732)

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3085915
+ * @license Angular v21.1.0-next.0+sha-70d8ed4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-3085915",
+    version: "21.1.0-next.0+sha-70d8ed4",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-3085915",
+    version: "21.1.0-next.0+sha-70d8ed4",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-3085915",
+  version: "21.1.0-next.0+sha-70d8ed4",
   ngImport: i0,
   type: Field,
   decorators: [{
