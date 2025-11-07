f2672a2476da447d865090c85e373be8e77f859e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 23:32:50 +0000

    d2b854b docs(core): add a11y considerations related to `@defer()`

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-1651908
+ * @license Angular v21.1.0-next.0+sha-d2b854b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1063,7 +1063,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-1651908",
+    version: "21.1.0-next.0+sha-d2b854b",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1071,7 +1071,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-1651908",
+    version: "21.1.0-next.0+sha-d2b854b",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1096,7 +1096,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-1651908",
+  version: "21.1.0-next.0+sha-d2b854b",
   ngImport: i0,
   type: Field,
   decorators: [{
