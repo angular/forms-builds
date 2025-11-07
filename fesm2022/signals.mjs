fec5de9abc9ae243b903e85e85b672c7709cfe39
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 18:17:58 +0000

    dff8035 docs(docs-infra): refactor update to use host property and refactor isWindows

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0efb3f6
+ * @license Angular v21.1.0-next.0+sha-dff8035
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1084,7 +1084,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-0efb3f6",
+    version: "21.1.0-next.0+sha-dff8035",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1092,7 +1092,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-0efb3f6",
+    version: "21.1.0-next.0+sha-dff8035",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1117,7 +1117,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-0efb3f6",
+  version: "21.1.0-next.0+sha-dff8035",
   ngImport: i0,
   type: Field,
   decorators: [{
