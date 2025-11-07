ecccb1c00cd9a63943c0e1b05925e502aeb7e1fc
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-07 15:52:56 +0000

    0efb3f6 refactor(forms): nicer type errors

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ff3013c
+ * @license Angular v21.1.0-next.0+sha-0efb3f6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1084,7 +1084,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-ff3013c",
+    version: "21.1.0-next.0+sha-0efb3f6",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1092,7 +1092,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-ff3013c",
+    version: "21.1.0-next.0+sha-0efb3f6",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1117,7 +1117,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-ff3013c",
+  version: "21.1.0-next.0+sha-0efb3f6",
   ngImport: i0,
   type: Field,
   decorators: [{
