5faf7a44d5768c13d6808c8e8ec27b190bce4fbd
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 18:54:01 +0000

    74dca6e build: lock file maintenance

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3b1fbf0
+ * @license Angular v21.1.0-next.0+sha-74dca6e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1063,7 +1063,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-3b1fbf0",
+    version: "21.1.0-next.0+sha-74dca6e",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1071,7 +1071,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-3b1fbf0",
+    version: "21.1.0-next.0+sha-74dca6e",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1096,7 +1096,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-3b1fbf0",
+  version: "21.1.0-next.0+sha-74dca6e",
   ngImport: i0,
   type: Field,
   decorators: [{
