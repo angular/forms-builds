97d2d575a181e75707d08a28808115e46c8846b2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 23:07:11 +0000

    31ca56a docs: Fixed mermaid diagram node label contrast in dark mode

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-be37129
+ * @license Angular v21.0.0-rc.2+sha-31ca56a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1078,7 +1078,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-be37129",
+    version: "21.0.0-rc.2+sha-31ca56a",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1086,7 +1086,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-rc.2+sha-be37129",
+    version: "21.0.0-rc.2+sha-31ca56a",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1111,7 +1111,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-be37129",
+  version: "21.0.0-rc.2+sha-31ca56a",
   ngImport: i0,
   type: Field,
   decorators: [{
