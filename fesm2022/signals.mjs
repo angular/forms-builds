d59ed786da90d877190087251f90cd724d4ac8b4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 17:40:46 +0000

    f737a9d docs(docs-infra): Add support for metadata on fenced code blocks

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3f80ae7
+ * @license Angular v21.1.0-next.0+sha-f737a9d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1078,7 +1078,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-3f80ae7",
+    version: "21.1.0-next.0+sha-f737a9d",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1086,7 +1086,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-3f80ae7",
+    version: "21.1.0-next.0+sha-f737a9d",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1111,7 +1111,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-3f80ae7",
+  version: "21.1.0-next.0+sha-f737a9d",
   ngImport: i0,
   type: Field,
   decorators: [{
