bc144b0218be82d8fcd4c9bc06665fd98838a29f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:08:34 +0000

    482e137 docs(docs-infra): improve theme picker accessibility and add animations

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-16d51f6
+ * @license Angular v21.0.0-rc.2+sha-482e137
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-16d51f6",
+    version: "21.0.0-rc.2+sha-482e137",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-rc.2+sha-16d51f6",
+    version: "21.0.0-rc.2+sha-482e137",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-16d51f6",
+  version: "21.0.0-rc.2+sha-482e137",
   ngImport: i0,
   type: Field,
   decorators: [{
