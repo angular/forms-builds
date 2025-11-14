dbaf7aebc8b25c25507f363cde54a82840073326
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 23:34:20 +0000

    a315afb docs: add formArray clear() example clarifies array manipulation

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-e1a7c35
+ * @license Angular v21.0.0-rc.2+sha-a315afb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-e1a7c35",
+    version: "21.0.0-rc.2+sha-a315afb",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-rc.2+sha-e1a7c35",
+    version: "21.0.0-rc.2+sha-a315afb",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-e1a7c35",
+  version: "21.0.0-rc.2+sha-a315afb",
   ngImport: i0,
   type: Field,
   decorators: [{
