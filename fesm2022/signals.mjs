484c156a745949b910fcb05432f92512b6e60921
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:05:49 +0000

    c2a52fd docs: add aria toolbar component guide

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.2+sha-afeba79
+ * @license Angular v21.0.0-rc.2+sha-c2a52fd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1081,7 +1081,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.2+sha-afeba79",
+    version: "21.0.0-rc.2+sha-c2a52fd",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -1089,7 +1089,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-rc.2+sha-afeba79",
+    version: "21.0.0-rc.2+sha-c2a52fd",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -1114,7 +1114,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.2+sha-afeba79",
+  version: "21.0.0-rc.2+sha-c2a52fd",
   ngImport: i0,
   type: Field,
   decorators: [{
