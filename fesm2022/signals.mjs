1de339e23f94f14ea8c58b4bb84f66985cc45de7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 23:13:09 +0000

    9723748 docs: replace @HostListener with host event bindings in attribute directives

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-91de094
+ * @license Angular v21.0.0-rc.3+sha-9723748
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-91de094",
+    version: "21.0.0-rc.3+sha-9723748",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-rc.3+sha-91de094",
+    version: "21.0.0-rc.3+sha-9723748",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.3+sha-91de094",
+  version: "21.0.0-rc.3+sha-9723748",
   ngImport: i0,
   type: Field,
   decorators: [{
