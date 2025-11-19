b0315d34cd3de1f442027650b358f25da333bd1f
Joey Perrott <josephperrott@gmail.com>
2025-11-19 21:38:54 +0000

    4e14a53 docs: add release blog to v21 page

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-6854776
+ * @license Angular v21.0.0+sha-4e14a53
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-6854776",
+    version: "21.0.0+sha-4e14a53",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0+sha-6854776",
+    version: "21.0.0+sha-4e14a53",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-6854776",
+  version: "21.0.0+sha-4e14a53",
   ngImport: i0,
   type: Field,
   decorators: [{
