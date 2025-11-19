eaffef395bad85ea15efdeda00ccdfebaa2f1a87
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:30:37 +0000

    ac34824 fix(docs-infra): add moduleResolution to TypeScript compiler options for playground

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-e69855a
+ * @license Angular v21.0.0-rc.3+sha-ac34824
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0-rc.3+sha-e69855a",
+    version: "21.0.0-rc.3+sha-ac34824",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0-rc.3+sha-e69855a",
+    version: "21.0.0-rc.3+sha-ac34824",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0-rc.3+sha-e69855a",
+  version: "21.0.0-rc.3+sha-ac34824",
   ngImport: i0,
   type: Field,
   decorators: [{
