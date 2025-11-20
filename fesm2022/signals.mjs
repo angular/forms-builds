55b29f4cead7cdf04605f1f38cbb8fdbfd961bcb
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 16:50:31 +0000

    183d194 docs: add 20 to 21 update guide

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29185a8
+ * @license Angular v21.1.0-next.0+sha-183d194
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-29185a8",
+    version: "21.1.0-next.0+sha-183d194",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-29185a8",
+    version: "21.1.0-next.0+sha-183d194",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-29185a8",
+  version: "21.1.0-next.0+sha-183d194",
   ngImport: i0,
   type: Field,
   decorators: [{
