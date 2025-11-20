ce13562136e25f993ebdc13acf88d3e297892a9f
kirjs <kirts@google.com>
2025-11-20 22:07:18 +0000

    70fb1d6 ci: try to resolve vscode extension e2e flakes

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-da97099
+ * @license Angular v21.0.0+sha-70fb1d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-da97099",
+    version: "21.0.0+sha-70fb1d6",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0+sha-da97099",
+    version: "21.0.0+sha-70fb1d6",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-da97099",
+  version: "21.0.0+sha-70fb1d6",
   ngImport: i0,
   type: Field,
   decorators: [{
