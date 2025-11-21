3bcca5d18892c2c24260fc81ffcd65f698cbcaee
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 16:00:07 +0000

    68f5cee docs: fix incorrect Signal Forms link

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b7d91a7
+ * @license Angular v21.1.0-next.0+sha-68f5cee
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-b7d91a7",
+    version: "21.1.0-next.0+sha-68f5cee",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-b7d91a7",
+    version: "21.1.0-next.0+sha-68f5cee",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-b7d91a7",
+  version: "21.1.0-next.0+sha-68f5cee",
   ngImport: i0,
   type: Field,
   decorators: [{
