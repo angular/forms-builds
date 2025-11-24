1efe41a3868be0392253a40b073889753907a4f7
kirjs <kirts@google.com>
2025-11-24 15:29:10 +0000

    1f20b54 docs(docs-infra): simplify tab selection

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-2d658b3
+ * @license Angular v21.0.0+sha-1f20b54
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -381,7 +381,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-2d658b3",
+    version: "21.0.0+sha-1f20b54",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -389,7 +389,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0+sha-2d658b3",
+    version: "21.0.0+sha-1f20b54",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -414,7 +414,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-2d658b3",
+  version: "21.0.0+sha-1f20b54",
   ngImport: i0,
   type: Field,
   decorators: [{
