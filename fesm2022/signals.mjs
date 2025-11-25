8ff68547db3de0470821fc8a2d89f299cc962884
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:29:34 +0000

    4c0ba06 release: cut the v21.1.0-next.0 release

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-6aaddba
+ * @license Angular v21.1.0-next.0+sha-4c0ba06
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -400,7 +400,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-6aaddba",
+    version: "21.1.0-next.0+sha-4c0ba06",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -408,7 +408,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-6aaddba",
+    version: "21.1.0-next.0+sha-4c0ba06",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -433,7 +433,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-6aaddba",
+  version: "21.1.0-next.0+sha-4c0ba06",
   ngImport: i0,
   type: Field,
   decorators: [{
