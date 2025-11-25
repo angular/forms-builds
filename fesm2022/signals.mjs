5db310207bd841590e86f3f57df17617d3efabdd
kirjs <kirts@google.com>
2025-11-25 18:51:54 +0000

    3240d85 fix(http): prevent XSRF token leakage to protocol-relative URLs

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-908b5a4
+ * @license Angular v21.0.0+sha-3240d85
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -400,7 +400,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.0+sha-908b5a4",
+    version: "21.0.0+sha-3240d85",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -408,7 +408,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.0+sha-908b5a4",
+    version: "21.0.0+sha-3240d85",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -433,7 +433,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.0+sha-908b5a4",
+  version: "21.0.0+sha-3240d85",
   ngImport: i0,
   type: Field,
   decorators: [{
