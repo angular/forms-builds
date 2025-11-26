ce9bd0e46ff03c64f95c7b26096434a3679dbfeb
kirjs <kirts@google.com>
2025-11-26 18:47:19 +0000

    4207d89 docs(docs-infra): fix visible lines/regions

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-1260d1a
+ * @license Angular v21.0.1+sha-4207d89
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -400,7 +400,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.1+sha-1260d1a",
+    version: "21.0.1+sha-4207d89",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -408,7 +408,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.1+sha-1260d1a",
+    version: "21.0.1+sha-4207d89",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -433,7 +433,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.1+sha-1260d1a",
+  version: "21.0.1+sha-4207d89",
   ngImport: i0,
   type: Field,
   decorators: [{
