bf896706b3b0a3bec6b16a0da4b37b76ce9b9263
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:52:02 +0000

    76d097e docs: release notes for the v20.3.15 release

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-ee53e6f
+ * @license Angular v21.1.0-next.0+sha-76d097e
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -400,7 +400,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-ee53e6f",
+    version: "21.1.0-next.0+sha-76d097e",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -408,7 +408,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-ee53e6f",
+    version: "21.1.0-next.0+sha-76d097e",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -433,7 +433,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-ee53e6f",
+  version: "21.1.0-next.0+sha-76d097e",
   ngImport: i0,
   type: Field,
   decorators: [{
