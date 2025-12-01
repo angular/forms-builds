56996ff71d8e4a87d497efa3a05cec39a1c882c5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:24:35 +0000

    8ab7594 docs: format typescript examples and replace <docs-code> with fenced code blocks

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-ed32e8c
+ * @license Angular v21.0.2+sha-8ab7594
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -400,7 +400,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-ed32e8c",
+    version: "21.0.2+sha-8ab7594",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -408,7 +408,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.2+sha-ed32e8c",
+    version: "21.0.2+sha-8ab7594",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -433,7 +433,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-ed32e8c",
+  version: "21.0.2+sha-8ab7594",
   ngImport: i0,
   type: Field,
   decorators: [{
