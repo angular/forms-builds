27f5c579b8f010f35294d27f7040e498c533960b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 18:16:45 +0000

    c2a8934 Revert "refactor(router): compress synchronous end to router navigation to single operator"

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-5a80a48
+ * @license Angular v21.0.2+sha-c2a8934
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -394,7 +394,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-5a80a48",
+    version: "21.0.2+sha-c2a8934",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -402,7 +402,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.2+sha-5a80a48",
+    version: "21.0.2+sha-c2a8934",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -427,7 +427,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-5a80a48",
+  version: "21.0.2+sha-c2a8934",
   ngImport: i0,
   type: Field,
   decorators: [{
