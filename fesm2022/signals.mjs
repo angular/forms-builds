6237ec6a04329645a93e1255c0e57c5a52be13a4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 09:42:21 +0000

    893fbdf docs: update syntax highlighting in component documentation

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-706d762
+ * @license Angular v21.0.2+sha-893fbdf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -400,7 +400,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.2+sha-706d762",
+    version: "21.0.2+sha-893fbdf",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -408,7 +408,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.2+sha-706d762",
+    version: "21.0.2+sha-893fbdf",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -433,7 +433,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.2+sha-706d762",
+  version: "21.0.2+sha-893fbdf",
   ngImport: i0,
   type: Field,
   decorators: [{
