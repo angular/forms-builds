3d58b649f35eb2838a87e1f666ad5f642459086c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 15:34:55 +0000

    7f8ffa3 fix(docs-infra): prevent readonly TypeScript keyword from being linked

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-13c0d45
+ * @license Angular v21.1.0-next.1+sha-7f8ffa3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -398,7 +398,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-13c0d45",
+    version: "21.1.0-next.1+sha-7f8ffa3",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -406,7 +406,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.1+sha-13c0d45",
+    version: "21.1.0-next.1+sha-7f8ffa3",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -431,7 +431,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.1+sha-13c0d45",
+  version: "21.1.0-next.1+sha-7f8ffa3",
   ngImport: i0,
   type: Field,
   decorators: [{
