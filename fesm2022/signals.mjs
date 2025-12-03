7f11111d14112de9e7a505d19395497d44b6d1ee
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 09:18:02 +0000

    e26aa86 build: update bazel dependencies

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29223c4
+ * @license Angular v21.1.0-next.0+sha-e26aa86
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -394,7 +394,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-29223c4",
+    version: "21.1.0-next.0+sha-e26aa86",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -402,7 +402,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-29223c4",
+    version: "21.1.0-next.0+sha-e26aa86",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -427,7 +427,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-29223c4",
+  version: "21.1.0-next.0+sha-e26aa86",
   ngImport: i0,
   type: Field,
   decorators: [{
