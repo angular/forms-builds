60952a08c604c3f896217e9e0ff23bc8953115d0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 13:11:52 +0000

    d608784 build: update all github actions

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-dfd7d3b
+ * @license Angular v21.1.0-next.0+sha-d608784
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -394,7 +394,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-dfd7d3b",
+    version: "21.1.0-next.0+sha-d608784",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -402,7 +402,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-dfd7d3b",
+    version: "21.1.0-next.0+sha-d608784",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -427,7 +427,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-dfd7d3b",
+  version: "21.1.0-next.0+sha-d608784",
   ngImport: i0,
   type: Field,
   decorators: [{
