fb40ceafada8786fef34b21d39011cebffa9485b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:10:23 +0000

    13353bf Revert "build: update cross-repo angular dependencies"

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e26aa86
+ * @license Angular v21.1.0-next.0+sha-13353bf
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -394,7 +394,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-e26aa86",
+    version: "21.1.0-next.0+sha-13353bf",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -402,7 +402,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.0+sha-e26aa86",
+    version: "21.1.0-next.0+sha-13353bf",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -427,7 +427,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-e26aa86",
+  version: "21.1.0-next.0+sha-13353bf",
   ngImport: i0,
   type: Field,
   decorators: [{
