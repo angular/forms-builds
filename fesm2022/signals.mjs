638e481841c8a572facf50e9b202e2524c41ad9f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:45:33 +0000

    c63ef9c docs: fix typos and clarify root effect

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-af6c01f
+ * @license Angular v21.1.0-next.1+sha-c63ef9c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.1+sha-af6c01f",
+    version: "21.1.0-next.1+sha-c63ef9c",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.1+sha-af6c01f",
+    version: "21.1.0-next.1+sha-c63ef9c",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.1+sha-af6c01f",
+  version: "21.1.0-next.1+sha-c63ef9c",
   ngImport: i0,
   type: Field,
   decorators: [{
