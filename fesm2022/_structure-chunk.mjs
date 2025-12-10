5b1041bee3edd9bfc797863a60507439f9d83f3b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 18:13:26 +0000

    2dd971b docs: hide menu icons from screen readers in aria example

--- fesm2022/_structure-chunk.mjs
+++ fesm2022/_structure-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-834593b
+ * @license Angular v21.0.3+sha-2dd971b
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1281,7 +1281,7 @@ class FieldNode {
     untracked(() => this._reset(value));
   }
   _reset(value) {
-    if (value) {
+    if (value !== undefined) {
       this.value.set(value);
     }
     this.nodeState.markAsUntouched();
