5fd0e74e0e079dbe7f2f67dbc0edf84d2d7dda80
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:53:16 +0000

    fa4e6e6 release: cut the v21.0.4 release

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.3+sha-c937da4
+ * @license Angular v21.0.4+sha-fa4e6e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.3+sha-c937da4",
+    version: "21.0.4+sha-fa4e6e6",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.3+sha-c937da4",
+    version: "21.0.4+sha-fa4e6e6",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.3+sha-c937da4",
+  version: "21.0.4+sha-fa4e6e6",
   ngImport: i0,
   type: Field,
   decorators: [{
