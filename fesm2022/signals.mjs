d0d7508676e68f9bc1b49f65ece75fe5a1ab209b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 01:22:50 +0000

    87d20fd build: update cross-repo angular dependencies

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-25f8007
+ * @license Angular v21.0.5+sha-87d20fd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-25f8007",
+    version: "21.0.5+sha-87d20fd",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.5+sha-25f8007",
+    version: "21.0.5+sha-87d20fd",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.5+sha-25f8007",
+  version: "21.0.5+sha-87d20fd",
   ngImport: i0,
   type: Field,
   decorators: [{
