9935bfd145d8ea6c9c07e802fa1347a2084f43db
Andrew Kushnir <akushnir@google.com>
2025-12-15 19:40:28 +0000

    0dd5417 docs: format CLI builder JSON examples using fenced blocks

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-15fd24d
+ * @license Angular v21.0.5+sha-0dd5417
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-15fd24d",
+    version: "21.0.5+sha-0dd5417",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.5+sha-15fd24d",
+    version: "21.0.5+sha-0dd5417",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.5+sha-15fd24d",
+  version: "21.0.5+sha-0dd5417",
   ngImport: i0,
   type: Field,
   decorators: [{
