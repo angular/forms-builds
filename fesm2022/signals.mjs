66200e328b341043a5047c5b953d0b0be4a2599e
Andrew Kushnir <akushnir@google.com>
2025-12-16 17:33:25 +0000

    61a4648 docs: update links to guides in signal forms overview

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-c250bd0
+ * @license Angular v21.0.5+sha-61a4648
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-c250bd0",
+    version: "21.0.5+sha-61a4648",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.5+sha-c250bd0",
+    version: "21.0.5+sha-61a4648",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.5+sha-c250bd0",
+  version: "21.0.5+sha-61a4648",
   ngImport: i0,
   type: Field,
   decorators: [{
