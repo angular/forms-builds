051623779c2b2bf16940ffcd67026840b358d235
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:18:57 +0000

    868cb7c build: migrate Bazel ignore rules from .bazelignore to REPO.bazel

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-2e58b66
+ * @license Angular v21.1.0-next.3+sha-868cb7c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-2e58b66",
+    version: "21.1.0-next.3+sha-868cb7c",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.3+sha-2e58b66",
+    version: "21.1.0-next.3+sha-868cb7c",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-2e58b66",
+  version: "21.1.0-next.3+sha-868cb7c",
   ngImport: i0,
   type: Field,
   decorators: [{
