f4e87b85114ab0d757bcdaabb23277926a76cb28
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 17:39:16 +0000

    f516370 fix(core): use mutable ResponseInit type for RESPONSE_INIT token

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-9fa77af
+ * @license Angular v21.1.0-next.3+sha-f516370
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.3+sha-9fa77af",
+    version: "21.1.0-next.3+sha-f516370",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.3+sha-9fa77af",
+    version: "21.1.0-next.3+sha-f516370",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.3+sha-9fa77af",
+  version: "21.1.0-next.3+sha-f516370",
   ngImport: i0,
   type: Field,
   decorators: [{
