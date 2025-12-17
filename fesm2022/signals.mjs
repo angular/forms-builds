0ec1931c4cd9aa75079a8f0cf4131f91808479e2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 22:30:56 +0000

    ad88c72 docs: fix incorrect line highlighting in tutorials

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-664fa39
+ * @license Angular v21.1.0-next.4+sha-ad88c72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.4+sha-664fa39",
+    version: "21.1.0-next.4+sha-ad88c72",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-next.4+sha-664fa39",
+    version: "21.1.0-next.4+sha-ad88c72",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.4+sha-664fa39",
+  version: "21.1.0-next.4+sha-ad88c72",
   ngImport: i0,
   type: Field,
   decorators: [{
