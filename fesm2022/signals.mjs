3a9eb535c898b5c997db5f76a40d57659788e9ab
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:04:52 +0000

    a27bdd3 build: update bazel dependencies

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-3b3b982
+ * @license Angular v21.0.5+sha-a27bdd3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.5+sha-3b3b982",
+    version: "21.0.5+sha-a27bdd3",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.5+sha-3b3b982",
+    version: "21.0.5+sha-a27bdd3",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.5+sha-3b3b982",
+  version: "21.0.5+sha-a27bdd3",
   ngImport: i0,
   type: Field,
   decorators: [{
