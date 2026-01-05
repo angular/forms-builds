d508c3d6e72a047d23090aa24ac0508686c084b4
kirjs <kirts@google.com>
2026-01-05 17:07:14 +0000

    73a3fd5 docs: add section on converting writable signals to readonly

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-6c911ad
+ * @license Angular v21.0.6+sha-73a3fd5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class Field {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.6+sha-6c911ad",
+    version: "21.0.6+sha-73a3fd5",
     ngImport: i0,
     type: Field,
     deps: [],
@@ -144,7 +144,7 @@ class Field {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.6+sha-6c911ad",
+    version: "21.0.6+sha-73a3fd5",
     type: Field,
     isStandalone: true,
     selector: "[field]",
@@ -169,7 +169,7 @@ class Field {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.6+sha-6c911ad",
+  version: "21.0.6+sha-73a3fd5",
   ngImport: i0,
   type: Field,
   decorators: [{
