d487de85617dd975fba1004f976146203bb8d834
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 16:36:33 +0000

    559797c docs: update hierarchical-dependency-injection.md

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-76a6984
+ * @license Angular v21.1.0-rc.0+sha-559797c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-76a6984",
+    version: "21.1.0-rc.0+sha-559797c",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -144,7 +144,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-rc.0+sha-76a6984",
+    version: "21.1.0-rc.0+sha-559797c",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -169,7 +169,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-rc.0+sha-76a6984",
+  version: "21.1.0-rc.0+sha-559797c",
   ngImport: i0,
   type: FormField,
   decorators: [{
