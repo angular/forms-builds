ee7d8cd0e9ecc1dece3d3a173395084c0ff4c007
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:06:04 +0000

    5594a53 docs(docs-infra): use Signals Forms focusBoundControl

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-6d3b093
+ * @license Angular v21.1.0-rc.0+sha-5594a53
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-rc.0+sha-6d3b093",
+    version: "21.1.0-rc.0+sha-5594a53",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -144,7 +144,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.0-rc.0+sha-6d3b093",
+    version: "21.1.0-rc.0+sha-5594a53",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -169,7 +169,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-rc.0+sha-6d3b093",
+  version: "21.1.0-rc.0+sha-5594a53",
   ngImport: i0,
   type: FormField,
   decorators: [{
