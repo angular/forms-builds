5762df91d3b092b17877de374dad02c30912919d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 19:34:48 +0000

    147ea13 release: cut the v21.0.9 release

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-79273c1
+ * @license Angular v21.0.9+sha-147ea13
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -136,7 +136,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.8+sha-79273c1",
+    version: "21.0.9+sha-147ea13",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -144,7 +144,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.0.8+sha-79273c1",
+    version: "21.0.9+sha-147ea13",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -169,7 +169,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.8+sha-79273c1",
+  version: "21.0.9+sha-147ea13",
   ngImport: i0,
   type: FormField,
   decorators: [{
