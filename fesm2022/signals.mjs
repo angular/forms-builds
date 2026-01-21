7c6b02f7bccd79241ef29dbf10ba2ff563468a4b
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 19:44:30 +0000

    f0b1061 docs(docs-infra): Handle additional description format

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-b885851
+ * @license Angular v21.2.0-next.0+sha-f0b1061
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -151,7 +151,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-b885851",
+    version: "21.2.0-next.0+sha-f0b1061",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -159,7 +159,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-next.0+sha-b885851",
+    version: "21.2.0-next.0+sha-f0b1061",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -184,7 +184,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-b885851",
+  version: "21.2.0-next.0+sha-f0b1061",
   ngImport: i0,
   type: FormField,
   decorators: [{
