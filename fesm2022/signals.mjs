fc5e5c4406d091fd2664e3044a0884f668933990
Andrew Scott <atscott01@gmail.com>
2026-01-23 18:48:15 +0000

    b203ab0 build: update bazel dependencies

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.1+sha-f7eb996
+ * @license Angular v21.1.1+sha-b203ab0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -151,7 +151,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.1+sha-f7eb996",
+    version: "21.1.1+sha-b203ab0",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -159,7 +159,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.1.1+sha-f7eb996",
+    version: "21.1.1+sha-b203ab0",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -184,7 +184,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.1+sha-f7eb996",
+  version: "21.1.1+sha-b203ab0",
   ngImport: i0,
   type: FormField,
   decorators: [{
