d596a1526d391e8db9a2751d6d2dfc39cb4e6122
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:05:13 +0000

    ceefceb docs: normalize spacing in animation example boxes

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-c921786
+ * @license Angular v21.2.0-next.0+sha-ceefceb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -164,7 +164,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.0+sha-c921786",
+    version: "21.2.0-next.0+sha-ceefceb",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -172,7 +172,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-next.0+sha-c921786",
+    version: "21.2.0-next.0+sha-ceefceb",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -198,7 +198,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.0+sha-c921786",
+  version: "21.2.0-next.0+sha-ceefceb",
   ngImport: i0,
   type: FormField,
   decorators: [{
