b7cfca22401d4836123f8a6cad3b4779723f4259
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:15:34 +0000

    907a94d feat(router): Update `IsActiveMatchOptions` APIs to accept a Partial

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-0e04233
+ * @license Angular v21.2.0-next.1+sha-907a94d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -164,7 +164,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-0e04233",
+    version: "21.2.0-next.1+sha-907a94d",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -172,7 +172,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-next.1+sha-0e04233",
+    version: "21.2.0-next.1+sha-907a94d",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -198,7 +198,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.1+sha-0e04233",
+  version: "21.2.0-next.1+sha-907a94d",
   ngImport: i0,
   type: FormField,
   decorators: [{
