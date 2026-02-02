b02501a4e007e1d5513a16427aa186a000d3d143
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 19:16:26 +0000

    8ab433a fix(core): export DirectiveWithBindings

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-a64a90d
+ * @license Angular v21.2.0-next.1+sha-8ab433a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -502,7 +502,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.1+sha-a64a90d",
+    version: "21.2.0-next.1+sha-8ab433a",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -510,7 +510,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-next.1+sha-a64a90d",
+    version: "21.2.0-next.1+sha-8ab433a",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -539,7 +539,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.1+sha-a64a90d",
+  version: "21.2.0-next.1+sha-8ab433a",
   ngImport: i0,
   type: FormField,
   decorators: [{
