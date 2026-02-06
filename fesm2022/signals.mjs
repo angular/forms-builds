33ab5623352e00acd9ecf0842660d062f23ca404
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 18:25:05 +0000

    460dbbb docs: remove non-existant method

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-15c71fb
+ * @license Angular v21.2.0-next.2+sha-460dbbb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -494,7 +494,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-next.2+sha-15c71fb",
+    version: "21.2.0-next.2+sha-460dbbb",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -502,7 +502,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-next.2+sha-15c71fb",
+    version: "21.2.0-next.2+sha-460dbbb",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -531,7 +531,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-next.2+sha-15c71fb",
+  version: "21.2.0-next.2+sha-460dbbb",
   ngImport: i0,
   type: FormField,
   decorators: [{
