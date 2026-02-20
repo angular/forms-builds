b878f965ec6038c165d8cd8b608a7c02fadc0144
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 00:11:07 +0000

    9ee99c7 docs: update NgTemplateOutlet documentation with injector

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-5b8a403
+ * @license Angular v21.2.0-rc.0+sha-9ee99c7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1020,7 +1020,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-rc.0+sha-5b8a403",
+    version: "21.2.0-rc.0+sha-9ee99c7",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -1028,7 +1028,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-rc.0+sha-5b8a403",
+    version: "21.2.0-rc.0+sha-9ee99c7",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -1060,7 +1060,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-rc.0+sha-5b8a403",
+  version: "21.2.0-rc.0+sha-9ee99c7",
   ngImport: i0,
   type: FormField,
   decorators: [{
@@ -1105,7 +1105,7 @@ class FormRoot {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-rc.0+sha-5b8a403",
+    version: "21.2.0-rc.0+sha-9ee99c7",
     ngImport: i0,
     type: FormRoot,
     deps: [],
@@ -1113,7 +1113,7 @@ class FormRoot {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-rc.0+sha-5b8a403",
+    version: "21.2.0-rc.0+sha-9ee99c7",
     type: FormRoot,
     isStandalone: true,
     selector: "form[formRoot]",
@@ -1139,7 +1139,7 @@ class FormRoot {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-rc.0+sha-5b8a403",
+  version: "21.2.0-rc.0+sha-9ee99c7",
   ngImport: i0,
   type: FormRoot,
   decorators: [{
