376a9e723cbc20bf3aadbab32a779a85234e5698
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:21:33 +0000

    30e486a fix(docs-infra): use shared heading ID generation logic

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-a73d543
+ * @license Angular v21.2.0-rc.0+sha-30e486a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1030,7 +1030,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-rc.0+sha-a73d543",
+    version: "21.2.0-rc.0+sha-30e486a",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -1038,7 +1038,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-rc.0+sha-a73d543",
+    version: "21.2.0-rc.0+sha-30e486a",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -1070,7 +1070,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-rc.0+sha-a73d543",
+  version: "21.2.0-rc.0+sha-30e486a",
   ngImport: i0,
   type: FormField,
   decorators: [{
@@ -1115,7 +1115,7 @@ class FormRoot {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.0-rc.0+sha-a73d543",
+    version: "21.2.0-rc.0+sha-30e486a",
     ngImport: i0,
     type: FormRoot,
     deps: [],
@@ -1123,7 +1123,7 @@ class FormRoot {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "21.2.0-rc.0+sha-a73d543",
+    version: "21.2.0-rc.0+sha-30e486a",
     type: FormRoot,
     isStandalone: true,
     selector: "form[formRoot]",
@@ -1149,7 +1149,7 @@ class FormRoot {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.0-rc.0+sha-a73d543",
+  version: "21.2.0-rc.0+sha-30e486a",
   ngImport: i0,
   type: FormRoot,
   decorators: [{
