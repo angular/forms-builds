17ed8b3a850b1fcdd100937a22886724cccc3e52
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 16:09:02 +0000

    957466d docs(docs-infra): scroll home features section into view on tab change

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-01a1795
+ * @license Angular v22.0.0-next.0+sha-957466d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1030,7 +1030,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-01a1795",
+    version: "22.0.0-next.0+sha-957466d",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -1038,7 +1038,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.0+sha-01a1795",
+    version: "22.0.0-next.0+sha-957466d",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -1070,7 +1070,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-01a1795",
+  version: "22.0.0-next.0+sha-957466d",
   ngImport: i0,
   type: FormField,
   decorators: [{
@@ -1115,7 +1115,7 @@ class FormRoot {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.0+sha-01a1795",
+    version: "22.0.0-next.0+sha-957466d",
     ngImport: i0,
     type: FormRoot,
     deps: [],
@@ -1123,7 +1123,7 @@ class FormRoot {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.0+sha-01a1795",
+    version: "22.0.0-next.0+sha-957466d",
     type: FormRoot,
     isStandalone: true,
     selector: "form[formRoot]",
@@ -1149,7 +1149,7 @@ class FormRoot {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.0+sha-01a1795",
+  version: "22.0.0-next.0+sha-957466d",
   ngImport: i0,
   type: FormRoot,
   decorators: [{
