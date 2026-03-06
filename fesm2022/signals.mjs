4ba1cfe742f919e2798705acfb17a191b11af44a
Andrew Scott <atscott01@gmail.com>
2026-03-06 18:26:23 +0000

    cc64def test(language-service): add inlay hint regression and integration coverage

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-c19066c
+ * @license Angular v22.0.0-next.1+sha-cc64def
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1051,7 +1051,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.1+sha-c19066c",
+    version: "22.0.0-next.1+sha-cc64def",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -1059,7 +1059,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.1+sha-c19066c",
+    version: "22.0.0-next.1+sha-cc64def",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -1091,7 +1091,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.1+sha-c19066c",
+  version: "22.0.0-next.1+sha-cc64def",
   ngImport: i0,
   type: FormField,
   decorators: [{
@@ -1136,7 +1136,7 @@ class FormRoot {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.1+sha-c19066c",
+    version: "22.0.0-next.1+sha-cc64def",
     ngImport: i0,
     type: FormRoot,
     deps: [],
@@ -1144,7 +1144,7 @@ class FormRoot {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.1+sha-c19066c",
+    version: "22.0.0-next.1+sha-cc64def",
     type: FormRoot,
     isStandalone: true,
     selector: "form[formRoot]",
@@ -1170,7 +1170,7 @@ class FormRoot {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.1+sha-c19066c",
+  version: "22.0.0-next.1+sha-cc64def",
   ngImport: i0,
   type: FormRoot,
   decorators: [{
