66c0a65e16437efb216a8868062358b305e4a4f2
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 22:41:09 +0000

    89c9a4d feat(router): Add `options` optional parameter for `withComponentInputBinding`

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-8edcf41
+ * @license Angular v22.0.0-next.3+sha-89c9a4d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1051,7 +1051,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.3+sha-8edcf41",
+    version: "22.0.0-next.3+sha-89c9a4d",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -1059,7 +1059,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.3+sha-8edcf41",
+    version: "22.0.0-next.3+sha-89c9a4d",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -1091,7 +1091,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.3+sha-8edcf41",
+  version: "22.0.0-next.3+sha-89c9a4d",
   ngImport: i0,
   type: FormField,
   decorators: [{
@@ -1136,7 +1136,7 @@ class FormRoot {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.3+sha-8edcf41",
+    version: "22.0.0-next.3+sha-89c9a4d",
     ngImport: i0,
     type: FormRoot,
     deps: [],
@@ -1144,7 +1144,7 @@ class FormRoot {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.3+sha-8edcf41",
+    version: "22.0.0-next.3+sha-89c9a4d",
     type: FormRoot,
     isStandalone: true,
     selector: "form[formRoot]",
@@ -1170,7 +1170,7 @@ class FormRoot {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.3+sha-8edcf41",
+  version: "22.0.0-next.3+sha-89c9a4d",
   ngImport: i0,
   type: FormRoot,
   decorators: [{
