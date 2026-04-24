fa81a5e6fe3fc6452b07d89944e9bffa84b391e7
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 20:37:07 +0000

    ac9babe docs: update cross-repo adev docs

--- fesm2022/signals.mjs
+++ fesm2022/signals.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-29081f7
+ * @license Angular v22.0.0-next.9+sha-ac9babe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -948,7 +948,7 @@ function nativeControlCreate(host, parent, parseErrorsSource, validityMonitor) {
 class InputValidityMonitor {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     ngImport: i0,
     type: InputValidityMonitor,
     deps: [],
@@ -956,7 +956,7 @@ class InputValidityMonitor {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     ngImport: i0,
     type: InputValidityMonitor,
     providedIn: 'root',
@@ -965,7 +965,7 @@ class InputValidityMonitor {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.9+sha-29081f7",
+  version: "22.0.0-next.9+sha-ac9babe",
   ngImport: i0,
   type: InputValidityMonitor,
   decorators: [{
@@ -1028,7 +1028,7 @@ class AnimationInputValidityMonitor extends InputValidityMonitor {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     ngImport: i0,
     type: AnimationInputValidityMonitor,
     deps: null,
@@ -1036,14 +1036,14 @@ class AnimationInputValidityMonitor extends InputValidityMonitor {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     ngImport: i0,
     type: AnimationInputValidityMonitor
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.9+sha-29081f7",
+  version: "22.0.0-next.9+sha-ac9babe",
   ngImport: i0,
   type: AnimationInputValidityMonitor,
   decorators: [{
@@ -1198,7 +1198,7 @@ class FormField {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     ngImport: i0,
     type: FormField,
     deps: [],
@@ -1206,7 +1206,7 @@ class FormField {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     type: FormField,
     isStandalone: true,
     selector: "[formField]",
@@ -1238,7 +1238,7 @@ class FormField {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.9+sha-29081f7",
+  version: "22.0.0-next.9+sha-ac9babe",
   ngImport: i0,
   type: FormField,
   decorators: [{
@@ -1289,7 +1289,7 @@ class FormRoot {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     ngImport: i0,
     type: FormRoot,
     deps: [],
@@ -1297,7 +1297,7 @@ class FormRoot {
   });
   static ɵdir = i0.ɵɵngDeclareDirective({
     minVersion: "17.1.0",
-    version: "22.0.0-next.9+sha-29081f7",
+    version: "22.0.0-next.9+sha-ac9babe",
     type: FormRoot,
     isStandalone: true,
     selector: "form[formRoot]",
@@ -1323,7 +1323,7 @@ class FormRoot {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.9+sha-29081f7",
+  version: "22.0.0-next.9+sha-ac9babe",
   ngImport: i0,
   type: FormRoot,
   decorators: [{
